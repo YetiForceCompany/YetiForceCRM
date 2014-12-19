dcf704b8d65b688bf42570204b173396d2fdf342
Michał Janicki <admin@Jabko.local>
2014-12-19 16:59:18 +0100

    #17

--- yetiforce_crm/trunk/modules/OSSPdf/special_functions/example.php
+++ yetiforce_crm/trunk/modules/OSSPdf/special_functions/example.php
@@ -1,5 +1,6 @@
 <?php
-/*+***********************************************************************************************************************************
+
+/* +***********************************************************************************************************************************
  * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
  * in compliance with the License.
  * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
@@ -7,27 +8,26 @@
  * The Original Code is YetiForce.
  * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
  * All Rights Reserved.
- *************************************************************************************************************************************/
-$permitted_modules = array( 'all' );
+ * *********************************************************************************************************************************** */
+$permitted_modules = array('all');
+
 /// ZNACZNIK WYWOLUJACY TE FUNKCJE => #special_function#example#end_special_function#
 /// funkcja MUSI miec taką samą nazwe jak PLIK
-function example( $module, $id, $templateid ,$content, $tcpdf) {
-{
-        $db =  PearDatabase::getInstance();
-	/// przykladowe zapytanie
-	$zapytanie = $db->query( "select accountname from vtiger_account LIMIT 5", true );
+
+function example($module, $id, $templateid, $content, $tcpdf) {
 	
-	$tresc = '';
+	$db = PearDatabase::getInstance();
+	/// przykladowe zapytanie
+	$sql = $db->query("select accountname from vtiger_account LIMIT 5", true);
+	
 	//poczatek tabeli
-	$tresc .= '<br/><table border="1">';
+	$content = '<br/><table border="1">';
 	// dla pobranych zapytaniem rekordów - tworz kolejne wiersze
-	for( $i = 0; $i < $db->num_rows( $zapytanie ); $i++ )
-	{
-		$tresc .= '<tr><td align="center"> Nazwa </td><td>'.$db->query_result( $zapytanie, $i, "accountname" ).'</td></tr>';
+	for ($i = 0; $i < $db->num_rows($sql); $i++) {
+		$content .= '<tr><td align="center"> Nazwa </td><td>' . $db->query_result($sql, $i, "accountname") . '</td></tr>';
 	}
 	//koniec tabeli
-	$tresc .= '</table><br/>';
+	$content .= '</table><br/>';
 	//zwracamy wynik
-	return $tresc;
+	return $content;
 }
-?>
