d11890b6fee7c3aca6dd11e752b94255b493ec3c
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2014-12-22 14:27:33 +0100

    Change version number

--- config/version.php
+++ config/version.php
@@ -9,6 +9,6 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
-$patch_version = '20141219';  
-$YetiForce_current_version = '1.0.20 RC';
+$patch_version = '2014122';  
+$YetiForce_current_version = '1.0.21 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
