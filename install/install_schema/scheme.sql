5caba3001e17c02c608fa2767e5500014e0a12f5
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2014-12-23 13:21:47 +0100

    Add a template email to calendar invitations

--- install/install_schema/scheme.sql
+++ install/install_schema/scheme.sql
@@ -157,7 +157,7 @@ CREATE TABLE `com_vtiger_workflowtasks` (
   `task` text,
   PRIMARY KEY (`task_id`),
   UNIQUE KEY `com_vtiger_workflowtasks_idx` (`task_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=122 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=123 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `com_vtiger_workflowtasks_entitymethod` */
 
