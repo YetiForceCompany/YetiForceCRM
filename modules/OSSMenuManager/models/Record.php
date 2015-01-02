227ea9fce9d9644453e6cc5773437129e6a07e99
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2015-01-02 12:08:03 +0100

    Improvement menu breadcrumbs

--- modules/OSSMenuManager/models/Record.php
+++ modules/OSSMenuManager/models/Record.php
@@ -172,7 +172,7 @@ class OSSMenuManager_Record_Model extends Vtiger_Record_Model {
 									$breadcrumbs[] = array('lable' => $recordLabel );
 								}
 							}
-						}else{
+						}elseif( $request->get('module') != '' ){
 							$parts = parse_url($url);
 							parse_str($parts['query'], $query);
 							if( $request->get('module') == $query['module'] && $request->get('view') == $query['view'] && $request->get('viewname') == $query['viewname'] ){
@@ -188,7 +188,7 @@ class OSSMenuManager_Record_Model extends Vtiger_Record_Model {
 				$menuStructureGroupe[$name]['picon'] = $locationicon;			
 				$menuStructureGroupe[$name]['icons'] = $sizeicon_second;
             }
-			if(count($breadcrumbs) == 0){
+			if(count($breadcrumbs) == 0 && $request->get('module') != ''){
 				$breadcrumbs[] = array('lable' => vtranslate($request->get('module'), $request->get('module')), 'url' => 'index.php?module='.$request->get('module').'&view=List');
 				if ( $request->get('view') == 'Edit' && $request->get('record') == '' ) {
 					$breadcrumbs[] = array('lable' => vtranslate('LBL_VIEW_CREATE', $request->get('module')) );
