eee1d641eab706ce919020ec37cdc666e7468712
WAW555 <waw555@yandex.ru>
2015-01-08 07:27:34 +0500

    Add Russian Language for Home Module

---
+++ languages/ru_ru/Home.php
@@ -0,0 +1,30 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+$languageStrings = array(
+	'ALVT'                         => 'Последние Контрагенты',
+	'CVLVT'                        => 'Ключевые Показатели',
+	'GRT'                          => 'Положение Моей Группы',
+	'HDB'                          => 'Панель Главной Страницы',
+	'HLT'                          => 'Топ Билеты Поддержка'         , 
+	'ILTI'                         => 'Последние Счета',
+	'Key Metrics'                  => 'Ключевые показатели',
+	'LTFAQ'                        => 'Мои Частые ЧаВо',
+	'OLTSO'                        => 'Последние Заказы',
+	'PA'                           => 'События в процессе',
+	'PLVT'                         => 'Последние Сделки',
+	'QLTQ'                         => 'Последние Предложения',
+	'UA'                           => 'Ближайшие События',
+    'Home'                         => 'Главная',
+    'OLTPO'                        => 'Последние Закупки',
+    'Tag Cloud'                    => 'Облако Тегов',
+);
+
+$jsLanguageStrings = array(
+);
