65ba9b3442e040f3a294cbb67ede13f6cf306170
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2015-01-08 21:41:42 +0100

    Renaming fields

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150108';  
-$YetiForce_current_version = '1.0.41 RC';
+$YetiForce_current_version = '1.0.42 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
