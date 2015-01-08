93cc70057097020baa9b04076c9922af4c1927c7
WAW555 <waw555@yandex.ru>
2015-01-09 00:59:14 +0500

    Add Russian Language

---
+++ languages/ru_ru/OSSDocumentControl.php
@@ -0,0 +1,76 @@
+<?php
+/*+***********************************************************************************************************************************
+ * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * See the License for the specific language governing rights and limitations under the License.
+ * The Original Code is YetiForce.
+ * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
+ * All Rights Reserved.
+ * Contributor(s): 
+ *************************************************************************************************************************************/
+$languageStrings = array(
+	'LBL_VIEW_EDIT2'	=>	'Добавление условий',
+	'LBL_NEW_TPL'	=>	'Добавить документ',
+	'OSSDocumentControl'	=>	'Менеджер документов',
+	'LBL_MODULE_NAME'	=>	'Название модуля',
+	'LBL_NO_PROJECT_TPL_ADDED'	=>	'Документов нет',
+	'TPL'	=>	'Документы',
+	'DELETE_MODULE'	=>	'Удалить модуль',
+	'LBL_HELP_SETTINGS'	=>	'Справка',
+	'Delete_panel'	=>	'Эта панель конфигурации предназначен для удаления модуля. Название модуля:',
+	'MSG_DEL_WARN1'	=>	'Предупреждение!',
+	'MSG_DEL_WARN2'	=>	'Если вы удалите этот модуль, вы потеряете все данные, связанные с этим модулем в CRM. Так же, после удаления необходимо удалить файлы, оставшиеся в:',
+    'MSG_DEL_PATH1' => ' Smarty/templates/modules/module_name',
+    'MSG_DEL_PATH2' => ' modules/module_name',
+	'MSG_DEL_WARN3'	=>	'и в папке:',
+	'MSG_DEL_WARN4'	=>	'Если вы планируете устанавливать этот модуль в будущем, необходимо удалить эти папки.',
+	'MSG_DEL_WARN5'	=>	'Если у вас есть какие-либо проблемы, вы можете связаться с нами по нашей почте или с помощью уведомлений, отправленных на www.opensaas.pl',
+	'MSG_DEL_WARN6'	=>	'С уважением, команда OpenSaaS',
+    
+	'HelpDescription'	=>	'На этом сайте, вы можете отправлять уведомления, ваши рекомендации о том, как улучшить модуль или просто прочитать описание модуля.',
+	'LBL_TroubleUrl'	=>	'Сайт модуля',
+	'LBL_OurWebsite'	=>	'Наш сайт',
+    'LBL_UrlLink' => 'http://apps.opensaas.pl',
+	'LBL_UrlLinkInfo'	=>	'Вы должны зарегистрировать учетную запись',
+    'LBL_OurWebsiteLink' => 'http://opensaas.pl',
+	'LBL_Manual'	=>	'Документация',
+	'OSSProjectTemplates_manual'	=>	'Шаблоны проектов',
+    
+	'LBL_Helpforthemodule'	=>	'Модуль Заявки: ',
+    'LBL_UrlLink2' => 'http://opensaas.pl/shop.html',
+    'LBL_UrlHelp' => 'serwis@opensaas.pl', 
+    'LBL_UrlHelp2' => 'bok@opensaas.pl',
+    'LBL_UrlLicense' => 'http://opensaas.pl/rules.html',
+	'LBL_License'	=>	'Информация о лицензии:',
+	'LBL_Company'	=>	'Информация о компании:',
+    'LBL_UrlCompany' => 'http://opensaas.pl/about-us.html ',
+	'Information'	=>	'Информация о модулях:',
+    
+	'LBL_CREATING_TPL'	=>	'Создание документа',
+	'LBL_FILL_BASE_DATA'	=>	'Информация',
+	'ADD_CONDITIONS'	=>	'Добавить условие',
+	'LBL_CONDITION_ALL'	=>	'Все условия',
+	'LBL_CONDITION_ALL_DSC'	=>	'Все условия в этом разделе должны быть выполнены',
+	'LBL_CONDITION_OPTION'	=>	'Любое условие',
+	'LBL_CONDITION_OPTION_DSC'	=>	'Одно из условий в этом разделе должно быть выполнено',
+	'FOLDER_LIST'	=>	'Выберите папку',
+	'DOC_NAME'	=>	'Название документа',
+	'DOC_REQUIRED'	=>	'Требовать документ',
+	'DOC_ORDER'	=>	'Сортировать',
+	'NEXT'	=>	'Далее',
+	'CANCEL'	=>	'Отменить',
+	'BACK'	=>	'Назад',
+    
+	'LIST_DOCUMENTS'	=>	'Список документов',
+	'DOC_REQ'	=>	'Обязательный документ',
+	'FOLDER'	=>	'Папка',
+	'ADD_DOCUMENT'	=>	'Добавить документ',
+	'ATTACHED'	=>	'Вложение',
+    
+	'NOT_ALLOW_TO_SAVE'	=>	'Вы не можете сохранить запись, так как необходимые документы не прикреплены',
+	'LIST_REQ_DOC'	=>	'Пожалуйста, приложите следующие документы',
+);
+$jsLanguageStrings = array(
+	'LBL_VIEW_EDIT2'	=>	'Добавление условий',
+);
