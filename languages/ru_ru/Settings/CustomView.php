5d0dd0e330882dcfd8db0a5e27e31ab6d86370a5
WAW555 <waw555@yandex.ru>
2015-01-09 22:12:44 +0500

    Final Translate
    * VERSION YetiForceCRM: 1.1.0 RC

---
+++ languages/ru_ru/Settings/CustomView.php
@@ -0,0 +1,27 @@
+<?php
+/*+***********************************************************************************************************************************
+ * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * See the License for the specific language governing rights and limitations under the License.
+ * The Original Code is YetiForce.
+ * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
+ * All Rights Reserved.
+ * VERSION YetiForceCRM: 1.1.0 RC
+ *************************************************************************************************************************************/
+$languageStrings = array(
+	'CustomView'	=> 'Редактор Фильтров',
+	'LBL_MODULE_DESC'	=> 'Модуль позволяет редактировать системные и пользовательские фильтры.',
+	'Module'	=> 'Модуль',
+	'ViewName'	=> 'Фильтр',
+	'SetDefault'	=> 'По умолчанию',
+	'Privileges'	=> 'Привилегии',
+	'Delete'	=> 'Удалить',
+	'Delete CustomView'	=> 'Фильтр удален',
+	'Actions'	=> 'Действия',
+	'Edit'	=> 'Редактировать',
+);
+$jsLanguageStrings = array(
+	'Saving changes'	=> 'Сохранение изменений...',
+	'Update labels'	=> 'Обновление',
+);
