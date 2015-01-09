e46a1143fdc0c6bca5ae4f24e940d2d91f10c897
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2015-01-09 14:31:34 +0100

    Fix #64 - shared calendar it gives a spinning disks and never ends

--- modules/Calendar/activityTypes/MultipleEvents.php
+++ modules/Calendar/activityTypes/MultipleEvents.php
@@ -8,12 +8,13 @@
  * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
  * All Rights Reserved.
  *************************************************************************************************************************************/
+vimport('~~modules/Calendar/activityTypes/Events.php');
 class Calendar_MultipleEvents_ActivityTypes{
 	public function process($feed, $request, $start, $end, &$result, $userid = false,$color = null,$textColor = 'white') {
 		foreach ($request->get('mapping') as $id=>$backgroundColorAndTextColor) {
 			$userEvents = array();
 			$colorComponents = explode(',',$backgroundColorAndTextColor);
-			Calendar_Events_ActivityTypes::process($start, $end, $userEvents ,$id, $colorComponents[0], $colorComponents[1]);
+			Calendar_Events_ActivityTypes::process($feed, $request, $start, $end, $userEvents ,$id, $colorComponents[0], $colorComponents[1]);
 			$result[$id] = $userEvents;
 		}
 	}
