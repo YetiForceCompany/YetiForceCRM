7705cc67f19dcfc80f2f24f8185c0193b1746a0f
WAW555 <waw555@yandex.ru>
2015-01-10 03:09:41 +0500

    Final Translate
    * VERSION YetiForceCRM: 1.1.0 RC

---
+++ languages/ru_ru/Settings/OSSMailScanner.php
@@ -0,0 +1,32 @@
+<?php
+/* +********************************************************************************
+ * Terms & Conditions are placed on the: http://opensaas.pl
+ * *******************************************************************************
+ *  Module				: OSSMailScanner
+ *  Author				: OpenSaaS Sp. z o.o. 
+ *  Help/Email			: bok@opensaas.pl
+ *  Website				: www.opensaas.pl
+   * VERSION YetiForceCRM: 1.1.0 RC
+ * *******************************************************************************+ */
+$languageStrings = array(
+	'Mail Scanner'	=>	'Сканер почты',
+    'OSSMailScanner' => 'Сканер почты',
+    'OSSMailScanner_manual' => 'Сканер почты',
+);
+
+$jsLanguageStrings = array(
+	'start_cron'	=>	'Планировщик запущен',
+	'end_cron_ok'	=>	'Планировщик завершил работу',
+	'end_cron_error'	=>	'Планировщик завершил работу с ошибкой',
+	'JS_mail_error'	=>	'Указан некорректный адрес электронной почты',
+	'JS_time_error'	=>	'Указан некорректный формат даты',
+	'JS_StopCron'	=>	'Планировщик остановлен вручную',
+	'stop_user'	=>	'Пользователь остановивший планировщик',
+	'Manually stopped'	=>	'Остановлено вручную',
+	'OK'	=>	'Выполнено',
+	'In progress'	=>	'В процессе',
+	'whether_remove_an_identity'	=>	'Следует ли удалять удостоверение',
+	'removed_identity'	=>	'Удостоверение удалено',
+);
+
+
