19dd5c5341849d69eaea8d30c68dd8000dc9883e
WAW555 <waw555@yandex.ru>
2015-01-11 23:45:11 +0500

    Final Translate
    * VERSION YetiForceCRM: 1.1.0 RC

---
+++ languages/ru_ru/PriceBooks.php
@@ -0,0 +1,23 @@
+<?php
+/*+***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * Contributor(s): YetiForce.com
+ * VERSION YetiForceCRM: 1.1.0 RC
+ ************************************************************************************/
+$languageStrings = array(
+	'LBL_ADD_RECORD' => 'Добавить Прайс-Лист',
+	'LBL_ADD_TO' => 'Добавить в',
+	'LBL_EDIT_LIST_PRICE' => 'Редактировать Прайс-Лист',
+	'LBL_PRICEBOOK_INFORMATION' => 'Информация о Прайс-Листе',
+	'LBL_RECORDS_LIST' => 'Список Прайс-Листов',
+	'LBL_UNIT_PRICE' => 'Цена за единицу',
+	'Price Book Name' => 'Название Прайс-Листа',
+	'PriceBook No' => 'Номер Прайс-Листа',
+	'PriceBooks' => 'Прайс-Листы',
+	'SINGLE_PriceBooks' => 'Прайс-Лист',
+);
