5595f5779fbe2f898417eb6863553c732d95c723
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-01-14 10:07:37 +0100

    Fix #74 The shift pattern when the announcement.

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150113';  
-$YetiForce_current_version = '1.1.9 RC';
+$YetiForce_current_version = '1.1.10 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
