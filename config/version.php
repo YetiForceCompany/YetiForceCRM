4378a9a60dc726bffc5656138aef8026bd58a16e
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2015-01-15 08:25:21 +0100

    Adding an alternate path to the PHP

--- config/version.php
+++ config/version.php
@@ -9,6 +9,6 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
-$patch_version = '20150114';  
-$YetiForce_current_version = '1.1.16 RC';
+$patch_version = '20150115';  
+$YetiForce_current_version = '1.1.17 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
