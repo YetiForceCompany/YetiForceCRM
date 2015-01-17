b2938dfbfdcfd030c4fd75142f469a5cf0773cb6
WAW555 <waw555@yandex.ru>
2015-01-18 00:26:27 +0500

    Changing the size of the popup window is rapidly establishing a

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150116';  
-$YetiForce_current_version = '1.1.29 RC';
+$YetiForce_current_version = '1.1.30 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
