8ff099a63b0592bd37f735168296fa81626af6c8
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2015-01-20 08:54:34 +0100

    Improve integration with mobile apps

--- config/version.php
+++ config/version.php
@@ -9,6 +9,6 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
-$patch_version = '20150119';  
-$YetiForce_current_version = '1.1.37 RC';
+$patch_version = '20150120';  
+$YetiForce_current_version = '1.1.38 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
