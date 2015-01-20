3b6b19539eaf2c33eb38e95bfa015d89ccedced7
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-01-20 10:04:53 +0100

    Amendment of file coding

--- modules/Vtiger/views/ShowWidget.php
+++ modules/Vtiger/views/ShowWidget.php
@@ -1,12 +1,13 @@
 <?php
-/*+***********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.1
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *************************************************************************************/
+ * Contributor(s): YetiForce.com
+ ************************************************************************************/
 
 class Vtiger_ShowWidget_View extends Vtiger_IndexAjax_View {
 
