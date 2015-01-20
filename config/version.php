571144cfd6f60fd023d013962b4391077e1aa78c
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2015-01-20 10:42:54 +0100

    Removed old files

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150120';  
-$YetiForce_current_version = '1.1.39 RC';
+$YetiForce_current_version = '1.1.40 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
