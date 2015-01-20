9e5ed05217c2712e5b0ef87da78b3aa4094b4d72
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2015-01-20 14:23:18 +0100

    YetiForce CRM ver. 1.2.0 RC

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150120';  
-$YetiForce_current_version = '1.1.41 RC';
+$YetiForce_current_version = '1.2.0 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
