3dcf08effc05ab5cd7cbd19f1359f265d2cae103
MichałJanicki <michal_janicki@wp.pl>
2015-01-21 08:37:27 +0100

    fixed error without adding "where" clause in method getListQuery

--- modules/ProjectTask/ProjectTask.php
+++ modules/ProjectTask/ProjectTask.php
@@ -175,7 +175,7 @@ class ProjectTask extends CRMEntity {
 
 		global $current_user;
 		$query .= $this->getNonAdminAccessControlQuery($module,$current_user);
-		$query .= "	WHERE vtiger_crmentity.deleted = 0 ".$usewhere;
+		$query .= "	WHERE vtiger_crmentity.deleted = 0 ".$where;
 		return $query;
     }
 
