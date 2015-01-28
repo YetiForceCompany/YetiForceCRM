8886fda19f0b705b3ef26f3a84b0a5b6f2d4f0e7
Алексей <waw555@yandex.ru>
2015-01-28 13:22:04 +0500

    Fix Unix Format

--- languages/ru_ru/Settings/ModuleManager.php
+++ languages/ru_ru/Settings/ModuleManager.php
@@ -1,56 +1,56 @@
-<?php
-/*+***********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
-  * VERSION YetiForceCRM: 1.1.0 RC
- *************************************************************************************/
-$languageStrings = array(
+<?php
+/*+***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+  * VERSION YetiForceCRM: 1.1.0 RC
+ *************************************************************************************/
+$languageStrings = array(
 	'ModuleManager'	=>	'Менеджер Модулей',
-	'LBL_IMPORT_MODULE_FROM_FILE' => 'Установка модуля из файла',
-	'LBL_IMPORT_MODULE' => 'Установка модуля',
-	'LBL_VTIGER_EXTENSION_STORE' => 'Магазин',
-	'LBL_PUBLISHER' => 'Разработчик',
-	'LBL_LICENSE' => 'Лицензия',
-	'LBL_PUBLISHED_ON' => 'Дата создания',
-	'LBL_INSTALL' => 'Установить',
-	'LBL_UPGRADE' => 'Обновить',
-	'LBL_VERSION' => 'Версия',
-	'LBL_DECLINE' => 'Ниже',
-	'LBL_ACCEPT_AND_INSTALL' => 'Подтверждение и установка',
-	'LBL_INVALID_FILE' => 'Неправильный файл',
-	'LBL_NO_LICENSE_PROVIDED' => 'Нет лицензии',
-	'LBL_INSTALLATION' => 'Установка',
-	'LBL_FAILED' => 'Ошибка',
-	'LBL_SUCCESSFULL' => 'Успешно',
-	'LBL_INSTALLATION_LOG' => 'Результаты установки',
-	//Install From file labels
-	'LBL_VERIFY_IMPORT_DETAILS' => 'Подтвердить',
-	'LBL_MODULE_NAME' => 'Название модуля',
-	'LBL_REQ_YETIFORCE_VERSION' => 'Версия YetiForce',
-	'LBL_LICENSE_ACCEPT_AGREEMENT' => 'Я принимаю лицензионные условия.',
-	'LBL_EXISTS'=>'Существует',
-	'LBL_PROCEED_WITH_IMPORT' => 'Вы хотите продолжить?',
-	'LBL_VERSION_NOT_SUPPORTED' => 'Версия модуля не совместима с данной версией системы',
-	'LBL_FINISH' => 'Завершено',
-	'LBL_IMPORT_ZIP' => 'Установка из файла',
-	'LBL_IMPORT_MODULE' => 'Установка из магазина',
-	'LBL_NOT_FOUND' => "Извините, на данной момент нет ни одного расширения для вашей версии CRM",
-	'LBL_INVALID_IMPORT_TRY_AGAIN' => ' предусмотрено для установки модуля! Попробуйте еще раз.',
-	'LBL_IMPORTED_MODULE' => ' был успешно установлен.',
-	'LBL_IMPORTING_MODULE' => 'Установка модуля',
-	'LBL_EXTENSION_STORE' => 'Магазин',
-	'LBL_DELETE_EXIST_DIRECTORY' => 'Пожалуйста, удалите существующую папку модуля для продолжения установки.',
-	'LBL_UPDATE_MODULE_FROM_FILE' => 'Обновить модуль из файла',
-	'LBL_UPDATED_MODULE' => ' был успешно обновлен.',
-	'LBL__UPDATING_MODULE' => 'Обновление модуля',
-	'LBL_ERROR_VERSION' => 'Некорректная версия модуля.Версия модуля: __MODULEVERSION__ - Версия системы: __CRMVERSION__',
-	'LBL_ERROR_NO_DEFAULT_LANGUAGE' => 'Модуль не содержит языковые файлы установленного по умолчанию языка системы. Язык системы (__DEFAULTLANGUAGE__).',
-	'LBL_ERROR_NO_LAYOUT_FILE' => 'Модуль не содержит файлы макета (Шаблона)',
-	
+	'LBL_IMPORT_MODULE_FROM_FILE' => 'Установка модуля из файла',
+	'LBL_IMPORT_MODULE' => 'Установка модуля',
+	'LBL_VTIGER_EXTENSION_STORE' => 'Магазин',
+	'LBL_PUBLISHER' => 'Разработчик',
+	'LBL_LICENSE' => 'Лицензия',
+	'LBL_PUBLISHED_ON' => 'Дата создания',
+	'LBL_INSTALL' => 'Установить',
+	'LBL_UPGRADE' => 'Обновить',
+	'LBL_VERSION' => 'Версия',
+	'LBL_DECLINE' => 'Ниже',
+	'LBL_ACCEPT_AND_INSTALL' => 'Подтверждение и установка',
+	'LBL_INVALID_FILE' => 'Неправильный файл',
+	'LBL_NO_LICENSE_PROVIDED' => 'Нет лицензии',
+	'LBL_INSTALLATION' => 'Установка',
+	'LBL_FAILED' => 'Ошибка',
+	'LBL_SUCCESSFULL' => 'Успешно',
+	'LBL_INSTALLATION_LOG' => 'Результаты установки',
+	//Install From file labels
+	'LBL_VERIFY_IMPORT_DETAILS' => 'Подтвердить',
+	'LBL_MODULE_NAME' => 'Название модуля',
+	'LBL_REQ_YETIFORCE_VERSION' => 'Версия YetiForce',
+	'LBL_LICENSE_ACCEPT_AGREEMENT' => 'Я принимаю лицензионные условия.',
+	'LBL_EXISTS'=>'Существует',
+	'LBL_PROCEED_WITH_IMPORT' => 'Вы хотите продолжить?',
+	'LBL_VERSION_NOT_SUPPORTED' => 'Версия модуля не совместима с данной версией системы',
+	'LBL_FINISH' => 'Завершено',
+	'LBL_IMPORT_ZIP' => 'Установка из файла',
+	'LBL_IMPORT_MODULE' => 'Установка из магазина',
+	'LBL_NOT_FOUND' => "Извините, на данной момент нет ни одного расширения для вашей версии CRM",
+	'LBL_INVALID_IMPORT_TRY_AGAIN' => ' предусмотрено для установки модуля! Попробуйте еще раз.',
+	'LBL_IMPORTED_MODULE' => ' был успешно установлен.',
+	'LBL_IMPORTING_MODULE' => 'Установка модуля',
+	'LBL_EXTENSION_STORE' => 'Магазин',
+	'LBL_DELETE_EXIST_DIRECTORY' => 'Пожалуйста, удалите существующую папку модуля для продолжения установки.',
+	'LBL_UPDATE_MODULE_FROM_FILE' => 'Обновить модуль из файла',
+	'LBL_UPDATED_MODULE' => ' был успешно обновлен.',
+	'LBL__UPDATING_MODULE' => 'Обновление модуля',
+	'LBL_ERROR_VERSION' => 'Некорректная версия модуля.Версия модуля: __MODULEVERSION__ - Версия системы: __CRMVERSION__',
+	'LBL_ERROR_NO_DEFAULT_LANGUAGE' => 'Модуль не содержит языковые файлы установленного по умолчанию языка системы. Язык системы (__DEFAULTLANGUAGE__).',
+	'LBL_ERROR_NO_LAYOUT_FILE' => 'Модуль не содержит файлы макета (Шаблона)',
+	
 	'LBL_CREATE_MODULE'	=>	'Создать Модуль',
 	'LBL_CREATING_MODULE'	=>	'Создание нового Модуля',
 	'LBL_ENTER_MODULE_NAME'	=>	'Системное имя Модуля',
@@ -59,16 +59,16 @@ $languageStrings = array(
 	'LBL_ENTITY_FIELDLABEL'	=>	'Название Поля',
 	'LBL_MODULE_ALREADY_EXISTS_TRY_ANOTHER'	=>	'Модуль с таким названием уже существует',
 	'LBL_INVALID_MODULE_NAME'	=>	'Неверное название Модуля',
-);
-$jsLanguageStrings = array(
-  'JS_ON' => 'вкл',
-  'JS_PLEASE_INSTALL_EXTENSION_LOADER_TO_INSTALL_THIS_EXTENSION_FROM_BELOW_LINK' => 'Пожалуйста установите загрузчик расширений для установки по ссылке ниже',
-  'JS_EXTENSION_STORE_IS_NOT_SUPPOTED_WOULD_YOU_LIKE_TO_INSTALL' => 'Расширение не поддерживается, хотите установить?',
-  'JS_PHP_EXTENSION_LOADER_NOT_INSTALLED' => 'PHP загрузчик расширений не установлен ',
-  'JS_CLICK_HERE' => 'Нажмите здесь ',
-  'JS_FOR_INSTALLATION' => 'для установки',
-  'JS_IMPORT_SUCCESSFULL' => 'Установка прошла успешно',
-  'JS_IMPORTED_MODULE' => 'Модуль успешно установлен.',
-  'JS_UPDATE_SUCCESSFULL' => 'Обновление успешно',
-  'JS_UPDATED_MODULE' => 'Модуль успешно обновлен',
+);
+$jsLanguageStrings = array(
+  'JS_ON' => 'вкл',
+  'JS_PLEASE_INSTALL_EXTENSION_LOADER_TO_INSTALL_THIS_EXTENSION_FROM_BELOW_LINK' => 'Пожалуйста установите загрузчик расширений для установки по ссылке ниже',
+  'JS_EXTENSION_STORE_IS_NOT_SUPPOTED_WOULD_YOU_LIKE_TO_INSTALL' => 'Расширение не поддерживается, хотите установить?',
+  'JS_PHP_EXTENSION_LOADER_NOT_INSTALLED' => 'PHP загрузчик расширений не установлен ',
+  'JS_CLICK_HERE' => 'Нажмите здесь ',
+  'JS_FOR_INSTALLATION' => 'для установки',
+  'JS_IMPORT_SUCCESSFULL' => 'Установка прошла успешно',
+  'JS_IMPORTED_MODULE' => 'Модуль успешно установлен.',
+  'JS_UPDATE_SUCCESSFULL' => 'Обновление успешно',
+  'JS_UPDATED_MODULE' => 'Модуль успешно обновлен',
 );
