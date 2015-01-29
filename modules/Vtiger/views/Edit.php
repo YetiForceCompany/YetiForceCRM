4c01b0cc10ffb93e2b18b19a209f61be250b2470
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-01-29 15:09:56 +0100

    Adding Contributor

--- modules/Vtiger/views/Edit.php
+++ modules/Vtiger/views/Edit.php
@@ -1,12 +1,13 @@
 <?php
-/*+***********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.1
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *************************************************************************************/
+ * Contributor(s): YetiForce.com
+ ************************************************************************************/
 
 Class Vtiger_Edit_View extends Vtiger_Index_View {
     protected $record = false;
