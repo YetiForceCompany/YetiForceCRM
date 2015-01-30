6ea0087a61bfc9f20ca53810d1d3219c63ff2264
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2015-01-30 08:55:22 +0100

    Change version number

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150130';  
-$YetiForce_current_version = '1.2.39 RC';
+$YetiForce_current_version = '1.2.40 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
