53a161cd945ce480c75687ad1bc6c62f98e6e518
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2015-02-03 11:00:39 +0100

    Improvement of value display in chart

--- config/version.php
+++ config/version.php
@@ -9,6 +9,6 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
-$patch_version = '20150202';  
-$YetiForce_current_version = '1.2.58 RC';
+$patch_version = '20150203';  
+$YetiForce_current_version = '1.2.59 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
