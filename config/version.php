37361308fc73131546aeb7c8b494096279bdbf1a
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-02-12 09:35:59 +0100

    Improvement of migration mechanisms

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150211';  
-$YetiForce_current_version = '1.2.102 RC';
+$YetiForce_current_version = '1.2.103 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
