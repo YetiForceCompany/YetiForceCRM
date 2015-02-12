46190df1232504a4cae16d27a4f3eb8026f339b3
mariuszkrzaczkowski <m.krzaczkowski@yetiforce.com>
2015-02-12 16:09:07 +0100

    Repair coding WYSIWYG

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150212';  
-$YetiForce_current_version = '1.2.106 RC';
+$YetiForce_current_version = '1.2.107 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
