d03a97f6f8620b7b9e5b59ef360458d8870ff5a2
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-02-12 08:11:08 +0100

    Add module Ideas

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150211';  
-$YetiForce_current_version = '1.2.100 RC';
+$YetiForce_current_version = '1.2.101 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
