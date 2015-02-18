e9433a8fba7e13d72a7884b5546999173f31a80d
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2015-02-18 13:34:16 +0100

    Improving validation of record

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150218';  
-$YetiForce_current_version = '1.3.20 RC';
+$YetiForce_current_version = '1.3.21 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
