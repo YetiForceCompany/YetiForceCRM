6f659479ef5e1b3c37c83489416679c45dc0f07c
Adrian Koń <a.kon@yetiforce.com>
2015-02-20 13:43:26 +0100

    Fix credits view, add translation

--- languages/pl_pl/Vtiger.php
+++ languages/pl_pl/Vtiger.php
@@ -616,6 +616,7 @@ $languageStrings = array(
 	'Delegated (overdue) Events/ToDos' => 'Zlecone (nieterminowe) terminy/zadania',
 	'Delegated project tasks' => 'Zlecone zadania projektu',
 	'Delegated (overdue) project tasks' => 'Zlecone (nieterminowe) zadania projektu',
+	'Credits' => 'Współtwórcy',
 	
 	'Upcoming Tasks' => 'Zadania',
 	'UPS' => 'UPS',
