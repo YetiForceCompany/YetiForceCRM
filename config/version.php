7347a7f5b4725a5f516101fdbe764c9d01b5db39
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2015-02-20 14:55:57 +0100

    Improving the tree display

--- config/version.php
+++ config/version.php
@@ -9,6 +9,6 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
-$patch_version = '20150219';  
-$YetiForce_current_version = '1.3.37 RC';
+$patch_version = '20150220';  
+$YetiForce_current_version = '1.3.38 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
