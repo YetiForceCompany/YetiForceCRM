6534ae2eb5e882dc313e7ee4a436884dac574517
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2015-02-21 23:54:59 +0100

    Adding update the time when you log in to Customer Portal

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150220';  
-$YetiForce_current_version = '1.3.45 RC';
+$YetiForce_current_version = '1.3.46 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
