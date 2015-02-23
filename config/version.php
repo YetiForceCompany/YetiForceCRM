f189a6e70bdcc54c30cdb4eeefa21a9fe07f6269
Adrian Koń <a.kon@yetiforce.com>
2015-02-23 12:23:59 +0100

    Add improved getCompanyDetails function

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150222';  
-$YetiForce_current_version = '1.3.51 RC';
+$YetiForce_current_version = '1.3.52 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
--- include/utils/utils.php
+++ include/utils/utils.php
@@ -2289,15 +2289,7 @@ function getCompanyDetails() {
 	$result = $adb->pquery($sql, array());
 	
 	$companyDetails = array();
-	$companyDetails['companyname'] = $adb->query_result($result,0,'organizationname');
-	$companyDetails['website'] = $adb->query_result($result,0,'website');
-	$companyDetails['address'] = $adb->query_result($result,0,'address');
-	$companyDetails['city'] = $adb->query_result($result,0,'city');
-	$companyDetails['state'] = $adb->query_result($result,0,'state');
-	$companyDetails['country'] = $adb->query_result($result,0,'country');
-	$companyDetails['phone'] = $adb->query_result($result,0,'phone');
-	$companyDetails['fax'] = $adb->query_result($result,0,'fax');
-	$companyDetails['logoname'] = $adb->query_result($result,0,'logoname');
+	$companyDetails = $adb->query_result_rowdata($result, $i);
 	
 	return $companyDetails;
 }
