84f866ba6a736e52bf816af044e2c6f94093a902
Maciej Stencel <m.stencel@yetiforce.com>
2015-03-04 00:38:17 +0100

    Fixed error when saving new order of menu blocks.

--- modules/OSSMenuManager/actions/SaveBlocks.php
+++ modules/OSSMenuManager/actions/SaveBlocks.php
@@ -1,4 +1,5 @@
-<?php/*+***********************************************************************************************************************************
+<?php
+/*+***********************************************************************************************************************************
  * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
  * in compliance with the License.
  * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
@@ -11,16 +12,16 @@ class OSSMenuManager_SaveBlocks_Action extends Vtiger_Action_Controller {
 	function checkPermission(Vtiger_Request $request) {
 		return;
 	}
-    
+
 	public function process(Vtiger_Request $request) {
 		$moduleName = $request->getModule();
-        $newSequence = $request->get( 'newSequence' );
-        
-        $recordModel = Vtiger_Record_Model::getCleanInstance($moduleName);
-		$Result = $recordModel->updateBlocks( $newSequence );
-        
+		$newSequence = $request->get( 'newSequence' );
+
+		$recordModel = Vtiger_Record_Model::getCleanInstance($moduleName);
+		$result = $recordModel->updateBlocks( $newSequence );
+
 		$response = new Vtiger_Response();
-		$response->setResult($Result);
+		$response->setResult($result);
 		$response->emit();
 	}
 }
