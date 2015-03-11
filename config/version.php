3322403e5d7772064251e46e1c4bd57f0644e255
Adrian Koń <a.kon@yetiforce.com>
2015-03-11 11:00:12 +0100

    Added translations. Correct spelling in vtiger_dataaccess

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150310';  
-$YetiForce_current_version = '1.3.145 RC';
+$YetiForce_current_version = '1.3.146 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
