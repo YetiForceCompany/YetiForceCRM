2f753a72780646f89804c9b2f3b50344fcd6b781
Skavenkf <skavenkf@gmx.de>
2015-03-16 22:29:47 +0100

    add german translation for Reservations.php

---
+++ languages/de_de/Reservations.php
@@ -0,0 +1,88 @@
+<?php
+/*+***********************************************************************************************************************************
+ * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * See the License for the specific language governing rights and limitations under the License.
+ * The Original Code is YetiForce.
+ * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
+ * All Rights Reserved.
+ *************************************************************************************************************************************/
+$languageStrings = array(
+	'SINGLE_Reservations' => 'Reservierungen',
+	'Reservations' => 'Reservierungen',
+	'LBL_MAIN_INFORMATION' => 'Informationen',
+	'LBL_BLOCK' => 'Abschnitt',
+	'LBL_TOTAL_TIME' => 'Gesamtdauer',
+	'LBL_STATUS' => 'Status',
+	'Assigned To' => 'Zugehörig zu',
+	'LBL_TITLE' => 'Bezeichnung',
+	'LBL_START_DATE' => 'Beginnt am',
+	'LBL_END_DATE' => 'Endet am',
+	'LBL_TIME_START' => 'Beginnt um',
+	'LBL_END_TIME' => 'Endet um',
+	'LBL_NUM' => 'Nummer',
+	'LBL_CALENDAR_VIEW' => 'Kalender',
+	'LBL_RESERVATIONS_TYPE' => 'Reservierungsart:',
+	'LBL_STATUS'=>'Status',
+	'PLL_ACCEPTED'=>'Angenommen',
+	'PLL_TO_APPROVAL'=>'Zur Überprüfung',
+	'LBL_RELATEDA'=>'Firma',
+	'LBL_RELATEDB'=>'Prozess',
+	'LBL_TYPE' => 'Typ',
+	'LBL_MEETING_ROOMS' => 'Besprechungsräume',
+	'LBL_EQUIPMENT' => 'Ausstattung',
+	'LBL_CARS' => 'Fahrzeug',
+);
+$jsLanguageStrings = array(
+    'MSG_ERROR_NUMBER' => 'Fehlerschlüssel: [0-9]',
+	'JS_DATE_SHOULD_BE_GREATER_THAN' => 'Datum sollte größer als das folgende sein',
+	'JS_HOURS_SHOULD_BE_SMALLER_THAN' => 'Zeit sollte unter der folgenden liegen',
+
+	
+	//Calendar
+	'JS_ALL_DAY' => 'Alle Tage',
+	'JS_APR' => 'Apr',
+	'JS_APRIL' => 'April',
+	'JS_AUG' => 'Aug',
+	'JS_AUGUST' => 'August',
+	'JS_DAY' => 'Tag',
+	'JS_DEC' => 'Dez',
+	'JS_DECEMBER' => 'Dezember',
+	'JS_FEB' => 'Feb',
+	'JS_FEBRUARY' => 'Februar',
+	'JS_FRIDAY' => 'Freitag',
+	'JS_FRI' => 'Fre',
+	'JS_JAN' => 'Han',
+	'JS_JANUARY' => 'Januar',
+	'JS_JUL' => 'Jul',
+	'JS_JULY' => 'Juli',
+	'JS_JUNE' => 'Juni',
+	'JS_JUN' => 'Jun',
+	'JS_MARCH' => 'März',
+	'JS_MAR' => 'Mär',
+	'JS_MAY' => 'Mai',
+	'JS_MAY' => 'Mai',
+	'JS_MONDAY' => 'Montag',
+	'JS_MON' => 'Mon',
+	'JS_MONTH' => 'Monat',
+	'JS_NOVEMBER' => 'November',
+	'JS_NOV' => 'Nov',
+	'JS_OCTOBER' => 'Oktober',
+	'JS_OCT' => 'Okt',
+	'JS_SAT' => 'Sam',
+	'JS_SATURDAY' => 'Samstag',
+	'JS_SEP' => 'Sep',
+	'JS_SEPTEMBER' => 'September',
+	'JS_SUNDAY' => 'Sonntag',
+	'JS_SUN' => 'Son',
+	'JS_THURSDAY' => 'Donnerstag',
+	'JS_THU' => 'Don',
+	'JS_TODAY' => 'Heute',
+	'JS_TUESDAY' => 'Dienstag',
+	'JS_TUE' => 'Die',
+	'JS_WEDNESDAY' => 'Mittwoch',
+	'JS_WED' => 'Mit',
+	'JS_WEEK' => 'Woche',
+	'JS_MORE' => 'Mehr',
+);
