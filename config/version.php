4750a1a4630da5db7e217d8ac4dd977da9a68b6d
Adrian Koń <a.kon@yetiforce.com>
2015-03-23 11:28:03 +0100

    Fixed not editable reccurency field in quick edit view.

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150319';  
-$YetiForce_current_version = '1.3.198 RC';
+$YetiForce_current_version = '1.3.199 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
