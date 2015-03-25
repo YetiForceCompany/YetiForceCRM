3032f86b6936f42c2bbccaceabdb17764281ba15
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-03-25 22:34:34 +0100

    Improve compatibility NuSOAP library with php 5.4

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150325';  
-$YetiForce_current_version = '1.4.0 RC';
+$YetiForce_current_version = '1.4.1 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
