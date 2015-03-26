439e8ece9f1a665d94fe3861ccf9b37a1388e31d
Adrian Koń <a.kon@yetiforce.com>
2015-03-26 09:00:01 +0100

    H927 Added translation

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150324';  
-$YetiForce_current_version = '1.3.206 RC';
+$YetiForce_current_version = '1.3.207 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
