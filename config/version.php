9058b29a24b7d7ff4c48f14ce1214e89300235bc
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2015-03-30 14:26:03 +0200

    added missing EN translation to Settings/Mail.php

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150330';  
-$YetiForce_current_version = '1.4.23 RC';
+$YetiForce_current_version = '1.4.24 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
