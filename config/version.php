105216df2db6a3091cc82cbd471ce5d27a4f47fb
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-03-30 11:16:28 +0200

    Improve copy and delete recursive.

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150330';  
-$YetiForce_current_version = '1.4.21 RC';
+$YetiForce_current_version = '1.4.22 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
