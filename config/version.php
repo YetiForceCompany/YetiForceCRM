8f58e2e18c2dd53127aad85b15ec57130655d2de
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-03 14:05:09 +0200

    Added missing translation

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150403';  
-$YetiForce_current_version = '1.4.47 RC';
+$YetiForce_current_version = '1.4.48 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
