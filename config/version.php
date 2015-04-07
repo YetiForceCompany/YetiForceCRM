17825e8a6da49f362dd1d9ed635bb0150f8ec731
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-07 09:35:09 +0200

    Fixed change your password

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150403';  
-$YetiForce_current_version = '1.4.56 RC';
+$YetiForce_current_version = '1.4.57 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
