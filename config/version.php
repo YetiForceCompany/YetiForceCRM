75da499fd9c3759b2550fffe32a98870fa5e90ca
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-13 12:33:30 +0200

    Added keyboard shortcuts

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150413';  
-$YetiForce_current_version = '1.4.81 RC';
+$YetiForce_current_version = '1.4.82 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
