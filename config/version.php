cef44770684de8535a40746aa268d0e6c0f37300
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-13 16:20:46 +0200

    Improving the default settings when converting a lead

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150413';  
-$YetiForce_current_version = '1.4.90 RC';
+$YetiForce_current_version = '1.4.91 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
