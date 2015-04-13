efeeafc5f03ba57d1f67b1c48f01e1c54dbadf90
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-13 11:21:07 +0200

    Removed library jquery.hotkeys.js

--- config/version.php
+++ config/version.php
@@ -9,6 +9,6 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
-$patch_version = '20150408';  
-$YetiForce_current_version = '1.4.72 RC';
+$patch_version = '20150413';  
+$YetiForce_current_version = '1.4.73 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
