72125245ff1e3cc06c7756e128ef4616db7fdb62
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-13 11:24:38 +0200

    Added library jsTree v3.1.0

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150413';  
-$YetiForce_current_version = '1.4.73 RC';
+$YetiForce_current_version = '1.4.74 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
