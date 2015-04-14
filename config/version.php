4ae9e4de1cdf2d7abc4942fff33464886097e1f8
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-14 13:44:22 +0200

    Fixed selection in the tree

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150414';  
-$YetiForce_current_version = '1.4.98 RC';
+$YetiForce_current_version = '1.4.99 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
