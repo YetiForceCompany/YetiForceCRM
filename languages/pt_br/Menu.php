d1793e0e1535b41a215ed30d68fc7ef1f32daab5
vctrin <valmir@ttcasolucoes.com.br>
2015-04-13 23:15:57 -0300

    Improved brazilian language

--- languages/pt_br/Menu.php
+++ languages/pt_br/Menu.php
@@ -6,9 +6,9 @@
  * See the License for the specific language governing rights and limitations under the License.
  * The Original Code is YetiForce.
  * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
- * All Rights Reserved.
+ * All Rights Reserved. Contributor: Valmir Carlos Trindade - Brazilian Portuguese Translation - valmir@ttcasolucoes.com.br
  *************************************************************************************************************************************/
 $languageStrings = Array (
 	'LBL_QUICK_CREATE_MODULE' => '__UNTRANSLATED__',
-	'LBL_SEPARATOR' => 'Separator',
+	'LBL_SEPARATOR' => 'Separador',
 );
