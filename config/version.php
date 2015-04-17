03290bc0f0a0f469c62b38ee79352e2ed0bf93a2
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2015-04-17 12:15:21 +0200

    Added missing translations

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150416';  
-$YetiForce_current_version = '1.4.130 RC';
+$YetiForce_current_version = '1.4.131 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
