627c10ec7bb0fac58f6c5f592fce550e9ea9bc0e
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-18 12:03:19 +0200

    Fixed debug file

--- config/debug.php
+++ config/debug.php
@@ -10,27 +10,27 @@
  *************************************************************************************************************************************/
 $GEBUG_CONFIG = Array(
 	// enable log4php -> cache/logs/system.log
-	'LOG4PHP_DEBUG' => false,
+	'LOG4PHP_DEBUG' => FALSE,
 	
 	// show php errors (display_errors),
-	'DISPLAY_PHP_ERRORS' => false,
+	'DISPLAY_PHP_ERRORS' => FALSE,
 	
 	// display sql queries in the browser during triggering
-	'DISPLAY_SQL_QUERY' => false,
+	'DISPLAY_SQL_QUERY' => FALSE,
 
 	// stop the running process of the system if there is and error in sql query
-	'SQL_DIE_ON_ERROR' => false,
+	'SQL_DIE_ON_ERROR' => FALSE,
 	
 	// debug Viewer => cache/logs/viewer-debug.log
-	'DEBUG_VIEWER' => false,
+	'DEBUG_VIEWER' => FALSE,
 	
 	// Display Smarty Debug Console
-	'DISPLAY_DEBUG_VIEWER' => false,
+	'DISPLAY_DEBUG_VIEWER' => FALSE,
 	
 	// sabre dav - This is a flag that allow or not showing file, line and code of the exception in the returned XML
-	'DAV_DEBUG_EXCEPTIONS' => false,
+	'DAV_DEBUG_EXCEPTIONS' => FALSE,
 	
-	'DAV_DEBUG_PLUGIN' => false,
+	'DAV_DEBUG_PLUGIN' => FALSE,
 	
 	/*+***************************************************************
 	 *	ROUNDCUBE 
@@ -40,28 +40,28 @@ $GEBUG_CONFIG = Array(
 	
 	// Activate this option if logs should be written to per-user directories.
 	// Data will only be logged if a directry cache/logs/<username>/ exists and is writable.
-	'ROUNDCUBE_PER_USER_LOGGING' => false,
+	'ROUNDCUBE_PER_USER_LOGGING' => FALSE,
 
 	// Log sent messages to cache/logs/sendmail or to syslog
-	'ROUNDCUBE_SMTP_LOG' => false,
+	'ROUNDCUBE_SMTP_LOG' => FALSE,
 
 	// Log successful/failed logins to cache/logs/userlogins or to syslog
-	'ROUNDCUBE_LOG_LOGINS' => false,
+	'ROUNDCUBE_LOG_LOGINS' => FALSE,
 
 	// Log session authentication errors to cache/logs/session or to syslog
-	'ROUNDCUBE_LOG_SESSION' => false,
+	'ROUNDCUBE_LOG_SESSION' => FALSE,
 
 	// Log SQL queries to cache/logs/sql or to syslog
-	'ROUNDCUBE_SQL_DEBUG' => false,
+	'ROUNDCUBE_SQL_DEBUG' => FALSE,
 
 	// Log IMAP conversation to cache/logs/imap or to syslog
-	'ROUNDCUBE_IMAP_DEBUG' => false,
+	'ROUNDCUBE_IMAP_DEBUG' => FALSE,
 
 	// Log LDAP conversation to cache/logs/ldap or to syslog
-	'ROUNDCUBE_LDAP_DEBUG' => false,
+	'ROUNDCUBE_LDAP_DEBUG' => FALSE,
 
 	// Log SMTP conversation to cache/logs/smtp or to syslog
-	'ROUNDCUBE_SMTP_DEBUG' => false,
+	'ROUNDCUBE_SMTP_DEBUG' => FALSE,
 );
 if($GEBUG_CONFIG['DISPLAY_PHP_ERRORS']){
 	ini_set('display_errors','on'); version_compare(PHP_VERSION, '5.4.0') <= 0 ? error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED) : error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT);
@@ -69,7 +69,7 @@ if($GEBUG_CONFIG['DISPLAY_PHP_ERRORS']){
 	ini_set('display_errors','off');version_compare(PHP_VERSION, '5.4.0') <= 0 ? error_reporting(E_WARNING & ~E_NOTICE & ~E_DEPRECATED) : error_reporting(E_WARNING & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT);
 }
 class SysDebug {
-	static function get($key, $defvalue=false) {
+	static function get($key, $defvalue=FALSE) {
 		global $GEBUG_CONFIG;
 		if(isset($GEBUG_CONFIG)){
 			if(isset($GEBUG_CONFIG[$key])) {
@@ -79,7 +79,7 @@ class SysDebug {
 		return $defvalue;
 	}
 	/** Get boolean value */
-	static function getBoolean($key, $defvalue=false) {
+	static function getBoolean($key, $defvalue=FALSE) {
 		return self::get($key, $defvalue);
 	}
 }
