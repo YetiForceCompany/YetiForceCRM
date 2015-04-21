6b1607f5ce8b795432784f8b69f7bbfe335b2159
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-21 08:42:56 +0200

    Added date and time to the history of recently viewed pages

--- config/version.php
+++ config/version.php
@@ -9,6 +9,6 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
-$patch_version = '20150420';  
-$YetiForce_current_version = '1.4.174 RC';
+$patch_version = '20150421';  
+$YetiForce_current_version = '1.4.175 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
