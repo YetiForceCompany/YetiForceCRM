ec2db5771cf92e4dac58791112bedcb232922b2f
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-21 14:26:31 +0200

    Changed downloading mail address

--- layouts/vlayout/modules/OSSMail/resources/Global.js
+++ layouts/vlayout/modules/OSSMail/resources/Global.js
@@ -501,9 +501,7 @@ function get_module_permissions(crm_path) {
     return resp;
 }
 function getAbsolutePath() {
-    var loc = window.location;
-    var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
-    return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
+    return jQuery('#site_URL').val();
 }
 function oss_get_config() {
     var params = {};
