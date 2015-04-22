a04b25b0a391a87c879965c49e56a85ba596b5d4
Adrian Koń <a.kon@yetiforce.com>
2015-04-22 19:48:00 +0200

    H939  Changed fields order

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150422';  
-$YetiForce_current_version = '1.4.208 RC';
+$YetiForce_current_version = '1.4.207 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
