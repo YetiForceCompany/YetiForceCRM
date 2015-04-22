d31941b4bdcc4963df30039776f5815053e08a98
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-22 13:16:49 +0200

    Added function to determine the remote ip

--- config/version.php
+++ config/version.php
@@ -9,6 +9,6 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
-$patch_version = '20150421';  
-$YetiForce_current_version = '1.4.199 RC';
+$patch_version = '20150422';  
+$YetiForce_current_version = '1.4.200 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
