ed7572310de8993f0b92744ff61f4df7331b88cd
Adrian Koń <a.kon@yetiforce.com>
2015-04-22 13:20:33 +0200

    Changed order fields in quick create

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150422';  
-$YetiForce_current_version = '1.4.201 RC';
+$YetiForce_current_version = '1.4.202 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
