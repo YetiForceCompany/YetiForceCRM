c0af2e6cabae62e8a81f33a563c84b6258a43eb4
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-23 12:16:22 +0200

    Added session life time to configuration file

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150423';  
-$YetiForce_current_version = '1.4.216 RC';
+$YetiForce_current_version = '1.4.217 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
