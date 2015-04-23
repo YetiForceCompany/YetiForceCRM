bb3f2eac72628863f5b51fe98df966847ba17edb
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-23 09:00:19 +0200

    Fixed auto-login mail

--- config/version.php
+++ config/version.php
@@ -9,6 +9,6 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
-$patch_version = '20150423';  
-$YetiForce_current_version = '1.4.208 RC';
+$patch_version = '20150424';  
+$YetiForce_current_version = '1.4.209 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
