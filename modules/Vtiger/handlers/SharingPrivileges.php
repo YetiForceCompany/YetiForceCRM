15ee87539ee3ccbb2918c8027f082672f953ba35
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-04-30 13:03:26 +0200

    Improved sharing permissions while associating of records

--- modules/Vtiger/handlers/SharingPrivileges.php
+++ modules/Vtiger/handlers/SharingPrivileges.php
@@ -1,43 +1,53 @@
 <?php
+/*+***********************************************************************************************************************************
+ * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * See the License for the specific language governing rights and limitations under the License.
+ * The Original Code is YetiForce.
+ * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
+ * All Rights Reserved.
+*************************************************************************************************************************************/
 require_once 'include/events/VTEventHandler.inc';
 class Vtiger_SharingPrivileges_Handler extends VTEventHandler {
-	function handleEvent($eventName, $data) {
-		global $adb,$shared_owners;
+
+	function handleEvent($eventName, $entityData) {
+		global $adb, $shared_owners;
 		if ($eventName == 'vtiger.entity.aftersave.final' && $shared_owners == true) {
-            $moduleName = $data->getModuleName();
-			$recordId = $data->getId();
-			if($data->get('inheritsharing') != NULL && $data->get('inheritsharing') !== 0){
+			$moduleName = $entityData->getModuleName();
+			$recordId = $entityData->getId();
+			if ($entityData->get('inheritsharing') != NULL && $entityData->get('inheritsharing') !== 0) {
 				$vtEntityDelta = new VTEntityDelta();
 				$delta = $vtEntityDelta->getEntityDelta($moduleName, $recordId, true);
-				if ( array_key_exists("shownerid",$delta) ){
-					$oldValue = Vtiger_Functions::getArrayFromValue( $delta['shownerid']['oldValue'] );
-					$currentValue = Vtiger_Functions::getArrayFromValue( $delta['shownerid']['currentValue'] );
-					if( count($oldValue) == 0 ){
+				if (array_key_exists("shownerid", $delta)) {
+					$oldValue = Vtiger_Functions::getArrayFromValue($delta['shownerid']['oldValue']);
+					$currentValue = Vtiger_Functions::getArrayFromValue($delta['shownerid']['currentValue']);
+					if (count($oldValue) == 0) {
 						$addUser = $currentValue;
 						$removeUser = array();
-					}else{
-						$removeUser = array_diff ($oldValue, $currentValue);
-						$addUser = array_diff ($currentValue, $oldValue);
+					} else {
+						$removeUser = array_diff($oldValue, $currentValue);
+						$addUser = array_diff($currentValue, $oldValue);
 					}
-					Users_Privileges_Model::setSharedOwnerRecursively( $recordId , $addUser, $removeUser, $moduleName );
+					Users_Privileges_Model::setSharedOwnerRecursively($recordId, $addUser, $removeUser, $moduleName);
 				}
 			}
 		}
-		if ($eventName == 'vtiger.entity.link.after' && $shared_owners == true && Vtiger_Processes_Model::getConfig('sales','popup','popup','update_shared_permissions') == 'true') {
+
+		if ($eventName == 'vtiger.entity.link.after' && $shared_owners == true && Vtiger_Processes_Model::getConfig('sales', 'popup', 'update_shared_permissions') == 'true') {
 			$destinationModule = array('Products', 'Services');
 			if ($entityData['sourceModule'] == 'Potentials' && in_array($entityData['destinationModule'], $destinationModule)) {
 				$adb = PearDatabase::getInstance();
-				$result1 = $adb->pquery('SELECT smownerid FROM vtiger_crmentity WHERE crmid = ?;', array($entityData['destinationRecordId']));
-				$result2 = $adb->pquery('SELECT smownerid,shownerid FROM vtiger_crmentity WHERE crmid = ?;', array($entityData['sourceRecordId']));
+				$result1 = $adb->pquery('SELECT smownerid, shownerid FROM vtiger_crmentity WHERE crmid = ?;', array($entityData['destinationRecordId']));
+				$result2 = $adb->pquery('SELECT shownerid FROM vtiger_crmentity WHERE crmid = ?;', array($entityData['sourceRecordId']));
 				if ($adb->num_rows($result1) == 1 && $adb->num_rows($result2) == 1) {
-					$smownerid = $adb->query_result($result1, 0, 'smownerid');
-					$shownerid = $adb->query_result($result2, 0, 'shownerid') . ',' . $smownerid;
+					$smownerid = $adb->query_result($result1, 0, 'smownerid') . ',' . $adb->query_result($result1, 0, 'shownerid');
+					$shownerid = $adb->query_result($result2, 0, 'shownerid') . ',' . trim($smownerid, ',');
 
-					if ($smownerid != $adb->query_result($result2, 0, 'smownerid')) {
-						$adb->pquery("UPDATE vtiger_crmentity SET shownerid = ? WHERE crmid = ?;", array(rtrim($shownerid, ','), $entityData['sourceRecordId']));
-					}
+					$adb->pquery("UPDATE vtiger_crmentity SET shownerid = ? WHERE crmid = ?;", [trim($shownerid, ','), $entityData['sourceRecordId']]);
 				}
 			}
 		}
 	}
+
 }
