acfc47964031a2019fc1a4d6e848c9d0c4172c06
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2015-05-05 12:26:49 +0200

    Improved translations

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150430';  
-$YetiForce_current_version = '1.4.293 RC';
+$YetiForce_current_version = '1.4.294 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
