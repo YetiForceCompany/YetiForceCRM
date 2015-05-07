19942c1ce65014ba24d9ff576f61b4bc0e7bea2e
Adrian Koń <a.kon@yetiforce.com>
2015-05-07 09:22:51 +0200

    Fixed responsive comments in summary record

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150506';  
-$YetiForce_current_version = '1.4.306 RC';
+$YetiForce_current_version = '1.4.307 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
