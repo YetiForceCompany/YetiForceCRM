1ac7e22ac25f86ed5459e8972569491b7dbd7bce
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-05-07 11:14:10 +0200

    Added refresh the number of records in a module associated after adding a record.

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150507';  
-$YetiForce_current_version = '1.4.314 RC';
+$YetiForce_current_version = '1.4.315 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
