831eac790af50a12077076cf78d94494c70befec
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-05-08 15:47:59 +0200

    Fixed download attachments

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150508';  
-$YetiForce_current_version = '1.4.327 RC';
+$YetiForce_current_version = '1.4.328 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
