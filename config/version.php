872a4ef721c5a0e68acb2479e830fc6b9b0908fd
Adrian Koń <a.kon@yetiforce.com>
2015-05-11 13:43:18 +0200

    Changed tooltips in time control widget

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150511';  
-$YetiForce_current_version = '1.4.333 RC';
+$YetiForce_current_version = '1.4.334 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
