2516ae62f06fd78b705858573eca8a7d280c72d9
Adrian Koń <a.kon@yetiforce.com>
2015-05-15 11:00:43 +0200

    Fix #770 Added alt, fixed translations

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150513';  
-$YetiForce_current_version = '1.4.393 RC';
+$YetiForce_current_version = '1.4.394 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
