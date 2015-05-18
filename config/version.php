dbe4ca3f1c4d473f5d99bd828248e158eca38f42
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-05-18 15:07:41 +0200

    Fixed display of the history of changes in the Dashboard

--- config/version.php
+++ config/version.php
@@ -10,5 +10,5 @@
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
 $patch_version = '20150518';  
-$YetiForce_current_version = '1.4.429 RC';
+$YetiForce_current_version = '1.4.430 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
