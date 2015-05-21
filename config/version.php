551908747de635427e141d1419aaae04be24d2db
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-05-21 11:50:54 +0200

    Fixed translations

--- config/version.php
+++ config/version.php
@@ -9,6 +9,6 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 // -ve timestamp before release, +ve timestamp after release.
-$patch_version = '20150520';  
-$YetiForce_current_version = '1.4.484 RC';
+$patch_version = '20150521';  
+$YetiForce_current_version = '1.4.485 RC';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
