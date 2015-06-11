12c9b4de07a85c39ee5cbd960f8c98bdbafcb67c
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-06-11 09:44:37 +0200

    Improving the conversion of a lead

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 $patch_version = '20150610';  
-$YetiForce_current_version = '2.0.90';
+$YetiForce_current_version = '2.0.91';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
