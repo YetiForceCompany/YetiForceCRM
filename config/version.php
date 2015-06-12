9664b412f6d8478959ad9ea350ef3778f2c42fcc
Maciej Stencel <m.stencel@yetiforce.com>
2015-06-12 23:54:45 +0200

    Fix #1045 - Fixed displaying of calendar records long enaugh that their start and end dates are outside of current calendar view.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 $patch_version = '20150612';  
-$YetiForce_current_version = '2.0.107';
+$YetiForce_current_version = '2.0.108';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
