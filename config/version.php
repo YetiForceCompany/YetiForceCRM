20f295faf75271247bfa05acadd84e0ed7cef44d
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-06-13 00:42:05 +0200

    Added ability customization languages (overwriting the default language files)

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  ********************************************************************************/
-$patch_version = '20150612';  
-$YetiForce_current_version = '2.0.107';
+$patch_version = '20150613';  
+$YetiForce_current_version = '2.0.108';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
