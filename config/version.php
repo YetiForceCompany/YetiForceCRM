e2f88bd947ba88fe92123248f07d8b8b4f1a86ae
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-06-15 11:02:13 +0200

    Improving the progression bar in backup

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 $patch_version = '20150615';  
-$YetiForce_current_version = '2.0.116';
+$YetiForce_current_version = '2.0.117';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
