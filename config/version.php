c01d672eb4998a575dfd41d4fd8317ec972e6a04
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-06-15 10:15:26 +0200

    Fixed abilty to customize the layouts

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  ********************************************************************************/
-$patch_version = '20150614';  
-$YetiForce_current_version = '2.0.115';
+$patch_version = '20150615';  
+$YetiForce_current_version = '2.0.116';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
