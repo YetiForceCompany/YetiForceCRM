9ff9c7d4c64863364d51ac898f5eaed12d954f4a
Maciej Stencel <m.stencel@yetiforce.com>
2015-06-15 21:37:20 +0200

    Fix #1042 - Fixed printing of PDF file when related record is deleted.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 $patch_version = '20150615';  
-$YetiForce_current_version = '2.0.122';
+$YetiForce_current_version = '2.0.123';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
