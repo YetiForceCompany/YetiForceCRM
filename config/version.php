e363d9a476dd19df7a46a02915a066260f23d2bb
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-06-17 08:53:56 +0200

    Secured search of records in a list view

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  ********************************************************************************/
-$patch_version = '20150616';  
-$YetiForce_current_version = '2.0.128';
+$patch_version = '20150617';  
+$YetiForce_current_version = '2.0.129';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
