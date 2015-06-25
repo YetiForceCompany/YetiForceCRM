20d40b472dc18138047bfa54c60fa2b8749aebbd
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-06-25 12:59:04 +0200

    Improved basic information and record

--- vtlib/Vtiger/Functions.php
+++ vtlib/Vtiger/Functions.php
@@ -227,7 +227,7 @@ class Vtiger_Functions {
 	// MODULE RECORD
 	protected static $crmRecordIdMetadataCache = array();
 
-	protected static function getCRMRecordMetadata($mixedid) {
+	public static function getCRMRecordMetadata($mixedid) {
 		$adb = PearDatabase::getInstance();
 
 		$multimode = is_array($mixedid);
@@ -241,7 +241,7 @@ class Vtiger_Functions {
 		}
 
 		if ($missing) {
-			$sql = sprintf("SELECT crmid, setype, label, searchlabel FROM vtiger_crmentity WHERE %s", implode(' OR ', array_fill(0, count($missing), 'crmid=?')));
+			$sql = sprintf("SELECT crmid, setype, deleted, smownerid, label, searchlabel FROM vtiger_crmentity WHERE %s", implode(' OR ', array_fill(0, count($missing), 'crmid=?')));
 			$result = $adb->pquery($sql, $missing);
 			while ($row = $adb->fetch_array($result)) {
 				self::$crmRecordIdMetadataCache[$row['crmid']] = $row;
