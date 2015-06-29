bbb3cea6a09dc17016db4ef0e5f165d1005b62cd
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-06-29 08:32:30 +0200

    Added uncompressed file the Bootbox library.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 $patch_version = '20150627';
-$YetiForce_current_version = '2.0.209';
+$YetiForce_current_version = '2.0.210';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
