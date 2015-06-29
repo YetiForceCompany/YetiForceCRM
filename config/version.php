423e6040fe78dd18d81bccdad9dcacfdb54f2ac7
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-06-29 09:35:37 +0200

    Fix mail configuration modules

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 $patch_version = '20150629';
-$YetiForce_current_version = '2.0.212';
+$YetiForce_current_version = '2.0.213';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
