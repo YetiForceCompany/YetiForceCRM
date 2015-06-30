fde0c3d6006d45c08808c3e552a6c695938c61fc
Adrian Koń <a.kon@yetiforce.com>
2015-06-30 15:14:09 +0200

    Fix #1251 Added translations

---
+++ languages/pl_pl/Export.php
@@ -0,0 +1,8 @@
+<?php
+/* {[The file is published on the basis of YetiForce Public License
+ * that can be found in the following directory: licenses/License.html]} */
+
+$languageStrings = [
+	'CallHistory' => 'Historię połączeń',
+	'Export' => 'Eksportuj',
+];
