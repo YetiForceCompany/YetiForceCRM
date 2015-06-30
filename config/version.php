ddfc17bdd0cd7f42434980101d300b21e3e4f161
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-06-30 06:24:24 +0200

    Fix #1209 Fixed a bug in the list view Workflow panel.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 $patch_version = '20150629';
-$YetiForce_current_version = '2.0.231';
+$YetiForce_current_version = '2.0.232';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
