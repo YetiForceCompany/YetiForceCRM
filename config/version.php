a34b1966df62759909ecbabf4999a5b571aa5782
Adrian Koń <a.kon@yetiforce.com>
2015-06-30 09:58:22 +0200

    Fix #1236 change the first letter to capital

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 $patch_version = '20150630';
-$YetiForce_current_version = '2.0.239';
+$YetiForce_current_version = '2.0.240';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
