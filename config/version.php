b7472d4b4b7113b778945aa71ac8c15be7095fa9
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-07-01 10:36:30 +0200

    Improved the display of global search results.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 $patch_version = '20150701';
-$YetiForce_current_version = '2.0.263';
+$YetiForce_current_version = '2.0.264';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
