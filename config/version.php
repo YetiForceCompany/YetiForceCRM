2d76047b5715523e3e4de574c008d31ae17f0c52
Adrian Koń <a.kon@yetiforce.com>
2015-07-01 15:56:35 +0200

    Improved users preference detail, users list, user edit views.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 $patch_version = '20150701';
-$YetiForce_current_version = '2.0.274';
+$YetiForce_current_version = '2.0.275';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
