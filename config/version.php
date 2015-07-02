6b3e6a82cfd949987c712e350739952937b98f13
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-07-02 23:11:22 +0200

    Improved the display of default text in edit fields.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 $patch_version = '20150702';
-$YetiForce_current_version = '2.0.303';
+$YetiForce_current_version = '2.0.304';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
