e0b06bac158a5b630d965af252f395071853fdc0
Adrian Koń <a.kon@yetiforce.com>
2015-07-07 10:57:05 +0200

    Fix #1393 Changed fileds type to mandatory

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 $patch_version = '20150707';
-$YetiForce_current_version = '2.0.388';
+$YetiForce_current_version = '2.0.389';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
