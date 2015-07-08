3b7df7be1cad0501b072406b72a42b76869e4665
Adrian Koń <a.kon@yetiforce.com>
2015-07-08 11:48:03 +0200

    Removed unnecessary classes

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  ********************************************************************************/
 $patch_version = '20150708';
-$YetiForce_current_version = '2.0.403';
+$YetiForce_current_version = '2.0.404';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
