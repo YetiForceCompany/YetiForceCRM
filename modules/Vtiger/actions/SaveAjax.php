3bf13d7f6d803c5c8fdde22a4b020cdb1f67c0b3
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-07-08 21:57:40 +0200

    Fix #1410 Fixed saving 'Share with users' fields.

--- modules/Vtiger/actions/SaveAjax.php
+++ modules/Vtiger/actions/SaveAjax.php
@@ -1,30 +1,35 @@
 <?php
-/*+***********************************************************************************
+/* +***********************************************************************************
  * The contents of this file are subject to the vtiger CRM Public License Version 1.0
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *************************************************************************************/
+ * *********************************************************************************** */
 
-class Vtiger_SaveAjax_Action extends Vtiger_Save_Action {
+class Vtiger_SaveAjax_Action extends Vtiger_Save_Action
+{
 
-	public function process(Vtiger_Request $request) {
+	public function process(Vtiger_Request $request)
+	{
 		$recordModel = $this->saveRecord($request);
 
 		$fieldModelList = $recordModel->getModule()->getFields();
 		$result = array();
 		foreach ($fieldModelList as $fieldName => $fieldModel) {
-            $recordFieldValue = $recordModel->get($fieldName);
-            if(is_array($recordFieldValue) && $fieldModel->getFieldDataType() == 'multipicklist') {
-                $recordFieldValue = implode(' |##| ', $recordFieldValue);
-            }
-			$fieldValue = $displayValue = Vtiger_Util_Helper::toSafeHTML($recordFieldValue);
-			if ($fieldModel->getFieldDataType() !== 'currency' && $fieldModel->getFieldDataType() !== 'datetime' && $fieldModel->getFieldDataType() !== 'date') { 
-				$displayValue = $fieldModel->getDisplayValue($fieldValue, $recordModel->getId()); 
+			$recordFieldValue = $recordModel->get($fieldName);
+			if (is_array($recordFieldValue) && $fieldModel->getFieldDataType() == 'multipicklist') {
+				$recordFieldValue = implode(' |##| ', $recordFieldValue);
 			}
-			
+			if (is_array($recordFieldValue) && $fieldModel->getFieldDataType() == 'sharedOwner') {
+				$recordFieldValue = implode(',', $recordFieldValue);
+			}
+			$fieldValue = $displayValue = Vtiger_Util_Helper::toSafeHTML($recordFieldValue);
+			if ($fieldModel->getFieldDataType() !== 'currency' && $fieldModel->getFieldDataType() !== 'datetime' && $fieldModel->getFieldDataType() !== 'date') {
+				$displayValue = $fieldModel->getDisplayValue($fieldValue, $recordModel->getId());
+			}
+
 			$result[$fieldName] = array('value' => $fieldValue, 'display_value' => $displayValue);
 		}
 
@@ -32,8 +37,9 @@ class Vtiger_SaveAjax_Action extends Vtiger_Save_Action {
 		if ($request->get('field') === 'firstname' && in_array($request->getModule(), array('Contacts'))) {
 			$salutationType = $recordModel->getDisplayValue('salutationtype');
 			$firstNameDetails = $result['firstname'];
-			$firstNameDetails['display_value'] = $salutationType. " " .$firstNameDetails['display_value'];
-			if ($salutationType != '--None--') $result['firstname'] = $firstNameDetails;
+			$firstNameDetails['display_value'] = $salutationType . " " . $firstNameDetails['display_value'];
+			if ($salutationType != '--None--')
+				$result['firstname'] = $firstNameDetails;
 		}
 
 		$result['_recordLabel'] = $recordModel->getName();
@@ -50,31 +56,32 @@ class Vtiger_SaveAjax_Action extends Vtiger_Save_Action {
 	 * @param Vtiger_Request $request
 	 * @return Vtiger_Record_Model or Module specific Record Model instance
 	 */
-	public function getRecordModelFromRequest(Vtiger_Request $request) {
+	public function getRecordModelFromRequest(Vtiger_Request $request)
+	{
 		$moduleName = $request->getModule();
 		$recordId = $request->get('record');
 
-		if(!empty($recordId)) {
+		if (!empty($recordId)) {
 			$recordModel = Vtiger_Record_Model::getInstanceById($recordId, $moduleName);
 			$recordModel->set('id', $recordId);
 			$recordModel->set('mode', 'edit');
 
 			$fieldModelList = $recordModel->getModule()->getFields();
 			foreach ($fieldModelList as $fieldName => $fieldModel) {
-                //For not converting craetedtime and modified time to user format
-                $uiType = $fieldModel->get('uitype');
-                if ($uiType == 70) {
-                    $fieldValue = $recordModel->get($fieldName);
-                } else {
-                    $fieldValue = $fieldModel->getUITypeModel()->getUserRequestValue($recordModel->get($fieldName));
-                }
-				
+				//For not converting craetedtime and modified time to user format
+				$uiType = $fieldModel->get('uitype');
+				if ($uiType == 70) {
+					$fieldValue = $recordModel->get($fieldName);
+				} else {
+					$fieldValue = $fieldModel->getUITypeModel()->getUserRequestValue($recordModel->get($fieldName));
+				}
+
 
 				if ($fieldName === $request->get('field')) {
 					$fieldValue = $request->get('value');
 				}
-                $fieldDataType = $fieldModel->getFieldDataType();
-                if ($fieldDataType == 'time') {
+				$fieldDataType = $fieldModel->getFieldDataType();
+				if ($fieldDataType == 'time') {
 					$fieldValue = Vtiger_Time_UIType::getTimeValueWithSeconds($fieldValue);
 				}
 				if ($fieldValue !== null) {
@@ -108,7 +115,7 @@ class Vtiger_SaveAjax_Action extends Vtiger_Save_Action {
 					}
 					$recordModel->set($fieldName, $fieldValue);
 				}
-			} 
+			}
 		}
 
 		return $recordModel;
--- modules/Vtiger/uitypes/SharedOwner.php
+++ modules/Vtiger/uitypes/SharedOwner.php
@@ -1,5 +1,5 @@
 <?php
-/*+***********************************************************************************************************************************
+/* +***********************************************************************************************************************************
  * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
  * in compliance with the License.
  * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
@@ -7,38 +7,45 @@
  * The Original Code is YetiForce.
  * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
  * All Rights Reserved.
- *************************************************************************************************************************************/
-class Vtiger_sharedOwner_UIType extends Vtiger_Base_UIType {
+ * *********************************************************************************************************************************** */
+
+class Vtiger_sharedOwner_UIType extends Vtiger_Base_UIType
+{
+
 	/**
 	 * Function to get the Template name for the current UI Type object
 	 * @return <String> - Template Name
 	 */
-	public function getTemplateName() {
+	public function getTemplateName()
+	{
 		return 'uitypes/SharedOwner.tpl';
 	}
-	
-	public function getListSearchTemplateName() {
-        return 'uitypes/SharedOwnerFieldSearchView.tpl';
-    }
+
+	public function getListSearchTemplateName()
+	{
+		return 'uitypes/SharedOwnerFieldSearchView.tpl';
+	}
 
 	/**
 	 * Function to get the Display Value, for the current field type with given DB Insert Value
 	 * @param <Object> $value
 	 * @return <Object>
 	 */
-	public function getDisplayValue($values) {
-        if($values == NULL) return;
-        foreach(Vtiger_Functions::getArrayFromValue($values) as $value){
+	public function getDisplayValue($values)
+	{
+		if ($values == NULL)
+			return;
+		foreach (Vtiger_Functions::getArrayFromValue($values) as $value) {
 			$userModel = Users_Record_Model::getCleanInstance('Users');
 			$userModel->set('id', $value);
 			$detailViewUrl = $userModel->getDetailViewUrl();
 			$currentUser = Users_Record_Model::getCurrentUserModel();
-			if(!$currentUser->isAdminUser()){
+			if (!$currentUser->isAdminUser()) {
 				return getOwnerName($value);
 			}
-            $displayvalue[] = "<a href=" .$detailViewUrl. ">" .getOwnerName($value). "</a>&nbsp";
-        }
-        $displayvalue = implode(',',$displayvalue);
-        return $displayvalue;
+			$displayvalue[] = "<a href=" . $detailViewUrl . ">" . rtrim(getOwnerName($value)) . "</a>";
+		}
+		$displayvalue = implode(', ', $displayvalue);
+		return $displayvalue;
 	}
 }
