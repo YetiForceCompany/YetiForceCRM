078338616419427c2b0f0831a60bce5bde038029
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-07-09 16:17:30 +0200

    Small improvements

--- api/webservice/modules/Users.php
+++ api/webservice/modules/Users.php
@@ -5,6 +5,6 @@ class API_Users
 
 	public function authentication($email, $password)
 	{
-		
+		return ['auth' => false, 'userID' => 2343, 'fullName' => 'TTTTTT FFFFFFF', 'email' => $email, 'error' => 'Błedne dane dostępowe'];
 	}
 }
