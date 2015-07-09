020c3bcf363395288724010228c7b8ccc728ae8e
Adrian Koń <a.kon@yetiforce.com>
2015-07-09 11:50:41 +0200

    Fix #1320 Improved adding class

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150709';
-$YetiForce_current_version = '2.0.413';
+$YetiForce_current_version = '2.0.414';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
