6828828712c2921063293919231037ffd81a477d
Adrian Koń <a.kon@yetiforce.com>
2015-07-10 14:38:05 +0200

    Fix #1467

--- languages/ru_ru/Menu.php
+++ languages/ru_ru/Menu.php
@@ -1,5 +1,5 @@
 <?php
-/*+***********************************************************************************************************************************
+/* +***********************************************************************************************************************************
  * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
  * in compliance with the License.
  * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
@@ -7,7 +7,7 @@
  * The Original Code is YetiForce.
  * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
  * All Rights Reserved.
- *************************************************************************************************************************************/
+ * *********************************************************************************************************************************** */
 $languageStrings = [
 	'LBL_QUICK_CREATE_MODULE' => 'Быстрое создание',
 	'LBL_SEPARATOR' => 'Разделитель',
@@ -24,5 +24,5 @@ $languageStrings = [
 	'MEN_TEAMWORK' => 'Команда',
 	'MEN_PRODUCTBASE' => 'База товаров',
 	'MEN_LISTS' => 'Список',
-	'MEN_SERVICESBASE' => 'База услуг',	'Menu' => 'Меню',
+	'MEN_SERVICESBASE' => 'База услуг',
 ];
