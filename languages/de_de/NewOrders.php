085444519b083cf6e2e463513c0852e80963eae6
Adrian Koń <a.kon@yetiforce.com>
2015-07-10 14:11:10 +0200

    Fix #1463 Removed unnecessary translations

--- languages/de_de/NewOrders.php
+++ languages/de_de/NewOrders.php
@@ -1,5 +1,5 @@
 <?php
-/*+***********************************************************************************************************************************
+/* +***********************************************************************************************************************************
  * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
  * in compliance with the License.
  * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
@@ -7,17 +7,12 @@
  * The Original Code is YetiForce.
  * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
  * All Rights Reserved.
- *************************************************************************************************************************************/
+ * *********************************************************************************************************************************** */
 $languageStrings = [
 	'NewOrders' => 'Neue Beauftragung',
 	'SINGLE_NewOrders' => 'Neue Beauftragung',
-	'LBL_ADD_RECORD' => 'Beauftragung hinzufügen',
-	'LBL_RECORDS_LIST' => 'Liste Beauftragungen',
-
 	//BLOCKS
 	'LBL_NEWORDERS_INFORMATION' => 'Information',
-
 	//FIELDS
 	'LBL_SUBJECT' => 'Bezeichnung',
-	'LBL_NUMBER' => 'Nummer',
 ];
