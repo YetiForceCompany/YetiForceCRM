a18d3d2268ce194b57652bbe833e32ec6ca133b7
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-07-13 10:18:06 +0200

    Fix #1475

--- languages/pl_pl/Invoice.php
+++ languages/pl_pl/Invoice.php
@@ -1,5 +1,5 @@
 <?php
-/*+***********************************************************************************************************************************
+/* +***********************************************************************************************************************************
  * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
  * in compliance with the License.
  * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
@@ -7,19 +7,19 @@
  * The Original Code is YetiForce.
  * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
  * All Rights Reserved.
- *************************************************************************************************************************************/
+ * *********************************************************************************************************************************** */
 $languageStrings = [
 	'Approved' => 'Zatwierdzona',
-	'AutoCreated'=>'Wymaga weryfikacji',
+	'AutoCreated' => 'Wymaga weryfikacji',
 	'Created' => 'Wersja robocza',
 	'Invoice entered' => 'Zaksięgowana',
 	'Derecognized' => 'Wyksięgowana',
 	'Cancel' => 'Anulowana',
-	'Credit Invoice'=>'Faktura Kredytowa',
+	'Credit Invoice' => 'Faktura Kredytowa',
 	'Customer No' => 'Kontrahent',
 	'Invoice Date' => 'Data faktury',
 	'Invoice No' => 'Numer faktury',
-	'LBL_ACTION'	=> 'Akcja',
+	'LBL_ACTION' => 'Akcja',
 	'LBL_ADD_RECORD' => 'Dodaj rekord',
 	'LBL_BALANCE' => 'Saldo',
 	'LBL_EXPORT_TO_PDF' => 'Eksport do PDF',
@@ -27,12 +27,12 @@ $languageStrings = [
 	'LBL_NOT_A_BUNDLE' => 'Pakiet nie',
 	'LBL_RECEIVED' => 'Odebrane',
 	'LBL_RECORDS_LIST' => 'Lista rekordów',
-	'LBL_SUB_PRODUCTS'	=> 'Produkty cząstkowe',
-	'Paid'=>'Zapłacona',
+	'LBL_SUB_PRODUCTS' => 'Produkty cząstkowe',
+	'Paid' => 'Zapłacona',
 	'Purchase Order' => 'Zamówienie do dostawcy',
 	'Sales Commission' => 'Marża',
 	'Sales Order' => 'Zamówienie',
-	'Sent'=>'Wysłana',
+	'Sent' => 'Wysłana',
 	'SINGLE_Invoice' => 'Faktura',
 	'Potential' => 'Szansa Sprzedaży',
 	'LBL_THIS_LINE_ITEM_IS_DELETED_FROM_THE_SYSTEM_PLEASE_REMOVE_THIS_LINE_ITEM' => 'Produkt/usługa została usunięta z systemu, usuń ją z rekordu.',
@@ -41,6 +41,7 @@ $languageStrings = [
 	'Form of payment' => 'Forma płatności',
 	'Transfer' => 'Przelew',
 	'Cash' => 'Gotówka',
+	'Payment balance' => 'Saldo płatności',
 ];
 
 $jsLanguageStrings = [
