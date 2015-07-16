ba4c495b90696cff99cedc52d8f201c20c985286
MaciejStencel <m.stencel@yetiforce.com>
2015-07-16 15:24:04 +0200

    Fix #1053

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150716';
-$YetiForce_current_version = '2.0.494';
+$YetiForce_current_version = '2.0.495';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
