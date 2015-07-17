878504889a0769f162d3224ca51544bb7a6dd743
Adrian Koń <a.kon@yetiforce.com>
2015-07-17 10:39:48 +0200

    Fix #1533 Increase width of description

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150717';
-$YetiForce_current_version = '2.0.509';
+$YetiForce_current_version = '2.0.510';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
