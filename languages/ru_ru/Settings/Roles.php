b8e8e29700af287717ace94c103b4f955c5a3e14
paula-w <p.walenczak@yetiforce.com>
2015-07-17 11:04:05 +0200

    Improved translations - Roles.php

--- languages/ru_ru/Settings/Roles.php
+++ languages/ru_ru/Settings/Roles.php
@@ -32,8 +32,8 @@ $languageStrings = [
 	'SINGLE_Roles' => 'Роли',
 	'LBL_USERS_LIST' => 'Список пользователей, принадлежащих к этой роли:',
 	'LBL_JUST_ME' => 'Только я',
-	'LBL_POSSIBLE_CHANGE_OWNER_OF_RECORD' => 'Enable name change of the record owner',
-	'LBL_SEARCH_WITHOUT_PERMISSION' => 'Search without permission',
+	'LBL_POSSIBLE_CHANGE_OWNER_OF_RECORD' => 'Enable change of the record owner',
+	'LBL_SEARCH_WITHOUT_PERMISSION' => 'Modules that can be searched without permission',
 ];
 $jsLanguageStrings = [
 	'JS_PERMISSION_DENIED' => 'Доступ запрещен',
