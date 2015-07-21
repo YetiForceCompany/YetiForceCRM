c16f0ca2c768b03298653e1a76db5cc1d540b33f
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-07-21 15:15:31 +0200

    PSR-2 - Corrected code formatting.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '201507121';
-$YetiForce_current_version = '2.0.541';
+$YetiForce_current_version = '2.0.542';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
