14a9acd1be2fde51ec047e9aede50a111f1b18f3
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-07-22 10:12:23 +0200

    Fixed name of the exported file

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '201507122';
-$YetiForce_current_version = '2.0.548';
+$YetiForce_current_version = '2.0.549';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
