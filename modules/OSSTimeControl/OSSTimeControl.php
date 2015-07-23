8b9efd1dd223e6d3242161d2c93819b8a1ee06eb
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-07-23 14:42:32 +0200

    PSR-2 - Corrected code formatting

--- modules/OSSTimeControl/OSSTimeControl.php
+++ modules/OSSTimeControl/OSSTimeControl.php
@@ -1,5 +1,5 @@
 <?php
-/*+***********************************************************************************************************************************
+/* +***********************************************************************************************************************************
  * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
  * in compliance with the License.
  * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
@@ -7,139 +7,138 @@
  * The Original Code is YetiForce.
  * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
  * All Rights Reserved.
- *************************************************************************************************************************************/
+ * *********************************************************************************************************************************** */
 include_once 'modules/Vtiger/CRMEntity.php';
 
-class OSSTimeControl extends Vtiger_CRMEntity {
-    var $table_name = 'vtiger_osstimecontrol';
-    var $table_index= 'osstimecontrolid';
-    var $column_fields = Array();
+class OSSTimeControl extends Vtiger_CRMEntity
+{
 
-    /** Indicator if this is a custom module or standard module */
-    var $IsCustomModule = true;
+	var $table_name = 'vtiger_osstimecontrol';
+	var $table_index = 'osstimecontrolid';
+	var $column_fields = Array();
 
-    /**
-     * Mandatory table for supporting custom fields.
-     */
-    var $customFieldTable = Array('vtiger_osstimecontrolcf', 'osstimecontrolid');
+	/** Indicator if this is a custom module or standard module */
+	var $IsCustomModule = true;
 
-    /**
-     * Mandatory for Saving, Include tables related to this module.
-     */
-    var $tab_name = Array('vtiger_crmentity', 'vtiger_osstimecontrol', 'vtiger_osstimecontrolcf');
+	/**
+	 * Mandatory table for supporting custom fields.
+	 */
+	var $customFieldTable = Array('vtiger_osstimecontrolcf', 'osstimecontrolid');
 
-    /**
-     * Mandatory for Saving, Include tablename and tablekey columnname here.
-     */
-    var $tab_name_index = Array(
-        'vtiger_crmentity' => 'crmid',
-        'vtiger_osstimecontrol'   => 'osstimecontrolid',
-        'vtiger_osstimecontrolcf' => 'osstimecontrolid');
+	/**
+	 * Mandatory for Saving, Include tables related to this module.
+	 */
+	var $tab_name = Array('vtiger_crmentity', 'vtiger_osstimecontrol', 'vtiger_osstimecontrolcf');
 
-    /**
-     * Mandatory for Listing (Related listview)
-     */
-    var $list_fields = Array (
-        /* Format: Field Label => Array(tablename, columnname) */
-        // tablename should not have prefix 'vtiger_'
-        'No.'=> Array('osstimecontrol', 'osstimecontrol_no'),
-		'Assigned To' => Array('crmentity','smownerid'),
-        'Created Time'=>Array('crmentity','createdtime'),
-    );
-    var $list_fields_name = Array(
-        /* Format: Field Label => fieldname */
-        'No.'=> 'osstimecontrol_no',
+	/**
+	 * Mandatory for Saving, Include tablename and tablekey columnname here.
+	 */
+	var $tab_name_index = Array(
+		'vtiger_crmentity' => 'crmid',
+		'vtiger_osstimecontrol' => 'osstimecontrolid',
+		'vtiger_osstimecontrolcf' => 'osstimecontrolid');
+
+	/**
+	 * Mandatory for Listing (Related listview)
+	 */
+	var $list_fields = Array(
+		/* Format: Field Label => Array(tablename, columnname) */
+		// tablename should not have prefix 'vtiger_'
+		'No.' => Array('osstimecontrol', 'osstimecontrol_no'),
+		'Assigned To' => Array('crmentity', 'smownerid'),
+		'Created Time' => Array('crmentity', 'createdtime'),
+	);
+	var $list_fields_name = Array(
+		/* Format: Field Label => fieldname */
+		'No.' => 'osstimecontrol_no',
 		'Assigned To' => 'assigned_user_id',
-        'Created Time'=>'createdtime',
-    );
-
-    // Make the field link to detail view from list view (Fieldname)
-    var $list_link_field = 'assigned_user_id';
-
-    // For Popup listview and UI type support
-    var $search_fields = Array(
-        'No.'=> Array('osstimecontrol', 'osstimecontrol_no'),
-		'Assigned To' => Array('crmentity','smownerid'),
-        'Created Time'=>Array('crmentity','createdtime'),
-    );
-    var $search_fields_name = Array(
-        'No.'=> 'osstimecontrol_no',
+		'Created Time' => 'createdtime',
+	);
+	// Make the field link to detail view from list view (Fieldname)
+	var $list_link_field = 'assigned_user_id';
+	// For Popup listview and UI type support
+	var $search_fields = Array(
+		'No.' => Array('osstimecontrol', 'osstimecontrol_no'),
+		'Assigned To' => Array('crmentity', 'smownerid'),
+		'Created Time' => Array('crmentity', 'createdtime'),
+	);
+	var $search_fields_name = Array(
+		'No.' => 'osstimecontrol_no',
 		'Assigned To' => 'assigned_user_id',
-        'Created Time'=>'createdtime',
-    );
+		'Created Time' => 'createdtime',
+	);
+	// For Popup window record selection
+	var $popup_fields = Array('name');
+	// Placeholder for sort fields - All the fields will be initialized for Sorting through initSortFields
+	var $sortby_fields = Array();
+	// For Alphabetical search
+	var $def_basicsearch_col = 'name';
+	// Column value to use on detail view record text display
+	var $def_detailview_recname = 'name';
+	// Required Information for enabling Import feature
+	var $required_fields = Array('assigned_user_id' => 1);
+	// Callback function list during Importing
+	var $special_functions = Array('set_import_assigned_user');
+	var $default_order_by = 'date_start,time_start';
+	var $default_sort_order = 'DESC';
+	// Used when enabling/disabling the mandatory fields for the module.
+	// Refers to vtiger_field.fieldname values.
+	var $mandatory_fields = Array('createdtime', 'modifiedtime', 'assigned_user_id');
 
-    // For Popup window record selection
-    var $popup_fields = Array('name');
-
-    // Placeholder for sort fields - All the fields will be initialized for Sorting through initSortFields
-    var $sortby_fields = Array();
-
-    // For Alphabetical search
-    var $def_basicsearch_col = 'name';
-
-    // Column value to use on detail view record text display
-    var $def_detailview_recname = 'name';
-
-    // Required Information for enabling Import feature
-    var $required_fields = Array('assigned_user_id'=>1);
-
-    // Callback function list during Importing
-    var $special_functions = Array('set_import_assigned_user');
-
-    var $default_order_by = 'date_start,time_start';
-    var $default_sort_order='DESC';
-    // Used when enabling/disabling the mandatory fields for the module.
-    // Refers to vtiger_field.fieldname values.
-    var $mandatory_fields = Array('createdtime', 'modifiedtime', 'assigned_user_id');
-
-    /**
-     * Invoked when special actions are performed on the module.
-     * @param String Module name
-     * @param String Event Type (module.postinstall, module.disabled, module.enabled, module.preuninstall)
-     */
-    function vtlib_handler($modulename, $event_type) {
+	/**
+	 * Invoked when special actions are performed on the module.
+	 * @param String Module name
+	 * @param String Event Type (module.postinstall, module.disabled, module.enabled, module.preuninstall)
+	 */
+	function vtlib_handler($modulename, $event_type)
+	{
 		$registerLink = false;
 		$displayLabel = 'Time Control';
-		$adb = PearDatabase::getInstance(); $log = vglobal('log');
-        if($event_type == 'module.postinstall') {
+		$adb = PearDatabase::getInstance();
+		$log = vglobal('log');
+		if ($event_type == 'module.postinstall') {
 
 			$tabid = getTabid($modulename);
-			$adb->query( "UPDATE `vtiger_field` SET `summaryfield` = '1' WHERE `tabid` = $tabid AND `columnname` IN ('name','osstimecontrol_no','osstimecontrol_status','smownerid','date_start','time_start','time_end','due_date','sum_time','platnosc');", true );
+			$adb->query("UPDATE `vtiger_field` SET `summaryfield` = '1' WHERE `tabid` = $tabid AND `columnname` IN ('name','osstimecontrol_no','osstimecontrol_status','smownerid','date_start','time_start','time_end','due_date','sum_time','platnosc');", true);
 			$ModuleInstance = CRMEntity::getInstance($modulename);
-			$ModuleInstance->setModuleSeqNumber("configure",$modulename,'TC','1');
-			include_once('vtlib/Vtiger/Module.php'); 
+			$ModuleInstance->setModuleSeqNumber("configure", $modulename, 'TC', '1');
+			include_once('vtlib/Vtiger/Module.php');
 
 			$modcommentsModuleInstance = Vtiger_Module::getInstance('ModComments');
-			if($modcommentsModuleInstance && file_exists('modules/ModComments/ModComments.php')) {
+			if ($modcommentsModuleInstance && file_exists('modules/ModComments/ModComments.php')) {
 				include_once 'modules/ModComments/ModComments.php';
-			if(class_exists('ModComments')) ModComments::addWidgetTo(array('OSSTimeControl'));
+				if (class_exists('ModComments'))
+					ModComments::addWidgetTo(array('OSSTimeControl'));
 			}
+		} else if ($event_type == 'module.disabled') {
+			// TODO Handle actions when this module is disabled.
+		} else if ($event_type == 'module.enabled') {
+			// TODO Handle actions when this module is enabled.
+		} else if ($event_type == 'module.preuninstall') {
+			// TODO Handle actions when this module is about to be deleted.
+		} else if ($event_type == 'module.preupdate') {
+			// TODO Handle actions before this module is updated.
+		} else if ($event_type == 'module.postupdate') {
 			
-        } else if($event_type == 'module.disabled') {
-            // TODO Handle actions when this module is disabled.
-        } else if($event_type == 'module.enabled') {
-            // TODO Handle actions when this module is enabled.
-        } else if($event_type == 'module.preuninstall') {
-            // TODO Handle actions when this module is about to be deleted.
-        } else if($event_type == 'module.preupdate') {
-            // TODO Handle actions before this module is updated.
-        } else if($event_type == 'module.postupdate') {
-		
-        }
-    }
-	function retrieve_entity_info($record, $module) {
+		}
+	}
+
+	function retrieve_entity_info($record, $module)
+	{
 		parent::retrieve_entity_info($record, $module);
-		$start = DateTimeField::convertToUserTimeZone($this->column_fields['date_start'].' '.$this->column_fields['time_start']);
+		$start = DateTimeField::convertToUserTimeZone($this->column_fields['date_start'] . ' ' . $this->column_fields['time_start']);
 		$this->column_fields['date_start'] = $start->format('Y-m-d');
-		$end = DateTimeField::convertToUserTimeZone($this->column_fields['due_date'].' '.$this->column_fields['time_end']);
+		$end = DateTimeField::convertToUserTimeZone($this->column_fields['due_date'] . ' ' . $this->column_fields['time_end']);
 		$this->column_fields['due_date'] = $end->format('Y-m-d');
 	}
-	function saveentity($module_name, $fileid = ''){
+
+	function saveentity($module_name, $fileid = '')
+	{
 		$date_start = $this->column_fields['date_start'];
 		$due_date = $this->column_fields['due_date'];
-		$start = DateTimeField::convertToDBTimeZone($this->column_fields['date_start'].' '.$this->column_fields['time_start']);
+		$start = DateTimeField::convertToDBTimeZone($this->column_fields['date_start'] . ' ' . $this->column_fields['time_start']);
 		$this->column_fields['date_start'] = $start->format(DateTimeField::getPHPDateFormat());
-		$end = DateTimeField::convertToDBTimeZone($this->column_fields['due_date'].' '.$this->column_fields['time_end']);
+		$end = DateTimeField::convertToDBTimeZone($this->column_fields['due_date'] . ' ' . $this->column_fields['time_end']);
 		$this->column_fields['due_date'] = $end->format(DateTimeField::getPHPDateFormat());
 		parent::saveentity($module_name, $fileid = '');
 		$this->column_fields['date_start'] = $date_start;
--- modules/OSSTimeControl/views/UsersList.php
+++ modules/OSSTimeControl/views/UsersList.php
@@ -1,5 +1,5 @@
 <?php
-/*+***********************************************************************************************************************************
+/* +***********************************************************************************************************************************
  * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
  * in compliance with the License.
  * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
@@ -7,16 +7,19 @@
  * The Original Code is YetiForce.
  * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
  * All Rights Reserved.
- *************************************************************************************************************************************/
+ * *********************************************************************************************************************************** */
 
-class OSSTimeControl_UsersList_View extends Vtiger_IndexAjax_View {
+class OSSTimeControl_UsersList_View extends Vtiger_IndexAjax_View
+{
 
-    function __construct() {
-        parent::__construct();
-        $this->exposeMethod('getUsersList');
-    }
-        
-	function getUsersList(Vtiger_Request $request) {
+	function __construct()
+	{
+		parent::__construct();
+		$this->exposeMethod('getUsersList');
+	}
+
+	function getUsersList(Vtiger_Request $request)
+	{
 		$viewer = $this->getViewer($request);
 		$moduleName = $request->getModule();
 		$currentUser = Users_Record_Model::getCurrentUserModel();
