8496ea0a3a3497d8d6d9e8ff261bd9687addab39
Adrian Koń <a.kon@yetiforce.com>
2015-07-31 08:33:47 +0200

    Improved translations Workflows names

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150731';
-$YetiForce_current_version = '2.0.689';
+$YetiForce_current_version = '2.0.690';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
