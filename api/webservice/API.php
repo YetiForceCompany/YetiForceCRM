2ee4df6d8358140e41ce6d3c20bbe461d21f42b1
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-08-01 00:29:58 +0200

    Added additional security to webservice API - part 2

--- api/webservice/API.php
+++ api/webservice/API.php
@@ -10,7 +10,8 @@ class API
 	 */
 	protected $method = '';
 	protected $acceptableMethods = ['GET', 'POST', 'PUT', 'DELETE'];
-	protected $modulesPath = 'api/webservice/modules/';
+	protected $modulesPath = 'api/webservice/';
+	protected $panel = '';
 
 	public function __construct()
 	{
@@ -32,7 +33,7 @@ class API
 		if (!in_array($this->method, $this->acceptableMethods)) {
 			throw new APIException('Invalid Method', 405);
 		}
-		
+
 		if (isset($_REQUEST['head']['encrypted']) && $_REQUEST['head']['encrypted']) {
 			$_REQUEST['data'] = $this->decryptData($_REQUEST['data']);
 		}
@@ -60,23 +61,37 @@ class API
 		return ($status[$code]) ? $status[$code] : $status[500];
 	}
 
+	public function preProcess()
+	{
+		$head = $this->request->get('head');
+		$apiKey = $head['apiKey'];
+		if ($apiKey == '') {
+			throw new APIException('Invalid api key');
+		}
+		$this->panel = 'Portal';
+	}
+
 	public function process()
 	{
-		$filePath = $this->modulesPath . $this->request->get('module') . '.php';
+		$filePath = $this->modulesPath . $this->panel . '/modules/' . $this->request->get('module') . '/' . $this->request->get('action') . '.php';
 		if (!file_exists($filePath)) {
-			throw new APIException('File does not exist: ' . $filePath);
+			throw new APIException('No action found: ' . $filePath);
 		}
 		require_once $filePath;
-		$handlerClass = 'API_' . $this->request->get('module');
+		$handlerClass = 'API_' . $this->request->get('module') . '_' . $this->request->get('action');
 		if (!class_exists($handlerClass)) {
 			throw new APIException('HANDLER_NOT_FOUND: ' . $handlerClass);
 		}
 
 		$handler = new $handlerClass();
-		$function = 'process';
+		if ($handler->getRequestMethod() != $this->method) {
+			throw new APIException('Invalid request type');
+		}
+
 		if ($this->request->get('action') != '') {
 			$function = $this->request->get('action');
 		}
+		
 		$response = call_user_func_array([$handler, $function], $this->data->getAll());
 		if (vglobal('encryptDataTransfer')) {
 			$response = $this->encryptData($response);
@@ -86,6 +101,11 @@ class API
 			'encrypted' => vglobal('encryptDataTransfer'),
 			'result' => $response
 		]);
+	}
+
+	public function postProcess()
+	{
+		
 	}
 
 	public function encryptData($data)
