a51b4f4e4c58b39a1431a714e2251cf85ad2e2ca
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-07-31 15:48:48 +0200

    Improved select2 while adding  a filter widget.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150731';
-$YetiForce_current_version = '2.0.699';
+$YetiForce_current_version = '2.0.700';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
