3a483168921c2a0e88423380f6435833c6784921
Adrian Koń <a.kon@yetiforce.com>
2015-08-06 14:56:07 +0200

    Improved buttons style

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150806';
-$YetiForce_current_version = '2.0.737';
+$YetiForce_current_version = '2.0.738';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
