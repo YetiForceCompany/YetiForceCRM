f4cded7a295282afb2d96d438f2cfa16962d96df
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-08-11 22:22:31 +0200

    Added improvements to API

--- api/webservice/Portal/modules/Users/Authentication.php
+++ api/webservice/Portal/modules/Users/Authentication.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Users authentication action class
  * @package YetiForce.WebserviceAction
@@ -7,17 +8,21 @@
  */
 class API_Users_Authentication extends BaseAction
 {
+
 	protected $requestMethod = 'POST';
-	
+
 	public function authentication($email, $password)
 	{
-		throw new APIException('Błedne dane dostępowe');
-		
+		//throw new APIException('Błedne dane dostępowe');
+
 		return [
-			'auth' => false,
-			'userID' => 2343,
-			'fullName' => 'TTTTTT FFFFFFF',
-			'email' => $email,
+			'logged' => false,
+			'id' => 111,
+			'fullName' => 'Mariusz Krzaczkowski',
+			'email' => 'm.krzaczkowski@yetiforce.com',
+			'lastLoginTime' => 'xx',
+			'supportStartDate' => 'xx',
+			'supportEndDate' => 'xx',
 		];
 	}
 }
