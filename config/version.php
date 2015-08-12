5591c81df11c961c0558c6c7be12bcbca8fab2d8
Adrian Koń <a.kon@yetiforce.com>
2015-08-12 09:25:00 +0200

    Commented out code repsonible for granting index to the alert (class)

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150806';
-$YetiForce_current_version = '2.1.72';
+$YetiForce_current_version = '2.1.73';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
