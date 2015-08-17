51622615d02df879233c7ace511a8303aaf41b35
MaciejStencel <m.stencel@yetiforce.com>
2015-08-17 18:38:18 +0200

    Fix #1808 - Fixed saving group tax in invoice.

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20150818';
-$YetiForce_current_version = '2.1.115';
+$patch_version = '20150817';
+$YetiForce_current_version = '2.1.116';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
