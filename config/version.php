082036d66e6eb47cedd850bbe9dd3b68201e8425
Adrian Koń <a.kon@yetiforce.com>
2015-08-17 09:31:15 +0200

    Fix #1802 Added translations

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150815';
-$YetiForce_current_version = '2.1.109';
+$YetiForce_current_version = '2.1.110';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
