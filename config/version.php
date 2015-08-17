293c405b3ac4cc02a0fba291edf6ee0974edeb8c
Adrian Koń <a.kon@yetiforce.com>
2015-08-17 07:52:51 +0200

    Fix #1797 Improved cancel button display

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150815';
-$YetiForce_current_version = '2.1.104';
+$YetiForce_current_version = '2.1.105';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
