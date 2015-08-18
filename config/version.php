14ac148bf95128dbe5254752afec0d6b74c32594
Adrian Koń <a.kon@yetiforce.com>
2015-08-18 14:42:18 +0200

    Improved display of the add button in Activities widget

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150817';
-$YetiForce_current_version = '2.1.123';
+$YetiForce_current_version = '2.1.124';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
