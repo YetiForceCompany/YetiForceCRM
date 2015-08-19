a9d5a077baca6ac6cad42e703f92b5f60a4d3be5
Adrian Koń <a.kon@yetiforce.com>
2015-08-19 09:20:18 +0200

    Improved code formating

--- languages/nl_nl/PurchaseOrder.php
+++ languages/nl_nl/PurchaseOrder.php
@@ -1,5 +1,5 @@
 <?php
-/*+***********************************************************************************
+/* +***********************************************************************************
  * The contents of this file are subject to the vtiger CRM Public License Version 1.0
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
@@ -7,20 +7,20 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
- ************************************************************************************/
+ * ********************************************************************************** */
 $languageStrings = [
 	'LBL_ADD_RECORD' => 'Add record',
 	'LBL_BALANCE' => 'Balance',
 	'LBL_COPY_BILLING_ADDRESS' => 'Copy Billing Address',
 	'LBL_COPY_SHIPPING_ADDRESS' => 'Copy Shipping Address',
 	'LBL_EXPORT_TO_PDF' => 'Export to PDF',
-	'LBL_LIST_PRICE'               => 'List Price',
+	'LBL_LIST_PRICE' => 'List Price',
 	'LBL_PAID' => 'Paid',
 	'LBL_PO_INFORMATION' => 'Purchase Order Details',
 	'LBL_RECORDS_LIST' => 'Purchase Order List',
-	'List Price'                   => 'List Price',
+	'List Price' => 'List Price',
 	'PurchaseOrder No' => 'Purchase Order Number',
-	'Received Shipment'=>'Received Shipment',
+	'Received Shipment' => 'Received Shipment',
 	'Requisition No' => 'Requisition Number',
 	'Sales Commission' => 'Sales Commission',
 	'SINGLE_PurchaseOrder' => 'Purchase Order',
