b5e996da7f30b20b26e43224e572f2c41455e9d6
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-08-19 09:25:24 +0200

    Fixed election mail

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20150817';
-$YetiForce_current_version = '2.1.130';
+$patch_version = '20150819';
+$YetiForce_current_version = '2.1.131';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
