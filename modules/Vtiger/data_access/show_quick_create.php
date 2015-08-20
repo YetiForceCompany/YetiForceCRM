014221072bdfc8471a7b2ac12fbdcea828191716
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-08-20 09:15:58 +0200

    Improved code formating.

--- modules/Vtiger/data_access/show_quick_create.php
+++ modules/Vtiger/data_access/show_quick_create.php
@@ -1,41 +1,46 @@
 <?php
 /*
-Return Description
-------------------------
-Info type: error, info, success
-Info title: optional
-Info text: mandatory
-Type: 0 - notify
-Type: 1 - show quick create mondal
-*/
-Class DataAccess_show_quick_create{
-    var $config = true;
-	
-    public function process( $ModuleName,$ID,$record_form,$config ) {
+  Return Description
+  ------------------------
+  Info type: error, info, success
+  Info title: optional
+  Info text: mandatory
+  Type: 0 - notify
+  Type: 1 - show quick create mondal
+ */
+
+Class DataAccess_show_quick_create
+{
+
+	var $config = true;
+
+	public function process($ModuleName, $ID, $record_form, $config)
+	{
 		$db = PearDatabase::getInstance();
-		if( !isset($ID)|| $ID == 0 || $ID == '')
-			return Array('save_record' => true );
+		if (!isset($ID) || $ID == 0 || $ID == '')
+			return Array('save_record' => true);
 		return Array(
 			'save_record' => false,
 			'type' => 1,
 			'module' => $config['modules']
 		);
-    }
+	}
 
-    public function getConfig( $id,$module,$baseModule ) {
+	public function getConfig($id, $module, $baseModule)
+	{
 		$db = PearDatabase::getInstance();
 		$modulesQuickCreate = Vtiger_Module_Model::getQuickCreateModules(true);
 		$modules = [];
-		foreach($modulesQuickCreate as $moduleName=>$moduleModel){
+		foreach ($modulesQuickCreate as $moduleName => $moduleModel) {
 			$quickCreateModule = $moduleModel->isQuickCreateSupported();
 			$singularLabel = $moduleModel->getSingularLabelKey();
-			if($singularLabel == 'SINGLE_Calendar'){
+			if ($singularLabel == 'SINGLE_Calendar') {
 				$singularLabel = 'LBL_EVENT_OR_TASK';
 			}
-			if($quickCreateModule == 1){
+			if ($quickCreateModule == 1) {
 				$modules[$moduleName] = $singularLabel;
 			}
 		}
-		return Array('modules'=>$modules);
-    }
+		return Array('modules' => $modules);
+	}
 }
