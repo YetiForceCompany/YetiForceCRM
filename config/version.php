77edb3f104447fc0bed561193791222544a8ff7f
Adrian Koń <a.kon@yetiforce.com>
2015-08-21 13:31:26 +0200

    Added hiding modal window

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150819';
-$YetiForce_current_version = '2.1.164';
+$YetiForce_current_version = '2.1.165';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
