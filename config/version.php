06cebd25dc1fc8d879324e3b9439e080e9ab1524
Adrian Koń <a.kon@yetiforce.com>
2015-08-21 11:56:29 +0200

    Improved pagination on users

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150819';
-$YetiForce_current_version = '2.1.161';
+$YetiForce_current_version = '2.1.162';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
