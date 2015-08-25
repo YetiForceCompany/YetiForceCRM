6d482e774d3472ae4c2d53ffc49908574fb549e0
Adrian Koń <a.kon@yetiforce.com>
2015-08-25 11:23:05 +0200

    Removed the possibility to change a module in data access validation

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150823';
-$YetiForce_current_version = '2.1.175';
+$YetiForce_current_version = '2.1.176';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
