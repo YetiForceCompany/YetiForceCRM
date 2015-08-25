1eb1e91be52caddaefe7ee0c87e44c424b43ba8a
Adrian Koń <a.kon@yetiforce.com>
2015-08-25 08:36:28 +0200

    Fixed creating a list of related module fields when selecting a module

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150823';
-$YetiForce_current_version = '2.1.171';
+$YetiForce_current_version = '2.1.172';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
