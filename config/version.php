7e3da801142c40bc8d3505ad7860d29893eade50
Adrian Koń <a.kon@yetiforce.com>
2015-08-26 08:58:03 +0200

    Align fields

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150823';
-$YetiForce_current_version = '2.1.184';
+$YetiForce_current_version = '2.1.185';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
