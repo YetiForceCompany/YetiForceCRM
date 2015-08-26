6648cb619e5819e28f3f261594ca28ebd02a2df7
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-08-26 10:40:39 +0200

    Added scanning the "Answer to" field to the mail scanner.

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20150823';
-$YetiForce_current_version = '2.1.187';
+$patch_version = '20150826';
+$YetiForce_current_version = '2.1.188';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
