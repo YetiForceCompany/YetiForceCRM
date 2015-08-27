319dcd5b5422ccffd1645cb2e7dfb8d76fd79ea8
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-08-27 02:12:44 +0200

    Improved code formating.

--- layouts/vlayout/modules/Settings/DataAccess/resources/SaveResult.js
+++ layouts/vlayout/modules/Settings/DataAccess/resources/SaveResult.js
@@ -12,10 +12,10 @@ function SaveResult() {
 	this.skipCheckDataOneTime = '';
 	this.executeTaskStatus = true
 	this.recordValue = false;
-	this.loadFormData = function(formData) {
+	this.loadFormData = function (formData) {
 		this.recordValue = formData
 	}
-	this.checkData = function(formData,form) {
+	this.checkData = function (formData, form) {
 		var thisInstnce = this;
 		var params = {};
 		var resp = [];
@@ -24,62 +24,62 @@ function SaveResult() {
 		delete thisInstnce.recordValue['picklistDependency'];
 		delete formData['__vtrftk'];
 		delete formData['picklistDependency'];
-		$.each( thisInstnce.recordValue, function( key, value ) {
-			formData['p_'+key] = value;
+		$.each(thisInstnce.recordValue, function (key, value) {
+			formData['p_' + key] = value;
 		});
 		params.data = {
 			module: 'DataAccess',
-			parent: 'Settings', 
+			parent: 'Settings',
 			action: 'ExecuteHandlers',
 			param: formData,
 		};
 		params.async = false;
 		params.dataType = 'json';
 		AppConnector.request(params).then(
-			function(response) {
-				resp = response['result']['data'];
-				Vtiger_Helper_Js.hidePnotify();
-				$.each( resp, function( key, object ) {
-					if(jQuery.inArray(key, thisInstnce.skipCheckData) != -1 || thisInstnce.skipCheckDataOneTime === key){
-						return;
-					}
-					if(typeof object.type != 'undefined'){
-						staus = thisInstnce.executeTask(object,key,form)
-					}
-				});
-				thisInstnce.skipCheckDataOneTime = '';
-			}
+				function (response) {
+					resp = response['result']['data'];
+					Vtiger_Helper_Js.hidePnotify();
+					$.each(resp, function (key, object) {
+						if (jQuery.inArray(key, thisInstnce.skipCheckData) != -1 || thisInstnce.skipCheckDataOneTime === key) {
+							return;
+						}
+						if (typeof object.type != 'undefined') {
+							staus = thisInstnce.executeTask(object, key, form)
+						}
+					});
+					thisInstnce.skipCheckDataOneTime = '';
+				}
 		);
 		return staus; // staus ==  true = ok, false = block edit
 	}
-	this.executeTask = function(data,key,form) {
+	this.executeTask = function (data, key, form) {
 		switch (data.type) {
 			case 0:
 				this.showNotify(data.info)
 				this.executeTaskStatus = data.save_record;
 				break;
 			case 1:
-				this.showQuickCreate(data.module,this.executeTaskStatus,form,key)
+				this.showQuickCreate(data.module, this.executeTaskStatus, form, key)
 				view = app.getViewName();
-				if( view != 'Detail'){
+				if (view != 'Detail') {
 					this.executeTaskStatus = false
 				}
 				break;
 			case 2:
-				if(data.save_record){
+				if (data.save_record) {
 					this.executeTaskStatus = data.save_record;
 					break;
 				}
-				this.showNotify(data.info,data.type,form,key)
+				this.showNotify(data.info, data.type, form, key)
 				view = app.getViewName();
-				if( view != 'Detail'){
+				if (view != 'Detail') {
 					this.executeTaskStatus = false
 				}
 				break;
 		}
 		return this.executeTaskStatus; // true = ok, false = block edit
 	}
-	this.showNotify = function(info,type,form,key) {
+	this.showNotify = function (info, type, form, key) {
 		var instance = this;
 		var params = {
 			text: info.text,
@@ -87,21 +87,21 @@ function SaveResult() {
 			animation: 'show',
 			width: 'auto'
 		};
-		if(info.ntype){
+		if (info.ntype) {
 			params.type = info.ntype;
 		}
-		if(typeof form != 'undefined' && type == 2){
+		if (typeof form != 'undefined' && type == 2) {
 			params.confirm = {
 				confirm: true,
 				buttons: [{
-					text: app.vtranslate('JS_OK'),
-					addClass: 'btn-primary saveButton',
-					click: function(notice){
-						instance.skipCheckData.push(key);
-						notice.remove();
-						form.submit();
-					}
-				},{	addClass: 'hide',}]
+						text: app.vtranslate('JS_OK'),
+						addClass: 'btn-primary saveButton',
+						click: function (notice) {
+							instance.skipCheckData.push(key);
+							notice.remove();
+							form.submit();
+						}
+					}, {addClass: 'hide', }]
 			};
 			params.buttons = {
 				closer: false,
@@ -111,41 +111,43 @@ function SaveResult() {
 				history: false
 			};
 			//waiting time before automatic saving.
-			params.before_open = function(notice) {
-				setTimeout(function () {notice.get().find('.saveButton').trigger('click');}, 10000);
+			params.before_open = function (notice) {
+				setTimeout(function () {
+					notice.get().find('.saveButton').trigger('click');
+				}, 10000);
 			}
 		}
-		params = jQuery.extend(info,params);
+		params = jQuery.extend(info, params);
 		Vtiger_Helper_Js.showPnotify(params);
 	}
 
-	this.showQuickCreate = function(moduleName,orgExecuteTaskStatus,form,key) {
+	this.showQuickCreate = function (moduleName, orgExecuteTaskStatus, form, key) {
 		var instance = this;
 		var sourceRecord = jQuery('input[name="record"]').val();
-		if(sourceRecord == undefined){
+		if (sourceRecord == undefined) {
 			sourceRecord = jQuery('#recordId').val();
 		}
 		var sourceModule = jQuery('input[name="module"]').val();
-		var preQuickCreateSave = function(data){
-			var index,queryParam,queryParamComponents;
-			if(moduleName == 'Calendar'){
-				jQuery('<input type="hidden" name="parent_id" value="'+sourceRecord+'">').appendTo(data);
+		var preQuickCreateSave = function (data) {
+			var index, queryParam, queryParamComponents;
+			if (moduleName == 'Calendar') {
+				jQuery('<input type="hidden" name="parent_id" value="' + sourceRecord + '">').appendTo(data);
 			}
-			if(sourceModule == 'HelpDesk'){
-				jQuery('<input type="hidden" name="ticketid" value="'+sourceRecord+'">').appendTo(data);
+			if (sourceModule == 'HelpDesk') {
+				jQuery('<input type="hidden" name="ticketid" value="' + sourceRecord + '">').appendTo(data);
 			}
-			if(sourceModule == 'ProjectTask'){
-				jQuery('<input type="hidden" name="projecttaskid" value="'+sourceRecord+'">').appendTo(data);
+			if (sourceModule == 'ProjectTask') {
+				jQuery('<input type="hidden" name="projecttaskid" value="' + sourceRecord + '">').appendTo(data);
 			}
-			if(sourceModule == 'Potentials'){
-				jQuery('<input type="hidden" name="potentialid" value="'+sourceRecord+'">').appendTo(data);
+			if (sourceModule == 'Potentials') {
+				jQuery('<input type="hidden" name="potentialid" value="' + sourceRecord + '">').appendTo(data);
 			}
-			jQuery('<input type="hidden" name="sourceModule" value="'+sourceModule+'" />').appendTo(data);
-			jQuery('<input type="hidden" name="sourceRecord" value="'+sourceRecord+'" />').appendTo(data);
+			jQuery('<input type="hidden" name="sourceModule" value="' + sourceModule + '" />').appendTo(data);
+			jQuery('<input type="hidden" name="sourceRecord" value="' + sourceRecord + '" />').appendTo(data);
 			jQuery('<input type="hidden" name="relationOperation" value="true" />').appendTo(data);
 		}
-		var postQuickCreateSave  = function(data) {
-			if(typeof form != 'undefined'){
+		var postQuickCreateSave = function (data) {
+			if (typeof form != 'undefined') {
 				instance.skipCheckData.push(key);
 				form.submit();
 			}
@@ -156,7 +158,7 @@ function SaveResult() {
 		relatedParams['sourceModule'] = sourceModule;
 		relatedParams['sourceRecord'] = sourceRecord;
 		relatedParams['relationOperation'] = true;
-		if((app.getViewName() == 'Detail' || app.getViewName() == 'Edit') && moduleName == 'OSSTimeControl' ){
+		if ((app.getViewName() == 'Detail' || app.getViewName() == 'Edit') && moduleName == 'OSSTimeControl') {
 			relatedParams['name'] = jQuery('.recordLabel').attr('title');
 		}
 		quickCreateParams['callbackFunction'] = postQuickCreateSave;
@@ -165,11 +167,11 @@ function SaveResult() {
 		quickCreateParams['noCache'] = true;
 		var progress = jQuery.progressIndicator();
 		var headerInstance = new Vtiger_Header_Js();
-		headerInstance.getQuickCreateForm(quickcreateUrl, moduleName, quickCreateParams).then(function(data) {
+		headerInstance.getQuickCreateForm(quickcreateUrl, moduleName, quickCreateParams).then(function (data) {
 			headerInstance.handleQuickCreateData(data, quickCreateParams);
 			progress.progressIndicator({'mode': 'hide'});
-			jQuery('form[name="QuickCreate"]').closest('.modal').on('click','.cancelLinkContainer,.close', function(e){
-				if(typeof form != 'undefined'){
+			jQuery('form[name="QuickCreate"]').closest('.modal').on('click', '.cancelLinkContainer,.close', function (e) {
+				if (typeof form != 'undefined') {
 					instance.skipCheckDataOneTime = key;
 					form.submit();
 				}
