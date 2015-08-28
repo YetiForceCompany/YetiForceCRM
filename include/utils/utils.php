ffb2188c699c63051f732616c7d6128810ae877f
MaciejStencel <m.stencel@yetiforce.com>
2015-08-28 16:48:32 +0200

    Fixed error in dabatase query causing "Cardinality violation: 1222" error.

--- include/utils/utils.php
+++ include/utils/utils.php
@@ -135,10 +135,10 @@ function get_user_array($add_blank = true, $status = "Active", $assigned_user = 
 			if ($private == 'private') {
 				$log->debug("Sharing is Private. Only the current user should be listed");
 				$query = "select id as id,user_name as user_name,first_name,last_name,is_admin from vtiger_users where id=? and status='Active' union select vtiger_user2role.userid as id,vtiger_users.user_name as user_name ,
-							  vtiger_users.first_name as first_name ,vtiger_users.last_name as last_name
+							  vtiger_users.first_name as first_name ,vtiger_users.last_name as last_name, is_admin 
 							  from vtiger_user2role inner join vtiger_users on vtiger_users.id=vtiger_user2role.userid inner join vtiger_role on vtiger_role.roleid=vtiger_user2role.roleid where vtiger_role.parentrole like ? and status='Active' union
 							  select shareduserid as id,vtiger_users.user_name as user_name ,
-							  vtiger_users.first_name as first_name ,vtiger_users.last_name as last_name  from vtiger_tmp_write_user_sharing_per inner join vtiger_users on vtiger_users.id=vtiger_tmp_write_user_sharing_per.shareduserid where status='Active' and vtiger_tmp_write_user_sharing_per.userid=? and vtiger_tmp_write_user_sharing_per.tabid=?";
+							  vtiger_users.first_name as first_name ,vtiger_users.last_name as last_name, is_admin from vtiger_tmp_write_user_sharing_per inner join vtiger_users on vtiger_users.id=vtiger_tmp_write_user_sharing_per.shareduserid where status='Active' and vtiger_tmp_write_user_sharing_per.userid=? and vtiger_tmp_write_user_sharing_per.tabid=?";
 				$params = array($current_user->id, $current_user_parent_role_seq . "::%", $current_user->id, getTabid($module));
 			} else {
 				$log->debug("Sharing is Public. All vtiger_users should be listed");
