dffa7ea0354d49d918abf704ca85da4f9cf59676
Adrian Koń <a.kon@yetiforce.com>
2015-08-28 12:38:44 +0200

    Changed file extension

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150827';
-$YetiForce_current_version = '2.1.229';
+$YetiForce_current_version = '2.1.230';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
