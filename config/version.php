b66da6d01cf2c067feab270ea66ba61b5c31e920
MaciejStencel <m.stencel@yetiforce.com>
2015-09-01 00:56:10 +0200

    Fix #1889 - Fixed showing of "Add record" button in Sold Services for users w/o write permissions in Potentials.

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20150831';
-$YetiForce_current_version = '2.2.6';
+$patch_version = '20150901';
+$YetiForce_current_version = '2.2.7';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
