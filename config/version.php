2e66378bdf6101d1ad78fc5bfdb67bda6569c95a
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-08-31 10:43:59 +0200

    Adeed relating the Vendor to the Tickets and the Project.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150831';
-$YetiForce_current_version = '2.1.237';
+$YetiForce_current_version = '2.1.238';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
