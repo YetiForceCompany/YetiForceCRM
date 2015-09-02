2f488b6af94d62a5783e26f8a88afcbcd1dc06ba
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-09-02 12:02:26 +0200

    Inventory module - Fixed display values

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150902';
-$YetiForce_current_version = '2.2.42';
+$YetiForce_current_version = '2.2.43';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
