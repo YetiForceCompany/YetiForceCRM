0f9d4b89abec0ac07cfa576330239c370c69e349
Paula W <p.walenczak@yetiforce.com>
2015-09-02 10:17:36 +0200

    Added translations - ModuleManager.php

--- languages/de_de/Settings/ModuleManager.php
+++ languages/de_de/Settings/ModuleManager.php
@@ -68,9 +68,9 @@ $languageStrings = [
 	'LBL_ENTITY_FIELDLABEL' => 'Zugehöriges Feldlabel',
 	'LBL_MODULE_ALREADY_EXISTS_TRY_ANOTHER' => 'Der Modulname existiert bereits, bitte anderen Namen wählen',
 	'LBL_INVALID_MODULE_NAME' => 'Fehlerhafter Modulname',
-	'LBL_MODULE_TYPE' => '__UNTRANSLATED__',
-	'LBL_BASE_MODULE' => '__UNTRANSLATED__',
-	'LBL_INVENTORY_MODULE' => '__UNTRANSLATED__',
+	'LBL_MODULE_TYPE' => 'Module type',
+	'LBL_BASE_MODULE' => 'Base module',
+	'LBL_INVENTORY_MODULE' => 'Extended module',
 ];
 $jsLanguageStrings = [
 	'JS_UPDATED_MODULE' => 'Update Erfolgreich',
