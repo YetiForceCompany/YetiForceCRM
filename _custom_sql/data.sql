ac1f8c1ee35bfde3c17b64f880cb9e6d8f9d9524
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-09-07 09:02:29 +0200

    change ID

--- _custom_sql/data.sql
+++ _custom_sql/data.sql
@@ -7,4 +7,4 @@ MySQL - 5.6.17
 
 insert into `a_yf_pdf` (`pdfid`, `module_name`, `summary`, `cola`, `colb`, `colc`, `cold`) values('1','Potentials','summary', 'a','b','c','d');
 insert into `vtiger_settings_field` (`fieldid`, `blockid`, `name`, `iconpath`, `description`, `linkto`, `sequence`, `active`, `pinned`) values ('89', '4', 'LBL_PDF', '', 'LBL_PDF_DESCRIPTION', 'index.php?module=PDF&parent=Settings&view=List', '27', '0', '0'); 
-update `vtiger_settings_field_seq` set `id` = '89';
+update `vtiger_settings_field_seq` set `id` = '92';
