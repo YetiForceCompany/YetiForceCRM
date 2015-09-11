2a256d202c4731b47d0c2fad36d28d19163f1813
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-09-11 09:52:26 +0200

    Added change to database

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150911';
-$YetiForce_current_version = '2.2.94';
+$YetiForce_current_version = '2.2.95';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
