e234898baf47f881563d40197b1b6937e5f50178
Adrian Koń <a.kon@yetiforce.com>
2015-09-14 09:57:09 +0200

    Improved change password modal window

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150914';
-$YetiForce_current_version = '2.2.109';
+$YetiForce_current_version = '2.2.110';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
