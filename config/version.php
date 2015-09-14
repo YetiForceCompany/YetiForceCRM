cd4862bfab281d779df2b1eecd2d45b78bf851b9
Adrian Koń <a.kon@yetiforce.com>
2015-09-14 13:54:42 +0200

    Fix #1892 Improved inactive users filtr

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150914';
-$YetiForce_current_version = '2.2.112';
+$YetiForce_current_version = '2.2.113';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
