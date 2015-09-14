ed54faa12d1ee20152c76921246a07591154dcc6
Adrian Koń <a.kon@yetiforce.com>
2015-09-14 09:14:54 +0200

    Fix #1876 Improved progress bars in bakcup

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20150911';
-$YetiForce_current_version = '2.2.108';
+$patch_version = '20150914';
+$YetiForce_current_version = '2.2.109';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
