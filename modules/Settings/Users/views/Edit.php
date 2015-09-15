1fef29deb6823e201634e1a48ec08d12cbbf1190
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-09-15 05:39:58 +0200

    Improved the appearance of user's settings edition and preview.

--- modules/Settings/Users/views/Edit.php
+++ modules/Settings/Users/views/Edit.php
@@ -6,50 +6,32 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
+ * Contributor(s): YetiForce.com
  *************************************************************************************/
 
-class Users_Detail_View extends Users_PreferenceDetail_View {
+Class Settings_Users_Edit_View extends Users_PreferenceEdit_View {
 
 	public function preProcess(Vtiger_Request $request) {
 		parent::preProcess($request, false);
 		$this->preProcessSettings($request);
 	}
 
-	public function preProcessSettings(Vtiger_Request $request) {
-		$currentUserModel = Users_Record_Model::getCurrentUserModel();
+	public function preProcessSettings(Vtiger_Request $request)
+	{
+
 		$viewer = $this->getViewer($request);
+
 		$moduleName = $request->getModule();
 		$qualifiedModuleName = $request->getModule(false);
 		$selectedMenuId = $request->get('block');
 		$fieldId = $request->get('fieldid');
-
 		$settingsModel = Settings_Vtiger_Module_Model::getInstance();
 		$menuModels = $settingsModel->getMenus();
-
-		if(!empty($selectedMenuId)) {
-			$selectedMenu = Settings_Vtiger_Menu_Model::getInstanceById($selectedMenuId);
-		} elseif(!empty($moduleName) && $moduleName != 'Vtiger') {
-			$fieldItem = Settings_Vtiger_Index_View::getSelectedFieldFromModule($menuModels,$moduleName);
-			if($fieldItem){
-				$selectedMenu = Settings_Vtiger_Menu_Model::getInstanceById($fieldItem->get('blockid'));
-				$fieldId = $fieldItem->get('fieldid');
-			} else {
-				reset($menuModels);
-				$firstKey = key($menuModels);
-				$selectedMenu = $menuModels[$firstKey];
-			}
-		} else {
-			reset($menuModels);
-			$firstKey = key($menuModels);
-			$selectedMenu = $menuModels[$firstKey];
-		}
-
-		$viewer->assign('SELECTED_FIELDID',$fieldId);
-		$viewer->assign('SELECTED_MENU', $selectedMenu);
-		$viewer->assign('SETTINGS_MENUS', $menuModels);
+		$menu = $settingsModel->prepareMenuToDisplay($menuModels, $moduleName, $selectedMenuId, $fieldId);
+		
+		$viewer->assign('MENUS', $menu);
 		$viewer->assign('MODULE', $moduleName);
 		$viewer->assign('QUALIFIED_MODULE', $qualifiedModuleName);
-		$viewer->assign('CURRENT_USER_MODEL', $currentUserModel);
 		$viewer->view('SettingsMenuStart.tpl', $qualifiedModuleName);
 	}
 
@@ -63,15 +45,7 @@ class Users_Detail_View extends Users_PreferenceDetail_View {
 		$this->postProcessSettings($request);
 		parent::postProcess($request);
 	}
-
-	public function process(Vtiger_Request $request) {
-		$viewer = $this->getViewer($request);
-
-		$viewer->assign('CURRENT_USER_MODEL', Users_Record_Model::getCurrentUserModel());
-		$viewer->view('UserViewHeader.tpl', $request->getModule());
-		parent::process($request);
-	}
-
+	
 	public function getFooterScripts(Vtiger_Request $request) {
 		$headerScriptInstances = parent::getFooterScripts($request);
 		$moduleName = $request->getModule();
@@ -84,16 +58,8 @@ class Users_Detail_View extends Users_PreferenceDetail_View {
 		$headerScriptInstances = array_merge($headerScriptInstances, $jsScriptInstances);
 		return $headerScriptInstances;
 	}
-
-	/**
-	 * Function to get Ajax is enabled or not
-	 * @param Vtiger_Record_Model record model
-	 * @return <boolean> true/false
-	 */
-	function isAjaxEnabled($recordModel) {
-		if($recordModel->get('status') != 'Active') {
-			return false;
-		}
-		return $recordModel->isEditable();
+	
+	public function process(Vtiger_Request $request) {
+		parent::process($request);
 	}
 }
