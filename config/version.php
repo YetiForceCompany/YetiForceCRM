abb793642249fae2d70a0e1ca905b51358b562e3
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-09-16 15:59:45 +0200

    Extension of permission

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150916';
-$YetiForce_current_version = '2.2.138';
+$YetiForce_current_version = '2.2.139';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
--- include/utils/UserInfoUtil.php
+++ include/utils/UserInfoUtil.php
@@ -379,7 +379,16 @@ function isPermitted($module, $actionname, $record_id = '')
 				$parentRecord = Users_Privileges_Model::getParentRecord($record_id, $module, $role['previewrelatedrecord']);
 				if ($parentRecord) {
 					$recordMetaData = Vtiger_Functions::getCRMRecordMetadata($parentRecord);
-					if ($current_user->id == $recordMetaData['smownerid']) {
+
+					if ($role['permissionsrelatedfield'] == 0) {
+						$relatedPermission = $current_user->id == $recordMetaData['smownerid'];
+					} else if ($role['permissionsrelatedfield'] == 1) {
+						$relatedPermission = in_array($current_user->id, explode(',', $recordMetaData['shownerid']));
+					} else if ($role['permissionsrelatedfield'] == 2) {
+						$relatedPermission = $current_user->id == $recordMetaData['smownerid'] || in_array($current_user->id, explode(',', $recordMetaData['shownerid']));
+					}
+
+					if ($relatedPermission) {
 						$permission = 'yes';
 						$log->debug('Exiting isPermitted method ... - Parent Record Owner');
 						return $permission;
