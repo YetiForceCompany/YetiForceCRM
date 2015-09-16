0b9b2a9168cfe5adf8befc4024f3043c971b812a
Paula W <p.walenczak@yetiforce.com>
2015-09-16 10:33:31 +0200

    Added translations - Vtiger.php

--- languages/ru_ru/Vtiger.php
+++ languages/ru_ru/Vtiger.php
@@ -1608,6 +1608,6 @@ $jsLanguageStrings = [
 	'JS_OK' => 'Ok',
 	'equal to' => 'Equal to',
 	'None' => 'None',
-	'JS_YESTERDAY' => '__UNTRANSLATED__',
-	'JS_OLDER' => '__UNTRANSLATED__'
+	'JS_YESTERDAY' => 'Yesterday',
+	'JS_OLDER' => 'Older'
 ];
