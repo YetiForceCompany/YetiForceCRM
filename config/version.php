da1fba496731151779688b74b2277b64a802d4e0
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-09-16 16:01:58 +0200

    Expanded loading metadata record

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150916';
-$YetiForce_current_version = '2.2.139';
+$YetiForce_current_version = '2.2.140';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
