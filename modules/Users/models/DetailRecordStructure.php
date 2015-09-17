b36f53aa9d612ad405494cce9cad48789150bc08
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-09-17 09:06:41 +0200

    Improved code formating

--- modules/Users/models/DetailRecordStructure.php
+++ modules/Users/models/DetailRecordStructure.php
@@ -1,21 +1,23 @@
 <?php
-/*+***********************************************************************************
+/* +***********************************************************************************
  * The contents of this file are subject to the vtiger CRM Public License Version 1.0
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *************************************************************************************/
+ * *********************************************************************************** */
 
-class Users_DetailRecordStructure_Model extends Vtiger_DetailRecordStructure_Model {
+class Users_DetailRecordStructure_Model extends Vtiger_DetailRecordStructure_Model
+{
 
 	/**
 	 * Function to get the values in stuctured format
 	 * @return <array> - values in structure array('block'=>array(fieldinfo));
 	 */
-	public function getStructure() {
-		if(!empty($this->structuredValues)) {
+	public function getStructure()
+	{
+		if (!empty($this->structuredValues)) {
 			return $this->structuredValues;
 		}
 
@@ -25,11 +27,11 @@ class Users_DetailRecordStructure_Model extends Vtiger_DetailRecordStructure_Mod
 		$recordId = $recordModel->getId();
 		$moduleModel = $this->getModule();
 		$blockModelList = $moduleModel->getBlocks();
-		foreach($blockModelList as $blockLabel => $blockModel) {
+		foreach ($blockModelList as $blockLabel => $blockModel) {
 			$fieldModelList = $blockModel->getFields();
-			if (!empty ($fieldModelList)) {
+			if (!empty($fieldModelList)) {
 				$values[$blockLabel] = array();
-				foreach($fieldModelList as $fieldName => $fieldModel) {
+				foreach ($fieldModelList as $fieldName => $fieldModel) {
 					if ($fieldModel->get('uitype') == 156 && $currentUserModel->isAdminUser() == true) {
 						$fieldModel->set('editable', true);
 						$fieldValue = false;
@@ -38,8 +40,8 @@ class Users_DetailRecordStructure_Model extends Vtiger_DetailRecordStructure_Mod
 						}
 						$recordModel->set($fieldName, $fieldValue);
 					}
-					if($fieldModel->isViewableInDetailView()) {
-						if($recordId) {
+					if ($fieldModel->isViewableInDetailView()) {
+						if ($recordId) {
 							$fieldModel->set('fieldvalue', $recordModel->get($fieldName));
 						}
 						$values[$blockLabel][$fieldName] = $fieldModel;
