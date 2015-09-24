f00eaf77863ccc7c4dcc8aa5133473675e05ff1b
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-09-24 07:00:10 +0200

    Improved layout widget.

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20150923';
-$YetiForce_current_version = '2.2.159';
+$patch_version = '20150924';
+$YetiForce_current_version = '2.2.160';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
