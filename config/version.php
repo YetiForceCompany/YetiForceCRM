b48584cd275bd72245ff839d4a4774af397244d4
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-09-25 16:15:14 +0200

    Fixed encoding preview email

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150925';
-$YetiForce_current_version = '2.2.173';
+$YetiForce_current_version = '2.2.174';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
