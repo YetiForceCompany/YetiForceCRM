b46d66bb90e10b731869576621fe2076eae03716
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-09-28 22:55:25 +0200

    Fixed calculations in reports

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150928';
-$YetiForce_current_version = '2.2.186';
+$YetiForce_current_version = '2.2.187';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
