ac3e0ca707b2917e815fbd291e312ef358be566d
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-09-30 23:34:14 +0200

    Fixed saving the record

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150930';
-$YetiForce_current_version = '2.2.198';
+$YetiForce_current_version = '2.2.199';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
