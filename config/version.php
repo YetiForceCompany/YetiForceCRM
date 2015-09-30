4371774390909cba0adcb83f8a6ae8ec7b7cec16
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-09-30 10:43:28 +0200

    Added license to UIType

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20150929';
-$YetiForce_current_version = '2.2.193';
+$patch_version = '20150930';
+$YetiForce_current_version = '2.2.194';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
