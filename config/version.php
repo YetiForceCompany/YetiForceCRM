f155602ec340204c3e7922189ee47d1c9d86af29
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-01 10:59:44 +0200

    Added filtering for uitype multiReferenceValue

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20150930';
-$YetiForce_current_version = '2.2.200';
+$YetiForce_current_version = '2.2.201';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
