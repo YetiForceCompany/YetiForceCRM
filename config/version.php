4606a352c01c8ad9075129ff70b8e22e0aebbca6
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-02 09:23:35 +0200

    Added the function of updating data in the database Cleaned up database functions

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20151001';
-$YetiForce_current_version = '2.2.209';
+$patch_version = '20151002';
+$YetiForce_current_version = '2.2.210';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
