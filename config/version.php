1dd14a23e77d36c35677c2f001144bb4dd9bff1a
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-10-05 12:11:45 +0200

    Improved closing pop-up for calendar action.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151005';
-$YetiForce_current_version = '2.2.229';
+$YetiForce_current_version = '2.2.230';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
