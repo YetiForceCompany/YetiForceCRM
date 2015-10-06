e7d3de925936c92c50278f5bb0600de92d18155f
MaciejStencel <m.stencel@yetiforce.com>
2015-10-06 13:55:01 +0200

    Stop updating system currency rates when not properly configured in settings.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151006';
-$YetiForce_current_version = '2.2.244';
+$YetiForce_current_version = '2.2.245';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
