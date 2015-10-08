396c78d0d4d722a1b9b3a212dd6496ec985ba621
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-10-08 14:39:07 +0200

    Fixed displaying actions available in list view.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151008';
-$YetiForce_current_version = '2.2.271';
+$YetiForce_current_version = '2.2.272';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
