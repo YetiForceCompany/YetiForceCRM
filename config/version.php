eb37deeb3dcb58ce22e81e0a1709302fab1a5c88
Adrian Koń <a.kon@yetiforce.com>
2015-10-08 10:53:46 +0200

    Fix #1987

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151008';
-$YetiForce_current_version = '2.2.266';
+$YetiForce_current_version = '2.2.267';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
