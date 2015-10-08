ed908444384cb5911e93b0f479749e0b5ebaa7a2
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-08 20:24:38 +0200

    Rebuilt header links

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151008';
-$YetiForce_current_version = '2.2.275';
+$YetiForce_current_version = '2.2.276';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
