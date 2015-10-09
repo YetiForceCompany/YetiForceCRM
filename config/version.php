a07868db1cdd3cddc3cbb130c87e6778b9000ae0
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-10-09 16:38:01 +0200

    Improved code formating

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151009';
-$YetiForce_current_version = '2.2.291';
+$YetiForce_current_version = '2.2.292';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
