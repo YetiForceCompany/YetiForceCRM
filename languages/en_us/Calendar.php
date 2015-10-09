edcc1a307e413913f97693b414ead9975d80077c
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-10-09 11:23:42 +0200

    Improved translations.

--- languages/en_us/Calendar.php
+++ languages/en_us/Calendar.php
@@ -129,6 +129,7 @@ $languageStrings = [
 	'LBL_SHIFT_BLOCK' => 'Show or hide block',
 	'LBL_ON_SWITCH' => 'ON',
 	'LBL_OFF_SWITCH' => 'OFF',
+	'LBL_FOLLOWUP' => 'Follow up',
 	
 	'PLL_PLANNED' => 'Planned',
 	'PLL_IN_REALIZATION' => 'To be realized',
