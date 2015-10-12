450f7e0bc3bdcbf6b3751c690d4eb331c9b5f62c
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-12 21:17:51 +0200

    Added the ability to run cron manually

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151012';
-$YetiForce_current_version = '2.2.308';
+$YetiForce_current_version = '2.2.309';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
