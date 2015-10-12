4e8b3088874049766c06122c0de68ef098927a77
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-12 11:27:40 +0200

    Removed profile directly related to role

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151012';
-$YetiForce_current_version = '2.2.301';
+$YetiForce_current_version = '2.2.302';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
