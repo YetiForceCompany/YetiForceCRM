a93b6852c15082589b6cd837d8655800877749e9
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-13 15:01:28 +0200

    Fixed saving version during installation

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151013';
-$YetiForce_current_version = '2.3.1';
+$YetiForce_current_version = '2.3.2';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
