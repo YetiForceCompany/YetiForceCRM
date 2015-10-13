3ac4ff7b60020a6d174ce0f2c9d420097421d490
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-13 12:56:59 +0200

    YetiForce CRM ver. 2.3.0

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151013';
-$YetiForce_current_version = '2.2.313';
+$YetiForce_current_version = '2.3.0';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
