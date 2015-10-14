a289d08a60ea53796e3e47febc131a0c4234a47c
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-14 10:13:53 +0200

    Fixed creating graphical calendar events

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20151013';
-$YetiForce_current_version = '2.3.5';
+$patch_version = '20151014';
+$YetiForce_current_version = '2.3.6';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
