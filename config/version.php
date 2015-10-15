c7bca33cdf280201b96fd8336f0a4dc1e828b810
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-15 13:16:48 +0200

    Optimized display of data in the field tree

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151015';
-$YetiForce_current_version = '2.3.23';
+$YetiForce_current_version = '2.3.24';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
