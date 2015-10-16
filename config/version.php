b15b30e76baab6c6ff9d8793287e4b6a08e838aa
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-16 15:24:16 +0200

    Improved logout mail handler

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151016';
-$YetiForce_current_version = '2.3.32';
+$YetiForce_current_version = '2.3.33';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
