46fe008a88a4e86fe9240a7937c45dbc3581a159
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-19 09:55:29 +0200

    Fixed system Installer

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151017';
-$YetiForce_current_version = '2.3.44';
+$YetiForce_current_version = '2.3.45';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
