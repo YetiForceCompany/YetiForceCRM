da8dd2e6160e190f3523695e9298cdc595ae7d08
Adrian Koń <a.kon@yetiforce.com>
2015-10-19 13:20:55 +0200

    Fix #2020

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20151017';
-$YetiForce_current_version = '2.3.45';
+$patch_version = '20151019';
+$YetiForce_current_version = '2.3.46';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
