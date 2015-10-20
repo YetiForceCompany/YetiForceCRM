01e44ff708202a7d592b4e406452d30ba72f73ba
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-20 14:04:02 +0200

    Inventory module - Fixed display comments

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151020';
-$YetiForce_current_version = '2.3.52';
+$YetiForce_current_version = '2.3.53';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
