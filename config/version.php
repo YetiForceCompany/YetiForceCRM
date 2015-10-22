fdf611116588f4625b1d5ff14378a973e05ca75c
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-22 13:28:42 +0200

    Added a new type of the workflow "on related"

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151022';
-$YetiForce_current_version = '2.3.73';
+$YetiForce_current_version = '2.3.74';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
