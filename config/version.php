dc9d55b015a6b1bf2a442baed5c55b6a7fedec2a
Adrian Koń <a.kon@yetiforce.com>
2015-10-23 08:29:43 +0200

    Improved code formating

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20151022';
-$YetiForce_current_version = '2.3.77';
+$patch_version = '20151023';
+$YetiForce_current_version = '2.3.78';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
