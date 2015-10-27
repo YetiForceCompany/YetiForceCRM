445b33cf3c211d91c67297a863417482e61a7910
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-27 08:32:17 +0100

    Fixed generate e-mail address book

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151026';
-$YetiForce_current_version = '2.3.106';
+$YetiForce_current_version = '2.3.107';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
