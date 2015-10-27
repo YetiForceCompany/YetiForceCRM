f1115a459a01827e8054ff5a1a76c2c644ff6956
Adrian Koń <a.kon@yetiforce.com>
2015-10-27 12:31:50 +0100

    Added cursor pointer to announcementBtn

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151027';
-$YetiForce_current_version = '2.3.113';
+$YetiForce_current_version = '2.3.114';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
