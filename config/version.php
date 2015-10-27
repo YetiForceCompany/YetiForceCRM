005ddbbfd3731d6589a62225e8c6804be4bc8839
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-10-27 13:30:11 +0100

    Fixed export module.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151027';
-$YetiForce_current_version = '2.3.116';
+$YetiForce_current_version = '2.3.117';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
