e84599088585e251cc4f00540a6ab9d01763c38f
Adrian Koń <a.kon@yetiforce.com>
2015-10-28 12:05:57 +0100

    Improved cancel button

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151028';
-$YetiForce_current_version = '2.3.130';
+$YetiForce_current_version = '2.3.131';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
