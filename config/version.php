7ac11f48aee2a45beb5165643f2658d3e063320c
Adrian Koń <a.kon@yetiforce.com>
2015-10-28 12:44:06 +0100

    Added cursor pointer to checkboxes

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151028';
-$YetiForce_current_version = '2.3.132';
+$YetiForce_current_version = '2.3.133';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
