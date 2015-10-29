a1bffa98d54d3b737aed2fed1fcdc78064a3867d
Adrian Koń <a.kon@yetiforce.com>
2015-10-29 10:00:25 +0100

    Improved delete groups transfer form

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151029';
-$YetiForce_current_version = '2.3.151';
+$YetiForce_current_version = '2.3.152';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
