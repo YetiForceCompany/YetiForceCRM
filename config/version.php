1dd9e13e66b86e25c5da58a24169e59a57f8ee26
Adrian Koń <a.kon@yetiforce.com>
2015-10-29 12:48:18 +0100

    Reduce distance in row

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151029';
-$YetiForce_current_version = '2.3.168';
+$YetiForce_current_version = '2.3.169';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
