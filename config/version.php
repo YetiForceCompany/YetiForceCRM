87e17754928bcd43c41bd4945666a33da99cd6d5
Adrian Koń <a.kon@yetiforce.com>
2015-10-29 11:38:24 +0100

    Removed unnecessary tab

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151029';
-$YetiForce_current_version = '2.3.160';
+$YetiForce_current_version = '2.3.161';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
