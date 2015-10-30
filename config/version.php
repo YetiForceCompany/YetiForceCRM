9f963216dbf94f7d2ce1730d90d6e678be084ac6
Adrian Koń <a.kon@yetiforce.com>
2015-10-30 08:21:29 +0100

    Removed redundant colon

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151030';
-$YetiForce_current_version = '2.3.177';
+$YetiForce_current_version = '2.3.178';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
