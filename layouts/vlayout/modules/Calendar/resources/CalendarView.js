2b9de00039b7f17b2f8c2426af5f243b5572ed49
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-10-30 18:52:16 +0100

    Changed time format

--- layouts/vlayout/modules/Calendar/resources/CalendarView.js
+++ layouts/vlayout/modules/Calendar/resources/CalendarView.js
@@ -94,10 +94,10 @@ jQuery.Class("Calendar_CalendarView_Js", {
 		var userDefaultTimeFormat = jQuery('#time_format').val();
 		var popoverTimeFormat;
 		if (userDefaultTimeFormat == 24) {
-			userDefaultTimeFormat = 'H(:mm)';
+			userDefaultTimeFormat = 'H:mm';
 			popoverTimeFormat = 'HH:MM';
 		} else {
-			userDefaultTimeFormat = 'h(:mm)t';
+			userDefaultTimeFormat = 'h:mmt';
 			popoverTimeFormat = 'hh:mm A';
 		}
 
