632742e95c7fbc048072e594161af1e81686e6cc
Tomasz Kur <t.kur@yetiforce.com>
2015-11-06 10:39:34 +0100

    Improved left panel in Settings for Edit view

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151105';
-$YetiForce_current_version = '2.3.256';
+$YetiForce_current_version = '2.3.257';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
