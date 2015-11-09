4426e521be161abd80b67763c91732c0b75f5942
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-11-09 08:53:50 +0100

    Added sorting the user list

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20151106';
-$YetiForce_current_version = '2.3.270';
+$patch_version = '20151109';
+$YetiForce_current_version = '2.3.271';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
