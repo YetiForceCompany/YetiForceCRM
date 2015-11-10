06d97d9221ee794f4a77a91fa9df7f9eaba57b4b
Tomasz Kur <t.kur@yetiforce.com>
2015-11-10 14:48:40 +0100

    Improved table with result of Import

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151110';
-$YetiForce_current_version = '2.3.315';
+$YetiForce_current_version = '2.3.316';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
