90075886c1cb0368ca18dad6667f9f54a0ec3c63
Tomasz Kur <t.kur@yetiforce.com>
2015-11-12 13:15:05 +0100

    Improved apearance edit View for Invoice module

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151112';
-$YetiForce_current_version = '2.3.329';
+$YetiForce_current_version = '2.3.330';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
