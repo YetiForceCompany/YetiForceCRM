1a4b38f7371a597083900225fd5ea03886b80f01
Tomasz Kur <t.kur@yetiforce.com>
2015-11-16 09:49:48 +0100

    Improved checkbox(for taxes) in edit view

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151116';
-$YetiForce_current_version = '2.3.357';
+$YetiForce_current_version = '2.3.358';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
