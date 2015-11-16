2f03c359f7b84df3560ab2cca1c6e843931eb144
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-11-16 13:59:57 +0100

    Improved displaying hierarchy in accounts.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151116';
-$YetiForce_current_version = '2.3.362';
+$YetiForce_current_version = '2.3.363';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
