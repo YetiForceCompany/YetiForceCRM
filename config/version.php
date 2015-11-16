90a32aa53499d91210883fbfb4d74ba2654f2cef
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-11-16 13:26:23 +0100

    Reconstructed get the list of records

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151116';
-$YetiForce_current_version = '2.3.361';
+$YetiForce_current_version = '2.3.362';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
