2af18b1558f5ddb1da709ec9235b09ca6e1ba6cf
Adrian Koń <a.kon@yetiforce.com>
2015-11-17 13:09:03 +0100

    Fixed translation

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151117';
-$YetiForce_current_version = '2.3.373';
+$YetiForce_current_version = '2.3.374';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
