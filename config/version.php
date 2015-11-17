326e5693d6102fffc08bb41d7d414080d51a26ec
Adrian Koń <a.kon@yetiforce.com>
2015-11-17 13:12:31 +0100

    Fixed punctuation mark

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151117';
-$YetiForce_current_version = '2.3.374';
+$YetiForce_current_version = '2.3.375';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
