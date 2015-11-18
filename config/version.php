cdba544412d581a97332b286b4a4952d4c1e0504
Adrian Koń <a.kon@yetiforce.com>
2015-11-18 14:59:09 +0100

    Added space in translations

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151118';
-$YetiForce_current_version = '2.3.422';
+$YetiForce_current_version = '2.3.423';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
