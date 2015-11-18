be1e5dea2d8408e2ef346fb04f885ccdc08fe185
Adrian Koń <a.kon@yetiforce.com>
2015-11-18 09:56:38 +0100

    Changed button color

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151118';
-$YetiForce_current_version = '2.3.406';
+$YetiForce_current_version = '2.3.407';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
