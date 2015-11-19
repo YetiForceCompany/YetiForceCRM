6b284220fe79eb4604d23910187cd36c378007ae
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-11-19 05:36:42 +0100

    Improved displaying popups in new Requirements Cards module.

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20151118';
-$YetiForce_current_version = '2.3.427';
+$patch_version = '20151119';
+$YetiForce_current_version = '2.3.428';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
