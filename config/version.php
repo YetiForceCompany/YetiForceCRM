9fd9aaf2bb07b043f39fb97a2a3806d1c7e7e4ff
Adrian Koń <a.kon@yetiforce.com>
2015-11-19 08:35:25 +0100

    Added dots to translations

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151119';
-$YetiForce_current_version = '2.3.431';
+$YetiForce_current_version = '2.3.432';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
