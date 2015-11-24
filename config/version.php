e28edc261bba8c3b9971e216dac365e7989a2895
Tomasz Kur <t.kur@yetiforce.com>
2015-11-24 11:43:00 +0100

    Bold star

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151124';
-$YetiForce_current_version = '2.3.496';
+$YetiForce_current_version = '2.3.497';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
