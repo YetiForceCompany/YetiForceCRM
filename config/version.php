c4313ec9c205ed3826624a05ea966502e9e9356f
Tomasz Kur <t.kur@yetiforce.com>
2015-11-26 13:59:51 +0100

    Fixed scroll in pages

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151126';
-$YetiForce_current_version = '2.3.518';
+$YetiForce_current_version = '2.3.519';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
