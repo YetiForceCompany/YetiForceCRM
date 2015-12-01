2bc6faeec1e13e15ecbef271ad6df59c103a6996
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-12-01 17:03:54 +0100

    Improved administration settings management

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151201';
-$YetiForce_current_version = '2.3.576';
+$YetiForce_current_version = '2.3.577';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
