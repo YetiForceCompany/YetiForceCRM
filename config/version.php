470c5a1b70e15769851696f231590a17ab29ebf8
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-12-02 16:18:32 +0100

    Fixed name of the class

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151202';
-$YetiForce_current_version = '2.3.591';
+$YetiForce_current_version = '2.3.592';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
