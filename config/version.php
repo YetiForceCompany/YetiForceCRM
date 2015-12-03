398cd702a8d8933bcb4a6c3f1cbbf9e1bfe01b64
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-12-03 07:27:15 +0100

    Added relations (Vendors-PaymentsOut,PaymentsIn).

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151203';
-$YetiForce_current_version = '2.3.596';
+$YetiForce_current_version = '2.3.597';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
