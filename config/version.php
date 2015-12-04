481f7a3bb4222ca62af69d865ae6fc51dddf4bae
TomaszQr <t.kur@yetiforce.com>
2015-12-04 20:59:50 +0100

    Added library TimelineJS3

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151203';
-$YetiForce_current_version = '2.3.612';
+$YetiForce_current_version = '2.3.613';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
