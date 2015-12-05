202bd62d18fce4b51c295cef2cc34b933265d4b8
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-12-05 23:20:31 +0100

    Removed unnecessary files

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151205';
-$YetiForce_current_version = '2.3.617';
+$YetiForce_current_version = '2.3.618';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
