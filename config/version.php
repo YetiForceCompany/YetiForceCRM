132264bf7d05042cf9c7e369dd1552fb9dcb003d
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-12-06 02:43:01 +0100

    Improved switch users.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151206';
-$YetiForce_current_version = '2.3.622';
+$YetiForce_current_version = '2.3.623';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
