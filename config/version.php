ff8d7b020eb27184d844f0e8e1fb1c5f45ad76a8
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-12-06 03:49:00 +0100

    Removed margin.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151206';
-$YetiForce_current_version = '2.3.623';
+$YetiForce_current_version = '2.3.624';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
