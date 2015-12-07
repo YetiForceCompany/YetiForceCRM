8437b24f1594d79ef17f532c390ede6ab2e8599b
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-12-07 08:13:57 +0100

    Improved the permission to remove a record from summary view.

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20151206';
-$YetiForce_current_version = '2.3.624';
+$patch_version = '20151207';
+$YetiForce_current_version = '2.3.625';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
