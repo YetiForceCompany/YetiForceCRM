d120ee61f5963c0437369789d48409ab9bff380c
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-12-07 08:54:25 +0100

    Improved integration with CalDAV

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151207';
-$YetiForce_current_version = '2.3.625';
+$YetiForce_current_version = '2.3.626';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
