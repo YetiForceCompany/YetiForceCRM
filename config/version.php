ae9f6f3ce608917e7ab7c0bcf4e1a2756f338845
Tomasz Kur <t.kur@yetiforce.com>
2015-12-07 09:10:24 +0100

    Fixed display of menus

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151207';
-$YetiForce_current_version = '2.3.626';
+$YetiForce_current_version = '2.3.627';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
