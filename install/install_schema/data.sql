ae42a6f2f9745cebaad700b293a2dc3c8b947dea
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-12-07 10:23:40 +0100

    Removed the Dutch language.

--- install/install_schema/data.sql
+++ install/install_schema/data.sql
@@ -5775,11 +5775,10 @@ insert  into `vtiger_language`(`id`,`name`,`prefix`,`label`,`lastupdated`,`seque
 insert  into `vtiger_language`(`id`,`name`,`prefix`,`label`,`lastupdated`,`sequence`,`isdefault`,`active`) values (3,'Deutsch','de_de','DE Deutsch','2014-11-21 11:20:40',NULL,0,1);
 insert  into `vtiger_language`(`id`,`name`,`prefix`,`label`,`lastupdated`,`sequence`,`isdefault`,`active`) values (4,'Portuguese','pt_br','Brazilian Portuguese','2014-12-11 11:12:39',NULL,0,1);
 insert  into `vtiger_language`(`id`,`name`,`prefix`,`label`,`lastupdated`,`sequence`,`isdefault`,`active`) values (5,'Russian','ru_ru','Russian','2015-01-13 15:12:39',NULL,0,1);
-insert  into `vtiger_language`(`id`,`name`,`prefix`,`label`,`lastupdated`,`sequence`,`isdefault`,`active`) values (6,'Dutch','nl_nl','Dutch',NULL,NULL,0,1);
 
 /*Data for the table `vtiger_language_seq` */
 
-insert  into `vtiger_language_seq`(`id`) values (6);
+insert  into `vtiger_language_seq`(`id`) values (5);
 
 /*Data for the table `vtiger_layout` */
 
