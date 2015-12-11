95d58707a89e7f34140534dfacc768eb91511769
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-12-11 17:08:51 +0100

    Improved display of the menu icons

--- modules/Vtiger/models/Menu.php
+++ modules/Vtiger/models/Menu.php
@@ -189,8 +189,10 @@ class Vtiger_Menu_Model
 		if (!empty($menu['icon'])) {
 			if (strpos($menu['icon'], 'adminIcon-') !== false || strpos($menu['icon'], 'userIcon-') !== false) {
 				return '<span class="menuIcon ' . $menu['icon'] . '" aria-hidden="true"></span>';
+			}else if (strpos($menu['icon'], 'glyphicon-') !== false) {
+				return '<span class="glyphicon ' . $menu['icon'] . '" aria-hidden="true"></span>';
 			}
-
+			
 			$icon = vimage_path($menu['icon']);
 			if (file_exists($icon)) {
 				return '<img src="' . $icon . '" alt="' . $title . '" title="' . $title . '" class="menuIcon" />';
