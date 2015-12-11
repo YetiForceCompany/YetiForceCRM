be510b4c24533bf1b8bcea16b2d2e183beba8cd2
Алексей <waw555@yandex.ru>
2015-12-11 19:01:45 +0500

    Improve Translation

--- languages/ru_ru/Settings/SupportProcesses.php
+++ languages/ru_ru/Settings/SupportProcesses.php
@@ -9,13 +9,13 @@
  * All Rights Reserved.
  *************************************************************************************************************************************/
 $languageStrings = [
-	'SupportProcesses' => 'Процесс поддержки',
-	'LBL_SUPPORT_PROCESSES' => 'Процесс поддержки',
+	'SupportProcesses' => 'Процесс - Поддержка',
+	'LBL_SUPPORT_PROCESSES' => 'Процесс - Поддержка',
 	'LBL_GENERAL_CONFIGURATION' => 'Основная конфигурация',
 	'LBL_OTHER' => 'Другое',
 	'LBL_INFO' => 'Информация',
 	'LBL_TYPE' => 'Тип',
-	'LBL_TICKET_STATUS_INFO' => 'Статус обозначающий закрытие заявки',
+	'LBL_TICKET_STATUS_INFO' => 'Статус определяющий закрытие заявки',
 	'LBL_SAVE_CONFIG_OK' => 'Изменения успешно сохранены',
 	'LBL_SAVE_CONFIG_ERROR' => 'Ошибка сохранения изменений',
 	
