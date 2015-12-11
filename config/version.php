6db0a7d2ce10124bf83deb854570aa0f6ba5e245
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-12-11 07:54:46 +0100

    Fix #2416

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20151210';
-$YetiForce_current_version = '2.3.676';
+$patch_version = '20151211';
+$YetiForce_current_version = '2.3.677';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
