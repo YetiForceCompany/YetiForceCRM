f1e6decf4b52e5053ae135b73835368d8c5d5424
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-12-11 16:06:32 +0100

    Added tree records to modules: Services, Products

---
+++ modules/Services/models/TreeView.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Services TreeView Model Class
+ * @package YetiForce.TreeView
+ * @license licenses/License.html
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+class Services_TreeView_Model extends Vtiger_TreeView_Model
+{
+
+	public function isActive()
+	{
+		return true;
+	}
+}
