deb8b3495f893e24e41054b029568dfcefcaa112
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-12-11 16:03:08 +0100

    Added tree records to modules: OSSOutsourcedServices, OSSSoldServices, OutsourcedProducts, Assets

---
+++ modules/OutsourcedProducts/models/TreeView.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * OutsourcedProducts TreeView Model Class
+ * @package YetiForce.TreeView
+ * @license licenses/License.html
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+class OutsourcedProducts_TreeView_Model extends Vtiger_TreeView_Model
+{
+
+	public function isActive()
+	{
+		return true;
+	}
+}
