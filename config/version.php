49edde9fdb9474e38599f2da6f228f2b1c53dde9
Tomasz Kur <t.kur@yetiforce.com>
2015-12-11 09:59:22 +0100

    Improved configurations timeline

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151211';
-$YetiForce_current_version = '2.3.679';
+$YetiForce_current_version = '2.3.680';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
