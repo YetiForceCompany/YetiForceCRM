985735ca90f8e589fd5b0d356727295f79c8d898
Tomasz Kur <t.kur@yetiforce.com>
2015-12-17 13:10:09 +0100

    Improved alphabet in ListView

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151217';
-$YetiForce_current_version = '2.3.754';
+$YetiForce_current_version = '2.3.755';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
