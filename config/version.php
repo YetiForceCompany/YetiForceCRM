75f2406d1cee2d627ece94b8eb78a277c7a2d662
Tomasz Kur <t.kur@yetiforce.com>
2015-12-20 22:54:45 +0100

    Moved buttons in dashboard

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151220';
-$YetiForce_current_version = '2.3.780';
+$YetiForce_current_version = '2.3.781';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
