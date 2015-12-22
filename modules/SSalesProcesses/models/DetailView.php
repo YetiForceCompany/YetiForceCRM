37281612ed70627967c220e4a3e0676ecab129e5
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-12-22 14:14:24 +0100

    Added products and services to sales opportunities

---
+++ modules/SSalesProcesses/models/DetailView.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * SSalesProcesses DetailView Model Class
+ * @package YetiForce.Model
+ * @license licenses/License.html
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+class SSalesProcesses_DetailView_Model extends Vtiger_DetailView_Model
+{
+
+	function getDetailViewRelatedLinks()
+	{
+		$recordModel = $this->getRecord();
+		$moduleName = $recordModel->getModuleName();
+		$parentModuleModel = $this->getModule();
+		$relatedLinks = Vtiger_DetailView_Model::getDetailViewRelatedLinks();
+		$showPSTab = vtlib_isModuleActive('OutsourcedProducts') || vtlib_isModuleActive('Products') || vtlib_isModuleActive('Services') || vtlib_isModuleActive('OSSOutsourcedServices') || vtlib_isModuleActive('Assets') || vtlib_isModuleActive('OSSSoldServices');
+		
+		if ($showPSTab) {
+			$relatedLinks[] = [
+				'linktype' => 'DETAILVIEWTAB',
+				'linklabel' => vtranslate('LBL_RECORD_SUMMARY_PRODUCTS_SERVICES', $moduleName),
+				'linkurl' => $recordModel->getDetailViewUrl() . '&mode=showRelatedProductsServices&requestMode=summary',
+				'linkicon' => '',
+				'linkKey' => 'LBL_RECORD_SUMMARY',
+				'related' => 'ProductsAndServices',
+				'countRelated' => true
+			];
+		}
+		return $relatedLinks;
+	}
+}
