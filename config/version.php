37893f4f926eee0c8f31e00d4ca250663caaa416
Tomasz Kur <t.kur@yetiforce.com>
2015-12-23 10:21:47 +0100

    Fixed toolbar in DetailView

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151223';
-$YetiForce_current_version = '2.3.813';
+$YetiForce_current_version = '2.3.814';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
