8a70cd251cb54ef4f1e7edd7262e6164efcf3f4c
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-12-23 15:06:09 +0100

    Removed tcpdf library

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151223';
-$YetiForce_current_version = '2.3.819';
+$YetiForce_current_version = '2.3.820';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
