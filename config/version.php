a2e9d4197c9b850604673f3c0616c0c4ab20e0fc
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-12-23 10:17:18 +0100

    Fix #2530 Removed old code

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151223';
-$YetiForce_current_version = '2.3.812';
+$YetiForce_current_version = '2.3.813';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
