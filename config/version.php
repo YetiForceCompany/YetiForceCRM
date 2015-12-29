f25320b458d49b7daac6b2d9958eccb34fb248cc
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-12-29 11:59:24 +0100

    Rebuilt the relationship calendar

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151229';
-$YetiForce_current_version = '2.3.847';
+$YetiForce_current_version = '2.3.848';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
