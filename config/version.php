5339e49621e626668268e4aa6f88e68087bb58ed
Tomasz Kur <t.kur@yetiforce.com>
2015-12-31 13:12:32 +0100

    Fixed appearance of fields during installation

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151231';
-$YetiForce_current_version = '2.3.886';
+$YetiForce_current_version = '2.3.887';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
