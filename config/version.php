9350b839728ffddaad0f10e12c61bb909a2776ba
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-12-31 10:13:54 +0100

    Fix #2564 Fix #2563

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20151231';
-$YetiForce_current_version = '2.3.882';
+$YetiForce_current_version = '2.3.883';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
