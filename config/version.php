af9263cd88549db0a683babd852cc37b2082a418
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-01-05 05:23:08 +0100

    Changed displaytype.

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20160104';
-$YetiForce_current_version = '2.3.909';
+$patch_version = '20160105';
+$YetiForce_current_version = '2.3.910';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
