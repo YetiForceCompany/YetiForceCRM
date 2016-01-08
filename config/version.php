2ee62d446bf17146edf0bcd92eb3080e50ff66d2
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-01-08 18:33:08 +0100

    Fixed custom view conditions

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160108';
-$YetiForce_current_version = '2.3.945';
+$YetiForce_current_version = '2.3.946';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
--- include/utils/utils.php
+++ include/utils/utils.php
@@ -193,7 +193,7 @@ function get_group_array($add_blank = true, $status = "Active", $assigned_user =
 		$query = 'SELECT groupid, groupname FROM vtiger_groups';
 		$params = [];
 
-		if ($module) {
+		if ($module && $module != 'CustomView') {
 			$query .= ' WHERE groupid IN (SELECT groupid FROM vtiger_group2modules WHERE tabid = ?)';
 			$params[] = $tabid;
 		}
