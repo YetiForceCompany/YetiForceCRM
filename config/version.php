af97f453ccee98ba6d9d8fb727034e7611e8b17e
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-01-11 09:03:23 +0100

    Fixed privileges for sharing

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160111';
-$YetiForce_current_version = '2.3.953';
+$YetiForce_current_version = '2.3.954';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
