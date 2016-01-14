8825b1fb6a21d0a49e5379448b62777756cc5ca1
Tomasz Kur <t.kur@yetiforce.com>
2016-01-14 08:48:52 +0100

    Fixed clockpicker

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20160113';
-$YetiForce_current_version = '2.3.989';
+$patch_version = '20160114';
+$YetiForce_current_version = '2.3.990';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
