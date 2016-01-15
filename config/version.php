56ea61fb365173416d0ef3928313eea09f2687d9
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-01-15 15:11:21 +0100

    Changed buttons preview

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160115';
-$YetiForce_current_version = '2.3.1004';
+$YetiForce_current_version = '2.3.1005';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
