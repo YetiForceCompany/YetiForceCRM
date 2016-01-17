a7543ce0b4bcec9fa0bb50186b10f1895c729f4d
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-01-17 03:21:17 +0100

    Improved getting a list of reference fields.

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20160115';
-$YetiForce_current_version = '2.3.1008';
+$patch_version = '20160117';
+$YetiForce_current_version = '2.3.1009';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
--- include/Webservices/WebserviceField.php
+++ include/Webservices/WebserviceField.php
@@ -295,21 +295,25 @@ class WebserviceField
 			}
 
 			$referenceTypes = array();
-			if ($this->getUIType() != $this->genericUIType) {
-				$sql = "select vtiger_ws_referencetype.`type` from vtiger_ws_referencetype INNER JOIN vtiger_tab ON vtiger_tab.`name` = vtiger_ws_referencetype.`type` where fieldtypeid=? AND vtiger_tab.`presence` NOT IN (?)";
-				$params = array($fieldTypeData['fieldtypeid'], 1);
+			if (!in_array($this->getUIType(), [66, 67, 68])) {
+				if ($this->getUIType() != $this->genericUIType) {
+					$sql = "select vtiger_ws_referencetype.`type` from vtiger_ws_referencetype INNER JOIN vtiger_tab ON vtiger_tab.`name` = vtiger_ws_referencetype.`type` where fieldtypeid=? AND vtiger_tab.`presence` NOT IN (?)";
+					$params = array($fieldTypeData['fieldtypeid'], 1);
+				} else {
+					$sql = 'select relmodule as type from vtiger_fieldmodulerel INNER JOIN vtiger_tab ON vtiger_tab.`name` = vtiger_fieldmodulerel.`relmodule` WHERE fieldid=? AND vtiger_tab.`presence` NOT IN (?) ORDER BY sequence ASC';
+					$params = array($this->getFieldId(), 1);
+				}
+				$result = $this->pearDB->pquery($sql, $params);
+				$numRows = $this->pearDB->num_rows($result);
+				for ($i = 0; $i < $numRows; ++$i) {
+					$referenceType = $this->pearDB->query_result($result, $i, "type");
+					if (in_array($referenceType, $accessibleTypes))
+						array_push($referenceTypes, $referenceType);
+				}
 			} else {
-				$sql = 'select relmodule as type from vtiger_fieldmodulerel INNER JOIN vtiger_tab ON vtiger_tab.`name` = vtiger_fieldmodulerel.`relmodule` WHERE fieldid=? AND vtiger_tab.`presence` NOT IN (?) ORDER BY sequence ASC';
-				$params = array($this->getFieldId(), 1);
+				$fieldModel = Vtiger_Field_Model::getInstanceFromFieldId($this->getFieldId());
+				$referenceTypes = $fieldModel->getUITypeModel()->getReferenceList();
 			}
-			$result = $this->pearDB->pquery($sql, $params);
-			$numRows = $this->pearDB->num_rows($result);
-			for ($i = 0; $i < $numRows; ++$i) {
-				$referenceType = $this->pearDB->query_result($result, $i, "type");
-				if (in_array($referenceType, $accessibleTypes))
-					array_push($referenceTypes, $referenceType);
-			}
-
 			$referenceTypesUnsorted = array_values(array_intersect($accessibleTypes, $referenceTypes));
 
 			$referenceTypesSorted = array();
