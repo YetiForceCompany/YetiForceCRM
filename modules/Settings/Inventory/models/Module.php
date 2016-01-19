d58e8b6dd28ea814d66c815d506a8d82dd15a56b
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2016-01-19 12:57:42 +0100

    Added license info

--- modules/Settings/Inventory/models/Module.php
+++ modules/Settings/Inventory/models/Module.php
@@ -1,6 +1,10 @@
 <?php
-/* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
-
+/**
+ * @package YetiForce.Model
+ * @license licenses/License.html
+ * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
+ 
 class Settings_Inventory_Module_Model extends Vtiger_Base_Model
 {
 
