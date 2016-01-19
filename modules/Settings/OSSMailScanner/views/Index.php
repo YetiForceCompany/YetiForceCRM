40ffe01372fdcfece638d25fac4613bd670f01c7
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2016-01-19 12:35:54 +0100

    Added license info

--- modules/Settings/OSSMailScanner/views/Index.php
+++ modules/Settings/OSSMailScanner/views/Index.php
@@ -1,6 +1,10 @@
 <?php
-/* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
-
+/**
+ * @package YetiForce.Views
+ * @license licenses/License.html
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
 class Settings_OSSMailScanner_Index_View extends Settings_Vtiger_Index_View
 {
 
