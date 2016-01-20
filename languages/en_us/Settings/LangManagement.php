ec963540ac77f11d63d231c83c1aeff7a765126e
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2016-01-20 11:54:23 +0100

    Updated license info

--- languages/en_us/Settings/LangManagement.php
+++ languages/en_us/Settings/LangManagement.php
@@ -1,12 +1,5 @@
 <?php
- /*+********************************************************************************
- * Terms & Conditions are placed on the: http://opensaas.pl
- ********************************************************************************
- *  Module				: LangManagement
- *  Author				: OpenSaaS Sp. z o.o. 
- *  Help/Email			: bok@opensaas.pl
- *  Website				: www.opensaas.pl
- ********************************************************************************+*/
+/* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 $languageStrings = [
 	'No data available in table' => 'No data available in table',
 	'Translations' => 'Translations',
