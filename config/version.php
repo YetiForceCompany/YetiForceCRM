94ac6b0ff9464a974b659543580b65b112f3e849
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-01-20 15:49:38 +0100

    Improved display of buttons in the detail view

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160120';
-$YetiForce_current_version = '2.3.1044';
+$YetiForce_current_version = '2.3.1045';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
