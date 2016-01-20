4e67bc1816968eb72cef78fbb6abd86be92c8719
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2016-01-20 12:40:34 +0100

    Updated license info

--- languages/pt_br/Settings/OSSMail.php
+++ languages/pt_br/Settings/OSSMail.php
@@ -1,13 +1,6 @@
 <?php
-/* +********************************************************************************
- * Terms & Conditions are placed on the: http://opensaas.pl
- * *******************************************************************************
- *  Module				: OSSMail
- *  Author				: OpenSaaS Sp. z o.o.
- *  Help/Email			: bok@opensaas.pl
- *  Website				: www.opensaas.pl
- *  Brazilian Portuguese Translation - Valmir Carlos Trindade - valmir@ttcasolucoes.com.br*
- * *******************************************************************************+ */
+/* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
+
 $languageStrings = [
     'OSSMail' => 'Minha Caixa de Correio',
 ];
