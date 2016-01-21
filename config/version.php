52ae89877f50aaac5603ac6d329c5baa9daa1d39
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-01-21 14:47:26 +0100

    Improved loading list of records

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160121';
-$YetiForce_current_version = '2.3.1051';
+$YetiForce_current_version = '2.3.1052';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
