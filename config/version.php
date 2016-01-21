de74a76b60dadcf8c7e0a995312bb0c44aeebace
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-01-21 14:46:41 +0100

    Fixed permissions in modal windows

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160121';
-$YetiForce_current_version = '2.3.1050';
+$YetiForce_current_version = '2.3.1051';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
