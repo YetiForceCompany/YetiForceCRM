56d213edddcef9c1bfc82abff4a971463f7e641c
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-01-22 09:18:03 +0100

    Fixed system installation Fix #2663 Fix #2651

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160122';
-$YetiForce_current_version = '2.3.1055';
+$YetiForce_current_version = '2.3.1056';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
