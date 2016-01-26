fd2fcd68738e035e90039dc1b3b8b0a5771b4019
Paula W <p.walenczak@yetiforce.com>
2016-01-26 09:19:41 +0100

    Added translations - Vtiger.php

--- languages/en_us/Vtiger.php
+++ languages/en_us/Vtiger.php
@@ -1094,6 +1094,7 @@ $languageStrings = [
 	'LBL_VIEW_INDEX' => 'Start',
 	'LBL_SU_UPDATED' => 'Record updated by a different user',
 	'LBL_CRMACTIVITY' => 'Calendar activity',
+	'FL_NUMBER' => 'Record number',
 ];
 $jsLanguageStrings = [
 	'JS_SUMMARY' => 'Summary',
