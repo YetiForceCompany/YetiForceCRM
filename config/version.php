03caea180cd2319b4535aea43cbd735f7339f361
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-01-27 08:39:57 +0100

    Improved .htaccess files

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160126';
-$YetiForce_current_version = '2.3.1085';
+$YetiForce_current_version = '2.3.1086';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
