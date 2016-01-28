52d652d147b93f5b05ff75b620b6f5499a3c056c
Adrián Granado <adrgranado@gmail.com>
2016-01-28 10:28:52 +0100

    some translation needed

--- languages/en_us/Vtiger.php
+++ languages/en_us/Vtiger.php
@@ -1042,6 +1042,10 @@ $languageStrings = [
 	'LBL_COMMENT' => 'Comment',
 	'LBL_CURRENT' => 'Current',
 	'LBL_SET_RECORD_STATUS' => 'Set status',
+
+	'LBL_FILTER_ALL' => 'All',
+	'LBL_FILTER_ACCOUNTS' => 'Accounts',
+	'LBL_FILTER_CONTACTS' => 'Contacts',
 ];
 $jsLanguageStrings = [
 	'JS_DATE_SHOULD_BE_GREATER_THAN' => 'End Date cannot be equal or less than Start Date',
