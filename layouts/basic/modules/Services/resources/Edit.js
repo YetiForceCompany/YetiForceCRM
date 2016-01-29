4e53c9e030006e236d59049d63f2241c73161869
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2016-01-29 09:02:44 +0100

    Updated license info

--- layouts/basic/modules/Services/resources/Edit.js
+++ layouts/basic/modules/Services/resources/Edit.js
@@ -1,10 +1,3 @@
-/*+***********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
- *************************************************************************************/
+/* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 
 Products_Edit_Js("Services_Edit_Js",{},{})
