fcd93a5b907ed081d588c3dfb66ae76fd877b8c6
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-02-03 11:09:19 +0100

    Added a new action locking

--- layouts/basic/modules/Vtiger/TreePopup.tpl
+++ layouts/basic/modules/Vtiger/TreePopup.tpl
@@ -1,14 +1,4 @@
-{*<!--
-/*+***********************************************************************************************************************************
- * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
- * in compliance with the License.
- * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
- * See the License for the specific language governing rights and limitations under the License.
- * The Original Code is YetiForce.
- * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
- * All Rights Reserved.
- *************************************************************************************************************************************/
--->*}
+{*<!-- {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} --!>*}
 {strip}
 <div id="treePopupContainer" class="contentsDiv paddingLeftRight10px">
 	<input type="hidden" class="triggerEventName" value="{$TRIGGER_EVENT_NAME}"/>
---
+++ modules/Vtiger/data_access/check_day_tasks.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Lock saving events after exceeding the limit
+ * @package YetiForce.DataAccess
+ * @license licenses/License.html
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+class DataAccess_check_day_tasks
+{
+
+	var $config = true;
+
+	public function process($moduleName, $ID, $recordData, $config)
+	{
+		if (!in_array($moduleName, ['Calendar', 'Events'])) {
+			return ['save_record' => true];
+		}
+
+		$db = PearDatabase::getInstance();
+		$typeInfo = 'info';
+		$saveRecord = true;
+
+		$result = $db->pquery('SELECT count(*) as count FROM vtiger_activity 
+			INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid = vtiger_activity.activityid 
+			WHERE vtiger_crmentity.deleted = ? AND vtiger_activity.date_start = ?', [0, $recordData['date_start']]);
+
+		if ($config['lockSave'] == 1) {
+			$typeInfo = 'error';
+			$saveRecord = false;
+		}
+		$count = $db->getSingleValue($result);
+		if ($count >= $config['maxActivites'])
+			return [
+				'save_record' => $saveRecord,
+				'type' => 0,
+				'info' => [
+					'text' => vtranslate($config['message'], 'DataAccess'),
+					'ntype' => $typeInfo
+				]
+			];
+		else
+			return ['save_record' => true];
+	}
+
+	public function getConfig($id, $module, $baseModule)
+	{
+		return [];
+	}
+}
