cbbc68d76a61847822086aa4c4b963b3921b9242
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-02-03 10:28:34 +0100

    Fixed displaying favorite records in widgets

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160203';
-$YetiForce_current_version = '2.3.1123';
+$YetiForce_current_version = '2.3.1124';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
