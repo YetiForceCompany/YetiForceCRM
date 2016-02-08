fb69114fd70af145862f2b4622fc759896db6016
Krzysztof Gastołek <krzysztof.gastolek@wars.pl>
2016-02-08 11:13:05 +0100

    Revert "Fixed method of creating slides"
    This reverts commit 77a4081b5887d3314d828b9730fae59a66bb85e9.

--- modules/KnowledgeBase/views/PreviewContent.php
+++ modules/KnowledgeBase/views/PreviewContent.php
@@ -19,9 +19,7 @@ class KnowledgeBase_PreviewContent_View extends Vtiger_Index_View
 		$template = ucfirst(strtolower($type)) . 'View.tpl';
 
 		if ($type === 'PRESENTATION') {
-			// TODO find a better solution to divide slides
-			$explode = '<div style="page-break-after: always"><span style="display: none;"> </span></div>';
-			$content = explode($explode, $recordModel->get('content'));
+			$content = explode('<div><span> </span></div>', $recordModel->get('content'));
 		} else {
 			$content = $recordModel->get('content');
 		}
