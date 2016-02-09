eb7ff9e0f25dd57125d97db9649929ac765df244
Paula W <p.walenczak@yetiforce.com>
2016-02-09 14:39:50 +0100

    Added translations - Calendar.php

--- languages/de_de/Calendar.php
+++ languages/de_de/Calendar.php
@@ -136,6 +136,7 @@ $languageStrings = [
 	'LBL_RELATED_TO' => 'zugeordnet',
 	'LBL_NO_AVAILABLE_ACTIONS' => 'Keine verfügbaren Aktionen',
 	'LBL_INVITE_USER_BLOCK' => 'Einladen',
+	'LBL_CALENDAR_FILTERS' => 'Filters',
 ];
 $jsLanguageStrings = [
 	'JS_NO_EDIT_PERMISSION' => 'Sie haben keine Berechtigung, die Kalendereinträge zu ändern',
