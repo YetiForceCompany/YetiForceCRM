3c57a904594109d2f858e6f3d60245b5b1ad9c3d
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-02-11 00:03:11 +0100

    Minor amendment in the new task workflow

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160210';
-$YetiForce_current_version = '2.3.1174';
+$YetiForce_current_version = '2.3.1175';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
