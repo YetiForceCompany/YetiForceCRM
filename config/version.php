bc59d8fbb238af778c8aa790f633566bcca6e37e
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-02-11 11:19:32 +0100

    Fixed queuing update records related

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160211';
-$YetiForce_current_version = '2.3.1179';
+$YetiForce_current_version = '2.3.1180';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
