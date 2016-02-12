7e9d4cc76947807154d4551fe968f03d9affdaa5
Krzysztof Gastołek <krzysztof.gastolek@wars.pl>
2016-02-12 14:11:14 +0100

    Removed colspans from table in special_functions/ProductsTableNew.php

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160212';
-$YetiForce_current_version = '2.3.1199';
+$YetiForce_current_version = '2.3.1200';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
