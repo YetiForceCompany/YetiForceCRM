05d73ce3528cc56c25bb0217621f510c404f5da1
Tomasz Kur <t.kur@yetiforce.com>
2016-02-15 15:53:35 +0100

    Added pdf templates to IGRN, IGDN, IIDN and IGIN

--- install/install_schema/scheme.sql
+++ install/install_schema/scheme.sql
@@ -106,7 +106,7 @@ CREATE TABLE `a_yf_pdf` (
   `template_members` varchar(255) NOT NULL,
   PRIMARY KEY (`pdfid`),
   KEY `module_name` (`module_name`,`status`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `a_yf_taxes_config` */
 
