5ac239853b2f2696c7443aa803c7467c9f091352
Paula W <p.walenczak@yetiforce.com>
2016-02-16 15:58:39 +0100

    Removed redundant spaces

--- languages/de_de/Settings/QuickCreateEditor.php
+++ languages/de_de/Settings/QuickCreateEditor.php
@@ -10,11 +10,11 @@
  *************************************************************************************************************************************/
 $languageStrings = [
 	'QuickCreateEditor' => 'Schnellerfassungs-Editor',
-	'LBL_QUICK_CREATE_EDITOR'	=>	'Fields – Quick Create',
-	'LBL_QUICK_CREATE_EDITOR_DESCRIPTION'	=>	'Modul ermöglicht, das Layout der Felder innerhalb im Schnellerfassungs-Fenster zu ändern',
-	'LBL_SEQUENCE'	=>	'Feld Reihenfolge',
-	'LBL_SAVE_FIELD_SEQUENCE'	=>	'Feld Reihenfolge speichern',
+	'LBL_QUICK_CREATE_EDITOR' => 'Fields – Quick Create',
+	'LBL_QUICK_CREATE_EDITOR_DESCRIPTION' => 'Modul ermöglicht, das Layout der Felder innerhalb im Schnellerfassungs-Fenster zu ändern',
+	'LBL_SEQUENCE' => 'Feld Reihenfolge',
+	'LBL_SAVE_FIELD_SEQUENCE' => 'Feld Reihenfolge speichern',
 ];
 $jsLanguageStrings = [
-	'JS_FIELD_SEQUENCE_UPDATED'	=>	'Feld Reihenfolge aktualisiert',
+	'JS_FIELD_SEQUENCE_UPDATED' => 'Feld Reihenfolge aktualisiert',
 ];
