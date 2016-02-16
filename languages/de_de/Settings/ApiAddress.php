d2d72bea04f9ebd37d5a8086d473f7821f202c12
Paula W <p.walenczak@yetiforce.com>
2016-02-16 14:39:45 +0100

    Removed redundant spaces

--- languages/de_de/Settings/ApiAddress.php
+++ languages/de_de/Settings/ApiAddress.php
@@ -11,14 +11,14 @@
  
 $languageStrings = [
 	'LBL_SAVE' => 'Save',
-	'ApiAddress'	=>	'Adressen Suche',
-	'LBL_API_ADDRESS'	=>	'Maps [Google & OSM]',
-	'LBL_API_ADDRESS_DESCRIPTION'	=>	'Module erlaubt während der Erstellung/Bearbeitung von Daten, die Suche/das Autovervollständigen einer Adresse im System.',
-	'LBL_USE_OPENCAGE_GEOCODER'	=>	'Benutze OpenCage Geocoder',
-	'LBL_REMOVE_CONNECTION'	=>	'Verbindung entfernen',
-	'LBL_ENTER_KEY_APPLICATION'	=>	'Schlüssel Anwendung eingeben',
-	'LBL_MIN_LOOKUP_LENGHT'	=>	'Die minimale Anzahl von Zeichen, die ein Benutzer eingeben muss, bevor die Suche durchgeführt wird.',
-	'LBL_SAVE_NOTIFY_OK'	=>	'Änderungen gespeichert',
+	'ApiAddress' => 'Adressen Suche',
+	'LBL_API_ADDRESS' => 'Maps [Google & OSM]',
+	'LBL_API_ADDRESS_DESCRIPTION' => 'Module erlaubt während der Erstellung/Bearbeitung von Daten, die Suche/das Autovervollständigen einer Adresse im System.',
+	'LBL_USE_OPENCAGE_GEOCODER' => 'Benutze OpenCage Geocoder',
+	'LBL_REMOVE_CONNECTION'	=> 'Verbindung entfernen',
+	'LBL_ENTER_KEY_APPLICATION' => 'Schlüssel Anwendung eingeben',
+	'LBL_MIN_LOOKUP_LENGHT'	=> 'Die minimale Anzahl von Zeichen, die ein Benutzer eingeben muss, bevor die Suche durchgeführt wird.',
+	'LBL_SAVE_NOTIFY_OK' =>	'Änderungen gespeichert',
 	'LBL_CHOOSE_API' => 'Wähle API',
 	'LBL_USE_GOOGLE_GEOCODER' => 'Nutze Google Geocoding',
 	'google_map_api' => 'Google Geocoding',
@@ -28,8 +28,8 @@ $languageStrings = [
 	'LBL_NUMBER_SEARCH_RESULTS' => 'Anzahl der Suchergebnisse',
 ];
 $jsLanguageStrings = [
-	'Invalid API key'	=>	'Fehlerhafter API Schlüssel',
-	'JS_ERROR'	=>	'Fehler',
-	'JS_LOADING_PLEASE_WAIT'	=>	'Wird geladen, bitte warten.',
-	'JS_WRONG_NUMBER'	=>	' Erlaubte Zeichen: Ziffern größer als 1.',
+	'Invalid API key' => 'Fehlerhafter API Schlüssel',
+	'JS_ERROR' => 'Fehler',
+	'JS_LOADING_PLEASE_WAIT' => 'Wird geladen, bitte warten.',
+	'JS_WRONG_NUMBER' => ' Erlaubte Zeichen: Ziffern größer als 1.',
 ];
