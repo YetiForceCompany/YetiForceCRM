3673950bc39215058a124f01c778b8239c20f80c
Paula W <p.walenczak@yetiforce.com>
2016-02-16 14:47:30 +0100

    Removed redundant spaces

--- languages/de_de/Settings/CustomView.php
+++ languages/de_de/Settings/CustomView.php
@@ -2,19 +2,19 @@
 /* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 
 $languageStrings = [
-	'CustomView'	=> 'Filters - Configuration',
-	'LBL_MODULE_DESC'	=> 'Dieses Tool ermöglicht die Erstellung benutzerdefinierter Ansichten für Module. Diese Module müssen Datensätze enthalten und verwenden Standard-Filter im CRM.',
-	'Module'	=> 'Modul',
-	'ViewName'	=> 'Ansichtsname',
-	'SetDefault'	=> 'Standard',
-	'Privileges'	=> 'Berechtigungen',
-	'Delete'	=> 'Löschen',
-	'Delete CustomView'	=> 'Benutzerdefinierte Ansicht wurde gelöscht',
+	'CustomView' => 'Filters - Configuration',
+	'LBL_MODULE_DESC' => 'Dieses Tool ermöglicht die Erstellung benutzerdefinierter Ansichten für Module. Diese Module müssen Datensätze enthalten und verwenden Standard-Filter im CRM.',
+	'Module' => 'Modul',
+	'ViewName' => 'Ansichtsname',
+	'SetDefault' => 'Standard',
+	'Privileges' => 'Berechtigungen',
+	'Delete' => 'Löschen',
+	'Delete CustomView' => 'Benutzerdefinierte Ansicht wurde gelöscht',
 	'Saving CustomView' => 'Benutzerdefinierte Ansicht wurde gespeichert',
-	'Actions'	=> 'Aktionen',
+	'Actions' => 'Aktionen',
 	'Edit'	=> 'Bearbeiten',
 ];
 $jsLanguageStrings = [
-	'Saving changes'	=> 'Speichern von Änderungen...',
+	'Saving changes'=> 'Speichern von Änderungen...',
 	'Update labels'	=> 'Aktualisierung',
 ];
