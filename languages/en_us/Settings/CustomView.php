2c9893912aad12823f18dc1fbac897c7e1510fe1
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2016-02-18 09:48:46 +0100

    Fixed code formatting

--- languages/en_us/Settings/CustomView.php
+++ languages/en_us/Settings/CustomView.php
@@ -2,19 +2,19 @@
 /* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 
 $languageStrings = [
-	'CustomView'	=> 'Filters - Configuration',
-	'LBL_MODULE_DESC'	=> 'This tool allows setting custom views for modules. These modules must contain records and use standard filters in YetiForce CRM.',
-	'Module'	=> 'Module',
-	'ViewName'	=> 'View name',
-	'SetDefault'	=> 'Default',
-	'Privileges'	=> 'Privileges',
-	'Delete'	=> 'Delete',
-	'Delete CustomView'	=> 'Custom view has been deleted',
-	'Saving CustomView'	=> 'View has been saved',
-	'Actions'	=> 'Actions',
-	'Edit'	=> 'Edit',
+	'CustomView' => 'Filters - Configuration',
+	'LBL_MODULE_DESC' => 'This tool allows setting custom views for modules. These modules must contain records and use standard filters in YetiForce CRM.',
+	'Module' => 'Module',
+	'ViewName' => 'View name',
+	'SetDefault' => 'Default',
+	'Privileges' => 'Privileges',
+	'Delete' => 'Delete',
+	'Delete CustomView' => 'Custom view has been deleted',
+	'Saving CustomView' => 'View has been saved',
+	'Actions' => 'Actions',
+	'Edit' => 'Edit',
 ];
 $jsLanguageStrings = [
-	'Saving changes'	=> 'Saving changes...',
+	'Saving changes' => 'Saving changes...',
 	'Update labels'	=> 'Update',
 ];
