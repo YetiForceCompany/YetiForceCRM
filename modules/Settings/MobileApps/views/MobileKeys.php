f0703d38cab3e7aecbd993e02af5f45cb5f39176
Tomasz Kur <t.kur@yetiforce.com>
2016-02-23 12:49:39 +0100

    Improved loading api.php file

--- modules/Settings/MobileApps/views/MobileKeys.php
+++ modules/Settings/MobileApps/views/MobileKeys.php
@@ -14,7 +14,7 @@ class Settings_MobileApps_MobileKeys_View extends Settings_Vtiger_Index_View
 
 	public function process(Vtiger_Request $request)
 	{
-		include_once 'config/api.php';
+		include 'config/api.php';
 		$moduleName = $request->getModule();
 		$qualifiedModuleName = $request->getModule(false);
 		$moduleModel = Settings_MobileApps_Module_Model::getInstance($qualifiedModuleName);
