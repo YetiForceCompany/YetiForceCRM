f0cbc65ddeabd35ae58d6bbc03f1edf8df148419
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-02-25 00:00:59 +0100

    Improved export and import modules

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160224';
-$YetiForce_current_version = '2.3.1310';
+$YetiForce_current_version = '2.3.1311';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
