2a0a47956eeb4d16677415ab9a4913df99060d2f
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-02-25 08:39:05 +0100

    Fixed getting WYWIWYG type fields values.

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20160224';
-$YetiForce_current_version = '2.3.1312';
+$patch_version = '20160225';
+$YetiForce_current_version = '2.3.1313';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
