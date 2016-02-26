97281dd14ef2efe3fd77be26a3b4f8723ea9ce8e
Tomasz Kur <t.kur@yetiforce.com>
2016-02-26 15:17:55 +0100

    Added pdfs to IStorage

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160226';
-$YetiForce_current_version = '2.3.1333';
+$YetiForce_current_version = '2.3.1334';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
