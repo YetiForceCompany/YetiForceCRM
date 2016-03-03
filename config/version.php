146476b6d3df49f8b3ec969196015fc95b9e8d3f
Krzysztof Gastołek <krzysztof.gastolek@wars.pl>
2016-03-03 10:43:19 +0100

    Added barcodes to ProductsTableNew special function

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160303';
-$YetiForce_current_version = '2.3.1384';
+$YetiForce_current_version = '2.3.1385';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
