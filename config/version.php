5db4bbf389ded27689299f4556b5a932b516e2ce
Tomasz Kur <t.kur@yetiforce.com>
2016-03-08 13:00:19 +0100

    Fixed translation in ISTN module

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160308';
-$YetiForce_current_version = '2.3.1421';
+$YetiForce_current_version = '2.3.1422';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
