1e4f0cdb75e7142495dd106ae2d12e9280176f39
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-03-10 10:27:35 +0100

    Added in configuration file time to update number of notifications in seconds

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160310';
-$YetiForce_current_version = '2.3.1442';
+$YetiForce_current_version = '2.3.1443';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
