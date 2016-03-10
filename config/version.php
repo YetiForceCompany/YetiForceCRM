64760edd780c89813faf5ddfc19767e31a1960c5
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-03-10 08:51:01 +0100

    Changed layout of records list in widgets related with Assets and Sold Services modules.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160310';
-$YetiForce_current_version = '2.3.1440';
+$YetiForce_current_version = '2.3.1441';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
