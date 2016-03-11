91a3e54b1a6562e8943a144c5ef41fca35f3802b
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-03-11 10:34:11 +0100

    Button has been added to the list of track records

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160311';
-$YetiForce_current_version = '2.3.1453';
+$YetiForce_current_version = '2.3.1454';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
