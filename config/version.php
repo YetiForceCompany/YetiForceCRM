3d4953faadbe2149e23f9d9c7f6874135bc6d00e
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-03-13 18:30:09 +0100

    Improved appearance of fields in header.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160313';
-$YetiForce_current_version = '2.3.1466';
+$YetiForce_current_version = '2.3.1467';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
