be827a986ae2eda6d4657fe133c7a4e152721aaf
Krzysztof Gastołek <krzysztof.gastolek@wars.pl>
2016-03-14 10:22:40 +0100

    Special function displaying timestamp in PDFs.

---
+++ modules/Vtiger/pdfs/special_functions/TimeStamp.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Special function displaying timestamp
+ * @package YetiForce.SpecialFunction
+ * @license licenses/License.html
+ * @author Krzysztof Gastołek <krzysztof.gastolek@wars.pl>
+ */
+class Pdf_TimeStamp extends Vtiger_SpecialFunction_Pdf
+{
+
+	public $permittedModules = ['all'];
+
+	public function process($module, $id, Vtiger_PDF_Model $pdf)
+	{
+		return DateTimeField::convertToUserFormat(date('Y-m-d'));
+	}
+}
