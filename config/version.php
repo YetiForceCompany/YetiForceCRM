8fb945a8ce7fdb23268df2ce4701a218f00cefc4
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-03-14 09:28:57 +0100

    Removed redundant columns in some advanced modules.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160314';
-$YetiForce_current_version = '2.3.1468';
+$YetiForce_current_version = '2.3.1469';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
