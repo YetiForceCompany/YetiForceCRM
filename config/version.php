b0c0d2dd8dfc92dd06328cd89aee9fc0504a0a17
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-03-14 16:05:53 +0100

    Fixed default status value in Sales enquiries module

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160314';
-$YetiForce_current_version = '2.3.1489';
+$YetiForce_current_version = '2.3.1490';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
