c431d0f31c3a8c30b60870ca306d6137f9240ff6
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-03-14 15:40:11 +0100

    Fixed appearance of the selection list

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160314';
-$YetiForce_current_version = '2.3.1487';
+$YetiForce_current_version = '2.3.1488';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
