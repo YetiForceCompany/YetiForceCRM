a098178a794fdb827eaf13edc3f011750da6c913
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-03-17 10:19:05 +0100

    Improved the order of fields in tickets

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160317';
-$YetiForce_current_version = '2.3.1531';
+$YetiForce_current_version = '2.3.1532';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
