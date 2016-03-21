eaedca3f399a6d0f073fa19cbfc6a9829e46977f
Tomasz Kur <t.kur@yetiforce.com>
2016-03-21 13:20:05 +0100

    Improved widgets for invoice

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160321';
-$YetiForce_current_version = '2.3.1558';
+$YetiForce_current_version = '2.3.1559';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
