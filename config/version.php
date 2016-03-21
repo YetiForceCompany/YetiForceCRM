a93cf82b4fa4ae5d25169425d0112c0530501e9b
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-03-21 04:36:34 +0100

    Fixed permissions when creating modules.

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20160320';
-$YetiForce_current_version = '2.3.1553';
+$patch_version = '20160321';
+$YetiForce_current_version = '2.3.1554';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
