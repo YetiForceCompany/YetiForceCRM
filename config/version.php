571d8557b12283991131147972d5a0cab292e692
Tomasz Kur <t.kur@yetiforce.com>
2016-03-21 12:40:59 +0100

    Improved permission when saving

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160321';
-$YetiForce_current_version = '2.3.1557';
+$YetiForce_current_version = '2.3.1558';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
