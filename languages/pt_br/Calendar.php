b941d03f16d97bdfefe2a9cdcaaba78c53790fe2
Paula W <p.walenczak@yetiforce.com>
2016-03-23 13:45:07 +0100

    Added translations - Calendar.php

--- languages/pt_br/Calendar.php
+++ languages/pt_br/Calendar.php
@@ -150,6 +150,8 @@ $languageStrings = [
 	'LBL_INVITE_USER_BLOCK' => 'Convite',
 	'LBL_CALENDAR_FILTERS' => 'Filters',
 	'LBL_AUTO_FILL_DESCRIPTION' => 'Fill the start date based on the first available date',
+	'LBL_CACHE_SELECTED_FILTERS' => 'Active filters:',
+	'LBL_CACHE_CLEAR' => 'Clear filters',
 ];
 
 $jsLanguageStrings = [
@@ -198,4 +200,6 @@ $jsLanguageStrings = [
 	'JS_SWITCHING_DAYS' => 'Switch days ',
 	'JS_ALL' => 'All',
 	'JS_WORK_DAYS' => 'Work days',
+	'JS_CHANGED_VIEW' => 'View',
+	'JS_CHANGED_DATE' => 'Date',
 ];
