0b257bad1ed8ca12469fcb7064fe9ea13e55efe2
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-03-23 12:54:08 +0100

    Added caching module settings

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160323';
-$YetiForce_current_version = '2.3.1570';
+$YetiForce_current_version = '2.3.1571';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
