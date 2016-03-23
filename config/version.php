b9ce447434e9b075f094a4d020047bba20ed4169
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-03-23 12:53:21 +0100

    Improved JS functions

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160323';
-$YetiForce_current_version = '2.3.1569';
+$YetiForce_current_version = '2.3.1570';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
