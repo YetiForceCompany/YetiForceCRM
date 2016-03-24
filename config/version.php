0c10e6285ba2919fa6d094088f6a4d9e05f0c996
Tomasz Kur <t.kur@yetiforce.com>
2016-03-24 08:35:56 +0100

    Fixed adding fields

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160324';
-$YetiForce_current_version = '2.3.1581';
+$YetiForce_current_version = '2.3.1582';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
