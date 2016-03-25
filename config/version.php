2fef88f72509ee33838100cb54a60e38e0c900b1
Tomasz Kur <t.kur@yetiforce.com>
2016-03-25 10:14:08 +0100

    Removed a pack of migrating from VT620 to YF

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160325';
-$YetiForce_current_version = '2.3.1604';
+$YetiForce_current_version = '2.3.1605';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
