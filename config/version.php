29fab1affee97f0a67de246bcabadac21683108b
Tomasz Kur <t.kur@yetiforce.com>
2016-03-25 16:17:30 +0100

    Fixed appearance of modal when convert Lead

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160325';
-$YetiForce_current_version = '2.3.1608';
+$YetiForce_current_version = '2.3.1609';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
