2d5eb33799e89cd864d6a835c41988952f2a4d05
Paula W <p.walenczak@yetiforce.com>
2016-03-25 09:40:40 +0100

    Changed translations - Calendar.php

--- languages/ru_ru/Calendar.php
+++ languages/ru_ru/Calendar.php
@@ -161,8 +161,8 @@ $languageStrings = [
 	'LBL_INVITE_USER_BLOCK' => 'Пригласить',
 	'LBL_CALENDAR_FILTERS' => 'Filters',
 	'LBL_AUTO_FILL_DESCRIPTION' => 'Fill the start date based on the first available date',
-	'LBL_CACHE_SELECTED_FILTERS' => 'Active filters:',
-	'LBL_CACHE_CLEAR' => 'Clear filters',
+	'LBL_CACHE_SELECTED_FILTERS' => 'Standard view in calendar changed. Click "Clear view" to clear it.',
+	'LBL_CACHE_CLEAR' => 'Clear view',
 ];
 $jsLanguageStrings = [
 	'JS_ADD_EVENT_TASK' => '+ Событие | Задача',
@@ -300,6 +300,4 @@ $jsLanguageStrings = [
 	'JS_SWITCHING_DAYS' => 'Switch days ',
 	'JS_ALL' => 'All',
 	'JS_WORK_DAYS' => 'Work days',
-	'JS_CHANGED_VIEW' => 'View',
-	'JS_CHANGED_DATE' => 'Date',
 ];
