71ed12176fb068ca75bb088150820ae9ccc401b1
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-03-29 15:39:34 +0200

    Improved vtlib functions

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160329';
-$YetiForce_current_version = '2.3.1620';
+$YetiForce_current_version = '2.3.1621';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
