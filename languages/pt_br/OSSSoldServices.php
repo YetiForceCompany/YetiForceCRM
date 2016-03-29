ec17b82a46e605fd260f841370e575720000987a
Paula W <p.walenczak@yetiforce.com>
2016-03-29 15:57:47 +0200

    Added translations & removed duplicates - OSSSoldServices.php

--- languages/pt_br/OSSSoldServices.php
+++ languages/pt_br/OSSSoldServices.php
@@ -10,15 +10,13 @@
  * Contributor(s): Brazilian Portuguese Translation - Valmir Carlos Trindade - valmir@ttcasolucoes.com.br
  * *********************************************************************************************************************************** */
 $languageStrings = [
-	'LBL_RECORDS_LIST' => 'Lista registros',
-	'LBL_ADD_RECORD' => 'Adicionar registro',
-	'Services' => 'Serviços',
-	'Invoice' => 'Fatura',
 	'OSSSoldServices' => 'Serviços Vendidos',
 	'SINGLE_OSSSoldServices' => 'Serviço Vendido',
+	'LBL_RECORDS_LIST' => 'Lista registros',
+	'LBL_ADD_RECORD' => 'Adicionar registro',
+	// Blocks
 	'LBL_INFORMATION' => 'Bloco Informação',
-//fields
-	'Invoice number' => 'No. Fatura',
+	// Field Labels
 	'Number' => 'Número',
 	'Assigned To' => 'Responsável',
 	'LBL_RODZAJUMOWY' => 'Tipo de Contrato',
@@ -32,9 +30,11 @@ $languageStrings = [
 	'LBL_DESCRIPTION_INFORMATION' => 'Informação descitiva',
 	'LBL_CUSTOM_INFORMATION' => 'Informação do Sistema',
 	'Where bought' => 'Onde comprou',
-	'Number' => 'Número',
+	'Services' => 'Serviços',
 	'LBL_GENERATE_EXTENSION' => 'Gerar extensão',
 	'LBL_ORDER_TIME' => 'Hora Pedido',
+	'FL_RENEWAL_INVOICE' => 'Renewal invoice',
+	// picklist values
 	'FL_RENEWAL' => 'Renewal',
 	'PLL_DRAFT' => 'Draft',
 	'PLL_WAITING_FOR_VERIFICATION' => 'Waiting for verification',
@@ -43,8 +43,6 @@ $languageStrings = [
 	'PLL_CANCELLED' => 'Cancelled',
 	'PLL_PLANNED' => 'Planned',
 	'PLL_WAITING_FOR_RENEWAL' => 'Waiting for renewal',
-	'PLL_WAITING_FOR_VERIFICATION' => 'Waiting for verification',
-	'PLL_WAITING_FOR_ACCEPTANCE' => 'Waiting for acceptance',
 	'PLL_RENEWED' => 'Renewed',
 	'PLL_NOT_RENEWED' => 'Not renewed',
 	'PLL_NOT_APPLICABLE' => 'Not applicable',
