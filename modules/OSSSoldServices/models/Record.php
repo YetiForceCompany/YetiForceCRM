ae20fa783b920d39c5a9f96dd5451a790c1cbeeb
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-03-29 15:38:30 +0200

    Added update renewals in SoldServices

--- modules/OSSSoldServices/models/Record.php
+++ modules/OSSSoldServices/models/Record.php
@@ -21,4 +21,37 @@ class OSSSoldServices_Record_Model extends Vtiger_Record_Model
 			'name' => 'ssservicesstatus',
 		];
 	}
+	
+	public function updateRenewal()
+	{
+		$value = $this->getRenewalValue();
+		if ($value) {
+			$this->set('osssoldservices_renew', $value);
+			$this->set('mode', 'edit');
+			$this->save();
+		}
+	}
+
+	public function getRenewalValue()
+	{
+		if ($this->has('serviceid') == false) {
+			return 'PLL_NOT_APPLICABLE';
+		}
+		$productsRecordModel = Vtiger_Record_Model::getInstanceById($this->get('serviceid'), 'Services');
+		$renewable = $productsRecordModel->get('renewable');
+		if (!$renewable) {
+			return 'PLL_NOT_APPLICABLE';
+		}
+		if (!$this->isEmpty('renewalinvoice')) {
+			return 'PLL_RENEWED';
+		}
+		$dateRenewable = strtotime(AppConfig::module('OSSSoldServices', 'RENEWAL_TIME'), strtotime($this->get('dateinservice')));
+		if ($dateRenewable > time()) {
+			return 'PLL_PLANNED';
+		}
+		if (strtotime("+1 month", $dateRenewable) > time()) {
+			return 'PLL_WAITING_FOR_RENEWAL';
+		}
+		return 'PLL_NOT_RENEWED';
+	}
 }
