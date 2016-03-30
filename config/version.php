3e6a8ac4968e98af834af802e680c4a90578257c
Tomasz Kur <t.kur@yetiforce.com>
2016-03-30 14:38:32 +0200

    Added menuitem

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160330';
-$YetiForce_current_version = '2.3.1632';
+$YetiForce_current_version = '2.3.1633';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
