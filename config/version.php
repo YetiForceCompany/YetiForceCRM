91543385b2dafc1bdbb2ca12a05ef8974fc921b8
Tomasz Kur <t.kur@yetiforce.com>
2016-04-04 15:46:21 +0200

    Fixed fields in modules FInvoice, FInvoiceProforma, FCorectingInvoice

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160404';
-$YetiForce_current_version = '2.3.1659';
+$YetiForce_current_version = '2.3.1660';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
