3e74d2b035104d34ad0f9a40f631e342eb4a46ce
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-04-05 12:25:08 +0200

    Changed to display the history of changes

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20160404';
-$YetiForce_current_version = '2.3.1660';
+$patch_version = '20160405';
+$YetiForce_current_version = '2.3.1661';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
