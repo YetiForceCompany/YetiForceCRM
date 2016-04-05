4a8adae1f298047d06733dec94b9c00dbcf440da
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-04-05 15:03:52 +0200

    Improved ModTracker

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160405';
-$YetiForce_current_version = '2.3.1665';
+$YetiForce_current_version = '2.3.1666';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
