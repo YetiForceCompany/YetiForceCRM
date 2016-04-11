5effe40fc038e1c42ca3bf867e035b8bd9be9d7e
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-04-11 10:18:41 +0200

    Fixed api

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160411';
-$YetiForce_current_version = '2.3.1703';
+$YetiForce_current_version = '2.3.1704';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
