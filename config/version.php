e33929dc59c4e28d7c9d16a3c3f10264351f8aa8
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-04-12 10:37:04 +0200

    Fixed display of the fields in the Layout Editor

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160412';
-$YetiForce_current_version = '2.3.1723';
+$YetiForce_current_version = '2.3.1724';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
