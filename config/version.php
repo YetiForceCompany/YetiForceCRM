b648735bcc3f5526e75af7ac178ca415b52fe50e
Adrian Koń <a.kon@yetiforce.com>
2016-04-15 13:33:54 +0200

    Added classes to icons

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160415';
-$YetiForce_current_version = '2.3.1767';
+$YetiForce_current_version = '2.3.1768';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
