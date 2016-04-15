b6f674aab8e2b36022d1bc5e3a199298e69d6681
Adrian Koń <a.kon@yetiforce.com>
2016-04-15 12:23:21 +0200

    Removed using deleted template

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160415';
-$YetiForce_current_version = '2.3.1763';
+$YetiForce_current_version = '2.3.1764';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
