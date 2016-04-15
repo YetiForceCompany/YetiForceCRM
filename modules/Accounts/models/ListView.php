234734e02bcefbc8174e7f2b1f57a8fc9b06a2ad
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-04-15 09:42:31 +0200

    Improved list of records

--- modules/Accounts/models/ListView.php
+++ modules/Accounts/models/ListView.php
@@ -1,29 +1,31 @@
 <?php
-/*+***********************************************************************************
+/* +***********************************************************************************
  * The contents of this file are subject to the vtiger CRM Public License Version 1.0
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *************************************************************************************/
+ * *********************************************************************************** */
 
-class Accounts_ListView_Model extends Vtiger_ListView_Model {
+class Accounts_ListView_Model extends Vtiger_ListView_Model
+{
 
 	/**
 	 * Function to get the list of Mass actions for the module
 	 * @param <Array> $linkParams
 	 * @return <Array> - Associative array of Link type to List of  Vtiger_Link_Model instances for Mass Actions
 	 */
-	public function getListViewMassActions($linkParams) {
+	public function getListViewMassActions($linkParams)
+	{
 		$links = parent::getListViewMassActions($linkParams);
 
 		$currentUserModel = Users_Privileges_Model::getCurrentUserPrivilegesModel();
 		$emailModuleModel = Vtiger_Module_Model::getInstance('Emails');
 		$moduleModel = $this->getModule();
-		
+
 		$massActionLinks = [];
-		if($currentUserModel->hasModulePermission($emailModuleModel->getId()) && $currentUserModel->hasModuleActionPermission($moduleModel->getId(), 'MassComposeEmail')) {
+		if ($currentUserModel->hasModulePermission($emailModuleModel->getId()) && $currentUserModel->hasModuleActionPermission($moduleModel->getId(), 'MassComposeEmail')) {
 			$massActionLinks[] = array(
 				'linktype' => 'LISTVIEWMASSACTION',
 				'linklabel' => 'LBL_MASS_SEND_EMAIL',
@@ -33,35 +35,38 @@ class Accounts_ListView_Model extends Vtiger_ListView_Model {
 		}
 
 		$SMSNotifierModuleModel = Vtiger_Module_Model::getInstance('SMSNotifier');
-		if(!empty($SMSNotifierModuleModel) && $currentUserModel->hasModulePermission($SMSNotifierModuleModel->getId()) && $currentUserModel->hasModuleActionPermission($moduleModel->getId(), 'MassSendSMS')) {
+		if (!empty($SMSNotifierModuleModel) && $currentUserModel->hasModulePermission($SMSNotifierModuleModel->getId()) && $currentUserModel->hasModuleActionPermission($moduleModel->getId(), 'MassSendSMS')) {
 			$massActionLinks[] = array(
 				'linktype' => 'LISTVIEWMASSACTION',
 				'linklabel' => 'LBL_MASS_SEND_SMS',
-				'linkurl' => 'javascript:Vtiger_List_Js.triggerSendSms("index.php?module='.$this->getModule()->getName().'&view=MassActionAjax&mode=showSendSMSForm","SMSNotifier");',
+				'linkurl' => 'javascript:Vtiger_List_Js.triggerSendSms("index.php?module=' . $this->getModule()->getName() . '&view=MassActionAjax&mode=showSendSMSForm","SMSNotifier");',
 				'linkicon' => ''
 			);
 		}
 
-		foreach($massActionLinks as $massActionLink) {
+		foreach ($massActionLinks as $massActionLink) {
 			$links['LISTVIEWMASSACTION'][] = Vtiger_Link_Model::getInstanceFromValues($massActionLink);
 		}
 		return $links;
 	}
-	
+
 	/**
 	 * Function to get the list of listview links for the module
 	 * @param <Array> $linkParams
 	 * @return <Array> - Associate array of Link Type to List of Vtiger_Link_Model instances
 	 */
-	function getListViewLinks($linkParams) {
+	function getListViewLinks($linkParams)
+	{
 		$links = parent::getListViewLinks($linkParams);
 
-		$index=0;
-		foreach($links['LISTVIEWBASIC'] as $link) {
-			if($link->linklabel == 'Send SMS') {
-				unset($links['LISTVIEWBASIC'][$index]);
+		if (!empty($links['LISTVIEWBASIC'])) {
+			$index = 0;
+			foreach ($links['LISTVIEWBASIC'] as $link) {
+				if ($link->linklabel == 'Send SMS') {
+					unset($links['LISTVIEWBASIC'][$index]);
+				}
+				$index++;
 			}
-			$index++;
 		}
 		return $links;
 	}
