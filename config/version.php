aacd3237cde08c1811a8d767885d0cde508d96a2
Adrian Koń <a.kon@yetiforce.com>
2016-04-18 11:26:00 +0200

    Fixed improved code with php 5.4 version

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160417';
-$YetiForce_current_version = '2.3.1781';
+$YetiForce_current_version = '2.3.1782';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
