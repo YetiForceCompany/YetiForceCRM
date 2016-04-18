263031e1f543f1272dab47c08e10edf6c4777001
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-04-18 11:13:51 +0200

    Fix PHP debug statements

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160417';
-$YetiForce_current_version = '2.3.1780';
+$YetiForce_current_version = '2.3.1781';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
