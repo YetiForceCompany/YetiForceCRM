46e84e6fc2b85054a4bf5c0c58a1fcc76cdeb347
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-04-27 10:30:46 +0200

    Fixed loading related module records.

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20160426';
-$YetiForce_current_version = '3.0.23';
+$patch_version = '20160427';
+$YetiForce_current_version = '3.0.24';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
