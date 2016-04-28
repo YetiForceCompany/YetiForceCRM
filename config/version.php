a9c32efe510cfb95318ebcb5556d5cfbab647f7a
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-04-28 11:03:22 +0200

    Improved registration errors

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160428';
-$YetiForce_current_version = '3.0.28';
+$YetiForce_current_version = '3.0.29';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
