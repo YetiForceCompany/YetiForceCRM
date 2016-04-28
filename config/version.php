8536c17ab29e119ac3982f3974996d90d42946da
Tomasz Kur <t.kur@yetiforce.com>
2016-04-28 13:49:07 +0200

    Improved formating time in the history

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160428';
-$YetiForce_current_version = '3.0.31';
+$YetiForce_current_version = '3.0.32';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
