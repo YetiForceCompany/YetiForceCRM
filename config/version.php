2fda1bc87e4f3fa3e15c2349d3db864b5ff75487
Tomasz Kur <t.kur@yetiforce.com>
2016-04-29 13:41:16 +0200

    Added action to API

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160429';
-$YetiForce_current_version = '3.0.43';
+$YetiForce_current_version = '3.0.44';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
