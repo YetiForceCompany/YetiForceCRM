a32d845265ea4a476a3e7955012a7730359f15f2
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-04-29 16:07:55 +0200

    Changed the temporary directory for mails

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160429';
-$YetiForce_current_version = '3.0.48';
+$YetiForce_current_version = '3.0.49';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
