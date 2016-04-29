dc2a94702946b04b926f78b004e3e8d0a97db096
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-04-29 09:26:31 +0200

    Improved displaying credit limits list.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160429';
-$YetiForce_current_version = '3.0.37';
+$YetiForce_current_version = '3.0.38';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
