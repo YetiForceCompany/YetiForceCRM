33d2b92f1174d16bcb2788e7781dda2dd331bf36
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-05-05 12:34:41 +0200

    Added the ability to send notifications to yourself

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160505';
-$YetiForce_current_version = '3.0.72';
+$YetiForce_current_version = '3.0.73';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
