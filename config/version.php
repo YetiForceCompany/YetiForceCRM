3a9a49a1356d6b90a4cdf7d73f3a5db96d2ae6cb
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-05-05 11:14:06 +0200

    Added update of the date of the last invoice

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160505';
-$YetiForce_current_version = '3.0.69';
+$YetiForce_current_version = '3.0.70';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
