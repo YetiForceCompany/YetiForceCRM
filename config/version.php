8c32e9de891156ca3ab112653c2523bca45d5d12
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-05-06 09:45:12 +0200

    Fixed name of cached variable in function that gets all advanced block fields.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160506';
-$YetiForce_current_version = '3.0.79';
+$YetiForce_current_version = '3.0.80';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
