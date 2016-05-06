32f74d7a7722c68f6701b6c3b01015fd7fce1770
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-05-06 08:30:29 +0200

    Fixed display of menu separator

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160506';
-$YetiForce_current_version = '3.0.74';
+$YetiForce_current_version = '3.0.75';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
