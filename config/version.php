38a592196d274eed7bb26c06508054b48f067ab7
Tomasz Kur <t.kur@yetiforce.com>
2016-05-06 08:47:42 +0200

    Fixed display of icons in menu

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160506';
-$YetiForce_current_version = '3.0.76';
+$YetiForce_current_version = '3.0.77';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
