28697b734c688c01c5ebb789a8543b2823e1bc53
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-05-10 13:04:49 +0200

    Improved Timeline widget.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160510';
-$YetiForce_current_version = '3.0.106';
+$YetiForce_current_version = '3.0.107';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
