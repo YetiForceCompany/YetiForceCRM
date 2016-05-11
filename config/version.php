94783bbd00111ece0151aabc103386ecf9cfc7be
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-05-11 14:00:56 +0200

    Improved text parser

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160511';
-$YetiForce_current_version = '3.0.117';
+$YetiForce_current_version = '3.0.118';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
