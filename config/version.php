77eca552e3c9e12f29c9645929900b1dc6d876d1
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-05-11 14:05:24 +0200

    Added minor fixes

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160511';
-$YetiForce_current_version = '3.0.120';
+$YetiForce_current_version = '3.0.121';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
