5dd56af94ea0f2c537aa2d101f4753c706c9a68a
Tomasz Kur <t.kur@yetiforce.com>
2016-05-12 12:35:05 +0200

    Added translations

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160512';
-$YetiForce_current_version = '3.0.127';
+$YetiForce_current_version = '3.0.128';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
