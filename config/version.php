758c1f2851fe9e4a3141a2a573fa0b3e550df6e5
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2016-05-17 11:48:52 +0200

    Revert "Fix #2986"
    This reverts commit 3a3493626404ab975b4fa57db2f63f5d853a8e6a.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160517';
-$YetiForce_current_version = '3.0.154';
+$YetiForce_current_version = '3.0.153';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
