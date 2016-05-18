e9f6383ebc59a19fc747f440e688e13aba00fb0b
Adrian Koń <a.kon@yetiforce.com>
2016-05-18 10:31:58 +0200

    Fix #3008

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160518';
-$YetiForce_current_version = '3.1.1';
+$YetiForce_current_version = '3.1.2';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
