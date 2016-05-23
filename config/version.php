93d8eee117eec1a4e9b8283b1fe7138c5d69502f
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2016-05-23 08:22:25 +0200

    #3042 Fixed the incorrect query

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160523';
-$YetiForce_current_version = '3.1.61';
+$YetiForce_current_version = '3.1.62';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
