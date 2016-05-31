cbe196c5bc97e4764d2ac4df64b0fe2831a99567
Adrian Koń <a.kon@yetiforce.com>
2016-05-31 13:57:54 +0200

    Revert the changes

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160531';
-$YetiForce_current_version = '3.1.182';
+$YetiForce_current_version = '3.1.183';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
