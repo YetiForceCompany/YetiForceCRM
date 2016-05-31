422209dc2fdf2c1e1bfcde7283f8c5748a670564
Tomasz Kur <t.kur@yetiforce.com>
2016-05-31 08:41:50 +0200

    Removed unnecessary code

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160531';
-$YetiForce_current_version = '3.1.165';
+$YetiForce_current_version = '3.1.166';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
