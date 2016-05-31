70ace15797018e0d07934ed48a8cde9d9483ff76
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-05-31 07:14:27 +0200

    Small fixes for status change using modal window in IGDNC module.

---
+++ modules/IGDNC/views/EditFieldByModal.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * EditFieldByModal View Class
+ * @package YetiForce.View
+ * @license licenses/License.html
+ * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
+class IGDNC_EditFieldByModal_View extends Vtiger_EditFieldByModal_View
+{
+
+	protected $restrictItems = ['PLL_ACCEPTED' => 'btn-success'];
+
+}
