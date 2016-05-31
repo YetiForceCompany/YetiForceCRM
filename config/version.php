e960e0e554334db601453dd1a6f6b2686a4dabc7
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-05-31 07:03:14 +0200

    Small fixes for status change using modal window in IGDN module.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160531';
-$YetiForce_current_version = '3.1.161';
+$YetiForce_current_version = '3.1.162';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
