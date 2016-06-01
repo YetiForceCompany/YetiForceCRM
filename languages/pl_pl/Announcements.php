17bfc45ec4cf02c5fe83efea3f8cbe85b03bc0c3
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2016-06-01 14:40:27 +0200

    Changed translation

--- languages/pl_pl/Announcements.php
+++ languages/pl_pl/Announcements.php
@@ -1,13 +1,11 @@
 <?php
 /* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 $languageStrings = [
-	'Announcements' => 'Zarządzenia',
-	'SINGLE_Announcements' => 'Zarządzenie',
-
-	//BLOCKS
+	'Announcements' => 'Komunikaty',
+	'SINGLE_Announcements' => 'Komunikat',
+	//Blocks
 	'LBL_ANNOUNCEMENTS_INFORMATION' => 'Podstawowe informacje',
-
-	//FIELDS
+	//Fields
 	'LBL_SUBJECT' => 'Temat',
 	'LBL_NUMBER' => 'Numer'
 ];
