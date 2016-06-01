1a1d5fb06f262b5dc93b93ff4c422254b6c64776
Paula W <p.walenczak@yetiforce.com>
2016-06-01 15:02:03 +0200

    Changed translations - Announcements.php

--- languages/en_us/Announcements.php
+++ languages/en_us/Announcements.php
@@ -1,12 +1,10 @@
 <?php
 /* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 $languageStrings = [
-	'Announcements' => 'New Orders',
-	'SINGLE_Announcements' => 'New Orders',
-
+	'Announcements' => 'Announcements',
+	'SINGLE_Announcements' => 'Announcement',
 	//BLOCKS
 	'LBL_ANNOUNCEMENTS_INFORMATION' => 'Basic information',
-
 	//FIELDS
 	'LBL_SUBJECT' => 'Subject',
 	'LBL_NUMBER' => 'Number'
