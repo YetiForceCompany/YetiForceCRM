ba171558fb177633083dfa3b0fd86e3d264b6cf0
Tomasz Kur <t.kur@yetiforce.com>
2016-06-02 14:15:44 +0200

    Updated icons

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160602';
-$YetiForce_current_version = '3.1.233';
+$YetiForce_current_version = '3.1.234';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
