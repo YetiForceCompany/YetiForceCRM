b683a243c9162c00f408de3a2fc985c33f43a1ab
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2016-06-02 12:34:54 +0200

    Added translations to Announcements.php

--- languages/en_us/Announcements.php
+++ languages/en_us/Announcements.php
@@ -8,4 +8,17 @@ $languageStrings = [
 	//FIELDS
 	'LBL_SUBJECT' => 'Subject',
 	'LBL_NUMBER' => 'Number'
+	'FL_STATUS' => 'Status',
+	'FL_INTERVAL' => 'Reminder time',
+	// Picklist values
+	'PLL_DRAFT' => 'Draft',
+	'PLL_FOR_ACCEPTANCE' => 'For approval',
+	'PLL_PUBLISHED' => 'Published',
+	'PLL_ARCHIVES' => 'Archived',
+	// Alert
+	'LBL_REMIND_ME_LATER' => 'Remind me later',
+	'LBL_MARK_AS_READ' => 'Mark as read',
+	// Others
+	'LBL_ACCEPT_ANNOUNCEMENT' => 'Was accepted?',
+	'LBL_DATE' => 'Read on',
 ];
