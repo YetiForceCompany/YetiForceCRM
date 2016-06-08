9078dd9afe24cdefc612765317fb0bf15c6b9941
Tomasz Kur <t.kur@yetiforce.com>
2016-06-08 18:50:02 +0200

    Improved appearance of login page

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160608';
-$YetiForce_current_version = '3.1.288';
+$YetiForce_current_version = '3.1.289';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
