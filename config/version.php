0cd2cd1f01bb663bc3124caeab275214784926f7
Tomasz Kur <t.kur@yetiforce.com>
2016-06-10 20:59:04 +0200

    Improved edit price in PriceBooks

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160610';
-$YetiForce_current_version = '3.1.304';
+$YetiForce_current_version = '3.1.305';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
--- modules/PriceBooks/views/ListPriceUpdate.php
+++ modules/PriceBooks/views/ListPriceUpdate.php
@@ -1,45 +1,41 @@
 <?php
-/*+***********************************************************************************
+
+/* +***********************************************************************************
  * The contents of this file are subject to the vtiger CRM Public License Version 1.0
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *************************************************************************************/
+ * *********************************************************************************** */
 
-class PriceBooks_ListPriceUpdate_View extends Vtiger_View_Controller {
+class PriceBooks_ListPriceUpdate_View extends Vtiger_BasicModal_View {
 
-	function checkPermssion(Vtiger_Request $request) {
+	function checkPermission(Vtiger_Request $request) {
 		$moduleName = $request->getModule();
 		$moduleModel = Vtiger_Module_Model::getInstance($moduleName);
 
 		$currentUserPriviligesModel = Users_Privileges_Model::getCurrentUserPrivilegesModel();
-		if(!$currentUserPriviligesModel->hasModulePermission($moduleModel->getId())) {
-			throw new AppException(vtranslate($moduleName).' '.vtranslate('LBL_NOT_ACCESSIBLE'));
+		if (!$currentUserPriviligesModel->hasModulePermission($moduleModel->getId())) {
+			throw new AppException(vtranslate($moduleName) . ' ' . vtranslate('LBL_NOT_ACCESSIBLE'));
 		}
 	}
 
-	function preProcess(Vtiger_Request $request, $display = true) {
-	}
-
 	function process(Vtiger_Request $request) {
+		parent::preProcess($request);
 		$moduleName = $request->getModule();
 		$priceBookId = $request->get('record');
 		$relId = $request->get('relid');
 		$currentPrice = $request->get('currentPrice');
 
 		$viewer = $this->getViewer($request);
-		$viewer->assign('MODULE',$moduleName);
+		$viewer->assign('MODULE', $moduleName);
 		$viewer->assign('PRICEBOOK_ID', $priceBookId);
 		$viewer->assign('REL_ID', $relId);
 		$viewer->assign('CURRENT_PRICE', $currentPrice);
 		$viewer->assign('USER_MODEL', Users_Record_Model::getCurrentUserModel());
 		$viewer->view('ListPriceUpdate.tpl', $moduleName);
+		parent::postProcess($request);
 	}
 
-	function postProcess(Vtiger_Request $request) {
-	}
 }
-
-?>
