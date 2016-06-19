f07668bab0c0ca0290ef14f6095d3806be8f449e
skavenkf <skavenkf@gmx.de>
2016-06-20 00:22:02 +0200

    update german language

--- languages/de_de/Settings/SalesProcesses.php
+++ languages/de_de/Settings/SalesProcesses.php
@@ -11,7 +11,7 @@
 $languageStrings = [
 	'LBL_SALES_PROCESSES' => 'Vertrieb',
 	'LBL_SALES_PROCESSES_DESCRIPTION' => 'Einstellungen zu den Vertriebsprozessen',
-	'LBL_LIMIT_PRODUCT_AND_SERVICE' => 'Limit the choice of products/services only to the ones related to currently selected Opportunity. Applies to modules: Quote Enquiries, Sales requirements, Calculations, Quotes, Sales Orders, Recurring Sales Orders.',
+	'LBL_LIMIT_PRODUCT_AND_SERVICE' => 'Auswahl der Produkte/Services auf die der zugeordneten Verkaufschance einschränken. Betrifft die folgenden Module: Kaufanfragen, Vertriebsanforderungen, Kalkulationen, Angebote, Verkaufsbestellung, Dauerauftrag.',
 	'LBL_PRODUCTS_AND_SERVICES_POPUP' => 'Liste der Produkte und Dienstleistungen',
 	'LBL_UPDATE_SHARED_PERMISSIONS' => 'Aktualisiert die geteilten Berechtigungen von Produkten/Dienstleistungen wenn eine Beziehung zwischen Opportunity und Produkt/Dienstleistung angelegt wird.',
 	'LBL_STATUSES_CLOSED_CALCULATION' => 'Status der die Kalkulation als beendet definiert',
@@ -22,8 +22,8 @@ $languageStrings = [
 	'LBL_CREATE_POTENTIALS' => 'Anlegen von Verkaufschancen für Organisationen/Kontakte unterbinden, in denen kein User zugeordnet ist',
 	'SalesProcesses' => 'Vetriebsprozesse',
 	
-	'LBL_SQUOTEENQUIRIES' => 'Verkaufsanfrage',
-	'LBL_SREQUIREMENTSCARD' => 'Verkaufs Anforderungen',
+	'LBL_SQUOTEENQUIRIES' => 'Kaufanfrage',
+	'LBL_SREQUIREMENTSCARD' => 'Vertriebsanforderungen',
 	'LBL_SCALCULATIONS' => 'Kalkulationen',
 	'LBL_SQUOTES' => 'Angebote',
 	'LBL_SSALESORDER' => 'Bestellung',
