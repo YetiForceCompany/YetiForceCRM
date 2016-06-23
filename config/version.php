22405e1a1622ca5ea9a94f11cdd27053171c5887
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2016-06-23 15:55:01 +0200

    Added translation

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160623';
-$YetiForce_current_version = '3.1.378';
+$YetiForce_current_version = '3.1.379';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
