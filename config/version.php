82653d09c1e64247b2b95ef403c580efe5b0238a
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2016-06-30 16:29:35 +0200

    Improved popover

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160630';
-$YetiForce_current_version = '3.1.432';
+$YetiForce_current_version = '3.1.433';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
