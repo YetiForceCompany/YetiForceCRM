8dc82048530fc3e41cb377de3756c96ac3848816
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-07-05 08:11:26 +0200

    Fixed names of variables

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20160704';
-$YetiForce_current_version = '3.1.449';
+$patch_version = '20160705';
+$YetiForce_current_version = '3.1.450';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
