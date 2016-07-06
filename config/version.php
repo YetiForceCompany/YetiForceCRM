2ee6f7a8476db4f38a48bccd3dfe18596a831c3f
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-07-06 15:41:35 +0200

    Added plugin CalDAV Schedule - draft version

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160706';
-$YetiForce_current_version = '3.1.469';
+$YetiForce_current_version = '3.1.470';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
