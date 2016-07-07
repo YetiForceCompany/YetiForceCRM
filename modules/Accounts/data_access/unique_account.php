1f24498be65bb81aaf32669f7354cdfa2ae349d9
Adrian Koń <a.kon@yetiforce.com>
2016-07-07 14:03:05 +0200

    Securing sql queries Assets/Account

--- modules/Accounts/data_access/unique_account.php
+++ modules/Accounts/data_access/unique_account.php
@@ -69,7 +69,8 @@ Class DataAccess_unique_account
 
 		if ($hierarchyAll && $vatId) {
 			$hierarchyParams = array_merge($params, array_keys($hierarchyAll));
-			$hierarchyQuery = 'SELECT accountid,accountname FROM vtiger_account WHERE ' . $where . ' AND accountid IN (' . $db->generateQuestionMarks($hierarchyAll) . ')';
+			$hierarchyQuery = 'SELECT accountid,accountname FROM vtiger_account WHERE %s AND accountid IN (' . $db->generateQuestionMarks($hierarchyAll) . ')';
+			$hierarchyQuery = sprintf($hierarchyQuery, $where);
 			$result = $db->pquery($hierarchyQuery, $hierarchyParams);
 			if ($db->getRowCount($result)) {
 				$hierarchyCheck = true;
