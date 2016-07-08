84125771620b827cbf1a5b13c48c2d3054554274
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-07-08 15:44:20 +0200

    Fixed #3212

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160708';
-$YetiForce_current_version = '3.1.523';
+$YetiForce_current_version = '3.1.524';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
