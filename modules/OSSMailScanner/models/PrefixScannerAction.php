4941e8adab07cf9643b214aa0576b10d24cf8640
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-07-11 16:01:20 +0200

    Improved search prefix

--- modules/OSSMailScanner/models/PrefixScannerAction.php
+++ modules/OSSMailScanner/models/PrefixScannerAction.php
@@ -6,7 +6,7 @@
  * @license licenses/License.html
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
-class OSSMailScanner_PrefixScannerAction_Model extends OSSMailScanner_BaseScannerAction_Model
+class OSSMailScanner_PrefixScannerAction_Model
 {
 
 	public function process($mail, $moduleName, $tableName, $tableColumn)
@@ -28,7 +28,7 @@ class OSSMailScanner_PrefixScannerAction_Model extends OSSMailScanner_BaseScanne
 			return $returnIds;
 		}
 
-		$prefix = $this->findEmailPrefix($moduleName, $mail->get('subject'));
+		$prefix = includes\fields\Email::findCrmidByPrefix($mail->get('subject'), $moduleName);
 		if (!$prefix) {
 			return false;
 		}
