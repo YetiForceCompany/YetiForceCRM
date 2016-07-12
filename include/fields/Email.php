c39b0083e5cdf226a7a852d8e534327e9ad1b5ff
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-07-12 15:17:02 +0200

    Improved search mail address

--- include/fields/Email.php
+++ include/fields/Email.php
@@ -28,41 +28,51 @@ class Email
 		$rows = $params = $fields = [];
 		$query = '';
 		$countWhere = 0;
-		$result = $db->query('SELECT vtiger_field.columnname,vtiger_field.tablename,vtiger_field.fieldlabel,vtiger_field.tabid,vtiger_tab.name FROM vtiger_field INNER JOIN vtiger_tab ON vtiger_tab.tabid = vtiger_field.tabid WHERE vtiger_tab.presence = 0 AND vtiger_field.presence <> 1 AND uitype = 13');
+		$result = $db->query('SELECT vtiger_field.columnname,vtiger_field.tablename,vtiger_field.fieldlabel,vtiger_field.tabid,vtiger_tab.name FROM vtiger_field INNER JOIN vtiger_tab ON vtiger_tab.tabid = vtiger_field.tabid WHERE vtiger_tab.presence = 0 AND vtiger_field.presence <> 1 AND (uitype = 13 OR uitype = 104)');
 		while ($row = $db->getRow($result)) {
 			$fields[$row['name']][$row['tablename']][$row['columnname']] = $row;
 		}
 		foreach ($fields as $moduleName => &$moduleFields) {
-			if (($modules && !in_array($moduleName, $allowedModules)) || in_array($moduleName, $skipModules)) {
+			if (($allowedModules && !in_array($moduleName, $allowedModules)) || in_array($moduleName, $skipModules)) {
 				continue;
 			}
 			$instance = \CRMEntity::getInstance($moduleName);
+			$isEntityType = isset($instance->tab_name_index['vtiger_crmentity']);
 			$join = $where = [];
 			foreach ($moduleFields as $tablename => &$columns) {
 				$tableIndex = $instance->tab_name_index[$tablename];
-				$query .= sprintf(' UNION (SELECT %s AS id,label,setype FROM %s', $tableIndex, $tablename);
+				if ($isEntityType) {
+					$selest = ',label,setype';
+				} else {
+					$selest = ",'$moduleName' AS setype";
+				}
+				$query .= sprintf(' UNION (SELECT %s AS id %s FROM %s', $tableIndex, $selest, $tablename);
 				foreach ($columns as $columnName => &$row) {
 					$join[$tablename] = $row;
 					$where[] = $tablename . '.' . $columnName;
 					$countWhere++;
 				}
 			}
-			foreach ($join as $columnName => &$row) {
-				$query .= sprintf(' INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid=%s', $tablename . '.' . $tableIndex);
+			$whereQuery = ' WHERE';
+			if ($isEntityType) {
+				foreach ($join as $columnName => &$row) {
+					$query .= sprintf(' INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid=%s', $tablename . '.' . $tableIndex);
+				}
+				$whereQuery .= ' vtiger_crmentity.deleted = 0 AND';
 			}
-			$query .= ' WHERE vtiger_crmentity.deleted = 0 AND (';
+			$whereQuery .= ' (';
 			$or = '';
 			foreach ($where as $columnName) {
 				$or .= sprintf(' OR %s = ?', $columnName);
 			}
-			$query .= ltrim($or, ' OR ') . '))';
+			$query .= $whereQuery . ltrim($or, ' OR ') . '))';
 		}
 		for ($index = 0; $index < $countWhere; $index++) {
 			$params[] = $value;
 		}
 		$result = $db->pquery(ltrim($query, ' UNION '), $params);
 		while ($row = $db->getRow($result)) {
-			$rows[] = ['crmid' => $row['id'], 'modules' => $row['setype'], 'label' => $row['label']];
+			$rows[] = ['crmid' => $row['id'], 'modules' => $row['setype'], 'label' => isset($row['label']) ? $row['label'] : false];
 		}
 		return $rows;
 	}
