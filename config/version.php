ad4da555d5f4859c57c122c31dc355cf16863213
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-07-12 09:01:07 +0200

    Improved database queries

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160712';
-$YetiForce_current_version = '3.1.555';
+$YetiForce_current_version = '3.1.556';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
