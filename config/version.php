c3df36ed66804cb4e0eca063b821f9a28110fc1c
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-07-19 11:11:01 +0200

    Improved error logs

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160719';
-$YetiForce_current_version = '3.1.603';
+$YetiForce_current_version = '3.1.604';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
