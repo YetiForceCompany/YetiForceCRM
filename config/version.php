52933402349fad9d33459c6a6082283965bb6976
Tomasz Kur <t.kur@yetiforce.com>
2016-07-19 13:12:25 +0200

    Fixed #3245

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160719';
-$YetiForce_current_version = '3.1.612';
+$YetiForce_current_version = '3.1.613';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
