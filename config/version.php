81d24f7f04c9c174804b29eb85f0aca9f3b8df8b
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-07-19 11:10:30 +0200

    Improved e-mail features

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160719';
-$YetiForce_current_version = '3.1.602';
+$YetiForce_current_version = '3.1.603';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
