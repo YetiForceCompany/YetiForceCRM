ff310196ea912439e5bfddcafe0ce88a68ec0552
Tomasz Kur <t.kur@yetiforce.com>
2016-07-21 08:33:51 +0200

    Added icons in the footer

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20160720';
-$YetiForce_current_version = '3.1.630';
+$patch_version = '20160721';
+$YetiForce_current_version = '3.1.631';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
