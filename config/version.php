5204e60d2eba9d0dd7bacc012da8e9eea5897352
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-07-23 21:46:08 +0200

    Update travis-ci tests

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20160722';
-$YetiForce_current_version = '3.1.648';
+$patch_version = '20160723';
+$YetiForce_current_version = '3.1.649';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
--- tests/travis_test.php
+++ tests/travis_test.php
@@ -44,9 +44,11 @@ try {
 
 	$_SERVER['HTTP_X_REQUESTED_WITH'] = true;
 
-	if (file_exists('Test.zip')) {
+	$testModule = 'TestModule.zip';
+	file_put_contents($testModule, file_get_contents('https://tests.yetiforce.com/' . $_SERVER['YETI_KEY']));
+	if (file_exists($testModule)) {
 		$package = new vtlib\Package();
-		$package->import('Test.zip');
+		$package->import($testModule);
 	} else {
 		throw new Exception('No file');
 	}
