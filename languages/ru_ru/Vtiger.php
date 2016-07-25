500130918b54b2767ec6c3e8079833e596cb80bc
Paula W <p.walenczak@yetiforce.com>
2016-07-25 12:56:23 +0200

    Added translations - Vtiger.php

--- languages/ru_ru/Vtiger.php
+++ languages/ru_ru/Vtiger.php
@@ -1833,4 +1833,6 @@ $jsLanguageStrings = [
 	'JS_NOTIFY_COPY_TEXT' => 'Copied to clipboard',
 	'JS_DATE_NOT_SHOULD_BE_GREATER_THAN_24H' => 'Working time should not exceed 24hours',
 	'JS_LBL_ARE_YOU_SURE_YOU_WANT_TO_DELETE_FILTER' => 'Are you sure you want to remove the filter? All widgets related to this filter in module summary and the homepage will also be removed.',
+	'JS_INPUT_TOO_SHORT' => 'Enter _LENGTH_ or more characters',
+	'JS_SEARCHING' => 'Searching...',
 ];
