8661c6191a7e809420305076a1ff377b98521c54
Tomasz Kur <t.kur@yetiforce.com>
2016-07-25 10:45:11 +0200

    Removed jquery.sparkline

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160725';
-$YetiForce_current_version = '3.1.657';
+$YetiForce_current_version = '3.1.658';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
