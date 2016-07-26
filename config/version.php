f3d616d0c41ab7989d28825c26b7b13e3d75e95d
Tomasz Kur <t.kur@yetiforce.com>
2016-07-26 19:45:00 +0200

    Fixed #3294

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160726';
-$YetiForce_current_version = '3.1.678';
+$YetiForce_current_version = '3.1.679';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
