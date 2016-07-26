dc88c9757a9e439b9a5faeddd2ae31b4efce38b2
Tomasz Kur <t.kur@yetiforce.com>
2016-07-26 10:00:15 +0200

    Added style.min.css to default skin

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160726';
-$YetiForce_current_version = '3.1.670';
+$YetiForce_current_version = '3.1.671';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
