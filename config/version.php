a51d6394effb5c977c84f8ac21278029e4722958
Tomasz Kur <t.kur@yetiforce.com>
2016-07-27 08:24:30 +0200

    Improved appearance of loader

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160726';
-$YetiForce_current_version = '3.1.686';
+$YetiForce_current_version = '3.1.687';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
