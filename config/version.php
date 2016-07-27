ba1794ee58dc2b89fb5e8521e45b8076aa657152
Tomasz Kur <t.kur@yetiforce.com>
2016-07-27 09:25:33 +0200

    Added license

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160727';
-$YetiForce_current_version = '3.1.688';
+$YetiForce_current_version = '3.1.689';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
