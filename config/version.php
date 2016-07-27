4f8dc704d7c51c9761702b1d57c2bd8f073e7b56
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-07-27 11:18:11 +0200

    Fixed mail scanner

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160727';
-$YetiForce_current_version = '3.1.694';
+$YetiForce_current_version = '3.1.695';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
