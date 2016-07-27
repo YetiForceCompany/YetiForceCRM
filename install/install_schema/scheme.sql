03abd09b058b550f0cbc3c196167b8a93a68ed07
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-07-27 11:16:06 +0200

    Added missing index

--- install/install_schema/scheme.sql
+++ install/install_schema/scheme.sql
@@ -3793,7 +3793,8 @@ CREATE TABLE `vtiger_crmentity` (
   KEY `crm_ownerid_del_setype_idx` (`smownerid`,`deleted`,`setype`),
   KEY `crmid` (`crmid`,`deleted`),
   KEY `crmid_2` (`crmid`,`setype`),
-  KEY `setype` (`setype`,`deleted`)
+  KEY `setype` (`setype`,`deleted`),
+  KEY `setype_2` (`setype`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Table structure for table `vtiger_crmentity_seq` */
