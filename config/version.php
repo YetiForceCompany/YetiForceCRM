1ffabee768c2b7966f4bafb5dd12ead2437b90f1
Adrian Koń <a.kon@yetiforce.com>
2016-07-28 10:49:50 +0200

    Improved skin blue

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160728';
-$YetiForce_current_version = '3.1.707';
+$YetiForce_current_version = '3.1.708';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
