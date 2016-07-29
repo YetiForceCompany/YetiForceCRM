6e980e64ac436a71f32a963f855c5ec01ba3bc61
Tomasz Kur <t.kur@yetiforce.com>
2016-07-29 14:30:12 +0200

    Improved blue skin

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160729';
-$YetiForce_current_version = '3.1.749';
+$YetiForce_current_version = '3.1.750';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
