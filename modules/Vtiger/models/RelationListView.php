4844e1bf5c923c1d20227aeb60af716d6efd04da
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-07-29 13:11:28 +0200

    Fixed #3201

--- modules/Vtiger/models/RelationListView.php
+++ modules/Vtiger/models/RelationListView.php
@@ -539,7 +539,7 @@ class Vtiger_RelationListView_Model extends Vtiger_Base_Model
 		if ($position) {
 			$split = preg_split('/FROM/i', $relationQuery, 2);
 			$splitCount = count($split);
-			$relationQuery = 'SELECT COUNT(1) AS count';
+			$relationQuery = 'SELECT COUNT(DISTINCT vtiger_crmentity.crmid) AS count';
 			for ($i = 1; $i < $splitCount; $i++) {
 				$relationQuery = $relationQuery . ' FROM ' . $split[$i];
 			}
