6a47a4f763b602753cd27459ff718780a3ff3447
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-08-03 09:49:07 +0200

    Fixed creating links in workflows

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160802';
-$YetiForce_current_version = '3.1.790';
+$YetiForce_current_version = '3.1.791';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
