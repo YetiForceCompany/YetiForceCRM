3abde55c6d7843dfd05fb1b6fe1b78f904cc9f05
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-08-03 13:30:49 +0200

    Fixed counting the number of records

--- modules/Vtiger/models/RelationListView.php
+++ modules/Vtiger/models/RelationListView.php
@@ -553,6 +553,23 @@ class Vtiger_RelationListView_Model extends Vtiger_Base_Model
 	}
 
 	/**
+	 * Function to get Total number of record in this relation
+	 * @return <Integer>
+	 */
+	public function getRelatedTreeEntriesCount()
+	{
+		$db = PearDatabase::getInstance();
+		$recordId = $this->getParentRecordModel()->getId();
+		$relModuleId = $this->getRelatedModuleModel()->getId();
+		$treeViewModel = $this->getTreeViewModel();
+		$template = $treeViewModel->getTemplate();
+		$result = $db->pquery('SELECT count(1) FROM vtiger_trees_templates_data tr '
+			. 'INNER JOIN u_yf_crmentity_rel_tree rel ON rel.tree = tr.tree '
+			. 'WHERE tr.templateid = ? AND rel.crmid = ? AND rel.relmodule = ?', [$template, $recordId, $relModuleId]);
+		return $db->getSingleValue($result);
+	}
+
+	/**
 	 * Function to update relation query
 	 * @param <String> $relationQuery
 	 * @return <String> $updatedQuery
