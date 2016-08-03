a557dd1a039731a8a16ef3feea28b441752c3f83
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-08-03 14:15:21 +0200

    Added Cron action that marks notifications that exceeded the limit as read

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160803';
-$YetiForce_current_version = '3.1.795';
+$YetiForce_current_version = '3.1.796';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
--- modules/Home/cron/Notifications.php
+++ modules/Home/cron/Notifications.php
@@ -7,55 +7,80 @@
  */
 require_once 'include/main/WebUI.php';
 $db = PearDatabase::getInstance();
+$notifications = new Cron_Notification();
 $result = $db->query('SELECT * FROM u_yf_watchdog_schedule');
 while ($row = $db->getRow($result)) {
-	executeScheduled($row);
+	$notifications->executeScheduled($row);
 }
+$notifications->markAsRead();
 
-function executeScheduled($row)
+class Cron_Notification
 {
-	$db = PearDatabase::getInstance();
-	$currentTime = time();
-	$timestampEndDate = empty($row['last_execution']) ? $currentTime : strtotime($row['last_execution'] . ' +' . $row['frequency'] . 'min');
-	if ($currentTime >= $timestampEndDate) {
-		$endDate = getEndDate($currentTime, $timestampEndDate, $row['frequency']);
-		if (existNotifications($row['userid'], $row['last_execution'], $endDate) && Users_Privileges_Model::isPermittedByUserId($row['userid'], 'Dashboard', 'ReceivingMailNotifications')) {
-			$data = [
-				'sysname' => 'SendNotificationsViaMail',
-				'to_email' => getUserEmail($row['userid']),
-				'module' => 'System',
-				'startDate' => $row['last_execution'],
-				'endDate' => $endDate,
-				'userId' => $row['userid']
-			];
-			$recordModel = Vtiger_Record_Model::getCleanInstance('OSSMailTemplates');
-			$recordModel->sendMailFromTemplate($data);
+
+	function __construct()
+	{
+		
+	}
+
+	function executeScheduled($row)
+	{
+		$db = PearDatabase::getInstance();
+		$currentTime = time();
+		$timestampEndDate = empty($row['last_execution']) ? $currentTime : strtotime($row['last_execution'] . ' +' . $row['frequency'] . 'min');
+		if ($currentTime >= $timestampEndDate) {
+			$endDate = $this->getEndDate($currentTime, $timestampEndDate, $row['frequency']);
+			if ($this->existNotifications($row['userid'], $row['last_execution'], $endDate) && Users_Privileges_Model::isPermittedByUserId($row['userid'], 'Dashboard', 'ReceivingMailNotifications')) {
+				$data = [
+					'sysname' => 'SendNotificationsViaMail',
+					'to_email' => getUserEmail($row['userid']),
+					'module' => 'System',
+					'startDate' => $row['last_execution'],
+					'endDate' => $endDate,
+					'userId' => $row['userid']
+				];
+				$recordModel = Vtiger_Record_Model::getCleanInstance('OSSMailTemplates');
+				$recordModel->sendMailFromTemplate($data);
+			}
+			$db->update('u_yf_watchdog_schedule', ['last_execution' => $endDate], 'userid = ?', [$row['userid']]);
 		}
-		$db->update('u_yf_watchdog_schedule', ['last_execution' => $endDate], 'userid = ?', [$row['userid']]);
 	}
-}
 
-function existNotifications($userId, $startDate, $endDate)
-{
-	$db = PearDatabase::getInstance();
-	$query = 'SELECT 1 FROM l_yf_notification WHERE `userid` = ?';
-	$params = [$userId];
-	if (empty($startDate)) {
-		$query .= ' AND `time` <= ?';
-		$params[] = $endDate;
-	} else {
-		$query .= ' AND `time` BETWEEN ? AND ?';
-		array_push($params, $startDate, $endDate);
+	function existNotifications($userId, $startDate, $endDate)
+	{
+		$db = PearDatabase::getInstance();
+		$query = 'SELECT 1 FROM l_yf_notification WHERE `userid` = ?';
+		$params = [$userId];
+		if (empty($startDate)) {
+			$query .= ' AND `time` <= ?';
+			$params[] = $endDate;
+		} else {
+			$query .= ' AND `time` BETWEEN ? AND ?';
+			array_push($params, $startDate, $endDate);
+		}
+		$query .= ' LIMIT 1';
+		$result = $db->pquery($query, $params);
+		return (bool) $result->rowCount();
 	}
-	$query .= ' LIMIT 1';
-	$result = $db->pquery($query, $params);
-	return (bool) $result->rowCount();
-}
 
-function getEndDate($currentTime, $timestampEndDate, $frequency)
-{
-	while ($timestampEndDate <= $currentTime && ($nextEndDateTime = $timestampEndDate + ($frequency * 60)) <= $currentTime) {
-		$timestampEndDate = $nextEndDateTime;
+	function getEndDate($currentTime, $timestampEndDate, $frequency)
+	{
+		while ($timestampEndDate <= $currentTime && ($nextEndDateTime = $timestampEndDate + ($frequency * 60)) <= $currentTime) {
+			$timestampEndDate = $nextEndDateTime;
+		}
+		return date('Y-m-d H:i:s', $timestampEndDate);
 	}
-	return date('Y-m-d H:i:s', $timestampEndDate);
+
+	function markAsRead()
+	{
+		$db = PearDatabase::getInstance();
+		$result = $db->query('SELECT userid, id FROM l_yf_notification ORDER BY userid, `time` DESC');
+		$notifications = $db->getRowsByGroup($result);
+		foreach ($notifications as $userId => $noticesByUser) {
+			$noticesByUser = array_slice($noticesByUser, AppConfig::module('Home', 'MAX_NUMBER_NOTIFICATIONS'));
+			foreach ($noticesByUser as $noticeId) {
+				$notice = Home_NoticeEntries_Model::getInstanceById($noticeId);
+				$notice->setMarked();
+			}
+		}
+	}
 }
