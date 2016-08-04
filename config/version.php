923bb66e66de7fd9d35ceaee27e1498ba07f504a
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-08-04 21:05:56 +0200

    Fixed reminders

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160804';
-$YetiForce_current_version = '3.1.823';
+$YetiForce_current_version = '3.1.824';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
