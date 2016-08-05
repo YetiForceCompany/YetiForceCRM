594e9cdb234f758cd593ac0979f772b216817628
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-08-05 09:30:41 +0200

    Added translations

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160805';
-$YetiForce_current_version = '3.1.833';
+$YetiForce_current_version = '3.1.834';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
