d696d785f64a5f6951179ef7804d580d836c392b
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-08-09 12:42:04 +0200

    Fixed display of pagination

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160809';
-$YetiForce_current_version = '3.1.856';
+$YetiForce_current_version = '3.1.857';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
