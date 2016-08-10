2c370948ce304effb0df33e2ff97c1798c4c0c00
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-08-10 15:43:42 +0200

    Search rebuilt in columns references

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160810';
-$YetiForce_current_version = '3.1.887';
+$YetiForce_current_version = '3.1.888';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
