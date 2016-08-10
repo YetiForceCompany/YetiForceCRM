1727a1e4f9ca3d06da8727c0e7395a5b3f66b3ee
Tomasz Kur <t.kur@yetiforce.com>
2016-08-10 16:05:04 +0200

    Fixed pagination after changing the filter

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160810';
-$YetiForce_current_version = '3.1.891';
+$YetiForce_current_version = '3.1.892';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
