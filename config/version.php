3f70a9dee55763a0a8f409612b2e1c792bb37aaf
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-08-12 13:45:40 +0200

    Optimized permission generate

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160812';
-$YetiForce_current_version = '3.1.927';
+$YetiForce_current_version = '3.1.928';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
