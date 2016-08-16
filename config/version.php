ae286fbf9cb8eb9f1602317f12acaf9da9d87f46
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-08-16 15:42:11 +0200

    Added minor improvements.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160816';
-$YetiForce_current_version = '3.1.949';
+$YetiForce_current_version = '3.1.950';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
