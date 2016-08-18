76ac7020163a5a6fe3926cd51031c3f8d6d7e245
Tomasz Kur <t.kur@yetiforce.com>
2016-08-18 15:54:27 +0200

    Improved appearance of PDF module

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160818';
-$YetiForce_current_version = '3.1.976';
+$YetiForce_current_version = '3.1.977';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
