d4f72fd5f1671851f12cdedb40808d658cffe48b
Алексей <waw555@yandex.ru>
2016-08-19 15:27:21 +0500

    Improve Translation

--- languages/ru_ru/IGDN.php
+++ languages/ru_ru/IGDN.php
@@ -1,24 +1,24 @@
 <?php
 /* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 $languageStrings = [
-	'IGDN' => 'Товарные накладные',
-	'SINGLE_IGDN' => 'Товарая накладная',
+	'IGDN' => 'Экспорт товара',
+	'SINGLE_IGDN' => 'Экспорт товара',
 	'LBL_DESCRIPTION_BLOCK' => 'Описание',
-	'LBL_ATTENTION_BLOCK' => 'Коментарии',
+	'LBL_ATTENTION_BLOCK' => 'Комментарии',
 	// Fields
-	'FL_ACCEPTANCE_DATE' => 'Дата приемки',
+	'FL_ACCEPTANCE_DATE' => 'Планируемая дата',
 	'FL_ACCOUNT' => 'Контрагент',
 	'FL_NUMBER' => 'Номер',
-	'FL_SUBJECT' => 'Предмет',
+	'FL_SUBJECT' => 'Наименование',
 	'FL_STORAGE' => 'Склад',
 	'FL_STATUS' => 'Статус',
 	'FL_SSIGNLEORDERS' => 'Заказ',
 	// Picklist
 	'PLL_DRAFT' => 'Черновик',
-	'PLL_IN_REALIZATION' => 'На реализации',
-	'PLL_OBJECTIONS' => 'Возражеия',
-	'PLL_FOR_APPROVAL' => 'Для подтверждения',
+	'PLL_IN_REALIZATION' => 'Обработка',
+	'PLL_OBJECTIONS' => 'Не согласовано',
+	'PLL_FOR_APPROVAL' => 'На согласовании',
 	'PLL_CANCELLED' => 'Отменено',
-	'PLL_ACCEPTED' => 'Подтверждено',
+	'PLL_ACCEPTED' => 'Согласовано',
 	'LBL_CHANGE_STATUS' => 'Изменить статус',
 ];
--- languages/ru_ru/IGDNC.php
+++ languages/ru_ru/IGDNC.php
@@ -1,24 +1,24 @@
 <?php
 /* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 $languageStrings = [
-	'IGDNC' => 'Корректировки товарных накладных',
-	'SINGLE_IGDNC' => 'Корректировка товарной накладной',
+	'IGDNC' => 'Корректировка экспорта товара',
+	'SINGLE_IGDNC' => 'Корректировка экспорта товара',
 	'LBL_DESCRIPTION_BLOCK' => 'Описание',
-	'LBL_ATTENTION_BLOCK' => 'Коментарии',
+	'LBL_ATTENTION_BLOCK' => 'Комментарии',
 	// Fields
-	'FL_ACCEPTANCE_DATE' => 'Дата приемки',
+	'FL_ACCEPTANCE_DATE' => 'Планируемая дата',
 	'FL_ACCOUNT' => 'Контрагент',
 	'FL_NUMBER' => 'Номер',
-	'FL_SUBJECT' => 'Предмет',
+	'FL_SUBJECT' => 'Наименование',
 	'FL_STORAGE' => 'Склад',
 	'FL_STATUS' => 'Статус',
-	'FL_IGDN' => 'Товарая накладная',
+	'FL_IGDN' => 'Связан с',
 	// Picklist
 	'PLL_DRAFT' => 'Черновик',
-	'PLL_IN_REALIZATION' => 'На реализации',
-	'PLL_OBJECTIONS' => 'Возражеия',
-	'PLL_FOR_APPROVAL' => 'Для подтверждения',
+	'PLL_IN_REALIZATION' => 'Обработка',
+	'PLL_OBJECTIONS' => 'Не согласовано',
+	'PLL_FOR_APPROVAL' => 'На согласовании',
 	'PLL_CANCELLED' => 'Отменено',
-	'PLL_ACCEPTED' => 'Подтверждено',
+	'PLL_ACCEPTED' => 'Согласовано',
 	'LBL_CHANGE_STATUS' => 'Изменить статус',
 ];
