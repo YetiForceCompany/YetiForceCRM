7ac974ffd3cdb6af188e7a24fb42e040a676a9ea
Adrian Koń <a.kon@yetiforce.com>
2016-08-23 11:24:38 +0200

    Fixed sql query

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160823';
-$YetiForce_current_version = '3.1.1013';
+$YetiForce_current_version = '3.1.1014';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
