80221a7be13ed70cfdf6657e834c9ffcfc208032
Tomasz Kur <t.kur@yetiforce.com>
2016-08-24 12:03:05 +0200

    Added searching users by ajax in transfer oners and trigger workflow

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160824';
-$YetiForce_current_version = '3.1.1033';
+$YetiForce_current_version = '3.1.1034';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
