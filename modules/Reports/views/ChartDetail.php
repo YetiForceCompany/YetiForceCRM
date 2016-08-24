010b268e6fda2e61c2ac596e839391c667df1cfc
Adrian Koń <a.kon@yetiforce.com>
2016-08-24 09:04:17 +0200

    Added license

--- modules/Reports/views/ChartDetail.php
+++ modules/Reports/views/ChartDetail.php
@@ -6,6 +6,7 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
+ * Contributor(s): YetiForce.com
  *************************************************************************************/
 
 class Reports_ChartDetail_View extends Vtiger_Index_View {
