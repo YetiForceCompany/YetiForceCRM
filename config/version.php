292aab4645e83dbbaa449955c92c22c2b9482ba1
Adrian Koń <a.kon@yetiforce.com>
2016-08-25 10:19:12 +0200

    Added icons from font awesome while selecting menu icon

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160825';
-$YetiForce_current_version = '3.1.1048';
+$YetiForce_current_version = '3.1.1049';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
