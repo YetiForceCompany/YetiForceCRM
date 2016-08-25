e297f45cc817971cfa635e528baf224bd946f92e
Tomasz Kur <t.kur@yetiforce.com>
2016-08-25 09:15:01 +0200

    Improved links to listview from widgets

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160825';
-$YetiForce_current_version = '3.1.1046';
+$YetiForce_current_version = '3.1.1047';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
