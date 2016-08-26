b3607e45d9215dcc1b68a6e4275733f1a32d8723
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-08-26 11:02:33 +0200

    Added option to block field edition for select2 type elements.

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160826';
-$YetiForce_current_version = '3.1.1069';
+$YetiForce_current_version = '3.1.1070';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
