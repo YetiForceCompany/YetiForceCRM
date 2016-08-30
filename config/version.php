6edc8bc66972a7950eecd7660cdc75d2a00d6acc
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-08-30 23:42:46 +0200

    #3446 Improvement requirements

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160830';
-$YetiForce_current_version = '3.2.9';
+$YetiForce_current_version = '3.2.10';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
