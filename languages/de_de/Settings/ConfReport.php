1d6574701b9ee2796691fa9f59478406380180da
Paula W <p.walenczak@yetiforce.com>
2016-08-31 13:16:33 +0200

    Added translations - ConfReport.php

--- languages/de_de/Settings/ConfReport.php
+++ languages/de_de/Settings/ConfReport.php
@@ -43,6 +43,7 @@ $languageStrings = [
 	'LBL_JSON_LIBRARY' => 'JSON',
 	'LBL_SESSION_LIBRARY' => 'SESSION',
 	'LBL_DOM_LIBRARY' => 'DOM',
+	'LBL_MBSTRING_LIBRARY' => 'php_mbstring (required for mPDF)',
 	'LBL_ZIP_ARCHIVE' => 'ZIP',
 	'LBL_SPACE' => 'Kapazität',
 	'LBL_SPACE_TOTAL' => 'Total',
