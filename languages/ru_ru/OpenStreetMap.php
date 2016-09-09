496189198263ed1b309ba2bf1e30a4673236158b
Paula W <p.walenczak@yetiforce.com>
2016-09-09 13:11:52 +0200

    Added translations - OpenStreetMap.php

--- languages/ru_ru/OpenStreetMap.php
+++ languages/ru_ru/OpenStreetMap.php
@@ -2,4 +2,11 @@
 /* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 $languageStrings = [
 	'LBL_GROUP_BY' => 'Group',
+	'LBL_GROUP_NEIGHBOURS' => 'Group neighboring elements',
+	'LBL_NOT_FOUND_PLACE' => 'Place not found',
+	'LBL_SEARCH_VALUE_DESCRIPTION' => 'City, street, building number ...',
+	'LBL_IN_RADIUS' => 'Search within radius [km]',
+	'LBL_CALCULATE_TRACK' => 'Set route',
+	'LBL_START' => 'Start',
+	'LBL_END' => 'End',
 ];
