654cc5ddcddfc60553562a039d95847208ea2394
Adrian Koń <a.kon@yetiforce.com>
2016-09-12 10:46:15 +0200

    Fixed conditions

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160912';
-$YetiForce_current_version = '3.2.152';
+$YetiForce_current_version = '3.2.153';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
