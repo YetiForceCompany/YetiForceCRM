a2c6cbb24c826bb94f7a2d7a5a52dac1b261c001
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-09-20 14:59:43 +0200

    Added minor fixes

--- libraries/antlr/util.php
+++ libraries/antlr/util.php
@@ -1,13 +1,15 @@
 <?php
-	public function strToIntArray($string){
-		$arr = array();
-		for($i=0, $n=strlen($string);$i<$n;$i++){
-			$arr[]= ord(substr($string, $i, 1));
-		}
-		return $arr;
+
+function strToIntArray($string)
+{
+	$arr = array();
+	for ($i = 0, $n = strlen($string); $i < $n; $i++) {
+		$arr[] = ord(substr($string, $i, 1));
 	}
-	
-	public function charAt($str, $i){
-		return ord(substr($str, $i, 1));
-	}
-?>
+	return $arr;
+}
+
+function charAt($str, $i)
+{
+	return ord(substr($str, $i, 1));
+}
