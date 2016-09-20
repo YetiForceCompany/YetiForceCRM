55ca5cd85357709c750ad71e71f5348c429c52f1
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-09-20 11:59:08 +0200

    Removed eval

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160919';
-$YetiForce_current_version = '3.2.271';
+$YetiForce_current_version = '3.2.272';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
--- libraries/htmlpurifier/library/HTMLPurifier/VarParser/Native.php
+++ libraries/htmlpurifier/library/HTMLPurifier/VarParser/Native.php
@@ -8,31 +8,34 @@
 class HTMLPurifier_VarParser_Native extends HTMLPurifier_VarParser
 {
 
-    /**
-     * @param mixed $var
-     * @param int $type
-     * @param bool $allow_null
-     * @return null|string
-     */
-    protected function parseImplementation($var, $type, $allow_null)
-    {
-        return $this->evalExpression($var);
-    }
+	/**
+	 * @param mixed $var
+	 * @param int $type
+	 * @param bool $allow_null
+	 * @return null|string
+	 */
+	protected function parseImplementation($var, $type, $allow_null)
+	{
+		return $this->evalExpression($var);
+	}
 
-    /**
-     * @param string $expr
-     * @return mixed
-     * @throws HTMLPurifier_VarParserException
-     */
-    protected function evalExpression($expr)
-    {
-        $var = null;
-        $result = eval("\$var = $expr;");
-        if ($result === false) {
-            throw new HTMLPurifier_VarParserException("Fatal error in evaluated code");
-        }
-        return $var;
-    }
+	/**
+	 * @param string $expr
+	 * @return mixed
+	 * @throws HTMLPurifier_VarParserException
+	 */
+	protected function evalExpression($expr)
+	{
+		$var = null;
+		// <--------   YetiForce Sp. z o.o.   -------->
+		throw new BadMethodCallException('Eval: evalExpression');
+		//$result = eval("\$var = $expr;");
+		// <--------   YetiForce Sp. z o.o.   -------->
+		if ($result === false) {
+			throw new HTMLPurifier_VarParserException("Fatal error in evaluated code");
+		}
+		return $var;
+	}
 }
 
 // vim: et sw=4 sts=4
