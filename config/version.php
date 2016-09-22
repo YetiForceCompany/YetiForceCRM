d49c597888d142bfd6fbb5d6c594c6b72cf35c9c
Tomasz Kur <t.kur@yetiforce.com>
2016-09-22 10:46:23 +0200

    Improved appearance of action bar in emails

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160922';
-$YetiForce_current_version = '3.2.308';
+$YetiForce_current_version = '3.2.309';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
