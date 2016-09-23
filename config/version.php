3f56e2def1849c332b1d123262622c8a6707c5f7
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-09-23 14:55:28 +0200

    YetiForce CRM ver. 3.3.0

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160923';
-$YetiForce_current_version = '3.2.319';
+$YetiForce_current_version = '3.3.0';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
