6082fcc0dbe46399c1582f32dcb09ae559dbdc66
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-09-26 14:24:54 +0200

    Credits update

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160926';
-$YetiForce_current_version = '3.3.6';
+$YetiForce_current_version = '3.3.7';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
