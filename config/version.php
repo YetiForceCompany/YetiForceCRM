4b3b192db6e10ed1d049f3aac6d403407a915ff9
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-09-27 12:54:30 +0200

    Fixed travis tests

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160927';
-$YetiForce_current_version = '3.3.20';
+$YetiForce_current_version = '3.3.21';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
