bf5fdb164d62996a99fa5834a5a54ac8b8663f47
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-09-29 09:13:21 +0200

    Fixed display of links in the widget

--- config/version.php
+++ config/version.php
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
-$patch_version = '20160928';
-$YetiForce_current_version = '3.3.42';
+$patch_version = '20160929';
+$YetiForce_current_version = '3.3.43';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
