50a8f19855d137406497fde7e2dfecf6824ef0d6
Tomasz Kur <t.kur@yetiforce.com>
2016-09-29 10:19:42 +0200

    Removed buttons (Reply, Reply All) from widget history relation

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160929';
-$YetiForce_current_version = '3.3.46';
+$YetiForce_current_version = '3.3.47';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
