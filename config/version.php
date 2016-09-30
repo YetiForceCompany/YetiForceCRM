4d66fae200a349c1cff020bd58c982339c9e2cd1
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-09-30 13:19:39 +0200

    Added optimization in cron

--- config/version.php
+++ config/version.php
@@ -9,5 +9,5 @@
  * Contributor(s): YetiForce.com
  * ****************************************************************************** */
 $patch_version = '20160930';
-$YetiForce_current_version = '3.3.61';
+$YetiForce_current_version = '3.3.62';
 $_SESSION['yetiforce_version'] = $YetiForce_current_version;
--- cron/PrivilegesUpdater.php
+++ cron/PrivilegesUpdater.php
@@ -8,12 +8,11 @@
 $adb = PearDatabase::getInstance();
 $limit = AppConfig::performance('CRON_MAX_NUMERS_RECORD_PRIVILEGES_UPDATER');
 
-$i = 0;
 $result = $adb->query(sprintf('SELECT * FROM u_yf_crmentity_search_label WHERE `userid` = \'\' LIMIT %s', $limit));
 while ($row = $adb->getRow($result)) {
-	\App\PrivilegeUpdater::updateGlobalSearch($row['crmid'], $row['setype']);
-	$i++;
-	if ($i == $limit) {
+	\App\PrivilegeUpdater::updateSearch($row['crmid'], $row['setype']);
+	$limit--;
+	if (0 === $limit) {
 		return;
 	}
 }
@@ -21,19 +20,19 @@ $resultUpdater = $adb->query(sprintf('SELECT * FROM s_yf_privileges_updater ORDE
 while ($row = $adb->getRow($resultUpdater)) {
 	$crmid = $row['crmid'];
 	if ($row['type'] == 0) {
-		\App\PrivilegeUpdater::updateGlobalSearch($crmid, $row['module']);
-		$i++;
-		if ($i == $limit) {
+		\App\PrivilegeUpdater::updatePrivilege($crmid, $row['module']);
+		$limit--;
+		if (0 === $limit) {
 			return;
 		}
 	} else {
 		$resultCrm = $adb->pquery(sprintf('SELECT crmid FROM vtiger_crmentity WHERE setype =? && crmid > ? LIMIT %s', $limit), [$row['module'], $crmid]);
 		while ($rowCrm = $adb->getRow($resultCrm)) {
-			\App\PrivilegeUpdater::updateGlobalSearch($rowCrm['crmid'], $row['module']);
+			\App\PrivilegeUpdater::updatePrivilege($rowCrm['crmid'], $row['module']);
 			$affected = $adb->update('s_yf_privileges_updater', ['crmid' => $rowCrm['crmid']], 'module =? && type =? && crmid =?', [$row['module'], 1, $crmid]);
 			$crmid = $rowCrm['crmid'];
-			$i++;
-			if ($i == $limit || $affected == 0) {
+			$limit--;
+			if (0 === $limit || $affected == 0) {
 				return;
 			}
 		}
