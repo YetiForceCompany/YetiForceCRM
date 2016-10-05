faf5736788847fe56e90a8652eeb93dc1d42fa25
Paula W <p.walenczak@yetiforce.com>
2016-10-05 09:47:32 +0200

    Created AdvancedPermission.php

---
+++ languages/ru_ru/Settings/AdvancedPermission.php
@@ -0,0 +1,22 @@
+<?php
+/* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
+$languageStrings = [
+	'LBL_ADVANCED_PERMISSION' => 'Advanced permissions',
+	'AdvancedPermission' => 'Advanced permissions',
+	'LBL_ADVANCED_PERMISSION_DESCRIPTION' => 'Custom permission configuration, grant or withdraw permissions to records.',
+	// Fields
+	'LBL_NAME' => 'Rule name',
+	'LBL_MODULE' => 'Module',
+	'LBL_ACTION' => 'Action',
+	'LBL_STATUS' => 'Status',
+	'LBL_ROLE' => 'Role',
+	// Picklist
+	'FL_ACTIVE' => 'Active',
+	'FL_INACTIVE' => 'Inactive',
+	'FL_UNLOCK_RECORD' => 'Grant access',
+	'FL_LOCK_RECORD' => 'Withdraw access',
+	// Others
+	'LBL_SAVE_AND_CONDITIONS' => 'Next',
+];
+$jsLanguageStrings = [
+];
