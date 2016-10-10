1094407f0b01aa0b73d80f13caf2a50beff2ee49
Tomasz Kur <t.kur@yetiforce.com>
2016-10-10 09:17:46 +0200

    Fixed #3605

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '3.3.150',
+	'appVersion' => '3.3.151',
 	'patchVersion' => '2016.10.10',
 	'lib_mPDF' => '0.0.0',
-	'lib_roundcube' => '0.0.1',
+	'lib_roundcube' => '0.0.2',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.0',
 ];
