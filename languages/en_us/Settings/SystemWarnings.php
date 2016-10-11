a2d2ba1fc75c9f49ea7627093f22330d8462dec0
Paula W <p.walenczak@yetiforce.com>
2016-10-11 13:41:12 +0200

    Created SystemWarnings.php

---
+++ languages/en_us/Settings/SystemWarnings.php
@@ -0,0 +1,17 @@
+<?php
+/* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
+$languageStrings = [
+	// Folders
+	'SystemRequirements' => 'System requirements',
+	// Actions
+	'LBL_LIBRARY_PDF' => 'mPDF library',
+	'LBL_LIBRARY_ROUNDCUBE' => 'Roundcube library',
+	'LBL_LIBRARY_PHPEXCEL' => 'PHPExcel library',
+	 'LBL_LIBRARY_CHAT' => 'AJAXChat library',
+ 	'LBL_CONFIG_REPORT' => 'Server configuration',
+	// Others
+	'BTN_SET_IGNORE' => 'Add to ignored',
+	'BTN_REMOVE_IGNORE' => 'Remove from ignored',
+	'LBL_MISSING_LIBRARY' => 'Detected missing library. Some features were disabled. In order to enable them please download the library. If the CRM has no Internet connection you can download the files and copy them to %s folder.',
+	'BTN_DOWNLOAD_LIBRARY' => 'Download library',
+];
