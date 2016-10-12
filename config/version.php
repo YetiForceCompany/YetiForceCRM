7f1f411ed335103ca52548081d9dd96fab527c42
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-10-13 01:36:31 +0200

    Changed library version

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '3.4.51',
+	'appVersion' => '3.4.52',
 	'patchVersion' => '2016.10.13',
 	'lib_mPDF' => '0.0.0',
-	'lib_roundcube' => '0.0.3',
+	'lib_roundcube' => '0.0.4',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.0',
 ];
