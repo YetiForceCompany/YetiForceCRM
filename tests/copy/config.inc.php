c9b1351da70d4943a6b13a817c53199e1ec3864a
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-10-14 18:31:05 +0200

    Fixed travis tests

--- tests/copy/config.inc.php
+++ tests/copy/config.inc.php
@@ -41,7 +41,7 @@ $dbconfig['db_port'] = '3306';
 $dbconfig['db_username'] = 'root';
 $dbconfig['db_password'] = '';
 $dbconfig['db_name'] = 'yetiforce';
-$dbconfig['db_type'] = 'mysqli';
+$dbconfig['db_type'] = 'mysql';
 $dbconfig['db_status'] = 'true';
 
 $dbconfig['db_hostname'] = $dbconfig['db_server'] . ':' . $dbconfig['db_port'];
