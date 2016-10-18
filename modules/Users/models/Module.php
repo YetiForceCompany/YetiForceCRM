92f8d5db08085b0a0c1110095336b986f55c97a1
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-10-18 16:06:56 +0200

    Rebuilt query - Login page

--- modules/Users/models/Module.php
+++ modules/Users/models/Module.php
@@ -153,19 +153,20 @@ class Users_Module_Model extends Vtiger_Module_Model
 
 	/**
 	 * Function to store the login history
-	 * @param type $username
+	 * @param type $userName
 	 */
-	public function saveLoginHistory($username, $status = 'Signed in', $browser = '')
+	public function saveLoginHistory($userName, $status = 'Signed in')
 	{
-		$adb = PearDatabase::getInstance();
-
 		$userIPAddress = \App\RequestUtil::getRemoteIP();
-		$userIPAddress = empty($userIPAddress) ? '-' : $userIPAddress;
-		$loginTime = date('Y-m-d H:i:s');
-		$browser = empty($browser) ? $browser : '-';
-		$query = "INSERT INTO vtiger_loginhistory (user_name, user_ip, logout_time, login_time, status, browser) VALUES (?,?,?,?,?,?)";
-		$params = array($username, $userIPAddress, '0000-00-00 00:00:00', $loginTime, $status, $browser);
-		$adb->pquery($query, $params);
+		\App\DB::getInstance()->createCommand()
+			->insert('vtiger_loginhistory', [
+				'user_name' => $userName,
+				'user_ip' => empty($userIPAddress) ? '-' : $userIPAddress,
+				'login_time' => date('Y-m-d H:i:s'),
+				'logout_time' => '0000-00-00 00:00:00',
+				'status' => $status,
+				'browser' => \App\RequestUtil::getBrowserInfo()->name
+			])->execute();
 	}
 
 	/**
