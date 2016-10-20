0ac7440d69181893a0af3fd5bcf1851a94f02ff4
Fernando Giacomino <info@electricidadder.com.ar>
2016-10-20 14:57:19 -0300

    Update GlobalPermission.php

--- languages/es_es/Settings/GlobalPermission.php
+++ languages/es_es/Settings/GlobalPermission.php
@@ -9,14 +9,14 @@
  * All Rights Reserved.
  * *********************************************************************************************************************************** */
 $languageStrings = [
-	'Access to record' => 'Access to records',
-	'GlobalPermission' => 'Special access',
-	'LBL_Module_desc' => 'Here you can define which profiles have access to all records regardless of roles.',
-	'LBL_PROFILE_NAME' => 'Profile name',
-	'LBL_DESCRIPTION' => 'Profile description',
-	'LBL_VIEW_ALL_DESC' => 'If you tick this option, all the modules will have a marked option of: Browsing',
-	'LBL_VIEW_ALL' => 'Enable preview of all records',
-	'LBL_EDIT_ALL_DESC' => 'If you tick this option, all the modules will have a marked option of: Create/Edit.',
-	'LBL_EDIT_ALL' => 'Enable editing of all records',
-	'LBL_SAVE_OK' => 'Saved change',
+	'Access to record' => 'Acceso a los registros',
+	'GlobalPermission' => 'Acceso Especial',
+	'LBL_Module_desc' => 'Aquí puede definir qué perfiles tienen acceso a todos los registros, independientemente de los roles.',
+	'LBL_PROFILE_NAME' => 'Nombre del Perfil',
+	'LBL_DESCRIPTION' => 'Descripción del Perfil',
+	'LBL_VIEW_ALL_DESC' => 'Si marca esta opción, todos los módulos tendrán marcada la opción: Leer',
+	'LBL_VIEW_ALL' => 'Habilita la Lectura de todos los registros',
+	'LBL_EDIT_ALL_DESC' => 'Si marca esta opción, todos los módulos tendrán marcada la opción: Crear/Editar.',
+	'LBL_EDIT_ALL' => 'Habilita la Edición de todos los registros',
+	'LBL_SAVE_OK' => 'Cambios guardados',
 ];
