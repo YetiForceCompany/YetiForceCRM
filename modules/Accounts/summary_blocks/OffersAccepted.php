6aaf4b37d64fc8fb456048eb47be50aeb9f704d0
Adrian Koń <a.kon@yetiforce.com>
2016-10-21 10:29:44 +0200

    Improved sql query

--- modules/Accounts/summary_blocks/OffersAccepted.php
+++ modules/Accounts/summary_blocks/OffersAccepted.php
@@ -9,11 +9,8 @@ class OffersAccepted
 
 	public function process($instance)
 	{
-		$db = PearDatabase::getInstance();
-		$quotes = 'SELECT COUNT(1) AS count FROM u_yf_squotes
-				INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid=u_yf_squotes.squotesid
-				WHERE vtiger_crmentity.deleted=0 && u_yf_squotes.accountid = ? && u_yf_squotes.squotes_status = ?';
-		$resultQuotes = $db->pquery($quotes, [$instance->getId(), 'PLL_ACCEPTED']);
-		return (int) $db->getSingleValue($resultQuotes);
+		return (int) (new \App\Db\Query())->from('u_yf_squotes')
+			->innerJoin('vtiger_crmentity', 'u_yf_squotes.squotesid = vtiger_crmentity.crmid')
+			->where(['deleted' => 0, 'u_yf_squotes.accountid' => $instance->getId(), 'u_yf_squotes.squotes_status' => 'PLL_ACCEPTED'])->count(1);
 	}
 }
