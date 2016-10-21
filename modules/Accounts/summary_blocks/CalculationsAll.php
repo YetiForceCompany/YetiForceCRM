87afb22dfa6d2a4fe08ed2e85e1f67b72f1de64d
Adrian Koń <a.kon@yetiforce.com>
2016-10-21 10:04:48 +0200

    Improved sql query

--- modules/Accounts/summary_blocks/CalculationsAll.php
+++ modules/Accounts/summary_blocks/CalculationsAll.php
@@ -9,11 +9,9 @@ class CalculationsAll
 
 	public function process($instance)
 	{
-		$db = PearDatabase::getInstance();
-		$calculations = 'SELECT COUNT(1) FROM u_yf_scalculations
-				INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid=u_yf_scalculations.scalculationsid
-				WHERE vtiger_crmentity.deleted=0 && u_yf_scalculations.accountid = ?';
-		$resultCalculations = $db->pquery($calculations, [$instance->getId()]);
-		return (int) $db->getSingleValue($resultCalculations);
+		$count = (new \App\Db\Query())->from('u_yf_scalculations')
+			->innerJoin('vtiger_crmentity', 'u_yf_scalculations.scalculationsid = vtiger_crmentity.crmid')
+			->where(['deleted' => 0, 'u_yf_scalculations.accountid' => $instance->getId()])->count(1);
+		return (int) $count;
 	}
 }
