a3444658425e0e4783dd41b4f055f3feaab654b0
Fernando Giacomino <info@electricidadder.com.ar>
2016-10-24 18:33:22 -0300

    Update ModTracker.php

--- languages/es_es/Settings/ModTracker.php
+++ languages/es_es/Settings/ModTracker.php
@@ -9,12 +9,12 @@
  * All Rights Reserved.
  * *********************************************************************************************************************************** */
 $languageStrings = [
-	'ModTracker' => 'Change history',
-	'LBL_MODTRACKER_SETTINGS' => 'Modules – Track Changes / History',
-	'LBL_MODTRACKER_SETTINGS_DESCRIPTION' => 'Manage change history in modules',
-	'LBL_MODULE' => 'Module',
-	'LBL_ACTIVE' => 'Track changes',
-	'LBL_TOOLS' => 'Tools',
-	'LBL_TRACK_CHANGES_ENABLED' => 'Track changes enabled',
-	'LBL_TRACK_CHANGES_DISABLE' => 'Track changes disabled',
+	'ModTracker' => 'Historial de Cambios',
+	'LBL_MODTRACKER_SETTINGS' => 'Módulos – Seguimiento de Cambios',
+	'LBL_MODTRACKER_SETTINGS_DESCRIPTION' => 'Gestionar el registro de cambios en cada módulo',
+	'LBL_MODULE' => 'Módulo',
+	'LBL_ACTIVE' => 'Registrar Cambios',
+	'LBL_TOOLS' => 'Herramientas',
+	'LBL_TRACK_CHANGES_ENABLED' => 'Registro de Cambios habilitado',
+	'LBL_TRACK_CHANGES_DISABLE' => 'Registro de Cambios deshabilitado',
 ];
