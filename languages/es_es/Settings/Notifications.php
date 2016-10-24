dbe98c6c7501d9ca6d208e8a5744e78112cf32b5
Fernando Giacomino <info@electricidadder.com.ar>
2016-10-24 19:57:44 -0300

    Update Notifications.php

--- languages/es_es/Settings/Notifications.php
+++ languages/es_es/Settings/Notifications.php
@@ -1,19 +1,19 @@
 <?php
 /* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 $languageStrings = [
-	'LBL_TYPE_NOTIFICATIONS' => 'Notification types',
-	'LBL_NOTIFICATIONS_CONFIGURATION' => 'Notifications - settings',
-	'LBL_DEFAULT_MENU' => 'Default',
-	'LBL_TITLE_ADDED' => 'Create notification',
-	'LBL_NAME' => 'Type name',
-	'LBL_WIDTH' => 'Width',
-	'LBL_HEIGHT' => 'Height',
-	'LBL_MODULE' => 'Module name',
-	'LBL_USERS_WATCHING' => 'Users watching',
-	'LBL_MESSAGES_FROM_USERS' => 'Messages from users',
-	'LBL_WATCHDOG' => 'Track changes',
+	'LBL_TYPE_NOTIFICATIONS' => 'Tipos de Notificaciones',
+	'LBL_NOTIFICATIONS_CONFIGURATION' => 'Notificaciones - ajustes',
+	'LBL_DEFAULT_MENU' => 'Por defecto',
+	'LBL_TITLE_ADDED' => 'Crear notificación',
+	'LBL_NAME' => 'Nombre de Tipo',
+	'LBL_WIDTH' => 'Ancho',
+	'LBL_HEIGHT' => 'Alto',
+	'LBL_MODULE' => 'Nombre de Módulo',
+	'LBL_USERS_WATCHING' => 'Usuarios notificados',
+	'LBL_MESSAGES_FROM_USERS' => 'Mensajes de los Usuarios',
+	'LBL_WATCHDOG' => 'Seguimiento de Cambios',
 ];
 $jsLanguageStrings = [
-	'JS_DELETE_CONFIRMATION' => 'Are you sure you want to delete this element?',
-	'JS_SAVE_CAHNGES' => 'Changes saved',
+	'JS_DELETE_CONFIRMATION' => 'Está seguro de borrar este elemento?',
+	'JS_SAVE_CAHNGES' => 'Cambios guardados',
 ];
