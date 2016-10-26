92eab661b1065691ccace2f59e8f377d4affdce2
Tomasz Kur <t.kur@yetiforce.com>
2016-10-26 15:12:30 +0200

    Improved addForeignKey function in Importer

--- vendor/yetiforce/Db/Importer.php
+++ vendor/yetiforce/Db/Importer.php
@@ -107,7 +107,7 @@ class Importer
 		}
 		foreach ($importer->foreignKey as $key) {
 			$importer->db->createCommand()->addForeignKey(
-				$key[0], $key[1], $key[2], $key[3], $key[4], $key[5]
+				$key[0], $key[1], $key[2], $key[3], $key[4], $key[5], $key[6]
 			)->execute();
 		}
 	}
