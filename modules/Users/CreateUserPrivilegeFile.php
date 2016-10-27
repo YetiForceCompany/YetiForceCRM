03e5010f7b93d7a9f33b070fb19aa5bb33a077ed
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-10-27 13:35:44 +0200

    Fixed saving user

--- modules/Users/CreateUserPrivilegeFile.php
+++ modules/Users/CreateUserPrivilegeFile.php
@@ -47,6 +47,7 @@ function createUserPrivilegesfile($userid)
 			$tabsPermissionArr = getCombinedUserTabsPermissions($userid);
 			$actionPermissionArr = getCombinedUserActionPermissions($userid);
 			$user_role = fetchUserRole($userid);
+			var_dump($userid, $user_role);
 			$user_role_info = \App\PrivilegeUtil::getRoleDetail($user_role);
 			$user_role_parent = $user_role_info['parentrole'];
 			$subRoles = getRoleSubordinates($user_role);
