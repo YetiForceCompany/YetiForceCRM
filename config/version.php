510e6285ebba1ea1ff3f6db38fa1a4fa7e04dc2c
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-11-05 17:36:37 +0100

    Added small fixes

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '3.4.426',
-	'patchVersion' => '2016.11.04',
+	'appVersion' => '3.4.427',
+	'patchVersion' => '2016.11.05',
 	'lib_mPDF' => '0.0.0',
-	'lib_roundcube' => '0.0.5',
+	'lib_roundcube' => '0.0.6',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.0',
 ];
