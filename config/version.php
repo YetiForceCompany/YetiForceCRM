3df20af64177e889cd8be786a88ca98df94a8fc3
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-11-09 14:26:58 +0100

    Added operators of field "Boolean" for QueryGenerator

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '3.4.489',
+	'appVersion' => '3.4.490',
 	'patchVersion' => '2016.11.09',
 	'lib_mPDF' => '0.0.0',
 	'lib_roundcube' => '0.0.9',
---
+++ vendor/yetiforce/QueryFieldCondition/OperatorBoolean.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\QueryFieldCondition;
+
+/**
+ * Boolean Query Condition Parser Class
+ * @package YetiForce.App
+ * @license licenses/License.html
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+class OperatorBoolean extends BaseFieldParser
+{
+	
+}
