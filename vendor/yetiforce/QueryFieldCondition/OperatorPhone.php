6198f8b58819f6cb2884294215e619a0e7b85dcd
Tomasz Kur <t.kur@yetiforce.com>
2016-11-09 14:32:23 +0100

    Added operators of field "Phone" for QueryGenerator

---
+++ vendor/yetiforce/QueryFieldCondition/OperatorPhone.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\QueryFieldCondition;
+
+/**
+ * Phone Query Condition Parser Class
+ * @package YetiForce.App
+ * @license licenses/License.html
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+class OperatorPhone extends OperatorString
+{
+	
+}
