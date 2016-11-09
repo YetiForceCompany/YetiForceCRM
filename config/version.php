b7d920283b17f11442a7e5ebac63e03ac5feffd1
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-11-09 08:45:01 +0100

    Updated version

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '3.4.478',
-	'patchVersion' => '2016.11.08',
+	'appVersion' => '3.4.479',
+	'patchVersion' => '2016.11.09',
 	'lib_mPDF' => '0.0.0',
-	'lib_roundcube' => '0.0.8',
+	'lib_roundcube' => '0.0.9',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.0',
 ];
