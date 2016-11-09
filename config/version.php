a1437938f9d78cd65dde7578174633167c25ca07
Tomasz Kur <t.kur@yetiforce.com>
2016-11-09 14:04:58 +0100

    Added operators of field "Double" for QueryGenerator

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '3.4.486',
+	'appVersion' => '3.4.487',
 	'patchVersion' => '2016.11.09',
 	'lib_mPDF' => '0.0.0',
 	'lib_roundcube' => '0.0.9',
---
+++ vendor/yetiforce/QueryFieldCondition/Double.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\QueryFieldCondition;
+
+/**
+ * String Query Condition Parser Class
+ * @package YetiForce.App
+ * @license licenses/License.html
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+class Double extends BaseFieldParser
+{
+
+	/**
+	 * Lower operator
+	 * @return array
+	 */
+	public function operatorL()
+	{
+		return ['<', $this->getColumnName(), $this->value];
+	}
+
+	/**
+	 * Greater operator
+	 * @return array
+	 */
+	public function operatorG()
+	{
+		return ['>', $this->getColumnName(), $this->value];
+	}
+
+	/**
+	 * Lower or equal operator
+	 * @return array
+	 */
+	public function operatorM()
+	{
+		return ['<=', $this->getColumnName(), $this->value];
+	}
+
+	/**
+	 * Greater or equal operator
+	 * @return array
+	 */
+	public function operatorH()
+	{
+		return ['>=', $this->getColumnName(), $this->value];
+	}
+}
