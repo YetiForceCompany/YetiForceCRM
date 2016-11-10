8e5e562650586f09d70131b5c2299a5abd7f41aa
Tomasz Kur <t.kur@yetiforce.com>
2016-11-10 15:19:39 +0100

    Added operators of field "Integer" for QueryGenerator

---
+++ vendor/yetiforce/QueryFieldCondition/IntegerCondition.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\QueryFieldCondition;
+
+/**
+ * Integer Query Condition Parser Class
+ * @package YetiForce.App
+ * @license licenses/License.html
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+class IntegerCondition extends BaseFieldParser
+{
+
+	/**
+	 * Lower operator
+	 * @return array
+	 */
+	public function operatorL()
+	{
+		return ['<', $this->getColumnName(), $this->value];
+	}
+
+	/**
+	 * Greater operator
+	 * @return array
+	 */
+	public function operatorG()
+	{
+		return ['>', $this->getColumnName(), $this->value];
+	}
+
+	/**
+	 * Lower or equal operator
+	 * @return array
+	 */
+	public function operatorM()
+	{
+		return ['<=', $this->getColumnName(), $this->value];
+	}
+
+	/**
+	 * Greater or equal operator
+	 * @return array
+	 */
+	public function operatorH()
+	{
+		return ['>=', $this->getColumnName(), $this->value];
+	}
+}
