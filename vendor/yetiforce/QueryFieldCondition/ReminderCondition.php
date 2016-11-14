b5bba1a29bf5e3eb683ec08dfa14f706467aa503
Tomasz Kur <t.kur@yetiforce.com>
2016-11-14 15:51:50 +0100

    Added operators of field "Reminder" for QueryGenerator

---
+++ vendor/yetiforce/QueryFieldCondition/ReminderCondition.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\QueryFieldCondition;
+
+/**
+ * Reference Query Condition Parser Class
+ * @package YetiForce.App
+ * @license licenses/License.html
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+class ReminderCondition extends IntegerCondition
+{
+	
+}
