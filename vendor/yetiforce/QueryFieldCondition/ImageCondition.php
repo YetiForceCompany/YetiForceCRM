8ed9e9f0e93e40375a14d035d3ba717323b3ee53
Tomasz Kur <t.kur@yetiforce.com>
2016-11-15 10:31:55 +0100

    Added operators of fields "CategoryMultipicklist", "CurrencyList", "DocumentsFileUpload", "FileLocationType", "Image" for QueryGenerator

---
+++ vendor/yetiforce/QueryFieldCondition/ImageCondition.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\QueryFieldCondition;
+
+/**
+ * Image Query Condition Parser Class
+ * @package YetiForce.App
+ * @license licenses/License.html
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+class ImageCondition extends StringCondition
+{
+	
+}
