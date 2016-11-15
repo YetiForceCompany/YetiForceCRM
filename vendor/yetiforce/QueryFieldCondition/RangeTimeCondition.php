fc9bbeee54e6761da408c9b2358a60a7721e8424
Tomasz Kur <t.kur@yetiforce.com>
2016-11-15 08:28:12 +0100

    Added operators of field "RangeTime" for QueryGenerator

---
+++ vendor/yetiforce/QueryFieldCondition/RangeTimeCondition.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\QueryFieldCondition;
+
+/**
+ * RangeTime Query Condition Parser Class
+ * @package YetiForce.App
+ * @license licenses/License.html
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+class RangeTimeCondition extends BaseFieldParser
+{
+	
+}
