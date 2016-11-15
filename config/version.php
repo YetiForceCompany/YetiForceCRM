7696e6d95dba85ada995035b81e8dfbf7bab60ff
Tomasz Kur <t.kur@yetiforce.com>
2016-11-16 00:08:13 +0100

    Fixed PosList field

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '3.4.577',
-	'patchVersion' => '2016.11.15',
+	'appVersion' => '3.4.578',
+	'patchVersion' => '2016.11.16',
 	'lib_mPDF' => '0.0.0',
 	'lib_roundcube' => '0.0.9',
 	'lib_PHPExcel' => '0.0.0',
--- vendor/yetiforce/QueryFieldCondition/PosListCondition.php
+++ vendor/yetiforce/QueryFieldCondition/PosListCondition.php
@@ -7,7 +7,23 @@ namespace App\QueryFieldCondition;
  * @license licenses/License.html
  * @author Tomasz Kur <t.kur@yetiforce.com>
  */
-class PosListCondition extends StringCondition
+class PosListCondition extends PicklistCondition
 {
+	/**
+	 * Equals operator
+	 * @return array
+	 */
+	public function operatorE()
+	{
+		return ['or like', $this->getColumnName(), $this->getValue()];
+	}
 	
+	/**
+	 * Not equal operator
+	 * @return array
+	 */
+	public function operatorN()
+	{
+		return ['or not like', $this->getColumnName(), $this->getValue()];
+	}
 }
