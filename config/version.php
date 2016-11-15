9a82ed0db9751b79ffa2ff88b8f1cd830f8e8812
Tomasz Kur <t.kur@yetiforce.com>
2016-11-15 09:48:58 +0100

    Added operators of field "InventoryLimit" for QueryGenerator

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '3.4.558',
+	'appVersion' => '3.4.559',
 	'patchVersion' => '2016.11.15',
 	'lib_mPDF' => '0.0.0',
 	'lib_roundcube' => '0.0.9',
---
+++ vendor/yetiforce/QueryFieldCondition/InventoryLimitCondition.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\QueryFieldCondition;
+
+/**
+ * InventoryLimit Query Condition Parser Class
+ * @package YetiForce.App
+ * @license licenses/License.html
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+class InventoryLimitCondition extends StringCondition
+{
+	
+}
