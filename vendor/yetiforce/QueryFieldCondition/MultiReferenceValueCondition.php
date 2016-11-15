9e98b37bff9c28706c226b49bd7ea92f0f0d6f56
Tomasz Kur <t.kur@yetiforce.com>
2016-11-15 09:30:31 +0100

    Added operators of field "MultiReferenceValue" for QueryGenerator

---
+++ vendor/yetiforce/QueryFieldCondition/MultiReferenceValueCondition.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\QueryFieldCondition;
+
+/**
+ * MultiReferenceValue Query Condition Parser Class
+ * @package YetiForce.App
+ * @license licenses/License.html
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+class MultiReferenceValueCondition extends BaseFieldParser
+{
+
+	public function getValue()
+	{
+		$valueArray = explode(',', $this->value);
+		foreach ($valueArray as $key => $value) {
+			$valueArray[$key] = '|#|' . $value . '|#|';
+		}
+		return$valueArray;
+	}
+
+	/**
+	 * Equals operator
+	 * @return array
+	 */
+	public function operatorE()
+	{
+		return ['or like', $this->getColumnName(), $this->getValue()];
+	}
+
+	/**
+	 * Not equal operator
+	 * @return array
+	 */
+	public function operatorN()
+	{
+		return ['or not like', $this->getColumnName(), $this->getValue()];
+	}
+}
