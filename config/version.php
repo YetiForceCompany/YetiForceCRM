e270ea611c9b66fc624f28f6d5209135e31ab368
Tomasz Kur <t.kur@yetiforce.com>
2016-11-15 08:20:20 +0100

    Added operators of field "Taxes" for QueryGenerator

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '3.4.552',
+	'appVersion' => '3.4.553',
 	'patchVersion' => '2016.11.15',
 	'lib_mPDF' => '0.0.0',
 	'lib_roundcube' => '0.0.9',
---
+++ vendor/yetiforce/QueryFieldCondition/TaxesCondition.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\QueryFieldCondition;
+
+/**
+ * Taxes Query Condition Parser Class
+ * @package YetiForce.App
+ * @license licenses/License.html
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+class TaxesCondition extends StringCondition
+{
+	
+}
