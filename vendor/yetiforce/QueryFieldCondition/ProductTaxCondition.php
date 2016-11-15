f44f4143f5f4c197140fe3709b0652d7e643a00f
Tomasz Kur <t.kur@yetiforce.com>
2016-11-15 08:51:48 +0100

    Added operators of field "ProductTax" for QueryGenerator

---
+++ vendor/yetiforce/QueryFieldCondition/ProductTaxCondition.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\QueryFieldCondition;
+
+/**
+ * ProductTax Query Condition Parser Class
+ * @package YetiForce.App
+ * @license licenses/License.html
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+class ProductTaxCondition extends StringCondition
+{
+	
+}
