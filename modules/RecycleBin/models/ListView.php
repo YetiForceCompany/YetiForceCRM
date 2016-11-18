bd8c676efd57193cf4a4d486af590d43391e244b
Tomasz Kur <t.kur@yetiforce.com>
2016-11-18 11:36:36 +0100

    Improved ListView - RecycleBin

--- modules/RecycleBin/models/ListView.php
+++ modules/RecycleBin/models/ListView.php
@@ -35,89 +35,12 @@ class RecycleBin_ListView_Model extends Vtiger_ListView_Model
 	 * @param Vtiger_Paging_Model $pagingModel
 	 * @return array - Associative array of record id mapped to Vtiger_Record_Model instance.
 	 */
-	public function getListViewEntries(Vtiger_Paging_Model $pagingModel, $searchResult = false)
+	public function getListViewEntries(Vtiger_Paging_Model $pagingModel)
 	{
-		$db = PearDatabase::getInstance();
-		$moduleName = $this->getModule()->get('name');
-		$moduleFocus = CRMEntity::getInstance($moduleName);
-		$moduleModel = Vtiger_Module_Model::getInstance($moduleName);
-
 		$queryGenerator = $this->get('query_generator');
-		$listViewContoller = $this->get('listview_controller');
-
-		$orderBy = $this->getForSql('orderby');
-		$sortOrder = $this->getForSql('sortorder');
-
-		if (!empty($orderBy)) {
-			$columnFieldMapping = $moduleModel->getColumnFieldMapping();
-			$orderByFieldName = $columnFieldMapping[$orderBy];
-			$orderByFieldModel = $moduleModel->getField($orderByFieldName);
-			if ($orderByFieldModel && $orderByFieldModel->isReferenceField()) {
-				//IF it is reference add it in the where fields so that from clause will be having join of the table
-				$queryGenerator = $this->get('query_generator');
-				$queryGenerator->setConditionField($orderByFieldName);
-			}
-		}
-		if (!empty($orderBy) && $orderBy === 'smownerid') {
-			$fieldModel = Vtiger_Field_Model::getInstance('assigned_user_id', $moduleModel);
-			if ($fieldModel->getFieldDataType() == 'owner') {
-				$orderBy = 'COALESCE(' . \vtlib\Deprecated::getSqlForNameInDisplayFormat(['first_name' => 'vtiger_users.first_name', 'last_name' => 'vtiger_users.last_name'], 'Users') . ',vtiger_groups.groupname)';
-			}
-		}
-
-		$listQuery = $this->getQuery();
-		$listQuery = preg_replace("/vtiger_crmentity.deleted\s*=\s*0/i", 'vtiger_crmentity.deleted = 1', $listQuery);
-
-		$startIndex = $pagingModel->getStartIndex();
-		$pageLimit = $pagingModel->getPageLimit();
-
-		if (!empty($orderBy)) {
-			if ($orderByFieldModel && $orderByFieldModel->isReferenceField()) {
-				$referenceModules = $orderByFieldModel->getReferenceList();
-				$referenceNameFieldOrderBy = array();
-				foreach ($referenceModules as $referenceModuleName) {
-					$referenceModuleModel = Vtiger_Module_Model::getInstance($referenceModuleName);
-					$referenceNameFields = $referenceModuleModel->getNameFields();
-
-					$columnList = array();
-					foreach ($referenceNameFields as $nameField) {
-						$fieldModel = $referenceModuleModel->getField($nameField);
-						$columnList[] = $fieldModel->get('table') . '.' . $fieldModel->get('column');
-					}
-					if (count($columnList) > 1) {
-						$referenceNameFieldOrderBy[] = \vtlib\Deprecated::getSqlForNameInDisplayFormat(array('first_name' => $columnList[0], 'last_name' => $columnList[1]), 'Users') . ' ' . $sortOrder;
-					} else {
-						$referenceNameFieldOrderBy[] = implode('', $columnList) . ' ' . $sortOrder;
-					}
-				}
-				$listQuery .= sprintf(' ORDER BY %s', implode(',', $referenceNameFieldOrderBy));
-			} else {
-				$listQuery .= sprintf(' ORDER BY %s %s', $orderBy, $sortOrder);
-			}
-		}
-		$listQuery .= " LIMIT " . ($pageLimit + 1) . ' OFFSET ' . $startIndex;
-
-		$listResult = $db->query($listQuery);
-		$listViewRecordModels = [];
-		$listViewEntries = $listViewContoller->getListViewRecords($moduleFocus, $moduleName, $listResult);
-		$count = $db->num_rows($listResult);
-		$pagingModel->calculatePageRange($count);
-
-		if ($count > $pageLimit) {
-			array_pop($listViewEntries);
-			$pagingModel->set('nextPageExists', true);
-		} else {
-			$pagingModel->set('nextPageExists', false);
-		}
-
-		$index = 0;
-		foreach ($listViewEntries as $recordId => $record) {
-			$rawData = $db->query_result_rowdata($listResult, $index++);
-			$record['id'] = $recordId;
-			$listViewRecordModels[$recordId] = $moduleModel->getRecordFromArray($record, $rawData);
-			$listViewRecordModels[$recordId]->colorList = Settings_DataAccess_Module_Model::executeColorListHandlers($moduleName, $recordId, $moduleModel->getRecordFromArray($listViewContoller->rawData[$recordId]));
-		}
-		return $listViewRecordModels;
+		$queryGenerator->deletedCondition = false;
+		$queryGenerator->addAndConditionNative(['vtiger_crmentity.deleted' => 1]);
+		return parent::getListViewEntries($pagingModel);
 	}
 
 	/**
@@ -127,27 +50,10 @@ class RecycleBin_ListView_Model extends Vtiger_ListView_Model
 	 */
 	public function getListViewCount()
 	{
-		$db = PearDatabase::getInstance();
 
 		$queryGenerator = $this->get('query_generator');
-		$listQuery = $queryGenerator->getQuery();
-		$listQuery = preg_replace("/vtiger_crmentity.deleted\s*=\s*0/i", 'vtiger_crmentity.deleted = 1', $listQuery);
-
-		$position = stripos($listQuery, ' from ');
-		if ($position) {
-			$split = preg_split('/ from /i', $listQuery, 2);
-			$listQuery = 'SELECT count(*) AS count ';
-			$countSplit = count($split);
-			for ($i = 1; $i < $countSplit; $i++) {
-				$listQuery .= sprintf(' FROM %s', $split[$i]);
-			}
-		}
-
-		if ($this->getModule()->get('name') == 'Calendar') {
-			$listQuery .= ' && activitytype <> "Emails"';
-		}
-
-		$listResult = $db->query($listQuery);
-		return $db->getSingleValue($listResult);
+		$queryGenerator->deletedCondition = false;
+		$queryGenerator->addAndConditionNative(['vtiger_crmentity.deleted' => 1]);
+		return parent::getListViewCount();
 	}
 }
