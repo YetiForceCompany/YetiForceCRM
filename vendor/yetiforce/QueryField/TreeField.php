e3a47729123fb92ccee07d931d8a80461453378d
Tomasz Kur <t.kur@yetiforce.com>
2016-11-21 13:13:35 +0100

    Improved condition for "Tree" field

--- vendor/yetiforce/QueryField/TreeField.php
+++ vendor/yetiforce/QueryField/TreeField.php
@@ -9,5 +9,27 @@ namespace App\QueryField;
  */
 class TreeField extends StringField
 {
-	
+
+	/**
+	 * Get value
+	 * @return mixed
+	 */
+	public function getValue()
+	{
+		return explode(',', $this->value);
+	}
+
+	/**
+	 * Get order by
+	 * @return array
+	 */
+	public function getOrderBy($order = false)
+	{
+		$this->queryGenerator->addJoin(['LEFT JOIN', 'vtiger_trees_templates_data', $this->getColumnName() . ' =  vtiger_trees_templates_data.tree AND vtiger_trees_templates_data.templateid = :template', [':template' => $this->fieldModel->getFieldParams()]]);
+		if ($order && strtoupper($order) === 'DESC') {
+			return ['vtiger_trees_templates_data.name' => SORT_DESC];
+		} else {
+			return ['vtiger_trees_templates_data.name' => SORT_ASC];
+		}
+	}
 }
