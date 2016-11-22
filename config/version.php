43339023219ac891a63b43f20a9dffe3b89a7845
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-11-22 22:03:15 +0100

    Changed height windows quickly create

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '3.4.709',
+	'appVersion' => '3.4.710',
 	'patchVersion' => '2016.11.22',
 	'lib_mPDF' => '0.0.0',
 	'lib_roundcube' => '0.0.9',
--- layouts/basic/modules/Vtiger/resources/Header.js
+++ layouts/basic/modules/Vtiger/resources/Header.js
@@ -192,9 +192,9 @@ jQuery.Class("Vtiger_Header_Js", {
 			var quickCreateContent = quickCreateForm.find('.quickCreateContent');
 			var quickCreateContentHeight = quickCreateContent.height();
 			var contentHeight = parseInt(quickCreateContentHeight);
-			if (contentHeight > 300) {
+			if (contentHeight > 500) {
 				app.showScrollBar(jQuery('.quickCreateContent'), {
-					'height': '300px'
+					'height': '500px'
 				});
 			}
 
