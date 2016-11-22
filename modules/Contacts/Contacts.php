5e0aaa250e6bb5b08085c8cda56738a8035af914
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-11-22 22:04:16 +0100

    Add a field in the relation

--- modules/Contacts/Contacts.php
+++ modules/Contacts/Contacts.php
@@ -101,6 +101,11 @@ class Contacts extends CRMEntity
 		'Member Of' => 'parent_id',
 		'Assigned To' => 'assigned_user_id'
 	);
+
+	/**
+	 * @var string[] List of fields in the RelationListView
+	 */
+	public $relationFields = ['firstname', 'lastname', 'jobtitle', 'email', 'phone', 'assigned_user_id'];
 	// This is the list of vtiger_fields that are required
 	public $required_fields = array("lastname" => 1);
 	// Used when enabling/disabling the mandatory fields for the module.
