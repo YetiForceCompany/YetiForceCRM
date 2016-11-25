b96097fee4a3346f363f9e822736bfd32ac4153f
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-11-25 09:21:22 +0100

    Fixed saving and editing calendar records

--- vendor/yetiforce/Record.php
+++ vendor/yetiforce/Record.php
@@ -225,7 +225,7 @@ class Record
 	public static function isExists($recordId, $moduleName = false)
 	{
 		$recordMetaData = Functions::getCRMRecordMetadata($recordId);
-		return (isset($recordMetaData) && $recordMetaData['deleted'] === 0 && ($moduleName ? $recordMetaData['setype'] === $moduleName : true)) ? true : false;
+		return (isset($recordMetaData) && $recordMetaData['deleted'] === 0 && ($moduleName ? $recordMetaData['setype'] === \App\Module::getTabName($moduleName) : true)) ? true : false;
 	}
 
 	public static function getType($recordId)
