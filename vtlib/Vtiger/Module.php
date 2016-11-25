a8f9335f23543cb7029abb44dfcc36e6e868e556
Adrian Koń <a.kon@yetiforce.com>
2016-11-25 11:28:04 +0100

    Removed space

--- vtlib/Vtiger/Module.php
+++ vtlib/Vtiger/Module.php
@@ -292,7 +292,7 @@ class Module extends ModuleBasic
 		}
 		$fire = self::fireEvent($moduleName, $eventType);
 		if ($fire) {
-			 \App\Db::getInstance()->createCommand()->update('vtiger_tab', ['presence' => $enableDisable], ['name' => $moduleName])->execute();
+			\App\Db::getInstance()->createCommand()->update('vtiger_tab', ['presence' => $enableDisable], ['name' => $moduleName])->execute();
 			Deprecated::createModuleMetaFile();
 			vtlib_RecreateUserPrivilegeFiles();
 			$menuRecordModel = new \Settings_Menu_Record_Model();
