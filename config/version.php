c6bf6a5e5660803399aa420953be09e643a624d5
Tomasz Kur <t.kur@yetiforce.com>
2016-11-26 16:39:29 +0100

    Added field "Salutation" for QueryGenerator

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '3.4.805',
-	'patchVersion' => '2016.11.25',
+	'appVersion' => '3.4.806',
+	'patchVersion' => '2016.11.26',
 	'lib_mPDF' => '0.0.0',
 	'lib_roundcube' => '0.0.9',
 	'lib_PHPExcel' => '0.0.0',
