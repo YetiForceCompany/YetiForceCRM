7da3d1583d3cb8a282c23e850d2fd4f227787f4f
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-12-07 01:16:16 +0100

    Added minor improvements

--- tests/phpunit/07_TestModule.php
+++ tests/phpunit/07_TestModule.php
@@ -12,13 +12,10 @@ class TestModule extends TestCase
 
 	public function test()
 	{
-
 		$testModule = 'TestModule.zip';
 		try {
 			file_put_contents($testModule, file_get_contents('https://tests.yetiforce.com/' . $_SERVER['YETI_KEY']));
-		} catch (Exception $exc) {
-			
-		}
+		} catch (Exception $exc) {}
 		if (file_exists($testModule)) {
 			$package = new vtlib\Package();
 			$package->import($testModule);
