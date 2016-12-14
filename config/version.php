a3dd114884e398f47eec6d9effbd35f891a708af
Adrian Koń <a.kon@yetiforce.com>
2016-12-14 07:33:00 +0100

    Fixed calling function

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '3.4.1139',
-	'patchVersion' => '2016.12.13',
+	'appVersion' => '3.4.1140',
+	'patchVersion' => '2016.12.14',
 	'lib_mPDF' => '0.0.0',
 	'lib_roundcube' => '0.0.9',
 	'lib_PHPExcel' => '0.0.0',
