c333530b8dde20dfd1693c69c52518f4f2aa8dd5
Tomasz Kur <t.kur@yetiforce.com>
2016-12-15 14:32:35 +0100

    Improved caching details about company

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '3.4.1163',
+	'appVersion' => '3.4.1164',
 	'patchVersion' => '2016.12.15',
 	'lib_mPDF' => '0.0.0',
 	'lib_roundcube' => '0.0.9',
--- modules/Vtiger/models/CompanyDetails.php
+++ modules/Vtiger/models/CompanyDetails.php
@@ -39,17 +39,18 @@ class Vtiger_CompanyDetails_Model extends Vtiger_Base_Model
 	 */
 	public static function getInstanceById($id = 1)
 	{
-		if (\App\Cache::staticHas('organizationDetails', $id)) {
-			return \App\Cache::staticGet('organizationDetails', $id);
+		if (\App\Cache::has('organizationDetails', $id)) {
+			$row = \App\Cache::get('organizationDetails', $id);
+		} else {
+			$row = (new \App\Db\Query())->from('vtiger_organizationdetails')
+				->where(['organization_id' => $id])
+				->one();
+			\App\Cache::save('organizationDetails', $id, $row, \App\Cache::LONG);
 		}
-		$row = (new \App\Db\Query())->from('vtiger_organizationdetails')
-			->where(['organization_id' => $id])
-			->one();
 		$companyDetails = new self();
 		if ($row) {
 			$companyDetails->setData($row);
 		}
-		\App\Cache::staticSave('organizationDetails', $id, $companyDetails, \App\Cache::LONG);
 		return $companyDetails;
 	}
 }
