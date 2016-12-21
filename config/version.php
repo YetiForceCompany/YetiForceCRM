cb308a45a5935264502185f8e4483de90ea9f9e1
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-12-21 10:53:33 +0100

    Fixed function that adds columns to database

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '3.4.1215',
-	'patchVersion' => '2016.12.20',
+	'appVersion' => '3.4.1216',
+	'patchVersion' => '2016.12.21',
 	'lib_mPDF' => '0.0.0',
 	'lib_roundcube' => '0.0.9',
 	'lib_PHPExcel' => '0.0.0',
