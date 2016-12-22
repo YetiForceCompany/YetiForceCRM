97b81b81787a6ff6d661ea7c9753d1a4b6509d60
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-12-22 16:40:15 +0100

    Fixed #3750

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '3.4.1250',
+	'appVersion' => '3.4.1251',
 	'patchVersion' => '2016.12.22',
 	'lib_mPDF' => '0.0.0',
-	'lib_roundcube' => '0.0.9',
+	'lib_roundcube' => '0.0.10',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.0',
 	'lib_gantt' => '0.0.0',
