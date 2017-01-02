11fd06b33c8e9588f731aa5b58b765b8acf9f971
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-01-02 11:53:19 +0100

    lib_roundcube: Added missing files

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '3.4.1315',
+	'appVersion' => '3.4.1316',
 	'patchVersion' => '2017.01.02',
 	'lib_mPDF' => '0.0.0',
-	'lib_roundcube' => '0.0.13',
+	'lib_roundcube' => '0.0.14',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.0',
 	'lib_gantt' => '0.0.0',
