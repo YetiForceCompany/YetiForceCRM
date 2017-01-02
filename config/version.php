192f1befc3a1e3da48d79f40b6c7282aa16df4fe
Tomasz Kur <t.kur@yetiforce.com>
2017-01-02 11:32:16 +0100

    Improved compatibility functions

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '3.4.1313',
+	'appVersion' => '3.4.1314',
 	'patchVersion' => '2017.01.02',
 	'lib_mPDF' => '0.0.0',
 	'lib_roundcube' => '0.0.11',
--- modules/OSSMailScanner/scanneractions/BindVendors.php
+++ modules/OSSMailScanner/scanneractions/BindVendors.php
@@ -9,7 +9,7 @@
 class OSSMailScanner_BindVendors_ScannerAction extends OSSMailScanner_EmailScannerAction_Model
 {
 
-	public function process(OSSMail_Mail_Model $mail)
+	public function process(OSSMail_Mail_Model $mail, $moduleName = 'Vendors')
 	{
 		return parent::process($mail, 'Vendors');
 	}
