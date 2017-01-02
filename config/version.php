ecb73ebcb12d707973e520d4476f212eea24dbd7
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-01-02 11:34:59 +0100

    Added minor fixes in the mail

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '3.4.1314',
+	'appVersion' => '3.4.1315',
 	'patchVersion' => '2017.01.02',
 	'lib_mPDF' => '0.0.0',
-	'lib_roundcube' => '0.0.11',
+	'lib_roundcube' => '0.0.13',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.0',
 	'lib_gantt' => '0.0.0',
