9ac5ee573ad8f77e79f8049452c1e7f774bb6b49
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-01-04 00:08:32 +0100

    Updated tests

--- tests/phpunit/08_Cron.php
+++ tests/phpunit/08_Cron.php
@@ -18,8 +18,10 @@ class Cron extends TestCase
 		echo PHP_EOL;
 		require 'cron/vtigercron.php';
 		$rows = (new \App\Db\Query())->select(['modue' => 'setype', 'rows' => 'count(*)'])->from('vtiger_crmentity')->groupBy('setype')->orderBy(['rows' => SORT_DESC])->all();
+		$c = '';
 		foreach ($rows as $value) {
-			echo "{$value['modue']} = {$value['rows']}" . PHP_EOL;
+			$c .= "{$value['modue']} = {$value['rows']}" . PHP_EOL;
 		}
+		file_put_contents('records.log', $c, FILE_APPEND);
 	}
 }
