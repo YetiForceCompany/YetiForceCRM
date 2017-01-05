8dfbefa4bfd87d184853f818de4b95077d98c404
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-01-05 15:44:03 +0100

    Added small fix

--- modules/EmailTemplates/EmailTemplates.php
+++ modules/EmailTemplates/EmailTemplates.php
@@ -16,20 +16,19 @@ class EmailTemplates extends Vtiger_CRMEntity
 	/**
 	 * Mandatory table for supporting custom fields.
 	 */
-	public $customFieldTable = ['u_yf_emailtemplatescf', 'emailtemplatesid'];
+	public $customFieldTable = [];
 
 	/**
 	 * Mandatory for Saving, Include tables related to this module.
 	 */
-	public $tab_name = ['vtiger_crmentity', 'u_yf_emailtemplates', 'u_yf_emailtemplatescf'];
+	public $tab_name = ['vtiger_crmentity', 'u_yf_emailtemplates'];
 
 	/**
 	 * Mandatory for Saving, Include tablename and tablekey columnname here.
 	 */
 	public $tab_name_index = [
 		'vtiger_crmentity' => 'crmid',
-		'u_yf_emailtemplates' => 'emailtemplatesid',
-		'u_yf_emailtemplatescf' => 'emailtemplatesid'
+		'u_yf_emailtemplates' => 'emailtemplatesid'
 	];
 
 	/**
