d272e9e6dc6c040eb896ccd54cacc4f544a9ab6b
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-01-05 09:14:42 +0100

    Improved ladp configuration

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '3.4.1367',
-	'patchVersion' => '2017.01.04',
+	'appVersion' => '3.4.1368',
+	'patchVersion' => '2017.01.05',
 	'lib_mPDF' => '0.0.0',
 	'lib_roundcube' => '0.0.15',
 	'lib_PHPExcel' => '0.0.0',
