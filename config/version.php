0afa717a0717a819767522f32d264abe94648682
Adrian Koń <a.kon@yetiforce.com>
2017-01-12 09:19:57 +0100

    Updated roudcube version

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '3.4.1443',
+	'appVersion' => '3.4.1444',
 	'patchVersion' => '2017.01.12',
 	'lib_mPDF' => '0.0.0',
-	'lib_roundcube' => '0.0.15',
+	'lib_roundcube' => '0.0.16',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.0',
 	'lib_gantt' => '0.0.0',
