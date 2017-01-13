4b5a3943d6c1c4b16a1a33177d9b49285f1be66c
Tomasz Kur <t.kur@yetiforce.com>
2017-01-13 15:41:10 +0100

    Changed number version for roundcube

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '3.4.1472',
+	'appVersion' => '3.4.1473',
 	'patchVersion' => '2017.01.13',
 	'lib_mPDF' => '0.0.0',
-	'lib_roundcube' => '0.0.16',
+	'lib_roundcube' => '0.0.17',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.0',
 	'lib_gantt' => '0.0.0',
