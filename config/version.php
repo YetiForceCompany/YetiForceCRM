48b9dcf21896ef1a38ec8b1549d01a3e348f5a27
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-01-19 16:30:06 +0100

    Improved authres_status

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '3.4.1561',
+	'appVersion' => '3.4.1562',
 	'patchVersion' => '2017.01.19',
 	'lib_mPDF' => '0.0.0',
-	'lib_roundcube' => '0.0.21',
+	'lib_roundcube' => '0.0.22',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.0',
 	'lib_gantt' => '0.0.0',
