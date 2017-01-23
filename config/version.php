a9b76e9ddbbc7b0561db8887232f7bc4bb9afdff
Tomasz Kur <t.kur@yetiforce.com>
2017-01-23 13:04:40 +0100

    Changed version

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '3.4.1603',
+	'appVersion' => '3.4.1604',
 	'patchVersion' => '2017.01.23',
 	'lib_mPDF' => '0.0.0',
-	'lib_roundcube' => '0.0.22',
+	'lib_roundcube' => '0.0.23',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.0',
 	'lib_gantt' => '0.0.0',
