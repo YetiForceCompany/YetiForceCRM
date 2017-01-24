e5bca4f2982c02f64607b7b1973d1c44b44772ba
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-01-24 22:12:43 +0100

    Added to the composer library phpmailer/phpmailer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -48,6 +48,24 @@ class ComposerAutoloaderInitce7aec416bba2e91816da8458449b895
 
 		$loader->register(true);
 
+		if ($useStaticLoader) {
+			$includeFiles = Composer\Autoload\ComposerStaticInitce7aec416bba2e91816da8458449b895::$files;
+		} else {
+			$includeFiles = require __DIR__ . '/autoload_files.php';
+		}
+		foreach ($includeFiles as $fileIdentifier => $file) {
+			composerRequirece7aec416bba2e91816da8458449b895($fileIdentifier, $file);
+		}
+
 		return $loader;
+	}
+}
+
+function composerRequirece7aec416bba2e91816da8458449b895($fileIdentifier, $file)
+{
+	if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+		require $file;
+
+		$GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 	}
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -75,10 +75,14 @@ class ComposerStaticInitce7aec416bba2e91816da8458449b895
 		),
 	);
 	public static $classMap = array(
-		'PHPMailer' => __DIR__ . '/..' . '/PHPMailer/class.phpmailer.php',
-		'POP3' => __DIR__ . '/..' . '/PHPMailer/class.pop3.php',
-		'SMTP' => __DIR__ . '/..' . '/PHPMailer/class.smtp.php',
-		'phpmailerException' => __DIR__ . '/..' . '/PHPMailer/class.phpmailer.php',
+		'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
+		'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+		'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
+		'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
+		'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
+		'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
+		'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
+		'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
 	);
 
 	public static function getInitializer(ClassLoader $loader)
