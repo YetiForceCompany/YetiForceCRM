0a10c72e4ca027c8e91fdce5ff79362ac719b698
Tomasz Kur <t.kur@yetiforce.com>
2017-01-24 10:47:00 +0100

    Added blinking button when you have unread messages

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return [
-	'appVersion' => '3.4.1611',
+	'appVersion' => '3.4.1612',
 	'patchVersion' => '2017.01.24',
 	'lib_mPDF' => '0.0.0',
 	'lib_roundcube' => '0.0.23',
 	'lib_PHPExcel' => '0.0.0',
-	'lib_AJAXChat' => '0.0.0',
+	'lib_AJAXChat' => '0.0.1',
 	'lib_gantt' => '0.0.0',
 ];
