24f7d657b994aab49417f2525f3bf409e01f6539
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2017-01-25 14:00:10 +0100

    Added minor improvements to webservice

--- api/webservice/Portal/BaseModule/Privileges.php
+++ api/webservice/Portal/BaseModule/Privileges.php
@@ -6,6 +6,7 @@ namespace Api\Portal\BaseModule;
  * @package YetiForce.WebserviceAction
  * @license licenses/License.html
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
  */
 class Privileges extends \Api\Core\BaseAction
 {
@@ -20,13 +21,17 @@ class Privileges extends \Api\Core\BaseAction
 	public function get()
 	{
 		$moduleName = $this->controller->request->get('module');
-		$moduleId = \App\Module::getModuleId($moduleName);
-		$actionPermissions = \App\User::getPrivilegesFile($this->session->get('user_id'));
-		$permission = isset($actionPermissions['profile_action_permission'][$moduleId]) ? $actionPermissions['profile_action_permission'][$moduleId] : false;
+		$userId = $this->session->get('user_id');
 		$privileges = [];
-		if ($permission) {
-			foreach (\Vtiger_Action_Model::$standardActions as $key => $value) {
-				$privileges[$value] = isset($permission[$key]) && $permission[$key] === \Settings_Profiles_Module_Model::IS_PERMITTED_VALUE;
+		if (\App\User::isExists($userId)) {
+			$moduleId = \App\Module::getModuleId($moduleName);
+			$actionPermissions = \App\User::getPrivilegesFile($userId);
+			$isAdmin = $actionPermissions['is_admin'];
+			$permission = isset($actionPermissions['profile_action_permission'][$moduleId]) ? $actionPermissions['profile_action_permission'][$moduleId] : false;
+			if ($permission || $isAdmin) {
+				foreach (\Vtiger_Action_Model::$standardActions as $key => $value) {
+					$privileges[$value] = $isAdmin ? true : isset($permission[$key]) && $permission[$key] === \Settings_Profiles_Module_Model::IS_PERMITTED_VALUE;
+				}
 			}
 		}
 		return ['standardActions' => $privileges];
