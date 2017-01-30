ad26d94dfa408c0f65abd32b28f16962619c3dc6
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-01-30 12:52:42 +0100

    Fixed https://github.com/mpdf/mpdf/issues/294

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 return [
-	'appVersion' => '3.4.1687',
+	'appVersion' => '3.4.1688',
 	'patchVersion' => '2017.01.30',
-	'lib_mPDF' => '0.0.1',
+	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.23',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.2',
