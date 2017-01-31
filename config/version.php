a81170a638abf55a84b041cc96be389082d514e8
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-01-31 08:28:30 +0100

    Added parsing of variables in the mail

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '3.4.1703',
+	'appVersion' => '3.4.1704',
 	'patchVersion' => '2017.01.31',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.23',
+	'lib_roundcube' => '0.0.24',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.2',
 	'lib_gantt' => '0.0.0',
