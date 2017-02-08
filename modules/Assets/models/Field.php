5e4d477dd6d082d77350893cab94a2cc02aec335
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-02-08 23:35:33 +0100

    Fixed write permissions fields

--- modules/Assets/models/Field.php
+++ modules/Assets/models/Field.php
@@ -9,31 +9,6 @@
 class Assets_Field_Model extends Vtiger_Field_Model
 {
 
-	/**
-	 * Function to check whether the current field is editable
-	 * @return boolean - true/false
-	 */
-	public function isEditable()
-	{
-
-		$notEditableFields = $this->getEditabeFields();
-		if (!$this->isEditEnabled() ||
-			( ((int) $this->get('displaytype')) != 1 && ((int) $this->get('displaytype')) != 10 ) ||
-			$this->isReadOnly() === true || $this->get('uitype') == 4 || in_array($this->get('column'), $notEditableFields)) {
-			return false;
-		}
-		return true;
-	}
-
-	/**
-	 * Function that returns uneditable fields
-	 * @return <array>
-	 */
-	public function getEditabeFields()
-	{
-		return ['sum_time'];
-	}
-
 	public function isAjaxEditable()
 	{
 		$edit = parent::isAjaxEditable();
