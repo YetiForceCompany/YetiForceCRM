8351b5999884b2559a5e7b75b69d016888142f84
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2017-02-08 10:12:50 +0100

    Fixed #3897

--- modules/Contacts/actions/Save.php
+++ modules/Contacts/actions/Save.php
@@ -6,11 +6,16 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
+ * Contributor(s): YetiForce.com
  * *********************************************************************************** */
 
 class Contacts_Save_Action extends Vtiger_Save_Action
 {
 
+	/**
+	 * Process
+	 * @param Vtiger_Request $request
+	 */
 	public function process(Vtiger_Request $request)
 	{
 		$result = Vtiger_Util_Helper::transformUploadedFiles($_FILES, true);
@@ -22,5 +27,21 @@ class Contacts_Save_Action extends Vtiger_Save_Action
 			$request->set('salutationtype', '');
 		}
 		parent::process($request);
+	}
+
+	/**
+	 * Function to get the record model based on the request parameters
+	 * @param Vtiger_Request $request
+	 * @return Vtiger_Record_Model or Module specific Record Model instance
+	 */
+	protected function getRecordModelFromRequest(Vtiger_Request $request)
+	{
+		$recordModel = parent::getRecordModelFromRequest($request);
+		$fieldModel = $recordModel->getModule()->getField('salutationtype');
+		if ($fieldModel && $request->has($fieldModel->getName())) {
+			$fieldName = $fieldModel->getName();
+			$recordModel->set($fieldName, $fieldModel->getUITypeModel()->getDBValue($request->get($fieldName, null), $recordModel));
+		}
+		return $recordModel;
 	}
 }
