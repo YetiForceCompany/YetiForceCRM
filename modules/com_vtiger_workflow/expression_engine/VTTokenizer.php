0fa53f992a4bb2827b125d9f566fde70ee85620a
Tomasz Kur <t.kur@yetiforce.com>
2017-02-09 19:47:14 +0100

    Fixed #3980

--- modules/com_vtiger_workflow/expression_engine/VTTokenizer.php
+++ modules/com_vtiger_workflow/expression_engine/VTTokenizer.php
@@ -32,6 +32,7 @@ class VTExpressionTokenizer
 
 	function __construct($expr)
 	{
+		$expr = decode_html($expr);
 		$tokenTypes = array(
 			"SPACE" => array('\s+', '_vt_processtoken_id'),
 			"SYMBOL" => array('[a-zA-Z][\w]*', '_vt_processtoken_symbol'),
