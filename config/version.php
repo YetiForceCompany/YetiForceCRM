f505d5791ba9f0c80fc4ba6ca6aaa8e8a8a5cde2
Adrian Koń <a.kon@yetiforce.com>
2017-02-09 07:54:55 +0100

    Improved translations

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '3.4.1773',
-	'patchVersion' => '2017.02.08',
+	'appVersion' => '3.4.1774',
+	'patchVersion' => '2017.02.09',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.24',
 	'lib_PHPExcel' => '0.0.0',
--- languages/pl_pl/Settings/SystemWarnings.php
+++ languages/pl_pl/Settings/SystemWarnings.php
@@ -10,11 +10,11 @@ $languageStrings = [
 	'LBL_LIBRARY_CHAT' => 'Biblioteka AJAXChat',
 	'LBL_CONFIG_REPORT' => 'Konfiguracja serwera',
 	'LBL_CONFIG_REPORT_LINK' => 'Pokaż brakujacą konfigurację',
-	'LBL_CONFIG_REPORT_DESC' => 'Konfiguracja serwera nie została poprawie ustawiona. Mogą wystąpić problemy z działaniem systemu lub wprowadzane dane mogą zostać uszkodzone lub usunięte. Więcej informacji znajduje sie w artykule <a target="_blank" href="https://yetiforce.com/pl/wdro%C5%BCeniowiec/instalacja-aktualizacja/99-wymagania-dla-serwera-www.html"><u>Wymagania dla serwera WWW na stronie yetiforce.com</u></a>',
+	'LBL_CONFIG_REPORT_DESC' => 'Konfiguracja serwera nie została poprawnie ustawiona. Mogą wystąpić problemy z działaniem systemu lub wprowadzane dane mogą zostać uszkodzone lub usunięte. Więcej informacji znajduje sie w artykule <a target="_blank" href="https://yetiforce.com/pl/wdro%C5%BCeniowiec/instalacja-aktualizacja/99-wymagania-dla-serwera-www.html"><u>Wymagania dla serwera WWW na stronie yetiforce.com</u></a>',
 	'LBL_NEWSLETTER' => 'Newsletter',
 	'LBL_NEWSLETTER_DESC' => 'Chcesz wiedzieć co się dzieje w świecie Yeti, nie chcesz przegapić nowej wersji i waznych aktualizacji, chcesz orzymywać wiadomości promocyjne i nowości.<br>Zapisz się do naszego newsletter-a a będziesz pierwszy wiedział o nowościach w systemie CRM i nie tylko. Wysłanie danych wymaga połączenia z internetem.',
 	'LBL_STATS' => 'Wyślij informacje statystyczne',
-	'LBL_STATS_DESC' => 'Zbieramy informację o wszystkich używanych wersjach systemu YetiForce CRM. Statystyki są w pełni anonimowe, nie wysyłamy twojich prywatnych danych. Wysyłanmy następujące informacje:<ul><li>unikalny klucz twojej instancji</li><li>wersja systemu</li><li>kraj</li></ul>Przyłącz się do budwania społeczności yeti.',
+	'LBL_STATS_DESC' => 'Zbieramy informację o wszystkich używanych wersjach systemu YetiForce CRM. Statystyki są w pełni anonimowe, nie wysyłamy twojich prywatnych danych. Wysyłamy następujące informacje:<ul><li>unikalny klucz twojej instancji</li><li>wersja systemu</li><li>kraj</li></ul>Przyłącz się do budwania społeczności yeti.',
 	// Alerts
 	'LBL_SAVE_TO_NEWSLETTER' => 'Zapisz się do newslettera',
 	'LBL_REMIND_LATER' => 'Przypomnij później ',
