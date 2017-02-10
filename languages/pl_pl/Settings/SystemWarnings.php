6d19f2280fdb83a2a9fa6366e68af96615fe3c89
Paula W <p.walenczak@yetiforce.com>
2017-02-10 09:55:34 +0100

    Improved translation

--- languages/pl_pl/Settings/SystemWarnings.php
+++ languages/pl_pl/Settings/SystemWarnings.php
@@ -9,15 +9,15 @@ $languageStrings = [
 	'LBL_LIBRARY_PHPEXCEL' => 'Biblioteka PHPExcel',
 	'LBL_LIBRARY_CHAT' => 'Biblioteka AJAXChat',
 	'LBL_CONFIG_REPORT' => 'Konfiguracja serwera',
-	'LBL_CONFIG_REPORT_LINK' => 'Pokaż brakujacą konfigurację',
-	'LBL_CONFIG_REPORT_DESC' => 'Konfiguracja serwera nie została poprawnie ustawiona. Mogą wystąpić problemy z działaniem systemu lub wprowadzane dane mogą zostać uszkodzone lub usunięte. Więcej informacji znajduje sie w artykule <a target="_blank" href="https://yetiforce.com/pl/wdro%C5%BCeniowiec/instalacja-aktualizacja/99-wymagania-dla-serwera-www.html"><u>Wymagania dla serwera WWW na stronie yetiforce.com</u></a>',
+	'LBL_CONFIG_REPORT_LINK' => 'Pokaż brakującą konfigurację',
+	'LBL_CONFIG_REPORT_DESC' => 'Konfiguracja serwera nie została poprawnie ustawiona. Mogą wystąpić problemy z działaniem systemu, lub wprowadzane dane mogą zostać uszkodzone, lub usunięte. Więcej informacji znajduje się w artykule <a target="_blank" href="https://yetiforce.com/pl/wdro%C5%BCeniowiec/instalacja-aktualizacja/99-wymagania-dla-serwera-www.html"><u>Wymagania dla serwera WWW na stronie yetiforce.com</u></a>',
 	'LBL_NEWSLETTER' => 'Newsletter',
 	'LBL_NEWSLETTER_DESC' => 'Zapisz się do darmowego newslettera YetiForce. Bądź na bieżąco z nowościami w systemie i poznaj lepiej branżę CRM. Będziemy dzielić się z Tobą informacjami o nowych wersjach, aktualizacjach, wydarzeniach, nowinkach w systemie i nie tylko.<br>Przyłącz się do naszej społeczności i nie pozostawaj w tyle.<br>Wysłanie zgłoszenia wymaga połączenia z Internetem.',
 	'LBL_STATS' => 'Wyślij informacje statystyczne',
 	'LBL_STATS_DESC' => 'Pomóż nam ulepszać YetiForce i oferowane przez nas usługi poprzez przesyłanie nam anonimowych informacji dotyczących używanej przez Ciebie wersji systemu.<br>Zbierane przez nas dane obejmują poniższe informacje: <ul><li>unikalny klucz twojej instancji generowany tylko na potrzeby newslettera</li><li>wersja systemu</li><li>język</li><li>strefa czasowa</li></ul>Kwestie bezpieczeństwa traktujemy niezwykle poważnie i zapewniamy, że dane zbierane są wyłącznie w celach statystycznych.<br>Przyłącz się do budowania społeczności YetiForce.',
 	// Alerts
 	'LBL_SAVE_TO_NEWSLETTER' => 'Zapisz się do newslettera',
-	'LBL_REMIND_LATER' => 'Przypomnij później ',
+	'LBL_REMIND_LATER' => 'Przypomnij później',
 	// Others
 	'BTN_SET_IGNORE' => 'Ignoruj',
 	'BTN_REMOVE_IGNORE' => 'Usuń z ignorowanych',
