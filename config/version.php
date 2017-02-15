3a269fe4e96308686ebb2437b12a063043181b78
Adrian Koń <a.kon@yetiforce.com>
2017-02-15 10:30:00 +0100

    Fixed #3905

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.0.0',
-	'patchVersion' => '2017.02.14',
+	'appVersion' => '4.0.1',
+	'patchVersion' => '2017.02.15',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.24',
 	'lib_PHPExcel' => '0.0.0',
