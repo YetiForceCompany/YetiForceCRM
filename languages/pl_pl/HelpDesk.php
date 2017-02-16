d1389ce4b573d2b970626c3e61ec3c1b1366b2af
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2017-02-16 09:11:05 +0100

    Improved translations

--- languages/pl_pl/HelpDesk.php
+++ languages/pl_pl/HelpDesk.php
@@ -11,7 +11,7 @@
 $languageStrings = [
 	// Fields
 	'FL_SERVICE_CONTRACTS' => 'Umowa wsparcia SLA',
-	'FL_SERVICE_CONTRACTS_TYPE' => 'Rodzaj umowy wsparcia SLA',
+	'FL_SERVICE_CONTRACTS_TYPE' => 'Rodzaj SLA',
 	'FL_SERVICE_CONTRACTS_DATE' => 'Data zakończenia wsparcia SLA',
 	'FL_REPORT_TIME' => 'Czas zgłoszenia ',
 	'FL_RESPONSE_TIME' => 'Czas odpowiedzi',
@@ -100,4 +100,7 @@ $languageStrings = [
 	'LBL_NOTICE_MODIFICATION' => 'Modyfikacja',
 	'LBL_NOTICE_CLOSE' => 'Zamknięto',
 	'LBL_NOTICE_FOOTER' => 'Jeżeli nie chcesz otrzymywać powiadomień z naszego systemu do zarządzania projektów, poinformuj nas o tym wysyłając nam wiadomość na adres <a href="mailto:help@yetiforce.com" target="_blank">help@yetiforce.com</a>. Jeżeli nie jesteś adresatem tej wiadomości poinformuj nas o tym i usuń tą wiadomość.',
+	'Services' => 'Usługi',
+	'Support' => 'Wsparcie',
+	'Administrative' => 'Administracja',
 ];
