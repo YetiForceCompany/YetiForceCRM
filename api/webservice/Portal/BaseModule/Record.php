da92a93eec2597ef8831e01c952421364f88449e
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2017-02-17 14:03:20 +0100

    Added minor improvements to webservice

--- api/webservice/Portal/BaseModule/Record.php
+++ api/webservice/Portal/BaseModule/Record.php
@@ -15,6 +15,55 @@ class Record extends \Api\Core\BaseAction
 	public $allowedMethod = ['GET', 'DELETE', 'PUT', 'POST'];
 
 	/**
+	 * Record model
+	 * @var \Vtiger_Record_Model 
+	 */
+	protected $recordModel = false;
+
+	/**
+	 * Check permission to method
+	 * @return boolean
+	 * @throws \Api\Core\Exception
+	 */
+	public function checkPermission()
+	{
+		parent::checkPermission();
+		$moduleName = $this->controller->request->get('module');
+		$method = $this->controller->method;
+		if ('POST' === $method) {
+			$this->recordModel = \Vtiger_Record_Model::getCleanInstance($moduleName);
+			if (!$this->recordModel->isCreateable()) {
+				throw new \Api\Core\Exception('No permissions to create record', 401);
+			}
+		} else {
+			$record = $this->controller->request->get('record');
+			if (!$record || !\App\Record::isExists($record, $moduleName)) {
+				throw new \Api\Core\Exception("Record doesn't exist", 401);
+			}
+			$this->recordModel = \Vtiger_Record_Model::getInstanceById($record, $moduleName);
+			switch ($method) {
+				case 'DELETE':
+					if (!$this->recordModel->isDeletable()) {
+						throw new \Api\Core\Exception('No permissions to remove record', 401);
+					}
+					break;
+				case 'GET':
+					if (!$this->recordModel->isViewable()) {
+						throw new \Api\Core\Exception('No permissions to view record', 401);
+					}
+					break;
+				case 'PUT':
+					if (!$this->recordModel->isEditable()) {
+						throw new \Api\Core\Exception('No permissions to edit record', 401);
+					}
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	/**
 	 * Get record detail
 	 * @return array
 	 */
@@ -22,7 +71,7 @@ class Record extends \Api\Core\BaseAction
 	{
 		$moduleName = $this->controller->request->get('module');
 		$record = $this->controller->request->get('record');
-		$recordModel = \Vtiger_Record_Model::getInstanceById($record, $moduleName);
+		$recordModel = $this->recordModel;
 		$rawData = $recordModel->getData();
 		$moduleModel = $recordModel->getModule();
 
@@ -78,15 +127,8 @@ class Record extends \Api\Core\BaseAction
 	 */
 	public function delete()
 	{
-		$moduleName = $this->controller->request->get('module');
-		$record = $this->controller->request->get('record');
-		$recordModel = \Vtiger_Record_Model::getInstanceById($record, $moduleName);
-		$status = false;
-		if ($recordModel->isDeletable()) {
-			$recordModel->delete();
-			$status = true;
-		}
-		return $status;
+		$this->recordModel->delete();
+		return true;
 	}
 
 	/**
@@ -97,24 +139,12 @@ class Record extends \Api\Core\BaseAction
 	{
 		$recordData = $this->controller->request->get('recordData');
 		$moduleName = $this->controller->request->get('module');
-		$record = $this->controller->request->get('record');
 		$result = false;
-		if (\App\Record::isExists($record, $moduleName)) {
-			$recordModel = \Vtiger_Record_Model::getInstanceById($record, $moduleName);
-			foreach ($recordData as $key => $value) {
-				$recordModel->set($key, $value);
-			}
-			if ($recordModel->isEditable()) {
-				$recordModel->save();
-				$result = true;
-			} else {
-				$message = \App\Language::translate('Permission to perform the operation is denied');
-			}
-		} else {
-			$message = \App\Language::translate('Permission to perform the operation is denied');
-		}
-
-		return ['id' => $record, 'result' => $result, 'message' => $message];
+		$newRequest = new \Vtiger_Request($recordData);
+		$modelClassName = \Vtiger_Loader::getComponentClassName('Action', 'Save', $moduleName);
+		$saveClass = new $modelClassName();
+		$recordModel = $saveClass->saveRecord($newRequest);
+		return ['id' => $recordModel->getId()];
 	}
 
 	/**
@@ -125,15 +155,10 @@ class Record extends \Api\Core\BaseAction
 	{
 		$recordData = $this->controller->request->get('recordData');
 		$moduleName = $this->controller->request->get('module');
-		$recordModel = \Vtiger_Record_Model::getCleanInstance($moduleName);
-		foreach ($recordData as $key => $value) {
-			$recordModel->set($key, $value);
-		}
-		if ($recordModel->isCreateable()) {
-			$recordModel->save();
-		} else {
-			$message = \App\Language::translate('Permission to perform the operation is denied');
-		}
-		return ['id' => $recordModel->getId(), 'result' => (bool) $recordModel->getId(), 'message' => $message];
+		$newRequest = new \Vtiger_Request($recordData);
+		$modelClassName = \Vtiger_Loader::getComponentClassName('Action', 'Save', $moduleName);
+		$saveClass = new $modelClassName();
+		$recordModel = $saveClass->saveRecord($newRequest);
+		return ['id' => $recordModel->getId()];
 	}
 }
