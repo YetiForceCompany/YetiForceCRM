b4fba484a6212eb112b3396eb18477622579091c
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-02-23 10:43:21 +0100

    Updated Thunderbird Labels Plugin for Roundcube Webmail

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.0.49',
+	'appVersion' => '4.0.50',
 	'patchVersion' => '2017.02.23',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.24',
+	'lib_roundcube' => '0.0.25',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.2',
 	'lib_gantt' => '0.0.0',
