1eb59e9f9590a4563b38dc732a57768b9c988f50
Alex Weber <migoi.snow@gmail.com>
2017-02-25 11:42:04 +0100

    ensure var is set

--- vendor/yetiforce/PrivilegeUtil.php
+++ vendor/yetiforce/PrivilegeUtil.php
@@ -506,7 +506,10 @@ class PrivilegeUtil
 	{
 		$modTabId = \App\Module::getModuleId($module);
 		$modShareWritePermission = $modShareReadPermission = ['ROLE' => [], 'GROUP' => []];
-		$modDefOrgShare = $defOrgShare[$modTabId];
+		$modDefOrgShare = null;
+		if (isset($defOrgShare[$modTabId])) {
+			$modDefOrgShare = $defOrgShare[$modTabId];
+		}
 		$shareIdMembers = [];
 		//If Sharing of leads is Private
 		if ($modDefOrgShare === 3 || $modDefOrgShare === 0) {
