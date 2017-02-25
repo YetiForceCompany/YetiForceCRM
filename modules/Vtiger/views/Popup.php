41d47dfcc83e7dcb983d41309f741a9208c3d51f
Alex Weber <migoi.snow@gmail.com>
2017-02-25 15:52:07 +0100

    also check for variable is set

--- modules/Vtiger/views/Popup.php
+++ modules/Vtiger/views/Popup.php
@@ -182,7 +182,7 @@ class Vtiger_Popup_View extends Vtiger_Footer_View
 		}
 
 		// If there are no related records with parent module then, we should show all the records
-		if (!$parentRelatedRecords && !empty($relatedParentModule) && !empty($relatedParentId)) {
+		if (empty($parentRelatedRecords) && !empty($relatedParentModule) && !empty($relatedParentId)) {
 			$relatedParentModule = null;
 			$relatedParentId = null;
 			$listViewModel = Vtiger_ListView_Model::getInstanceForPopup($moduleName, $sourceModule);
