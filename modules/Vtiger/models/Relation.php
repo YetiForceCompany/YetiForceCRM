e621e0c4589027da84a831060815e5656b77b713
Alex Weber <migoi.snow@gmail.com>
2017-02-25 15:44:30 +0100

    fix: this method takes no parameters and check before use

--- modules/Vtiger/models/Relation.php
+++ modules/Vtiger/models/Relation.php
@@ -347,10 +347,12 @@ class Vtiger_Relation_Model extends Vtiger_Base_Model
 	 */
 	public function getDependentsList()
 	{
-		$fieldModel = $this->getRelationField(true);
-		$this->getQueryGenerator()->addNativeCondition([
-			$fieldModel->getTableName() . '.' . $fieldModel->getColumnName() => $this->get('parentRecord')->getId()
-		]);
+		$fieldModel = $this->getRelationField();
+		if (!empty($fieldModel)) {
+			$this->getQueryGenerator()->addNativeCondition([
+				$fieldModel->getTableName() . '.' . $fieldModel->getColumnName() => $this->get('parentRecord')->getId()
+			]);
+		}
 	}
 
 	/**
