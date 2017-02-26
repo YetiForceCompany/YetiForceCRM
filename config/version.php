164024b26594e0536d55995688b5b1cbaea8b7cc
Alex Weber <migoi.snow@gmail.com>
2017-02-26 14:38:06 +0100

    added missing language files

--- config/version.php
+++ config/version.php
@@ -3,7 +3,7 @@ return [
 	'appVersion' => '4.0.58',
 	'patchVersion' => '2017.02.25',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.25',
+	'lib_roundcube' => '0.0.24',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.2',
 	'lib_gantt' => '0.0.0',
