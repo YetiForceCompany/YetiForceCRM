1fd6c928da8185684527b9debc0a17f936b12563
Alex Weber <migoi.snow@gmail.com>
2017-03-04 11:31:49 +0100

    restore current numbers

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.0.58',
-	'patchVersion' => '2017.02.25',
+	'appVersion' => '4.0.85',
+	'patchVersion' => '2017.03.04',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.25',
+	'lib_roundcube' => '0.0.26',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.2',
 	'lib_gantt' => '0.0.0',
