c981ec2b520d189361fda15dc01955a042d8b25f
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-03-17 16:23:57 +0100

    Enhanced prefix support in mail scanners

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.0.103',
-	'patchVersion' => '2017.03.16',
+	'appVersion' => '4.0.104',
+	'patchVersion' => '2017.03.17',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.26',
+	'lib_roundcube' => '0.0.27',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.2',
 	'lib_gantt' => '0.0.0',
