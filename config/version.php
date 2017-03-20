3a6b100b012f86905377062e40990192d8bf8ad5
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-03-20 15:26:50 +0100

    Improved appearance of integrating bar in mail

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.0.129',
+	'appVersion' => '4.0.130',
 	'patchVersion' => '2017.03.20',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.28',
+	'lib_roundcube' => '0.0.29',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.3',
 	'lib_gantt' => '0.0.0',
