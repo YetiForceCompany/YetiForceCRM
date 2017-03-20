2c7569a78d9b7af95b1786c74f230a4f30e72992
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-03-20 13:42:41 +0100

    Improved mail scanner

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.0.124',
-	'patchVersion' => '2017.03.18',
+	'appVersion' => '4.0.125',
+	'patchVersion' => '2017.03.20',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.27',
 	'lib_PHPExcel' => '0.0.0',
