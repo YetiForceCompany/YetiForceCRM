167f90d1196f94c66a03513c1351aaf201c76033
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-03-20 13:47:43 +0100

    Improved email address search

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.0.127',
+	'appVersion' => '4.0.128',
 	'patchVersion' => '2017.03.20',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.27',
+	'lib_roundcube' => '0.0.28',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.2',
 	'lib_gantt' => '0.0.0',
