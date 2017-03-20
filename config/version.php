833276650502e1b279bba1ddfbd77c40ff5e2390
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-03-20 14:26:48 +0100

    Improved chat

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return [
-	'appVersion' => '4.0.128',
+	'appVersion' => '4.0.129',
 	'patchVersion' => '2017.03.20',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.28',
 	'lib_PHPExcel' => '0.0.0',
-	'lib_AJAXChat' => '0.0.2',
+	'lib_AJAXChat' => '0.0.3',
 	'lib_gantt' => '0.0.0',
 ];
