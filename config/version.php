a3427214533daac6c36e504120bb0b6051aef88f
Tomasz Kur <t.kur@yetiforce.com>
2017-03-23 08:27:29 +0100

    Added blinking button when you receive a new message

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return [
-	'appVersion' => '4.0.147',
-	'patchVersion' => '2017.03.22',
+	'appVersion' => '4.0.148',
+	'patchVersion' => '2017.03.23',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.29',
 	'lib_PHPExcel' => '0.0.0',
-	'lib_AJAXChat' => '0.0.3',
+	'lib_AJAXChat' => '0.0.4',
 	'lib_gantt' => '0.0.0',
 ];
