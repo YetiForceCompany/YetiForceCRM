2826902822dabb46e7be6fd00b64ab79f33d3f9c
Tomasz Kur <t.kur@yetiforce.com>
2017-03-27 21:02:58 +0200

    Fixed #4325

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.0.153',
-	'patchVersion' => '2017.03.26',
+	'appVersion' => '4.0.154',
+	'patchVersion' => '2017.03.27',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.29',
 	'lib_PHPExcel' => '0.0.0',
