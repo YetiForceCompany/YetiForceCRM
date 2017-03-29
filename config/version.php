f1a5ab75025f5ea9edbe20aba2762e037bc6dcf0
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-03-29 23:12:06 +0200

    Fixed permissions for groups

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.0.155',
-	'patchVersion' => '2017.03.27',
+	'appVersion' => '4.0.156',
+	'patchVersion' => '2017.03.29',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.29',
 	'lib_PHPExcel' => '0.0.0',
