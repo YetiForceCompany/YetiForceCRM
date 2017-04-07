d42bf2121b6f3280cf6621a8f6568b79da158db5
Michał Lorencik <michal11991@gmail.com>
2017-04-07 16:20:05 +0200

    Added new version to commit before

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.0.175',
-	'patchVersion' => '2017.04.06',
+	'appVersion' => '4.0.176',
+	'patchVersion' => '2017.04.07',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.29',
 	'lib_PHPExcel' => '0.0.0',
