4d4da0fe4d81e7f4252b63acf6d1dd8d096f03ed
Sławomir Kłos <s.klos@yetiforce.com>
2017-04-26 17:33:17 +0200

    Fixed #4375

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '4.0.203',
+	'appVersion' => '4.0.204',
 	'patchVersion' => '2017.04.26',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.29',
--- modules/Vtiger/models/Record.php
+++ modules/Vtiger/models/Record.php
@@ -451,8 +451,9 @@ class Vtiger_Record_Model extends Vtiger_Base_Model
 			$row['users'] = ',' . \App\User::getCurrentUserId() . ',';
 			$this->set('createdtime', $time);
 		}
-		if (!empty($this->valueMap['modifiedtime'])) {
-			$time = $this->valueMap['modifiedtime'];
+
+		if ($this->getPreviousValue('modifiedtime')) {
+			$time = $this->getPreviousValue('modifiedtime');
 		}
 		$row['modifiedtime'] = $time;
 		$row['modifiedby'] = \App\User::getCurrentUserRealId();
