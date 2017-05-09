bdc18d90bbe7262bc9e8f51686c5bd8c006d1d8b
Arkadiusz Sołek <a.solek@yetiforce.com>
2017-05-09 11:16:58 +0200

    License updates

--- api/webservice/Portal/BaseModule/Record.php
+++ api/webservice/Portal/BaseModule/Record.php
@@ -4,7 +4,8 @@ namespace Api\Portal\BaseModule;
 /**
  * Get record detail class
  * @package YetiForce.WebserviceAction
- * @license licenses/License.html
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
  */
--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.0.235',
-	'patchVersion' => '2017.05.08',
+	'appVersion' => '4.0.236',
+	'patchVersion' => '2017.05.09',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.29',
 	'lib_PHPExcel' => '0.0.0',
