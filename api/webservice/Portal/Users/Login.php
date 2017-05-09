5ab91df9d5d11365cf7438191a1c33fe3ed74231
Sławomir Kłos <s.klos@yetiforce.com>
2017-05-09 12:36:02 +0200

    More user profile info in webservice

--- api/webservice/Portal/Users/Login.php
+++ api/webservice/Portal/Users/Login.php
@@ -69,6 +69,8 @@ class Login extends \Api\Core\BaseAction
 				'hour_format' => $userModel->getDetail('hour_format'),
 				'start_hour' => $userModel->getDetail('start_hour'),
 				'date_format' => $userModel->getDetail('date_format'),
+				'date_format_js' => \App\Fields\DateTime::currentUserJSDateFormat($userModel->getDetail('date_format')),
+				'dayoftheweek' => $userModel->getDetail('dayoftheweek'),
 				'time_zone' => $userModel->getDetail('time_zone'),
 				'currency_id' => $userModel->getDetail('currency_id'),
 				'currency_grouping_pattern' => $userModel->getDetail('currency_grouping_pattern'),
