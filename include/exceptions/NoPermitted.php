4b3ff3ec6e1d7ca69a1513c2f48c9ba2efec3f24
Arkadiusz Sołek <a.solek@yetiforce.com>
2017-05-15 15:59:41 +0200

    Changed version of the license

--- include/exceptions/NoPermitted.php
+++ include/exceptions/NoPermitted.php
@@ -1,9 +1,11 @@
-<?php namespace Exception;
+<?php
+namespace Exception;
 
 /**
- * No Permitted Exception class
+ * No permitted exception class
  * @package YetiForce.Exception
- * @license licenses/License.html
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
 class NoPermitted extends \Exception
--- include/exceptions/NoPermittedToRecord.php
+++ include/exceptions/NoPermittedToRecord.php
@@ -2,9 +2,10 @@
 namespace Exception;
 
 /**
- * No Permitted Exception class
+ * No Permitted to record exception class
  * @package YetiForce.Exception
- * @license licenses/License.html
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
 class NoPermittedToRecord extends NoPermitted
