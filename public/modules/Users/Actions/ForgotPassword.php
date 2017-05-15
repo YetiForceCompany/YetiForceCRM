c582990b0c90f34189bdea1ce1d23f50968edfdf
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-05-15 16:58:13 +0200

    Public directory improved

---
+++ public/modules/Users/Actions/ForgotPassword.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Forgot password public file
+ * @package YetiForce
+ * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ * @copyright YetiForce Sp. z o.o.
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+chdir(dirname(__FILE__) . '/../../../../modules/Users/Actions/');
+
+require 'ForgotPassword.php';
