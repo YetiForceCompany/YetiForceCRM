271395765ed3faed628f85c12e2d3e2e575dc546
Sławomir Kłos <s.klos@yetiforce.com>
2017-05-16 18:48:49 +0200

    Added disabled https warning

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '4.0.272',
+	'appVersion' => '4.0.273',
 	'patchVersion' => '2017.05.16',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.29',
---
+++ vendor/yetiforce/SystemWarnings/SystemRequirements/ServerHttps.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\SystemWarnings\SystemRequirements;
+
+/**
+ * Privilege File basic class
+ * @package YetiForce.SystemWarnings
+ * @license licenses/License.html
+ * @author Sławomir Kłos <s.klos@yetiforce.com>
+ */
+class ServerHttps extends \App\SystemWarnings\Template
+{
+
+	protected $status = 0;
+	protected $title = 'LBL_SERVER_HTTPS';
+	protected $priority = 7;
+
+	/**
+	 * Checking whether there is a https connection
+	 */
+	public function process()
+	{
+		if (isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+			$this->status = 1;
+		} else {
+			$this->status = 0;
+		}
+		if ($this->status === 0) {
+			$this->link = 'https://yetiforce.com/en/implementer/installation-updates/103-web-server-requirements.html';
+			$this->linkTitle = vtranslate('BTN_CONFIGURE_HTTPS', 'Settings:SystemWarnings');
+			$this->description = vtranslate('LBL_MISSING_HTTPS', 'Settings:SystemWarnings', \Settings_ModuleManager_Library_Model::TEMP_DIR);
+		}
+	}
+}
