5eb8d1d3d0032036eff633e5205a529169e9afb2
Arkadiusz Sołek <a.solek@yetiforce.com>
2017-05-16 11:44:14 +0200

    Changed version of the license

--- languages/de_de/Settings/PBXManager.php
+++ languages/de_de/Settings/PBXManager.php
@@ -1,12 +1,10 @@
 <?php
-/* +***********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
- * *********************************************************************************** */
+/**
+ * Pbx manager german translation
+ * @package YetiForce.Languages
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ */
 $languageStrings = [
 	'PBXManager' => 'PBX Manager',
 	'LBL_SELECT_ONE' => 'Auswählen',
--- languages/de_de/Settings/WebserviceUsers.php
+++ languages/de_de/Settings/WebserviceUsers.php
@@ -1,5 +1,10 @@
 <?php
-/* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
+/**
+ * Webservice users german translation
+ * @package YetiForce.Languages
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ */
 $languageStrings = [
 	'WebserviceUsers' => 'Webservice - Users',
 	'LBL_WEBSERVICE_USERS' => 'Webservice - Users',
