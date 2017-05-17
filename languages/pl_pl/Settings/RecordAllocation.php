5b76b43a71840362764c2d1ba2b45f6836c74d47
Arkadiusz Sołek <a.solek@yetiforce.com>
2017-05-17 09:18:42 +0200

    Changed version of the license

--- languages/pl_pl/Settings/RecordAllocation.php
+++ languages/pl_pl/Settings/RecordAllocation.php
@@ -1,5 +1,10 @@
 <?php
-/* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
+/**
+ * RecordAllocation polish translation
+ * @package YetiForce.Languages
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ */
 $languageStrings = [
 	'LBL_OWNER_ALLOCATION' => 'Przydział właściciela rekordu',
 	'LBL_OWNER_ALLOCATION_DESCRIPTION' => ' ',
