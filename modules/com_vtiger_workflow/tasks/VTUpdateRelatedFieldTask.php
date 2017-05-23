32ab5a3650e9fe2a8b9f54b03198d37b03054beb
Arkadiusz Sołek <a.solek@yetiforce.com>
2017-05-23 15:02:33 +0200

    Changed version of the license

--- modules/com_vtiger_workflow/tasks/VTUpdateRelatedFieldTask.php
+++ modules/com_vtiger_workflow/tasks/VTUpdateRelatedFieldTask.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Update Related Field Task Handler Class
- * @package YetiForce.WorkflowTask
+ * @package YetiForce.Workflow
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
