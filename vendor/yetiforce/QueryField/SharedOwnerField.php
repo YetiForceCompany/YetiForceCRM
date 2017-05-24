faa247261d4afe9c26309d742bd7d1ebdc4d0757
Arkadiusz Sołek <a.solek@yetiforce.com>
2017-05-24 10:42:07 +0200

    Changed version of the license

--- vendor/yetiforce/QueryField/SharedOwnerField.php
+++ vendor/yetiforce/QueryField/SharedOwnerField.php
@@ -4,7 +4,8 @@ namespace App\QueryField;
 /**
  * Shared Owner Query Field Class
  * @package YetiForce.App
- * @license licenses/License.html
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
  */
