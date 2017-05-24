559ce012da203bedee2325c46042dbd7dcb087d7
Arkadiusz Sołek <a.solek@yetiforce.com>
2017-05-24 14:29:46 +0200

    Change name @package

--- modules/Calendar/actions/ActivityStateAjax.php
+++ modules/Calendar/actions/ActivityStateAjax.php
@@ -2,7 +2,7 @@
 
 /**
  * Calendar ActivityStateAjax action class
- * @package YetiForce.actions
+ * @package YetiForce.Action
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
--- modules/Settings/LayoutEditor/actions/SaveAjax.php
+++ modules/Settings/LayoutEditor/actions/SaveAjax.php
@@ -2,7 +2,7 @@
 
 /**
  * Save Inventory Action Class
- * @package YetiForce.Actions
+ * @package YetiForce.Action
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
