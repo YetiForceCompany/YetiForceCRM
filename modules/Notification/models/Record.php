e804fe46810810b6962064b4f0deb20442ade95e
Arkadiusz Sołek <a.solek@yetiforce.com>
2017-05-24 16:05:22 +0200

    Change name @package

--- modules/Notification/models/Record.php
+++ modules/Notification/models/Record.php
@@ -2,7 +2,7 @@
 
 /**
  * Notification Record Model
- * @package YetiForce.View
+ * @package YetiForce.Model
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  * @author Tomasz Kur <t.kur@yetiforce.com>
--- modules/OSSTimeControl/uitypes/Double.php
+++ modules/OSSTimeControl/uitypes/Double.php
@@ -2,7 +2,7 @@
 
 /**
  * 
- * @package YetiForce.uitypes
+ * @package YetiForce.Uitype
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
