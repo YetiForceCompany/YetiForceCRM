e05bca49109e47d9acf63900b84d9379533c0eb9
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-05-24 12:54:59 +0200

    Added minor enhancements

--- modules/Settings/ConfReport/models/Module.php
+++ modules/Settings/ConfReport/models/Module.php
@@ -148,10 +148,9 @@ class Settings_ConfReport_Module_Model extends Settings_Vtiger_Module_Model
 			$directiveValues['file_uploads']['status'] = true;
 		$directiveValues['file_uploads']['current'] = self::getFlag(ini_get('file_uploads'));
 
-		if (ini_get('output_buffering') !== 'On' && ini_get('output_buffering') !== '1') {
+		if (ini_get('output_buffering') !== 'On') {
 			$directiveValues['output_buffering']['status'] = true;
 		}
-
 		$directiveValues['output_buffering']['current'] = ini_get('output_buffering');
 
 		if (ini_get('max_execution_time') != 0 && ini_get('max_execution_time') < 600)
