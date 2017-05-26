845bc70ae9d769aeafef1eaac11e92301a796877
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-05-26 23:26:57 +0200

    Improved cron

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '4.0.323',
+	'appVersion' => '4.0.324',
 	'patchVersion' => '2017.05.26',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.30',
--- cron/BatchProcesses.php
+++ cron/BatchProcesses.php
@@ -6,6 +6,7 @@
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
+$disable = true;
 $iterator = new \DirectoryIterator(ROOT_DIRECTORY . DIRECTORY_SEPARATOR . 'cron' . DIRECTORY_SEPARATOR . 'Batch');
 foreach ($iterator as $item) {
 	if ($item->isFile() && $item->getExtension() === 'php') {
@@ -17,5 +18,12 @@ foreach ($iterator as $item) {
 		if ($handler->postProcess()) {
 			unlink($item->getPathname());
 		}
+		$disable = false;
 	}
 }
+if ($disable) {
+	\App\Db::getInstance()->createCommand()->update('vtiger_cron_task', ['status' => 0], ['name' => 'LBL_BATCH_PROCESSES'])
+		->execute();
+}
+
+
