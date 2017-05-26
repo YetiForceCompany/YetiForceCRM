91f30260210707f9921665fd374fd19a918aa284
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-05-26 19:22:26 +0200

    Improved cron

--- public_html/cron/cron.php
+++ public_html/cron/cron.php
@@ -6,6 +6,6 @@
  * @copyright YetiForce Sp. z o.o.
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
-chdir(dirname(__FILE__) . '/../../cron/');
+chdir((__DIR__). '/../../cron/');
 define('IS_PUBLIC_DIR', true);
-require 'cron.php';
+require 'vtigercron.php';
