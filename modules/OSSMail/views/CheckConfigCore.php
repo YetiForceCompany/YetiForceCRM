b3e4120b567f793f0b270730ab3c611c507f4e74
Arkadiusz Sołek <a.solek@yetiforce.com>
2017-06-05 10:55:54 +0200

    Changed version of the license

--- modules/OSSMail/views/CheckConfigCore.php
+++ modules/OSSMail/views/CheckConfigCore.php
@@ -5,15 +5,12 @@
 </style>
 <form action="index.php" method="get">
 	<?php
-	/* +***********************************************************************************************************************************
-	 * The contents of this file are subject to the YetiForce Public License Version 2.0 (the "License"); you may not use this file except
-	 * in compliance with the License.
-	 * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
-	 * See the License for the specific language governing rights and limitations under the License.
-	 * The Original Code is YetiForce.
-	 * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
-	 * All Rights Reserved.
-	 * *********************************************************************************************************************************** */
+	/**
+	 * CheckConfigCore view
+	 * @package YetiForce.View
+	 * @copyright YetiForce Sp. z o.o.
+	 * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+	 */
 	$required_php_exts = array(
 		'PCRE' => 'pcre',
 		'DOM' => 'dom',
