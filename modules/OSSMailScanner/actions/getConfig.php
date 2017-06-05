f45b8430998d44be3b368f03f990deac763ae704
Arkadiusz Sołek <a.solek@yetiforce.com>
2017-06-05 09:31:11 +0200

    Changed version of the license

--- modules/OSSMailScanner/actions/getConfig.php
+++ modules/OSSMailScanner/actions/getConfig.php
@@ -1,6 +1,11 @@
 <?php
-/* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 
+/**
+ * OSSMailScanner getConfig action class
+ * @package YetiForce.Cron
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ */
 class OSSMailScanner_getConfig_Action extends Vtiger_Action_Controller
 {
 
