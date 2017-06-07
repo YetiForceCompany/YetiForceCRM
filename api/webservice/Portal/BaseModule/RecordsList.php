714fa5ba2d2d47c013de10705147ab018b7163d9
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-06-07 22:48:59 +0200

    Fixed #4464 Added conditions to the list of records in the webservice

--- api/webservice/Portal/BaseModule/RecordsList.php
+++ api/webservice/Portal/BaseModule/RecordsList.php
@@ -73,6 +73,16 @@ class RecordsList extends \Api\Core\BaseAction
 			$queryGenerator->setFields(\App\Json::decode($requestFields));
 			$queryGenerator->setField('id');
 		}
+		if ($conditions = $this->controller->request->getHeader('X-CONDITION')) {
+			$conditions = \App\Json::decode($conditions);
+			if (isset($conditions['fieldName'])) {
+				$queryGenerator->addCondition($conditions['fieldName'], $conditions['value'], $conditions['operator']);
+			} else {
+				foreach ($conditions as $condition) {
+					$queryGenerator->addCondition($condition['fieldName'], $condition['value'], $condition['operator']);
+				}
+			}
+		}
 		return $queryGenerator;
 	}
 
--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '4.0.369',
+	'appVersion' => '4.0.370',
 	'patchVersion' => '2017.06.07',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.30',
