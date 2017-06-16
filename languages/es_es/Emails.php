4026bc9e4510c3f76430ccbc972af1edf36af841
luespi <cuauhtemocescajeda@gmail.com>
2017-06-16 16:05:49 -0500

    Hello I translated this file hope it helps
    It seems the file "Emails.php" hadn't been translated. My installalation had that file so I am not sure why it is not here translated. But, either way I did tranaslated it. I hope it helps.

---
+++ languages/es_es/Emails.php
@@ -0,0 +1,53 @@
+<?php
+/* +***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * Contributor(s): YetiForce.com
+ * ********************************************************************************** */
+$languageStrings = [
+	'LBL_TO' => 'Para',
+	'LBL_FROM' => 'De',
+	'LBL_ATTACHMENT' => 'Archivo adjunto',
+	'Email Flag' => 'Nota de correo',
+	'Activtiy Type' => 'Tipo de actividad',
+	'Sales Enity Module' => 'Modulo de entidad de ventas',
+	'Parent ID' => 'ID del correo padre',
+	'Owner' => 'Propietario',
+	'Access Count' => 'Cuenta de acceso',
+	'Date & Time Sent' => 'Fecha y hora de envío',
+	'Emails' => 'Emails',
+	'LBL_ADD_BCC' => 'Add BCC',
+	'LBL_ADD_CC' => 'Add CC',
+	'LBL_BCC' => 'BCC',
+	'LBL_BROWSE_CRM' => 'Navegar y buscar por el CRM',
+	'LBL_CC' => 'CC',
+	'LBL_COMPOSE_EMAIL' => 'Escribir email',
+	'LBL_DESCRIPTION' => 'Descripción',
+	'LBL_MAIL_DATE' => 'Fecha del email',
+	'LBL_DRAFTED_ON' => 'Borrador creado en',
+	'LBL_DRAFT' => 'Borrador',
+	'LBL_ATTACHED' => 'Adjuntado',
+	'LBL_EMAIL_INFORMATION' => 'Información del Email',
+	'LBL_EXCEEDED' => 'Sobrepasado',
+	'LBL_FORWARD' => 'Adelante',
+	'LBL_GO_TO_PREVIEW' => 'Ir a la vista previa',
+	'LBL_INFO' => 'Información',
+	'LBL_MAX_UPLOAD_SIZE' => 'Tamaño máximo de todo lo subido',
+	'LBL_OWNER' => 'Dueño',
+	'LBL_PRINT' => '    Imprimir',
+	'LBL_SAVE_AS_DRAFT' => 'Guardar como borrador',
+	'LBL_SELECT_EMAIL_IDS' => 'Seleccionar direcciones de correo',
+	'LBL_SELECT_EMAIL_TEMPLATE' => 'Seleccionar plantilla(template) de email',
+	'LBL_SEND' => 'Enviar',
+	'LBL_SENT_ON' => 'Fue enviado en',
+	'LBL_SUBJECT' => 'Asunto',
+	'SINGLE_Emails' => 'Email',
+	'Time Start' => 'Hora en que se envió',
+	'Emails_Block1' => 'Bloque de Email 1',
+	'Emails_Block2' => 'Bloque de Email 2',
+	'Emails_Block3' => 'Bloque de Email 3',
+];
