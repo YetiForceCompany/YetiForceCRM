f26060164cda0dff18bec531ed45b4308204dad5
Arkadiusz Sołek <a.solek@yetiforce.com>
2017-06-29 13:46:19 +0200

    test

--- config/developer.php
+++ config/developer.php
@@ -9,7 +9,7 @@ $DEVELOPER_CONFIG = [
 	// Turn the possibility to change generatedtype
 	'CHANGE_GENERATEDTYPE' => false,
 	// Enable minimize JS files
-	'MINIMIZE_JS' => true,
+	'MINIMIZE_JS' => false,
 	// Enable minimize CSS files
 	'MINIMIZE_CSS' => true,
 	// Change of fields visibility
