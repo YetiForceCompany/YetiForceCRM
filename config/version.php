83fe48a43307a2f1693b1b30e96c598e7ea31127
Tomasz Kur <t.kur@yetiforce.com>
2017-07-12 20:51:08 +0200

    Improved unit test

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.0.446',
-	'patchVersion' => '2017.07.11',
+	'appVersion' => '4.0.447',
+	'patchVersion' => '2017.07.12',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.30',
 	'lib_PHPExcel' => '0.0.0',
