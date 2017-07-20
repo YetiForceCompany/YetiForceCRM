2d123634b15761e5126aef558c408ed2bb095092
skavenkf <skavenkf@gmx.de>
2017-07-20 21:32:35 +0200

    update german language

--- languages/de_de/Settings/ConfReport.php
+++ languages/de_de/Settings/ConfReport.php
@@ -11,8 +11,8 @@ $languageStrings = [
 	'LBL_PARAMETER' => 'Parameter',
 	'LBL_VALUE' => 'Aktuelle Konfiguration',
 	'LBL_RECOMMENDED' => 'Empfohlene Konfiguration',
-	'LBL_SYSTEM_STABILITY' => 'System stability configuration',
-	'LBL_SYSTEM_SECURITY' => 'System security configuration',
+	'LBL_SYSTEM_STABILITY' => 'Konfiguration zur Systemstabilität',
+	'LBL_SYSTEM_SECURITY' => 'Konfiguration zur Systemsicherheit',
 	'LBL_FILES_PERMISSIONS' => 'Dateien/Verzeichnisse',
 	'LBL_FILE' => 'Name',
 	'LBL_PATH' => 'Pfad',
@@ -62,10 +62,10 @@ $languageStrings = [
 	'LBL_CRON_LOG_FILE' => 'Cron - Error Logs',
 	'LBL_CRON_PHP_SAPI' => 'Cron - API Server',
 	'LBL_ENVIRONMENTAL_INFORMATION' => 'Umgebungsinformationen',
-	'HTTPS_HELP_TEXT' => 'SSL is the standard security technology for establishing an encrypted link between a web server and a browser',
-	'HTACCESS_HELP_TEXT' => '.htaccess files can be used to change the server software configuration to configure and enable / disable additional services. YetiForceCRM requires access to the file to some work functions properly.',
-	'PUBLIC_HTML_HELP_TEXT' => 'We suggest that you install the system so that only the public_html folder is used. This improves data security by not having access to unpublic directories and files directly from the address'
+	'HTTPS_HELP_TEXT' => 'SSL ist die Standard Sicherheitstechnologie für die verschlüsselte Verbindung zwischen dem Webserver und dem Browser',
+	'HTACCESS_HELP_TEXT' => '.htaccess Dateien können verwendet werden um die Servereinstellungen anzupassen. YetiForceCRM benötigt den Zugriff auf die Dateien um korrekt zu funktionieren.',
+	'PUBLIC_HTML_HELP_TEXT' => 'Wir empfehlen das System so zu installieren, dass nur das öffentliche HTML Verzeichnis benutzt wird. Es erhöht die Datensicherheit, wenn auf nichtöffentliche Verzeichnisse und Dateien nicht direkt über die Adresse(URL) zugegriffen werden kann.'
 ];
 $jsLanguageStrings = [
-	'JS_SPEED_TEST_START' => 'Performance ermitteln ...',
+	'JS_SPEED_TEST_START' => 'Performance wird gemessen ...',
 ];
