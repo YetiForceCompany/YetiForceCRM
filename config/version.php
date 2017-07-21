1565fccfe73e511155d7d8456f186b57e6555cfd
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-07-21 10:20:16 +0200

    Update gantt library to v.4.1.0 Stardard

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.0.486',
-	'patchVersion' => '2017.07.20',
+	'appVersion' => '4.0.487',
+	'patchVersion' => '2017.07.21',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.30',
 	'lib_PHPExcel' => '0.0.0',
-	'lib_gantt' => '0.0.0',
+	'lib_gantt' => '0.0.1',
 ];
