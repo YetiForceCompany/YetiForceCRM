42451162f25234fb09191bff456ecbc302b677fd
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-07-25 10:48:56 +0200

    Removed webservice.php

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.0.493',
-	'patchVersion' => '2017.07.21',
+	'appVersion' => '4.0.494',
+	'patchVersion' => '2017.07.25',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.30',
 	'lib_PHPExcel' => '0.0.0',
