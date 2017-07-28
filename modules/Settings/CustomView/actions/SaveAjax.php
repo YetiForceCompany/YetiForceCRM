9e470f9c0a090a2d489e28988e8e8f55a44a3969
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2017-07-28 10:20:33 +0200

    Update SaveAjax.php

--- modules/Settings/CustomView/actions/SaveAjax.php
+++ modules/Settings/CustomView/actions/SaveAjax.php
@@ -47,6 +47,7 @@ class Settings_CustomView_SaveAjax_Action extends Settings_Vtiger_IndexAjax_View
 	public function upadteSequences(\App\Request $request)
 	{
 		$params = $request->get('param');
+		Settings_CustomView_Module_Model::upadteSequences($params);
 		$response = new Vtiger_Response();
 		$response->setResult([
 			'message' => \App\Language::translate('LBL_SAVE_SEQUENCES', $request->getModule(false))
@@ -57,7 +58,6 @@ class Settings_CustomView_SaveAjax_Action extends Settings_Vtiger_IndexAjax_View
 	public function setFilterPermissions(\App\Request $request)
 	{
 		$params = $request->get('param');
-		Settings_CustomView_Module_Model::upadteSequences($params);
 		$type = $request->get('type');
 		if ($type == 'default') {
 			$result = Settings_CustomView_Module_Model::setDefaultUsersFilterView($params['tabid'], $params['cvid'], $params['user'], $params['action']);
