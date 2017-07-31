65fe85f608fad9f3619632fd10ffbdf5ce8e1585
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-07-31 15:18:43 +0200

    Fixed integration actions with mail

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.0.538',
+	'appVersion' => '4.0.539',
 	'patchVersion' => '2017.07.31',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.30',
+	'lib_roundcube' => '0.0.31',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_gantt' => '0.0.1',
 ];
