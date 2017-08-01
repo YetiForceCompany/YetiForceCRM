07a1967c5083b7e9197d06cae8a56b3b94157feb
Adrian Koń <a.kon@yetiforce.com>
2017-08-01 09:55:10 +0200

    Improved query

--- modules/OSSMail/models/Module.php
+++ modules/OSSMail/models/Module.php
@@ -139,11 +139,8 @@ class OSSMail_Module_Model extends Vtiger_Module_Model
 	public static function getComposeParameters()
 	{
 		if (!self::$composeParam) {
-			$dataReader = (new \App\Db\Query())->select(['parameter', 'value'])->from('vtiger_ossmailscanner_config')
-					->where(['conf_type' => 'email_list'])->createCommand()->query();
-			while ($row = $dataReader->read()) {
-				$config[$row['parameter']] = $row['value'];
-			}
+			$config = (new \App\Db\Query())->select(['parameter', 'value'])->from('vtiger_ossmailscanner_config')
+					->where(['conf_type' => 'email_list'])->createCommand()->queryAllByGroup(0);
 			$config['popup'] = $config['target'] == '_blank' ? true : false;
 			self::$composeParam = $config;
 		}
