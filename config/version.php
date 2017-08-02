1fb4a92997510531f03de157e1e5ac7dcf7883dd
Adrian Koń <a.kon@yetiforce.com>
2017-08-02 14:10:54 +0200

    Improved query

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '4.1.48',
+	'appVersion' => '4.1.49',
 	'patchVersion' => '2017.08.02',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.31',
--- vendor/yetiforce/Fields/Picklist.php
+++ vendor/yetiforce/Fields/Picklist.php
@@ -135,15 +135,10 @@ class Picklist
 	 */
 	public static function getPickListModules()
 	{
-		$adb = \PearDatabase::getInstance();
-		// vtlib customization: Ignore disabled modules.
-		$query = 'select distinct vtiger_field.fieldname,vtiger_field.tabid,vtiger_tab.tablabel, vtiger_tab.name as tabname,uitype from vtiger_field inner join vtiger_tab on vtiger_tab.tabid=vtiger_field.tabid where uitype IN (15,33) and vtiger_field.tabid != 29 and vtiger_tab.presence != 1 and vtiger_field.presence in (0,2) order by vtiger_field.tabid ASC';
-		// END
-		$result = $adb->pquery($query, []);
-		while ($row = $adb->fetch_array($result)) {
-			$modules[$row['tablabel']] = $row['tabname'];
-		}
-		return $modules;
+		return (new \App\Db\Query())->select(['vtiger_tab.tablabel', 'vtiger_tab.name as tabname'])->from('vtiger_field')
+				->innerJoin('vtiger_tab', 'vtiger_field.tabid = vtiger_tab.tabid')->where(['uitype' => [15, 33], 'vtiger_field.presence' => [0, 2]])
+				->andWhere((['<>', 'vtiger_field.tabid', 29]))
+				->andWhere((['<>', 'vtiger_tab.presence', 1]))->createCommand()->queryAllByGroup(0);
 	}
 
 	/**
