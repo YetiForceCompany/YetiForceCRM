9d5459af6bb01fca2e9ca797ac47b43dbb3cb1b9
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-08-02 14:33:34 +0200

    Fixed email address search

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.1.50',
+	'appVersion' => '4.1.51',
 	'patchVersion' => '2017.08.02',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.31',
+	'lib_roundcube' => '0.0.32',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_gantt' => '0.0.1',
 ];
