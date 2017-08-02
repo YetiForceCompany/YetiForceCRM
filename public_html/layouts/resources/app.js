cfa1a1b4b3f8d4725fed82dfb10b085c395cd8f0
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2017-08-02 21:23:38 +0200

    Added minor improvements

--- public_html/layouts/resources/app.js
+++ public_html/layouts/resources/app.js
@@ -903,6 +903,10 @@ var app = {
 		ranges[app.vtranslate('JS_LAST_7_DAYS')] = [moment().subtract(6, 'days'), moment()];
 		ranges[app.vtranslate('JS_CURRENT_MONTH')] = [moment().startOf('month'), moment().endOf('month')];
 		ranges[app.vtranslate('JS_LAST_MONTH')] = [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')];
+		if($.fn.datepicker.dates[language] == undefined){
+			var langCodes = Object.keys($.fn.datepicker.dates);
+			language = langCodes[0];
+		}
 		var params = {
 			autoUpdateInput: false,
 			autoApply: true,
