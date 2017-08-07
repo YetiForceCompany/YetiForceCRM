d1aa176e72091b78931b28c91b2391e8e979cf5b
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-07 11:25:50 +0200

    Updated comments

--- modules/Settings/Workflows/models/RecordStructure.php
+++ modules/Settings/Workflows/models/RecordStructure.php
@@ -8,22 +8,48 @@
  * All Rights Reserved.
  * *********************************************************************************** */
 
+/**
+ * class settings workflows recordstructure model
+ */
 class Settings_Workflows_RecordStructure_Model extends Vtiger_RecordStructure_Model
 {
 
+	/**
+	 * Record structure default mode
+	 * @var string
+	 */
 	const RECORD_STRUCTURE_MODE_DEFAULT = '';
+
+	/**
+	 * Record structure mode filter
+	 * @var string
+	 */
 	const RECORD_STRUCTURE_MODE_FILTER = 'Filter';
 
+	/**
+	 * Set workflow model
+	 * @param object $workFlowModel
+	 */
 	public function setWorkFlowModel($workFlowModel)
 	{
 		$this->workFlowModel = $workFlowModel;
 	}
 
+	/**
+	 * Get workflow model
+	 * @return object
+	 */
 	public function getWorkFlowModel()
 	{
 		return $this->workFlowModel;
 	}
 
+	/**
+	 * Get instance for workflow module
+	 * @param object $workFlowModel
+	 * @param string $mode
+	 * @return object
+	 */
 	public static function getInstanceForWorkFlowModule($workFlowModel, $mode)
 	{
 		$className = Vtiger_Loader::getComponentClassName('Model', $mode . 'RecordStructure', 'Settings:Workflows');
