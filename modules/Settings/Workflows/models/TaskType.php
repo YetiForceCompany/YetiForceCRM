46b5e76bab5f4ffa01f86e576e917a8b5390962a
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-07 10:36:22 +0200

    Updated comments and query

--- modules/Settings/Workflows/models/TaskType.php
+++ modules/Settings/Workflows/models/TaskType.php
@@ -13,44 +13,75 @@
  */
 require_once 'modules/com_vtiger_workflow/VTTaskManager.php';
 
+/**
+ * Settings Workflows TaskType Model
+ */
 class Settings_Workflows_TaskType_Model extends \App\Base
 {
 
+	/**
+	 * Return record id
+	 * @return int
+	 */
 	public function getId()
 	{
 		return $this->get('id');
 	}
 
+	/**
+	 * Return task name
+	 * @return string
+	 */
 	public function getName()
 	{
 		return $this->get('tasktypename');
 	}
 
+	/**
+	 * Return task label
+	 * @return string
+	 */
 	public function getLabel()
 	{
 		return $this->get('label');
 	}
 
+	/**
+	 * Return template path
+	 * @return string
+	 */
 	public function getTemplatePath()
 	{
 		$templatePath = vtemplate_path('Tasks/' . $this->getName() . '.tpl', 'Settings:Workflows');
 		return $templatePath;
 	}
 
+	/**
+	 * Return edit view url
+	 * @return string
+	 */
 	public function getEditViewUrl()
 	{
 		return '?module=Workflows&parent=Settings&view=EditTask&type=' . $this->getName();
 	}
 
+	/**
+	 * Create instance from class name
+	 * @param VTTask $taskClass
+	 * @return $this
+	 */
 	public static function getInstanceFromClassName($taskClass)
 	{
-		$db = PearDatabase::getInstance();
-		$result = $db->pquery("SELECT * FROM com_vtiger_workflow_tasktypes where classname=?", array($taskClass));
-		$row = $db->query_result_rowdata($result, 0);
+		$row = (new \App\Db\Query())->from('com_vtiger_workflow_tasktypes')->where(['classname' => $taskClass])->one();
 		$taskTypeObject = VTTaskType::getInstance($row);
 		return self::getInstanceFromTaskTypeObject($taskTypeObject);
 	}
 
+	/**
+	 * Return all tasks for module
+	 * @param object $moduleModel
+	 * @return array
+	 */
 	public static function getAllForModule($moduleModel)
 	{
 		$taskTypes = VTTaskType::getAll($moduleModel->getName());
@@ -61,17 +92,31 @@ class Settings_Workflows_TaskType_Model extends \App\Base
 		return $taskTypeModels;
 	}
 
+	/**
+	 * Return task type instance
+	 * @param string $taskType
+	 * @return object
+	 */
 	public static function getInstance($taskType)
 	{
 		$taskTypeObject = VTTaskType::getInstanceFromTaskType($taskType);
 		return self::getInstanceFromTaskTypeObject($taskTypeObject);
 	}
 
+	/**
+	 * Return instance from task type object
+	 * @param object $taskTypeObject
+	 * @return \self
+	 */
 	public static function getInstanceFromTaskTypeObject($taskTypeObject)
 	{
 		return new self($taskTypeObject->data);
 	}
 
+	/**
+	 * Return task base module object
+	 * @return object
+	 */
 	public function getTaskBaseModule()
 	{
 		$taskTypeName = $this->get('tasktypename');
