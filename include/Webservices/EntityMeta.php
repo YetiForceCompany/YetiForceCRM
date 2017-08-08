9b661e028b2c37af7b81f643b454822bd150a61e
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-08 12:04:41 +0200

    Updated queries and comments

--- include/Webservices/EntityMeta.php
+++ include/Webservices/EntityMeta.php
@@ -284,13 +284,13 @@ abstract class EntityMeta
 	public function getEntityDeletedQuery()
 	{
 		if ($this->getEntityName() == 'Leads') {
-			return "vtiger_crmentity.deleted=0 and vtiger_leaddetails.converted=0";
+			return ['vtiger_crmentity.deleted' => 0, 'vtiger_leaddetails.converted' => 0];
 		}
 		if ($this->getEntityName() != "Users") {
-			return "vtiger_crmentity.deleted=0";
+			return ['vtiger_crmentity.deleted' => 0];
 		}
 		// not sure whether inactive users should be considered deleted or not.
-		return "vtiger_users.status='Active'";
+		return ['vtiger_users.status' => 'Active'];
 	}
 
 	abstract public function hasPermission($operation, $webserviceId);
