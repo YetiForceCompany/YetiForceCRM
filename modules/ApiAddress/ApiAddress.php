edb7501f3071386481ed9e627aca6f22f23679b1
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-09 15:31:24 +0200

    Updated comments and queries

--- modules/ApiAddress/ApiAddress.php
+++ modules/ApiAddress/ApiAddress.php
@@ -19,24 +19,22 @@ class ApiAddress
 		require_once('include/utils/utils.php');
 		$adb = PearDatabase::getInstance();
 		$registerLink = false;
-		if ($eventType == 'module.postinstall') {
+		if ($eventType === 'module.postinstall') {
 			//Add Assets Module to Customer Portal
 			$adb = PearDatabase::getInstance();
 			$registerLink = true;
+			\App\Db::getInstance()->createCommand()->update('vtiger_tab', ['customized' => 0], ['name' => $moduleName])->execute();
+			\App\Db::getInstance()->createCommand()->insert('vtiger_apiaddress', ['nominatim' => 0, 'key' => 0, 'source' => 'https://api.opencagedata.com/geocode/v1/', 'min_length' => 3])->execute();
+		} else if ($eventType === 'module.disabled') {
 
-			$adb->query("UPDATE vtiger_tab SET customized=0 WHERE name='$moduleName'");
-			$sql = "INSERT INTO `vtiger_apiaddress` ( `nominatim`, `key`, `source`, `min_length` ) VALUES ( ?, ?, ?, ?);";
-			$adb->pquery($sql, array(0, 0, 'https://api.opencagedata.com/geocode/v1/', 3), true);
-		} else if ($eventType == 'module.disabled') {
-			
-		} else if ($eventType == 'module.enabled') {
-			
-		} else if ($eventType == 'module.preuninstall') {
-			
-		} else if ($eventType == 'module.preupdate') {
-			
-		} else if ($eventType == 'module.postupdate') {
-			
+		} else if ($eventType === 'module.enabled') {
+
+		} else if ($eventType === 'module.preuninstall') {
+
+		} else if ($eventType === 'module.preupdate') {
+
+		} else if ($eventType === 'module.postupdate') {
+
 		}
 		$displayLabel = 'LBL_API_ADDRESS';
 		if ($registerLink) {
