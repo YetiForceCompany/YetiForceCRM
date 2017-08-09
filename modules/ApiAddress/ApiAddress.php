4ff93f48a8ce8e3a29d7ffeffbd703ee6e0418fb
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-09 15:33:51 +0200

    Small fixes

--- modules/ApiAddress/ApiAddress.php
+++ modules/ApiAddress/ApiAddress.php
@@ -1,10 +1,13 @@
 <?php
-
 /**
  * ApiAddress model class
  * @package YetiForce.CRMEntity
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ */
+
+/**
+ * Class ApiAddress
  */
 class ApiAddress
 {
@@ -17,11 +20,9 @@ class ApiAddress
 	public function vtlib_handler($moduleName, $eventType)
 	{
 		require_once('include/utils/utils.php');
-		$adb = PearDatabase::getInstance();
 		$registerLink = false;
 		if ($eventType === 'module.postinstall') {
 			//Add Assets Module to Customer Portal
-			$adb = PearDatabase::getInstance();
 			$registerLink = true;
 			\App\Db::getInstance()->createCommand()->update('vtiger_tab', ['customized' => 0], ['name' => $moduleName])->execute();
 			\App\Db::getInstance()->createCommand()->insert('vtiger_apiaddress', ['nominatim' => 0, 'key' => 0, 'source' => 'https://api.opencagedata.com/geocode/v1/', 'min_length' => 3])->execute();
