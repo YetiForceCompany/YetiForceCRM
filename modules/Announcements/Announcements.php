56fff4baf99edaa9ca4060bdc48a01d4d6c3c4f4
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-09 14:49:38 +0200

    Updated query

--- modules/Announcements/Announcements.php
+++ modules/Announcements/Announcements.php
@@ -85,15 +85,15 @@ class Announcements extends Vtiger_CRMEntity
 		$adb = PearDatabase::getInstance();
 		if ($eventType == 'module.postinstall') {
 			\App\Fields\RecordNumber::setNumber($moduleName, 'NO', '1');
-			$adb->pquery('UPDATE vtiger_tab SET customized=0 WHERE name=?', array('Announcements'));
+			\App\Db::getInstance()->createCommand()->update('vtiger_tab', ['customized' => 0], ['name' => 'Announcements'])->execute();
 		} else if ($eventType == 'module.disabled') {
-			
+
 		} else if ($eventType == 'module.preuninstall') {
-			
+
 		} else if ($eventType == 'module.preupdate') {
-			
+
 		} else if ($eventType == 'module.postupdate') {
-			
+
 		}
 	}
 }
