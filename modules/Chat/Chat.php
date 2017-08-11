98920512cbb667b3386c70a009b1b151cb1fac90
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-11 09:44:52 +0200

    Chat

--- modules/Chat/Chat.php
+++ modules/Chat/Chat.php
@@ -1,19 +1,26 @@
 <?php
-
 /**
  * Chat model class
  * @package YetiForce.CRMEntity
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  */
+
+/**
+ * Class chat
+ */
 class Chat
 {
 
+	/**
+	 *
+	 * @param string $moduleName
+	 * @param string $eventType
+	 */
 	public function vtlib_handler($moduleName, $eventType)
 	{
-		if ($eventType == 'module.postinstall') {
-			$db = PearDatabase::getInstance();
-			$db->pquery('UPDATE vtiger_tab SET customized=0 WHERE name=?', [$moduleName]);
+		if ($eventType === 'module.postinstall') {
+			\App\Db::getInstance()->update('vtiger_tab', ['customized' => 0], ['name' => $moduleName])->execute();
 		}
 	}
 }
