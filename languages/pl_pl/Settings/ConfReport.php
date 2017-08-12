dcd43fb2f2b7d1daf0c6ff8f566c4ca627e70853
Błażej Pabiszczak <bpabiszczak@users.noreply.github.com>
2017-08-12 22:38:30 +0200

    Update ConfReport.php

--- languages/pl_pl/Settings/ConfReport.php
+++ languages/pl_pl/Settings/ConfReport.php
@@ -70,7 +70,7 @@ $languageStrings = [
 	'LBL_VULNERABILITY_NAME' => 'Nazwa podatności',
 	'LBL_VULNERABILITY_URL' => 'Adres',
 	'HTTPS_HELP_TEXT' => 'SSL to technologia zabezpieczeń służąca do tworzenia zaszyfrowanego połaczenia między serwerem internetowym a przeglądarką. Zalecamy właczenie aby nikt nie kopiował Twojich danych.',
-	'HTACCESS_HELP_TEXT' => 'Pliki .htaccess jest nie aktywny, jest on potrzeby do prawidłowej obsługi wszystkich integracji oraz do zabezpieczenia dostępu do katalogów z danymi użytkownika.',
+	'HTACCESS_HELP_TEXT' => 'Plik .htaccess jest nieaktywny, jest on potrzebny do prawidłowej obsługi wszystkich integracji oraz do zabezpieczenia dostępu do katalogów z danymi użytkownika.',
 	'PUBLIC_HTML_HELP_TEXT' => 'Zalecamy aby domena kierowała do katalogu public_html, dzięki temu pliki systemu oraz twoje dane nie będą publiczne i nikt nie będzie mógł ich pobrać.'
 ];
 $jsLanguageStrings = [
