fbc0bd634e214e74adc99c52381a2135e21259d4
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-16 15:08:06 +0200

    Updated query

--- modules/Leads/summary_blocks/TotalContacts.php
+++ modules/Leads/summary_blocks/TotalContacts.php
@@ -18,12 +18,7 @@ class TotalContacts
 	{
 
 		\App\Log::trace("Entering TotalContacts::process() method ...");
-		$adb = PearDatabase::getInstance();
-		$contact = 'SELECT COUNT(contactid) as count FROM vtiger_contactdetails
-			INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid=vtiger_contactdetails.contactid
-			WHERE vtiger_crmentity.deleted=0 &&  vtiger_contactdetails.parentid = ?';
-		$result_contact = $adb->pquery($contact, array($instance->getId()));
-		$count = $adb->query_result($result_contact, 0, 'count');
+		$count = $query = (new \App\Db\Query())->from('vtiger_contactdetails')->innerJoin('vtiger_crmentity', 'vtiger_crmentity.crmid=vtiger_contactdetails.contactid')->where(['vtiger_crmentity.deleted' => 0, 'vtiger_contactdetails.parentid' => $instance->getId()])->count();
 		\App\Log::trace("Exiting TotalContacts::process() method ...");
 		return $count;
 	}
