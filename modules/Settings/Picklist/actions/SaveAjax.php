200b1183b45f6fb2284068b0d458b46e78498807
areksolek <arkadiusz_s9887@wp.pl>
2017-08-16 15:07:24 +0200

    Update SaveAjax.php

--- modules/Settings/Picklist/actions/SaveAjax.php
+++ modules/Settings/Picklist/actions/SaveAjax.php
@@ -153,7 +153,7 @@ class Settings_Picklist_SaveAjax_Action extends Settings_Vtiger_Basic_Action
 
 		$response = new Vtiger_Response();
 		try {
-			$moduleModel->enableOrDisableValuesForRole($request->getForSql('picklistName'), $request->getArray('assignValues'), [], $roleIdList);
+			$moduleModel->enableOrDisableValuesForRole($request->getForSql('picklistName'), $request->getArray('assign_values'), [], $roleIdList);
 			$response->setResult(array('success', true));
 		} catch (Exception $e) {
 			$response->setError($e->getCode(), $e->getMessage());
