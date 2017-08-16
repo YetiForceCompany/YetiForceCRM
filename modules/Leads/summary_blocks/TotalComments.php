c28bc7e932fa7643f37311a56f15943b504538f0
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-16 14:49:15 +0200

    Updated query

--- modules/Leads/summary_blocks/TotalComments.php
+++ modules/Leads/summary_blocks/TotalComments.php
@@ -17,11 +17,7 @@ class TotalComments
 	{
 
 		\App\Log::trace("Entering TotalComments::process() method ...");
-		$adb = PearDatabase::getInstance();
-		$modcomments = 'SELECT COUNT(modcommentsid) AS comments FROM vtiger_modcomments
-			WHERE vtiger_modcomments.related_to = ?';
-		$result_modcomments = $adb->pquery($modcomments, array($instance->getId()));
-		$count = $adb->query_result($result_modcomments, 0, 'comments');
+		$count = (new \App\Db\Query())->from('vtiger_modcomments')->where(['vtiger_modcomments.related_to' => $instance->getId()])->count();
 		\App\Log::trace("Exiting TotalComments::process() method ...");
 		return $count;
 	}
