d0b70e2aec120e2f0e07d33d3544fc64c2aa1d37
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-18 13:39:53 +0200

    Updated comments and query

--- modules/OSSMailScanner/actions/SaveCRMuser.php
+++ modules/OSSMailScanner/actions/SaveCRMuser.php
@@ -1,14 +1,22 @@
 <?php
-
 /**
  * OSSMailScanner SaveCRMuser action class
  * @package YetiForce.Action
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  */
+
+/**
+ * OSSMailScanner SaveCRMuser action class
+ */
 class OSSMailScanner_SaveCRMuser_Action extends Vtiger_Action_Controller
 {
 
+	/**
+	 * Check permission
+	 * @param \App\Request $request
+	 * @throws \Exception\NoPermittedForAdmin
+	 */
 	public function checkPermission(\App\Request $request)
 	{
 		$currentUserModel = Users_Record_Model::getCurrentUserModel();
@@ -17,20 +25,23 @@ class OSSMailScanner_SaveCRMuser_Action extends Vtiger_Action_Controller
 		}
 	}
 
+	/**
+	 * Process
+	 * @param \App\Request $request
+	 */
 	public function process(\App\Request $request)
 	{
 		$userid = $request->get('userid');
 		$value = $request->get('value');
 		if ($userid) {
-			$adb = PearDatabase::getInstance();
-			$adb->pquery('update roundcube_users set crm_user_id = ? WHERE user_id = ?', [$value, $userid]);
+			\App\Db::getInstance()->createCommand()->update('roundcube_users', ['crm_user_id' => $value], ['user_id' => $userid])->execute();
 			$success = true;
 			$data = \App\Language::translate('JS_saveuser_info', 'OSSMailScanner');
 		} else {
 			$success = false;
 			$data = 'Error: Brak userid';
 		}
-		$result = array('success' => $success, 'data' => $data);
+		$result = ['success' => $success, 'data' => $data];
 		$response = new Vtiger_Response();
 		$response->setResult($result);
 		$response->emit();
