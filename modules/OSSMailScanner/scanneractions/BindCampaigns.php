dfc4934bab1149c6c6928183945ea190a66b7262
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-18 14:07:55 +0200

    Updated comments and query

--- modules/OSSMailScanner/scanneractions/BindCampaigns.php
+++ modules/OSSMailScanner/scanneractions/BindCampaigns.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Mail scanner action bind Campaigns
  * @package YetiForce.MailScanner
@@ -7,13 +6,36 @@
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
+
+/**
+ * Mail scanner action bind Campaigns
+ */
 class OSSMailScanner_BindCampaigns_ScannerAction extends OSSMailScanner_PrefixScannerAction_Model
 {
 
+	/**
+	 * Module name
+	 * @var string
+	 */
 	public $moduleName = 'Campaigns';
+
+	/**
+	 * Table name
+	 * @var string
+	 */
 	public $tableName = 'vtiger_campaign';
+
+	/**
+	 * Table column
+	 * @var string
+	 */
 	public $tableColumn = 'campaign_no';
 
+	/**
+	 * Process
+	 * @param OSSMail_Mail_Model $mail
+	 * @return array
+	 */
 	public function process(OSSMail_Mail_Model $mail)
 	{
 		$this->mail = $mail;
@@ -26,14 +48,10 @@ class OSSMailScanner_BindCampaigns_ScannerAction extends OSSMailScanner_PrefixSc
 			$crmIds = OSSMailScanner_Record_Model::_merge_array($crmIds, $crmidsToaddress);
 			$crmIds = OSSMailScanner_Record_Model::_merge_array($crmIds, $crmidsCcaddress);
 			$crmIds = OSSMailScanner_Record_Model::_merge_array($crmIds, $crmidsBccaddress);
-
-			$db = PearDatabase::getInstance();
+			$dbCommand = \App\Db::getInstance()->createCommand();
 			foreach ($campaignIds as $campaignId) {
 				foreach ($crmIds as $recordId) {
-					$db->update('vtiger_campaign_records', [
-						'campaignrelstatusid' => 1
-						], 'campaignid = ? && crmid = ?', [$campaignId, $recordId]
-					);
+					$dbCommand->update('vtiger_campaign_records', ['campaignrelstatusid' => 1], ['campaignid' => $campaignId, 'crmid' => $recordId])->execute();
 				}
 			}
 		}
