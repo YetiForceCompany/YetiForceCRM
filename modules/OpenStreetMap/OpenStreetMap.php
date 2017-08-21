597c8bb77a582b4a7039aa8582a428b86b8171fb
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-21 15:28:29 +0200

    Updated query

--- modules/OpenStreetMap/OpenStreetMap.php
+++ modules/OpenStreetMap/OpenStreetMap.php
@@ -17,20 +17,20 @@ class OpenStreetMap
 	 */
 	public function moduleHandler($moduleName, $eventType)
 	{
-		$db = PearDatabase::getInstance();
-		if ($eventType == 'module.postinstall') {
-			$db->update('vtiger_tab', ['customized' => 0], 'name = ?', [$moduleName]);
+		$dbCommand = \App\Db::getInstance()->createCommand();
+		if ($eventType === 'module.postinstall') {
+			$dbCommand->update('vtiger_tab', ['customized' => 0], ['name' => $moduleName])->execute();
 			App\EventHandler::registerHandler('EntityAfterSave', 'OpenStreetMap_OpenStreetMapHandler_Handler', 'Accounts,Leads,Partners,Vendors,Competition,Contacts', '', 3);
 			\vtlib\Cron::register('LBL_UPDATER_COORDINATES', 'modules/OpenStreetMap/cron/UpdaterCoordinates.php', 60, 'OpenStreetMap', 1);
 			\vtlib\Cron::register('LBL_UPDATER_RECORDS_COORDINATES', 'modules/OpenStreetMap/cron/UpdaterRecordsCoordinates.php', 300, 'OpenStreetMap', 1);
-		} else if ($eventType == 'module.disabled') {
+		} else if ($eventType === 'module.disabled') {
 			App\EventHandler::setInActive('OpenStreetMap_OpenStreetMapHandler_Handler');
 			\vtlib\Cron::getInstance('LBL_UPDATER_COORDINATES')->updateStatus(\vtlib\Cron::$STATUS_DISABLED);
 			\vtlib\Cron::getInstance('LBL_UPDATER_RECORDS_COORDINATES')->updateStatus(\vtlib\Cron::$STATUS_DISABLED);
-		} else if ($eventType == 'module.enabled') {
+		} else if ($eventType === 'module.enabled') {
 			App\EventHandler::setActive('OpenStreetMap_OpenStreetMapHandler_Handler');
 			\vtlib\Cron::getInstance('LBL_UPDATER_RECORDS_COORDINATES')->updateStatus(\vtlib\Cron::$STATUS_ENABLED);
-		} else if ($eventType == 'module.preuninstall') {
+		} else if ($eventType === 'module.preuninstall') {
 			App\EventHandler::deleteHandler('OpenStreetMap_OpenStreetMapHandler_Handler');
 			\vtlib\Cron::deregister('LBL_UPDATER_RECORDS_COORDINATES');
 			\vtlib\Cron::deregister('LBL_UPDATER_COORDINATES');
