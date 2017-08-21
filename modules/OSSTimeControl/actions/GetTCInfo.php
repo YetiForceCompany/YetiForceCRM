1f42c297bdec29e55450964b428f154e0c37c898
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-21 14:31:25 +0200

    Updated query

--- modules/OSSTimeControl/actions/GetTCInfo.php
+++ modules/OSSTimeControl/actions/GetTCInfo.php
@@ -28,7 +28,6 @@ class OSSTimeControl_GetTCInfo_Action extends Vtiger_Action_Controller
 
 	public function process(\App\Request $request)
 	{
-		$adb = PearDatabase::getInstance();
 		$moduleName = $request->getModule();
 
 		$id = $request->get('id');
@@ -40,26 +39,27 @@ class OSSTimeControl_GetTCInfo_Action extends Vtiger_Action_Controller
 			$record = Vtiger_Record_Model::getInstanceById($id, $sourceModule);
 			$entity = $record->getEntity();
 			$sourceData = $entity->column_fields;
-			if ($sourceModule == 'HelpDesk') {
-				$sourceData['contact_label'] = vtlib\Functions::getCRMRecordLabel($sourceData['contact_id']);
-				if (vtlib\Functions::getCRMRecordType($sourceData['parent_id']) != 'Accounts')
+			if ($sourceModule === 'HelpDesk') {
+				$sourceData['contact_label'] = \App\Record::getLabel($sourceData['contact_id']);
+				if (vtlib\Functions::getCRMRecordType($sourceData['parent_id']) !== 'Accounts') {
 					unset($sourceData['parent_id']);
-				else
-					$sourceData['account_label'] = vtlib\Functions::getCRMRecordLabel($sourceData['parent_id']);
-			} else if ($sourceModule == 'Project') {
-				$query = sprintf("select * from vtiger_account where accountid = %s", $sourceData['linktoaccountscontacts']);
-				$ifExist = $adb->query($query, true, "Błąd podczas pobierania danych z vtiger_crmentityrel");
-				if ($adb->num_rows($ifExist) > 0)
-					$sourceData['account_label'] = vtlib\Functions::getCRMRecordLabel($sourceData['linktoaccountscontacts']);
-				else
-					$sourceData['contact_label'] = vtlib\Functions::getCRMRecordLabel($sourceData['linktoaccountscontacts']);
+				} else {
+					$sourceData['account_label'] = \App\Record::getLabel($sourceData['parent_id']);
+				}
+			} else if ($sourceModule === 'Project') {
+				$ifExist = (new \App\Db\Query())->from('vtiger_account')->where(['accountid' => $sourceData['linktoaccountscontacts']])->exists();
+				if ($ifExist) {
+					$sourceData['account_label'] = \App\Record::getLabel($sourceData['linktoaccountscontacts']);
+				} else {
+					$sourceData['contact_label'] = \App\Record::getLabel($sourceData['linktoaccountscontacts']);
+				}
 			}
 		}
 
 		if ($sourceData === false) {
-			$result = array('success' => false, 'message' => \App\Language::translate('LBL_FAILED_TO_IMPORT_INFO', $moduleName));
+			$result = ['success' => false, 'message' => \App\Language::translate('LBL_FAILED_TO_IMPORT_INFO', $moduleName)];
 		} else {
-			$result = array('success' => true, 'sourceData' => $sourceData);
+			$result = ['success' => true, 'sourceData' => $sourceData];
 		}
 
 		$response = new Vtiger_Response();
