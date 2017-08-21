3200cc28510b3a0c02ca889f38cc8270e9de61fa
Arkadiusz Sołek <a.solek@yetiforce.com>
2017-08-21 11:54:42 +0200

    Changed Exception to App\Exceptions NotAllowedMethod.php

--- modules/OSSMail/OSSMail.php
+++ modules/OSSMail/OSSMail.php
@@ -16,7 +16,7 @@ class OSSMail
 	 * Module name
 	 * @param string $moduleName
 	 * @param string $eventType
-	 * @throws \Exception\NotAllowedMethod
+	 * @throws \App\Exceptions\NotAllowedMethod
 	 */
 	public function moduleHandler($moduleName, $eventType)
 	{
@@ -38,14 +38,14 @@ class OSSMail
 			$dbCommand->insert('vtiger_ossmails_logs', ['action' => 'Action_DisabledModule', 'info' => $moduleName, 'user' => $user_id, 'start_time' => date('Y-m-d H:i:s')])->execute();
 		} else if ($eventType === 'module.enabled') {
 			if (Settings_ModuleManager_Library_Model::checkLibrary('roundcube')) {
-				throw new \Exception\NotAllowedMethod(\App\Language::translate('ERR_NO_REQUIRED_LIBRARY', 'Settings:Vtiger', 'roundcube'));
+				throw new \App\Exceptions\NotAllowedMethod(\App\Language::translate('ERR_NO_REQUIRED_LIBRARY', 'Settings:Vtiger', 'roundcube'));
 			}
 			$user_id = Users_Record_Model::getCurrentUserModel()->get('user_name');
 			$dbCommand->insert('vtiger_ossmails_logs', ['action' => 'Action_EnabledModule', 'info' => $moduleName, 'user' => $user_id, 'start_time' => date('Y-m-d H:i:s')])->execute();
 		} else if ($eventType === 'module.preuninstall') {
-
+			
 		} else if ($eventType === 'module.preupdate') {
-
+			
 		} else if ($eventType === 'module.postupdate') {
 			$OSSMail = vtlib\Module::getInstance('OSSMail');
 			if (version_compare($OSSMail->version, '1.39', '>')) {
