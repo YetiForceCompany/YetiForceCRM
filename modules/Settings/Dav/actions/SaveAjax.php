97dea05a7b43d4a763e48f36d3b1f32997e7c1d2
Tomasz Kur <t.kur@yetiforce.com>
2017-08-21 20:33:44 +0200

    Fixed #4943

--- modules/Settings/Dav/actions/SaveAjax.php
+++ modules/Settings/Dav/actions/SaveAjax.php
@@ -9,6 +9,9 @@
 class Settings_Dav_SaveAjax_Action extends Settings_Vtiger_IndexAjax_View
 {
 
+	/**
+	 * Construct
+	 */
 	public function __construct()
 	{
 		parent::__construct();
@@ -16,6 +19,10 @@ class Settings_Dav_SaveAjax_Action extends Settings_Vtiger_IndexAjax_View
 		$this->exposeMethod('deleteKey');
 	}
 
+	/**
+	 * Action to create key for user
+	 * @param \App\Request $request
+	 */
 	public function addKey(\App\Request $request)
 	{
 		$params = $request->get('params');
@@ -24,22 +31,26 @@ class Settings_Dav_SaveAjax_Action extends Settings_Vtiger_IndexAjax_View
 		$result = $moduleModel->addKey($params);
 		$success = true;
 		$message = \App\Language::translate('LBL_SUCCESS_SAVE_KEY', $request->getModule(false));
-		if ($result == 0) {
+		if ($result === 0) {
 			$success = false;
 			$message = \App\Language::translate('LBL_ERROR_SAVE_KEY', $request->getModule(false));
-		} elseif ($result == 1) {
+		} elseif ($result === 1) {
 			$success = false;
 			$message = \App\Language::translate('LBL_DUPLICATE_USER_SERVICES', $request->getModule(false));
 		}
 		$response = new Vtiger_Response();
-		$response->setResult(array(
+		$response->setResult([
 			'success' => $success,
 			'key' => $result,
 			'message' => $message
-		));
+		]);
 		$response->emit();
 	}
 
+	/**
+	 * Action to remove key
+	 * @param \App\Request $request
+	 */
 	public function deleteKey(\App\Request $request)
 	{
 		$params = $request->get('params');
@@ -47,10 +58,10 @@ class Settings_Dav_SaveAjax_Action extends Settings_Vtiger_IndexAjax_View
 		$moduleModel = Settings_Dav_Module_Model::getInstance($qualifiedModuleName);
 		$moduleModel->deleteKey($params);
 		$response = new Vtiger_Response();
-		$response->setResult(array(
+		$response->setResult([
 			'success' => true,
 			'message' => \App\Language::translate('LBL_KEY_HAS_BEEN_REMOVED', $request->getModule(false))
-		));
+		]);
 		$response->emit();
 	}
 }
