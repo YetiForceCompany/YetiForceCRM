00daa9debf7d1fa49f55ebad43fbd76284e137a2
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-21 11:38:27 +0200

    Updated query

--- modules/OSSSoldServices/cron/Renewal.php
+++ modules/OSSSoldServices/cron/Renewal.php
@@ -8,19 +8,11 @@
  * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
  */
 require_once 'include/main/WebUI.php';
-$db = PearDatabase::getInstance();
 
 $renewal = ['PLL_PLANNED', 'PLL_WAITING_FOR_RENEWAL', ''];
-$query = sprintf('SELECT 
-					vtiger_osssoldservices.osssoldservicesid 
-				  FROM
-					vtiger_osssoldservices 
-					INNER JOIN vtiger_crmentity 
-					  ON vtiger_crmentity.crmid = vtiger_osssoldservices.osssoldservicesid 
-				  WHERE vtiger_crmentity.deleted = 0 
-					AND osssoldservices_renew IN (%s) OR osssoldservices_renew IS NULL', $db->generateQuestionMarks($renewal));
-$result = $db->pquery($query, $renewal);
-while (($recordId = $db->getSingleValue($result)) !== false) {
+$query = (new App\Db\Query())->select(['vtiger_osssoldservices.osssoldservicesid'])->from('vtiger_osssoldservices')->innerJoin('vtiger_crmentity', 'vtiger_osssoldservices.osssoldservicesid = vtiger_crmentity.crmid')->where(['and', ['vtiger_crmentity.deleted' => 0], ['or', ['osssoldservices_renew' => $renewal], ['osssoldservices_renew' => null]]]);
+$dataReader = $query->createCommand()->query();
+while ($recordId = $dataReader->readColumn(0)) {
 	$recordModel = Vtiger_Record_Model::getInstanceById($recordId, 'OSSSoldServices');
 	$recordModel->updateRenewal();
 }
