5a016eae9a71176c62cc87bf6cb5a4fa621f576e
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-23 10:01:21 +0200

    Updated query

--- modules/Project/summary_blocks/NormalTasks.php
+++ modules/Project/summary_blocks/NormalTasks.php
@@ -16,15 +16,9 @@ class NormalTasks
 	public function process($instance)
 	{
 
-		\App\Log::trace("Entering NormalTasks::process() method ...");
-		$adb = PearDatabase::getInstance();
-		$query = 'SELECT COUNT(projecttaskid) as count 
-				FROM vtiger_projecttask
-						INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid=vtiger_projecttask.projecttaskid
-						WHERE vtiger_projecttask.projectid = ? && vtiger_projecttask.projecttaskpriority = ? && vtiger_crmentity.deleted=0';
-		$result = $adb->pquery($query, array($instance->getId(), 'normal'));
-		$count = $adb->query_result($result, 0, 'count');
-		\App\Log::trace("Exiting NormalTasks::process() method ...");
+		\App\Log::trace('Entering NormalTasks::process() method ...');
+		$count = (new App\Db\Query())->from('vtiger_projecttask')->innerJoin('vtiger_crmentity', 'vtiger_projecttask.projecttaskid = vtiger_crmentity.crmid')->where(['vtiger_projecttask.projectid' => $instance->getId(), 'vtiger_projecttask.projecttaskpriority' => 'normal', 'vtiger_crmentity.deleted' => 0])->count();
+		\App\Log::trace('Exiting NormalTasks::process() method ...');
 		return $count;
 	}
 }
