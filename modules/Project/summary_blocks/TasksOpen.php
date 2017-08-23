1c12c6f2aa61148f3e20cf94f025bcac7e83e3b4
Sławomir Kłos <s.klos@yetiforce.com>
2017-08-23 11:27:56 +0200

    Updated query

--- modules/Project/summary_blocks/TasksOpen.php
+++ modules/Project/summary_blocks/TasksOpen.php
@@ -16,15 +16,9 @@ class TasksOpen
 	public function process($instance)
 	{
 
-		\App\Log::trace("Entering TasksOpen::process() method ...");
-		$adb = PearDatabase::getInstance();
-		$query = 'SELECT COUNT(projecttaskid) as count 
-				FROM vtiger_projecttask
-						INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid=vtiger_projecttask.projecttaskid
-						WHERE vtiger_crmentity.deleted=0 && vtiger_projecttask.projectid = ? && vtiger_projecttask.projecttaskstatus = ? ';
-		$result = $adb->pquery($query, array($instance->getId(), 'Open'));
-		$count = $adb->query_result($result, 0, 'count');
-		\App\Log::trace("Exiting TasksOpen::process() method ...");
+		\App\Log::trace('Entering TasksOpen::process() method ...');
+		$count = (new App\Db\Query())->from('vtiger_projecttask')->innerJoin('vtiger_crmentity', 'vtiger_projecttask.projecttaskid = vtiger_crmentity.crmid')->where(['vtiger_projecttask.projectid' => $instance->getId(), 'vtiger_crmentity.deleted' => 0, 'vtiger_projecttask.projecttaskstatus' => 'Open'])->count();
+		\App\Log::trace('Exiting TasksOpen::process() method ...');
 		return $count;
 	}
 }
