4689f6f0005106fd30b2f4923b0339c943319d52
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-08-30 16:05:30 +0200

    Added missing file

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.2.128',
+	'appVersion' => '4.2.129',
 	'patchVersion' => '2017.08.30',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.32',
+	'lib_roundcube' => '0.0.33',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_gantt' => '0.0.1',
 ];
