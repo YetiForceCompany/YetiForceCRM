643c8aaf548e4ab25b6d2c81c328bfa3e29fefe9
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2017-08-31 12:44:32 +0200

    Added translations

--- languages/en_us/Settings/Users.php
+++ languages/en_us/Settings/Users.php
@@ -21,6 +21,12 @@ $languageStrings = [
 	'LBL_GROUP_NAME' => 'Group name',
 	'LBL_COLORS' => 'Colors',
 	'LBL_PRIORITY' => 'Priority',
+	'LBL_PICKLISTS' => 'Picklists',
+	'LBL_WARNING' => 'Warning',
+	'LBL_SELECT_PICKLIST' => 'Select picklist',
+	'LBL_PICKLIST_COLOR_COL_NOT_FOUND' => 'Color functionality has not been activated for this picklist',
+	'LBL_UPDATE_COLOR_COLUMN' => 'Activate',
+	'LBL_ITEM' => 'Value',
 	'LBL_MODULE' => 'Module',
 	'LBL_LDAP_RECOMMENDED_INFO' => 'We do not recommend selecting users with admin permissions for LDAP authorization. In case of authorization problems there must be a user who is able to log in without LDAP integration.',
 	//Auth
@@ -51,6 +57,7 @@ $languageStrings = [
 ];
 $jsLanguageStrings = [
 	'JS_USER_MAIL_EXIST' => 'User with this e-mail address exists',
+	'JS_PLEASE_SELECT_MODULE' => 'Please select module',
 	'JS_DECIMAL_SEPARATOR_AND_GROUPING_SEPARATOR_CANT_BE_SAME' => 'Decimal separator and Grouping separator cant be the same',
 	'Changed password successfully' => 'Changed password successfully',
 ];
