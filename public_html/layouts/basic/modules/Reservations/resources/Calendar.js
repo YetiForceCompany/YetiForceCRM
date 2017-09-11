1920ddb7b179fc54e5b49eeb9ac8f251a04f0888
Sławomir Kłos <s.klos@yetiforce.com>
2017-09-11 10:08:43 +0200

    Removed isPrivate from reservations calendar popup

--- public_html/layouts/basic/modules/Reservations/resources/Calendar.js
+++ public_html/layouts/basic/modules/Reservations/resources/Calendar.js
@@ -121,7 +121,6 @@ jQuery.Class("Reservations_Calendar_Js", {
 							'<div><span class="glyphicon glyphicon-time" aria-hidden="true"></span> <label>' + app.vtranslate('JS_END_DATE') + '</label>: ' + event.end.format('YYYY-MM-DD ' + popoverTimeFormat) + '</div>' +
 							'<div><span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span> <label>' + app.vtranslate('JS_TYPE') + '</label>: ' + event.type + '</div>' +
 							(event.status ? '<div><span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span> <label>' + app.vtranslate('JS_STATUS') + '</label>: ' + app.vtranslate(event.status) + '</div>' : '') +
-							'<div><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> <label>' + app.vtranslate('JS_IS_PRIVATE') + '</label>: ' + (event.isPrivate ? app.vtranslate('JS_YES') : app.vtranslate('JS_NO')) + '</div>' +
 							(event.smownerid ? '<div><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <label>' + app.vtranslate('JS_ASSIGNED_TO') + '</label>: ' + event.smownerid + '</div>' : '')
 				});
 				element.find('.fc-content, .fc-info').click(function () {
