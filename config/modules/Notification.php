3904c118aed283b8bc812eca9aefd5a4f93ebd58
Sławomir Kłos <s.klos@yetiforce.com>
2017-09-12 10:03:22 +0200

    Fixed #4746

--- config/modules/Notification.php
+++ config/modules/Notification.php
@@ -8,4 +8,6 @@
 return [
 	// Auto refresh reminders in header
 	'AUTO_REFRESH_REMINDERS' => true, // Boolean
+	// Auto mark notifications as readed after send emails to users
+	'AUTO_MARK_NOTIFICATIONS_READ_AFTER_EMAIL_SEND' => true, // Boolean
 ];
