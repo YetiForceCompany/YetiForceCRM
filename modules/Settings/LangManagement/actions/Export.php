f8b7b3bbe0f382ac10bc67c80d3aec5c20b69fed
Sławomir Kłos <s.klos@yetiforce.com>
2017-09-13 11:47:08 +0200

    Fixed #5173

--- modules/Settings/LangManagement/actions/Export.php
+++ modules/Settings/LangManagement/actions/Export.php
@@ -1,19 +1,26 @@
 <?php
-
 /**
  * Settings LangManagement export action class
  * @package YetiForce.Action
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  */
+
+/**
+ * Export language action
+ */
 class Settings_LangManagement_Export_Action extends Settings_Vtiger_IndexAjax_View
 {
 
+	/**
+	 * Process request
+	 * @param \App\Request $request
+	 */
 	public function process(\App\Request $request)
 	{
 		$lang = $request->getByType('lang', 1);
 
 		$package = new vtlib\LanguageExport();
-		$package->export($lang, '', $lang . '.zip', true);
+		$package->exportLanguage($lang, '', $lang . '.zip', true);
 	}
 }
