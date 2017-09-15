698082d14fc8dc955eb34bffbeae07c43711b2ba
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2017-09-15 13:31:46 +0200

    Added translations

--- languages/ru_ru/OSSTimeControl.php
+++ languages/ru_ru/OSSTimeControl.php
@@ -109,4 +109,6 @@ $jsLanguageStrings = [
 	'JS_END_DATE' => 'Дата окончания',
 	'JS_TOTAL_TIME' => 'Total time',
  	'JS_ASSIGNED_TO' => 'Assigned to',
+	'JS_TYPE' => 'Type',
+	'JS_NUMBER' => 'Number',
 ];
