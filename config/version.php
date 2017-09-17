58ad10fb7cad079438e4af0d7518ea5d63baf288
Tomasz Kur <t.kur@yetiforce.com>
2017-09-17 18:15:08 +0200

    Removed unnecessary file

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.2.192',
-	'patchVersion' => '2017.09.15',
+	'appVersion' => '4.2.193',
+	'patchVersion' => '2017.09.17',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.33',
 	'lib_PHPExcel' => '0.0.0',
