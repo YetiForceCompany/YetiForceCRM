32870fa5d09b01c7e0091a35be4e9958088234ed
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-09-19 22:19:42 +0200

    Update class description

--- vendor/yetiforce/Utils.php
+++ vendor/yetiforce/Utils.php
@@ -2,7 +2,7 @@
 namespace App;
 
 /**
- * Privilege Util basic class
+ * Utils class
  * @package YetiForce.App
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
