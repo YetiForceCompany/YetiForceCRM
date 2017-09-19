9b3174fed07d127c063c02c580b0c95888d68939
Sławomir Kłos <s.klos@yetiforce.com>
2017-09-19 09:43:41 +0200

    Updated comments

--- modules/OSSMail/actions/CheckMails.php
+++ modules/OSSMail/actions/CheckMails.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * OSSMail CheckMails action class
+ * OSSMail check mails action class
  * @package YetiForce.Action
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
--- modules/OSSMailScanner/actions/RestartCron.php
+++ modules/OSSMailScanner/actions/RestartCron.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * OSSMailScanner RestartCron action class
+ * OSSMailScanner restart cron action class
  * @package YetiForce.Action
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
