a54fbc551c38294e37ae770b50668a193f982ba7
Sławomir Kłos <s.klos@yetiforce.com>
2017-09-19 09:43:41 +0200

    Updated comments

--- modules/OSSMail/actions/ExecuteActions.php
+++ modules/OSSMail/actions/ExecuteActions.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * OSSMail ExecuteActions action class
+ * OSSMail execute actions action class
  * @package YetiForce.Action
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
--- modules/OSSMail/actions/GetContactMail.php
+++ modules/OSSMail/actions/GetContactMail.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * OSSMail GetContactMail action class
+ * OSSMail get contact mail action class
  * @package YetiForce.Action
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
