d497fa605e4b3247a807f5354015b45d95d77887
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-09-19 11:22:48 +0200

    lib_roundcube - Class names should be declared in StudlyCaps.

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.2.203',
+	'appVersion' => '4.2.204',
 	'patchVersion' => '2017.09.19',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.33',
+	'lib_roundcube' => '0.0.34',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_gantt' => '0.0.1',
 ];
