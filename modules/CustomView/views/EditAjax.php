09d82bc79fa55d642dbc09a1fb681cf53638288a
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-09-20 14:46:12 +0200

    Fixed filter creation

--- modules/CustomView/views/EditAjax.php
+++ modules/CustomView/views/EditAjax.php
@@ -15,7 +15,7 @@ Class CustomView_EditAjax_View extends Vtiger_IndexAjax_View
 	public function process(\App\Request $request)
 	{
 		$viewer = $this->getViewer($request);
-		$moduleName = $request->getByType('source_module', 1);
+		$moduleName = $request->getInteger('source_module', 2);
 		$module = $request->getModule();
 		$record = $request->get('record');
 		$duplicate = $request->get('duplicate');
