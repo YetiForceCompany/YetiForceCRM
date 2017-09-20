e644cc0ec1fe0581ad8f7c1524ffee0cffc7351c
Sławomir Kłos <s.klos@yetiforce.com>
2017-09-20 10:01:29 +0200

    Updated JqPlot to v1.0.9

--- public_html/libraries/jquery/jqplot/plugins/jqplot.categoryAxisRenderer.js
+++ public_html/libraries/jquery/jqplot/plugins/jqplot.categoryAxisRenderer.js
@@ -2,10 +2,10 @@
  * jqPlot
  * Pure JavaScript plotting plugin using jQuery
  *
- * Version: 1.0.8
- * Revision: 1250
+ * Version: 1.0.9
+ * Revision: d96a669
  *
- * Copyright (c) 2009-2013 Chris Leonello
+ * Copyright (c) 2009-2016 Chris Leonello
  * jqPlot is currently available for use in all personal or commercial projects 
  * under both the MIT (http://www.opensource.org/licenses/mit-license.php) and GPL 
  * version 2.0 (http://www.gnu.org/licenses/gpl-2.0.html) licenses. This means that you can 
@@ -264,6 +264,7 @@
             this.max = max;
             var track = 0;
             
+            // todo: adjust this so more ticks displayed.
             var maxVisibleTicks = parseInt(3+dim/10, 10);
             var skip = parseInt(numcats/maxVisibleTicks, 10);
 
