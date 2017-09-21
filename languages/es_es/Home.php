9850da9304e0178a0b8ed80856addec50da5e3a4
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2017-09-21 13:55:58 +0200

    Added translations

--- languages/es_es/Home.php
+++ languages/es_es/Home.php
@@ -35,6 +35,15 @@ $languageStrings = [
 	'LBL_TIME' => 'Tiempos',
 	'LBL_NOTIFICATION_BY_SENDER' => 'Número de notificaciones por remitente',
 	'LBL_NOTIFICATION_BY_RECIPIENT' => 'Número de notificaciones por destinatario',
+	//Widgets
+	'LBL_MINI_LIST' => 'Mini lista',
+	'LBL_FILTERING_BY_DATE' => 'Filtering by date',		
+ 	'LBL_TIMELINE_BY_DATE' => 'Timeline by date',
+	'LBL_WIDGET_NAME' => 'Nombre del Widget',
+	'LBL_SELECT_CHART' => 'Seleccione gráfico',
+	'LBL_FILTER' => 'Filtrar',
+	'LBL_GROUP_FIELD' => 'Agrupar por',
+	'LBL_GROUP_VALUES' => 'Sectores',
 ];
 
 $jsLanguageStrings = [
