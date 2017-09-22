2eed50f2023f430271097e140b8733970c1d8b93
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-09-22 14:31:46 +0200

    Fixed #4397 https://github.com/YetiForceCompany/lib_roundcube/commit/c8618cc6b29951de96f577ebd961a0fcc3dccd97

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.2.234',
+	'appVersion' => '4.2.235',
 	'patchVersion' => '2017.09.22',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.34',
+	'lib_roundcube' => '0.0.35',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_gantt' => '0.0.1',
 ];
