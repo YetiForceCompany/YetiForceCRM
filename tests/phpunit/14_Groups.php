115895dd7fab99d5ba9e5bd0c167dacc34edb4e0
arek1-618 <arek@wp4b.pl>
2017-09-29 16:14:41 +0200

    PHPUnit: Unit tests of groups

--- tests/phpunit/14_Groups.php
+++ tests/phpunit/14_Groups.php
@@ -15,7 +15,7 @@ class Groups extends TestCase
 {
 
 	/**
-	 * Vtiger_groups record id
+	 * Group id
 	 */
 	private static $id;
 
@@ -28,7 +28,7 @@ class Groups extends TestCase
 		$recordModel = new Settings_Groups_Record_Model();
 		$recordModel->set('groupname', 'Test groups');
 		$recordModel->set('description', 'Test description');
-		$recordModel->set('group_members', [0 => 'Users:1', 1 => 'Groups:2']);
+		$recordModel->set('group_members', [0 => 'Users:1', 1 => 'Groups:2', 2 => 'Roles:H6', 3 => 'RoleAndSubordinates:H34']);
 		$recordModel->set('modules', $modules);
 		$recordModel->save();
 
@@ -41,64 +41,79 @@ class Groups extends TestCase
 		$this->assertEquals($row['groupname'], 'Test groups');
 		$this->assertEquals($row['description'], 'Test description');
 
-		$modulesFromDb = [];
-		$dataReader = (new App\Db\Query())
-				->from('vtiger_group2modules')
-				->where(['groupid' => static::$id])
-				->createCommand()->query();
-		while ($row = $dataReader->read()) {
-			$modulesFromDb[] = $row['tabid'];
-		}
+		$modulesFromDb = (new App\Db\Query())->from('vtiger_group2modules')->select('tabid')
+				->where(['groupid' => static::$id])->column();
 
 		$this->assertCount(count($modules), $modulesFromDb);
 		foreach ($modules as $val) {
 			$this->assertTrue(in_array($val, $modules));
 		}
 
+		$users2Group = (new App\Db\Query())->from('vtiger_users2group')->select('userid')
+				->where(['groupid' => static::$id])->column();
 
-		$users2Group = [];
-		$dataReader = (new App\Db\Query())
-				->from('vtiger_users2group')
-				->where(['groupid' => static::$id])
-				->createCommand()->query();
-		while ($row = $dataReader->read()) {
-			$users2Group[] = $row['userid'];
-		}
 		$this->assertCount(1, $users2Group);
 		$this->assertEquals($users2Group[0], 1);
 
+		$group2Grouprel = (new App\Db\Query())->from('vtiger_group2grouprel')->select('containsgroupid')
+				->where(['groupid' => static::$id])->column();
 
-		$group2Grouprel = [];
-		$dataReader = (new App\Db\Query())
-				->from('vtiger_group2grouprel')
-				->where(['groupid' => static::$id])
-				->createCommand()->query();
-		while ($row = $dataReader->read()) {
-			$group2Grouprel[] = $row['containsgroupid'];
-		}
 		$this->assertCount(1, $group2Grouprel);
 		$this->assertEquals($group2Grouprel[0], 2);
+
+		$group2Rs = (new App\Db\Query())->from('vtiger_group2rs')->select('roleandsubid')
+				->where(['groupid' => static::$id])->column();
+
+		$this->assertCount(1, $group2Rs);
+		$this->assertEquals($group2Rs[0], 'H34');
+
+		$group2Role = (new App\Db\Query())->from('vtiger_group2role')->select('roleid')
+				->where(['groupid' => static::$id])->column();
+
+		$this->assertCount(1, $group2Role);
+		$this->assertEquals($group2Role[0], 'H6');
 	}
 
+	/**
+	 * Testing groups edit
+	 */
 	public function testEditGroups()
 	{
-		$lastId = static::$id;
 		$recordModel = Settings_Groups_Record_Model::getInstance(static::$id);
 
+		$modules = [0 => '7'];
 		$recordModel->set('groupname', 'Test groups edit');
 		$recordModel->set('description', 'Test description edit');
 		$recordModel->set('group_members', [0 => 'Users:1']);
-		$recordModel->set('modules', [0 => '7']);
+		$recordModel->set('modules', $modules);
 		$recordModel->save();
-
-		static::$id = $recordModel->getId();
-		$this->assertInternalType('int', static::$id);
-		$this->assertEquals(static::$id, $lastId);
 
 		$row = (new \App\Db\Query())->from('vtiger_groups')->where(['groupid' => static::$id])->one();
 
-		$this->assertNotFalse($row, 'No record id: ' . static::$id);
 		$this->assertEquals($row['groupname'], 'Test groups edit');
 		$this->assertEquals($row['description'], 'Test description edit');
+
+		$modulesFromDb = (new App\Db\Query())->from('vtiger_group2modules')->select('tabid')
+				->where(['groupid' => static::$id])->column();
+
+		$this->assertCount(count($modules), $modulesFromDb);
+		foreach ($modules as $val) {
+			$this->assertTrue(in_array($val, $modules));
+		}
+
+		$users2Group = (new App\Db\Query())->from('vtiger_users2group')->select('userid')
+				->where(['groupid' => static::$id])->column();
+
+		$this->assertCount(1, $users2Group);
+		$this->assertEquals($users2Group[0], 1);
+
+		$cnt = (new App\Db\Query())->from('vtiger_group2grouprel')->where(['groupid' => static::$id])->count();
+		$this->assertEquals(0, $cnt);
+
+		$cnt = (new App\Db\Query())->from('vtiger_group2rs')->where(['groupid' => static::$id])->count();
+		$this->assertEquals(0, $cnt);
+
+		$cnt = (new App\Db\Query())->from('vtiger_group2role')->where(['groupid' => static::$id])->count();
+		$this->assertEquals(0, $cnt);
 	}
 }
