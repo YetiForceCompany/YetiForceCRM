e2b9e9e25df8647ca4bb9aa24853f91a586c68fd
arek1-618 <arek@wp4b.pl>
2017-09-29 13:00:39 +0200

    PHPUnit: Unit tests of groups

--- tests/phpunit/14_Groups.php
+++ tests/phpunit/14_Groups.php
@@ -8,37 +8,31 @@
  */
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @covers Groups::<public>
+ */
 class Groups extends TestCase
 {
-
-	private $id;
 
 	/**
 	 * Testing groups creation
 	 */
 	public function testAddGroups()
 	{
-		$gData = array(
-			'groupname' => 'Test groups',
-			'description' => 'Test description',
-			'group_members' => array(0 => 'Users:1', 1 => 'Groups:2'),
-			'modules' => array(0 => '4', 1 => '7')
-		);
-
 		$recordModel = new Settings_Groups_Record_Model();
-		foreach ($gData as $key => $value) {
-			$recordModel->set($key, $value);
-		}
+		$recordModel->set('groupname', 'Test groups');
+		$recordModel->set('description', 'Test description');
+		$recordModel->set('group_members', [0 => 'Users:1', 1 => 'Groups:2']);
+		$recordModel->set('modules', [0 => '4', 1 => '7']);
 		$recordModel->save();
 
 		$id = $recordModel->getId();
-		$this->id = $id;
 		$this->assertInternalType('int', $id);
 
 		$row = (new \App\Db\Query())->from('vtiger_groups')->where(['groupid' => $id])->one();
 
 		$this->assertNotFalse($row, "No record id: $id");
-		$this->assertEquals($row['groupname'], $gData['groupname']);
-		$this->assertEquals($row['description'], $gData['description']);
+		$this->assertEquals($row['groupname'], 'Test groups');
+		$this->assertEquals($row['description'], 'Test description');
 	}
 }
