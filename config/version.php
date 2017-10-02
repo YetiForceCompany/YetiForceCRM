3e3d5bbd5646bb693d4a549eee4bc23b1c651d94
Tomasz Kur <t.kur@yetiforce.com>
2017-10-02 20:51:50 +0200

    Fixed #5218

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '4.2.276',
+	'appVersion' => '4.2.277',
 	'patchVersion' => '2017.10.02',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.35',
--- modules/Announcements/actions/BasicAjax.php
+++ modules/Announcements/actions/BasicAjax.php
@@ -7,23 +7,46 @@
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
-class Announcements_BasicAjax_Action extends Vtiger_BasicAjax_Action
+class Announcements_BasicAjax_Action extends Vtiger_Action_Controller
 {
 
+	/**
+	 * Function to check permission
+	 * @param \App\Request $request
+	 * @throws \App\Exceptions\NoPermitted
+	 */
+	public function checkPermission(\App\Request $request)
+	{
+		$currentUserPriviligesModel = Users_Privileges_Model::getCurrentUserPrivilegesModel();
+		if (!$currentUserPriviligesModel->hasModulePermission($request->getModule())) {
+			throw new \App\Exceptions\NoPermitted('LBL_PERMISSION_DENIED');
+		}
+	}
+
+	/**
+	 * Construct
+	 */
 	public function __construct()
 	{
 		$this->exposeMethod('mark');
 	}
 
+	/**
+	 * Main process
+	 * @param \App\Request $request
+	 */
 	public function process(\App\Request $request)
 	{
 		$mode = $request->getMode();
-
 		if ($mode) {
 			$this->invokeExposedMethod($mode, $request);
 		}
 	}
 
+	/**
+	 * Action to mark announcements
+	 * @param \App\Request $request
+	 */
 	public function mark(\App\Request $request)
 	{
 		$moduleName = $request->getModule();
