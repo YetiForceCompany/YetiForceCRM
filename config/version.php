f2db221c7bda8908484dc8ed97dd43fc01385fa0
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-10-04 08:32:54 +0200

    Improved tests

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.2.281',
-	'patchVersion' => '2017.10.03',
+	'appVersion' => '4.2.282',
+	'patchVersion' => '2017.10.04',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.35',
 	'lib_PHPExcel' => '0.0.0',
