641d36319f876d2c4b7f9dcaf21c6e985e72bb53
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-10-09 22:39:04 +0200

    Update tests

--- tests/selenium/0_Base.php
+++ tests/selenium/0_Base.php
@@ -16,7 +16,7 @@ class GuiBase extends PHPUnit_Extensions_Selenium2TestCase
 	{
 		parent::setUp();
 
-		$this->setBrowserUrl('http://127.0.0.1/');
+		$this->setBrowserUrl('http://yeti/');
 		$this->setBrowser('chrome');
 
 		$screenshotsDir = __DIR__ . '/../screenshots';
