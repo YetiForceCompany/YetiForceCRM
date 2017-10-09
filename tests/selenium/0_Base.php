27d11be1af24dc0999218acf1492885c51d6490a
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-10-09 21:46:27 +0200

    Added login test

---
+++ tests/selenium/0_Base.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Base test class
+ * @package YetiForce.Test
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+class GuiBase extends PHPUnit_Extensions_Selenium2TestCase
+{
+
+	protected $captureScreenshotOnFailure = TRUE;
+
+	protected function setUp()
+	{
+		parent::setUp();
+
+		$this->setBrowserUrl('http://yeti/');
+		$this->setBrowser('chrome');
+
+		$screenshotsDir = __DIR__ . '/../screenshots';
+		if (!file_exists($screenshotsDir)) {
+			mkdir($screenshotsDir, 0777, true);
+		}
+		$this->listener = new PHPUnit_Extensions_Selenium2TestCase_ScreenshotListener($screenshotsDir);
+		$this->prepareSession();
+	}
+
+	public function onNotSuccessfulTest($e)
+	{
+		$this->listener->addError($this, $e, null);
+		parent::onNotSuccessfulTest($e);
+	}
+}
---
+++ tests/selenium/1_LogiIn.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * LogiIn test class
+ * @package YetiForce.Test
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+class LogiIn extends GuiBase
+{
+
+	/**
+	 * Testing login page display
+	 */
+	public function testLoadPage()
+	{
+		$this->url('index.php');
+		$this->byId('username')->value('admin');
+		$this->byId('password')->value('admin');
+		$this->byTag('form')->submit();
+		$this->assertEquals('Home', $this->byId('module')->value());
+		$this->assertEquals('DashBoard', $this->byId('view')->value());
+	}
+}
