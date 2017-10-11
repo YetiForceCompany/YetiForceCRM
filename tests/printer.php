7296cf7efd4cd81ebe0f1e79530e59a4e08e5bb5
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-10-11 09:17:06 +0200

    Update tests

--- tests/printer.php
+++ tests/printer.php
@@ -1,6 +1,32 @@
 <?php
+/**
+ * Travis CI result printer class
+ * @package YetiForce.Test
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+use PHPUnit\Framework\Test;
 
-class YtResultPrinter implements \PHPUnit\TextUI\ResultPrinter
+// @codeCoverageIgnoreStart
+class YtResultPrinter extends \PHPUnit\TextUI\ResultPrinter
 {
-	
+
+	/**
+	 * A test started.
+	 *
+	 * @param Test $test
+	 */
+	public function startTest(Test $test)
+	{
+		if ($this->debug) {
+			$this->write(
+				\sprintf(
+					"\n%s", \PHPUnit\Util\Test::describe($test)
+				)
+			);
+		}
+	}
 }
+
+// @codeCoverageIgnoreEnd
