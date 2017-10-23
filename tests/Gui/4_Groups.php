35bbe8d6ae0dcdd8e75c787afa459acfe669a0c7
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-10-23 22:38:26 +0200

    Improved tests

--- tests/Gui/4_Groups.php
+++ tests/Gui/4_Groups.php
@@ -20,4 +20,38 @@ class Gui_Groups extends \Tests\GuiBase
 		$this->assertEquals('List', $this->byId('view')->value());
 		$this->assertEquals('Settings', $this->byId('parent')->value());
 	}
+
+	/**
+	 * Testing the record list order
+	 */
+	public function testOrder()
+	{
+		$this->byCssSelector('.listViewHeaderValues[data-columnname="groupname"]')->click();
+		$this->assertStringEndsWith('&orderby=groupname&sortorder=ASC', strstr($this->url(), '&orderby=groupname&sortorder='));
+		$this->url('/index.php?module=Groups&parent=Settings&view=List');
+		$this->byCssSelector('.listViewHeaderValues[data-columnname="description"]')->click();
+		$this->assertStringEndsWith('&orderby=description&sortorder=ASC', strstr($this->url(), '&orderby=description&sortorder='));
+	}
+
+	/**
+	 * Testing the add group
+	 */
+	public function testAdd()
+	{
+		$this->url('/index.php?module=Groups&parent=Settings&view=List');
+		$this->byCssSelector('.contentsDiv .addButton')->click();
+
+		$this->assertEquals('Edit', $this->byId('view')->value());
+
+		$this->byName('groupname')->value('Test groupname');
+		$this->byName('description')->value('Test description');
+		$this->select($this->byId('modulesList'))->selectOptionByValue(\App\Module::getModuleId('Contacts'));
+		$this->select($this->byId('memberList'))->selectOptionByValue('Users:1');
+
+		try {
+			$this->byCssSelector('form')->submit();
+		} catch (Exception $exc) {
+			
+		}
+	}
 }
