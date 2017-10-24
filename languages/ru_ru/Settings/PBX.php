0dc9f6d3baed6ceb499531c896fab5632634ed19
Foxmillet <nick.ladushkin@gmail.com>
2017-10-24 16:44:43 +0300

    Update PBX.php

--- languages/ru_ru/Settings/PBX.php
+++ languages/ru_ru/Settings/PBX.php
@@ -6,17 +6,17 @@
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
  */
 $languageStrings = [
-	'LBL_NAME' => 'Name',
-	'LBL_TYPE' => 'Type',
-	'LBL_DEFAULT' => 'Default',
-	'LBL_ADD_RECORD' => 'Add record',
-	'LBL_CREATE_RECORD' => 'Add telephone exchange',
-	'LBL_EDIT_RECORD' => 'Edit telephone exchange',
+	'LBL_NAME' => 'Название',
+	'LBL_TYPE' => 'Тип',
+	'LBL_DEFAULT' => 'По умолчанию',
+	'LBL_ADD_RECORD' => 'Добавить запись',
+	'LBL_CREATE_RECORD' => 'Добавить АТС',
+	'LBL_EDIT_RECORD' => 'Изменить АТС',
 	//Mixpbx
-	'LBL_URL' => 'Server address',
-	'LBL_USERNAME' => 'Login',
-	'LBL_PASSWORD' => 'Password',
+	'LBL_URL' => 'Адрес сервера',
+	'LBL_USERNAME' => 'Логин',
+	'LBL_PASSWORD' => 'Пароль',
 ];
 $jsLanguageStrings = [
-	'JS_SAVE_SUCCESS' => 'Changes saved',
+	'JS_SAVE_SUCCESS' => 'Изменения сохранены',
 ];
