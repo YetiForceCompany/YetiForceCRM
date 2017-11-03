d3bd467e1b52a25cc12c2133f1aee387f7059392
skavenkf <skavenkf@gmx.de>
2017-11-03 13:37:34 +0100

    update german language

--- languages/de_de/Settings/SystemWarnings.php
+++ languages/de_de/Settings/SystemWarnings.php
@@ -4,6 +4,7 @@
  * @package YetiForce.Language
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ * @author skavenkf (K.Fink) 
  */
 $languageStrings = [
 	// Folders
