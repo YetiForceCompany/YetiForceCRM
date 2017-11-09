6e173abf5928a3f507b0b266b6420092c0266522
Paula W <p.walenczak@yetiforce.com>
2017-11-09 15:54:53 +0100

    Removed duplicate translation

--- languages/pl_pl/Settings/SMSNotifier.php
+++ languages/pl_pl/Settings/SMSNotifier.php
@@ -16,14 +16,13 @@ $languageStrings = [
 	'LBL_SMSNOTIFIER' => 'Konfiguracja dostawcy SMS',
 	'LBL_SMSNOTIFIER_DESCRIPTION' => ' ',
 	'LBL_ADD_CONFIGURATION' => 'Nowa konfiguracja',
-	'LBL_EDIT_CONFIGURATION' => 'Edytuj konfiguracje',
-	'LBL_EDIT_CONFIGURATION' => 'Edytuj konfiguracje',
+	'LBL_EDIT_CONFIGURATION' => 'Edytuj konfigurację',
 	'BTN_SAVE' => 'Zapisz',
 	'BTN_CLOSE' => 'Anuluj',
 ];
 
 $jsLanguageStrings = [
-	'LBL_DELETE_CONFIRMATION' => 'Czy jesteś pewien, że chcesz usunąć tę konfiguracje?',
+	'LBL_DELETE_CONFIRMATION' => 'Czy jesteś pewien, że chcesz usunąć tę konfigurację?',
 	'JS_RECORD_DELETED_SUCCESSFULLY' => 'Konfiguracja została poprawnie usunięta',
 	'JS_SAVE_SUCCESS' => 'Pomyślnie zapisano zmiany',
 ];
