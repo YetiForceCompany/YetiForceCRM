972cfe2234bb68b8395acf8b63851113b4857979
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-11-09 14:12:24 +0100

    License change

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.2.460',
+	'appVersion' => '4.2.461',
 	'patchVersion' => '2017.11.09',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.35',
+	'lib_roundcube' => '0.0.36',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_gantt' => '0.0.1',
 ];
