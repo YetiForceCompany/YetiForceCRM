67aa0ea5105447c9ba1c2875bfec7265a4325c08
Foxmillet <nick.ladushkin@gmail.com>
2017-11-10 10:43:57 +0300

    Update Inventory.php

--- languages/ru_ru/Settings/Inventory.php
+++ languages/ru_ru/Settings/Inventory.php
@@ -6,10 +6,10 @@
  * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  */
 $languageStrings = [
-	'LBL_CREDITLIMITS' => 'Merchant limits',
+	'LBL_CREDITLIMITS' => 'Кредитные лимиты',
 	'LBL_CREDITLIMITS_DESCRIPTION' => ' Кредитование Контрагентов',
-	'LBL_CREDITLIMITS_SINGLE' => 'Merchant limit',
-	'LBL_CREDITLIMITS_INFO' => 'In the YetiForce system we got the ability to manage merchant limits for Accounts. This functionality is used to limit the Account\'s excessive debt in permanent cooperation. Merchant limits are mainly granted to these Accounts who cooperate with us for a longer period of time and we want to extend their payment deadline, which means that the Account won\'t be able to place a new order unless his previous balance has been regulated [current balance and issued document exceed granted limit].',
+	'LBL_CREDITLIMITS_SINGLE' => 'Кредитный лимит',
+	'LBL_CREDITLIMITS_INFO' => 'В YetiForce есть возможность управления кредитными лимитами для компаний. Этот функционал используется для ограничения кредитной задолженности. Кредитные лимиты в основном предоставляются организациям, которые работают с нами длительное время и мы хотим выдать им отсрочку платежа, что означает, что компания не сможет разместить новый заказ, пока не покроет старые долги (текущий баланс должен превысить лимит).',
 	'LBL_ENTER_NAME' => 'Наименование',
 	'LBL_ENTER_VALUE' => 'Значение',
 	'LBL_NAME' => 'Название',
@@ -42,7 +42,7 @@ $languageStrings = [
 	'LBL_NO' => 'Нет',
 	//history
 	'Inventory' => 'Расширенные модули',
-	'LBL_VIEW_CREDITLIMITS' => 'Merchant limits base',
+	'LBL_VIEW_CREDITLIMITS' => 'Базовый кредитный лимит',
 	'LBL_VIEW_TAXES' => 'Базовый налог',
 	'LBL_VIEW_TAXCONFIGURATION' => 'Настройка налога',
 	'LBL_VIEW_DISCOUNTCONFIGURATION' => 'Настройка скидки',
