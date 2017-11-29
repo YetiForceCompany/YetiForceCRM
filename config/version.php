77253d4c528fa5e712f0b87c63df4e3c58ee9406
Tomasz Kur <t.kur@yetiforce.com>
2017-11-29 21:07:09 +0100

    Improved update a status of warnings

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '4.2.569',
+	'appVersion' => '4.2.570',
 	'patchVersion' => '2017.11.29',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.36',
--- modules/Settings/Vtiger/actions/SystemWarnings.php
+++ modules/Settings/Vtiger/actions/SystemWarnings.php
@@ -24,12 +24,11 @@ class Settings_Vtiger_SystemWarnings_Action extends Settings_Vtiger_Basic_Action
 	public function update(\App\Request $request)
 	{
 		$className = $request->get('id');
-		if (!class_exists($className)) {
+		if (!is_subclass_of($className, '\App\SystemWarnings\Template')) {
 			$result = false;
+		} else {
+			$result = (new $className)->update($request->get('params'));
 		}
-		$instace = new $className;
-		$result = $instace->update($request->get('params'));
-
 		$response = new Vtiger_Response();
 		$response->setResult($result);
 		$response->emit();
