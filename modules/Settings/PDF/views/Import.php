0d96bbdcb36e59b1f60716ea2610b1ee6c54ca0b
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-11-30 13:10:19 +0100

    PDF template import has been rebuilt

--- modules/Settings/PDF/views/Import.php
+++ modules/Settings/PDF/views/Import.php
@@ -17,59 +17,54 @@ class Settings_PDF_Import_View extends Settings_Vtiger_Index_View
 		\App\Log::trace('Start ' . __METHOD__);
 		$qualifiedModule = $request->getModule(false);
 		$viewer = $this->getViewer($request);
-
-		if ($request->has('upload') && $request->get('upload') === 'true') {
-			$xmlName = $_FILES['imported_xml']['name'];
-			$uploadedXml = $_FILES['imported_xml']['tmp_name'];
-			$xmlError = $_FILES['imported_xml']['error'];
-			$extension = end(explode('.', $xmlName));
+		if ($request->has('upload') && $request->getBoolean('upload')) {
+			$fileInstance = \App\Fields\File::loadFromRequest($_FILES['imported_xml']);
+			if (!$fileInstance->validate() || $fileInstance->getExtension(true) !== 'xml') {
+				throw new \App\Exceptions\Security('ERR_ILLEGAL_FILE');
+			}
 			$imagePath = '';
 			$base64Image = false;
-
 			$pdfModel = Settings_PDF_Record_Model::getCleanInstance();
-			if ($xmlError == UPLOAD_ERR_OK && $extension === 'xml') {
-				$xml = simplexml_load_file($uploadedXml);
-
-				foreach ($xml as $fieldsKey => $fieldsValue) {
-					foreach ($fieldsValue as $fieldKey => $fieldValue) {
-						foreach ($fieldValue as $columnKey => $columnValue) {
-							switch ($columnKey) {
-								case 'imageblob':
-									$base64Image = (string) $columnValue;
-									break;
-
-								case 'watermark_image':
-									$imagePath = (string) $columnValue;
-									$pdfModel->set($columnKey, '');
-									break;
-
-								default:
-									$value = (string) $columnValue;
-									$pdfModel->set($columnKey, $value);
-							}
+			$xml = simplexml_load_file($fileInstance->getPath());
+			foreach ($xml as $fieldsKey => $fieldsValue) {
+				foreach ($fieldsValue as $fieldKey => $fieldValue) {
+					foreach ($fieldValue as $columnKey => $columnValue) {
+						switch ($columnKey) {
+							case 'imageblob':
+								$base64Image = (string) $columnValue;
+								break;
+							case 'watermark_image':
+								$imagePath = (string) $columnValue;
+								$pdfModel->set($columnKey, '');
+								break;
+							default:
+								$value = (string) $columnValue;
+								$pdfModel->set($columnKey, $value);
 						}
 					}
 				}
-				Settings_PDF_Record_Model::save($pdfModel, 'import');
-
-				if ($pdfModel->getId() && $imagePath != '' && $base64Image) {
-					$targetDir = Settings_PDF_Module_Model::$uploadPath;
-					$imageExt = end(explode('.', basename($imagePath)));
-					$imageData = base64_decode($base64Image);
-					$newFilePath = $targetDir . $pdfModel->getId() . '.' . $imageExt;
-
-					$pdfModel->set('watermark_image', $newFilePath);
-					Settings_PDF_Record_Model::save($pdfModel, 8);
-					file_put_contents($newFilePath, $imageData);
-				}
-				$viewer->assign('RECORDID', $pdfModel->getId());
-				$viewer->assign('UPLOAD', true);
-			} else {
-				$viewer->assign('UPLOAD_ERROR', \App\Language::translate('LBL_UPLOAD_ERROR', $qualifiedModule));
-				$viewer->assign('UPLOAD', false);
 			}
+			Settings_PDF_Record_Model::save($pdfModel, 'import');
+			if ($pdfModel->getId() && $imagePath && $base64Image) {
+				$targetDir = Settings_PDF_Module_Model::$uploadPath;
+				$imageInstance = \App\Fields\File::loadFromInfo([
+						'content' => base64_decode($base64Image),
+						'path' => $imagePath,
+						'name' => 'watermark_image',
+						'size' => 1,
+						'validateAllCodeInjection' => true
+				]);
+				if (!$imageInstance->validate('image')) {
+					throw new \App\Exceptions\Security('ERR_ILLEGAL_WATERMARK_IMAGE');
+				}
+				$newFilePath = $targetDir . $pdfModel->getId() . '.' . $imageInstance->getExtension();
+				$pdfModel->set('watermark_image', $newFilePath);
+				Settings_PDF_Record_Model::save($pdfModel, 8);
+				file_put_contents($newFilePath, $imageInstance->getContents());
+			}
+			$viewer->assign('RECORDID', $pdfModel->getId());
+			$viewer->assign('UPLOAD', true);
 		}
-
 		$viewer->assign('QUALIFIED_MODULE', $qualifiedModule);
 		$viewer->view('Import.tpl', $qualifiedModule);
 		\App\Log::trace('End ' . __METHOD__);
