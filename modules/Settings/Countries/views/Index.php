be1730902639e93beb38144c9e78f39ffa83c44c
Wojciech Brüggemann <w.bruggemann@yetiforce.com>
2017-12-05 10:47:32 +0100

    Added list of countries in administration

---
+++ modules/Settings/Countries/views/Index.php
@@ -0,0 +1,20 @@
+<?php
+
+class Settings_Countries_Index_View extends Settings_Vtiger_Index_View
+{
+
+	/**
+	 * Process
+	 * @param \App\Request $request
+	 */
+	public function process(\App\Request $request)
+	{
+		$moduleName = $request->getModule();
+		$qualifiedModuleName = $request->getModule(false);
+
+		$viewer = $this->getViewer($request);
+		$viewer->assign('QUALIFIED_MODULE', $qualifiedModuleName);
+		$viewer->assign('MODULE', $moduleName);
+		$viewer->view('Index.tpl', $qualifiedModuleName);
+	}
+}
