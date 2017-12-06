404bcc08284dd4e95cade74b380d6303556072f0
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-12-06 12:41:21 +0100

    Improved mail bar for new field

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.2.608',
+	'appVersion' => '4.2.609',
 	'patchVersion' => '2017.12.06',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.36',
+	'lib_roundcube' => '0.0.37',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_gantt' => '0.0.1',
 ];
