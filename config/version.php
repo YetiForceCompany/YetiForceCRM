90ce0433187e2809fb054acf166975775c82ad76
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-12-08 13:50:29 +0100

    Fixed Multipicklist

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.2.619',
-	'patchVersion' => '2017.12.07',
+	'appVersion' => '4.2.620',
+	'patchVersion' => '2017.12.08',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.37',
 	'lib_PHPExcel' => '0.0.0',
