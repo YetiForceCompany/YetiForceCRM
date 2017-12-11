9a86fb5975b2578fcfaa01a0f294aac8b4908a66
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-12-11 22:44:50 +0100

    System update functionality has been improved

--- vtlib/Vtiger/PackageImport.php
+++ vtlib/Vtiger/PackageImport.php
@@ -1029,6 +1029,11 @@ class PackageImport extends PackageExport
 		}
 		Functions::recurseDelete($dirName);
 		Functions::recurseDelete('cache/templates_c');
+
+		\vtlib\Access::syncSharingAccess();
+		\vtlib\Deprecated::createModuleMetaFile();
+		\App\Cache::clear();
+		\App\Cache::clearOpcache();
 		file_put_contents('cache/logs/update.log', ob_get_contents(), FILE_APPEND);
 		ob_end_clean();
 	}
