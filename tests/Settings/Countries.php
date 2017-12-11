a8faca6b04c54c0d68b13ab1fae4d79a0442686d
Wojciech Brüggemann <w.bruggemann@yetiforce.com>
2017-12-11 11:12:23 +0100

    Added unit tests

--- tests/Settings/Countries.php
+++ tests/Settings/Countries.php
@@ -24,6 +24,7 @@ class Countries extends \Tests\Base
 	 */
 	public function testUpdateSequence()
 	{
+		$moduleModel = new \Settings_Countries_Module_Model();
 		$rows = $this->allRows();
 		$keys = [];
 		$values = [];
@@ -31,12 +32,58 @@ class Countries extends \Tests\Base
 			$keys[] = $row['sortorderid'];
 			$values[] = $row['id'];
 		}
+		$originKeys = $keys;
 		shuffle($keys);
 		$sequence = array_combine($keys, $values);
-		$moduleModel = new \Settings_Countries_Module_Model();
 		$moduleModel->updateSequence($sequence);
 		$rows2 = $this->allRows();
 		$this->assertTrue($rows !== $rows2);
+
+		$sequence = array_combine($originKeys, $values);
+		$moduleModel->updateSequence($sequence);
+		$rows3 = $this->allRows();
+		$this->assertTrue($rows === $rows3);
+	}
+
+	public function testUpdateStatus()
+	{
+		$moduleModel = new \Settings_Countries_Module_Model();
+		$row = (new \App\Db\Query())->from('u_#__countries')->one();
+		$id = $row['id'];
+		$status = $row['status'] ? 0 : 1;
+		$result = $moduleModel->updateStatus($id, $status);
+		$this->assertGreaterThan(0, $result);
+		$status2 = $this->scalarOfField($id, 'status');
+		$this->assertEquals($status, $status2);
+	}
+
+	public function testUpdatePhone()
+	{
+		$moduleModel = new \Settings_Countries_Module_Model();
+		$row = (new \App\Db\Query())->from('u_#__countries')->one();
+		$id = $row['id'];
+		$phone = $row['phone'] ? 0 : 1;
+		$result = $moduleModel->updatePhone($id, $phone);
+		$this->assertGreaterThan(0, $result);
+		$status2 = $this->scalarOfField($id, 'phone');
+		$this->assertEquals($phone, $status2);
+	}
+
+	public function testUpdateUitype()
+	{
+		$moduleModel = new \Settings_Countries_Module_Model();
+		$row = (new \App\Db\Query())->from('u_#__countries')->one();
+		$id = $row['id'];
+		$uitype = $row['uitype'] ? 0 : 1;
+		$result = $moduleModel->updateUitype($id, $uitype);
+		$this->assertGreaterThan(0, $result);
+		$status2 = $this->scalarOfField($id, 'uitype');
+		$this->assertEquals($uitype, $status2);
+	}
+
+	protected function scalarOfField($id, $fieldName)
+	{
+		return (new \App\Db\Query())->from('u_#__countries')->select($fieldName)->where(['id' => $id])->scalar();
 	}
 
 	protected function allRows()
