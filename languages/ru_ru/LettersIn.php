eda10cb78b1c41970389251c9c875960f48718b9
Paula W <p.walenczak@yetiforce.com>
2017-12-13 14:44:21 +0100

    Added translations - LettersIn.php

--- languages/ru_ru/LettersIn.php
+++ languages/ru_ru/LettersIn.php
@@ -30,6 +30,8 @@ $languageStrings = [
 	'Contact' => 'Контакт',
 	'FL_CUSTOM_SENDER' => 'Customized sender',
 	'FL_TYPE' => 'Type',
+	'FL_CASH_AMOUNT_ON_DELIVERY' => 'Amount due on delivery',
+	'FL_DATE_OF_RECEIPT' => 'Reception date',
 	// picklist values
 	'PLL_LOCAL_PICKUP' => 'Внутренняя почта',
 	'PLL_POST_OFFICE_PL' => 'Иностранная почта',
