5e949a0a74749d23c44ad37134db83f52ee01d8a
Paula W <p.walenczak@yetiforce.com>
2017-12-13 14:40:27 +0100

    Added translations - LettersIn.php

--- languages/en_us/LettersIn.php
+++ languages/en_us/LettersIn.php
@@ -30,6 +30,8 @@ $languageStrings = [
 	'Contact' => 'Contact',
 	'FL_CUSTOM_SENDER' => 'Customized sender',
 	'FL_TYPE' => 'Type',
+	'FL_CASH_AMOUNT_ON_DELIVERY' => 'Amount due on delivery',
+	'FL_DATE_OF_RECEIPT' => 'Reception date',
 	// picklist values
 	'PLL_LOCAL_PICKUP' => 'Collection in person',
 	'PLL_POST_OFFICE_PL' => 'Polish Post Office',
