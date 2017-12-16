6e388724178a5a665c27dcad45024e111d59e84b
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2017-12-16 17:56:06 +0100

    Fixed saving forms

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.2.655',
-	'patchVersion' => '2017.12.15',
+	'appVersion' => '4.2.656',
+	'patchVersion' => '2017.12.16',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.37',
 	'lib_PHPExcel' => '0.0.0',
