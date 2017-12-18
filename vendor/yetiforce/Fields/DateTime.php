097b32edd3cd2f8ecddbc6635739421cdd96fb76
areksolek <a.solek@yetiforce.com>
2017-12-18 11:18:58 +0100

    A function has been created that returns the date according to the user's settings

---
+++ vendor/yetiforce/Fields/DateTime.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Tools for datetime class
+ * @package YetiForce.App
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author Arkadiusz Sołek <a.solek@yetiforce.com>
+ */
+namespace App\Fields;
+
+/**
+ * DateTime class
+ */
+class DateTime
+{
+
+	/**
+	 * The function returns the date according to the user's settings
+	 * @param string $dateTime
+	 * @return string
+	 */
+	public static function getViewDateFormat($dateTime)
+	{
+		switch (\App\User::getCurrentUserModel()->getDetail('view_date_format')) {
+			case 'PLL_FULL':
+				return '<span title="' . \Vtiger_Util_Helper::formatDateDiffInStrings($dateTime) . '">' . \Vtiger_Datetime_UIType::getDisplayDateTimeValue($dateTime) . '</span>';
+				break;
+			case 'PLL_ELAPSED':
+				return '<span title="' . \Vtiger_Datetime_UIType::getDisplayDateTimeValue($dateTime) . '">' . \Vtiger_Util_Helper::formatDateDiffInStrings($dateTime) . '</span>';
+				break;
+		}
+	}
+}
