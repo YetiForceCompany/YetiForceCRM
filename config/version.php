0f31b07a544e4ee994b0cf815675828b9a4c41d2
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-12-21 09:08:55 +0100

    Configuration verification has been improved

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.2.688',
-	'patchVersion' => '2017.12.20',
+	'appVersion' => '4.2.689',
+	'patchVersion' => '2017.12.21',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.37',
 	'lib_PHPExcel' => '0.0.0',
