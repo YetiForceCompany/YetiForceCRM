d5e12eb51aadade46a50e12a67542aa546b98b8a
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-12-23 12:29:15 +0100

    Added Italian translate

---
+++ languages/it_it/FBookkeeping.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * FBookkeeping english translation
+ * @package YetiForce.Language
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ */
+$languageStrings = [
+	'FBookkeeping' => 'Bookkeeping',
+	'SINGLE_FBookkeeping' => 'Bookkeeping',
+	'FBookkeeping ID' => 'Bookkeeping ID',
+	'FL_SUBJECT' => 'Subject',
+];
