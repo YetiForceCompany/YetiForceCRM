7eadc611d30117595e0a57b25aa058065fdee8bb
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-01-02 13:03:28 +0100

    [Insight] Error silenced by the at sign (@)

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '4.3.4',
+	'appVersion' => '4.3.5',
 	'patchVersion' => '2018.01.02',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.38',
--- modules/OSSMail/models/Record.php
+++ modules/OSSMail/models/Record.php
@@ -124,7 +124,7 @@ class OSSMail_Record_Model extends Vtiger_Record_Model
 		}
 		static::$imapConnectMailbox = "{{$host}:{$port}/imap{$sslMode}{$validatecert}}{$folder}";
 		\App\Log::trace("imap_open(({" . static::$imapConnectMailbox . ", $user , $password. $options, $maxRetries, " . var_export($params, true) . ') method ...');
-		$mbox = @imap_open(static::$imapConnectMailbox, $user, $password, $options, $maxRetries, $params);
+		$mbox = imap_open(static::$imapConnectMailbox, $user, $password, $options, $maxRetries, $params);
 		if (!$mbox) {
 			\App\Log::error('Error OSSMail_Record_Model::imapConnect(): ' . imap_last_error());
 			if ($dieOnError) {
