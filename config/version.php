4459f8de980c55bf2782a5595a8c6f452bd1d647
Tomasz Kur <t.kur@yetiforce.com>
2018-01-03 19:41:49 +0100

    Improved sql query

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '4.3.13',
+	'appVersion' => '4.3.14',
 	'patchVersion' => '2018.01.03',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.38',
--- vendor/yetiforce/Fields/Email.php
+++ vendor/yetiforce/Fields/Email.php
@@ -40,10 +40,8 @@ class Email
 	 */
 	public static function findCrmidByEmail($value, $allowedModules = [], $skipModules = [])
 	{
-		$db = \PearDatabase::getInstance();
-		$rows = $ids = $params = $fields = [];
-		$query = '';
-		$countWhere = 0;
+		$db = \App\Db::getInstance();
+		$rows = $fields = [];
 		$dataReader = (new \App\Db\Query())->select(['vtiger_field.columnname', 'vtiger_field.tablename', 'vtiger_field.fieldlabel', 'vtiger_field.tabid', 'vtiger_tab.name'])
 				->from('vtiger_field')->innerJoin('vtiger_tab', 'vtiger_field.tabid = vtiger_tab.tabid')
 				->where(['vtiger_tab.presence' => 0])
@@ -52,45 +50,34 @@ class Email
 		while ($row = $dataReader->read()) {
 			$fields[$row['name']][$row['tablename']][$row['columnname']] = $row;
 		}
-		foreach ($fields as $moduleName => &$moduleFields) {
+		$queryUnion = null;
+		foreach ($fields as $moduleName => $moduleFields) {
 			if (($allowedModules && !in_array($moduleName, $allowedModules)) || in_array($moduleName, $skipModules)) {
 				continue;
 			}
 			$instance = \CRMEntity::getInstance($moduleName);
 			$isEntityType = isset($instance->tab_name_index['vtiger_crmentity']);
-			$join = $where = [];
-			foreach ($moduleFields as $tablename => &$columns) {
+			foreach ($moduleFields as $tablename => $columns) {
 				$tableIndex = $instance->tab_name_index[$tablename];
-				$query .= sprintf(' UNION (SELECT %s AS id, \'%s\' AS setype FROM %s', $tableIndex, $moduleName, $tablename);
-				foreach ($columns as $columnName => &$row) {
-					$join[$tablename] = $row;
-					$where[] = $tablename . '.' . $columnName;
-					$countWhere++;
+				$query = (new \App\Db\Query())->select(['crmid' => $tableIndex, 'modules' => new \yii\db\Expression($db->quoteValue($moduleName))])
+					->from($tablename);
+				if ($isEntityType) {
+					$query->innerJoin('vtiger_crmentity', "vtiger_crmentity.crmid = {$tablename}.{$tableIndex}")->where(['vtiger_crmentity.deleted' => 0]);
+				}
+				$orWhere = ['or'];
+				foreach ($columns as $row) {
+					$orWhere[] = ["{$row['tablename']}.{$row['columnname']}" => $value];
+				}
+				$query->andWhere($orWhere);
+				if ($queryUnion) {
+					$queryUnion->union($query);
+				} else {
+					$queryUnion = $query;
 				}
 			}
-			$whereQuery = ' WHERE';
-			if ($isEntityType) {
-				foreach ($join as $columnName => &$row) {
-					$query .= sprintf(' INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid=%s', $tablename . '.' . $tableIndex);
-				}
-				$whereQuery .= ' vtiger_crmentity.deleted = 0 AND';
-			}
-			$whereQuery .= ' (';
-			$or = '';
-			foreach ($where as $columnName) {
-				$or .= sprintf(' OR %s = ?', $columnName);
-			}
-			$query .= $whereQuery . ltrim($or, ' OR ') . '))';
 		}
-		for ($index = 0; $index < $countWhere; $index++) {
-			$params[] = $value;
-		}
-		$result = $db->pquery(ltrim($query, ' UNION '), $params);
-		while ($row = $db->getRow($result)) {
-			$ids[] = $row['id'];
-			$rows[] = ['crmid' => $row['id'], 'modules' => $row['setype'], 'label' => isset($row['label']) ? $row['label'] : false];
-		}
-		$labels = \App\Record::getLabel($ids);
+		$rows = $queryUnion->all();
+		$labels = \App\Record::getLabel(array_column($rows, 'crmid'));
 		foreach ($rows as &$row) {
 			$row['label'] = $labels[$row['crmid']];
 		}
@@ -104,7 +91,6 @@ class Email
 	 */
 	public static function getUserMail($userId)
 	{
-		$userModel = \App\User::getUserModel($userId);
-		return $userModel->getDetail('email1');
+		return \App\User::getUserModel($userId)->getDetail('email1');
 	}
 }
