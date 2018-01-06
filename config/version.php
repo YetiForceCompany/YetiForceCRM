e9f28e2ec76576725645d317c72319b57ac70adc
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2018-01-06 14:11:27 +0100

    Fixed #5832

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.3.26',
-	'patchVersion' => '2018.01.05',
+	'appVersion' => '4.3.27',
+	'patchVersion' => '2018.01.06',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.38',
 	'lib_PHPExcel' => '0.0.0',
