30e92b70e83357feffab9ca8b09c80301f02a775
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-01-08 00:04:13 +0100

    Added to optimize database queries

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.3.34',
-	'patchVersion' => '2018.01.07',
+	'appVersion' => '4.3.35',
+	'patchVersion' => '2018.01.08',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.38',
 	'lib_PHPExcel' => '0.0.0',
