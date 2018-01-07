8ea8191b5bac469032958fcdb922e25b57dc044a
Tomasz Kur <t.kur@yetiforce.com>
2018-01-07 22:30:09 +0100

    Improved sql queries

--- modules/Vtiger/models/Record.php
+++ modules/Vtiger/models/Record.php
@@ -811,16 +811,15 @@ class Vtiger_Record_Model extends \App\Base
 
 	/**
 	 * Function to delete corresponding image
-	 * @param <type> $imageId
+	 * @param int $imageId
 	 */
 	public function deleteImage($imageId)
 	{
-		$db = PearDatabase::getInstance();
-		$checkResult = $db->pquery('SELECT crmid FROM vtiger_seattachmentsrel WHERE attachmentsid = ?', [$imageId]);
-		$crmId = $db->queryResult($checkResult, 0, 'crmid');
+		$crmId = (new App\Db\Query())->select(['crmid'])->from('vtiger_seattachmentsrel')->where(['attachmentsid' => $imageId])->scalar();
 		if ($this->getId() == $crmId) {
-			$db->pquery('DELETE FROM vtiger_attachments WHERE attachmentsid = ?', [$imageId]);
-			$db->pquery('DELETE FROM vtiger_seattachmentsrel WHERE attachmentsid = ?', [$imageId]);
+			$dbCommand = \App\Db::getInstance()->createCommand();
+			$dbCommand->delete('vtiger_attachments', ['attachmentsid' => $imageId])->execute();
+			$dbCommand->delete('vtiger_seattachmentsrel', ['attachmentsid' => $imageId])->execute();
 			return true;
 		}
 		return false;
