0974de4a51fcb29eaaa4397c96f0e1e5eca56df0
areksolek <a.solek@yetiforce.com>
2018-01-08 08:10:05 +0100

    Update commit

--- api/webservice/Core/Auth/Digest.php
+++ api/webservice/Core/Auth/Digest.php
@@ -46,7 +46,7 @@ class DigestAuth
 		$this->api->response->setStatus(401);
 	}
 
-	public function getDigestHash($username)
+	public function getDigestHash($realm, $username)
 	{
 		$stmt = $this->pdo->prepare(sprintf('SELECT digesta1 FROM %s WHERE username = ?', $this->tableName));
 		$stmt->execute([$username]);
