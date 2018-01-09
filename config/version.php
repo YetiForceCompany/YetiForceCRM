85e58508e3249ef2e4d5393767770ec79c630c0f
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-01-09 10:44:28 +0100

    Improved functionality verification of input data

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.3.39',
-	'patchVersion' => '2018.01.08',
+	'appVersion' => '4.3.40',
+	'patchVersion' => '2018.01.09',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.38',
 	'lib_PHPExcel' => '0.0.0',
