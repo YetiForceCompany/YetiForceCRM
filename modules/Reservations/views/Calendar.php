aabf4b091509d05f7fcfe672e67984db9002fad0
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-01-10 13:17:33 +0100

    Corrected path.

--- modules/Reservations/views/Calendar.php
+++ modules/Reservations/views/Calendar.php
@@ -58,7 +58,7 @@ class Reservations_Calendar_View extends Vtiger_Index_View
 	{
 		$headerCssInstances = parent::getHeaderCss($request);
 		$cssFileNames = [
-			'~libraries/fullcalendar/dist/fullcalendar.min.css'
+			'~libraries/fullcalendar/dist/fullcalendar.css'
 		];
 		$cssInstances = $this->checkAndConvertCssStyles($cssFileNames);
 		$headerCssInstances = array_merge($headerCssInstances, $cssInstances);
