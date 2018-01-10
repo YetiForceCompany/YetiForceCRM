2e237a51c5cd440920890a0bf9fad4d24831f88d
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-01-10 13:12:03 +0100

    Deleted calendar useless path.

--- modules/OSSTimeControl/views/Calendar.php
+++ modules/OSSTimeControl/views/Calendar.php
@@ -58,8 +58,7 @@ class OSSTimeControl_Calendar_View extends Vtiger_Index_View
 	{
 		$headerCssInstances = parent::getHeaderCss($request);
 		$cssFileNames = [
-			'~libraries/fullcalendar/dist/fullcalendar.min.css',
-			'~libraries/fullcalendar/dist/fullcalendarCRM.css',
+			'~libraries/fullcalendar/dist/fullcalendar.min.css'
 		];
 		$cssInstances = $this->checkAndConvertCssStyles($cssFileNames);
 		$headerCssInstances = array_merge($headerCssInstances, $cssInstances);
