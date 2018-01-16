372a761a169123224a28930db84f92a191e802bf
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-01-16 09:43:12 +0100

    Fixed add relations button

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.3.58',
-	'patchVersion' => '2018.01.10',
+	'appVersion' => '4.3.59',
+	'patchVersion' => '2018.01.16',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.38',
 	'lib_PHPExcel' => '0.0.0',
