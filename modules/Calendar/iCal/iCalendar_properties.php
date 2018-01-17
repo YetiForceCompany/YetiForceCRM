d9822853c73dea6a8ec7a9c04a1a88119ab15159
areksolek <a.solek@yetiforce.com>
2018-01-17 15:06:34 +0100

    Class were separated into separate files

--- modules/Calendar/iCal/iCalendar_properties.php
+++ modules/Calendar/iCal/iCalendar_properties.php
@@ -186,37 +186,6 @@ class IcalendarProperty
 	}
 }
 
-// 4.8.8 Miscellaneous Component Properties
-// ----------------------------------------
-
-class IcalendarPropertyX extends IcalendarProperty
-{
-
-	public $name = RFC2445_XNAME;
-	public $val_type = NULL;
-
-	public function construct()
-	{
-		$this->valid_parameters = [
-			'LANGUAGE' => RFC2445_OPTIONAL | RFC2445_ONCE,
-			RFC2445_XNAME => RFC2445_OPTIONAL
-		];
-	}
-
-	public function setName($name)
-	{
-
-		$name = strtoupper($name);
-
-		if (rfc2445_is_xname($name)) {
-			$this->name = $name;
-			return true;
-		}
-
-		return false;
-	}
-}
-
 class IcalendarPropertyRequeststatus extends IcalendarProperty
 {
 
