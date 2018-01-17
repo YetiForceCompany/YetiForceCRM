bf8aa5eccbd06fd7bb048f1563bbc2b6a2731ae9
areksolek <a.solek@yetiforce.com>
2018-01-17 12:44:02 +0100

    Classes were separated into separate files

--- modules/Calendar/iCal/iCalendar_properties.php
+++ modules/Calendar/iCal/iCalendar_properties.php
@@ -186,23 +186,6 @@ class IcalendarProperty
 	}
 }
 
-class IcalendarPropertyProdid extends IcalendarProperty
-{
-
-	public $name = 'PRODID';
-	public $val_type = RFC2445_TYPE_TEXT;
-	public $val_default = NULL;
-
-	public function construct()
-	{
-		$this->val_default = '-//YetiForce CRM//YetiForce CRM ' . \App\Version::get() . '//EN';
-
-		$this->valid_parameters = [
-			RFC2445_XNAME => RFC2445_OPTIONAL
-		];
-	}
-}
-
 class IcalendarPropertyVersion extends IcalendarProperty
 {
 
