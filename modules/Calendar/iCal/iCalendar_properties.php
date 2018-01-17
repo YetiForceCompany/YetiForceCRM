7a3481f67becbdecafb15b5b1d5a4d86418989bc
areksolek <a.solek@yetiforce.com>
2018-01-17 13:33:31 +0100

    Class were separated into separate files

--- modules/Calendar/iCal/iCalendar_properties.php
+++ modules/Calendar/iCal/iCalendar_properties.php
@@ -186,28 +186,6 @@ class IcalendarProperty
 	}
 }
 
-class IcalendarPropertyClass extends IcalendarProperty
-{
-
-	public $name = 'CLASS';
-	public $val_type = RFC2445_TYPE_TEXT;
-	public $val_default = 'PUBLIC';
-
-	public function construct()
-	{
-		$this->valid_parameters = [
-			RFC2445_XNAME => RFC2445_OPTIONAL
-		];
-	}
-
-	public function isValidValue($value)
-	{
-		$value = strtoupper($value);
-		// If this is not an xname, it is case-sensitive
-		return ($value === 'PUBLIC' || $value === 'PRIVATE' || $value === 'CONFIDENTIAL' || rfc2445_is_xname(strtoupper($value)));
-	}
-}
-
 class IcalendarPropertyComment extends IcalendarProperty
 {
 
