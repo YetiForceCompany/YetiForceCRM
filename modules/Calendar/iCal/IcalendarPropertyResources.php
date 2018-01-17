760f97e03a6085a76b9daddf3b5d3b8e5e7f42da
areksolek <a.solek@yetiforce.com>
2018-01-17 14:03:41 +0100

    Class were separated into separate files

---
+++ modules/Calendar/iCal/IcalendarPropertyResources.php
@@ -0,0 +1,18 @@
+<?php
+
+class IcalendarPropertyResources extends IcalendarProperty
+{
+
+	public $name = 'RESOURCES';
+	public $val_type = RFC2445_TYPE_TEXT;
+	public $val_multi = true;
+
+	public function construct()
+	{
+		$this->valid_parameters = [
+			'ALTREP' => RFC2445_OPTIONAL | RFC2445_ONCE,
+			'LANGUAGE' => RFC2445_OPTIONAL | RFC2445_ONCE,
+			RFC2445_XNAME => RFC2445_OPTIONAL
+		];
+	}
+}
