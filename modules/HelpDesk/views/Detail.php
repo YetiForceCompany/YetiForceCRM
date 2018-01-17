df123e3c51eb59f66c38bcd9a8af3deec23cece3
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-01-17 09:38:24 +0100

    Fixed detail files

---
+++ modules/HelpDesk/views/Detail.php
@@ -0,0 +1,41 @@
+<?php
+/* +***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * *********************************************************************************** */
+
+class HelpDesk_Detail_View extends Vtiger_Detail_View
+{
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->exposeMethod('showRelatedRecords');
+		$this->exposeMethod('showCharts');
+	}
+
+	public function showCharts(\App\Request $request)
+	{
+		$recordId = $request->getInteger('record');
+		$moduleName = $request->getModule();
+
+		$viewer = $this->getViewer($request);
+		if (!Users_Privileges_Model::getCurrentUserPrivilegesModel()->hasModulePermission('OSSTimeControl')) {
+			throw new \App\Exceptions\NoPermitted('LBL_PERMISSION_DENIED', 406);
+		}
+		$moduleModel = Vtiger_Module_Model::getInstance('OSSTimeControl');
+		if ($moduleModel && $moduleModel->isActive()) {
+			$data = $moduleModel->getTimeUsers($recordId, $moduleName);
+		}
+		$viewer->assign('MODULE_NAME', $moduleName);
+		$viewer->assign('DATA', $data);
+		$viewer->view('charts/ShowTimeHelpDesk.tpl', $moduleName);
+	}
+}
---
+++ modules/Leads/views/Detail.php
@@ -0,0 +1,23 @@
+<?php
+/* +***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * *********************************************************************************** */
+
+class Leads_Detail_View extends Vtiger_Detail_View
+{
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function preProcess(\App\Request $request, $display = true)
+	{
+		$viewer = $this->getViewer($request);
+		$viewer->assign('CONVERSION_AVAILABLE_STATUS', \App\Json::encode(Leads_Module_Model::getConversionAvaibleStatuses()));
+		parent::preProcess($request);
+	}
+}
