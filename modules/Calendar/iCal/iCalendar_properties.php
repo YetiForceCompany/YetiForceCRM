8ff7f32590918209f4a74aa0d886d01564bab59d
areksolek <a.solek@yetiforce.com>
2018-01-17 15:08:20 +0100

    Class were separated into separate files

--- modules/Calendar/iCal/iCalendar_properties.php
+++ modules/Calendar/iCal/iCalendar_properties.php
@@ -186,20 +186,6 @@ class IcalendarProperty
 	}
 }
 
-class IcalendarPropertyTrigger extends IcalendarProperty
-{
-
-	public $name = 'TRIGGER';
-	public $val_type = RFC2445_TYPE_TEXT;
-
-	public function construct()
-	{
-		$this->valid_parameters = [
-			RFC2445_XNAME => RFC2445_OPTIONAL
-		];
-	}
-}
-
 class IcalendarPropertyAction extends IcalendarProperty
 {
 
