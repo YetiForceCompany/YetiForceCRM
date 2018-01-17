eae41f54307f551e03da9cfcfb7ca45cfc36dfbe
areksolek <a.solek@yetiforce.com>
2018-01-17 13:38:23 +0100

    Class were separated into separate files

--- modules/Calendar/iCal/iCalendar_properties.php
+++ modules/Calendar/iCal/iCalendar_properties.php
@@ -186,22 +186,6 @@ class IcalendarProperty
 	}
 }
 
-class IcalendarPropertyComment extends IcalendarProperty
-{
-
-	public $name = 'COMMENT';
-	public $val_type = RFC2445_TYPE_TEXT;
-
-	public function construct()
-	{
-		$this->valid_parameters = [
-			'ALTREP' => RFC2445_OPTIONAL | RFC2445_ONCE,
-			'LANGUAGE' => RFC2445_OPTIONAL | RFC2445_ONCE,
-			RFC2445_XNAME => RFC2445_OPTIONAL
-		];
-	}
-}
-
 class IcalendarPropertyDescription extends IcalendarProperty
 {
 
