3b1a0f6a198c08b7250a722c44df91e15057dd4b
areksolek <a.solek@yetiforce.com>
2018-01-18 09:39:23 +0100

    Update commit

--- modules/Calendar/iCal/IcalendarAlarm.php
+++ modules/Calendar/iCal/IcalendarAlarm.php
@@ -1,6 +1,6 @@
 <?php
+
 // $Id: IcalendarAlarm.php,v 1.8 2005/07/21 22:31:44 defacer Exp $
-require_once('include/utils/utils.php');
 
 class IcalendarAlarm extends IcalendarComponent
 {
--- modules/Calendar/iCal/IcalendarJournal.php
+++ modules/Calendar/iCal/IcalendarJournal.php
@@ -1,6 +1,6 @@
 <?php
+
 // $Id: IcalendarJournal.php,v 1.8 2005/07/21 22:31:44 defacer Exp $
-require_once('include/utils/utils.php');
 
 class IcalendarJournal extends IcalendarComponent
 {
