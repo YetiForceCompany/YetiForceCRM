10d68ce4f780116b425724785387a047cfdf0668
areksolek <a.solek@yetiforce.com>
2018-01-18 12:19:01 +0100

    Added second parameter

--- modules/OSSTimeControl/uitypes/Double.php
+++ modules/OSSTimeControl/uitypes/Double.php
@@ -16,8 +16,7 @@ class OSSTimeControl_Double_UIType extends Vtiger_Double_UIType
 	public function getDisplayValue($value, $record = false, $recordModel = false, $rawText = false, $length = false)
 	{
 		if ($this->get('field')->getFieldName() === 'sum_time') {
-			$return = \App\Fields\DateTime::formatToHourText((double) $value);
-			return $return['short'];
+			return \App\Fields\DateTime::formatToHourText((double) $value, 'short');
 		} else {
 			return parent::getDisplayValue($value, $record, $recordModel, $rawText, $length);
 		}
