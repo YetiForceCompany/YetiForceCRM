661d47f7b1e3c39d6687cf7dc90b2ae83a7f96fb
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-01-22 10:45:45 +0100

    Added space as tokenSeparator to select2 input

--- public_html/layouts/basic/modules/Settings/Workflows/resources/Edit3.js
+++ public_html/layouts/basic/modules/Settings/Workflows/resources/Edit3.js
@@ -525,7 +525,7 @@ Settings_Workflows_Edit_Js("Settings_Workflows_Edit3_Js", {}, {
 		fieldUiHolder.html(fieldSpecificUi);
 		if (fieldSpecificUi.is('input.select2')) {
 			var tagElements = fieldSpecificUi.data('tags');
-			var params = {tags: tagElements, tokenSeparators: [","]}
+			var params = {tags: tagElements, tokenSeparators: [",", " "]}
 			app.showSelect2ElementView(fieldSpecificUi, params)
 		} else if (fieldSpecificUi.is('select')) {
 			if (fieldSpecificUi.hasClass('chzn-select')) {
