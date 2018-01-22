2ad4e3ed023faf847cd92ab19393ec54ce67d751
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-01-22 13:05:25 +0100

    Added correction

--- public_html/layouts/basic/modules/Calendar/resources/CalendarView.js
+++ public_html/layouts/basic/modules/Calendar/resources/CalendarView.js
@@ -122,7 +122,7 @@ jQuery.Class("Calendar_CalendarView_Js", {
 			selectable: true,
 			selectHelper: true,
 			hiddenDays: hiddenDays,
-			height: ($(window).width() > 993) ? ($(window).height() - 150) : 'auto',
+			height: ($(window).width() > 993) ? ($(window).height() - 205) : 'auto',
 			views: {
 				basic: {
 					eventLimit: false,
