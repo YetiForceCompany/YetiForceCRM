e98640128044d564a65e0361a4023e38bce4d92a
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-01-22 09:47:34 +0100

    Added bootstrap-switches to credits

--- config/developer.php
+++ config/developer.php
@@ -9,9 +9,9 @@ $DEVELOPER_CONFIG = [
 	// Turn the possibility to change generatedtype
 	'CHANGE_GENERATEDTYPE' => false,
 	// Enable minimize JS files
-	'MINIMIZE_JS' => true,
+	'MINIMIZE_JS' => false,
 	// Enable minimize CSS files
-	'MINIMIZE_CSS' => true,
+	'MINIMIZE_CSS' => false,
 	// Change of fields visibility
 	'CHANGE_VISIBILITY' => false,
 	// Adding/Deleting relations between modules.
