e164556ed01195a8a771f784f6815d17dcf7c01f
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-01-25 21:31:00 +0100

    Yarn update

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.3.89',
-	'patchVersion' => '2018.01.24',
+	'appVersion' => '4.3.90',
+	'patchVersion' => '2018.01.25',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.38',
 	'lib_gantt' => '0.0.1',
