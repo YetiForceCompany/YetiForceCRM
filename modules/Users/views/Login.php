5384445b5659f5e4ff639d5e377d586798a0c869
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-01-29 11:20:12 +0100

    Login directory changed

--- modules/Users/views/Login.php
+++ modules/Users/views/Login.php
@@ -83,7 +83,7 @@ class Users_Login_View extends Vtiger_View_Controller
 		$headerCssInstances = parent::getHeaderCss($request);
 
 		$cssFileNames = [
-			'~layouts/basic/styles/pages/login.css',
+			'~layouts/basic/styles/pages/login/login.css',
 		];
 		$cssInstances = $this->checkAndConvertCssStyles($cssFileNames);
 		$headerCssInstances = array_merge($headerCssInstances, $cssInstances);
