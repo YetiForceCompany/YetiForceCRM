f4702b646a0d44fbfb6d2b9a6c5137e04fb062b4
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-02-01 13:37:25 +0100

    Update configuration .sensiolabs

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.3.99',
-	'patchVersion' => '2018.01.31',
+	'appVersion' => '4.3.100',
+	'patchVersion' => '2018.02.01',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.38',
 	'lib_gantt' => '0.0.1',
