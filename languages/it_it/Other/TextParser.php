4dd4907397f7dff4e83988d067b75c75d4f46467
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2018-02-05 11:43:39 +0100

    Updated translations for TextParsr

---
+++ languages/it_it/Other/TextParser.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * TextParser italian translation
+ * @package YetiForce.Language
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+$languageStrings = [
+	'LBL_TRANSLATE' => 'Translation',
+	'LBL_COMPANY_VARIABLES' => 'Company details',
+	'LBL_CUSTOM_VARIABLES' => 'Custom variables',
+	'LBL_EMPLOYEE_NAME' => 'Employee name',
+	'LBL_CURRENT_DATE' => 'Current date',
+	'LBL_CURRENT_TIME' => 'Current time',
+	'LBL_BASE_TIMEZONE' => 'System time zone',
+	'LBL_USER_TIMEZONE' => 'User time zone',
+	'LBL_SITE_URL' => 'Site URL',
+	'LBL_PORTAL_URL' => 'Portal URL',
+	'LBL_CRM_DETAIL_VIEW_URL' => 'Record\'s URL address in CRM',
+	'LBL_PORTAL_DETAIL_VIEW_URL' => 'Record\'s URL address in Portal',
+	'LBL_RECORD_ID' => 'Record ID',
+	'LBL_RECORD_LABEL' => 'Record label',
+	'LBL_LIST_OF_CHANGES_IN_RECORD' => 'List of changes in record',
+	'LBL_LIST_OF_NEW_VALUES_IN_RECORD' => 'List of new values in record',
+	'LBL_RECORD_COMMENT' => 'Comment(s)',
+	'LBL_RELATED_RECORD_LABEL' => 'Related record label',
+	'LBL_OWNER_EMAIL' => 'Email owner',
+	'LBL_MODULE_FIELDS' => 'Fields from module',
+	'LBL_DEPENDENT_MODULE_FIELDS' => 'Fields from dependent modules',
+	'LBL_ADDITIONAL_VARIABLES' => 'Additional variables',
+	'LBL_SOURCE_MODULE_FIELDS' => 'Fields from source module',
+	'LBL_ENTITY_VARIABLES' => 'General variables',
+	'LBL_COPY_TO_CLIPBOARD' => 'Copy to clipboard',
+	'LBL_COPY_VALUE' => 'Field value',
+	'LBL_COPY_LABEL' => 'Field name',
+	'LBL_AUTOMATIC' => 'Automatic',
+	'LBL_RELATED_RECORDS_LIST' => 'Lista dei record collegati',
+	'LBL_RECORDS_LIST' => 'List of all records',
+  	// PDF variable
+	'LBL_PRODUCTS_TABLE' => '',
+	'LBL_PRODUCTS_TABLE_DESCRIPTION' => '',
+	'LBL_PRODUCTS_TABLE_NEW' => '',
+	'LBL_PRODUCTS_TABLE_RELATED_MODULE' => '',
+  	'LBL_GROSS_AMOUNT_IN_WORDS' => '',
+  	'LBL_TABLE_DISCOUNT_SUMMARY' => '',
+  	'LBL_OVERDUE_DEADLINES' => '',
+  	'LBL_TABLE_TAX_SUMMARY' => '',
+];
