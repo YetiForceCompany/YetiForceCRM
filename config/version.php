27aeb3940199b15c2845da417364f198386bf3ff
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2018-02-08 23:41:38 +0100

    Fixed class name

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'appVersion' => '4.3.125',
+	'appVersion' => '4.3.126',
 	'patchVersion' => '2018.02.08',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.38',
--- vtlib/Vtiger/Link.php
+++ vtlib/Vtiger/Link.php
@@ -306,7 +306,7 @@ class Link
 	public static function getLinkData($linkId)
 	{
 		if (\App\Cache::has('Link', $linkId)) {
-			return App\Cache::get('Link', $linkId);
+			return \App\Cache::get('Link', $linkId);
 		}
 		$linkData = (new \App\Db\Query())->from('vtiger_links')->where(['linkId' => $linkId])->one();
 		\App\Cache::save('Link', $linkId, $linkData);
