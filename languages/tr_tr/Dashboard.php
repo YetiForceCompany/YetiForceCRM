c076a49ebdb0162442e1a7063d86e425c5ea4377
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-10 19:00:34 +0300

    Create Dashboard.php

---
+++ languages/tr_tr/Dashboard.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Dashboard english translation
+ * @package YetiForce.Language
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ */
+$languageStrings = [
+	// Others
+	'Dashboards' => 'Ana Sayfam',
+];
