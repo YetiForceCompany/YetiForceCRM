e4e38f5811af8eb898ec675d352f122f7f7bb177
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-10 19:27:08 +0300

    Create FBookkeeping.php

---
+++ languages/tr_tr/FBookkeeping.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * FBookkeeping english translation
+ * @package YetiForce.Language
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ */
+$languageStrings = [
+	'FBookkeeping' => 'Defter tutma',
+	'SINGLE_FBookkeeping' => 'Defter tutma',
+	'FBookkeeping ID' => 'Defter Tutma ID',
+	'FL_SUBJECT' => 'Konu',
+];
