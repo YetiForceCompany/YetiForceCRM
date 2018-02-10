2e88f5df748242fa8b5799b599521817dde9283f
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-10 19:00:00 +0300

    Create CustomView.php

---
+++ languages/tr_tr/CustomView.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * CustomView english translation
+ * @package YetiForce.Language
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ */
+$languageStrings = [
+	'CustomView' => 'Filtreler - Yapılandırma',
+	'LBL_FEATURED' => 'Favorilere ekle',
+	'LBL_COLOR_VIEW' => 'Rengi göster',
+];
