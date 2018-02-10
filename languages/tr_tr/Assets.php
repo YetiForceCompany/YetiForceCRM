ff7ec38f419382283669044dd9e27559be7f6628
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-10 18:12:02 +0300

    Create Assets.php

---
+++ languages/tr_tr/Assets.php
@@ -0,0 +1,53 @@
+<?php		
+/* +***********************************************************************************		
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0		
+ * ("License"); You may not use this file except in compliance with the License		
+ * The Original Code is:  vtiger CRM Open Source		
+ * The Initial Developer of the Original Code is vtiger.		
+ * Portions created by vtiger are Copyright (C) vtiger.		
+ * All Rights Reserved.		
+ * Contributor(s): YetiForce.com		
+ * ********************************************************************************** */		
+$languageStrings 	 [	
+	// Basic Strings	
+	'Assets' => 'Varlıklar',
+	'SINGLE_Assets' => 'Varlık',
+	// Blocks	
+	'LBL_ASSET_INFORMATION' => 'Varlık ayrıntıları',
+	//Field Labels	
+	'Asset No' => 'Varlık numarası',
+	'Serial Number' => 'Seri numarası',
+	'Date Sold' => 'Satılan tarih',
+	'Date in Service' => 'Son kullanma tarihi',
+	'Invoice Name' => 'Fatura numarası',
+	'Shipping Tracking Number' => 'Nakliye takip numarası',
+	'Asset Name' => 'İsim',
+	'Customer Name' => 'Müşteri adı',
+	'Notes' => 'Notlar',
+	'FL_RENEWAL' => 'Yenileme',
+	'LBL_SET_RENEWAL' => 'Yenilemeyi ayarla',
+	'FL_RENEWAL_INVOICE' => 'Yenileme faturası',
+	'FL_CONTACT' => 'İletişim',
+	/* picklist values */	
+	'PLL_DRAFT' => 'Taslak',
+	'PLL_ACCEPTED' => 'Kabul Edildi'
+	'PLL_CANCELLED' => 'İptal edildi',
+	'PLL_PLANNED' => 'Planlı',
+	'PLL_WAITING_FOR_RENEWAL' => 'Yenileme için bekleniyor',
+	'PLL_WAITING_FOR_VERIFICATION' => 'Doğrulamayı bekliyor',
+	'PLL_WAITING_FOR_ACCEPTANCE' => 'Kabul bekliyor',
+	'PLL_RENEWED' => 'Yenilendi'
+	'PLL_NOT_RENEWED' => 'Yenilenmedi'
+	'PLL_NOT_APPLICABLE' => 'Geçerli değil',
+	'PLL_RENEWED_VERIFICATION' => 'Yenilendi (doğrulama)',
+	'PLL_NOT_RENEWED_VERIFICATION' => 'Yenilenmedi (doğrulama)',
+	'PLL_NOT_APPLICABLE_VERIFICATION' => 'Geçerli değil (doğrulama)',
+	//YetiForce	
+	'description' => 'Açıklama',
+	'attentions' => 'Yorumlar',
+	'BLOCK_INFORMATION_TIME' => 'Zaman bilgisi',
+	'LBL_GENERATE_EXTENSION' => 'Uzantıyı oluştur',
+	'LBL_ORDER_TIME' => 'Sipariş süresi',
+	'LBL_EXPIRING_SOLD_PRODUCTS' => 'Satılan ürünlerin süresinin dolması',
+	'LBL_CHANGE_STATUS' => 'Durumu Değiştir',
+];		
