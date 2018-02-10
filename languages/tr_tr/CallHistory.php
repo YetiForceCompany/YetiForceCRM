c499be050c450e5766246f0014408cd53121307e
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-10 18:36:44 +0300

    Create CallHistory.php

---
+++ languages/tr_tr/CallHistory.php
@@ -0,0 +1,41 @@
+<?php		
+/**		
+ * CallHistory english translation		
+ * @package YetiForce.Language		
+ * @copyright YetiForce Sp. z o.o.		
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)		
+ */		
+$languageStrings =	 [	
+	'CallHistory' => 'Arama geçmişi',
+	'SINGLE_CallHistory' => 'Arama geçmişi',
+	//Bloks	
+	'LBL_CALLHISTORY_INFORMATION' => 'Arama geçmişi bilgileri',
+	'LBL_CUSTOM_INFORMATION' => 'Ek bilgi',
+	'LBL_PHONE_INFORMATION' => 'Telefon bilgileri',
+	'LBL_RELATED_INFORMATION' => 'İlgili bilgiler',
+	//Fields	
+	'LBL_TYPE' => 'Tip',
+	'LBL_FROM_NUMBER' => 'FROM numara',
+	'LBL_TO_NUMBER' => 'Sayıya git',
+	'LBL_LOCATION' => 'Yer',
+	'LBL_PHONE_CALL_ID' => 'Telefonda kimlik araması',
+	'LBL_DURATION' => 'Arama süresi',
+	'LBL_STATUS' => 'Durum',
+	'LBL_START_TIME' => 'Çağrı başlatıldı',
+	'LBL_END_TIME' => 'Çağrı sona erdi',
+	'LBL_COUNTRY_CODE' => 'Ülke kodu',
+	'LBL_IMEI' => 'IMEI',
+	'LBL_IPADDRESS' => 'Kimlik adresi',
+	'LBL_SIMSERIAL' => 'SIM numarası',
+	'LBL_SUBSCRIBERID' => 'Abone numarası',
+	'LBL_DESTINATION' => 'Nereden çağrıldı'
+	'LBL_SOURCE' => 'Çağrı kaynağı',
+	'Record Id' => 'Kayıt Kimliği',
+	//pick list values	
+	'Outgoing missed' => 'Giden cevapsız',
+	'Outgoing received' => 'Giden alındı',
+	'Incoming' => 'Gelen alındı',
+	'Missed' => 'Cevapsız' mesajı
+	'Voicemail' => 'Sesli mesaj gelme',
+	'Call history' => 'Çağrı Geçmişi',
+];		
