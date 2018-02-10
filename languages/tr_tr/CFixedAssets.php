66c9c674b27a51bd0f19f026288b7c7c4bd878e0
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-10 18:50:54 +0300

    Create CFixedAssets.php

---
+++ languages/tr_tr/CFixedAssets.php
@@ -0,0 +1,64 @@
+<?php		
+/**		
+ * CFixedAssets english translation		
+ * @package YetiForce.Language		
+ * @copyright YetiForce Sp. z o.o.		
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)		
+ */		
+$languageStrings =	 [	
+	'CFixedAssets' => 'Sabit kıymetler',
+	'SINGLE_CFixedAssets' => 'Sabit kıymetler',
+	//BLOCKS	
+	'LBL_VEHICLE' => 'Araç',
+	'LBL_BASIC_INFORMATION' => 'Temel bilgiler',
+	'LBL_DESCRIPTION_BLOCK' => 'Açıklama',
+	'LBL_ATTENTION_BLOCK' => 'Notlar',
+	//FIELDS	
+	'FL_SUBJECT' => 'İsim',
+	'FL_TYPE' => 'Tip',
+	'FL_STATUS' => 'Durum',
+	'FL_PRODUCENT_DESIGNATION' => 'Üreticinin işareti',
+	'FL_ADDITIONAL_DESIGNATION' => 'Ek işaretleme',
+	'FL_INTERNAL_DESIGNATION' => 'Dahili işaretleme',
+	'FL_DATE_PRODUCTION' => 'Üretim verileri',
+	'FL_DATE_ACQUISITION' => 'Satın alma tarihi',
+	'FL_PURCHASE_PRICE' => 'Satın alma fiyatı',
+	'FL_ACTUAL_PRICE' => 'Gerçek fiyat',
+	'FL_RESERVATION' => 'Rezervasyon var',
+	'FL_CATEGORY' => 'Kategori',
+	'FL_FUEL_TYPE' => 'Yakıt türü',
+	'FL_TIMING_CHANGE' => 'Eksantrik mili değiştirme periyodu',
+	'FL_OIL_TYPE' => 'Yağ değişim dönemi',
+	'FL_AVARAGE_FUEL_CONSUPTION' => '100 km'lik ortalama yakıt tüketimi',
+	'FL_CURRENT_ODOMETER_READING' => 'Şu anki kilometre sayacı okuma',
+	'FL_NUMBER_REPAIR' => 'Tamirat sayısı',
+	'FL_DATE_OF_LAST_REPAIR' => 'Son onarım tarihi',
+	'FL_MAX_FUEL_CONSUPTION' => 'Maksimum yakıt tüketimi',
+	'FL_TYPE_OIL' => 'Yağ türü',
+	'FL_CURRENT_ODOMETER_READING_PORTERS' => 'Resepsiyondaki mevcut kilometre sayacı okuma',
+	'FL_PERIOD_CAR_REVIEW' => 'MOT test süresi',
+	'FL_DATE_CAR_REVIEW' => 'MOT test verileri',
+	'FL_NUMBER_FUEL_CARD' => 'Yakıt kartı numarası',
+	'FL_DATE_INSURANCE' => 'Sigorta tarihi',
+	'FL_COST_INSURANCE_OC' => 'Sorumluluk Sigorta bedeli',
+	'FL_COST_INSURANCE_AC' => 'Çarpışma Kapsamı sigorta bedeli',
+	'FL_COST_INSURANCE_NW' => 'Kişisel Yaralanma Koruması Sigorta Bedeli',
+	'FL_NUMBER_MOTOHOURS' => 'Moto saat sayısı',
+	'FL_EQUIPMENT' => 'Ekipmanlar',
+	// Picklist	
+	'PLL_UNAVAILABLE' => 'Kullanılamıyor',
+	'PLL_AVAILABLE' => 'Uygun',
+	'PLL_DEMAGED' => 'Hasar görmüş'
+	'PLL_IN_USE' => 'Kullanımda',
+	'PLL_UNDER_REPAIR' => 'Tamir altında',
+	'PLL_PENDING_FOR_REPROCESSING' => 'Yeniden işleme tabi tutulması bekleniyor',
+	'PLL_PENDING_FOR_SALE' => 'Satışa hazır',
+	'PLL_REPROCESSED' => 'Yeniden İşlenmiş'
+	'PLL_SOLD' => 'Satıldı',
+	'PLL_DEVICE' => 'Cihaz',
+	'PLL_MACHINE' => 'Makine',
+	'PLL_VEHICLE' => 'Araç',
+	'PLL_PROPERTY' => 'Özellik',
+	'PLL_INVENTORY' => 'Envanter',
+	'PLL_DIESEL' => 'Dizel',
+];		
