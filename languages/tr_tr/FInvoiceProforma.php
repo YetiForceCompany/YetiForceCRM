4b211f93326fb24020222e7788340ae299b21278
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-10 19:34:18 +0300

    Create FInvoiceProforma.php

---
+++ languages/tr_tr/FInvoiceProforma.php
@@ -0,0 +1,28 @@
+<?php		
+/**		
+ * FInvoiceProforma english translation		
+ * @package YetiForce.Language		
+ * @copyright YetiForce Sp. z o.o.		
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)		
+ */		
+$languageStrings =	 [	
+	'FInvoiceProforma' => 'Proforma faturalar',
+	'SINGLE_FInvoiceProforma' => 'Proforma fatura',
+	//BLOCKS	
+	'LBL_CUSTOM_INFORMATION' => 'Sistem bilgisi',
+	'LBL_ADDRESS_DELIVERY_INFORMATION' => 'Teslimat adresi',
+	//FIELDS	
+	'FL_SUBJECT' => 'Konu',
+	'FL_PAYMENT_DATE' => 'Teslim tarihi',
+	'FL_SALE_DATE' => 'Satış tarihi',
+	'FL_FORM_PAYMENT' => 'Ödeme yöntemi',
+	'FL_STATUS' => 'Durum',
+	'FL_BANK_ACCOUNT' => 'Banka hesap numarası',
+	'FL_NUMBER' => 'Fatura numarası',
+	'FL_ACCOUNT' => 'Hesap',
+	'FL_TOTAL' => 'Net'
+	'FL_GROSS' => 'Brüt',
+	//PICKLIST VALUES	
+	'PLL_TRANSFER' => 'transfer',
+	'PLL_CASH' => 'nakit',
+];		
