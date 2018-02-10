dd69eb8695dc045fd90911ea3c37bed71fe95cc4
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-10 18:54:21 +0300

    Create CMileageLogbook.php

---
+++ languages/tr_tr/CMileageLogbook.php
@@ -0,0 +1,25 @@
+<?php		
+/**		
+ * CMileageLogbook english translation		
+ * @package YetiForce.Language		
+ * @copyright YetiForce Sp. z o.o.		
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)		
+ */		
+$languageStrings =	 [	
+	'CMileageLogbook' => 'Kilometre logosu kitabı'
+	'SINGLE_CMileageLogbook' => 'Kilometre logosu kitabı'
+	//BLOCKS	
+	'LBL_ADDITIONAL_INFORMATION' => 'Ek bilgi',
+	'LBL_BASIC_INFORMATION' => 'Temel bilgiler',
+	'LBL_DESCRIPTION_BLOCK' => 'Açıklama',
+	'LBL_ATTENTION_BLOCK' => 'Notlar',
+	//FIELDS	
+	'FL_SUBJECT' => 'Konu',
+	'FL_STATUS' => 'Durum',
+	'FL_NUMBER_KILOMETERS' => 'Kilometre sayısı',
+	//PickList	
+	'PLL_DRAFT' => 'Taslak',
+	'PLL_FOR_APPROVAL' => 'Onay için'
+	'PLL_ACCEPTED' => 'Kabul Edildi'
+	'PLL_CANCELLED' => 'İptal edildi',
+];		
