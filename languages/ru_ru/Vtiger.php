775cb193329b1e73b0b9dc5e836311f606c90496
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2018-02-12 11:03:44 +0100

    Added translation

--- languages/ru_ru/Vtiger.php
+++ languages/ru_ru/Vtiger.php
@@ -1318,6 +1318,7 @@ $languageStrings = [
 	'LBL_SHOW_INVENTORY_ROW' => 'Show additional information',
 	'LBL_CALCULATE_SUM_FOR_THIS_FIELD' => 'Calculate sum for this field',
 	'LBL_RELATED_RECORDS_LIST' => 'Related records list',
+	'LBL_ADVANCED_SEARCH' => 'Advanced search',
 ];
 $jsLanguageStrings = [
 	'JS_SUMMARY' => 'Подитог',
