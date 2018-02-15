c15fd6503b4acdb7667a85329973b6531645cc08
areksolek <a.solek@yetiforce.com>
2018-02-15 15:00:01 +0100

    Bootstrap icons have been changed to font-awesome icons

--- modules/Announcements/models/DetailView.php
+++ modules/Announcements/models/DetailView.php
@@ -9,25 +9,26 @@
  */
 class Announcements_DetailView_Model extends Vtiger_DetailView_Model
 {
-    /**
-     * Function to get the detail view related links.
-     *
-     * @return <array> - list of links parameters
-     */
-    public function getDetailViewRelatedLinks()
-    {
-        $recordModel = $this->getRecord();
-        $relatedLinks = parent::getDetailViewRelatedLinks();
 
-        $relatedLinks[] = [
-            'linktype' => 'DETAILVIEWTAB',
-            'linklabel' => 'LBL_USERS',
-            'linkurl' => $recordModel->getDetailViewUrl().'&mode=showUsers',
-            'linkicon' => 'glyphicon-user',
-            'linkKey' => 'LBL_USERS',
-            'related' => 'Users',
-        ];
+	/**
+	 * Function to get the detail view related links.
+	 *
+	 * @return <array> - list of links parameters
+	 */
+	public function getDetailViewRelatedLinks()
+	{
+		$recordModel = $this->getRecord();
+		$relatedLinks = parent::getDetailViewRelatedLinks();
 
-        return $relatedLinks;
-    }
+		$relatedLinks[] = [
+			'linktype' => 'DETAILVIEWTAB',
+			'linklabel' => 'LBL_USERS',
+			'linkurl' => $recordModel->getDetailViewUrl() . '&mode=showUsers',
+			'linkicon' => 'fa-user',
+			'linkKey' => 'LBL_USERS',
+			'related' => 'Users',
+		];
+
+		return $relatedLinks;
+	}
 }
