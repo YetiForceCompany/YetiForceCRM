93bf9cb1b0e4dc12a3c840dae7053cd62514650a
Tomasz Kur <t.kur@yetiforce.com>
2018-02-18 20:37:40 +0100

    Removed test case

--- tests/Gui/8_OSSMail.php
+++ tests/Gui/8_OSSMail.php
@@ -19,24 +19,4 @@ class Gui_OSSMail extends \Tests\GuiBase
         $this->assertEquals('Index', $this->byId('view')->value(), 'There is not a correct view');
         $this->assertEquals('Settings', $this->byId('parent')->value(), 'There is not a correct parent');
     }
-
-    /**
-     * Testing save configuration.
-     */
-    public function testSave()
-    {
-        $this->url('/index.php?module=OSSMail&parent=Settings&view=Index');
-        $this->execute([
-            'script' => '$(\'[name="product_name"]\').val(\'YetiForce_GUI_TEST\');',
-            'args' => [],
-        ]);
-        $this->execute([
-            'script' => '$(\'[name="session_lifetime"]\').val(\'35\');',
-            'args' => [],
-        ]);
-        $this->byClassName('saveButton')->click();
-        $this->url('/index.php?module=OSSMail&parent=Settings&view=Index');
-        $this->assertEquals('YetiForce_GUI_TEST', $this->byName('product_name')->attribute('value'), 'Incorrect value');
-        $this->assertEquals('35', $this->byName('session_lifetime')->attribute('value'), 'Incorrect value');
-    }
 }
