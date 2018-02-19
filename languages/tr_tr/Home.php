e2e18a797127ea82306d0afa70d37163bc9e35c0
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-20 01:53:01 +0300

    Create Home.php

---
+++ languages/tr_tr/Home.php
@@ -0,0 +1,53 @@
+<?php		
+	/* +***********************************************************************************	
+	* The contents of this file are subject to the vtiger CRM Public License Version 1.0	
+	* ("License"); You may not use this file except in compliance with the License	
+	* The Original Code is: vtiger CRM Open Source	
+	* The Initial Developer of the Original Code is vtiger.	
+	* Portions created by vtiger are Copyright (C) vtiger.	
+	* All Rights Reserved.	
+	* Contributor(s): YetiForce.com	
+	* ********************************************************************************** */	
+	$languageStrings = [	
+	'ALVT' => 'En Hesaplar',
+	'CVLVT' => 'Temel Ölçütler',
+	'GRT' => 'Grubum Tahsisi',
+	'HDB' => 'Ana Sayfa Gösterge Tablosu',
+	'HLT' => 'En Destek Olunan Destek Talepleri',
+	'Home' => 'Ana sayfam',
+	'ILTI' => 'En Faturalar',
+	'LTFAQ' => 'En Son Sorulan Sorularım',
+	'OLTPO' => 'En Çok Satın Alma Siparişleri',
+	'PA' => 'Bekleyen Etkinlikler',
+	'PLVT' => 'En İyi Fırsatlar',
+	'QLTQ' => 'En Çok Alıntılar',
+	'UA' => 'Gelecek Faaliyetler',
+	'LBL_MARK_AS_READ' => 'Okundu olarak işaretle',
+	'Notifications' => 'Bildirimler',
+	'LBL_MESSAGES_FROM_USERS' => 'Kullanıcılardan',
+	'LBL_WATCHDOG' => 'Değişiklikleri izle',
+	'LBL_SEND_NOTIFICATION_MAIL' => 'E-posta ile gönder',
+	'Home page' => 'Ana sayfa',
+	'LBL_NOTIFICATION_SETTINGS' => 'Bildirim ayarları',
+	'LBL_TYPE_NOTIFICATIONS' => 'Bildirim türleri',
+	'LBL_TIME' => 'Zaman',
+	'LBL_NOTIFICATION_BY_SENDER' => 'Gönderene göre bildirim sayısı',
+	'LBL_NOTIFICATION_BY_RECIPIENT' => 'Alıcıya göre bildirim sayısı',
+	//Widgets	
+	'LBL_MINI_LIST' => 'Mini liste',
+	'LBL_FILTERING_BY_DATE' => 'Alana tarihine göre filtreleme',
+	'LBL_TIMELINE_BY_DATE' => 'Tarihe göre zaman çizelgesi',
+	'LBL_WIDGET_NAME' => 'Eklenti adı',
+	'LBL_SELECT_CHART' => 'Grafiği seçin',
+	'LBL_VALUE_TYPE' => 'Değer türü',
+	'LBL_VALUE_FIELD' => 'Değer alanı',
+	'LBL_NUMBER_OF_RECORDS' => 'Kayıtların sayısı',
+	'LBL_SUM' => 'Toplam',
+	'LBL_GROUP_FIELD' => 'Alana göre gruplandırma',
+	'LBL_GROUP_VALUES' => 'Bölünmüş gruplama değerleri',
+	'LBL_FILTER_BY_OWNER' => 'Sahibine göre filtrele',
+	'LBL_ALL_OWNERS' => 'Hepsi',
+];	
+		
+$jsLanguageStrings = [	
+];	
