d1a2d3124c269d38bb6c1ac52d27f241ee088fea
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-20 02:29:23 +0300

    Create IGDN.php

---
+++ languages/tr_tr/IGDN.php
@@ -0,0 +1,29 @@
+<?php		
+	/**	
+	* IGDN english translation.	
+	*	
+	* @copyright YetiForce Sp. z o.o	
+	* @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)	
+	*/	
+	$languageStrings = [	
+	'IGDN' =	'Gönderilen malların Notu',
+	'SINGLE_IGDN' =	  'Gönderilen malların Notu',
+	'LBL_DESCRIPTION_BLOCK' =	  'Açıklama',
+	'LBL_ATTENTION_BLOCK' =	  'Yorum',
+	// Fields	
+	'FL_ACCEPTANCE_DATE' =	  'Kabul tarihi',
+	'FL_ACCOUNT' =	  'Hesap',
+	'FL_NUMBER' =	  'Numara',
+	'FL_SUBJECT' =	  'Konu',
+	'FL_STORAGE' =	  'Depolama',
+	'FL_STATUS' =	  'Durum',
+	'FL_SSIGNLEORDERS' =	  'Satın alma emri',
+	// Picklist	
+	'PLL_DRAFT' =	  'Taslak',
+	'PLL_IN_REALIZATION' =	  'Gerçekleştirilmesinde',
+	'PLL_OBJECTIONS' =	  'İtirazlar',
+	'PLL_FOR_APPROVAL' =	  'Onay için',
+	'PLL_CANCELLED' =	  'İptal edildi',
+	'PLL_ACCEPTED' =	  'Kabul edilmiş',
+	'LBL_CHANGE_STATUS' =	  'Durumu değiştir',
+];	
