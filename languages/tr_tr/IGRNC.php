8fcada4e9f6d2d07e2e95b05a32238a18ad16dd7
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-20 02:55:33 +0300

    Create IGRNC.php

---
+++ languages/tr_tr/IGRNC.php
@@ -0,0 +1,27 @@
+<?php		
+	/**	
+	* IGRNC english translation.	
+	*	
+	* @copyright YetiForce Sp. z o.o	
+	* @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)	
+	*/	
+	$languageStrings = [	
+	'IGRNC' => 'Alınan Malların Not Düzeltmeleri',
+	'SINGLE_IGRNC' => 'Alınan Malın Not Düzeltme',
+	'LBL_DESCRIPTION_BLOCK' => 'Açıklama',
+	'LBL_ATTENTION_BLOCK' => 'Yorum',
+	'FL_ACCEPTANCE_DATE' => 'Kabul tarihi',
+	'FL_VENDOR' => 'Satıcı',
+	'FL_NUMBER' => 'Numara',
+	'FL_SUBJECT' => 'Konu',
+	'FL_STORAGE' => 'Depolama',
+	'FL_STATUS' => 'Durum',
+	'FL_IGRN' => 'Mallar alındı belgesi',
+	'PLL_DRAFT' => 'Taslak',
+	'PLL_IN_REALIZATION' => 'Gerçekleşmede',
+	'PLL_OBJECTIONS' => 'İtirazlar',
+	'PLL_FOR_APPROVAL' => 'Onay için',
+	'PLL_CANCELLED' => 'İptal edildi',
+	'PLL_ACCEPTED' => 'Kabul edildi',
+	'LBL_CHANGE_STATUS' => 'Durumu değiştir',
+];	
