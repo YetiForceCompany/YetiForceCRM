6067f774e99acff4e1713cfacad0f6ddf2fe8b9e
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-20 01:29:08 +0300

    Create HolidaysEntitlement.php

---
+++ languages/tr_tr/HolidaysEntitlement.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * HolidaysEntitlement english translation.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ */
+$languageStrings = [
+	'HolidaysEntitlement' => 'Yıllık tatil hakkı',
+	'SINGLE_HolidaysEntitlement' => 'Yıllık tatil hakkı',
+	//Bloks
+	'LBL_HOLIDAYSENTITLEMENT_INFORMATION' => 'Özel Bilgiler',
+	'LBL_CUSTOM_INFORMATION' => 'Sistem bilgisi',
+	//Fields
+	'LBL_YEAR' => 'Yıl',
+	'LBL_DAYS' => 'Tatil sayısı',
+	'LBL_EMPLOYEE' => 'İşçi',
+	'LBL_NUMBER' => 'Numara',
+];
