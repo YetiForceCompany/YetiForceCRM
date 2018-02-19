e77d740ef0dd14758083c9838ca0fc1dbef4dd43
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-20 02:46:21 +0300

    Create IGIN.php

---
+++ languages/tr_tr/IGIN.php
@@ -0,0 +1,25 @@
+<?php		
+	/**	
+	* IGIN english translation.	
+	*	
+	* @copyright YetiForce Sp. z o.o	
+	* @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)	
+	*/	
+	$languageStrings = [	
+	'IGIN' => 'İhraç Edilen Malların Notu',
+	'SINGLE_IGIN' => 'İhraç Edilen Malın Notu',
+	'LBL_DESCRIPTION_BLOCK' => 'Açıklama',
+	'LBL_ATTENTION_BLOCK' => 'Yorum',
+	'FL_ACCEPTANCE_DATE' => 'Kabul tarihi',
+	'FL_NUMBER' => 'Numara',
+	'FL_SUBJECT' => 'Konu',
+	'FL_STORAGE' => 'Depolama',
+	'FL_STATUS' => 'Durum',
+	'PLL_DRAFT' => 'Taslak',
+	'PLL_IN_REALIZATION' => 'Gerçekleşmede',
+	'PLL_OBJECTIONS' => 'İtirazlar',
+	'PLL_FOR_APPROVAL' => 'Onay için',
+	'PLL_CANCELLED' => 'İptal edildi',
+	'PLL_ACCEPTED' => 'Kabul edilmiş',
+	'LBL_CHANGE_STATUS' => 'Durumu değiştir',
+];	
