ff3be14944dcf35bc56de4bd40a90ede0f3193b3
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-02-19 14:23:51 +0100

    lib_roundcube fix PHP Fatal error

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-	'appVersion' => '4.3.149',
-	'patchVersion' => '2018.02.15',
+	'appVersion' => '4.3.150',
+	'patchVersion' => '2018.02.19',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.38',
+	'lib_roundcube' => '0.0.39',
 	'lib_gantt' => '0.0.1',
 ];
