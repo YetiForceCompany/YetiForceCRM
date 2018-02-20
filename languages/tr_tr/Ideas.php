7cc20234ca281a389c63151b2a05064403a43d3b
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-20 04:25:19 +0300

    Create Ideas.php

---
+++ languages/tr_tr/Ideas.php
@@ -0,0 +1,23 @@
+<?php		
+/**	
+* Ideas english translation.	
+*	
+* @copyright YetiForce Sp. z o.o	
+* @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)	
+*/	
+$languageStrings = [	
+	'Ideas' => 'Fikirler',
+	'SINGLE_Ideas' => 'Fikir',
+	//Bloks	
+	'LBL_IDEAS_INFORMATION' => 'Temel Bilgiler',
+	//Fields	
+	'LBL_SUBJECT' => 'Konu',
+	'LBL_STATUS' => 'Durum',
+	'Description' => 'Temel açıklama',
+	'LBL_EXTENT_DESCRIPTION' => 'Detaylı Açıklama',
+	'Attention' => 'Yorum',
+	'LBL_NO' => 'Numara',
+	//pick list values	
+	'Draft' => 'Taslak',
+	'Proposition' => 'Öneri',
+];	
