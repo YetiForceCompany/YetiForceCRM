8c8da47c474aae53eb2402c654bf0c49a0c24af4
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2018-02-20 10:43:38 +0100

    Update HelpInfo.php

--- languages/tr_tr/HelpInfo.php
+++ languages/tr_tr/HelpInfo.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * HelpInfo english translation.
+ * HelpInfo turkish translation.
  *
  * @copyright YetiForce Sp. z o.o
  * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
