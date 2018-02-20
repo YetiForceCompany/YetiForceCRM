d43d2b67fd4da3ff23872129aa21e2a678984208
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2018-02-20 09:57:14 +0100

    Update CMileageLogbook.php

--- languages/tr_tr/CMileageLogbook.php
+++ languages/tr_tr/CMileageLogbook.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * CMileageLogbook english translation.
+ * CMileageLogbook turkish translation.
  *
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
