4db5ab037926fa73cafa8a9b0d7bd63185660c75
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-20 03:47:44 +0300

    Update Install.php

--- languages/tr_tr/Install.php
+++ languages/tr_tr/Install.php
@@ -1,14 +1,14 @@
 <?php					
-	/* +***********************************************************************************				
-	* The contents of this file are subject to the vtiger CRM Public License Version 1.0				
-	* ("License"); You may not use this file except in compliance with the License				
-	* The Original Code is: vtiger CRM Open Source				
-	* The Initial Developer of the Original Code is vtiger.				
-	* Portions created by vtiger are Copyright (C) vtiger.				
-	* All Rights Reserved.				
-	* Contributor(s): YetiForce.com				
-	* *********************************************************************************** */				
-	$languageStrings = [				
+/* +***********************************************************************************				
+* The contents of this file are subject to the vtiger CRM Public License Version 1.0				
+* ("License"); You may not use this file except in compliance with the License				
+* The Original Code is: vtiger CRM Open Source				
+* The Initial Developer of the Original Code is vtiger.				
+* Portions created by vtiger are Copyright (C) vtiger.				
+* All Rights Reserved.				
+* Contributor(s): YetiForce.com				
+* *********************************************************************************** */				
+$languageStrings = [				
 	'LANGNAME' => 'Türkçe',			
 	'ERR_DATABASE_CONNECTION_FAILED' => 'Veritabanı sunucusuna bağlanılamıyor',			
 	'ERR_DB_NOT_FOUND' => 'Bu veritabanı bulunamadı. Veritabanı ayarlarını değiştirmeyi deneyin ',			
