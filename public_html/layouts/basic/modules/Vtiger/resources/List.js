21dea760f0086f60f2b00051dc1d92e5c8da10dd
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-02-20 15:23:03 +0100

    hover -> mouseenter mouseleave

--- public_html/layouts/basic/modules/Vtiger/resources/List.js
+++ public_html/layouts/basic/modules/Vtiger/resources/List.js
@@ -1522,7 +1522,7 @@ jQuery.Class("Vtiger_List_Js", {
 		var listViewTopMenuDiv = this.getListViewTopMenuContainer();
 		var filterBlock = this.getFilterBlock()
 		if (filterBlock != false) {
-			filterBlock.on('hover', 'li.select2-results__option[role="treeitem"]', function (event) {
+			filterBlock.on('mouseenter mouseleave', 'li.select2-results__option[role="treeitem"]', function (event) {
 				var liElement = $(event.currentTarget);
 				var liFilterImages = liElement.find('.filterActionImgs');
 				if (liElement.hasClass('group-result')) {
