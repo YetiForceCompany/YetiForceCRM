8c8dd624db322614c6cae38dbe461f476300a76f
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-20 04:09:06 +0300

    Create ISTN.php

---
+++ languages/tr_tr/ISTN.php
@@ -0,0 +1,27 @@
+<?php		
+/**	
+* ISTN english translation.	
+*	
+* @copyright YetiForce Sp. z o.o	
+* @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)	
+*/	
+$languageStrings = [	
+	'ISTN' => 'Depolama Devri Notları',
+	'SINGLE_ISTN' => 'Depolama Devri Notu',
+	'LBL_DESCRIPTION_BLOCK' => 'Açıklama',
+	'LBL_ATTENTION_BLOCK' => 'Yorum',
+	'FL_ESTIMATED_DATE' => 'Tahmin edilen tarih',
+	'FL_NUMBER' => 'Numara',
+	'FL_SUBJECT' => 'Konu',
+	'FL_STATUS' => 'Durum',
+	'PLL_DRAFT' => 'Taslak',
+	'PLL_IN_REALIZATION' => 'Gerçekleşmede',
+	'PLL_OBJECTIONS' => 'İtirazlar',
+	'PLL_FOR_APPROVAL' => 'Onay için',
+	'PLL_CANCELLED' => 'İptal edildi',
+	'PLL_ACCEPTED' => 'Kabul edildi',
+	'PLL_RECEIVED' => 'Alınan',
+	'PLL_TRANSFER' => 'Aktar',
+	'PLL_DISPATCHED' => 'Gönderildi',
+	'FL_TYPE' => 'Tür',
+];	
