8ab68c367050696af361eb44a74b5bec05ec1f33
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-02-22 10:32:56 +0100

    Improved owner colors

--- vendor/yetiforce/Fields/Owner.php
+++ vendor/yetiforce/Fields/Owner.php
@@ -647,7 +647,11 @@ class Owner
 	public static function getColor($id)
 	{
 		if (!static::$colorsCache) {
-			static::$colorsCache = require 'user_privileges/owners_colors.php';
+			if (file_exists('user_privileges/owners_colors.php')) {
+				static::$colorsCache = require 'user_privileges/owners_colors.php';
+			} else {
+				static::$colorsCache = [];
+			}
 		}
 		return static::$colorsCache[$id] ?? '#24ff00';
 	}
