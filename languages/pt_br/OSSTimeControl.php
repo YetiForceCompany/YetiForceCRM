c4550cd7f4ef23ac8e4abd949b3875ea34dce8f1
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2018-02-23 10:15:43 +0100

    Added translations

--- languages/pt_br/OSSTimeControl.php
+++ languages/pt_br/OSSTimeControl.php
@@ -100,6 +100,8 @@ $languageStrings = [
 	'LBL_WORKDAYS_INFO' => 'Dias de trabalho',
 	'LBL_SUM' => 'Soma',
 	'LBL_SUM_OF_WORKING_TIME_DIVIDED_INTO_USERS' => 'Gráfico do tempo de trabalho por Usuário',
+	'LBL_TIME_CONTROL_LIST' => 'Time Control List',
+	'LBL_TIME_CONTROL_USER_GROUP' => 'Time Control List with additional information about users',
 ];
 $jsLanguageStrings = [
 	'MSG_ERROR_NUMBER' => 'Caracteres permitidos: [0-9]',
