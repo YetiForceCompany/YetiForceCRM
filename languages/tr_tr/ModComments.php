597b50feec6122b1446d2ae59c46679a6dad3c85
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-26 04:41:15 +0300

    Create ModComments.php

---
+++ languages/tr_tr/ModComments.php
@@ -0,0 +1,38 @@
+<?php				
+/* +***********************************************************************************				
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0				
+ * ("License"); You may not use this file except in compliance with the License				
+ * The Original Code is:  vtiger CRM Open Source				
+ * The Initial Developer of the Original Code is vtiger.				
+ * Portions created by vtiger are Copyright (C) vtiger.				
+ * All Rights Reserved.				
+ * Contributor(s): YetiForce.com				
+ * ********************************************************************************** */				
+$languageStrings = [				
+	// Basic Strings			
+	'SINGLE_ModComments' =>	'Yorum Yap',		
+	'LBL_RECORDS_LIST' => 'Yorumlar listesi',		
+	// Blocks			
+	'LBL_MODCOMMENTS_INFORMATION' => 'Yorum',		
+	'LBL_OTHER_INFORMATION' => 'Diğer bilgiler',		
+	'LBL_ADDING_COMMENT' => 'Yorum ekle',		
+	'LBL_WRITE_YOUR_COMMENT_HERE' => 'Buraya yorumlar gir',		
+	//Field Labels			
+	'Comment' => 'Yorum Yap',		
+	'Creator' => 'Oluşturan',		
+	'Related To Comments' => 'İle ilgili',		
+	'UserId' => 'Kullanıcı adı',		
+	'Customer' => 'Müşteri',		
+	'ReasonToEdit' => 'Düzenleme nedeni',		
+	//Email notifications			
+	'LBL_ADDED_COMMENT_TO_TICKET' => 'Destek Talebine bir yorum ekledi',		
+	'LBL_NEW_COMMENT_FOR_TICKET' => 'Destek Talebine bir açıklama eklendi',		
+	'LBL_NOTICE_WELCOME' => 'Platforma hoş geldiniz',		
+	'LBL_NOTICE_FOOTER' => 'Eğer proje yönetim sistemimizden herhangi bir bildirim almak istemiyorsanız, lütfen <a href="mailto:help@yetiforce.com" target="_ blank">help@yetiforce.com</a> adresine bir e-posta göndererek bizi bilgilendirin. Bu iletinin bir alıcı değilseniz, lütfen bize bildirin ve iletiyi silin.',
+	'LBL_NOTICE_CREATED' => 'Tarafından Yaratıldı',		
+	//Others			
+	'LBL_COMMENTS_0' => 'Geçerli kayıt',		
+	'LBL_COMMENTS_1' => 'Süreçler',		
+	'LBL_COMMENTS_2' => 'Alt süreçler',		
+	'LBL_COMMENTS_SEARCH' => 'Arama',		
+];				
