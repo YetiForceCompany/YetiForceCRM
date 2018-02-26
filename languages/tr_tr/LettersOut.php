bed78771104b6af7ecbf8751b5ed03ca74a30347
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-26 04:24:08 +0300

    Create LettersOut.php

---
+++ languages/tr_tr/LettersOut.php
@@ -0,0 +1,64 @@
+<?php		
+	/**	
+	* LettersOut turkish translation.	
+	*	
+	* @copyright YetiForce Sp. z o.o	
+	* @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)	
+	*/	
+	$languageStrings = [	
+	// Basic Strings	
+	'LettersOut' => 'Giden yazışmalar',
+	'SINGLE_LettersOut' => 'Giden yazışma',
+	// Blocks	
+	'LBL_MAIN_INFORMATION' => 'Yazışmalarla ilgili bilgi',
+	//Field Labels	
+	'Number' => 'Numara',
+	'Title' => 'Konu',
+	'Date of adoption' => 'Alınan tarih',
+	'Type of shipping' => 'Sevkıyat türü',
+	'Person receiving' => 'Kişi alıyor',
+	'Deadline for reply' => 'Yanıt için son gün',
+	'Document no' => 'Döküman No.',
+	'No internal' => 'İç No.',
+	'Parent letters' => 'Yazışmalarla ilgili',
+	'Overall dimensions' => 'Boyut',
+	'Notes' => 'Notlar',
+	'Related' => 'Hesap / Fırsat',
+	'Contact' => 'Temas',
+	'Type of document' => 'Belge Türü',
+	'FL_INCOMING_CORRESPONDENCE' => 'Gelen yazışmalar',
+	// picklist values	
+	'PLL_LOCAL_PICKUP' => 'Kişisel koleksiyon',
+	'PLL_POST_OFFICE_PL' => 'Polonya Posta Ofisi',
+	'PLL_POST_OFFICE' => 'Postane',
+	'PLL_COURIER' => 'Kurye',
+	'PLL_COURIER_POCZTEX' => 'Kurye Pocztex',
+	'PLL_COURIER_7' => 'Kurye Siodemka',
+	'PLL_COURIER_DPD' => 'Kurye DPD',
+	'PLL_COURIER_DHL' => 'Kurye DHL',
+	'PLL_COURIER_UPS' => 'Kurye UPS',
+	'PLL_COURIER_GLS' => 'Kurye GLS',
+	'PLL_TELEX' => 'Teleks',
+	'PLL_EMAIL' => 'E-posta',
+	'PLL_POCZTEX' => 'Pocztex',
+	'PLL_TNT' => 'TNT',
+	'PLL_DEMAND_PAYMENT' => 'Ödeme talebi',
+	'PLL_BANK_STATEMENTS' => 'Banka tabloları',
+	'PLL_ORDER' => 'Sipariş',
+	'PLL_ENQUIRY' => 'Sorgulama',
+	'PLL_CONTRACT' => 'Sözleşme',
+	'PLL_RECLAMATION' => 'Şikayet',
+	'PLL_ADVERTISEMENT' => 'Reklam',
+	'PLL_MESSAGE' => 'Mesaj',
+	'PLL_RECEIPT' => 'Fiş',
+	'PLL_APPLICATION' => 'Uygulama',
+	'PLL_OFFER' => 'Alıntı',
+	'PLL_PERSONAL_LETTER' => 'Kişisel mektup',
+	'PLL_PROFORMA' => 'Proforma fatura',
+	'PLL_CORRECTION' => 'Fatura düzeltme',
+	'PLL_INVOICE' => 'Fatura',
+	'PLL_NEW' => 'Yeni',
+	'PLL_SETTLED' => 'Yerleşik',
+	'PLL_A' => 'A',
+	'PLL_B' => 'B',
+	];	
