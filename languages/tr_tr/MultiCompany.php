828166ed0b97c969cd2746d96141be048705fb0a
rtonline <36324712+rtonline@users.noreply.github.com>
2018-02-26 05:10:18 +0300

    Create MultiCompany.php

---
+++ languages/tr_tr/MultiCompany.php
@@ -0,0 +1,37 @@
+<?php		
+/**		
+ * MultiCompany turkish translation.		
+ *		
+ * @copyright YetiForce Sp. z o.o		
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)		
+ */		
+$languageStrings = [		
+	'MultiCompany' => 'Bölümler',
+	'SINGLE_MultiCompany' => 'Bölüm',
+	//BLOCKS	
+	'LBL_BASIC_INFORMATION' => 'Şirket Detayları',
+	'LBL_CONTACT_INFORMATION' => 'İletişim bilgileri',
+	'LBL_REGISTRATION_INFORMATION' => 'Kayıt bilgisi',
+	'LBL_ADDRESS_INFORMATION' => 'Adres detayları',
+	'LBL_SYSTEM_INFORMATION' => 'Sistem bilgisi',
+	'LBL_ADDITIONAL_INFORMATION' => 'İlave bilgi',
+	//FIELDS	
+	'FL_COMPANY_NAME' => 'Şirket Adı',
+	'FL_PARENT' => 'Ana bölüm',
+	'FL_STATUS' => 'Durum',
+	'FL_EMAIL_1' => 'Öncelikli E-posta',
+	'FL_EMAIL_2' => 'İkincil e-posta',
+	'FL_PHONE' => 'Birincil telefon',
+	'FL_MOBILE' => 'İkincil telefon',
+	'FL_FAX' => 'Faks',
+	'FL_VATID' => 'Vergi numarası',
+	'FL_COMPANY_ID1' => 'İş kayıt numarası',
+	'FL_COMPANY_ID2' => 'NCR',
+	'FL_INTERNAL_NOTES' => 'İç notlar',
+	'FL_PUBLIC_NOTES' => 'Genel notlar',
+	//Picklist	
+	'PLL_ACTIVE' => 'Aktif',
+	'PLL_INACTIVE' => 'Etkin',
+	//others	
+	'LBL_SHOW_HIERARCHY' => 'Hiyerarşi',
+];		
