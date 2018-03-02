b98cbb33be9b74f0fa9cf14f0f81036acddb9281
rtonline <36324712+rtonline@users.noreply.github.com>
2018-03-03 02:44:36 +0300

    Create OSSOutsourcedServices.php

---
+++ languages/tr_tr/OSSOutsourcedServices.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * OSSOutsourcedServices turkish translation.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ */	
+$languageStrings = [		
+	'OSSOutsourcedServices' => 'Dış Kaynaklı Hizmetler',
+	'SINGLE_OSSOutsourcedServices' =>   'Dış Kaynaklı Hizmet',
+	'LBL_INFORMATION' => 'Bilgi bloğu',
+	//fields	
+	'Assigned To' => 'Atandı',
+	'LBL_osservicesstatus' => 'Durum',
+	'Individual Agreement' => 'Bireysel Anlaşma',
+	'In service' => 'Desteklenen',
+	'Finished support' => 'Destek sonlandırıldı',
+	'Product Name' => 'Hizmet adı',
+	'Sub Category' => 'Alt kategori',
+	'Category' => 'Kategori',
+	'Date Sold' => 'Satış tarihi',
+	'Date in Service' => 'Destek sonlandırma tarihi',
+	'Description' => 'Açıklama',
+	'LBL_DESCRIPTION_INFORMATION' => 'Tanımlayıcı bilgi',
+	'LBL_CUSTOM_INFORMATION' => 'Sistem bilgisi',
+	'Where bought' => 'Nereden satın alındı',
+	'Number' => 'Numara',
+	'Save' => 'Kayıt et',
+	'Selling proposition' => 'Fırsat',
+	'LBL_GENERATE_EXTENSION' => 'Uzantı Üret',
+	'Active' => 'Aktif',
+	'Inactive' => 'Pasif',
+];		
