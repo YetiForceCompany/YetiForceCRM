f0e99f3abcc93d4cf3514cf8be116cb29939ceb6
rtonline <36324712+rtonline@users.noreply.github.com>
2018-03-03 02:51:30 +0300

    Update OSSSoldServices.php

--- languages/tr_tr/OSSSoldServices.php
+++ languages/tr_tr/OSSSoldServices.php
@@ -1,6 +1,6 @@
 <?php		
 /**		
- * OSSSoldServices english translation.		
+ * OSSSoldServices turkish translation.		
  *		
  * @copyright YetiForce Sp. z o.o		
  * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)		
