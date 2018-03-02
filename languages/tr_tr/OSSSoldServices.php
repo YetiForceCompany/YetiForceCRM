f8205607083a2c2860b50e43711b2a50b7c2776a
rtonline <36324712+rtonline@users.noreply.github.com>
2018-03-03 02:50:55 +0300

    Create OSSSoldServices.php

---
+++ languages/tr_tr/OSSSoldServices.php
@@ -0,0 +1,48 @@
+<?php		
+/**		
+ * OSSSoldServices english translation.		
+ *		
+ * @copyright YetiForce Sp. z o.o		
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)		
+ */		
+$languageStrings = [		
+	'OSSSoldServices' => 'Satılan Hizmetler',
+	'SINGLE_OSSSoldServices' => 'Satılan Hizmet',
+	// Blocks	
+	'LBL_INFORMATION' => 'Bilgi bloğu',
+	// Field Labels	
+	'Number' => 'Numara',
+	'Assigned To' => 'Atandı',
+	'LBL_RODZAJUMOWY' => 'Sözleşme türü',
+	'LBL_ssservicesstatus' => 'Durumu',
+	'Product Name' => 'Ürün adı',
+	'Sub Category' => 'Alt kategori',
+	'Category' => 'Kategori',
+	'Date Sold' => 'Satış tarihi',
+	'Date in Service' => 'Destek bitiş tarihi',
+	'Description' => 'Açıklama',
+	'LBL_DESCRIPTION_INFORMATION' => 'Tanımlayıcı bilgi',
+	'LBL_CUSTOM_INFORMATION' => 'Sistem bilgisi',
+	'Where bought' => 'Nereden satın alındı',
+	'Services' => 'Hizmetler',
+	'LBL_GENERATE_EXTENSION' => 'Uzantıyı üret',
+	'LBL_ORDER_TIME' => 'Sipariş zamanı',
+	'FL_RENEWAL_INVOICE' => 'Yenileme faturası',
+	'FL_CONTACT' => 'İletişim',
+	// picklist values	
+	'FL_RENEWAL' => 'Yenileme',
+	'PLL_DRAFT' => 'Taslak',
+	'PLL_WAITING_FOR_VERIFICATION' => 'Doğrulama bekliyor',
+	'PLL_WAITING_FOR_ACCEPTANCE' => 'Kabul bekliyor',
+	'PLL_ACCEPTED' => 'Kabul edildi',
+	'PLL_CANCELLED' => 'İptal edildi',
+	'PLL_PLANNED' => 'Planlı',
+	'PLL_WAITING_FOR_RENEWAL' => 'Yenilemeyi bekliyor',
+	'PLL_RENEWED' => 'Yenilenen',
+	'PLL_NOT_RENEWED' => 'Yenilenmedi',
+	'PLL_NOT_APPLICABLE' => 'Geçerli değil',
+	'LBL_SET_RENEWAL' => 'Yenilemeyi ayarla',
+	'PLL_RENEWED_VERIFICATION' => 'Yenilendi (doğrulama)',
+	'PLL_NOT_RENEWED_VERIFICATION' => 'Yenilenmedi (doğrulama)',
+	'PLL_NOT_APPLICABLE_VERIFICATION' => 'Geçerli değil (doğrulama)',
+];		
