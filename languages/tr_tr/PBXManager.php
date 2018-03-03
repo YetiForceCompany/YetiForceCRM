8d634155b993e5b7e86b3cfd024b9a31035dfe0e
rtonline <36324712+rtonline@users.noreply.github.com>
2018-03-03 03:17:33 +0300

    Create PBXManager.php

---
+++ languages/tr_tr/PBXManager.php
@@ -0,0 +1,38 @@
+<?php		
+/* +***********************************************************************************		
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0		
+ * ("License"); You may not use this file except in compliance with the License		
+ * The Original Code is:  vtiger CRM Open Source		
+ * The Initial Developer of the Original Code is vtiger.		
+ * Portions created by vtiger are Copyright (C) vtiger.		
+ * All Rights Reserved.		
+ * Contributor(s): YetiForce.com		
+ * ********************************************************************************** */		
+$languageStrings = [		
+	//Blocks	
+	'LBL_PBXMANAGER_INFORMATION' => 'Çağrı detayları',
+	'LBL_CUSTOM_INFORMATION' => 'Özel bilgiler',
+	// list view settings links	
+	'LBL_SERVER_CONFIGURATION' => 'Sağlayıcı yapılandırması',
+	//Detail view header title	
+	'LBL_CALL_FROM' => 'Gelen Arama',
+	'LBL_CALL_TO' => 'Yapılan Arama',
+	//Incoming call pop-up	
+	'LBL_HIDDEN' => '(GİZLİ)',
+	// Fields	
+	'Total Duration' => 'Süre (sn)',
+	'Bill Duration' => 'Fatura süresi (sn)',
+	'Recording URL' => 'Kayıt',
+	'Start Time' => 'Başlama zamanı',
+	'Call Status' => 'Çağrı durumu',
+	'Customer Number' => 'Müşteri numarası',
+	'Customer' => 'Müşteri',
+	'User' => 'Kullanıcı',
+	'SINGLE_PBXManager' => 'Arama kaydı',
+	'Direction' => 'Yön',
+	'Gateway' => 'Ağ',
+	'Customer type' => 'Müşteri tipi',
+	'Source UUID' => 'Kaynak UUID',
+	'End Time' => 'Bitiş zamanı',
+	'Record Id' => 'Kayıt ID',
+];	
