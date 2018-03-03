197200bc071a0455a4ce8c2fc650edd21e9724b9
rtonline <36324712+rtonline@users.noreply.github.com>
2018-03-03 03:18:54 +0300

    Create Partners.php

---
+++ languages/tr_tr/Partners.php
@@ -0,0 +1,21 @@
+<?php		
+/**		
+ * Partners turkish translation.		
+ *		
+ * @copyright YetiForce Sp. z o.o		
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)		
+ */		
+$languageStrings = [		
+	'Partners' => 'Ortaklar',
+	'SINGLE_Partners' =>   'Ortak',
+	//BLOCKS	
+	'LBL_PARTNERS_INFORMATION' =>   'Temel bilgiler',
+	'LBL_CUSTOM_INFORMATION' =>   'Sistem bilgisi',
+	'LBL_DESCRIPTION_INFORMATION' =>   'Açıklama ayrıntıları',
+	//FIELDS	
+	'LBL_SUBJECT' =>   'Konu',
+	'LBL_NUMBER' =>   'Numara',
+	'LBL_CLOSED_TIME' =>   'Kapanış saati',
+	'LBL_VAT_ID' =>   'Vergi numarası',
+	'FL_CATEGORY' =>   'Kategori',
+];		
