5e9126998b71fa0812f4bf8749fe13f5b86c0605
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-03-05 15:15:46 +0100

    Added minor improvements

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.3.182',
-	'patchVersion' => '2018.03.02',
+	'appVersion' => '4.3.183',
+	'patchVersion' => '2018.03.05',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.40',
 	'lib_gantt' => '0.0.1',
