435b3cb0f2686dcf3e8cb22271952c57c3876634
rtonline <36324712+rtonline@users.noreply.github.com>
2018-03-08 02:11:53 +0300

    Update SQuoteEnquiries.php

--- languages/tr_tr/SQuoteEnquiries.php
+++ languages/tr_tr/SQuoteEnquiries.php
@@ -1,30 +1,30 @@
 <?php
 /**
- * SQuoteEnquiries english translation.
+ * SQuoteEnquiries turkish translation.
  *
  * @copyright YetiForce Sp. z o.o
  * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  */
 $languageStrings = [
-	'SQuoteEnquiries' => 'Sales enquiries',
-	'SINGLE_SQuoteEnquiries' => 'Sales enquiry',
+	'SQuoteEnquiries' => 'Satış araştırmaları',
+	'SINGLE_SQuoteEnquiries' => 'Satış araştırması',
 	//BLOCKS
-	'LBL_QUOTESENQUIRES_INFORMATION' => 'Basic information',
-	'LBL_CUSTOM_INFORMATION' => 'System information',
-	'LBL_DESCRIPTION_INFORMATION' => 'Description details',
-	'LBL_STATISTICS' => 'Statistics',
+	'LBL_QUOTESENQUIRES_INFORMATION' => 'Temel bilgiler',
+	'LBL_CUSTOM_INFORMATION' => 'Sistem bilgisi',
+	'LBL_DESCRIPTION_INFORMATION' => 'Açıklama ayrıntıları',
+	'LBL_STATISTICS' => 'İstatistik',
 	//FIELDS
-	'LBL_SUBJECT' => 'Subject',
-	'LBL_NUMBER' => 'Number',
-	'LBL_STATUS' => 'Status',
-	'LBL_CLOSED_TIME' => 'Closed time',
-	'LBL_RESPONSE_TIME' => 'Respone time',
-	'SINGLE_SSalesProcesses' => 'Opportunity',
+	'LBL_SUBJECT' => 'Konu',
+	'LBL_NUMBER' => 'Numara',
+	'LBL_STATUS' => 'Durumu',
+	'LBL_CLOSED_TIME' => 'Kapanış saati',
+	'LBL_RESPONSE_TIME' => 'Yanıt süresi',
+	'SINGLE_SSalesProcesses' => 'Fırsat',
 	//PICKLIST VALUES
-	'PLL_DRAFT' => 'Draft',
-	'PLL_IN_REALIZATION' => 'In realization',
-	'PLL_FOR_VERIFICATION' => 'For verification',
-	'PLL_CANCELLED' => 'Cancelled',
-	'PLL_COMPLETED' => 'Completed',
-	'LBL_CHANGE_STATUS' => 'Change status',
+	'PLL_DRAFT' => 'Taslak',
+	'PLL_IN_REALIZATION' => 'Gerçekleşmede',
+	'PLL_FOR_VERIFICATION' => 'Doğrulama için',
+	'PLL_CANCELLED' => 'İptal edildi',
+	'PLL_COMPLETED' => 'Tamamlandı',
+	'LBL_CHANGE_STATUS' => 'Durumu değiştir',
 ];
