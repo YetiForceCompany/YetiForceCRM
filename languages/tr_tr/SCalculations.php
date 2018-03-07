01e4ca186cfa9f1a6f90dd9f025ec2e98ed9e312
rtonline <36324712+rtonline@users.noreply.github.com>
2018-03-08 02:10:13 +0300

    Update SCalculations.php

--- languages/tr_tr/SCalculations.php
+++ languages/tr_tr/SCalculations.php
@@ -1,33 +1,33 @@
 <?php
 /**
- * SCalculations english translation.
+ * SCalculations turkish translation.
  *
  * @copyright YetiForce Sp. z o.o
  * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  */
 $languageStrings = [
-	'SCalculations' => 'Calculations',
-	'SINGLE_SCalculations' => 'Calculation',
+	'SCalculations' => 'Hesaplamalar',
+	'SINGLE_SCalculations' => 'Hesaplama',
 	//BLOCKS
-	'LBL_SCALCULATIONS_INFORMATION' => 'Basic information',
-	'LBL_CUSTOM_INFORMATION' => 'System information',
-	'LBL_DESCRIPTION_INFORMATION' => 'Description details',
-	'LBL_STATISTICS' => 'Statistics',
+	'LBL_SCALCULATIONS_INFORMATION' => 'Temel bilgiler',
+	'LBL_CUSTOM_INFORMATION' => 'Sistem bilgisi',
+	'LBL_DESCRIPTION_INFORMATION' => 'Açıklama ayrıntıları',
+	'LBL_STATISTICS' => 'istatistikler',
 	//FIELDS
-	'LBL_SUBJECT' => 'Subject',
-	'LBL_NUMBER' => 'Number',
-	'LBL_STATUS' => 'Status',
-	'LBL_CLOSED_TIME' => 'Closed time',
-	'LBL_RESPONSE_TIME' => 'Respone time',
-	'SINGLE_SSalesProcesses' => 'Opportunity',
-	'FL_TOTAL_PRICE' => 'Net total',
-	'FL_MARGINP' => 'Total margin percentage',
-	'FL_MARGIN' => 'Total margin',
+	'LBL_SUBJECT' => 'Konu',
+	'LBL_NUMBER' => 'Numara',
+	'LBL_STATUS' => 'Durumu',
+	'LBL_CLOSED_TIME' => 'Kapanış saati',
+	'LBL_RESPONSE_TIME' => 'Yanıt süresi',
+	'SINGLE_SSalesProcesses' => 'Fırsat',
+	'FL_TOTAL_PRICE' => 'Net toplam',
+	'FL_MARGINP' => 'Toplam marj yüzdesi',
+	'FL_MARGIN' => 'Toplam marj',
 	//PICKLIST VALUES
-	'PLL_DRAFT' => 'Draft',
-	'PLL_IN_REALIZATION' => 'In realization',
-	'PLL_FOR_VERIFICATION' => 'For verification',
-	'PLL_CANCELLED' => 'Cancelled',
-	'PLL_COMPLETED' => 'Completed',
-	'LBL_CHANGE_STATUS' => 'Change status',
+	'PLL_DRAFT' => 'Taslak',
+	'PLL_IN_REALIZATION' => 'Gerçekleşmede',
+	'PLL_FOR_VERIFICATION' => 'Doğrulama için',
+	'PLL_CANCELLED' => 'İptal edildi',
+	'PLL_COMPLETED' => 'Tamamlandı',
+	'LBL_CHANGE_STATUS' => 'Durumu değiştir',
 ];
