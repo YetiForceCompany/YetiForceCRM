963f9f78a2f8ae1f2c349f46bfcedf7682964ecb
rtonline <36324712+rtonline@users.noreply.github.com>
2018-03-08 01:50:33 +0300

    Update PermissionInspector.php

--- languages/tr_tr/PermissionInspector.php
+++ languages/tr_tr/PermissionInspector.php
@@ -1,36 +1,36 @@
 <?php
 /**
- * PermissionInspector english translation.
+ * PermissionInspector turkish translation.
  *
  * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
 $languageStrings = [
-	'PermissionInspector' => 'Permission Inspector',
-	'LBL_INSPECTION_PERMISSION_HEADER' => 'Permission Inspector',
-	'LBL_FULL_USER_NAME' => 'User',
-	'LBL_VIEW_PRVILIGE' => 'View',
-	'LBL_CREATE_PRIVILIGE' => 'Create',
-	'LBL_EDIT_PRIVILIGE' => 'Edit',
-	'LBL_DELETE_PRIVILIGE' => 'Delete',
-	'LBL_PROFILES' => 'Profile:',
+	'PermissionInspector' => 'İzin Müfettişi',
+	'LBL_INSPECTION_PERMISSION_HEADER' => 'İzin Müfettişi',
+	'LBL_FULL_USER_NAME' => 'Kullanıcı',
+	'LBL_VIEW_PRVILIGE' => 'Görünüm',
+	'LBL_CREATE_PRIVILIGE' => 'Oluşturmak',
+	'LBL_EDIT_PRIVILIGE' => 'Düzenle',
+	'LBL_DELETE_PRIVILIGE' => 'Sil',
+	'LBL_PROFILES' => 'Profil:',
 	// Access rights
-	'SEC_MODULE_DONT_HAVE_SECURITY_RIGHT' => 'Module/action excluded from verification of permissions',
-	'SEC_USER_IS_ADMIN' => 'User is Administrator',
-	'SEC_NO_ACTION_MODULE_PERMISSIONS_YES' => 'Permissions to module, user has access to this module\'s data',
-	'SEC_MODULE_VIEW_ALL_PERMISSION' => 'Global permissions to view data',
-	'SEC_MODULE_EDIT_ALL_PERMISSION' => 'Global permissions to edit data',
-	'SEC_RECORID_IS_NULL' => 'General access to actions in module',
-	'SEC_MODULE_RIGHTS_TO_ACTION' => 'Access based on permissions to perform actions in the module',
-	'SEC_RECORD_BY_SHARING_YES' => 'Access based on access rules (Module access)',
-	'SEC_RECORD_OWNER_CURRENT_USER' => 'Access based on owner %s',
-	'SEC_RECORD_OWNER_CURRENT_GROUP' => 'Access based on group %s',
-	'SEC_RECORD_OWNER_SUBORDINATE_USER' => 'Access based on company\'s hierarchy (roles), user is higher in hierarchy than record owner',
-	'SEC_RECORD_HIERARCHY_USER' => 'Access based on permission\'s hierarchy',
+	'SEC_MODULE_DONT_HAVE_SECURITY_RIGHT' => 'İzinlerin doğrulamasından hariç tutulan modül/eylem',
+	'SEC_USER_IS_ADMIN' => 'Kullanıcı Yöneticidir',
+	'SEC_NO_ACTION_MODULE_PERMISSIONS_YES' => 'Modül için izinler, Kullanıcının bu modül verilere erişimi vardır',
+	'SEC_MODULE_VIEW_ALL_PERMISSION' => 'Verileri görüntülemek için genel izinler',
+	'SEC_MODULE_EDIT_ALL_PERMISSION' => 'Verileri düzenlemek için genel izinler',
+	'SEC_RECORID_IS_NULL' => 'Modül içindeki eylemlere genel erişim',
+	'SEC_MODULE_RIGHTS_TO_ACTION' => 'Modüldeki eylemleri gerçekleştirmek için izinlere dayalı olarak erişim',
+	'SEC_RECORD_BY_SHARING_YES' => 'Erişim kurallarına göre erişim (Modül erişimi)',
+	'SEC_RECORD_OWNER_CURRENT_USER' => '%s kullanıcısına dayanan erişim',
+	'SEC_RECORD_OWNER_CURRENT_GROUP' => '%s grubuna dayalı erişim',
+	'SEC_RECORD_OWNER_SUBORDINATE_USER' => 'Şirketin hiyerarşisine (roller) dayalı erişim, Kullanıcı kayıt sahibine göre hiyerarşide daha yüksektir',
+	'SEC_RECORD_HIERARCHY_USER' => 'İzinlerin hiyerarşisine dayalı erişim',
 	// No access rights
-	'SEC_NO_ACTION_MODULE_PERMISSIONS_NO' => 'No permissions to module, user has no access to the module',
-	'SEC_MODULE_PERMISSIONS_NO' => 'No permissions to module, user has no access to the module',
-	'SEC_MODULE_NO_RIGHTS_TO_ACTION' => 'No permissions to perform actions in the module',
-	'SEC_RECORD_BY_SHARING_NO' => 'No permissions based on access rules (Module access)',
-	'SEC_MODULE_NO_ACTION_TOOL' => 'No permissions for action - Action not found',
+	'SEC_NO_ACTION_MODULE_PERMISSIONS_NO' => 'Modül için izin yok, kullanıcının modüle erişim izni yok',
+	'SEC_MODULE_PERMISSIONS_NO' => 'Modül için izin yok, kullanıcının modüle erişim izni yok',
+	'SEC_MODULE_NO_RIGHTS_TO_ACTION' => 'Modüldeki eylemleri gerçekleştirmek için izin yok',
+	'SEC_RECORD_BY_SHARING_NO' => 'Erişim kurallarına dayalı izin yok (Modül erişimi)',
+	'SEC_MODULE_NO_ACTION_TOOL' => 'Eylem için izin yok - Eylem bulunamadı',
 ];
