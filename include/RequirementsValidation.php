ebdf4bfe5b308ae109c6062c5a120320859c4043
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2018-03-08 16:16:00 +0100

    Update RequirementsValidation.php

--- include/RequirementsValidation.php
+++ include/RequirementsValidation.php
@@ -5,6 +5,6 @@
  * @copyright YetiForce Sp. z o.o
  * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  */
-if (version_compare(PHP_VERSION, '7.0', '<')) {
+if (version_compare(PHP_VERSION, '7.1', '<')) {
 	throw new \App\Exceptions\AppException('Wrong PHP version, recommended version >= 7.0');
 }
