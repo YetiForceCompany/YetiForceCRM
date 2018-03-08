c781a9576e02e56283bfd4bba99f3167b8eba65c
Katarzyna Ulichnowska <k.ulichnowska@yetiforce.com>
2018-03-08 12:35:55 +0100

    Added translations

--- languages/de_de/Vtiger.php
+++ languages/de_de/Vtiger.php
@@ -1593,4 +1593,11 @@ $jsLanguageStrings = [
 	'JS_CUSTOM' => 'Benutzerdefinierter Datumsbereich',
 	'JS_CALCULATING_IN_PROGRESS' => 'Kalkulation wird gerade durchgeführt',
 	'JS_FRAME_IN_PROGRESS' => 'Vorschau wird geladen ...',
+	'JS_IGNORE_EMPTY_VALUES' => 'Ignore empty values',
+	'JS_H' => 'h',
+	'JS_M' => 'm',
+	'JS_S' => 's',
+	'JS_H_LONG' => 'hours',
+	'JS_M_LONG' => 'minutes',
+	'JS_S_LONG' => 'seconds',
 ];
