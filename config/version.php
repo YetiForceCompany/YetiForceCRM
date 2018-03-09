dfe258c0f505d63ac826147670808889859a47be
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-03-09 08:45:05 +0100

    Update configuration tests

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.3.196',
-	'patchVersion' => '2018.03.08',
+	'appVersion' => '4.3.197',
+	'patchVersion' => '2018.03.09',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.40',
 	'lib_gantt' => '0.0.1',
--- tests/times.php
+++ tests/times.php
@@ -1,4 +1,10 @@
 <?php
+
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\Test;
+use PHPUnit\Framework\TestSuite;
+use PHPUnit\Framework\Warning;
+
 /**
  * Travis CI test script.
  *
@@ -8,73 +14,73 @@
  */
 // @codeCoverageIgnoreStart
 
-class times implements PHPUnit\Framework\TestListener
+class YtTimes implements PHPUnit\Framework\TestListener
 {
-    public function startTest(PHPUnit\Framework\Test $test)
-    {
-        //printf("Test %s started.\n", $test->getName());
-        //echo "\n";
-    }
+	public function startTest(Test $test): void
+	{
+		//printf("Test %s started.\n", $test->getName());
+		//echo "\n";
+	}
 
-    public function endTest(PHPUnit\Framework\Test $test, $time)
-    {
-        $time = round($time, 2);
-        echo " - $time second(s) | Assertions: ".$test->getNumAssertions();
-    }
+	public function endTest(Test $test, float $time): void
+	{
+		$time = round($time, 2);
+		echo " - $time second(s) | Assertions: " . $test->getNumAssertions();
+	}
 
-    public function addError(PHPUnit\Framework\Test $test, \Exception $e, $time)
-    {
-        $time = round($time, 2);
-        echo '! Test '.$test->getName()." error.\n";
-        //echo "Exception Message: " . $e->getMessage() . "\n";
-        //echo "Exception Trace:\n" . $e->getTraceAsString() . "\n";
-    }
+	public function addError(Test $test, \Throwable $t, float $time): void
+	{
+		$time = round($time, 2);
+		echo '! Test ' . $test->getName() . " error.\n";
+		//echo "Exception Message: " . $e->getMessage() . "\n";
+		//echo "Exception Trace:\n" . $e->getTraceAsString() . "\n";
+	}
 
-    public function addWarning(PHPUnit\Framework\Test $test, PHPUnit\Framework\Warning $e, $time)
-    {
-        $time = round($time, 2);
-        echo '! Test '.$test->getName()." warning.\n";
-        //echo "Exception Message: " . $e->getMessage() . "\n";
-        //echo "Exception Trace:\n" . $e->getTraceAsString() . "\n";
-    }
+	public function addWarning(Test $test, Warning $e, float $time): void
+	{
+		$time = round($time, 2);
+		echo '! Test ' . $test->getName() . " warning.\n";
+		//echo "Exception Message: " . $e->getMessage() . "\n";
+		//echo "Exception Trace:\n" . $e->getTraceAsString() . "\n";
+	}
 
-    public function addFailure(PHPUnit\Framework\Test $test, PHPUnit\Framework\AssertionFailedError $e, $time)
-    {
-        $time = round($time, 2);
-        echo '! Test '.$test->getName()." failed.\n";
-        //echo "Exception Message: " . $e->getMessage() . "\n";
-        //echo "Exception Trace:\n" . $e->getTraceAsString() . "\n";
-    }
+	public function addFailure(Test $test, AssertionFailedError $e, float $time): void
+	{
+		$time = round($time, 2);
+		echo '! Test ' . $test->getName() . " failed.\n";
+		//echo "Exception Message: " . $e->getMessage() . "\n";
+		//echo "Exception Trace:\n" . $e->getTraceAsString() . "\n";
+	}
 
-    public function addIncompleteTest(PHPUnit\Framework\Test $test, \Exception $e, $time)
-    {
-        $time = round($time, 2);
-        printf("addIncompleteTest: Test '%s' is incomplete.\n", $test->getName());
-    }
+	public function addIncompleteTest(Test $test, \Throwable $t, float $time): void
+	{
+		$time = round($time, 2);
+		printf("addIncompleteTest: Test '%s' is incomplete.\n", $test->getName());
+	}
 
-    public function addRiskyTest(PHPUnit\Framework\Test $test, \Exception $e, $time)
-    {
-        printf("! Test %s is deemed risky.\n", $test->getName());
-        //echo "Exception Message: " . $e->getMessage() . "\n";
-        //echo "Exception Trace:\n" . $e->getTraceAsString() . "\n";
-    }
+	public function addRiskyTest(Test $test, \Throwable $t, float $time): void
+	{
+		printf("! Test %s is deemed risky.\n", $test->getName());
+		//echo "Exception Message: " . $e->getMessage() . "\n";
+		//echo "Exception Trace:\n" . $e->getTraceAsString() . "\n";
+	}
 
-    public function addSkippedTest(PHPUnit\Framework\Test $test, \Exception $e, $time)
-    {
-        $time = round($time, 2);
-        printf("! Test '%s' has been skipped. ($time second(s))\n", $test->getName());
-        //echo "Exception Message: " . $e->getMessage() . "\n";
-        //echo "Exception Trace:\n" . $e->getTraceAsString() . "\n";
-    }
+	public function addSkippedTest(Test $test, \Throwable $t, float $time): void
+	{
+		$time = round($time, 2);
+		printf("! Test '%s' has been skipped. ($time second(s))\n", $test->getName());
+		//echo "Exception Message: " . $e->getMessage() . "\n";
+		//echo "Exception Trace:\n" . $e->getTraceAsString() . "\n";
+	}
 
-    public function startTestSuite(PHPUnit\Framework\TestSuite $suite)
-    {
-    }
+	public function startTestSuite(TestSuite $suite): void
+	{
+	}
 
-    public function endTestSuite(PHPUnit\Framework\TestSuite $suite)
-    {
-        //printf("Ended all tests: %s.\n", $suite->getName());
-    }
+	public function endTestSuite(TestSuite $suite): void
+	{
+		//printf("Ended all tests: %s.\n", $suite->getName());
+	}
 }
 
 // @codeCoverageIgnoreEnd
