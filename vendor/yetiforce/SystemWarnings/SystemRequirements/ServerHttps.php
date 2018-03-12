3201408f51bdb87fd83844b01bf64d97bb399bf6
Wojciech Brüggemann <w.bruggemann@yetiforce.com>
2018-03-12 15:26:58 +0100

    Revert "Updated system warnings list to bootstrap 4 (fixed link)"
    This reverts commit 0b1e87d86a9e95b12f1cf90dfb545f3ea0dd31aa.

--- vendor/yetiforce/SystemWarnings/SystemRequirements/ServerHttps.php
+++ vendor/yetiforce/SystemWarnings/SystemRequirements/ServerHttps.php
@@ -30,8 +30,7 @@ class ServerHttps extends \App\SystemWarnings\Template
 			$this->status = 0;
 		}
 		if ($this->status === 0) {
-			$requestUrl = (\App\RequestUtil::getBrowserInfo()->https ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-			$this->link = $requestUrl . '?parent=Settings&module=ConfReport&view=Index';
+			$this->link = 'https://yetiforce.com/en/implementer/installation-updates/103-web-server-requirements.html';
 			$this->linkTitle = \App\Language::translate('BTN_CONFIGURE_HTTPS', 'Settings:SystemWarnings');
 			$this->description = \App\Language::translate('LBL_MISSING_HTTPS', 'Settings:SystemWarnings', \Settings_ModuleManager_Library_Model::TEMP_DIR);
 		}
