3a35d8eff85f4fbf8f03c27062c8dbfe39896c03
Adrian Koń <a.kon@yetiforce.com>
2018-03-12 09:43:16 +0100

    Added info to comment

--- config/modules/Accounts.php
+++ config/modules/Accounts.php
@@ -6,18 +6,18 @@
  * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  */
 return [
-    // Columns visible in Account hierarchy [$label => $columnName]
-    'COLUMNS_IN_HIERARCHY' => [],
-    // Max depth of hierarchy
-    'MAX_HIERARCHY_DEPTH' => 50,
-    // Count Accounts in hierarchy
-    'COUNT_IN_HIERARCHY' => true,
-    // Show summary products services bookmark
-    'SHOW_SUMMARY_PRODUCTS_SERVICES' => true,
-    // Default view for record detail view. Values: LBL_RECORD_DETAILS or LBL_RECORD_SUMMARY
-    'DEFAULT_VIEW_RECORD' => 'LBL_RECORD_PREVIEW',
-    // Default module view. Values: List, ListPreview or DashBoard
-    'defaultViewName' => 'List',
-    // Default record view for list preview. Values: full or summary
-    'defaultDetailViewName' => 'full',
+	// Columns visible in Account hierarchy [$label => $columnName]
+	'COLUMNS_IN_HIERARCHY' => [],
+	// Max depth of hierarchy
+	'MAX_HIERARCHY_DEPTH' => 50,
+	// Count Accounts in hierarchy
+	'COUNT_IN_HIERARCHY' => true,
+	// Show summary products services bookmark
+	'SHOW_SUMMARY_PRODUCTS_SERVICES' => true,
+	// Default view for record detail view. Values: LBL_RECORD_DETAILS or LBL_RECORD_SUMMARY
+	'DEFAULT_VIEW_RECORD' => 'LBL_RECORD_PREVIEW',
+	// Default module view. Values: List, ListPreview or DashBoard, refresh menu files after you change this value
+	'defaultViewName' => 'List',
+	// Default record view for list preview. Values: full or summary
+	'defaultDetailViewName' => 'full',
 ];
