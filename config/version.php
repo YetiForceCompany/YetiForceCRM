64e789bc764a954e37708e199661f81605c13cee
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-03-12 14:51:39 +0100

    Fixed icons loading

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 return [
-	'appVersion' => '4.3.205',
+	'appVersion' => '4.3.206',
 	'patchVersion' => '2018.03.12',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.40',
+	'lib_roundcube' => '0.0.41',
 	'lib_gantt' => '0.0.1',
 ];
