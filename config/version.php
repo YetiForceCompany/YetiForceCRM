8f5e4b8c7aa36acf299d3a1ddd5ca437359f9b5b
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-03-13 23:17:38 +0100

    Generated css file

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.3.206',
-	'patchVersion' => '2018.03.12',
+	'appVersion' => '4.3.207',
+	'patchVersion' => '2018.03.13',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.41',
 	'lib_gantt' => '0.0.1',
