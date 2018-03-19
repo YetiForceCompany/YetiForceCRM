612c4040bbbe987da6a24956391e76376fc8a536
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-03-19 08:45:27 +0100

    Improved translations

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.3.218',
-	'patchVersion' => '2018.03.18',
+	'appVersion' => '4.3.219',
+	'patchVersion' => '2018.03.19',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.41',
 	'lib_gantt' => '0.0.1',
