e1c94c384caa7b78f52a17ee06c9843e01fb8ba6
Adrian Koń <a.kon@yetiforce.com>
2018-03-20 13:19:12 +0100

    Fixed #6335

--- public_html/layouts/basic/modules/Vtiger/resources/Edit.js
+++ public_html/layouts/basic/modules/Vtiger/resources/Edit.js
@@ -966,7 +966,14 @@ jQuery.Class("Vtiger_Edit_Js", {
 						'enabled': true
 					}
 				});
-
+				var blocks = editViewForm.find('.blockHeader');
+				blocks.each(function (index, block) {
+					var currentBlock = jQuery(block);
+					var headerAnimationElement = currentBlock.find('.blockToggle').not('.d-none');
+					var bodyContents = currentBlock.closest('.js-toggle-panel').find('.blockContent');
+					currentBlock.find("[data-mode='hide']").removeClass('d-none');
+					bodyContents.removeClass('d-none');
+				});
 				var module = jQuery(e.currentTarget).find('[name="module"]').val();
 				if (editViewForm.validationEngine('validate')) {
 					//Once the form is submiting add data attribute to that form element
