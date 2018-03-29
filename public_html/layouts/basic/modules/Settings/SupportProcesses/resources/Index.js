54eca34c740ad8fa071af172189f692d44d4a1ad
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2018-03-29 22:54:25 +0200

    Improved process panels

--- public_html/layouts/basic/modules/Settings/SupportProcesses/resources/Index.js
+++ public_html/layouts/basic/modules/Settings/SupportProcesses/resources/Index.js
@@ -1,9 +1,13 @@
 /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
 
 jQuery.Class('Settings_SupportProcesses_Index_Js', {}, {
+	/**
+	 * Register change field value
+	 * @param {jQuery} content
+	 */
 	registerChangeVal: function (content) {
 		var thisInstance = this;
-		content.find('.configField').on('change', function (e) {
+		content.find('.js-config-field').on('change', function (e) {
 			var target = $(e.currentTarget);
 			var params = {};
 			params['type'] = target.data('type');
