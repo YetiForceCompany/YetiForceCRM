b8885cb08ff0c4c5b649cadd85c12c9bdeb731e1
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-04-04 13:11:15 +0200

    Improved email address validation

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.269',
-	'patchVersion' => '2018.04.03',
+	'appVersion' => '4.3.270',
+	'patchVersion' => '2018.04.04',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.41',
 	'lib_gantt' => '0.0.1',
