07a21b3c1d6b383202b8b720ff2f107000c6ee48
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-04-06 16:11:33 +0200

    Configured full ckeditor configuration

--- public_html/layouts/basic/modules/Vtiger/resources/CkEditor.js
+++ public_html/layouts/basic/modules/Vtiger/resources/CkEditor.js
@@ -71,7 +71,30 @@ class Vtiger_CkEditor_Js {
 						evt.editor.updateElement();
 					});
 				}
-			}
+			},
+			fullPage: false,
+			allowedContent: true,
+			scayt_autoStartup: false,
+			enterMode: CKEDITOR.ENTER_BR,
+			shiftEnterMode: CKEDITOR.ENTER_P,
+			toolbar: [
+			{name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
+			{name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'Scayt']},
+			{name: 'links', items: ['Link', 'Unlink']},
+			{name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar', 'PageBreak']},
+			{name: 'tools', items: ['Maximize', 'ShowBlocks']},
+			{name: 'paragraph', items: ['Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv']},
+			{name: 'document', items: ['Source', 'Print']},
+			'/',
+			{name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize']},
+			{name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript']},
+			{name: 'colors', items: ['TextColor', 'BGColor']},
+			{
+				name: 'paragraph',
+				items: ['NumberedList', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl']
+			},
+			{name: 'basicstyles', items: ['CopyFormatting', 'RemoveFormat']},
+		]
 		};
 
 		if (typeof customConfig !== 'undefined') {
@@ -92,3 +115,43 @@ class Vtiger_CkEditor_Js {
 		editor.setData(editor.getData().replace(editorData, contents));
 	}
 }
+
+// CKEDITOR.editorConfig = function (config) {
+// 	// Define changes to default configuration here. For example:
+// 	// config.language = 'fr';
+// 	// config.uiColor = '#AADC6E';
+// 	config.fullPage = false;
+// 	config.allowedContent = true;
+// 	config.scayt_autoStartup = false;
+// 	config.enterMode = CKEDITOR.ENTER_BR;
+// 	config.shiftEnterMode = CKEDITOR.ENTER_P;
+// 	config.toolbar = 'Full';
+// 	config.toolbar_Full = [
+// 		{name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
+// 		{name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'Scayt']},
+// 		{name: 'links', items: ['Link', 'Unlink']},
+// 		{name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar', 'PageBreak']},
+// 		{name: 'tools', items: ['Maximize', 'ShowBlocks']},
+// 		{name: 'paragraph', items: ['Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv']},
+// 		{name: 'document', items: ['Source', 'Print']},
+// 		'/',
+// 		{name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize']},
+// 		{name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript']},
+// 		{name: 'colors', items: ['TextColor', 'BGColor']},
+// 		{
+// 			name: 'paragraph',
+// 			items: ['NumberedList', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl']
+// 		},
+// 		{name: 'basicstyles', items: ['CopyFormatting', 'RemoveFormat']},
+// 	];
+// 	config.toolbar_Min = [
+// 		{name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript']},
+// 		{name: 'colors', items: ['TextColor', 'BGColor']},
+// 		{name: 'tools', items: ['Maximize']},
+// 		{
+// 			name: 'paragraph',
+// 			items: ['NumberedList', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl']
+// 		},
+// 		{name: 'basicstyles', items: ['CopyFormatting', 'RemoveFormat']},
+// 	];
+// };
