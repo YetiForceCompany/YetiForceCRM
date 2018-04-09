0f708da7fb3e21e61253f70edd71886634839bf3
Adrian Koń <a.kon@yetiforce.com>
2018-04-09 19:36:34 +0200

    Added missing files to new credits

---
+++ modules/Settings/Vtiger/views/LibraryMoreInfo.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Library More Info View Class.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Adrian Koń <a.kon@yetiforce.com>
+ */
+class Settings_Vtiger_LibraryMoreInfo_View extends Vtiger_BasicModal_View
+{
+	/**
+	 * Checking permissions.
+	 *
+	 * @param \App\Request $request
+	 *
+	 * @throws \App\Exceptions\NoPermittedForAdmin
+	 */
+	public function checkPermission(\App\Request $request)
+	{
+		if (!\App\User::getCurrentUserModel()->isAdmin()) {
+			throw new \App\Exceptions\NoPermittedForAdmin('LBL_PERMISSION_DENIED');
+		}
+	}
+
+	public function process(\App\Request $request)
+	{
+		$result = false;
+		$fileContent = '';
+		if ($request->isEmpty('type') || $request->isEmpty('libraryName')) {
+			$result = false;
+		} else {
+			if ($request->getByType('type', 1) === 'public') {
+				$dir = ROOT_DIRECTORY . DIRECTORY_SEPARATOR . 'public_html' . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR;
+				$packageFile = $dir . $request->getByType('libraryName', 'Text') . DIRECTORY_SEPARATOR . 'package.json';
+				if (file_exists($packageFile)) {
+					$fileContent = file_get_contents($packageFile);
+					$result = true;
+				} else {
+					$result = false;
+				}
+			} elseif ($request->getByType('type', 1) === 'vendor') {
+				$filePath = 'vendor' . DIRECTORY_SEPARATOR . $request->getByType('libraryName', 'Text') . DIRECTORY_SEPARATOR . 'composer.json';
+				if (file_exists($filePath)) {
+					$fileContent = file_get_contents($filePath);
+				} else {
+					$result = false;
+				}
+			} else {
+				$result = false;
+			}
+		}
+		$this->preProcess($request);
+		$viewer = $this->getViewer($request);
+		$qualifiedModuleName = $request->getModule(false);
+		$viewer = $this->getViewer($request);
+		$viewer->assign('QUALIFIED_MODULE', $qualifiedModuleName);
+		$viewer->assign('RESULT', $result);
+		$viewer->assign('FILE_CONTENT', $fileContent);
+		$viewer->view('LibraryMoreInfo.tpl', $qualifiedModuleName);
+		$this->postProcess($request);
+	}
+}
