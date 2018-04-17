383d46ee54c1b11e7cc1f1cd8208e6baf6888e0f
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-04-17 15:20:37 +0200

    Update lib_roundcube

--- config/version.php
+++ config/version.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-	'appVersion' => '4.3.300',
+	'appVersion' => '4.3.301',
 	'patchVersion' => '2018.04.17',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.41',
+	'lib_roundcube' => '0.0.42',
 	'lib_gantt' => '0.0.1',
 ];
