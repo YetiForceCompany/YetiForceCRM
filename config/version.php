7aff51788b0bf1edefec65c60fa44af45ddcf19f
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-04-18 07:56:35 +0200

    Fixed path documents

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.302',
-	'patchVersion' => '2018.04.17',
+	'appVersion' => '4.3.303',
+	'patchVersion' => '2018.04.18',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.42',
 	'lib_gantt' => '0.0.1',
