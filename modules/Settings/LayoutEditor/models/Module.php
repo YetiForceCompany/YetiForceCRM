c2692d754838d5cdccbee8a37f3dff97e458c0c3
rafalpospiech <r.pospiech@yetiforce.com>
2018-04-19 10:49:13 +0200

    Image field type added

--- modules/Settings/LayoutEditor/models/Module.php
+++ modules/Settings/LayoutEditor/models/Module.php
@@ -109,7 +109,7 @@ class Settings_LayoutEditor_Module_Model extends Vtiger_Module_Model
 	public function getAddSupportedFieldTypes()
 	{
 		return [
-			'Text', 'Decimal', 'Integer', 'Percent', 'Currency', 'Date', 'Email', 'Phone', 'Picklist', 'URL', 'Checkbox', 'TextArea', 'MultiSelectCombo', 'Skype', 'Time', 'Related1M', 'Editor', 'Tree', 'MultiReferenceValue', 'CategoryMultipicklist', 'DateTime', 'MultiImage'
+			'Text', 'Decimal', 'Integer', 'Percent', 'Currency', 'Date', 'Email', 'Phone', 'Picklist', 'URL', 'Checkbox', 'TextArea', 'MultiSelectCombo', 'Skype', 'Time', 'Related1M', 'Editor', 'Tree', 'MultiReferenceValue', 'CategoryMultipicklist', 'DateTime', 'Image', 'MultiImage'
 		];
 	}
 
@@ -389,6 +389,11 @@ class Settings_LayoutEditor_Module_Model extends Vtiger_Module_Model
 				break;
 			case 'MultiImage':
 				$uitype = 311;
+				$type = $importerType->text();
+				$uichekdata = 'V~O';
+				break;
+			case 'Image':
+				$uitype = 312;
 				$type = $importerType->text();
 				$uichekdata = 'V~O';
 				break;
