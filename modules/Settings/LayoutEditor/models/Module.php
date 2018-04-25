bd9e2cfcbafdaf29062f594a6b47053c09c4adfd
rafalpospiech <r.pospiech@yetiforce.com>
2018-04-25 11:10:44 +0200

    Custom field added - Country UI type

--- modules/Settings/LayoutEditor/models/Module.php
+++ modules/Settings/LayoutEditor/models/Module.php
@@ -109,12 +109,12 @@ class Settings_LayoutEditor_Module_Model extends Vtiger_Module_Model
 	public function getAddSupportedFieldTypes()
 	{
 		return [
-			'Text', 'Decimal', 'Integer', 'Percent', 'Currency', 'Date', 'Email', 'Phone', 'Picklist', 'URL', 'Checkbox', 'TextArea', 'MultiSelectCombo', 'Skype', 'Time', 'Related1M', 'Editor', 'Tree', 'MultiReferenceValue', 'CategoryMultipicklist', 'DateTime', 'Image', 'MultiImage'
+			'Text', 'Decimal', 'Integer', 'Percent', 'Currency', 'Date', 'Email', 'Phone', 'Picklist', 'Country', 'URL', 'Checkbox', 'TextArea', 'MultiSelectCombo', 'Skype', 'Time', 'Related1M', 'Editor', 'Tree', 'MultiReferenceValue', 'CategoryMultipicklist', 'DateTime', 'Image', 'MultiImage'
 		];
 	}
 
 	/**
-	 * Function whcih will give information about the field types that are supported for add.
+	 * Function which will give information about the field types that are supported for add.
 	 *
 	 * @return <Array>
 	 */
@@ -406,6 +406,11 @@ class Settings_LayoutEditor_Module_Model extends Vtiger_Module_Model
 				$uichekdata = 'DT~O';
 				$uitype = 79;
 				$type = $importerType->dateTime();
+				break;
+			case 'Country':
+				$uitype = 35;
+				$uichekdata = 'V~O';
+				$type = $importerType->text();
 				break;
 		}
 
