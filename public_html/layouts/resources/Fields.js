e272d337ab8a5e2e04b832ab6ba6d5085e77eb7b
rafalpospiech <r.pospiech@yetiforce.com>
2018-04-27 13:57:54 +0200

    fixed select2 search bug inside modals

--- public_html/layouts/resources/Fields.js
+++ public_html/layouts/resources/Fields.js
@@ -496,6 +496,10 @@ App.Fields = {
 					this.showSelect2ElementView($(element).eq(0), params);
 				});
 			}
+			const modalParent = $(selectElement).closest('.modal');
+			if (modalParent.length) {
+				params.dropdownParent = modalParent;
+			}
 			let data = selectElement.data();
 			if (data != null) {
 				params = $.extend(data, params);
