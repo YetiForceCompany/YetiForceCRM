c6d40a8d021953548711357fc640319e69a9c5c2
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-04-27 10:44:13 +0200

    Added minor improvements in the modal records list Generated css, js file

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.322',
-	'patchVersion' => '2018.04.26',
+	'appVersion' => '4.3.323',
+	'patchVersion' => '2018.04.27',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.42',
 	'lib_gantt' => '0.0.1',
