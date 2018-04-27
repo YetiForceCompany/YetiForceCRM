683163a7c012318e702135b21fb8f01fc4881066
Walter Luis <walterluisglez@gmail.com>
2018-04-27 11:19:10 +0100

    Fix error in process install, can not use the ".length" function of "undefined"

--- public_html/layouts/resources/app.js
+++ public_html/layouts/resources/app.js
@@ -875,32 +875,34 @@ app = {
 	},
 	updateRowHeight: function () {
 		var rowType = CONFIG.rowHeight;
-		if (rowType.length <= 0) {
-			//Need to update the row height
-			var widthType = app.cacheGet('widthType', 'mediumWidthType');
-			var serverWidth = widthType;
-			switch (serverWidth) {
-				case 'narrowWidthType' :
-					serverWidth = 'narrow';
-					break;
-				case 'wideWidthType' :
-					serverWidth = 'wide';
-					break;
-				default :
-					serverWidth = 'medium';
-			}
-			var userid = CONFIG.userId;
-			var params = {
-				'module': 'Users',
-				'action': 'SaveAjax',
-				'record': userid,
-				'value': serverWidth,
-				'field': 'rowheight'
-			};
-			AppConnector.request(params).then(function () {
-				$(rowType).val(serverWidth);
-			});
-		}
+		if (typeof rowType !== 'undefined') {
+      if (rowType.length <= 0) {
+        //Need to update the row height
+        var widthType = app.cacheGet('widthType', 'mediumWidthType');
+        var serverWidth = widthType;
+        switch (serverWidth) {
+          case 'narrowWidthType' :
+            serverWidth = 'narrow';
+            break;
+          case 'wideWidthType' :
+            serverWidth = 'wide';
+            break;
+          default :
+            serverWidth = 'medium';
+        }
+        var userid = CONFIG.userId;
+        var params = {
+          'module': 'Users',
+          'action': 'SaveAjax',
+          'record': userid,
+          'value': serverWidth,
+          'field': 'rowheight'
+        };
+        AppConnector.request(params).then(function () {
+          $(rowType).val(serverWidth);
+        });
+      }
+    }
 	},
 	getCookie: function (c_name) {
 		var c_value = document.cookie;
