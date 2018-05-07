dbbed983eabd3fdc4b828aba7f1e1c97ea54436f
Adrian Kon <a.kon@yetiforce.com>
2018-05-07 18:56:12 +0200

    Added DataSetRegister module

---
+++ modules/DataSetRegister/DataSetRegister.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Adrian Koń <a.kon@yetiforce.com>
+ */
+include_once 'modules/Vtiger/CRMEntity.php';
+
+class DataSetRegister extends Vtiger_CRMEntity
+{
+	public $table_name = 'u_yf_datasetregister';
+	public $table_index = 'datasetregisterid';
+
+	/**
+	 * Mandatory table for supporting custom fields.
+	 */
+	public $customFieldTable = ['u_yf_datasetregistercf', 'datasetregisterid'];
+
+	/**
+	 * Mandatory for Saving, Include tables related to this module.
+	 */
+	public $tab_name = ['vtiger_crmentity', 'u_yf_datasetregister', 'u_yf_datasetregistercf'];
+
+	/**
+	 * Mandatory for Saving, Include tablename and tablekey columnname here.
+	 */
+	public $tab_name_index = [
+		'vtiger_crmentity' => 'crmid',
+		'u_yf_datasetregister' => 'datasetregisterid',
+		'u_yf_datasetregistercf' => 'datasetregisterid',
+	];
+
+	/**
+	 * Mandatory for Listing (Related listview).
+	 */
+	public $list_fields = [
+		// Format: Field Label => Array(tablename, columnname)
+		// tablename should not have prefix 'vtiger_'
+		'LBL_SUBJECT' => ['datasetregister', 'subject'],
+		'Assigned To' => ['crmentity', 'smownerid'],
+	];
+	public $list_fields_name = [
+		// Format: Field Label => fieldname
+		'LBL_SUBJECT' => 'subject',
+		'Assigned To' => 'assigned_user_id',
+	];
+	// Make the field link to detail view
+	public $list_link_field = 'subject';
+	// For Popup listview and UI type support
+	public $search_fields = [
+		// Format: Field Label => Array(tablename, columnname)
+		// tablename should not have prefix 'vtiger_'
+		'LBL_SUBJECT' => ['datasetregister', 'subject'],
+		'Assigned To' => ['vtiger_crmentity', 'assigned_user_id'],
+	];
+	public $search_fields_name = [
+		// Format: Field Label => fieldname
+		'LBL_SUBJECT' => 'subject',
+		'Assigned To' => 'assigned_user_id',
+	];
+	// For Popup window record selection
+	public $popup_fields = ['subject'];
+	// For Alphabetical search
+	public $def_basicsearch_col = 'subject';
+	// Column value to use on detail view record text display
+	public $def_detailview_recname = 'subject';
+	// Used when enabling/disabling the mandatory fields for the module.
+	// Refers to vtiger_field.fieldname values.
+	public $mandatory_fields = ['subject', 'assigned_user_id'];
+	public $default_order_by = '';
+	public $default_sort_order = 'ASC';
+
+	/**
+	 * Invoked when special actions are performed on the module.
+	 *
+	 * @param string Module name
+	 * @param string Event Type
+	 */
+	public function moduleHandler($moduleName, $eventType)
+	{
+		if ($eventType === 'module.postinstall') {
+		} elseif ($eventType === 'module.disabled') {
+		} elseif ($eventType === 'module.preuninstall') {
+		} elseif ($eventType === 'module.preupdate') {
+		} elseif ($eventType === 'module.postupdate') {
+		}
+	}
+}
