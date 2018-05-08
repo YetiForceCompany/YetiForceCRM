cec21301ed6f688c449cd2d94fc7af66118cb6d2
Tomasz Kur <t.kur@yetiforce.com>
2018-05-08 22:39:15 +0200

    Fixed #6220

--- modules/Vtiger/inventoryfields/NetPrice.php
+++ modules/Vtiger/inventoryfields/NetPrice.php
@@ -37,6 +37,6 @@ class Vtiger_NetPrice_InventoryField extends Vtiger_Basic_InventoryField
 		if (empty($column) || $column === '-' || !$request->has($column . $i)) {
 			return false;
 		}
-		$insertData[$column] = CurrencyField::convertToDBFormat($request->getByType($column . $i, 'NumberInUserFormat'), null, true);
+		$insertData[$column] = $request->getByType($column . $i, 'NumberInUserFormat');
 	}
 }
