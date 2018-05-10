df497ecc37d6cd963458870a272033b8dccb4854
rafalpospiech <r.pospiech@yetiforce.com>
2018-05-10 15:49:51 +0200

    demo is working now

--- public_html/layouts/basic/modules/Project/resources/Gantt.js
+++ public_html/layouts/basic/modules/Project/resources/Gantt.js
@@ -34,43 +34,6 @@ $(document).ready(() => {
 	$('#j-gantt__fullscreen-btn').on('click', function () {
 		$('#workSpace').trigger('fullScreen.gantt');
 	});
-	$.JST.loadDecorator("RESOURCE_ROW", function (resTr, res) {
-		resTr.find(".delRes").click(function () {
-			$(this).closest("tr").remove()
-		});
-	});
-
-	$.JST.loadDecorator("ASSIGNMENT_ROW", function (assigTr, taskAssig) {
-		var resEl = assigTr.find("[name=resourceId]");
-		var opt = $("<option>");
-		resEl.append(opt);
-		for (var i = 0; i < taskAssig.task.master.resources.length; i++) {
-			var res = taskAssig.task.master.resources[i];
-			opt = $("<option>");
-			opt.val(res.id).html(res.name);
-			if (taskAssig.assig.resourceId == res.id)
-				opt.attr("selected", "true");
-			resEl.append(opt);
-		}
-		var roleEl = assigTr.find("[name=roleId]");
-		for (var i = 0; i < taskAssig.task.master.roles.length; i++) {
-			var role = taskAssig.task.master.roles[i];
-			var optr = $("<option>");
-			optr.val(role.id).html(role.name);
-			if (taskAssig.assig.roleId == role.id)
-				optr.attr("selected", "true");
-			roleEl.append(optr);
-		}
-
-		if (taskAssig.task.master.permissions.canWrite && taskAssig.task.canWrite) {
-			assigTr.find(".delAssig").click(function () {
-				var tr = $(this).closest("[assId]").fadeOut(200, function () {
-					$(this).remove()
-				});
-			});
-		}
-
-	});
 
 	function getDemoProject() {
 		//console.debug("getDemoProject")
@@ -272,7 +235,8 @@ $(document).ready(() => {
 				{"id": "tmp_2", "name": "Worker"},
 				{"id": "tmp_3", "name": "Stakeholder"},
 				{"id": "tmp_4", "name": "Customer"}
-			], "canWrite": true, "canDelete": true, "canWriteOnParent": true, canAdd: true
+			],
+			"canWrite": false, "canDelete": false, "canWriteOnParent": false, canAdd: false
 		}
 
 
