36e9afc97f58feae3abbeaee175d94845c3c7c55
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-05-11 14:22:29 +0200

    Removed lib_gantt

--- config/version.php
+++ config/version.php
@@ -4,6 +4,5 @@ return [
 	'appVersion' => '4.3.331',
 	'patchVersion' => '2018.05.11',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.45',
-	'lib_gantt' => '0.0.1',
+	'lib_roundcube' => '0.0.45'
 ];
