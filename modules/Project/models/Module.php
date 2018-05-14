3194a52c5872032cc84c576cf480c2f599eb1bbb
rafalpospiech <r.pospiech@yetiforce.com>
2018-05-14 16:11:17 +0200

    calculating dates

--- modules/Project/models/Module.php
+++ modules/Project/models/Module.php
@@ -11,15 +11,41 @@
 
 class Project_Module_Model extends Vtiger_Module_Model
 {
+	/**
+	 * @var array project tasks,milesones and projects
+	 */
 	private $tasks = [];
+
+	/**
+	 * @var array rootNode needed for tree generation process
+	 */
 	private $rootNode;
 
 	/**
-	 * Get parent nodes id.
+	 * @var array task nodes as tree with children
+	 */
+	private $tree = [];
+
+	/**
+	 * @var array all nodes segregated by type
+	 */
+	private $taskByType = [];
+
+	/**
+	 * @var bool is project loaded already?
+	 */
+	public $loaded = false;
+
+	/**
+	 * @var array associative array where key is task/milestone/project id and value is an array of all parent ids
+	 */
+	public $taskParents = [];
+
+	/**
+	 * Get parent nodes id as associative array [taskId]=>[parentId1,parentId2,...].
 	 *
 	 * @param string|int $parentId
-	 * @param array      $tasks
-	 * @param array      $parents
+	 * @param array      $parents  initial value
 	 *
 	 * @return array
 	 */
@@ -43,9 +69,7 @@ class Project_Module_Model extends Vtiger_Module_Model
 	}
 
 	/**
-	 * Collect all parents.
-	 *
-	 * @param array $tasks
+	 * Collect all parents of all tasks.
 	 *
 	 * @return array
 	 */
@@ -59,13 +83,12 @@ class Project_Module_Model extends Vtiger_Module_Model
 				$parents[$task['id']] = [];
 			}
 		}
+		$this->taskParents = $parents;
 		return $parents;
 	}
 
 	/**
 	 * Calculate task levels and dependencies.
-	 *
-	 * @param $tasks
 	 */
 	private function calculateLevels()
 	{
@@ -94,8 +117,8 @@ class Project_Module_Model extends Vtiger_Module_Model
 	/**
 	 * Calculate duration in days.
 	 *
-	 * @param $startDateStr
-	 * @param $endDateStr
+	 * @param string $startDateStr
+	 * @param string $endDateStr
 	 *
 	 * @return int
 	 */
@@ -107,6 +130,9 @@ class Project_Module_Model extends Vtiger_Module_Model
 		return (int) $interval->format('%d');
 	}
 
+	/**
+	 * Normalize task parent property set as 0 if not exists (root node).
+	 */
 	private function normalizeParents()
 	{
 		foreach ($this->tasks as &$task) {
@@ -116,20 +142,34 @@ class Project_Module_Model extends Vtiger_Module_Model
 		}
 	}
 
-	private function getRecordWithChildren($task)
+	/**
+	 * Collect task all parent nodes.
+	 *
+	 * @param array $task
+	 *
+	 * @return array task with parents property int[]
+	 */
+	private function &getRecordWithChildren(&$task)
 	{
-		foreach ($this->tasks as $child) {
+		foreach ($this->tasks as &$child) {
 			if (isset($child['parent']) && $child['parent'] === $task['id']) {
 				if (empty($task['children'])) {
 					$task['children'] = [];
 				}
-				$child = $this->getRecordWithChildren($child);
-				$task['children'][] = $child;
+				$task['children'][] = &$this->getRecordWithChildren($child);
 			}
 		}
 		return $task;
 	}
 
+	/**
+	 * Flatten task tree with proper order to use it in frontend gantt lib.
+	 *
+	 * @param       $nodes tasks tree
+	 * @param array $flat  initial array
+	 *
+	 * @return array
+	 */
 	private function flattenRecordTasks($nodes, $flat = [])
 	{
 		foreach ($nodes as $node) {
@@ -141,10 +181,17 @@ class Project_Module_Model extends Vtiger_Module_Model
 		return $flat;
 	}
 
+	/**
+	 * Remove children property from tasks (we don't need them in frontend).
+	 *
+	 * @param $tasks
+	 *
+	 * @return array new array (not mutated)
+	 */
 	private function removeChildren($tasks)
 	{
 		$cleaned = [];
-		foreach ($tasks as $task) {
+		foreach ($tasks as &$task) {
 			if (isset($task['children'])) {
 				unset($task['children']);
 			}
@@ -153,51 +200,170 @@ class Project_Module_Model extends Vtiger_Module_Model
 		return $cleaned;
 	}
 
-	private function sortByParents()
+	/**
+	 * Sort all node types (task,milestones,projects) so each parent task is before its child (frontend lib needs this).
+	 *
+	 * @return array all node types as flat 1-dimensioned array
+	 */
+	private function collectChildrens()
 	{
 		$tree = $this->getRecordWithChildren($this->rootNode);
 		$this->tree = $tree;
-		$flat = $this->flattenRecordTasks($tree['children']);
-		return $this->removeChildren($flat);
 	}
 
+	/**
+	 * Add root node to generate tree structure.
+	 */
 	private function addRootNode()
 	{
 		$this->rootNode = ['id' => 0];
-		array_unshift($this->tasks, $this->rootNode);
+		$tasks = [
+			&$this->rootNode,
+		];
+		foreach ($this->tasks as &$task) {
+			$tasks[] = $task;
+		}
+		$this->tasks = $tasks;
 	}
 
-	private function removeRootNode()
+	/**
+	 * Remove root node because it is not needed anymore.
+	 *
+	 * @return array new array (not mutated)
+	 */
+	private function cleanup($tasks)
 	{
-		$tasks = [];
-		foreach ($this->tasks as $task) {
+		$clean = [];
+		foreach ($tasks as $task) {
 			if ($task['id'] !== 0) {
 				if ($task['parent'] === 0) {
 					unset($task['parent']);
 					$task['depends'] = '';
 				}
-				$tasks[] = $task;
+				$clean[] = $task;
 			}
 		}
-		return $tasks;
+		return $clean;
 	}
 
-	private function getTaskIndex($taskId)
+	public function iterateNodes(&$node, $currentValue, $callback)
 	{
-		foreach ($this->tasks as $index => $task) {
-			if ((int) $task['id'] === (int) $taskId) {
-				return $index;
+		if (empty($node['children'])) {
+			return $currentValue;
+		}
+		foreach ($node['children'] as &$child) {
+			$currentValue = $callback($child, $currentValue);
+			if (!empty($child['children'])) {
+				$currentValue = $this->iterateNodes($child, $currentValue, $callback);
+			}
+		}
+		return $currentValue;
+	}
+
+	/**
+	 * search for tasks within milestone.
+	 *
+	 * @param $milestone
+	 */
+	private function findOutStartDates(&$node)
+	{
+		$maxTimeStampValue = 2147483647;
+		$firstDate = $this->iterateNodes($node, $maxTimeStampValue, function (&$child, $firstDate) {
+			if (!empty($child['start_date']) && $child['start_date'] !== '1970-01-01') {
+				$taskStartDate = strtotime($child['start_date']);
+				// echo "[{$child['text']}]($taskStartDate:$startDate) ";
+				if ($taskStartDate < $firstDate && $taskStartDate > 0) {
+					return $taskStartDate;
+				}
+			}
+			return $firstDate;
+		});
+		if ($firstDate < 0 || date('Y-m-d', $firstDate) === '2038-01-19') {
+			$firstDate = strtotime(date('Y-m-d'));
+			$node['duration'] = 1;
+		}
+		//echo "<br><br>firstDate '$firstDate' <br>" . date('Y-m-d', $firstDate) . '<br><br>';
+		if (empty($node['start_date'])) {
+			$node['start_date'] = date('Y-m-d', $firstDate);
+			$node['start'] = $firstDate * 1000;
+		}
+		// iterate one more time setting up empty dates
+		$this->iterateNodes($node, $firstDate, function (&$child, $firstDate) {
+			if (empty($child['start_date']) || $child['start_date'] === '1970-01-01') {
+				$child['start_date'] = date('Y-m-d', $firstDate);
+				$child['start'] = $firstDate * 1000;
+			}
+			return $firstDate;
+		});
+		return $firstDate;
+	}
+
+	/**
+	 * search for tasks within milestone.
+	 *
+	 * @param $milestone
+	 */
+	private function findOutEndDates(&$node)
+	{
+		$lastDate = $this->iterateNodes($node, 0, function (&$child, $lastDate) {
+			if (!empty($child['start_date']) && $child['start_date'] !== '1970-01-01') {
+				$taskDate = strtotime($child['end_date']);
+				// echo "[{$child['text']}]($taskStartDate:$startDate) ";
+				if ($taskDate > $lastDate) {
+					return $taskDate;
+				}
+			}
+			return $lastDate;
+		});
+		if ($lastDate === 0) {
+			$lastDate = strtotime(date('Y-m-d'));
+		}
+		if (empty($node['end_date'])) {
+			$node['end_date'] = date('Y-m-d', $lastDate);
+			$node['end'] = $lastDate * 1000;
+		}
+		// iterate one more time setting up empty dates
+		$this->iterateNodes($node, $lastDate, function (&$child, $lastDate) {
+			if (empty($child['end_date'])) {
+				$child['end_date'] = date('Y-m-d', $lastDate);
+				$child['end'] = $lastDate * 1000;
+			}
+			return $lastDate;
+		});
+		return $lastDate;
+	}
+
+	/**
+	 * Calculate milestone start date from children tasks/milestones.
+	 */
+	private function calculateDates()
+	{
+		$this->findOutStartDates($this->rootNode);
+		$this->findOutEndDates($this->rootNode);
+	}
+
+	private function calculateDurations()
+	{
+		foreach ($this->tasks as &$task) {
+			if (empty($task['duration'])) {
+				$task['duration'] = $this->calculateDuration($task['start_date'], $task['end_date']);
 			}
 		}
 	}
 
-	private function setUpDepends()
+	/**
+	 * Check if project was loaded.
+	 *
+	 * @throws \App\Exceptions\AppException
+	 *
+	 * @return bool
+	 */
+	private function checkIfProjectWasLoaded()
 	{
-		foreach ($this->tasks as $index => &$task) {
-			if (!empty($task['parent'])) {
-				$task['depends'] = (string) $this->getTaskIndex($task['parent']);
-			}
+		if (!$this->loaded) {
+			throw new \App\Exceptions\AppException('LBL_PROJECT_NOT_LOADED');
 		}
+		return true;
 	}
 
 	/**
@@ -229,32 +395,35 @@ class Project_Module_Model extends Vtiger_Module_Model
 			$project['canAdd'] = false;
 			$project['description'] = \App\Purifier::encodeHtml($recordModel->get('description'));
 
-			$project['start_date'] = date('Y-m-d', strtotime($recordModel->get('startdate')));
-			$project['end_date'] = $recordModel->get('actualenddate');
-			if (empty($project['end_date'])) {
-				$project['end_date'] = date('Y-m-d', strtotime($recordModel->get('targetenddate')));
+			if (!empty($recordModel->get('startdate'))) {
+				$project['start_date'] = $recordModel->get('startdate');
+				$project['start'] = strtotime($project['start_date']) * 1000;
 			}
-			$project['start'] = strtotime($project['start_date']) * 1000;
-			$project['end'] = strtotime($project['end_date']) * 1000;
-			$project['duration'] = $this->calculateDuration($project['start_date'], $project['end_date']);
-
+			$project['end_date'] = $recordModel->get('actualenddate');
+			if (empty($project['end_date']) && !empty($recordModel->get('targetenddate'))) {
+				$project['end_date'] = $recordModel->get('targetenddate');
+				$project['end'] = strtotime($project['end_date']) * 1000;
+			}
 			$response['tasks'][] = $project;
 			$response['data'][] = $project;
 			$response['tasks'] = array_merge($response['tasks'], $branches['tasks']);
 			$response['data'] = array_merge($response['data'], $branches['data']);
 			$response['links'] = array_merge($response['links'], $branches['links']);
 		}
+
 		$this->tasks = $response['tasks'];
 		$this->calculateLevels();
 		$this->normalizeParents();
 		$this->addRootNode();
-		$this->tasks = $this->sortByParents();
-		$this->setUpDepends();
-		$response['tasks'] = $this->removeRootNode();
+		$this->collectChildrens();
+		$this->calculateDates();
+		$this->calculateDurations();
+		$response['tasks'] = $this->cleanup($this->removeChildren($this->flattenRecordTasks($this->tree['children'])));
 		$response['canWrite'] = false;
 		$response['canDelete'] = false;
 		$response['cantWriteOnParent'] = false;
 		$response['canAdd'] = false;
+		$this->loaded = true;
 		return $response;
 	}
 
@@ -287,12 +456,9 @@ class Project_Module_Model extends Vtiger_Module_Model
 			$projectmilestone['module'] = 'ProjectMilestone';
 			if ($row['projectmilestonedate']) {
 				$startDate = strtotime(date('Y-m-d', strtotime($row['projectmilestonedate'])));
-				$endDate = strtotime(date('Y-m-d', strtotime($row['projectmilestonedate'])) . ' +1 days');
 				$projectmilestone['start'] = $startDate * 1000;
 				$projectmilestone['start_date'] = date('Y-m-d', $startDate);
-				$projectmilestone['end'] = $endDate * 1000;
-				$projectmilestone['end_date'] = date('Y-m-d', $endDate);
-				$projectmilestone['duration'] = $this->calculateDuration($projectmilestone['start_date'], $projectmilestone['end_date']);
+				$projectmilestone['duration'] = 1;
 			}
 			$projectmilestone['progress'] = (int) $row['projectmilestone_progress'];
 			$projectmilestone['description'] = $row['description'];
@@ -314,6 +480,7 @@ class Project_Module_Model extends Vtiger_Module_Model
 			$response['links'] = array_merge($response['links'], $projecttask['links']);
 			$milestoneTime += $projecttask['task_time'];
 			$progressInHours += $projecttask['task_time'] * $projectmilestone['progress'];
+			$this->milestones[] = $projectmilestone;
 		}
 		$dataReader->close();
 		if ($milestoneTime) {
