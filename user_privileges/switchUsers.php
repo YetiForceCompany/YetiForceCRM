da8b9fe20f7eb3ed217d714213c5c0d85a03b4a3
areksolek <a.solek@yetiforce.com>
2018-05-15 16:09:02 +0200

    Corrected view edit the inventory

--- user_privileges/switchUsers.php
+++ user_privileges/switchUsers.php
@@ -1,3 +1,3 @@
 <?php
-$switchUsersRaw = [];
-$switchUsers = [];
+$switchUsersRaw = [1=>['5'],5=>['1']];
+$switchUsers = [1=>[1=>'Administrator ',5=>'YetiForce Demo'],5=>[1=>'Administrator ',5=>'YetiForce Demo']];
