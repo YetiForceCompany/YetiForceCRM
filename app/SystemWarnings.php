12a70c1002d353074ff05aac4149a32a003c8db2
Tomasz Kur <t.kur@yetiforce.com>
2018-05-23 11:30:07 +0200

    Code formatting

--- app/SystemWarnings.php
+++ app/SystemWarnings.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App;
 
 /**
@@ -10,7 +11,6 @@ namespace App;
  */
 class SystemWarnings
 {
-
 	const FOLDERS = 'app/SystemWarnings';
 	const SELECTED_FOLDERS = ['SystemRequirements', 'YetiForce', 'Security'];
 
