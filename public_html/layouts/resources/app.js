4c8fa5b94c038092161fb4874284e07ef92dbf33
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-05-23 16:54:43 +0200

    Added function to animate modals

--- public_html/layouts/resources/app.js
+++ public_html/layouts/resources/app.js
@@ -202,6 +202,22 @@ app = {
 		}
 		return keyValueMap;
 	},
+	/**
+	 * Function animates bootstrap modal with animate.css
+	 * @params: jQuery object with class .modal,
+	 * @params: string with animation name,
+	 * @params: string with animation name,
+	 */
+	animateModal(modal, openAnimation, closeAnimation) {
+		modal.on('show.bs.modal', function (e) {
+			modal.removeClass(`animated ${closeAnimation}`);
+			modal.addClass(`animated ${openAnimation}`);
+		});
+		modal.on('hide.bs.modal', function (e) {
+			modal.removeClass(`animated ${openAnimation}`);
+			modal.addClass(`animated ${closeAnimation}`);
+		});
+	},
 	showModalData(data, container, paramsObject, cb, url, sendByAjaxCb) {
 		const thisInstance = this;
 		let params = {
@@ -1419,6 +1435,7 @@ $(document).ready(function () {
 		window.pageController = pageController;
 		window.pageController.registerEvents();
 	}
+	app.animateModal($('.modal'), 'fadeIn', 'fadeOut');
 });
 (function ($) {
 	$.fn.getNumberFromValue = function () {
