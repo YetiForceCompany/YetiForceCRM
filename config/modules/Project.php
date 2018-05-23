b29f2e991692aab101da8c6886449ef3a0002def
rafalpospiech <r.pospiech@yetiforce.com>
2018-05-23 09:46:16 +0200

    default flat theme colors for gantt

---
+++ config/modules/Project.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+	'defaultStatusColors'=>[
+		'Project'=>[
+			'prospecting'=>'#7B1FA2',
+			'initiated'=>'#303F9F',
+			'in progress'=>'#1976D2',
+			'waiting for feedback'=>'#F57C00',
+			'on hold'=>'#455A64',
+			'completed'=>'#388E3C',
+			'delivered'=>'#5D4037',
+			'archived'=>'#616161',
+		],
+		'ProjectMilestone'=>[
+			'PLL_OPEN'=>'#3F51B5',
+			'PLL_IN_PROGRESS'=>'#2196F3',
+			'PLL_COMPLETED'=>'#4CAF50',
+			'PLL_DEFERRED'=>'#607D8B',
+			'PLL_CANCELLED'=>'#9E9E9E',
+		],
+		'ProjectTask'=>[
+			'Open'=>'#7986CB',
+			'In Progress'=>'#64B5F6',
+			'Completed'=>'#81C784',
+			'Deferred'=>'#90A4AE',
+			'Cancelled'=>'#E0E0E0'
+		],
+	]
+];
