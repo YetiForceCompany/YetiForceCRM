29105f20d19aa5ede4648ac316f7428e810c982e
rafalpospiech <r.pospiech@yetiforce.com>
2018-05-29 10:37:00 +0200

    Revert "license updates"
    This reverts commit f7b695c

--- modules/Project/models/ListView.php
+++ modules/Project/models/ListView.php
@@ -1,11 +1,12 @@
 <?php
-/**
- * Project ListView Model.
- *
- * @copyright YetiForce Sp. z o.o.
- * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
- * @author    Rafal Pospiech <r.pospiech@yetiforce.com>
- */
+/* +***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * *********************************************************************************** */
 
 /**
  * ListView Model Class for Project module.
