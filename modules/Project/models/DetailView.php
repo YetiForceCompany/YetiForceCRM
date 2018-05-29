e81fce252501ef641afbba1f9a2776cdf554fea3
rafalpospiech <r.pospiech@yetiforce.com>
2018-05-29 10:18:56 +0200

    license updates

--- modules/Project/models/DetailView.php
+++ modules/Project/models/DetailView.php
@@ -1,14 +1,11 @@
 <?php
-/* +***********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
- * Contributor(s): YetiForce.com
- * *********************************************************************************** */
-
+/**
+ * Project DetailView Model.
+ *
+ * @copyright YetiForce Sp. z o.o.
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Rafal Pospiech <r.pospiech@yetiforce.com>
+ */
 class Project_DetailView_Model extends Vtiger_DetailView_Model
 {
 	public function getDetailViewLinks($linkParams)
