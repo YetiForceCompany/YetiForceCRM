534b279dd017ca3b55fc6feb9a0b01b550db245a
rafalpospiech <r.pospiech@yetiforce.com>
2018-05-29 10:37:00 +0200

    Revert "license updates"
    This reverts commit f7b695c

--- modules/Project/Project.php
+++ modules/Project/Project.php
@@ -1,11 +1,13 @@
 <?php
-/**
- * Project.
- *
- * @copyright YetiForce Sp. z o.o
- * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
- * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
- */
+/* +**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * ********************************************************************************** */
+
 class Project extends CRMEntity
 {
 	public $table_name = 'vtiger_project';
