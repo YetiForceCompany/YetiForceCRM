0b83dcbab0ba4d6e653ff4d4585f9331b5d55335
Tomasz Kur <t.kur@yetiforce.com>
2018-06-03 21:43:28 +0200

    Fixed - Files does not attach at"file from CRM" using RoundCube Module (#6737)

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.364',
+	'appVersion' => '4.3.365',
 	'patchVersion' => '2018.05.30',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.45'
+	'lib_roundcube' => '0.0.46'
 ];
