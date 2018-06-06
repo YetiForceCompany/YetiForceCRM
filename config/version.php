d6017100eceba894376cf0ebe5b756669d1b3ba6
Tomasz Kur <t.kur@yetiforce.com>
2018-06-06 10:05:16 +0200

    Email Binding - missing the Add and Relate symbols on buttons (#6808)

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.366',
+	'appVersion' => '4.3.367',
 	'patchVersion' => '2018.06.03',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.46'
+	'lib_roundcube' => '0.0.47'
 ];
