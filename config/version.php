438c22eaea950acb4765b1ce8be5b13322c8132d
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-06-06 11:31:21 +0200

    Changed library select2-theme-bootstrap4

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.367',
-	'patchVersion' => '2018.06.03',
+	'appVersion' => '4.3.368',
+	'patchVersion' => '2018.06.06',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.47'
 ];
