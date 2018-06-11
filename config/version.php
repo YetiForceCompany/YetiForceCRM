cc39bcf95645442b206be6b40198f835955503bf
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-06-11 13:58:07 +0200

    Added minor improvements in validate request

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.372',
-	'patchVersion' => '2018.06.08',
+	'appVersion' => '4.3.373',
+	'patchVersion' => '2018.06.11',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.47'
 ];
