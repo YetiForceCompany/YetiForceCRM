41fbbe5ac3122e67c3f0e30d66603ffb1b3ac446
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-06-12 10:26:59 +0200

    Ipdated translations

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.374',
-	'patchVersion' => '2018.06.11',
+	'appVersion' => '4.3.375',
+	'patchVersion' => '2018.06.12',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.47'
 ];
