9a1d808a22ee511148ded60fb526dc09b14ea90b
areksolek <a.solek@yetiforce.com>
2018-06-12 14:16:42 +0200

    Added comments to the functions

--- modules/Vtiger/widgets/DetailView.php
+++ modules/Vtiger/widgets/DetailView.php
@@ -8,24 +8,41 @@
  */
 class Vtiger_DetailView_Widget extends Vtiger_Basic_Widget
 {
+	/**
+	 * Return url.
+	 *
+	 * @return string
+	 */
 	public function getUrl()
 	{
 		return 'module=' . $this->Module . '&view=Detail&record=' . $this->Record . '&mode=showModuleDetailView';
 	}
 
+	/**
+	 * Function return config template name.
+	 *
+	 * @return string
+	 */
 	public function getConfigTplName()
 	{
 		return 'DetailViewConfig';
 	}
 
+	/**
+	 * Function return widget.
+	 *
+	 * @return array
+	 */
 	public function getWidget()
 	{
+		$widget = [];
 		$moduleName = $this->Module;
 		$model = Vtiger_Module_Model::getInstance($moduleName);
 		if ($model->isPermitted($moduleName)) {
 			$this->Config['url'] = $this->getUrl();
 			$this->Config['tpl'] = 'DetailView.tpl';
+			$widget = $this->Config;
 		}
-		return $this->Config;
+		return $widget;
 	}
 }
