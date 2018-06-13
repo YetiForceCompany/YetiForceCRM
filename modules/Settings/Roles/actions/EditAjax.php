d0f8ff2f4516b18a268415e26802337bd89ac351
Tomasz Kur <t.kur@yetiforce.com>
2018-06-13 13:08:09 +0200

    Improved checking duplicate roles

--- modules/Settings/Roles/actions/EditAjax.php
+++ modules/Settings/Roles/actions/EditAjax.php
@@ -18,13 +18,16 @@ class Settings_Roles_EditAjax_Action extends Settings_Vtiger_Basic_Action
 		$this->exposeMethod('checkDuplicate');
 	}
 
+	/**
+	 * Checks duplicate of roles.
+	 *
+	 * @param \App\Request $request
+	 */
 	public function checkDuplicate(\App\Request $request)
 	{
 		$roleName = $request->getByType('rolename', 'Text');
-		$recordId = $request->getByType('record', 2);
-
-		$recordModel = Settings_Roles_Record_Model::getInstanceByName($roleName, [$recordId]);
-
+		$records = $request->isEmpty('record') ? null : [$request->getByType('record', 2)];
+		$recordModel = Settings_Roles_Record_Model::getInstanceByName($roleName, $records);
 		$response = new Vtiger_Response();
 		if (!empty($recordModel)) {
 			$response->setResult(['success' => true, 'message' => \App\Language::translate('LBL_DUPLICATES_EXIST', $request->getModule(false))]);
