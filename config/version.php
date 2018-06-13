ffb6e3482bd3feae73b32b666e69f025990ec33c
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-06-13 09:57:55 +0200

    Update composer and yarn

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.377',
-	'patchVersion' => '2018.06.12',
+	'appVersion' => '4.3.378',
+	'patchVersion' => '2018.06.13',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.47'
 ];
