50beed79b92c3ed72b001541f94a8871a93a7e94
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-06-16 19:21:49 +0200

    Added minor improvements

--- app/SystemWarnings/Security/Dependencies.php
+++ app/SystemWarnings/Security/Dependencies.php
@@ -6,8 +6,8 @@ namespace App\SystemWarnings\Security;
  * Check for vulnerabilities in dependencies warnings class.
  *
  * @copyright YetiForce Sp. z o.o
- * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
- * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
 class Dependencies extends \App\SystemWarnings\Template
 {
@@ -28,9 +28,9 @@ class Dependencies extends \App\SystemWarnings\Template
 		}
 		if ($this->status === 0) {
 			$this->link = 'index.php?module=Vtiger&parent=Settings&view=Index&mode=security';
-			$this->linkTitle = \App\Language::translate('BTN_CONFIGURE_ENCRYPTION', 'Settings:SystemWarnings');
+			$this->linkTitle = \App\Language::translate('Security', 'Settings:SystemWarnings');
 			$this->description = \App\Language::translate('LBL_VULNERABILITIES_IN_DEPENDENCIES_DESC', 'Settings:SystemWarnings') . '<br />';
-			foreach ($vulnerabilities as $name =>$vulnerability) {
+			foreach ($vulnerabilities as $name => $vulnerability) {
 				$this->description .= "$name({$vulnerability['version']}):<br />";
 				foreach ($vulnerability['advisories'] as $data) {
 					$this->description .= "{$data['title']} {$data['cve']}<br />";
