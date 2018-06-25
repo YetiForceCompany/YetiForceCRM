4711da9a19e2fafb0013db18d6d76a626de607ed
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-06-25 23:19:52 +0200

    Added minor improvements in cleaning redundant files

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.389',
-	'patchVersion' => '2018.06.21',
+	'appVersion' => '4.3.390',
+	'patchVersion' => '2018.06.25',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.47'
 ];
