bd6f351aa4489e9aab2cdb578f3c714de8242737
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-07-05 15:51:30 +0200

    Corrected license

--- modules/HolidaysEntitlement/models/Module.php
+++ modules/HolidaysEntitlement/models/Module.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * LogiIn test class
- * @package   YetiForce.Test
+ * HolidaysEntitlement module model class.
+ *
  * @copyright YetiForce Sp. z o.o.
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Tomasz Poradzewski <t.poradzewski@yetiforce.com>
  */
-
 class HolidaysEntitlement_Module_Model extends Vtiger_Module_Model
 {
 	/**
