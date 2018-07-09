2dabcfc56c55ae1373b663cda971c9dcbd23af50
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-07-09 12:50:37 +0200

    Added minor improvements

---
+++ tests/setup/environment.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Travis CI test script.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+chdir(__DIR__ . '/../../');
+set_include_path(getcwd());
+define('ROOT_DIRECTORY', getcwd());
+
+if (!class_exists('Vtiger_WebUI')) {
+	require_once 'include/main/WebUI.php';
+}
+echo 'locale: ' . setlocale(LC_ALL, 0) . PHP_EOL . PHP_EOL;
+echo '$_SERVER:' . print_r($_SERVER) . PHP_EOL;
+$error = $ok = $info = $files = [];
+foreach (Settings_ConfReport_Module_Model::getStabilityConf() as $key => $value) {
+	if (empty($value['incorrect'])) {
+		$ok[$key] = $value;
+	} else {
+		$error[$key] = $value;
+	}
+}
+foreach (Settings_ConfReport_Module_Model::getSecurityConf() as $key => $value) {
+	if (empty($value['status'])) {
+		$ok[$key] = $value;
+	} else {
+		$error[$key] = $value;
+	}
+}
+foreach (Settings_ConfReport_Module_Model::getSystemInfo() as $key => $value) {
+	$info[$key] = print_r($value, true);
+}
+foreach (Settings_ConfReport_Module_Model::getPermissionsFiles() as $key => $value) {
+	$files[$key] = $value;
+}
+foreach (Settings_ConfReport_Module_Model::getDbConf() as $key => $value) {
+	if (empty($value['status'])) {
+		$ok[$key] = $value;
+	} else {
+		$error[$key] = $value;
+	}
+}
+foreach (\Settings_ConfReport_Module_Model::getLibrary() as $key => $value) {
+	if ($value['status'] === 'LBL_NO') {
+		$libs[$key] = $value['name'];
+	}
+}
+echo 'ConfReport:' . print_r(['errors' => $error, 'ok' => $ok, 'files' => $files, 'libs' => $libs]) . PHP_EOL . PHP_EOL;
+echo 'ini_get_all:' . print_r(ini_get_all()) . PHP_EOL . PHP_EOL;
