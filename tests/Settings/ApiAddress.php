9b125edd5ad7c2948b772b14cf52b2a1f7bd58fa
Sławomir Kłos <s.klos@yetiforce.com>
2018-07-12 10:24:04 +0200

    Added ApiAddress settings module tests

---
+++ tests/Settings/ApiAddress.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * ApiAddress test class.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Sławomir Kłos <s.klos@yetiforce.com>
+ */
+
+namespace Tests\Settings;
+
+class ApiAddress extends \Tests\Base
+{
+	/**
+	 * Testing global config save.
+	 */
+	public function testSaveConfig()
+	{
+		$data = ['min_length' => 5, 'result_num' => 15, 'api_name' => 'global'];
+
+		$result = \Settings_ApiAddress_Module_Model::getInstance('Settings:ApiAddress')->setConfig($data);
+		$this->assertTrue($result);
+	}
+
+	/**
+	 * Testing global config getter.
+	 */
+	public function testGetConfig()
+	{
+		$dataReference = ['min_length' => 5, 'result_num' => 15];
+
+		$result = \Settings_ApiAddress_Module_Model::getInstance('Settings:ApiAddress')->getConfig('global');
+		$this->assertSame((int)$result['global']['min_length'], $dataReference['min_length']);
+		$this->assertSame((int)$result['global']['result_num'], $dataReference['result_num']);
+	}
+
+	/**
+	 * Reset to default values
+	 */
+	public function testResetToDefault()
+	{
+		$data = ['min_length' => 3, 'result_num' => 10, 'api_name' => 'global'];
+
+		$result = \Settings_ApiAddress_Module_Model::getInstance('Settings:ApiAddress')->setConfig($data);
+		$this->assertTrue($result);
+	}
+
+}
