e0ae8ee5b13af568d090839b5283a270b03ce47c
Sławomir Kłos <s.klos@yetiforce.com>
2018-07-16 17:07:27 +0200

    Added more tests and fixes to CustomView settings module

--- tests/Settings/CustomView.php
+++ tests/Settings/CustomView.php
@@ -16,18 +16,34 @@ class CustomView extends \Tests\Base
 	 */
 	public function testModuleModel()
 	{
+		$moduleId= \App\Module::getModuleId('Leads');
 		$moduleModel = \Settings_CustomView_Module_Model::getInstance('Settings:CustomView');
-		$recordModels = $moduleModel->getCustomViews(\App\Module::getModuleId('Leads'));
-		$recordModel = \array_pop($recordModels);
-		$this->assertNotEmpty($recordModel, 'Leads module should contain view');
+		$recordModels = $moduleModel->getCustomViews($moduleId);
 		$this->assertInternalType('array', $recordModels, 'Custom views list should be array type');
+		$this->assertNotEmpty($recordModels, 'Leads module should contain views');
+		$recordModel = \array_pop($recordModels);
+		$this->assertNotEmpty($recordModel, 'Leads custom view record should contain data');
 		if ($recordModel) {
 			$this->assertInternalType('array', $moduleModel->getFilterPermissionsView($recordModel['cvid'], 'default'), 'Custom view permissions list(default) should be array type');
+			$this->assertEmpty($moduleModel->getFilterPermissionsView($recordModel['cvid'], 'default'), 'Custom view permissions list(default) should be empty');
+			$moduleModel->setDefaultUsersFilterView($moduleId, $recordModel['cvid'], \App\User::getActiveAdminId(), 'add');
+			$this->assertNotEmpty($moduleModel->getFilterPermissionsView($recordModel['cvid'], 'default'), 'Custom view permissions list(default) should be not empty');
+			$moduleModel->setDefaultUsersFilterView($moduleId, $recordModel['cvid'], \App\User::getActiveAdminId(), 'remove');
+			$this->assertEmpty($moduleModel->getFilterPermissionsView($recordModel['cvid'], 'default'), 'Custom view permissions list(default) should be emptied');
+
 			$this->assertInternalType('array', $moduleModel->getFilterPermissionsView($recordModel['cvid'], 'featured'), 'Custom view permissions list(featured) should be array type');
+			$this->assertEmpty($moduleModel->getFilterPermissionsView($recordModel['cvid'], 'featured'), 'Custom view permissions list(featured) should be empty');
+			$moduleModel->setFeaturedFilterView($recordModel['cvid'], \App\User::getActiveAdminId(), 'add');
+			$this->assertNotEmpty($moduleModel->getFilterPermissionsView($recordModel['cvid'], 'featured'), 'Custom view permissions list(featured) should be not empty');
+			$moduleModel->setFeaturedFilterView($recordModel['cvid'], \App\User::getActiveAdminId(), 'remove');
+			$this->assertEmpty($moduleModel->getFilterPermissionsView($recordModel['cvid'], 'featured'), 'Custom view permissions list(featured) should be empty');
 		}
 
 		$supportedModules = \Settings_CustomView_Module_Model::getSupportedModules();
 		$this->assertNotEmpty($supportedModules, 'System should have any custom view supported modules');
 		$this->assertSame($supportedModules[\App\Module::getModuleId('Leads')], 'Leads', 'Module mapping mismatch');
+
+		$this->assertSame('module=CustomView&view=EditAjax&source_module=Leads&record=115', $moduleModel->getUrlToEdit('Leads', 115), 'Generated edit url mismatch');
+		$this->assertSame('index.php?module=CustomView&view=EditAjax&source_module=Leads', $moduleModel->getCreateFilterUrl('Leads'), 'Generated create filter url mismatch');
 	}
 }
