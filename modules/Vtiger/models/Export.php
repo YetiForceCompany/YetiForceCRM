28faf76c102b1d759c30a8339907f782ae265dd6
rafalpospiech <r.pospiech@yetiforce.com>
2018-07-17 14:00:37 +0200

    code formatting

--- modules/Vtiger/models/Export.php
+++ modules/Vtiger/models/Export.php
@@ -4,8 +4,8 @@
  * Export Model Class.
  *
  * @copyright YetiForce Sp. z o.o
- * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
- * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
  */
 class Vtiger_Export_Model extends \App\Base
 {
