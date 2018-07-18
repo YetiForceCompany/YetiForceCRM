df0d63d2e557530fd827ed720d44e9fcb805003e
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-07-18 11:11:04 +0200

    Removed unused code

--- tests/Init/3_Install.php
+++ tests/Init/3_Install.php
@@ -16,7 +16,6 @@ class Install extends \Tests\Base
 	{
 		require_once 'install/models/InitSchema.php';
 
-		$_SESSION['config_file_info']['currency_name'] = 'Poland, Zlotych';
 		$_SESSION['config_file_info']['currency_code'] = 'PLN';
 		$_SESSION['config_file_info']['currency_symbol'] = 'zł';
 
