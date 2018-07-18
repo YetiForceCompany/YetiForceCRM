04feb99f57837989b76171f8cd290db930c06507
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-07-18 10:00:58 +0200

    Fixed #7258

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.419',
-	'patchVersion' => '2018.07.17',
+	'appVersion' => '4.3.420',
+	'patchVersion' => '2018.07.18',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.48'
 ];
