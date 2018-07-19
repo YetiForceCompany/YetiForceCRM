f8d58cf9bf1e0d86826574d6c36c303fb5e6f43d
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-07-19 11:07:51 +0200

    Fixed dav integration

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.426',
-	'patchVersion' => '2018.07.18',
+	'appVersion' => '4.3.428',
+	'patchVersion' => '2018.07.19',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.48'
 ];
