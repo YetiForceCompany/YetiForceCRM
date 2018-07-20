ff809f87d33ebc556aff6ec62ae5982754fe1d80
Sławomir Kłos <s.klos@yetiforce.com>
2018-07-20 12:08:04 +0200

    Removed require_once for deleted file

--- modules/Vtiger/helpers/Util.php
+++ modules/Vtiger/helpers/Util.php
@@ -210,7 +210,6 @@ class Vtiger_Util_Helper
 	 */
 	public static function convertTimeIntoUsersDisplayFormat($time, $userObject = null)
 	{
-		require_once 'include/runtime/LanguageHandler.php';
 		if ($userObject) {
 			$userModel = Users_Privileges_Model::getInstanceFromUserObject($userObject);
 		} else {
