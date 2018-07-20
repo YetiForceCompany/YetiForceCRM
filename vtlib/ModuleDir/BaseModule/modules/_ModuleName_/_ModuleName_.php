16412952292afaaab156b3efed29c4e596f9efba
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-07-20 10:34:34 +0200

    Added improvements in the names of tables

--- vtlib/ModuleDir/BaseModule/modules/_ModuleName_/_ModuleName_.php
+++ vtlib/ModuleDir/BaseModule/modules/_ModuleName_/_ModuleName_.php
@@ -13,26 +13,26 @@ include_once 'modules/Vtiger/CRMEntity.php';
 
 class _ModuleName_ extends Vtiger_CRMEntity
 {
-	public $table_name = 'vtiger_<modulename>';
+	public $table_name = '<_baseTableName_>';
 	public $table_index = '<modulename>id';
 
 	/**
 	 * Mandatory table for supporting custom fields.
 	 */
-	public $customFieldTable = ['vtiger_<modulename>cf', '<modulename>id'];
+	public $customFieldTable = ['<_baseTableName_>cf', '<modulename>id'];
 
 	/**
 	 * Mandatory for Saving, Include tables related to this module.
 	 */
-	public $tab_name = ['vtiger_crmentity', 'vtiger_<modulename>', 'vtiger_<modulename>cf'];
+	public $tab_name = ['vtiger_crmentity', '<_baseTableName_>', '<_baseTableName_>cf'];
 
 	/**
 	 * Mandatory for Saving, Include tablename and tablekey columnname here.
 	 */
 	public $tab_name_index = [
 		'vtiger_crmentity' => 'crmid',
-		'vtiger_<modulename>' => '<modulename>id',
-		'vtiger_<modulename>cf' => '<modulename>id',
+		'<_baseTableName_>' => '<modulename>id',
+		'<_baseTableName_>cf' => '<modulename>id',
 	];
 
 	/**
@@ -40,7 +40,6 @@ class _ModuleName_ extends Vtiger_CRMEntity
 	 */
 	public $list_fields = [
 		// Format: Field Label => Array(tablename, columnname)
-		// tablename should not have prefix 'vtiger_'
 		'<entityfieldlabel>' => ['<modulename>', '<entitycolumn>'],
 		'Assigned To' => ['crmentity', 'smownerid'],
 	];
@@ -54,7 +53,6 @@ class _ModuleName_ extends Vtiger_CRMEntity
 	// For Popup listview and UI type support
 	public $search_fields = [
 		// Format: Field Label => Array(tablename, columnname)
-		// tablename should not have prefix 'vtiger_'
 		'<entityfieldlabel>' => ['<modulename>', '<entitycolumn>'],
 		'Assigned To' => ['vtiger_crmentity', 'assigned_user_id'],
 	];
