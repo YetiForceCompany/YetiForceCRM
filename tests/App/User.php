c9a00b8a73c62562e950e67564098b2bd873a602
Sławomir Kłos <s.klos@yetiforce.com>
2018-07-30 17:16:53 +0200

    Improved \App\User tests

--- tests/App/User.php
+++ tests/App/User.php
@@ -50,5 +50,28 @@ class User extends \Tests\Base
 	public function testGetRoleInstance()
 	{
 		$this->assertInstanceOf('\Settings_Roles_Record_Model', \App\User::getCurrentUserModel()->getRoleInstance(), 'Expected current user role object');
+		$this->assertInstanceOf('\Settings_Roles_Record_Model', \App\User::getCurrentUserModel()->getRoleInstance(), 'Expected current user role object(cached)');
+	}
+
+	/**
+	 * Testing if current user is admin.
+	 */
+	public function testIsAdmin()
+	{
+		$this->assertSame('1', \App\User::getCurrentUserModel()->isAdmin(), 'Expected that current user is admin');
+	}
+
+	/**
+	 * Testing if current user is active.
+	 */
+	public function testIsActive()
+	{
+		$this->assertTrue(\App\User::getCurrentUserModel()->isActive(), 'Expected that current user is active');
+	}
+
+	public function testIsExists()
+	{
+		$this->assertTrue(\App\User::isExists(\App\User::getCurrentUserId()), 'Expected that current user exists');
+		$this->assertTrue(\App\User::isExists(\App\User::getCurrentUserId()), 'Expected that current user exists(cached)');
 	}
 }
