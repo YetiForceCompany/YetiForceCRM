7e8609e5e0fd869fd4b825fa275066a0b2d6f362
Sławomir Kłos <s.klos@yetiforce.com>
2018-07-31 16:04:02 +0200

    Improved \App\Field tests coverage

--- tests/App/Field.php
+++ tests/App/Field.php
@@ -17,7 +17,7 @@ class Field extends \Tests\Base
 	public function testGetFieldPermission()
 	{
 		$moduleId = \App\Module::getModuleId('Leads');
-		$fieldId = (new \App\Db\Query())->select(['fieldid'])->from('vtiger_field')->where(['tabid'=>\App\Module::getModuleId('Leads'), 'fieldname'=>'email'])->scalar();
+		$fieldId = (new \App\Db\Query())->select(['fieldid'])->from('vtiger_field')->where(['tabid'=>$moduleId, 'fieldname'=>'email'])->scalar();
 		$this->assertTrue(\App\Field::getFieldPermission('Leads', 'email', true), 'Expected read perms(strings)');
 		$this->assertTrue(\App\Field::getFieldPermission('Leads', 'email', false), 'Expected write perms(strings)');
 		$this->assertTrue(\App\Field::getFieldPermission('Leads', 'email', false), 'Expected write perms(strings,cached)');
@@ -27,14 +27,36 @@ class Field extends \Tests\Base
 		$this->assertFalse(\App\Field::getFieldPermission($moduleId, 'NxField', false), 'Expected no write perms(field not exists)');
 	}
 
+	/**
+	 * Testing getColumnPermission function.
+	 */
 	public function testGetColumnPermission()
 	{
 		$moduleId = \App\Module::getModuleId('Leads');
-		$fieldColumn = (new \App\Db\Query())->select(['columnname'])->from('vtiger_field')->where(['tabid' => \App\Module::getModuleId('Leads'), 'fieldname' => 'email'])->scalar();
+		$fieldColumn = (new \App\Db\Query())->select(['columnname'])->from('vtiger_field')->where(['tabid' => $moduleId, 'fieldname' => 'vat_id'])->scalar();
 		$this->assertTrue(\App\Field::getColumnPermission('Leads', $fieldColumn, true), 'Expected read perms(string)');
 		$this->assertTrue(\App\Field::getColumnPermission($moduleId, $fieldColumn, true), 'Expected read perms(ids)');
 		$this->assertTrue(\App\Field::getColumnPermission($moduleId, $fieldColumn, false), 'Expected write perms(ids)');
 		$this->assertFalse(\App\Field::getColumnPermission($moduleId, 'NxColumn', true), 'Expected no read perms(field not exists)');
 		$this->assertFalse(\App\Field::getColumnPermission($moduleId, 'NxColumn', false), 'Expected write perms(field not exists)');
+	}
+
+	public function relationsProvider()
+	{
+		return (new \App\Db\Query())->select(['tabid', 'related_tabid'])->from('vtiger_relatedlists')->all();
+	}
+
+	/**
+	 * @dataProvider relationsProvider
+	 */
+	public function testGetRelatedFieldForModulePair($relatedId, $moduleId)
+	{
+		$result = \App\Field::getRelatedFieldForModule(\App\Module::getModuleName($relatedId), \App\Module::getModuleName($moduleId));
+		$this->assertInternalType('array', $result, 'Relation list should be array type');
+	}
+
+	public function testGetRelatedFieldForModuleAll()
+	{
+		$this->assertNotEmpty(\App\Field::getRelatedFieldForModule(), 'All relations list should be not empty');
 	}
 }
