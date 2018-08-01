8f8449311b273cdc73c39ead71b9ffe0a5bcc531
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-08-01 10:15:37 +0200

    Added 'use strict' in some Settings modules and corrected its bugs

--- public_html/layouts/basic/modules/Settings/LayoutEditor/resources/LayoutEditor.js
+++ public_html/layouts/basic/modules/Settings/LayoutEditor/resources/LayoutEditor.js
@@ -7,6 +7,7 @@
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  *************************************************************************************/
+'use strict';
 
 $.Class('Settings_LayoutEditor_Js', {}, {
 	updatedBlockSequence: {},
