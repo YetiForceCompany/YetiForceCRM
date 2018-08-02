1cfb39d0b36e645dda8905fdaefc37f02e259815
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-08-02 12:41:22 +0200

    Improved unpacking zip files

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.465',
-	'patchVersion' => '2018.08.01',
+	'appVersion' => '4.3.466',
+	'patchVersion' => '2018.08.02',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.49'
 ];
