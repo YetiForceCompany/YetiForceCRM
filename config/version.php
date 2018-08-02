9c531ea0591d755fd49ad9be6f274200b5aa777d
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-08-02 15:39:05 +0200

    YetiForce CRM ver. 4.4.0

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.3.467',
+	'appVersion' => '4.4.0',
 	'patchVersion' => '2018.08.02',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.49'
+	'lib_roundcube' => '0.0.48'
 ];
