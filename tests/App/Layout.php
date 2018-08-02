662dbed53120b0c0822d828fba94b0cd608fe0d2
Sławomir Kłos <s.klos@yetiforce.com>
2018-08-02 13:27:25 +0200

    Improved \App\Layout tests coverage

--- tests/App/Layout.php
+++ tests/App/Layout.php
@@ -39,6 +39,27 @@ class Layout extends \Tests\Base
 		\App\Db::getInstance()->createCommand()->insert('vtiger_layout', ['name'=>'unitTest0', 'label'=>'UnitTest0'])->execute();
 		$result1 = \App\Layout::getAllLayouts();
 		$this->assertCount(2, $result1, 'Expected only default layout');
-		$this->assertSame('UnitTest0', $result0['unitTest0'], 'Expected test layout with reference label');
+		$this->assertSame('UnitTest0', $result1['unitTest0'], 'Expected test layout with reference label');
 	}
+
+	/**
+	 * Testing getPublicUrl.
+	 */
+	public function testGetPublicUrl()
+	{
+		$this->assertSame(\AppConfig::main('site_URL') . 'public_html/unitTest', \App\Layout::getPublicUrl('unitTest', true), 'Url differs from reference');
+	}
+
+	/**
+	 * Testing getImagePath.
+	 */
+	public function testGetImagePath()
+	{
+		$this->assertSame('public_html/layouts/basic/images/Accounts.png', \App\Layout::getImagePath('Accounts.png'), 'Image path differs from provided');
+	}
+
+//	public function testGetTemplatePath()
+//	{
+//		$this->assertSame('public_html/layouts/basic/modules/Accounts/', \App\Layout::getTemplatePath('basic', 'Accounts'), 'Tempate path differs from provided');
+//	}
 }
