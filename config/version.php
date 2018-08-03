5cb70b794e21ecae19552a610faafcd8ffb1c4d4
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-08-03 14:43:48 +0200

    Unnecessary table removed

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.4.0',
-	'patchVersion' => '2018.08.02',
+	'appVersion' => '4.4.1',
+	'patchVersion' => '2018.08.03',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.49'
+	'lib_roundcube' => '0.0.48'
 ];
