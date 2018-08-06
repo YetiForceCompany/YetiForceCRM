673f681717d6ba9a2405f8b4188b3258f051de8f
Arkadiusz Adach <arek@wp4b.pl>
2018-08-06 15:57:26 +0200

    SocialMedia - config model

---
+++ modules/Settings/SocialMedia/models/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Social media config model class.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Arkadiusz Adach <a.adach@yetiforce.com>
+ */
+class Settings_SocialMedia_Config_Model
+{
+	/**
+	 * The name of the tables in the database.
+	 */
+	private const TABLE_NAME = 'u_yf_social_media_config';
+}
