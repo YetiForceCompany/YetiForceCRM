4dcb21d39cfc4d2cb1331d884f565d037ad8a1e9
Michał Lorencik <m.lorencik@yetiforce.com>
2018-08-06 12:25:08 +0200

    Fix undefined index LISTVIEWBASIC

--- modules/Vtiger/models/ListView.php
+++ modules/Vtiger/models/ListView.php
@@ -360,6 +360,7 @@ class Vtiger_ListView_Model extends \App\Base
 		$links = [];
 
 		$basicLinks = $this->getBasicLinks();
+		$links['LISTVIEWBASIC'] = [];
 		foreach ($basicLinks as $basicLink) {
 			$links['LISTVIEWBASIC'][] = Vtiger_Link_Model::getInstanceFromValues($basicLink);
 		}
