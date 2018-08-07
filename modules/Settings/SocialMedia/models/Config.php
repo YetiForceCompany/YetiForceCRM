0e78d7239964ef997e9cb0d4617e979ed2d5a5c1
Arkadiusz Adach <arek@wp4b.pl>
2018-08-07 11:41:22 +0200

    Social media config class

--- modules/Settings/SocialMedia/models/Config.php
+++ modules/Settings/SocialMedia/models/Config.php
@@ -7,10 +7,131 @@
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Arkadiusz Adach <a.adach@yetiforce.com>
  */
-class Settings_SocialMedia_Config_Model
+class Settings_SocialMedia_Config_Model extends \App\Base
 {
 	/**
 	 * The name of the tables in the database.
 	 */
-	private const TABLE_NAME = 'u_yf_social_media_config';
+	private const TABLE_NAME = 'u_#__social_media_config';
+
+	/**
+	 * Configuration type.
+	 *
+	 * @var string
+	 */
+	protected $type;
+	/**
+	 * Array with information which fields have changed.
+	 *
+	 * @var array
+	 */
+	protected $changes = [];
+	/**
+	 * Array with information which fields are new.
+	 *
+	 * @var array
+	 */
+	protected $newRecords = [];
+	/**
+	 * Array with information on which fields to delete.
+	 *
+	 * @var array
+	 */
+	protected $removeRecords = [];
+
+	/**
+	 * Settings_SocialMedia_Config_Model constructor.
+	 *
+	 * @param $type string
+	 */
+	public function __construct($type)
+	{
+		$this->getConfig($type);
+	}
+
+	/**
+	 * @param $type string
+	 *
+	 * @throws \App\Exceptions\AppException
+	 *
+	 * @return array
+	 */
+	public function getConfig($type)
+	{
+		$this->type = $type;
+		$this->value = [];
+		$dataReader = (new \App\Db\Query())
+			->select(['name', 'value'])
+			->from(static::TABLE_NAME)
+			->where(['type' => $type])
+			->createCommand()
+			->query();
+		while ($row = $dataReader->read()) {
+			$this->value[$row['name']] = \App\Json::decode($row['value']);
+		}
+		$dataReader->close();
+		return $this->value;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function set($key, $value)
+	{
+		if (!in_array($key, $this->changes)) {
+			$this->changes[] = $key;
+		}
+		if (!array_key_exists($key, $this->value)) {
+			$this->newRecords[] = $key;
+		}
+		return parent::set($key, $value);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function remove($key)
+	{
+		if (array_key_exists($key, $this->value)) {
+			$this->removeRecords[] = $key;
+		}
+		parent::remove($key);
+	}
+
+	/**
+	 * Save changes to DB.
+	 *
+	 * @throws \yii\db\Exception
+	 */
+	public function save()
+	{
+		$db = \App\Db::getInstance();
+		$transaction = $db->beginTransaction();
+		$transaction->begin();
+		try {
+			foreach ($this->changes as $key) {
+				$val = $this->value[$key];
+				if (in_array($key, $this->newRecords)) {
+					$db->createCommand()->insert(static::TABLE_NAME, [
+						'name' => $key,
+						'value' => \App\Json::encode($val),
+						'type' => $this->type
+					])->execute();
+				} else {
+					$db->createCommand()->update(static::TABLE_NAME,
+						['value' => \App\Json::encode($val)],
+						['type' => $this->type, 'name' => $key]
+					)->execute();
+				}
+			}
+			//Remove records
+			foreach ($this->removeRecords as $key) {
+				$db->createCommand()->delete(static::TABLE_NAME, ['type' => $this->type, 'name' => $key])->execute();
+			}
+			$transaction->commit();
+		} catch (\Exception $e) {
+			$transaction->rollBack();
+			throw $e;
+		}
+	}
 }
