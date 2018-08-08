abcb3d6aa67604c4d6175c6e7a126482848c7ddc
rafalpospiech <r.pospiech@yetiforce.com>
2018-08-08 14:42:19 +0200

    global save button changed to local form save button

--- public_html/layouts/basic/modules/Settings/Vtiger/resources/CustomRecordNumbering.js
+++ public_html/layouts/basic/modules/Settings/Vtiger/resources/CustomRecordNumbering.js
@@ -63,7 +63,7 @@ jQuery.Class('Settings_CustomRecordNumbering_Js', {}, {
 			sequenceNumberElement.validationEngine('showPrompt', app.vtranslate('JS_SEQUENCE_NUMBER_MESSAGE') + " " + oldSequenceNumber, 'error', 'topLeft', true);
 			return;
 		}
-		$('.saveButton').attr("disabled", "disabled");
+		editViewForm.find('.saveButton').attr("disabled", "disabled");
 		AppConnector.request({
 			'module': app.getModuleName(),
 			'parent': app.getParentModuleName(),
@@ -140,46 +140,47 @@ jQuery.Class('Settings_CustomRecordNumbering_Js', {}, {
 		const value = editViewForm.find('[name="reset_sequence"]').val();
 		const prefix = editViewForm.find('[name="prefix"]').val();
 		const postfix = editViewForm.find('[name="postfix"]').val();
+		const saveBtn = editViewForm.find('.saveButton');
 		switch (value) {
 			case 'Y':
 				if (prefix.indexOf('{{YY}}') === -1 && prefix.indexOf('{{YYYY}}') === -1 && postfix.indexOf('{{YY}}') === -1 && postfix.indexOf('{{YYYY}}') === -1) {
-					$('.saveButton').attr('disabled', 'disabled');
+					saveBtn.attr('disabled', 'disabled');
 					Vtiger_Helper_Js.showMessage({
 						type: 'error',
 						text: app.vtranslate('JS_RS_ADD_YEAR_VARIABLE')
 					});
 				} else {
-					$('.saveButton').removeAttr('disabled');
+					saveBtn.removeAttr('disabled');
 					sequenceExists = true;
 				}
 				break;
 			case 'M':
 				if (prefix.indexOf('{{MM}}') === -1 && prefix.indexOf('{{M}}') === -1 && postfix.indexOf('{{MM}}') === -1 && postfix.indexOf('{{M}}') === -1) {
-					$('.saveButton').attr('disabled', 'disabled');
+					saveBtn.attr('disabled', 'disabled');
 					Vtiger_Helper_Js.showMessage({
 						type: 'error',
 						text: app.vtranslate('JS_RS_ADD_MONTH_VARIABLE')
 					});
 				} else {
-					$('.saveButton').removeAttr('disabled');
+					saveBtn.removeAttr('disabled');
 					sequenceExists = true;
 				}
 				break;
 			case 'D':
 				if (prefix.indexOf('{{DD}}') === -1 && prefix.indexOf('{{D}}') === -1 && postfix.indexOf('{{DD}}') === -1 && postfix.indexOf('{{D}}') === -1) {
-					$('.saveButton').attr('disabled', 'disabled');
+					saveBtn.attr('disabled', 'disabled');
 					Vtiger_Helper_Js.showMessage({
 						type: 'error',
 						text: app.vtranslate('JS_RS_ADD_DAY_VARIABLE')
 					});
 				} else {
-					$('.saveButton').removeAttr('disabled');
+					saveBtn.removeAttr('disabled');
 					sequenceExists = true;
 				}
 				break;
 			case 'X':
 			default:
-				$('.saveButton').removeAttr('disabled');
+				saveBtn.removeAttr('disabled');
 				sequenceExists = true;
 				break;
 		}
