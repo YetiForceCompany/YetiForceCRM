98980d462fda77cb034184387d39682e66375620
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-08-09 09:08:21 +0200

    Improved listing the users and groups on the calendar

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.4.8',
-	'patchVersion' => '2018.08.08',
+	'appVersion' => '4.4.9',
+	'patchVersion' => '2018.08.09',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.53'
 ];
