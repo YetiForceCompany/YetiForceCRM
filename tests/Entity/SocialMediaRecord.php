91d9ad60e9e0d0f51906478436f681894a09785f
Arkadiusz Adach <arek@wp4b.pl>
2018-08-09 14:56:50 +0200

    Tests for updating and deleting

--- tests/Entity/SocialMediaRecord.php
+++ tests/Entity/SocialMediaRecord.php
@@ -31,14 +31,14 @@ class SocialMediaRecord extends \Tests\Base
 	}
 
 	/**
-	 * Test user for 2FA.
+	 * Test insert record.
 	 */
 	public function testInsert()
 	{
 		$createAt = date('Y-m-d H:i:s');
 		$recordModel = new \SocialMedia_Record_Model();
 		$recordModel->set('twitter_login', 'spacex');
-		$recordModel->set('id_twitter', '4543645654456654');
+		$recordModel->set('id_twitter', '299792456');
 		$recordModel->set('message', 'gdshgjkfdjfdgfds');
 		$recordModel->set('created_at', $createAt);
 		$recordModel->set('data_json', \App\Json::encode(['test' => 'data']));
@@ -49,10 +49,46 @@ class SocialMediaRecord extends \Tests\Base
 		$row = (new \App\Db\Query())->from('u_#__social_media_twitter')->where(['id' => static::$id])->one();
 		$this->assertNotFalse($row, 'No record id: ' . static::$id);
 		$this->assertSame('spacex', $row['twitter_login']);
+		$this->assertSame('299792456', $row['id_twitter']);
 		$this->assertSame('gdshgjkfdjfdgfds', $row['message']);
 		$this->assertSame(\App\Json::encode(['test' => 'data']), $row['data_json']);
 		$this->assertSame($createAt, $row['created_at']);
 		$this->assertSame($createAt, $row['created_time']);
+	}
+
+	/**
+	 * Test update record.
+	 */
+	public function testUpdate()
+	{
+		$createAt = date('Y-m-d H:i:s');
+		$recordModel = \SocialMedia_Record_Model::getInstanceById(static::$id);
+		$recordModel->set('twitter_login', 'space');
+		$recordModel->set('id_twitter', '1618033988749');
+		$recordModel->set('message', 'hkfdsfds fdsfds');
+		$recordModel->set('created_at', $createAt);
+		$recordModel->set('data_json', \App\Json::encode(['test' => 'data 2']));
+		$recordModel->set('created_time', $createAt);
+		$recordModel->save();
+		$row = (new \App\Db\Query())->from('u_#__social_media_twitter')->where(['id' => static::$id])->one();
+		$this->assertNotFalse($row, 'No record id: ' . static::$id);
+		$this->assertSame('space', $row['twitter_login']);
+		$this->assertSame('hkfdsfds fdsfds', $row['message']);
+		$this->assertSame('1618033988749', $row['id_twitter']);
+		$this->assertSame(\App\Json::encode(['test' => 'data 2']), $row['data_json']);
+		$this->assertSame($createAt, $row['created_at']);
+		$this->assertSame($createAt, $row['created_time']);
+	}
+
+	/**
+	 * Testing record deletion.
+	 */
+	public function testDelete()
+	{
+		$recordModel = \SocialMedia_Record_Model::getInstanceById(static::$id);
+		$recordModel->delete();
+		$row = (new \App\Db\Query())->from('u_#__social_media_twitter')->where(['id' => static::$id])->one();
+		$this->assertFalse($row, 'The record was not removed from the database ID: ' . static::$id);
 	}
 
 	/**
