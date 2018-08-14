ebeae4408bb26ffc5259859b4a576467cc0a8050
Arkadiusz Adach <arek@wp4b.pl>
2018-08-14 16:17:24 +0200

    fix type casting

--- modules/Settings/Leads/models/Mapping.php
+++ modules/Settings/Leads/models/Mapping.php
@@ -131,7 +131,7 @@ class Settings_Leads_Mapping_Model extends Settings_Vtiger_Module_Model
 	public function getFieldsInfo($fieldIdsList)
 	{
 		$leadModel = Vtiger_Module_Model::getInstance($this->getName());
-		$leadId = $leadModel->getId();
+		$leadId = (int)$leadModel->getId();
 		$dataReader = (new App\Db\Query())->select(['fieldid', 'fieldlabel', 'uitype', 'typeofdata', 'fieldname', 'tablename', 'tabid'])
 			->from('vtiger_field')
 			->where(['fieldid' => $fieldIdsList])
@@ -139,7 +139,7 @@ class Settings_Leads_Mapping_Model extends Settings_Vtiger_Module_Model
 		$fieldLabelsList = [];
 		while ($rowData = $dataReader->read()) {
 			$fieldInfo = ['id' => $rowData['fieldid'], 'label' => $rowData['fieldlabel']];
-			if ($rowData['tabid'] === $leadId) {
+			if ((int)$rowData['tabid'] === $leadId) {
 				$fieldModel = Settings_Leads_Field_Model::getCleanInstance();
 				$fieldModel->set('uitype', $rowData['uitype']);
 				$fieldModel->set('typeofdata', $rowData['typeofdata']);
@@ -151,7 +151,6 @@ class Settings_Leads_Mapping_Model extends Settings_Vtiger_Module_Model
 			$fieldLabelsList[$rowData['fieldid']] = $fieldInfo;
 		}
 		$dataReader->close();
-
 		return $fieldLabelsList;
 	}
 
