5e1d82c4370c2977153ced08294b43e21817569a
Arkadiusz Adach <arek@wp4b.pl>
2018-08-16 14:16:14 +0200

    Cleaning code

--- public_html/layouts/basic/modules/Settings/SocialMedia/resources/Index.js
+++ public_html/layouts/basic/modules/Settings/SocialMedia/resources/Index.js
@@ -3,9 +3,61 @@
 
 jQuery.Class('Settings_SocialMedia_Index_Js', {}, {
 	/**
+	 * Container (Form)
+	 */
+	container: null,
+	/**
+	 * Get Container (Form)
+	 * @returns {Object}
+	 */
+	getContainer() {
+		if (this.container === null) {
+			this.container = $('form.js-social-media-twitter__form');
+		}
+		return this.container;
+	},
+	/**
+	 * Submit form
+	 * @param {jQuery} container
+	 */
+	submitForm(container) {
+		container.validationEngine(app.validationEngineOptions);
+		if (container.validationEngine('validate')) {
+			let progressIndicatorElement = jQuery.progressIndicator({
+				position: 'html',
+				blockInfo: {
+					enabled: true
+				}
+			});
+			AppConnector.request(container.serializeFormData()).done((response) => {
+				progressIndicatorElement.progressIndicator({mode: 'hide'});
+				Vtiger_Helper_Js.showPnotify({
+					text: response.result.message,
+					type: 'info',
+				});
+			});
+		}
+	},
+	/**
+	 * Register events for form
+	 */
+	registerForm() {
+		let container = this.getContainer();
+		let thisInstance = this;
+		container.on('change', (event) => {
+			event.preventDefault();
+			thisInstance.submitForm(container);
+		});
+		//Executed when the enter key is pressed.
+		container.on('submit', (event) => {
+			event.preventDefault();
+			thisInstance.submitForm(container);
+		});
+	},
+	/**
 	 * Register all events in view
 	 */
 	registerEvents() {
-		Settings_SocialMedia_Twitter_Js.getInstance().registerEvents();
+		this.registerForm();
 	}
 });
