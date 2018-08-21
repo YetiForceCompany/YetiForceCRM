e194758947cfd9740e2ba6656a946de0bab2ac53
Michał Lorencik <m.lorencik@yetiforce.com>
2018-08-21 10:18:46 +0200

    Try to fix SensioLabs bug by use unused variable before try block

--- app/AddressFinder/OpenCageGeocoder.php
+++ app/AddressFinder/OpenCageGeocoder.php
@@ -35,12 +35,12 @@ class OpenCageGeocoder extends Base
 	 */
 	public function find($value)
 	{
+		$config = \App\AddressFinder::getConfig();
+		$url = static::$url . 'json?q=' . $value . '&pretty=1';
+		$url .= '&language=' . \App\Language::getLanguageTag();
+		$url .= '&limit=' . $config['global']['result_num'];
+		$url .= '&key=' . $config['opencage_data']['key'];
 		try {
-			$config = \App\AddressFinder::getConfig();
-			$url = static::$url . 'json?q=' . $value . '&pretty=1';
-			$url .= '&language=' . \App\Language::getLanguageTag();
-			$url .= '&limit=' . $config['global']['result_num'];
-			$url .= '&key=' . $config['opencage_data']['key'];
 			$response = \Requests::get($url);
 			if (!$response->success) {
 				\App\Log::warning($response->status_code . ' ' . $response->body, __NAMESPACE__);
