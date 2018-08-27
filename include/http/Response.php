f64178f455d2415d34ab0cad3f102649292a6f23
Michał Lorencik <m.lorencik@yetiforce.com>
2018-08-27 14:06:51 +0200

    Add contributions to include/http/Response.php

--- include/http/Response.php
+++ include/http/Response.php
@@ -6,6 +6,7 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
+ * Contributor(s): YetiForce.com.
  * ********************************************************************************** */
 
 class Vtiger_Response
