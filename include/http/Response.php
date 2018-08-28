7d2e3903a27a54777ccce53f5320e5f4d5c89ad5
Michał Lorencik <m.lorencik@yetiforce.com>
2018-08-28 11:57:56 +0200

    Clear some code change in pull

--- include/http/Response.php
+++ include/http/Response.php
@@ -6,7 +6,6 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- * Contributor(s): YetiForce.com.
  * ********************************************************************************** */
 
 class Vtiger_Response
