cb076af04dcb16512856b528f902a78b542b6161
rafalpospiech <r.pospiech@yetiforce.com>
2018-08-29 09:53:36 +0200

    validate reset sequence

--- modules/Settings/Vtiger/actions/CustomRecordNumberingAjax.php
+++ modules/Settings/Vtiger/actions/CustomRecordNumberingAjax.php
@@ -67,7 +67,9 @@ class Settings_Vtiger_CustomRecordNumberingAjax_Action extends Settings_Vtiger_I
 		$moduleModel->set('leading_zeros', $request->getByType('leading_zeros', 'Integer'));
 		$moduleModel->set('sequenceNumber', $request->getByType('sequenceNumber', 'Integer'));
 		$moduleModel->set('postfix', $request->getByType('postfix', 'Text'));
-		$moduleModel->set('reset_sequence', $request->getByType('reset_sequence'));
+		if (in_array($request->getByType('reset_sequence'), ['Y', 'M', 'D'])) {
+			$moduleModel->set('reset_sequence', $request->getByType('reset_sequence'));
+		}
 		$result = $moduleModel->setModuleSequence();
 		$response = new Vtiger_Response();
 		if ($result['success']) {
