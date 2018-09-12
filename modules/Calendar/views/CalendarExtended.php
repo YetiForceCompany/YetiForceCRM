7c3e7796252f0367a6fdfb322e78c8cfd4ab5451
Arkadiusz Adach <arek@wp4b.pl>
2018-09-12 16:13:52 +0200

    Extended calendar class

--- modules/Calendar/views/CalendarExtended.php
+++ modules/Calendar/views/CalendarExtended.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Calendar right panel view model class.
+ * Calendar view Class.
  *
  * @copyright YetiForce Sp. z o.o
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Arkadiusz Adach <a.adach@yetiforce.com>
  */
-class Calendar_RightPanelExtended_View extends Calendar_RightPanel_View
+class Calendar_CalendarExtended_View extends Calendar_Calendar_View
 {
 }
