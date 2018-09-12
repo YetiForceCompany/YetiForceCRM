d677a84a32957e0745729d57abd777c6fc44a08d
areksolek <a.solek@yetiforce.com>
2018-09-12 12:13:47 +0200

    Fixed #7997

--- public_html/layouts/basic/modules/Vtiger/resources/Inventory.js
+++ public_html/layouts/basic/modules/Vtiger/resources/Inventory.js
@@ -110,7 +110,7 @@ $.Class("Vtiger_Inventory_Js", {
 	 * @return jQuery object which you can use to
 	 */
 	getBasicRow: function () {
-		return this.getForm().find('.js-inventory-base-item').eq(0).clone(true, true);
+		return this.getForm().find('.js-inventory-base-item').children().clone(true, true);
 	},
 	isRecordSelected: function (element) {
 		var parentRow = element.closest('tr');
@@ -1199,12 +1199,16 @@ $.Class("Vtiger_Inventory_Js", {
 	addItem(module, baseTableId, rowData = false) {
 		const items = this.getInventoryItemsContainer();
 		let newRow = this.getBasicRow();
+		let newRowBlock = $(newRow[0]);
+		let newRowHiddenBlock = $(newRow[1]);
+		let table = items.find('.js-inventory-items-body');
 		const sequenceNumber = this.getNextLineItemRowNumber();
-		const replaced = newRow.html().replace(/_NUM_/g, sequenceNumber);
-		newRow.html(replaced);
-		newRow = newRow.find('.inventoryRow').appendTo(items.find('.js-inventory-items-body'));
-		newRow.find('.rowName input[name="popupReferenceModule"]').val(module).data('field', baseTableId);
-		newRow.find('.colPicklistField select').each(function (index, select) {
+		const replaced = newRowBlock.html().replace(/_NUM_/g, sequenceNumber);
+		newRowBlock.html(replaced);
+		newRowBlock = newRowBlock.appendTo(table);
+		newRowHiddenBlock.appendTo(table);
+		newRowBlock.find('.rowName input[name="popupReferenceModule"]').val(module).data('field', baseTableId);
+		newRowBlock.find('.colPicklistField select').each(function (index, select) {
 			select = $(select);
 			select.find('option').each(function (index, option) {
 				option = $(option);
@@ -1213,11 +1217,11 @@ $.Class("Vtiger_Inventory_Js", {
 				}
 			});
 		});
-		this.initItem(newRow);
-		Vtiger_Edit_Js.getInstance().registerAutoCompleteFields(newRow);
-		app.showPopoverElementView(newRow.find('.js-popover-tooltip'));
+		this.initItem(newRowBlock);
+		Vtiger_Edit_Js.getInstance().registerAutoCompleteFields(newRowBlock);
+		app.showPopoverElementView(newRowBlock.find('.js-popover-tooltip'));
 		if (rowData) {
-			this.setRowData(newRow, rowData);
+			this.setRowData(newRowBlock, rowData);
 		}
 	},
 	/**
@@ -1271,7 +1275,7 @@ $.Class("Vtiger_Inventory_Js", {
 		thisInstance.form.on('click', '.toggleVisibility', function (e) {
 			var element = $(e.currentTarget);
 			var row = thisInstance.getClosestRow(element);
-			if (element.data('status') === '0') {
+			if (element.data('status') == 0) {
 				thisInstance.showExpandedRow(row);
 			} else {
 				thisInstance.hideExpandedRow(row);
