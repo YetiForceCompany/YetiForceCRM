44c1e983212e61f739b55f4f4b791ffe41a3d882
Michał Lorencik <m.lorencik@yetiforce.com>
2018-09-14 09:51:37 +0200

    Add table for picklist closes state

--- install/install_schema/scheme.sql
+++ install/install_schema/scheme.sql
@@ -2907,6 +2907,14 @@ CREATE TABLE `u_yf_partnerscf` (
   CONSTRAINT `fk_1_u_yf_partnerscf` FOREIGN KEY (`partnersid`) REFERENCES `u_yf_partners` (`partnersid`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+/*Table structure for table `u_yf_picklist_close_state` */
+
+CREATE TABLE `u_yf_picklist_close_state` (
+  `fieldid` int(11) DEFAULT NULL,
+  `valueid` int(11) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 /*Table structure for table `u_yf_recurring_info` */
 
 CREATE TABLE `u_yf_recurring_info` (
