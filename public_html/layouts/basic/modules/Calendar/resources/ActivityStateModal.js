9f7a373982b7b53ece47346bf8d8148cef6c4645
Arek Dudek <a.dudek@yetiforce.com>
2018-09-17 15:41:03 +0200

    Fixed settings status on event

--- public_html/layouts/basic/modules/Calendar/resources/ActivityStateModal.js
+++ public_html/layouts/basic/modules/Calendar/resources/ActivityStateModal.js
@@ -2,32 +2,31 @@
 'use strict';
 
 jQuery.Class("Calendar_ActivityStateModal_Js", {}, {
-	registerActivityState: function () {
-		var thisInstance = this;
-		jQuery('#activityStateModal button:not(.close)').on('click', function (e) {
-			var currentTarget = jQuery(e.currentTarget);
+	registerActivityState() {
+		const self = this;
+		$('.js-activity-state button:not(.close)').on('click', function (e) {
+			let currentTarget = $(e.currentTarget);
 			currentTarget.closest('.modal').addClass('d-none');
 			if (currentTarget.data('type') == '1') {
-				thisInstance.updateActivityState(currentTarget);
+				self.updateActivityState(currentTarget);
 			}
 			if (currentTarget.hasClass('showQuickCreate')) {
-				var progressIndicatorElement = jQuery.progressIndicator({
-					'position': 'html',
-					'blockInfo': {
-						'enabled': true
-					}
-				});
-				var moduleName = 'Calendar';
-				var url = 'index.php?module=Calendar&view=QuickCreateAjax&addRelation=true&sourceModule=Calendar&sourceRecord=' + currentTarget.data('id');
-				var params = {};
+				let progressIndicatorElement = $.progressIndicator({
+						'position': 'html',
+						'blockInfo': {
+							'enabled': true
+						}
+					}),
+					url = 'index.php?module=Calendar&view=QuickCreateAjax&addRelation=true&sourceModule=Calendar&sourceRecord=' + currentTarget.data('id'),
+					params = {},
+					subject = currentTarget.closest('.modalEditStatus').find('.modalSummaryValues .fieldVal').data('subject'),
+					headerInstance = Vtiger_Header_Js.getInstance();
 				params.noCache = true;
-				var subject = currentTarget.closest('.modalEditStatus').find('.modalSummaryValues .fieldVal').data('subject');
-				var headerInstance = Vtiger_Header_Js.getInstance();
-				headerInstance.getQuickCreateForm(url, moduleName, params).done(function (data) {
+				headerInstance.getQuickCreateForm(url, 'Calendar', params).done(function (data) {
 					progressIndicatorElement.progressIndicator({'mode': 'hide'});
 					if (currentTarget.data('type') == '0' && typeof subject !== "undefined" && subject.length > 0) {
-						data = jQuery(data);
-						var element = data.find('[name="subject"]');
+						data = $(data);
+						let element = data.find('[name="subject"]');
 						if (element.length) {
 							element.val(subject);
 						}
@@ -35,7 +34,7 @@ jQuery.Class("Calendar_ActivityStateModal_Js", {}, {
 					headerInstance.handleQuickCreateData(data, {
 						callbackFunction: function (data) {
 							if (data && data.success && data.result.followup.value == currentTarget.data('id')) {
-								thisInstance.updateActivityState(currentTarget);
+								self.updateActivityState(currentTarget);
 							}
 						}
 					});
@@ -60,32 +59,32 @@ jQuery.Class("Calendar_ActivityStateModal_Js", {}, {
 		});
 		AppConnector.request(params).done(function (data) {
 			if (data.success) {
-				var viewName = app.getViewName();
+				let viewName = app.getViewName();
 				if (viewName === 'Detail') {
-					var widget = jQuery('.activityWidgetContainer .widgetContentBlock');
-					var thisInstance = Vtiger_Detail_Js.getInstance();
+					const thisInstance = Vtiger_Detail_Js.getInstance();
+					let widget = $('.activityWidgetContainer .widgetContentBlock');
 					if (widget.length) {
 						thisInstance.loadWidget(widget);
 					} else {
-						var recentActivitiesTab = thisInstance.getTabByLabel(thisInstance.detailViewRecentActivitiesTabLabel);
+						let recentActivitiesTab = thisInstance.getTabByLabel(thisInstance.detailViewRecentActivitiesTabLabel);
 						if (recentActivitiesTab) {
 							recentActivitiesTab.trigger('click');
 						}
-						if (app.getModuleName() == 'Calendar') {
-							recentActivitiesTab = ((!thisInstance.getSelectedTab().length || thisInstance.getSelectedTab().data('linkKey') == thisInstance.detailViewDetailsTabLabel) ? thisInstance.getTabContainer().find('[data-link-key="' + thisInstance.detailViewDetailsTabLabel + '"]:not(.d-none)') : jQuery('<div></div>'));
-							jQuery('.showModal.closeCalendarRekord').addClass('d-none');
+						if (app.getModuleName() === 'Calendar') {
+							recentActivitiesTab = ((!thisInstance.getSelectedTab().length || thisInstance.getSelectedTab().data('linkKey') == thisInstance.detailViewDetailsTabLabel) ? thisInstance.getTabContainer().find('[data-link-key="' + thisInstance.detailViewDetailsTabLabel + '"]:not(.d-none)') : $('<div></div>'));
+							$('.showModal.closeCalendarRekord').addClass('d-none');
 							recentActivitiesTab.trigger('click');
 						}
 					}
 				}
-				if (viewName == 'List') {
-					var listinstance = new Vtiger_List_Js();
+				if (viewName === 'List') {
+					let listinstance = new Vtiger_List_Js();
 					listinstance.getListViewRecords();
 				}
-				if (viewName == 'DashBoard') {
+				if (viewName === 'DashBoard') {
 					(new Vtiger_DashBoard_Js()).getContainer().find('a[name="drefresh"]').trigger('click');
 				}
-				if (app.getModuleName() == 'Calendar' && viewName == 'Calendar') {
+				if (app.getModuleName() === 'Calendar' && (viewName === 'Calendar' || viewName === 'CalendarExtended')) {
 					(Calendar_CalendarView_Js.getInstanceByView()).loadCalendarData();
 				}
 				//updates the Calendar Reminder popup's status
