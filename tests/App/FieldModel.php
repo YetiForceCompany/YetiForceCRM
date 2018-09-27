53bb387d530297f9e99bf691da9c0f48597af55f
Arkadiusz Adach <arek@wp4b.pl>
2018-09-27 16:15:06 +0200

    Default values for uitype

---
+++ tests/App/FieldModel.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * FieldModel test class.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Arkadiusz Adach <a.adach@yetiforce.com>
+ */
+
+namespace Tests\Integrations;
+
+/**
+ * Class FieldModel for test.
+ *
+ * @package   Tests
+ */
+class FieldModel extends \Tests\Base
+{
+	/**
+	 * @var \Vtiger_Field_Model[]
+	 */
+	protected static $fields;
+
+	/**
+	 * @param int    $uiType
+	 * @param string $typeOfData
+	 * @param string $columnType
+	 * @param        $value
+	 *
+	 * @return \Vtiger_Field_Model
+	 * @codeCoverageIgnore
+	 */
+	protected function createFieldModel(int $uiType, string $typeOfData, string $columnType, $value)
+	{
+		$key = \md5($uiType . $typeOfData . $columnType);
+		if (isset(static::$fields[$key])) {
+			return static::$fields[$key];
+		}
+		$fieldInstance = new \vtlib\Field();
+		$fieldInstance->name = 'custom_sender';
+		$fieldInstance->table = 'vtiger_contactsubdetails';
+		$fieldInstance->label = 'FL_LABEL';
+		$fieldInstance->column = 'custom_sender';
+		$fieldInstance->uitype = $uiType;
+		$fieldInstance->typeofdata = $typeOfData;
+		$fieldInstance->columntype = $columnType;
+		return static::$fields[$key] = \Vtiger_Field_Model::getInstanceFromFieldObject($fieldInstance);
+	}
+
+	/**
+	 * @param int    $uiType
+	 * @param string $typeOfData
+	 * @param string $columnType
+	 * @param        $value
+	 * @dataProvider providerGoodData
+	 */
+	public function testValidateGoodData(int $uiType, string $typeOfData, string $columnType, $value)
+	{
+		$fieldModel = static::createFieldModel($uiType, $typeOfData, $columnType, $value);
+		$this->assertNull($fieldModel->getUITypeModel()->validate($value, true));
+	}
+
+	/**
+	 * @param int    $uiType
+	 * @param string $typeOfData
+	 * @param string $columnType
+	 * @param        $value
+	 * @dataProvider providerWrongData
+	 */
+	public function testValidateWrongeData(int $uiType, string $typeOfData, string $columnType, $value)
+	{
+		$fieldModel = static::createFieldModel($uiType, $typeOfData, $columnType, $value);
+		$this->expectExceptionCode(406);
+		$fieldModel->getUITypeModel()->validate($value, true);
+	}
+
+	/**
+	 * @param int    $uiType
+	 * @param string $typeOfData
+	 * @param string $columnType
+	 * @param        $value
+	 *
+	 * @throws \App\Exceptions\Security
+	 *
+	 * @dataProvider providerGoodDataForDefault
+	 */
+	public function testDefaultValueGoodData(int $uiType, string $typeOfData, string $columnType, $value)
+	{
+		$fieldModel = static::createFieldModel($uiType, $typeOfData, $columnType, $value);
+		$fieldModel->name = 'val';
+		$request = new \App\Request([]);
+		$request->set('val', $value);
+		$this->assertNull($fieldModel->getUITypeModel()->setDefaultValueFromRequest($request));
+		$this->assertSame($fieldModel->getDBValue($value), $fieldModel->get('defaultvalue'));
+	}
+
+	/**
+	 * @param int    $uiType
+	 * @param string $typeOfData
+	 * @param string $columnType
+	 * @param        $value
+	 *
+	 * @throws \App\Exceptions\Security
+	 *
+	 * @dataProvider providerWrongDataForDefault
+	 */
+	public function testDefaultValueWrongData(int $uiType, string $typeOfData, string $columnType, $value)
+	{
+		$fieldModel = static::createFieldModel($uiType, $typeOfData, $columnType, $value);
+		$fieldModel->name = 'val';
+		$request = new \App\Request([]);
+		$request->set('val', $value);
+		$this->expectExceptionCode(406);
+		$fieldModel->getUITypeModel()->setDefaultValueFromRequest($request);
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return []
+	 * @codeCoverageIgnore
+	 */
+	public function providerGoodData()
+	{
+		return [
+			[5, 'D~O', 'varchar(255)', '2018-07-02'], //date
+		];
+	}
+
+	/**
+	 * Data provider for testValidate.
+	 *
+	 * @return []
+	 * @codeCoverageIgnore
+	 */
+	public function providerWrongData()
+	{
+		return [
+			[5, 'D~O', 'varchar(255)', '201872a'], //date
+			[5, 'D~O', 'varchar(255)', '2018-72-12'], //date
+			[5, 'D~O', 'varchar(255)', '-2018-02-12'], //date
+		];
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return []
+	 * @codeCoverageIgnore
+	 */
+	public function providerGoodDataForDefault()
+	{
+		return [
+			[5, 'D~O', 'varchar(255)', '2018-07-02'], //date
+			[5, 'D~O', 'varchar(255)', '$(date : now)$'], //date
+			[5, 'D~O', 'varchar(255)', '$(date : tomorrow)$'], //date
+			[5, 'D~O', 'varchar(255)', '$(date : yesterday)$'], //date
+			[5, 'D~O', 'varchar(255)', '$(date : monday this week)$'], //date
+			[5, 'D~O', 'varchar(255)', '$(date : monday next week)$'], //date
+			[5, 'D~O', 'varchar(255)', '$(date : first day of this month)$'], //date
+			[5, 'D~O', 'varchar(255)', '$(date : last day of this month)$'], //date
+			[5, 'D~O', 'varchar(255)', '$(date : first day of next month)$'], //date
+		];
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return []
+	 * @codeCoverageIgnore
+	 */
+	public function providerWrongDataForDefault()
+	{
+		return [
+			[5, 'D~O', 'varchar(255)', '201807o2'], //date
+			[5, 'D~O', 'varchar(255)', 'abc'], //date
+			[5, 'D~O', 'varchar(255)', 20180612], //date
+			[5, 'D~O', 'varchar(255)', '(date : now)'], //date
+			[5, 'D~O', 'varchar(255)', '$date : now$'], //date
+			[5, 'D~O', 'varchar(255)', '$(date)$'], //date
+		];
+	}
+}
