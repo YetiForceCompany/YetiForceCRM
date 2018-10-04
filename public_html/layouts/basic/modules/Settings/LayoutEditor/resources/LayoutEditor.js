6224552373afbc657c63d91af4aa0f279f1bdf0e
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-10-04 10:26:52 +0200

    Fixed #7925

--- public_html/layouts/basic/modules/Settings/LayoutEditor/resources/LayoutEditor.js
+++ public_html/layouts/basic/modules/Settings/LayoutEditor/resources/LayoutEditor.js
@@ -1502,7 +1502,7 @@ $.Class('Settings_LayoutEditor_Js', {}, {
 			contents = $('#layoutEditorContainer').find('.contents');
 		}
 		App.Fields.Date.register(contents);
-		app.registerEventForClockPicker(contents);
+		app.registerEventForClockPicker();
 		App.Fields.Picklist.changeSelectElementView(contents);
 
 		thisInstance.makeFieldsListSortable();
