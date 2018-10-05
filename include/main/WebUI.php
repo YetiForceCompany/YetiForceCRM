fc0b0e290f83975adabdca55e2bb1b10790b7269
Arkadiusz Adach <arek@wp4b.pl>
2018-10-05 07:23:51 +0200

    Fix "Notice: Undefined index"

--- include/main/WebUI.php
+++ include/main/WebUI.php
@@ -50,9 +50,9 @@ class Vtiger_WebUI extends Vtiger_EntryPoint
 	{
 		if (!$this->hasLogin()) {
 			$returnUrl = $request->getServer('QUERY_STRING');
-			if ($returnUrl && !$_SESSION['return_params']) {
+			if ($returnUrl && !\App\Session::has('return_params')) {
 				//Take the url that user would like to redirect after they have successfully logged in.
-				App\Session::set('return_params', str_replace('&amp;', '&', $returnUrl));
+				\App\Session::set('return_params', str_replace('&amp;', '&', $returnUrl));
 			}
 			if (!$request->isAjax()) {
 				header('Location: index.php');
@@ -184,7 +184,7 @@ class Vtiger_WebUI extends Vtiger_EntryPoint
 			$this->triggerPostProcess($handler, $request);
 		} catch (Exception $e) {
 			\App\Log::error($e->getMessage() . PHP_EOL . $e->__toString());
-			$messageHeader ='LBL_ERROR';
+			$messageHeader = 'LBL_ERROR';
 			if ($e instanceof \App\Exceptions\NoPermittedToRecord || $e instanceof WebServiceException) {
 				$messageHeader = 'LBL_PERMISSION_DENIED';
 			} elseif ($e instanceof \App\Exceptions\Security) {
