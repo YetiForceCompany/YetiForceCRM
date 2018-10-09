34a2a3798e2d2837aab2776f0d50470461c712eb
areksolek <a.solek@yetiforce.com>
2018-10-09 14:05:20 +0200

    Scrutinizer - Unused Code

--- modules/Vtiger/views/Detail.php
+++ modules/Vtiger/views/Detail.php
@@ -88,7 +88,7 @@ class Vtiger_Detail_View extends Vtiger_Index_View
 
 		$detailViewLinkParams = ['MODULE' => $moduleName, 'RECORD' => $recordId, 'VIEW' => $request->getByType('view', 2)];
 		$detailViewLinks = $this->record->getDetailViewLinks($detailViewLinkParams);
-		$this->record->getWidgets($detailViewLinkParams);
+		$this->record->getWidgets();
 
 		$viewer = $this->getViewer($request);
 		$viewer->assign('RECORD', $recordModel);
