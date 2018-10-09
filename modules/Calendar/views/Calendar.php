b32af9393e5afd2688249dc0e78716f0dc32a194
Tomasz Poradzewski <tomek.poradzewski@gmail.com>
2018-10-09 17:25:04 +0200

    Added week count config option in calendar

--- modules/Calendar/views/Calendar.php
+++ modules/Calendar/views/Calendar.php
@@ -98,6 +98,7 @@ class Calendar_Calendar_View extends Vtiger_Index_View
 		$viewer = $this->getViewer($request);
 		$currentUserModel = Users_Record_Model::getCurrentUserModel();
 		$viewer->assign('CURRENT_USER', $currentUserModel);
+		$viewer->assign('WEEK_COUNT', AppConfig::module('Calendar', 'WEEK_COUNT'));
 		$viewer->assign('EVENT_LIMIT', AppConfig::module('Calendar', 'EVENT_LIMIT'));
 		$viewer->assign('WEEK_VIEW', AppConfig::module('Calendar', 'SHOW_TIMELINE_WEEK') ? 'agendaWeek' : 'basicWeek');
 		$viewer->assign('DAY_VIEW', AppConfig::module('Calendar', 'SHOW_TIMELINE_DAY') ? 'agendaDay' : 'basicDay');
