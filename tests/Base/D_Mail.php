d9d36a76709e6e275aff538a298ecea2a621ad41
Sławomir Kłos <s.klos@yetiforce.com>
2018-10-17 15:36:07 +0200

    Improve Base D_Mail test

--- tests/Base/D_Mail.php
+++ tests/Base/D_Mail.php
@@ -13,30 +13,38 @@ namespace Tests\Base;
 class D_Mail extends \Tests\Base
 {
 	/**
+	 * Check if mailbox access data is set.
+	 */
+	protected function setUp()
+	{
+		if (empty($_SERVER['YETI_MAIL_PASS'])) {
+			$this->markTestSkipped('No mailbox password provided, please set YETI_MAIL_PASS in $_SERVER array.');
+		}
+	}
+
+	/**
 	 * Load configuration.
 	 */
 	public function testLoadConfig()
 	{
-		if (!empty($_SERVER['YETI_MAIL_PASS'])) {
-			$db = \App\Db::getInstance();
-			$db->createCommand()
-				->insert('roundcube_users', [
-					'username' => 'yetiforcetestmail@gmail.com',
-					'mail_host' => 'imap.gmail.com',
-					'language' => 'en_US',
-					'preferences' => 'a:3:{s:9:"junk_mbox";s:12:"[Gmail]/Spam";s:10:"trash_mbox";s:12:"[Gmail]/Kosz";s:11:"client_hash";s:32:"0e1f51526f56ef769dbd1f58a674f106";}',
-					'password' => $_SERVER['YETI_MAIL_PASS'],
-					'crm_user_id' => '1',
-					'actions' => 'CreatedEmail,CreatedHelpDesk,BindAccounts,BindContacts,BindLeads,BindHelpDesk,BindSSalesProcesses,BindCampaigns,BindCompetition,BindOSSEmployees,BindPartners,BindProject,BindServiceContracts,BindVendors',
-				])->execute();
-			$db->createCommand()
-				->insert('vtiger_ossmailscanner_folders_uid', [
-					'user_id' => '1',
-					'type' => 'Received',
-					'folder' => 'INBOX',
-					'uid' => '0',
-				])->execute();
-		}
+		$db = \App\Db::getInstance();
+		$db->createCommand()
+			->insert('roundcube_users', [
+				'username' => 'yetiforcetestmail@gmail.com',
+				'mail_host' => 'imap.gmail.com',
+				'language' => 'en_US',
+				'preferences' => 'a:3:{s:9:"junk_mbox";s:12:"[Gmail]/Spam";s:10:"trash_mbox";s:12:"[Gmail]/Kosz";s:11:"client_hash";s:32:"0e1f51526f56ef769dbd1f58a674f106";}',
+				'password' => $_SERVER['YETI_MAIL_PASS'],
+				'crm_user_id' => '1',
+				'actions' => 'CreatedEmail,CreatedHelpDesk,BindAccounts,BindContacts,BindLeads,BindHelpDesk,BindSSalesProcesses,BindCampaigns,BindCompetition,BindOSSEmployees,BindPartners,BindProject,BindServiceContracts,BindVendors',
+			])->execute();
+		$db->createCommand()
+			->insert('vtiger_ossmailscanner_folders_uid', [
+				'user_id' => '1',
+				'type' => 'Received',
+				'folder' => 'INBOX',
+				'uid' => '0',
+			])->execute();
 		$this->assertTrue(true);
 	}
 }
