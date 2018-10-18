e86c6a3f90c4297e090cddd814c7ce89a7b99b15
areksolek <a.solek@yetiforce.com>
2018-10-18 08:43:22 +0200

    Added picklist field

--- modules/Vtiger/uitypes/Picklist.php
+++ modules/Vtiger/uitypes/Picklist.php
@@ -76,4 +76,16 @@ class Vtiger_Picklist_UIType extends Vtiger_Base_UIType
 	{
 		return ['e', 'n', 'y', 'ny', 'd'];
 	}
+
+	/**
+	 * Returns template for operator.
+	 *
+	 * @param string $operator
+	 *
+	 * @return string
+	 */
+	public function getOperatorTemplateName(string $operator = '')
+	{
+		return 'ConditionBuilder/Picklist.tpl';
+	}
 }
