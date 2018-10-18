3a6655667182223abe84412ea7c8670069ffb95a
Sławomir Kłos <s.klos@yetiforce.com>
2018-10-18 13:57:51 +0200

    Moved check permissions to user module

---
+++ modules/Users/files/MultiImage.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Multi image class to handle files.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Sławomir Kłos <s.klos@yetiforce.com>
+ */
+
+/**
+ * Image class to handle files.
+ */
+class Users_MultiImage_File extends Vtiger_MultiImage_File
+{
+
+	/**
+	 * Checking permission in get method.
+	 *
+	 * @param \App\Request $request
+	 *
+	 * @throws \App\Exceptions\NoPermitted
+	 *
+	 * @return bool
+	 */
+	public function getCheckPermission(\App\Request $request)
+	{
+		if (!$request->isEmpty('record')) {
+			$moduleName = $request->getModule();
+			$fieldName = $request->getByType('field', 2);
+			if ($fieldName === 'imagename') {
+				return true;
+			} elseif (!\App\Privilege::isPermitted($moduleName, 'DetailView', $request->getInteger('record')) || !\App\Field::getFieldPermission($moduleName, $fieldName)) {
+				throw new \App\Exceptions\NoPermitted('LBL_PERMISSION_DENIED', 406);
+			}
+		} else {
+			throw new \App\Exceptions\NoPermitted('LBL_PERMISSION_DENIED', 406);
+		}
+		return true;
+	}
+}
