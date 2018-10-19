e905e48b517f87fe74695a578449e5eb472dd8e6
Sławomir Kłos <s.klos@yetiforce.com>
2018-10-19 11:05:03 +0200

    CR fixes

--- modules/Vtiger/models/InventoryField.php
+++ modules/Vtiger/models/InventoryField.php
@@ -527,8 +527,8 @@ class Vtiger_InventoryField_Model extends App\Base
 	 */
 	public function getUniqueID($instance)
 	{
-		return (int)(new \App\Db\Query())->from($this->getTableName('fields'))->where(['invtype' => $instance->getName()])
-				->max('id') + 1;
+		return (int) (new \App\Db\Query())->from($this->getTableName('fields'))->where(['invtype' => $instance->getName()])
+			->max('id') + 1;
 	}
 
 	/**
