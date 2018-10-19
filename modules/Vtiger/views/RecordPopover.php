fad9b90e86a1ba108e723a18f83547c6255ae230
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2018-10-19 10:38:18 +0200

    Added popover ajax mechanism

---
+++ modules/Vtiger/views/RecordPopover.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * RecordPopover view Class.
+ *
+ * @package   View
+ *
+ * @copyright YetiForce Sp. z o.o.
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
+
+/**
+ * Class Vtiger_RecordPopover_View
+ */
+class Vtiger_RecordPopover_View extends \App\Controller\View
+{
+	/**
+	 * Show empty field
+	 * @var bool
+	 */
+	protected $showEmptyField = false;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function checkPermission(\App\Request $request)
+	{
+		if ($request->isEmpty('record', true) || !\App\Privilege::isPermitted($request->getModule(), 'DetailView', $request->getInteger('record'))) {
+			throw new \App\Exceptions\NoPermittedToRecord('ERR_NO_PERMISSIONS_FOR_THE_RECORD', 406);
+		}
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function process(\App\Request $request)
+	{
+		$viewer = $this->getViewer($request);
+		$moduleName = $request->getModule();
+		$recordModel = Vtiger_Record_Model::getInstanceById($request->getInteger('record'));
+		$summaryFields = [];
+		foreach ($recordModel->getModule()->getFields() as $fieldName => &$fieldModel) {
+			if ($fieldModel->isSummaryField() && $fieldModel->isViewableInDetailView() &&
+				($this->showEmptyField || (!$this->showEmptyField && !$recordModel->isEmpty($fieldName)))
+			) {
+				$summaryFields[$fieldName] = $fieldModel;
+			}
+		}
+		$viewer->assign('RECORD', $recordModel);
+		$viewer->assign('FIELDS', $summaryFields);
+		$viewer->view('RecordPopover.tpl', $moduleName);
+	}
+}
