180cfc476983c200b15af680d76b2b53ccca33ee
TomaszQr <t.kur@yetiforce.com>
2018-10-24 12:54:29 +0200

    Improved description

--- app/QueryField/MultiownerField.php
+++ app/QueryField/MultiownerField.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * MultiEmail Query Field Class.
+ * MultiOwner Query Field Class.
  *
  * @copyright YetiForce Sp. z o.o
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
- * @author    Arkadiusz Adach <a.adach@yetiforce.com>
+ * @author    Tomasz Kur <t.kur@yetiforce.com>
  */
 
 namespace App\QueryField;
