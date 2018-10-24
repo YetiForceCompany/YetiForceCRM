14f817611c8124563f3c2e137777692eb51011f0
TomaszQr <t.kur@yetiforce.com>
2018-10-24 13:49:18 +0200

    Improved field recurrence

--- modules/Vtiger/uitypes/Recurrence.php
+++ modules/Vtiger/uitypes/Recurrence.php
@@ -168,6 +168,6 @@ class Vtiger_Recurrence_UIType extends Vtiger_Base_UIType
 	 */
 	public function getOperators()
 	{
-		return ['e', 'n', 's', 'ew', 'c', 'k', 'y', 'ny', 'd'];
+		return ['y', 'ny', 'd'];
 	}
 }
