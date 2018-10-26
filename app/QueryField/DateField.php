318f9ae6ee6f9519ed654ec562084867252d16e6
Arkadiusz Adach <arek@wp4b.pl>
2018-10-26 09:41:07 +0200

    Package

--- app/QueryField/DateField.php
+++ app/QueryField/DateField.php
@@ -2,14 +2,14 @@
 
 /**
  * Date Query Field Class.
+ *
+ * @package   App
  */
 
 namespace App\QueryField;
 
 /**
  * Date Query Field Class.
- *
- * @package   App
  *
  * @copyright YetiForce Sp. z o.o
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
