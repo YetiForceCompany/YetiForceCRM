b59b67a54d871730aa9c3bdca184902c4782e903
Sławomir Kłos <s.klos@yetiforce.com>
2018-10-31 17:32:17 +0100

    Test

--- include/database/PearDatabase.php
+++ include/database/PearDatabase.php
@@ -97,7 +97,7 @@ class PearDatabase
 
 		// Set options
 		$options = [
-			PDO::ATTR_EMULATE_PREPARES => false,
+			PDO::ATTR_EMULATE_PREPARES => true,
 			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 			PDO::ATTR_TIMEOUT => 5,
 		];
