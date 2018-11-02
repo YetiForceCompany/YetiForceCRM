16054d7bb3042e9526a4ff0065e2d0b4d24cdd82
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-11-02 09:41:08 +0100

    Improved credits

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.4.55',
-	'patchVersion' => '2018.10.31',
+	'appVersion' => '4.4.56',
+	'patchVersion' => '2018.11.02',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.55'
 ];
