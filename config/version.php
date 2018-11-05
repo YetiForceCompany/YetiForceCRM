15197dd3a92ac78936ca24ad207f51da4a769e30
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-11-05 09:32:55 +0100

    Added minor improvements in SessionCleaner

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.4.57',
-	'patchVersion' => '2018.11.02',
+	'appVersion' => '4.4.58',
+	'patchVersion' => '2018.11.05',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.55'
 ];
