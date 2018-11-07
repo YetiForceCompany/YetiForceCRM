b297bc9bcc01152ee00a8bd137b78455ae649c07
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2018-11-07 11:15:59 +0100

    Update RightPanel.php

--- modules/Reservations/views/RightPanel.php
+++ modules/Reservations/views/RightPanel.php
@@ -58,7 +58,6 @@ class Reservations_RightPanel_View extends Vtiger_IndexAjax_View
 	public function getTypesList(\App\Request $request)
 	{
 		$viewer = $this->getViewer($request);
-		$moduleName = $request->getModule();
 		$viewer->assign('ALL_ACTIVETYPES_LIST', Reservations_Calendar_Model::getCalendarTypes());
 		$viewer->view('RightPanel.tpl', $request->getModule());
 	}
