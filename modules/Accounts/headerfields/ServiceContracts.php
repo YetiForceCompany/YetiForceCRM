f2786b1c0899e5fb0fcbee1b36ac6052613adcd6
Arek Dudek <a.dudek@yetiforce.com>
2018-11-13 13:50:36 +0100

    Remove file

--- modules/Accounts/headerfields/ServiceContracts.php
+++ modules/Accounts/headerfields/ServiceContracts.php
@@ -14,7 +14,7 @@ class Accounts_ServiceContracts_HeaderField
 		$row = (new \App\Db\Query())->select('MAX(due_date) AS date,count(*) AS total')->from('vtiger_servicecontracts')
 			->innerJoin('vtiger_crmentity', 'vtiger_servicecontracts.servicecontractsid = vtiger_crmentity.crmid')
 			->where(['deleted' => 0, 'sc_related_to' => $viewModel->getRecord()->getId(), 'contract_status' => 'In Progress'])
-			->create();
+			->one();
 		if (!empty($row['date']) || !empty($row['total'])) {
 			return [
 				'class' => 'btn-success',
