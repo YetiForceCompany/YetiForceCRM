1c6e88aa9d433b12033bccec1c9b805b5e147c78
TomaszQr <t.kur@yetiforce.com>
2018-11-21 16:01:51 +0100

    Added minor improvements

--- modules/OSSMail/models/Record.php
+++ modules/OSSMail/models/Record.php
@@ -26,7 +26,7 @@ class OSSMail_Record_Model extends Vtiger_Record_Model
 		}
 		if ($onlyMy) {
 			$userModel = \App\User::getCurrentUserModel();
-			$crmUsers  = $userModel->get('groups');
+			$crmUsers  = $userModel->getGroups();
 			$crmUsers []= $userModel->getId();
 			$query->andWhere(['crm_user_id' => $crmUsers]);
 		}
