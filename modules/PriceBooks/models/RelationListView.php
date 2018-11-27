de9ecf04ab02eb72ab0b038196100e48a1dd14ae
Sławomir Kłos <s.klos@yetiforce.com>
2018-11-27 16:24:24 +0100

    CodeReview cleanup

--- modules/PriceBooks/models/RelationListView.php
+++ modules/PriceBooks/models/RelationListView.php
@@ -26,7 +26,7 @@ class PriceBooks_RelationListView_Model extends Vtiger_RelationListView_Model
 		$field->set('isEditable', true);
 		$field->set('fromOutsideList', true);
 		$field->set('maximumlength', '99999999999999999999');
-		$field->set('class', 'validate[required,funcCall[Vtiger_Currency_Validator_Js.invokeValidation]] js-format-numer');
+		$field->set('class', 'validate[required,funcCall[Vtiger_Currency_Validator_Js.invokeValidation]]');
 		$headerFields['listprice'] = $field;
 		return $headerFields;
 	}
--- modules/Products/models/RelationListView.php
+++ modules/Products/models/RelationListView.php
@@ -85,7 +85,7 @@ class Products_RelationListView_Model extends Vtiger_RelationListView_Model
 			$field->set('typeofdata', 'N~O');
 			$field->set('isEditable', true);
 			$field->set('fromOutsideList', true);
-			$field->set('class', 'js-format-numer validate[required,funcCall[Vtiger_Currency_Validator_Js.invokeValidation]]');
+			$field->set('class', 'validate[required,funcCall[Vtiger_Currency_Validator_Js.invokeValidation]]');
 			$headerFields['listprice'] = $field;
 		}
 		return $headerFields;
