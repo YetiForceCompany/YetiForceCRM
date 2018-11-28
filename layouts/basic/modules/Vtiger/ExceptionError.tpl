b79e99d77bb9dd066e7bad78b3c304ee3be2f35b
Sławomir Kłos <s.klos@yetiforce.com>
2018-11-28 15:04:46 +0100

    Error handling improvements, scan source for errors, code cleanup.

--- layouts/basic/modules/Vtiger/ExceptionError.tpl
+++ layouts/basic/modules/Vtiger/ExceptionError.tpl
@@ -26,7 +26,10 @@
 				<h3 class="align-items-center card-title d-flex justify-content-center">{\App\Purifier::encodeHtml($HEADER_MESSAGE)}</h3>
 			</div>
 			<div class="card-body bg-color-grey-50">
-				<p class="card-text u-font-size-19px">{if $MESSAGE_EXPANDED}{\App\Purifier::encodeHtml($MESSAGE['message'])}.{else}{\App\Purifier::encodeHtml($MESSAGE)}{/if}</p>
+				<p class="card-text u-font-size-19px">
+					<YF_ERROR>{if $MESSAGE_EXPANDED}{\App\Purifier::encodeHtml($MESSAGE['message'])}.{else}{\App\Purifier::encodeHtml($MESSAGE)}{/if}
+					</YF_ERROR>
+				</p>
 			</div>
 			<div class="card-footer d-flex flex-wrap flex-sm-nowrap">
 				<a class="btn btn-lg btn-default mr-sm-2 mb-1 mb-sm-0 w-100" role="button"
--- tests/GuiBase.php
+++ tests/GuiBase.php
@@ -16,19 +16,38 @@ use Facebook\WebDriver\WebDriverBy;
 abstract class GuiBase extends \PHPUnit\Framework\TestCase
 {
 	/**
-	 * Last logs.
+	 * Log messages from current call.
 	 *
 	 * @var mixed
 	 */
 	public $logs = [];
+	/**
+	 * Facebook WebDriver connection.
+	 *
+	 * @var \Facebook\WebDriver\RemoteWebDriver
+	 */
 	public $driver;
+	/**
+	 * @var string current page url
+	 */
 	public $currentUrl;
+	/**
+	 * @var string current page source
+	 */
 	public $loadedPageSource;
+	/**
+	 * @var string string to search for exceptions in source
+	 */
 	public $sourceErrorString = 'YF_ERROR';
+	/**
+	 * @var string Artifacts directory path
+	 */
 	public $artifactsDir = 'tests' . \DIRECTORY_SEPARATOR . 'tmp' . \DIRECTORY_SEPARATOR . 'artifacts' . \DIRECTORY_SEPARATOR;
 	protected $errorMsgTranslations = ['invalid session id' => 'Cant connect to browser window, check if is open'];
 
 	/**
+	 * Display logs to console.
+	 *
 	 * @codeCoverageIgnore
 	 */
 	protected function displayLogs()
@@ -38,21 +57,27 @@ abstract class GuiBase extends \PHPUnit\Framework\TestCase
 				echo '--------- ' . \strtoupper($log['level']) . ' ---------' . \PHP_EOL;
 				echo "URL: {$log['url']}" . \PHP_EOL;
 				echo "TYPE: {$log['source']}" . \PHP_EOL;
-				echo "MSG: {$log['message']}" . \PHP_EOL;
+				echo 'MSG: ' . \PHP_EOL;
+				echo $log['message'] . \PHP_EOL;
 				echo '-----------------------------------' . \PHP_EOL;
 			}
 			$this->logs = [];
 		}
 	}
 
+	/**
+	 * Setup connection before each test.
+	 */
 	public function setUp()
 	{
 		parent::setUp();
 
 		$this->driver = RemoteWebDriver::create('http://localhost:4444/wd/hub', DesiredCapabilities::chrome(), 5000);
-		$this->login();
 	}
 
+	/**
+	 * Close connection and get artifacts after each test.
+	 */
 	public function tearDown()
 	{
 		if ($this->hasFailed()) {
@@ -64,6 +89,12 @@ abstract class GuiBase extends \PHPUnit\Framework\TestCase
 		parent::tearDown();
 	}
 
+	/**
+	 * Open new url in selenium.
+	 *
+	 * @param string $url       page url
+	 * @param bool   $autoLogin Do autologin if login page detected
+	 */
 	public function url($url, $autoLogin = true)
 	{
 		$this->currentUrl = \AppConfig::main('site_URL') . $url;
@@ -77,18 +108,34 @@ abstract class GuiBase extends \PHPUnit\Framework\TestCase
 		$this->validateConsole();
 	}
 
+	/**
+	 * Validate page source syntax and detect error.
+	 *
+	 * @param $source html code
+	 */
 	public function validateSource($source)
 	{
-		if (\strpos($source, $this->sourceErrorString) !== false) {
+		if (\strpos($source, $this->sourceErrorString) !== false || \strpos($source, \strtolower($this->sourceErrorString)) !== false) {
 			foreach ($this->getSourceErrors($source) as $error) {
-				$this->log($error['msg'], 'page', $error['level']);
+				$this->log($error['msg'], 'source', $error['level']);
 			}
 		}
 		$this->validateHtml($source);
 	}
 
+	/**
+	 * Validate html source syntax.
+	 *
+	 * @param string $source Html code
+	 * @param bool   $parser Parser version
+	 *
+	 * @throws \HtmlValidator\Exception\ServerException
+	 * @throws \HtmlValidator\Exception\UnknownParserException
+	 * @codeCoverageIgnore
+	 */
 	public function validateHtml($source, $parser = false)
 	{
+		return;
 		if (!$parser) {
 			$parser = \HtmlValidator\Validator::PARSER_HTML5;
 		}
@@ -100,8 +147,8 @@ abstract class GuiBase extends \PHPUnit\Framework\TestCase
 				$this->log((string)$result, 'page');
 				$toFile = $this->currentUrl . \PHP_EOL;
 				$toFile .= (string)$result;
-				print('HTML validation error: ' . $this->currentUrl) . \PHP_EOL;
-				$file = $this->getName() . '_' . date("Ymd_His");
+				echo ('HTML validation error: ' . $this->currentUrl) . \PHP_EOL;
+				$file = $this->getName() . '_' . date('Ymd_His');
 				$this->takeScreenshot($file . '_htmlValidation');
 				$dir = $this->artifactsDir . 'pages' . \DIRECTORY_SEPARATOR;
 				if (!is_dir($dir) && !mkdir($dir, 0777, true) && !\is_dir($dir)) {
@@ -119,10 +166,14 @@ abstract class GuiBase extends \PHPUnit\Framework\TestCase
 		}
 	}
 
+	/**
+	 * @param string      $source Html source code
+	 * @param null|string $file   file path
+	 */
 	public function saveSource($source, $file = null)
 	{
 		if (!$file) {
-			$file = $this->getName() . '_' . date("Ymd_His");
+			$file = $this->getName() . '_' . date('Ymd_His');
 		}
 		$dir = $this->artifactsDir . 'pages' . \DIRECTORY_SEPARATOR;
 		if (!is_dir($dir) && !mkdir($dir, 0777, true) && !\is_dir($dir)) {
@@ -136,6 +187,9 @@ abstract class GuiBase extends \PHPUnit\Framework\TestCase
 		}
 	}
 
+	/**
+	 * Browser console validation.
+	 */
 	public function validateConsole()
 	{
 		$logs = $this->driver->manage()->getLog('browser');
@@ -147,6 +201,13 @@ abstract class GuiBase extends \PHPUnit\Framework\TestCase
 		}
 	}
 
+	/**
+	 * Check if console log entry should be ignored.
+	 *
+	 * @param string $msg Browser console log entry
+	 *
+	 * @return bool
+	 */
 	public function ignoredBrowserError($msg)
 	{
 		$result = false;
@@ -157,15 +218,42 @@ abstract class GuiBase extends \PHPUnit\Framework\TestCase
 		return $result;
 	}
 
+	/**
+	 * Search and return array of detected source errors.
+	 *
+	 * @param string $source
+	 *
+	 * @return array
+	 */
 	public function getSourceErrors($source)
 	{
-		return [];
+		$errors = [];
+		preg_match_all('/' . $this->sourceErrorString . '>(.*?)<\/' . $this->sourceErrorString . '>/s', $source, $matchesUpper);
+		preg_match_all('/' . \strtolower($this->sourceErrorString) . '>(.*?)<\/' . strtolower($this->sourceErrorString) . '>/s', $source, $matchesLower);
+		foreach (\array_merge($matchesUpper[1], $matchesLower[1]) as $item) {
+			if (\strpos($item, 'E_NOTICE') !== false) {
+				$level = 'warning';
+			} elseif (\strpos($item, 'E_WARNING') !== false) {
+				$level = 'warning';
+			} elseif (\strpos($item, 'E_ERROR') !== false) {
+				$level = 'error';
+			} else {
+				$level = 'info';
+			}
+			$errors[] = ['msg' => $item, 'level' => $level];
+		}
+		return $errors;
 	}
 
+	/**
+	 * Take screenshot of current page and save to specified file.
+	 *
+	 * @param null|string $name
+	 */
 	public function takeScreenshot($name = null)
 	{
 		if (empty($name)) {
-			$name = $this->getName() . '_' . date("Ymd_His");
+			$name = $this->getName() . '_' . date('Ymd_His');
 		}
 		$dir = $this->artifactsDir . 'screenshots' . \DIRECTORY_SEPARATOR;
 		if (!is_dir($dir) && !mkdir($dir, 0777, true) && !\is_dir($dir)) {
@@ -179,6 +267,13 @@ abstract class GuiBase extends \PHPUnit\Framework\TestCase
 		}
 	}
 
+	/**
+	 * Add log message.
+	 *
+	 * @param string $msg
+	 * @param string $source
+	 * @param string $level
+	 */
 	public function log($msg, $source = 'page', $level = 'info')
 	{
 		$msg = $this->translateErrorMessage($msg);
@@ -188,42 +283,69 @@ abstract class GuiBase extends \PHPUnit\Framework\TestCase
 		}
 	}
 
+	/**
+	 * Return more human friendly error info.
+	 *
+	 * @param string $msg
+	 *
+	 * @return string
+	 */
 	protected function translateErrorMessage($msg)
 	{
 		return $this->errorMsgTranslations[$msg] ?? $msg;
 	}
 
-	protected function detectLoginPage(): bool
+	/**
+	 * Check if current open page is login screen.
+	 *
+	 * @param string $calledFrom
+	 *
+	 * @return bool
+	 */
+	protected function detectLoginPage($calledFrom = 'Test'): bool
 	{
 		$return = false;
 		try {
 			if ($this->driver->findElement(WebDriverBy::id('login-area'))->getAttribute('id') ? true : false) {
-				$this->log('Login page detected');
+				$this->log("Login page detected [{$calledFrom}]");
 				$return = true;
+			} else {
+				$this->log("Login page NOT detected [{$calledFrom}]");
 			}
 			return $return;
 		} catch (\Exception $exception) {
+			$this->log("Login page NOT detected [{$calledFrom}]");
 			return false;
 		}
 	}
 
 	/**
-	 * Testing login page display.
+	 * Perform login of user to system.
+	 *
+	 * @param bool $navigate
 	 */
 	public function login($navigate = true)
 	{
 		if ($navigate) {
-			$this->url('index.php', false);
+			$this->url('index.php?module=Users&view=Login', false);
 		}
-		if ($this->detectLoginPage()) {
+		if ($this->detectLoginPage('GuiBase::login')) {
 			$this->findElBy('id', 'username')->sendKeys('demo');
 			$this->findElBy('id', 'password')->sendKeys(\Tests\Base\A_User::$defaultPassrowd);
 			$this->findElBy('tagName', 'form')->submit();
 		} else {
-			$this->log('Login page not detected');
+			$this->log('Login not possible, not on login page', 'page', 'error');
 		}
 	}
 
+	/**
+	 * Find element by wrapper for Facebook webdriver functions.
+	 *
+	 * @param string $method    Find method name
+	 * @param string $condition Condition for choosen method
+	 *
+	 * @return object|null
+	 */
 	public function findElBy($method, $condition)
 	{
 		try {
@@ -233,6 +355,15 @@ abstract class GuiBase extends \PHPUnit\Framework\TestCase
 		}
 	}
 
+	/**
+	 * Translate findElBy errors to more human friendly format.
+	 *
+	 * @param string $msg
+	 * @param string $method
+	 * @param string $condition
+	 *
+	 * @return string
+	 */
 	protected function parseFindElByError($msg, $method, $condition)
 	{
 		if (\strpos($msg, 'no such element: Unable to locate element: {"method":"') !== false) {
