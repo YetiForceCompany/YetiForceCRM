da43c7fcc88c6cc74577cfc20a8aabb520fd5679
Sławomir Kłos <s.klos@yetiforce.com>
2018-11-29 09:29:46 +0100

    CodeReview fixes

--- app/SystemWarnings/Security/Dependencies.php
+++ app/SystemWarnings/Security/Dependencies.php
@@ -23,19 +23,19 @@ class Dependencies extends \App\SystemWarnings\Template
 			$this->status = 1;
 			return;
 		}
-		$vulnResults = (new \SensioLabs\Security\SecurityChecker())->check(ROOT_DIRECTORY);
-		if ($vulnResults->count()) {
+		$results = (new \SensioLabs\Security\SecurityChecker())->check(ROOT_DIRECTORY);
+		if ($results->count()) {
 			$this->status = 0;
 		} else {
 			$this->status = 1;
 		}
 		if ($this->status === 0) {
-			$vulns = \App\Json::decode((string)$vulnResults);
-			$vulns = (\is_array($vulns) && !empty($vulns)) ? $vulns : [];
+			$vulnerabilities = \App\Json::decode((string) $results);
+			$vulnerabilities = (\is_array($vulnerabilities) && !empty($vulnerabilities)) ? $vulnerabilities : [];
 			$this->link = 'index.php?module=Vtiger&parent=Settings&view=Index&mode=security';
 			$this->linkTitle = \App\Language::translate('Security', 'Settings:SystemWarnings');
 			$this->description = \App\Language::translate('LBL_VULNERABILITIES_IN_DEPENDENCIES_DESC', 'Settings:SystemWarnings') . '<br />';
-			foreach ($vulns as $name => $vulnerability) {
+			foreach ($vulnerabilities as $name => $vulnerability) {
 				$this->description .= "$name({$vulnerability['version']}):<br />";
 				foreach ($vulnerability['advisories'] as $data) {
 					$this->description .= "{$data['title']} {$data['cve']}<br />";
