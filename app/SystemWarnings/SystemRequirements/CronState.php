8c7098f6ae0184c3dc95e8f151307c387fd0fa13
Sławomir Kłos <s.klos@yetiforce.com>
2018-11-29 16:33:45 +0100

    Added tasks validation

--- app/SystemWarnings/SystemRequirements/CronState.php
+++ app/SystemWarnings/SystemRequirements/CronState.php
@@ -25,16 +25,21 @@ class CronState extends \App\SystemWarnings\Template
 	public function process()
 	{
 		$lastStart = \Settings_CronTasks_Module_Model::getInstance('Settings:CronTasks')->getLastCronStart();
+		$checkTasks = (new \App\Db\Query())
+			->select(['id'])
+			->from('vtiger_cron_task')
+			->where(['status' => 1, 'lastend' => null])
+			->limit(10)->count() ? false : true;
 		$timeOut = round((\AppConfig::main('maxExecutionCronTime') + 300) / 60);
-		if ($lastStart === 0 || strtotime("-{$timeOut} minutes") > $lastStart) {
+		if ($lastStart === 0 || strtotime("-{$timeOut} minutes") > $lastStart || !$checkTasks) {
 			$this->status = 0;
 		} else {
 			$this->status = 1;
 		}
 		if ($this->status === 0) {
-			$this->link = 'https://yetiforce.com/en/knowledge-base/documentation/administrator-documentation/item/enable-cron';
-			$this->linkTitle = \App\Language::translate('LBL_HOW_TO_ENABLE_CRON', 'Settings:SystemWarnings');
-			$this->description = \App\Language::translate('LBL_CRON_DISABLED_DESC', 'Settings:SystemWarnings') . '<br />';
+			$this->link = $checkTasks ? 'https://yetiforce.com/en/knowledge-base/documentation/administrator-documentation/item/enable-cron' : 'index.php?module=CronTasks&parent=Settings&view=List';
+			$this->linkTitle = \App\Language::translate($checkTasks ? 'LBL_HOW_TO_ENABLE_CRON' : 'LBL_CRON_TASKS_LIST', 'Settings:SystemWarnings');
+			$this->description = \App\Language::translate($checkTasks ? 'LBL_CRON_DISABLED_DESC' : 'LBL_CRON_TASK_FAILED_DESC', 'Settings:SystemWarnings') . '<br />';
 		}
 	}
 }
