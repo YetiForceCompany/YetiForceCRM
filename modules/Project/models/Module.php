7d2903af80da7bf250c7ac3513431030bbeaa54c
Arkadiusz Adach <arek@wp4b.pl>
2018-11-29 14:52:39 +0100

    Amendment of comments

--- modules/Project/models/Module.php
+++ modules/Project/models/Module.php
@@ -1,14 +1,15 @@
 <?php
 
-/**
- * Project module model class.
- *
- * @package   Model
- *
- * @copyright YetiForce Sp. z o.o
- * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
- * @author    Arkadiusz Adach <a.adach@yetiforce.com>
- */
+/* +***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * Contributor(s): YetiForce.com
+ * *********************************************************************************** */
+
 class Project_Module_Model extends Vtiger_Module_Model
 {
 	/**
