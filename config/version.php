f758277618cc0bc1fc05112df3a7ddbc75356b69
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-12-03 12:13:30 +0100

    Fixed mail scanner

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.4.98',
-	'patchVersion' => '2018.11.29',
+	'appVersion' => '4.4.100',
+	'patchVersion' => '2018.12.03',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.56'
 ];
