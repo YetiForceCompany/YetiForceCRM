ed0ff53d46f8df09bf72f74a728aa72471bad369
Arkadiusz Adach <arek@wp4b.pl>
2018-12-07 14:55:06 +0100

    Improved Security\Dependency class

--- app/Security/Dependency.php
+++ app/Security/Dependency.php
@@ -19,7 +19,7 @@ class Dependency
 	/**
 	 * Cache file name.
 	 */
-	const CACHE_FILE_NAME = ROOT_DIRECTORY . DIRECTORY_SEPARATOR . 'cache/security/dependency.json';
+	const CACHE_FILE_NAME = ROOT_DIRECTORY . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR . 'security' . DIRECTORY_SEPARATOR . 'dependency.json';
 
 	/**
 	 * SensioLabs security checker.
@@ -30,12 +30,10 @@ class Dependency
 	 */
 	public function securityChecker(): array
 	{
-		$result = null;
+		$result = [];
 		if ($this->hasCache()) {
 			$result = $this->getCache();
-		} elseif (!\App\RequestUtil::isNetConnection()) {
-			throw new \App\Exceptions\AppException('ERR_NO_INTERNET_CONNECTION');
-		} else {
+		} elseif (\App\RequestUtil::isNetConnection()) {
 			$resultObject = (new \SensioLabs\Security\SecurityChecker())->check(ROOT_DIRECTORY);
 			$result = \App\Json::decode((string) $resultObject);
 			$result = (\is_array($result) && !empty($result)) ? $result : [];
