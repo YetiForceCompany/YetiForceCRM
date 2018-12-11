acabf8090ee9c5301577d59e33fba6fe8ddf986f
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-12-11 09:43:10 +0100

    Added minor improvements

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'appVersion' => '4.4.107',
-	'patchVersion' => '2018.12.10',
+	'appVersion' => '4.4.108',
+	'patchVersion' => '2018.12.11',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.57'
 ];
