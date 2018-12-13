6b9dfa4b05e4d8fd237abb130f68c2fdc09a2dd7
TomaszQr <t.kur@yetiforce.com>
2018-12-13 12:31:39 +0100

    Improved redirect into listview from dashboard (to only active records)

--- modules/HelpDesk/dashboards/ClosedTicketsByUser.php
+++ modules/HelpDesk/dashboards/ClosedTicketsByUser.php
@@ -28,7 +28,7 @@ class HelpDesk_ClosedTicketsByUser_Dashboard extends Vtiger_IndexAjax_View
 			$conditions[] = ['assigned_user_id', 'e', $owner];
 		}
 		$listSearchParams[] = $conditions;
-		return '&viewname=All&search_params=' . json_encode($listSearchParams);
+		return '&entityState=Active&viewname=All&search_params=' . json_encode($listSearchParams);
 	}
 
 	/**
