9fbb441443cb007636dce5e03d06f565a3bca6cd
Sławomir Kłos <s.klos@yetiforce.com>
2018-12-19 10:36:29 +0100

    CodeReview fixes

--- public_html/layouts/resources/Tools.js
+++ public_html/layouts/resources/Tools.js
@@ -3,15 +3,23 @@
 
 App.Tools = {
 	VariablesPanel: {
+		/**
+		 * Generate values based on selected company
+		 * @param container
+		 */
 		refreshCompanyVariables(container) {
 			const companyId = container.find(".companyList").val();
-			container.find(".companyVariable > optgroup > option").each(function () {
+			container.find(".js-company-variable > optgroup > option").each(function () {
 				let template = $(this).data('value-template');
 				this.value = template.replace(/__X__/i, companyId);
 			});
 		},
+		/**
+		 * Register change company event
+		 * @param container
+		 */
 		registerRefreshCompanyVariables(container) {
-			container.find('.companyList').on('change', function (e) {
+			container.find('.js-company-list').on('change', function (e) {
 				App.Tools.VariablesPanel.refreshCompanyVariables(container);
 			});
 		},
