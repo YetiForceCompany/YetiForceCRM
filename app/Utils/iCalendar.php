ed86528724da85bdb33de2231db611b39b643043
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-01-11 10:36:05 +0100

    Added app/Utils/iCalendar.php

---
+++ app/Utils/iCalendar.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * iCalendar class.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Tomasz Poradzewski <t.poradzewski@yetiforce.com>
+ */
+
+namespace App\Utils;
+
+class iCalendar
+{
+	public static function import(string $filePath)
+	{
+		$userModel = \App\User::getCurrentUserModel();
+		$lastImport = new IcalLastImport();
+		$lastImport->clearRecords($userModel->getId());
+		$eventModule = 'Events';
+		$todoModule = 'Calendar';
+		$totalCount = $skipCount = [$eventModule => 0, $todoModule => 0];
+		$ical = new Ical();
+		$icalActivities = $ical->iCalReader($filePath);
+		$noOfActivities = count($icalActivities);
+		$moduleModel = Vtiger_Module_Model::getInstance($todoModule);
+
+		for ($i = 0; $i < $noOfActivities; ++$i) {
+			if ($icalActivities[$i]['TYPE'] == 'VEVENT') {
+				$activity = new IcalendarEvent();
+				$module = $eventModule;
+			} else {
+				$activity = new IcalendarTodo();
+				$module = $todoModule;
+			}
+			$skipRecord = false;
+			++$totalCount[$module];
+			$activityFieldsList = $activity->generateArray($icalActivities[$i]);
+			$activityFieldsList['assigned_user_id'] = $userModel->getId();
+			$activityFieldsList['time_end'] = $activityFieldsList['time_end'] ?? $userModel->getDetail('end_hour') . ':00';
+			$recordModel = Vtiger_Record_Model::getCleanInstance($moduleModel->getName());
+			foreach ($moduleModel->getFields() as $fieldName => $fieldModel) {
+				if (empty($activityFieldsList[$fieldName]) && $fieldModel->isActiveField() && $fieldModel->isMandatory()) {
+					++$skipCount[$module];
+					$skipRecord = true;
+					break;
+				}
+				if (!$fieldModel->isWritable() || !isset($activityFieldsList[$fieldName])) {
+					continue;
+				}
+				$recordModel->set($fieldName, $activityFieldsList[$fieldName]);
+			}
+			if ($skipRecord) {
+				continue;
+			}
+			$recordModel->save();
+			$lastImport = new IcalLastImport();
+			$lastImport->setFields(['userid' => $userModel->getId(), 'entitytype' => $moduleModel->getName(), 'crmid' => $recordModel->getId()]);
+			$lastImport->save();
+		}
+		return ['events' => $totalCount[$eventModule] - $skipCount[$eventModule], 'skipped_events' => $skipCount[$eventModule], 'task' => $totalCount[$todoModule] - $skipCount[$todoModule], 'skipped_task' => $skipCount[$todoModule]];
+	}
+}
