9eb18616237ae8bdb7d5f9fbf0f014af29f18f1c
Arkadiusz Adach <arek@wp4b.pl>
2019-01-11 14:42:16 +0100

    code optimization

--- file.php
+++ file.php
@@ -21,7 +21,7 @@ try {
 	if (\AppConfig::debug('DISPLAY_EXCEPTION_BACKTRACE') && is_object($e)) {
 		$trace = str_replace(ROOT_DIRECTORY . DIRECTORY_SEPARATOR, '', $e->getTraceAsString());
 	}
-	$response->setHeader($_SERVER['SERVER_PROTOCOL'] . ' ' . $e->getCode() . ' Internal Server Error');
+	$response->setHeader(\App\Request::_getServer('SERVER_PROTOCOL') . ' ' . $e->getCode() . ' Internal Server Error');
 	$response->setError($e->getCode(), $e->getMessage(), $trace);
 	$response->emit();
 }
