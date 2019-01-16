271f231ee806604e417137980accfa6750e94efd
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-01-16 13:36:43 +0100

    Added alert and translations to downloading languages

--- modules/Settings/YetiForce/actions/DownloadLanguage.php
+++ modules/Settings/YetiForce/actions/DownloadLanguage.php
@@ -7,7 +7,7 @@
  *
  * @copyright YetiForce Sp. z o.o
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
- * @author    Sławomir Kłos <s.klos@yetiforce.com>
+ * @author    Sławomir Kłos <t.poradzewski@yetiforce.com>
  */
 
 /**
