2c8875c2b2f88cf8d81752a6d2f03b1aaa18208c
TomaszQr <t.kur@yetiforce.com>
2019-01-16 11:28:34 +0100

    Added hierarchy into Contacts

--- modules/Contacts/models/Record.php
+++ modules/Contacts/models/Record.php
@@ -100,4 +100,28 @@ class Contacts_Record_Model extends Vtiger_Record_Model
 		}
 		return $links;
 	}
+
+	/**
+	 * Function returns the details of IStorages Hierarchy.
+	 *
+	 * @return array
+	 */
+	public function getHierarchy()
+	{
+		$focus = CRMEntity::getInstance($this->getModuleName());
+		$hierarchy = $focus->getHierarchy($this->getId());
+		foreach ($hierarchy['entries'] as $competitionId => $data) {
+			preg_match('/<a href="+/', $data[0], $matches);
+			if (!empty($matches)) {
+				preg_match('/[.\s]+/', $data[0], $dashes);
+				preg_match("/<a(.*)>(.*)<\/a>/i", $data[0], $name);
+
+				$recordModel = Vtiger_Record_Model::getCleanInstance($this->getModuleName());
+				$recordModel->setId($competitionId);
+				$hierarchy['entries'][$competitionId][0] = $dashes[0] . '<a href=' . $recordModel->getDetailViewUrl() . '>' . $name[2] .
+					'</a>';
+			}
+		}
+		return $hierarchy;
+	}
 }
