7d79dec5c5fa40f529ec53d599678b30367f5249
Arek Dudek <a.dudek@yetiforce.com>
2019-01-18 12:11:40 +0100

    Fix: modal close bug [IT210]

--- public_html/layouts/basic/modules/Calendar/resources/ActivityStateModal.js
+++ public_html/layouts/basic/modules/Calendar/resources/ActivityStateModal.js
@@ -23,6 +23,7 @@ jQuery.Class("Calendar_ActivityStateModal_Js", {}, {
 						sourceRecord: currentTarget.data('id')
 					});
 				} else if (currentTarget.hasClass('showQuickCreate')) {
+					app.hideModalWindow();
 					let progressIndicatorElement = $.progressIndicator({
 							'position': 'html',
 							'blockInfo': {
