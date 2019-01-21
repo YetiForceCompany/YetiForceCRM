8f16817c2986a9ed1f9d9bc501cb557096d751f3
Arkadiusz Adach <arek@wp4b.pl>
2019-01-21 11:45:37 +0100

    Password validation error fixed

--- public_html/install/tpl/resources/Index.js
+++ public_html/install/tpl/resources/Index.js
@@ -64,7 +64,6 @@ jQuery.Class('Install_Index_Js', {
 	},
 	checkPwd: function (pass) {
 		var error = false;
-
 		if (pass.length < 8) {
 			jQuery('#passwordError').html(app.vtranslate('LBL_PASS_TO_SHORT'));
 			error = true;
@@ -81,7 +80,6 @@ jQuery.Class('Install_Index_Js', {
 			jQuery('#passwordError').html(app.vtranslate('LBL_PASS_LACK_OF_LOWERCASE_LETTERS'));
 			error = true;
 		}
-
 		return error;
 	},
 	registerEventForStep4: function () {
@@ -103,7 +101,6 @@ jQuery.Class('Install_Index_Js', {
 				}
 			}
 		});
-		var thisInstance = this;
 		jQuery('input[name="create_db"]').on('click', function () {
 			var userName = jQuery('#root_user');
 			var password = jQuery('#root_password');
@@ -140,104 +137,18 @@ jQuery.Class('Install_Index_Js', {
 				clearPasswordError();
 			}
 		});
-
 		jQuery('input[name="retype_password"]').on('keypress', function (e) {
 			clearPasswordError();
 		});
-
-		jQuery('input[name="step5"]').on('click', function () {
-			var error = false;
-			var validateFieldNames = ['db_hostname', 'db_username', 'db_name', 'password', 'retype_password', 'lastname', 'admin_email'];
-			for (var fieldName in validateFieldNames) {
-				var field = jQuery('input[name="' + validateFieldNames[fieldName] + '"]');
-				if (field.val() == '') {
-					field.addClass('error').focus();
-					error = true;
-					break;
-				} else {
-					field.removeClass('error');
-				}
-			}
-
-			var createDatabase = jQuery('input[name="create_db"]:checked');
-			if (createDatabase.length > 0) {
-				var dbRootUser = jQuery('input[name="db_root_username"]');
-				if (dbRootUser.val() == '') {
-					dbRootUser.addClass('error').focus();
-					error = true;
-				} else {
-					dbRootUser.removeClass('error');
-				}
-			}
-			var password = jQuery('#passwordError');
-			if (password.html().trim())
-				error = true;
-
-			var emailField = jQuery('input[name="admin_email"]');
-			var invalidEmailAddress = false;
-			var regex = /^[_/a-zA-Z0-9*]+([!"#$%&'()*+,./:;<=>?\^_`{|}~-]?[a-zA-Z0-9/_/-])*@[a-zA-Z0-9]+([\_\-\.]?[a-zA-Z0-9]+)*\.([\-\_]?[a-zA-Z0-9])+(\.?[a-zA-Z0-9]+)?$/;
-			if (!regex.test(emailField.val()) && emailField.val() != '') {
-				invalidEmailAddress = true;
-				emailField.addClass('error').focus();
-				error = true;
+		$('form[name="step4"]').on('submit', (e) => {
+			if (this.checkForm()) {
+				e.preventDefault();
 			} else {
-				emailField.removeClass('error');
-			}
-
-			var checkPwdError = false;
-			checkPwdError = thisInstance.checkPwd(jQuery('input[name="password"]').val());
-
-			if (checkPwdError) {
-				error = true;
-			}
-
-			if (error) {
-				var content;
-				if (invalidEmailAddress) {
-					content = '<div class="span12">' +
-						'<div class="alert alert-error">' +
-						'<button class="close" data-dismiss="alert" type="button">x</button>' +
-						jQuery('[name="invalidEmailError"]').val() +
-						'</div>' +
-						'</div>';
-				} else {
-					if (checkPwdError) {
-						content = '<div class="span12">' +
-							'<div class="alert alert-error">' +
-							'<button class="close" data-dismiss="alert" type="button">x</button>' +
-							jQuery('[name="insufficientlyStrongPassword"]').val() +
-							'</div>' +
-							'</div>';
-					} else {
-						content = '<div class="span12">' +
-							'<div class="alert alert-error">' +
-							'<button class="close" data-dismiss="alert" type="button">x</button>' +
-							app.vtranslate('LBL_MANDATORY_FIELDS_ERROR') +
-							'</div>' +
-							'</div>';
-					}
-				}
-				jQuery('#errorMessage').html(content).show();
-			} else {
-				var config = {
-					db_hostname: document.step4.db_hostname.value,
-					db_username: document.step4.db_username.value,
-					db_name: document.step4.db_name.value,
-					create_db: jQuery('[name="create_db"]').prop('checked'),
-					db_root_username: document.step4.db_root_username.value,
-					currency_name: document.step4.currency_name.value,
-					firstname: document.step4.firstname.value,
-					lastname: document.step4.lastname.value,
-					admin_email: document.step4.admin_email.value,
-					dateformat: document.step4.dateformat.value,
-					timezone: document.step4.timezone.value
-				};
-				window.localStorage.setItem('yetiforce_install', JSON.stringify(config));
-				jQuery('form[name="step4"]').submit();
+				$('form[name="step4"]').off('submit');
+				this.submitForm();
 			}
 		});
 		this.checkPwdEvent();
-
 	},
 	registerEventForStep5: function () {
 		jQuery('input[name="step6"]').on('click', function () {
@@ -266,6 +177,97 @@ jQuery.Class('Install_Index_Js', {
 				jQuery('#progressIndicator').show();
 				jQuery('#mainContainer').hide();
 			});
+		}
+	},
+	checkForm() {
+		var thisInstance = this;
+		var error = false;
+		var validateFieldNames = ['db_hostname', 'db_username', 'db_name', 'password', 'retype_password', 'lastname', 'admin_email'];
+		for (var fieldName in validateFieldNames) {
+			var field = jQuery('input[name="' + validateFieldNames[fieldName] + '"]');
+			if (field.val() == '') {
+				field.addClass('error').focus();
+				error = true;
+				break;
+			} else {
+				field.removeClass('error');
+			}
+		}
+		var createDatabase = jQuery('input[name="create_db"]:checked');
+		if (createDatabase.length > 0) {
+			var dbRootUser = jQuery('input[name="db_root_username"]');
+			if (dbRootUser.val() == '') {
+				dbRootUser.addClass('error').focus();
+				error = true;
+			} else {
+				dbRootUser.removeClass('error');
+			}
+		}
+		var password = jQuery('#passwordError');
+		if (password.html().trim()) {
+			error = true;
+		}
+		var emailField = jQuery('input[name="admin_email"]');
+		var invalidEmailAddress = false;
+		var regex = /^[_/a-zA-Z0-9*]+([!"#$%&'()*+,./:;<=>?\^_`{|}~-]?[a-zA-Z0-9/_/-])*@[a-zA-Z0-9]+([\_\-\.]?[a-zA-Z0-9]+)*\.([\-\_]?[a-zA-Z0-9])+(\.?[a-zA-Z0-9]+)?$/;
+		if (!regex.test(emailField.val()) && emailField.val() != '') {
+			invalidEmailAddress = true;
+			emailField.addClass('error').focus();
+			error = true;
+		} else {
+			emailField.removeClass('error');
+		}
+		var checkPwdError = false;
+		checkPwdError = thisInstance.checkPwd(jQuery('input[name="password"]').val());
+		if (checkPwdError) {
+			error = true;
+		}
+		return error;
+	},
+	submitForm(error) {
+		if (error) {
+			var content;
+			if (invalidEmailAddress) {
+				content = '<div class="span12">' +
+					'<div class="alert alert-error">' +
+					'<button class="close" data-dismiss="alert" type="button">x</button>' +
+					jQuery('[name="invalidEmailError"]').val() +
+					'</div>' +
+					'</div>';
+			} else {
+				if (checkPwdError) {
+					content = '<div class="span12">' +
+						'<div class="alert alert-error">' +
+						'<button class="close" data-dismiss="alert" type="button">x</button>' +
+						jQuery('[name="insufficientlyStrongPassword"]').val() +
+						'</div>' +
+						'</div>';
+				} else {
+					content = '<div class="span12">' +
+						'<div class="alert alert-error">' +
+						'<button class="close" data-dismiss="alert" type="button">x</button>' +
+						app.vtranslate('LBL_MANDATORY_FIELDS_ERROR') +
+						'</div>' +
+						'</div>';
+				}
+			}
+			jQuery('#errorMessage').html(content).show();
+		} else {
+			var config = {
+				db_hostname: document.step4.db_hostname.value,
+				db_username: document.step4.db_username.value,
+				db_name: document.step4.db_name.value,
+				create_db: jQuery('[name="create_db"]').prop('checked'),
+				db_root_username: document.step4.db_root_username.value,
+				currency_name: document.step4.currency_name.value,
+				firstname: document.step4.firstname.value,
+				lastname: document.step4.lastname.value,
+				admin_email: document.step4.admin_email.value,
+				dateformat: document.step4.dateformat.value,
+				timezone: document.step4.timezone.value
+			};
+			window.localStorage.setItem('yetiforce_install', JSON.stringify(config));
+			jQuery('form[name="step4"]').submit();
 		}
 	},
 	changeLanguage: function (e) {
