b9fb1d7a374399daf36b7de25b6c03831d095ccf
Arek Dudek <a.dudek@yetiforce.com>
2019-01-24 09:01:53 +0100

    Changes from $request->get() to correct methods part 2

--- modules/Settings/LoginHistory/views/List.php
+++ modules/Settings/LoginHistory/views/List.php
@@ -13,7 +13,7 @@ class Settings_LoginHistory_List_View extends Settings_Vtiger_List_View
 		$loginHistoryRecordModel = new Settings_LoginHistory_Record_Model();
 		$usersList = $loginHistoryRecordModel->getAccessibleUsers();
 		$viewer->assign('USERSLIST', $usersList);
-		$viewer->assign('SELECTED_USER', $request->get('user_name'));
+		$viewer->assign('SELECTED_USER', $request->getByType('user_name', 'Alnum'));
 		parent::preProcess($request, false);
 	}
 }
