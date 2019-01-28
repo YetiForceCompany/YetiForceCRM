40101e1b961e6d2f561a460d933d75c7c4c7e9b9
adudekyeti <a.dudek@yetiforce.com>
2019-01-28 15:59:26 +0100

    Code improvement

--- modules/Settings/WidgetsManagement/actions/SaveAjax.php
+++ modules/Settings/WidgetsManagement/actions/SaveAjax.php
@@ -17,7 +17,34 @@ class Settings_WidgetsManagement_SaveAjax_Action extends Settings_Vtiger_Basic_A
 
 	public function save(\App\Request $request)
 	{
-		$data = $request->getArray('form', 'Text');
+		$data = $request->getMultiDimensionArray('form', [
+			'module' => 'Alnum',
+			'parent' => 'Alnum',
+			'widgets' => 'Integer',
+			'action' => 'Alnum',
+			'width' => 'Integer',
+			'height' => 'Integer',
+			'data' => 'Text',
+			'blockid' => 'Integer',
+			'linkid' => 'Integer',
+			'label' => 'Text',
+			'title' => 'Text',
+			'name' => 'Text',
+			'filterid' => 'Text',
+			'isdefault' => 'Integer',
+			'owners_all' => [
+				'Standard',
+				'Standard',
+				'Standard',
+				'Standard',
+			],
+			'default_owner' => 'Standard',
+			'dashboardId' => 'Integer',
+			'limit' => 'Integer',
+			'cache' => 'Integer',
+			'default_date' => 'Standard',
+			'authorized' => 'Alnum',
+		]);
 		$moduleName = $request->getByType('sourceModule', 2);
 		$addToUser = $request->getBoolean('addToUser');
 		if (!is_array($data) || !$data) {
@@ -37,7 +64,11 @@ class Settings_WidgetsManagement_SaveAjax_Action extends Settings_Vtiger_Basic_A
 
 	public function delete(\App\Request $request)
 	{
-		$data = $request->getArray('form', 'Text');
+		$data = $request->getMultiDimensionArray('form', [
+			'action' => 'Alnum',
+			'id' => 'Integer',
+			'blockid' => 'Integer',
+		]);
 		$moduleName = $request->getByType('sourceModule', 2);
 		if (!is_array($data) || !$data) {
 			$result = ['success' => false, 'message' => \App\Language::translate('LBL_INVALID_DATA', $moduleName)];
