cdfd4887bd73184c82a3a4d5f28140be12cd6d1a
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2019-01-29 05:38:04 +0100

    Added deprecated to function

--- include/ConfigUtils.php
+++ include/ConfigUtils.php
@@ -3,8 +3,10 @@
 /**
  * App config class.
  *
- * @copyright YetiForce Sp. z o.o
- * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @copyright  YetiForce Sp. z o.o
+ * @license    YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ *
+ * @deprecated Use \App\Config
  */
 class AppConfig
 {
@@ -23,6 +25,8 @@ class AppConfig
 	/**
 	 * Function to get main configuration of system.
 	 *
+	 * @deprecated Use \App\Config::main()
+	 *
 	 * @param string $key
 	 * @param mixed  $value
 	 *
@@ -33,51 +37,81 @@ class AppConfig
 		return \App\Config::main($key, $value);
 	}
 
+	/**
+	 * @deprecated Use \App\Config::module()
+	 */
 	public static function module($module, $key = null, $defaultValue = null)
 	{
 		return \App\Config::module($module, $key, $defaultValue);
 	}
 
+	/**
+	 * @deprecated Use \App\Config::api()
+	 */
 	public static function api($key, $defvalue = false)
 	{
 		return \App\Config::api($key, $defvalue);
 	}
 
+	/**
+	 * @deprecated Use \App\Config::debug()
+	 */
 	public static function debug($key, $defvalue = false)
 	{
 		return \App\Config::debug($key, $defvalue);
 	}
 
+	/**
+	 * @deprecated Use \App\Config::developer()
+	 */
 	public static function developer($key, $defvalue = false)
 	{
 		return \App\Config::developer($key, $defvalue);
 	}
 
+	/**
+	 * @deprecated Use \App\Config::security()
+	 */
 	public static function security($key, $defvalue = false)
 	{
 		return \App\Config::security($key, $defvalue);
 	}
 
+	/**
+	 * @deprecated Use \App\Config::securityKeys()
+	 */
 	public static function securityKeys($key, $defvalue = false)
 	{
 		return \App\Config::securityKeys($key, $defvalue);
 	}
 
+	/**
+	 * @deprecated Use \App\Config::module()
+	 */
 	public static function performance($key, $defvalue = false)
 	{
 		return \App\Config::performance($key, $defvalue);
 	}
 
+	/**
+	 * @deprecated Use \App\Config::relation()
+	 */
 	public static function relation($key, $defvalue = false)
 	{
 		return \App\Config::relation($key, $defvalue);
 	}
 
+	/**
+	 * @deprecated Use \App\Config::sounds()
+	 */
 	public static function sounds(?string $arg = null, $default = null)
 	{
-		return \App\Config::search($arg, $default);
+		return \App\Config::sounds($arg, $default);
 	}
 
+	/**
+	 * @deprecated Use \App\Config::search()
+	 */
 	public static function search($key, $defvalue = false)
 	{
 		return \App\Config::search($key, $defvalue);
