bedd2d06652c6e9891d7a9a7b9bca1c5ef1975ee
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2019-01-31 06:38:06 +0100

    Revert "Config change improved"

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'appVersion' => '4.4.154',
-	'patchVersion' => '2019.01.31',
-	'lib_roundcube' => '0.0.63'
+	'appVersion' => '4.4.153',
+	'patchVersion' => '2019.01.30',
+	'lib_roundcube' => '0.0.61'
 ];
