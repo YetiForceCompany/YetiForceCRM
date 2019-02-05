154b866a25711e4235d2b2ee157f1a0a4eb10933
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-02-05 16:43:23 +0100

    Fixed copying value after comment's edit

--- public_html/layouts/basic/modules/Vtiger/resources/Detail.js
+++ public_html/layouts/basic/modules/Vtiger/resources/Detail.js
@@ -2038,7 +2038,7 @@ jQuery.Class("Vtiger_Detail_Js", {
 			let commentInfoBlock = $(e.currentTarget).closest('.js-comment-single'),
 				commentInfoContent = commentInfoBlock.find('.js-comment-info'),
 				editCommentBlock = self.getEditCommentBlock();
-			editCommentBlock.find('.js-comment-content').html(commentInfoContent.text());
+			editCommentBlock.find('.js-comment-content').html(commentInfoContent.html());
 			editCommentBlock.find('.js-reason-to-edit').html(commentInfoBlock.find('.js-edit-reason-span').text());
 			commentInfoContent.hide();
 			commentInfoBlock.find('.js-comment-container').hide();
