b9793e21dbb9d3859e845b613cfbad5dfa246cb2
rafalpospiech <r.pospiech@yetiforce.com>
2019-02-13 14:57:28 +0100

    no scheme, just hardcoded values ¯\_(ツ)_/¯

--- app/Exceptions/Csrf.php
+++ app/Exceptions/Csrf.php
@@ -15,18 +15,16 @@ class Csrf extends Security
 	{
 		parent::__construct($message, $code, $previous);
 		\App\Session::init();
-		$log = \App\Db::getInstance('log');
-		$schema = $log->getTableSchema('o_#__csrf');
 		$userName = \App\Session::get('full_user_name');
-		$userName = empty($userName) ? '-' : substr($userName, 0, $schema->getColumn('username')->size | 50);
-		$log->createCommand()
+		$userName = empty($userName) ? '-' : \App\TextParser::textTruncate($userName, 50);
+		\App\Db::getInstance('log')->createCommand()
 			->insert('o_#__csrf', [
 				'username' => $userName,
 				'date' => date('Y-m-d H:i:s'),
-				'ip' => substr(\App\RequestUtil::getRemoteIP(), 0, $schema->getColumn('ip')->size | 100),
-				'referer' => substr(\App\Request::_getServer('HTTP_REFERER', '-'), 0, $schema->getColumn('referer')->size | 300),
-				'url' => substr(\App\RequestUtil::getBrowserInfo()->url, 0, $schema->getColumn('url')->size | 300),
-				'agent' => substr(\App\Request::_getServer('HTTP_USER_AGENT', '-'), 0, $schema->getColumn('agent')->size | 255),
+				'ip' => \App\TextParser::textTruncate(\App\RequestUtil::getRemoteIP(), 100),
+				'referer' => \App\TextParser::textTruncate(\App\Request::_getServer('HTTP_REFERER', '-'), 300),
+				'url' => \App\TextParser::textTruncate(\App\RequestUtil::getBrowserInfo()->url, 300),
+				'agent' => \App\TextParser::textTruncate(\App\Request::_getServer('HTTP_USER_AGENT', '-'), 255),
 			])->execute();
 	}
 }
