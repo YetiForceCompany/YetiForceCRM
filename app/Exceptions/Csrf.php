5b633329ff6cdac190a280f780adae498a0538dd
rafalpospiech <r.pospiech@yetiforce.com>
2019-02-13 15:26:49 +0100

    no dots

--- app/Exceptions/Csrf.php
+++ app/Exceptions/Csrf.php
@@ -16,15 +16,15 @@ class Csrf extends Security
 		parent::__construct($message, $code, $previous);
 		\App\Session::init();
 		$userName = \App\Session::get('full_user_name');
-		$userName = empty($userName) ? '-' : \App\TextParser::textTruncate($userName, 100);
+		$userName = empty($userName) ? '-' : \App\TextParser::textTruncate($userName, 100, false);
 		\App\Db::getInstance('log')->createCommand()
 			->insert('o_#__csrf', [
 				'username' => $userName,
 				'date' => date('Y-m-d H:i:s'),
-				'ip' => \App\TextParser::textTruncate(\App\RequestUtil::getRemoteIP(), 100),
-				'referer' => \App\TextParser::textTruncate(\App\Request::_getServer('HTTP_REFERER', '-'), 300),
-				'url' => \App\TextParser::textTruncate(\App\RequestUtil::getBrowserInfo()->url, 300),
-				'agent' => \App\TextParser::textTruncate(\App\Request::_getServer('HTTP_USER_AGENT', '-'), 255),
+				'ip' => \App\TextParser::textTruncate(\App\RequestUtil::getRemoteIP(), 100, false),
+				'referer' => \App\TextParser::textTruncate(\App\Request::_getServer('HTTP_REFERER', '-'), 300, false),
+				'url' => \App\TextParser::textTruncate(\App\RequestUtil::getBrowserInfo()->url, 300, false),
+				'agent' => \App\TextParser::textTruncate(\App\Request::_getServer('HTTP_USER_AGENT', '-'), 255, false),
 			])->execute();
 	}
 }
