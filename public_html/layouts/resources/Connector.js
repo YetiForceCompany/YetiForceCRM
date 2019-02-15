18fb049936dc34f33d4daa584ab00e7806c248be
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-02-15 10:05:10 +0100

    Corrected js globals initialization [SONAR alert]

--- public_html/layouts/resources/Connector.js
+++ public_html/layouts/resources/Connector.js
@@ -8,7 +8,7 @@
  *************************************************************************************/
 'use strict';
 
-AppConnector = {
+window.AppConnector = {
 
 	/**
 	 * Sends a pjax request (push state +ajax)
