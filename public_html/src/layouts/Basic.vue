d7e1e2c5d661f132fdc00dfddc85b5ab89f2f4f3
rafalpospiech <r.pospiech@yetiforce.com>
2019-02-20 22:47:42 +0100

    branding

---
+++ public_html/src/layouts/Basic.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+
+        <q-toolbar-title>
+          YetiForceCRM
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>YetiForceCRM</q-item-label>
+        <q-item clickable tag="a" target="_blank" href="https://yetiforce.com">
+          <q-item-section avatar>
+            <q-icon name="star" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>YetiForce</q-item-label>
+            <q-item-label caption>YetiForce Home Page</q-item-label>
+          </q-item-section>
+        </q-item>
+     </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
--- public_html/src/pages/Index.vue
+++ public_html/src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    YetiForceCRM
   </q-page>
 </template>
 
