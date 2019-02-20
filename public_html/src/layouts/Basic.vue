974d5e4eede9a8fa41d68d871f5702b6463972a7
rafalpospiech <r.pospiech@yetiforce.com>
2019-02-20 22:17:30 +0100

    cleanup just a little bit

--- public_html/src/layouts/Basic.vue
+++ public_html/src/layouts/Basic.vue
@@ -9,14 +9,12 @@
           @click="leftDrawerOpen = !leftDrawerOpen"
           aria-label="Menu"
         >
-          <q-icon name="menu" />
+          <q-icon name="menu"/>
         </q-btn>
 
         <q-toolbar-title>
-          Quasar App
+          YetiForceCRM
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -29,7 +27,7 @@
         <q-item-label header>Essential Links</q-item-label>
         <q-item clickable tag="a" target="_blank" href="http://v1.quasar-framework.org">
           <q-item-section avatar>
-            <q-icon name="school" />
+            <q-icon name="school"/>
           </q-item-section>
           <q-item-section>
             <q-item-label>Docs</q-item-label>
@@ -38,7 +36,7 @@
         </q-item>
         <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/">
           <q-item-section avatar>
-            <q-icon name="code" />
+            <q-icon name="code"/>
           </q-item-section>
           <q-item-section>
             <q-item-label>Github</q-item-label>
@@ -47,7 +45,7 @@
         </q-item>
         <q-item clickable tag="a" target="_blank" href="http://chat.quasar-framework.org">
           <q-item-section avatar>
-            <q-icon name="chat" />
+            <q-icon name="chat"/>
           </q-item-section>
           <q-item-section>
             <q-item-label>Discord Chat Channel</q-item-label>
@@ -56,7 +54,7 @@
         </q-item>
         <q-item clickable tag="a" target="_blank" href="https://forum.quasar-framework.org">
           <q-item-section avatar>
-            <q-icon name="record_voice_over" />
+            <q-icon name="record_voice_over"/>
           </q-item-section>
           <q-item-section>
             <q-item-label>Forum</q-item-label>
@@ -65,7 +63,7 @@
         </q-item>
         <q-item clickable tag="a" target="_blank" href="https://twitter.com/quasarframework">
           <q-item-section avatar>
-            <q-icon name="rss_feed" />
+            <q-icon name="rss_feed"/>
           </q-item-section>
           <q-item-section>
             <q-item-label>Twitter</q-item-label>
@@ -76,25 +74,25 @@
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+      <router-view/>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import { openURL } from 'quasar'
+  import {openURL} from 'quasar'
 
-export default {
-  name: 'MyLayout',
-  data () {
-    return {
-      leftDrawerOpen: this.$q.platform.is.desktop
+  export default {
+    name: 'MyLayout',
+    data() {
+      return {
+        leftDrawerOpen: this.$q.platform.is.desktop
+      }
+    },
+    methods: {
+      openURL
     }
-  },
-  methods: {
-    openURL
   }
-}
 </script>
 
 <style>
