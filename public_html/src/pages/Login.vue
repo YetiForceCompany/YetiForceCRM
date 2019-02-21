d06ce738438291913b269db1c84d6a3305202dc0
rafalpospiech <r.pospiech@yetiforce.com>
2019-02-21 19:14:24 +0100

    index page bugfix, icons removed

--- public_html/src/pages/Login.vue
+++ public_html/src/pages/Login.vue
@@ -2,20 +2,20 @@
   <q-page class="flex flex-center column">
     <form @submit.prevent.stop="onSubmit" class="q-gutter-md">
       <q-input
-              ref="user"
-              v-model="user"
-              label="User Name"
-              lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Please type something']"
+        ref="user"
+        v-model="user"
+        label="User Name"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
 
       <q-input
-              ref="password"
-              type="password"
-              v-model="password"
-              label="Password"
-              lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Please type something']"
+        ref="password"
+        type="password"
+        v-model="password"
+        label="Password"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
       <div>
         <q-btn label="Submit" type="submit" color="primary"/>
@@ -28,28 +28,28 @@
 </style>
 
 <script>
-  export default {
-    name: 'Login',
-    data() {
-      return {
-        user: '',
-        password: ''
-      }
-    },
-    methods: {
-      onSubmit() {
-        this.$refs.user.validate()
-        this.$refs.password.validate()
+export default {
+  name: "Login",
+  data() {
+    return {
+      user: "",
+      password: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.user.validate();
+      this.$refs.password.validate();
 
-        if (this.$refs.user.hasError || this.$refs.password.hasError) {
-          this.formHasError = true
-        } else {
-          this.$store.commit('login/isLoggedIn', true)
-          let user = this.user
-          let password = this.password
-          this.$router.push('/')
-        }
+      if (this.$refs.user.hasError || this.$refs.password.hasError) {
+        this.formHasError = true;
+      } else {
+        this.$store.commit("login/isLoggedIn", true);
+        let user = this.user;
+        let password = this.password;
+        this.$router.push("/");
       }
     }
   }
+};
 </script>
