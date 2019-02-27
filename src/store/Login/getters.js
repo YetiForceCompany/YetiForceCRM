f5bc43ec67b3dd1a238b74d9e081d37b8bae02eb
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-02-27 10:19:17 +0100

    Added licenses to edited files

--- src/store/Login/getters.js
+++ src/store/Login/getters.js
@@ -1,3 +1,1 @@
-export function isAuthenticated(state) {
-  return state.tokenId !== null
-}
+/* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
