d100a7a25f055bd6dceab3d476df0ea4c0479a79
Adrian Kon <a.kon@yetiforce.com>
2019-02-27 08:30:42 +0100

    Fixed comments, improved checking is currency field exists

--- app/QueryField/CurrencyInventoryField.php
+++ app/QueryField/CurrencyInventoryField.php
@@ -1,13 +1,19 @@
 <?php
 
-namespace App\QueryField;
-
 /**
- * Double Query Field Class.
+ * CurrencyInventory Query Field Class.
+ *
+ * @package   App
  *
  * @copyright YetiForce Sp. z o.o
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Adrian Koń <a.kon@yetiforce.com>
+ */
+
+namespace App\QueryField;
+
+/**
+ * Class CurrencyInventoryField.
  */
 class CurrencyInventoryField extends DoubleField
 {
