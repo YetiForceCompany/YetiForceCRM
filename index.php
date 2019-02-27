d341cee269d45b9b783f35a8c1a5e26a54bf4e8e
rafalpospiech <r.pospiech@yetiforce.com>
2019-02-27 16:26:49 +0100

    updated comment

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
-/*
- * This file is part of PHP CS Fixer.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
+/* +**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.1
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * Contributor(s): YetiForce.com
+ * ********************************************************************************** */
 
 require_once 'include/ConfigUtils.php';
 \App\Process::$startTime = microtime(true);
