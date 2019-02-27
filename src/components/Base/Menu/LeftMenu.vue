076252d60db1cb633475aa92e10dc0a4a02b0da1
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-02-27 17:36:00 +0100

    Added footer, improved header and menu layout

--- src/components/Base/Menu/LeftMenu.vue
+++ src/components/Base/Menu/LeftMenu.vue
@@ -1,19 +1,40 @@
 <!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
 <template>
   <q-list>
-    <q-item-label header>YetiForceCRM</q-item-label>
+    <q-item header class="bg-black text-white q-toolbar q-px-md">
+      <q-item-section avatar>
+        <q-icon name="person" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ userName }}</q-item-label>
+      </q-item-section>
+    </q-item>
     <component
       v-for="position in positions"
       :is="position.component"
       :key="position.key"
       v-bind="position.props"
     ></component>
+    <q-item header bordered class="bg-grey-12 border-right text-black q-toolbar q-px-md absolute-bottom">
+      <q-item-section avatar>
+        <q-icon name="domain" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ companyName }}</q-item-label>
+      </q-item-section>
+    </q-item>
   </q-list>
 </template>
 
 <script>
 import RoutePush from './Positions/RoutePush'
 export default {
+  data() {
+    return {
+      userName: 'User Name',
+      companyName: 'Company Name'
+    }
+  },
   name: 'BaseLeftMenu',
   components: {
     RoutePush
@@ -25,3 +46,8 @@ export default {
   }
 }
 </script>
+<style>
+.border-right {
+  border-right: solid 1px #bdbdbd;
+}
+</style>
--- src/layouts/Basic.vue
+++ src/layouts/Basic.vue
@@ -1,22 +1,38 @@
-<!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header>
+  <q-layout view="lHr Lpr lff">
+    <q-header elevated class="bg-white text-black">
       <q-toolbar>
-        <q-btn flat dense round @click="leftDrawerOpen = !leftDrawerOpen" aria-label="Menu">
-          <q-icon name="menu"/>
-        </q-btn>
-
+        <q-btn dense flat round icon="menu" @click="leftDrawerOpen = !leftDrawerOpen" />
         <q-toolbar-title>YetiForceCRM</q-toolbar-title>
       </q-toolbar>
     </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" bordered content-class="bg-grey-2">
-      <left-menu/>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      content-class="bg-blue-grey-10 text-white"
+      :mini="$q.platform.is.mobile ? !leftDrawerOpen : miniState"
+      @mouseover="miniState = false"
+      @mouseout="miniState = true"
+      :width="200"
+      :breakpoint="500"
+      show-if-above
+    >
+      <left-menu />
     </q-drawer>
     <q-page-container>
-      <router-view/>
+      <router-view />
     </q-page-container>
+    <q-footer class="bg-grey-12 text-black">
+      <q-toolbar>
+        <div class="q-px-md q-gutter-sm">
+          <q-breadcrumbs>
+            <q-breadcrumbs-el icon="home" to="/" />
+            <q-breadcrumbs-el label="Docs" icon="widgets" to="/docs" />
+            <q-breadcrumbs-el label="Breadcrumbs" icon="navigation" to="/vue-components/breadcrumbs" />
+            <q-breadcrumbs-el label="Build" icon="build" />
+          </q-breadcrumbs>
+        </div>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
@@ -31,7 +47,8 @@ export default {
   },
   data() {
     return {
-      leftDrawerOpen: this.$q.platform.is.desktop
+      leftDrawerOpen: this.$q.platform.is.desktop,
+      miniState: true
     }
   },
   methods: {
