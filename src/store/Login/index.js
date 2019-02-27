b99a2ad47e72021533956bdaea130bfe756e0ff1
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-02-27 14:05:02 +0100

    Corrected login mutations

--- src/store/Login/index.js
+++ src/store/Login/index.js
@@ -1,7 +1,7 @@
 /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
 import state from './state'
 import * as getters from './getters'
-import * as mutations from './mutations'
+import mutations from './mutations'
 import * as actions from './actions'
 
 export default {
