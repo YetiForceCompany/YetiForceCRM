3106c5737228d676965b6dc81674ba6c1229f310
areksolek <a.solek@yetiforce.com>
2019-03-01 13:53:39 +0100

    corrected commit

--- modules/Users/uitypes/String.php
+++ modules/Users/uitypes/String.php
@@ -20,7 +20,7 @@ class Users_String_UIType extends Vtiger_Base_UIType
 		if ($this->validate || empty($value)) {
 			return;
 		}
-		if ($this->getFieldModel() > getUIType() === 106) {
+		if ($this->getFieldModel()->getUIType() === 106) {
 			if (!preg_match('/^[a-zA-Z0-9_.@]{3,32}$/', $value)) {
 				throw new \App\Exceptions\Security('ERR_ILLEGAL_FIELD_VALUE||' . $this->getFieldModel()->getFieldName() . '||' . $this->getFieldModel()->getModuleName() . '||' . $value, 406);
 			}
