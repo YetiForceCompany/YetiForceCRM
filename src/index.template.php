355f9e9102f8f69b2b01214863ebc2468a290155
rafalpospiech <r.pospiech@yetiforce.com>
2019-03-04 17:20:28 +0100

    comments update

--- src/index.template.php
+++ src/index.template.php
@@ -1,12 +1,11 @@
 <?php
-/*
+/**
  * Index file.
  *
  * @copyright YetiForce Sp. z o.o
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Rafal Pospiech <r.pospiech@yetiforce.com>
  */
-
 $config = [
 	'baseURL' => \AppConfig::main('site_URL'),
 	'publicDir' => '/dist',
