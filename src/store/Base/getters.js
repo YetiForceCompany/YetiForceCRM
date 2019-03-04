b39fcbca4951b90d9480d747ecf0cd720331cf45
rafalpospiech <r.pospiech@yetiforce.com>
2019-03-04 14:33:40 +0100

    comment added

--- src/store/Base/getters.js
+++ src/store/Base/getters.js
@@ -1,3 +1,4 @@
+/* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
 import getters from '../../store/getters.js'
 import Objects from '../../utilities/Objects.js'
 
