7e40a2f6ce3f5f99d0c6ac811c0ecdb68d24278c
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-03-05 16:13:46 +0100

    Made i18n console alerts silent

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -6,7 +6,9 @@ export default async ({ app, Vue }) => {
   Vue.use(VueI18n)
   // Set i18n instance on app
   app.i18n = new VueI18n({
+    locale: '_Base',
     fallbackLocale: '_Base',
+    silentTranslationWarn: true,
     messages
   })
 }
