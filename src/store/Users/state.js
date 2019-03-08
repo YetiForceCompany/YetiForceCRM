d3c0c4c3c9985b3a9b599e7bcde9509795142ef2
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-03-08 11:59:45 +0100

    Changed remote states to null

--- src/store/Users/state.js
+++ src/store/Users/state.js
@@ -1,12 +1,12 @@
 /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
 
 export default {
-  isLoggedIn: true,
-  isBlockedIp: false,
-  message: '',
-  messageType: '',
-  loginPageRememberCredentials: true,
-  forgotPassword: true,
-  languageSelection: true,
-  layoutSelection: true
+  isLoggedIn: null,
+  isBlockedIp: null,
+  message: null,
+  messageType: null,
+  loginPageRememberCredentials: null,
+  forgotPassword: null,
+  languageSelection: null,
+  layoutSelection: null
 }
