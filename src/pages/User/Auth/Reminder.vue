eac93c36335744abddb90b758864d6ec58cb6647
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-03-08 10:49:51 +0100

    Corrected login and remind form

--- src/pages/User/Auth/Reminder.vue
+++ src/pages/User/Auth/Reminder.vue
@@ -1,12 +1,28 @@
 <!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
 <template v-if="$store.state.User.forgotPassword && showReminderForm">
-  <form class="col q-gutter-md q-mx-lg" action="index.php?module=Users&action=ForgotPassword" method="POST">
-    <q-input type="text" v-model="reminderUser" :title="$t('LBL_USER')" :label="$t('LBL_USER')" autocomplete="off">
+  <form class="col q-gutter-md q-mx-lg" @submit.prevent.stop="onSubmit">
+    <q-input
+      type="text"
+      ref="reminderUser"
+      v-model="reminderUser"
+      :label="$t('LBL_USER')"
+      autocomplete="off"
+      lazy-rules
+      :rules="[val => (val && val.length > 0) || 'Please type something']"
+    >
       <template v-slot:prepend>
         <q-icon name="person" />
       </template>
     </q-input>
-    <q-input type="text" v-model="reminderEmail" autocomplete="off" :title="$t('LBL_EMAIL')" :label="$t('LBL_EMAIL')">
+    <q-input
+      type="text"
+      ref="reminderEmail"
+      v-model="reminderEmail"
+      autocomplete="off"
+      :label="$t('LBL_EMAIL')"
+      lazy-rules
+      :rules="[val => (val && val.length > 0) || 'Please type something']"
+    >
       <template v-slot:prepend>
         <q-icon name="mail_outline" />
       </template>
@@ -16,6 +32,7 @@
   </form>
 </template>
 <script>
+import actions from 'src/store/actions.js'
 /**
  * @vue-data     {String} reminderEmail
  * @vue-data     {String} reminderUser
@@ -24,8 +41,22 @@ export default {
   name: 'Reminder',
   data() {
     return {
-      reminderEmail: '',
-      reminderUser: ''
+      reminderUser: '',
+      reminderEmail: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.reminderUser.validate()
+      this.$refs.reminderEmail.validate()
+      if (this.$refs.reminderUser.hasError || this.$refs.reminderEmail.hasError) {
+        this.formHasError = true
+      } else {
+        this.$store.dispatch(actions.User.remind, {
+          reminderUser: this.reminderUser,
+          reminderEmail: this.reminderEmail
+        })
+      }
     }
   },
   mounted() {
--- src/store/Url/state.js
+++ src/store/Url/state.js
@@ -1,6 +1,7 @@
 /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
 export default {
   User: {
-    login: 'login.php'
+    login: 'login.php',
+    remind: 'login.php&mode=remind'
   }
 }
