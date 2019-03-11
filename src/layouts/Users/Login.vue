fa889f4ef68dab296deac9ff279dc163d087588b
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-03-11 12:10:37 +0100

    Corrected login img style

--- src/layouts/Users/Login.vue
+++ src/layouts/Users/Login.vue
@@ -21,9 +21,12 @@
   </q-layout>
 </template>
 
-<style>
+<style scoped>
 .card-shadow {
   box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2), 0 2px 2px rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12);
+}
+img {
+  width: 100px;
 }
 </style>
 
