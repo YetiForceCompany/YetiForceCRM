7a22cd1f54f4a8bce7b5370d4c9a3925dfdd8b91
Adrian Kon <a.kon@yetiforce.com>
2019-03-11 11:35:49 +0100

    Change query to query generator

--- modules/ProjectTask/models/Module.php
+++ modules/ProjectTask/models/Module.php
@@ -26,26 +26,24 @@ class ProjectTask_Module_Model extends Vtiger_Module_Model
 		if (!$user) {
 			$user = \App\User::getCurrentUserId();
 		}
-		$query->select(['vtiger_crmentity.crmid', 'vtiger_crmentity.smownerid', 'vtiger_crmentity.setype', 'vtiger_projecttask.*'])
-			->from('vtiger_projecttask')
-			->innerJoin('vtiger_crmentity', 'vtiger_crmentity.crmid = vtiger_projecttask.projecttaskid')
-			->where(['vtiger_crmentity.deleted' => 0, 'vtiger_projecttask.projecttaskstatus' => $params['projecttaskstatus']]);
-
+		$queryGenerator = new App\QueryGenerator('ProjectTask');
+		$queryGenerator->setFields(['id', 'projecttaskname', 'projecttaskprogress', 'projectid', 'projectmilestoneid', 'targetenddate', 'enddate']);
+		$queryGenerator->addNativeCondition(['projecttaskstatus' => $params['projecttaskstatus']]);
 		if (isset($params['projecttaskpriority'])) {
-			$query->andWhere(['vtiger_projecttask.projecttaskpriority' => $params['projecttaskpriority']]);
+			$queryGenerator->addNativeCondition(['projecttaskpriority' => $params['projecttaskpriority']]);
 		}
-		\App\PrivilegeQuery::getConditions($query, 'ProjectTask');
-		if ($user !== 'all' && !empty($user)) {
-			$subQuery = (new \App\Db\Query())->select(['crmid'])->from('u_#__crmentity_showners')->innerJoin('vtiger_projecttask', 'u_#__crmentity_showners.crmid=vtiger_projecttask.projecttaskid')->where(['userid' => $user])->distinct('crmid');
-			$query->andWhere(['or', ['vtiger_crmentity.smownerid' => $user], ['vtiger_crmentity.crmid' => $subQuery]]);
+		if ('all' !== $user && !empty($user)) {
+			$queryGenerator->addCondition('shownerid', $user, 'e', false);
+			$queryGenerator->addCondition('assigned_user_id', $user, 'e', false);
 		}
-		$query->limit($pagingModel->getPageLimit() + 1)->offset($pagingModel->getStartIndex());
-		$dataReader = $query->createCommand()->query();
+		$queryGenerator->setLimit($pagingModel->getPageLimit() + 1);
+		$queryGenerator->setOffset($pagingModel->getStartIndex());
+		$dataReader = $queryGenerator->createQuery()->createCommand()->query();
 		$projectTasks = [];
 		while ($row = $dataReader->read()) {
 			$model = Vtiger_Record_Model::getCleanInstance('ProjectTask');
 			$model->setData($row);
-			$model->setId($row['crmid']);
+			$model->setId($row['id']);
 			$projectTasks[] = $model;
 		}
 		$pagingModel->calculatePageRange($dataReader->count());
