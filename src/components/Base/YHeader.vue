1d73092a3c79caf29c7b1f0967b4a5fc0bc88333
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-03-13 14:24:16 +0100

    Added y-header

---
+++ src/components/Base/YHeader.vue
@@ -0,0 +1,42 @@
+<!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
+<template>
+  <q-header elevated class="bg-white text-muted">
+    <q-toolbar>
+      <div class="q-pl-md" style="min-width: 200px">
+        <q-select v-model="searchModule" :options="searchModules" placeholder="Placeholder" dense />
+      </div>
+      <q-input class="q-pl-lg" v-model="searchText" placeholder="Placeholder" dense>
+        <template v-slot:after>
+          <q-btn round :size="iconSize" flat icon="search" dense />
+          <q-btn round :size="iconSize" flat icon="mdi-format-text" dense />
+          <q-btn round :size="iconSize" flat icon="mdi-feature-search-outline" dense />
+        </template>
+      </q-input>
+      <div class="q-ml-auto">
+        <q-btn round :size="iconSize" flat icon="mdi-plus" />
+        <q-btn round :size="iconSize" flat icon="mdi-email-outline" />
+        <q-btn round :size="iconSize" flat icon="mdi-bell-ring-outline" />
+        <q-btn round :size="iconSize" flat icon="mdi-calendar-multiselect" />
+        <q-btn round :size="iconSize" flat icon="mdi-history" />
+        <q-btn round :size="iconSize" flat icon="mdi-settings-outline" />
+        <q-btn round :size="iconSize" flat icon="mdi-power-standby" />
+      </div>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+export default {
+  name: 'YHeader',
+  data() {
+    return {
+      searchModule: 'All records',
+      searchModules: ['All records', 'acc', 'con'],
+      searchText: '',
+      iconSize: '.75rem'
+    }
+  }
+}
+</script>
+
+<style module lang="stylus"></style>
