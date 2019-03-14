47817be79b3e43b93f2f15662b655c24dfa656c2
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-03-14 15:04:56 +0100

    Improved footer RWD, cleaned code
    # Conflicts:
    #	src/components/Base/YFooter.vue

--- src/components/Base/YBreadcrumbs.vue
+++ src/components/Base/YBreadcrumbs.vue
@@ -1,13 +1,11 @@
 <!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
-<template>
-  <q-toolbar>
-    <q-breadcrumbs active-color="info">
-      <q-breadcrumbs-el icon="home" to="/" />
-      <q-breadcrumbs-el label="Docs" icon="widgets" to="/docs" />
-      <q-breadcrumbs-el label="Breadcrumbs" icon="navigation" to="/vue-components/breadcrumbs" />
-      <q-breadcrumbs-el label="Build" icon="build" />
-    </q-breadcrumbs>
-  </q-toolbar>
+<template functional>
+  <q-breadcrumbs active-color="info" :class="[$style.breadcrumbs, '']">
+    <q-breadcrumbs-el icon="home" to="/" />
+    <q-breadcrumbs-el label="Docs" icon="widgets" to="/docs" />
+    <q-breadcrumbs-el label="Breadcrumbs" icon="navigation" to="/vue-components/breadcrumbs" />
+    <q-breadcrumbs-el label="Build" icon="build" />
+  </q-breadcrumbs>
 </template>
 
 <script>
@@ -16,4 +14,8 @@ export default {
 }
 </script>
 
-<style module></style>
+<style module>
+.breadcrumbs * {
+  flex-wrap: nowrap;
+}
+</style>
--- src/components/Base/YFooter.vue
+++ src/components/Base/YFooter.vue
@@ -1,15 +1,16 @@
 <!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
-<template>
-  <q-footer elevated class="bg-blue-grey-10 text-blue-grey-11">
-    <q-toolbar class="q-pl-none">
-      <q-toolbar-title :class="[$style.footerLeft, 'flex flex-center q-px-none']">
-        <q-avatar>
-          <img :src="env.publicDir + '/statics/Logo/white_logo_yetiforce.png'" alt>
-        </q-avatar>
-      </q-toolbar-title>
-      <q-separator class="q-my-xs" dark vertical />
-      <y-breadcrumbs />
-      <y-yeti-info :class="[$style.footerRight, 'q-pl-auto']" />
+<template functional>
+  <q-footer elevated class="bg-blue-grey-10 text-blue-grey-11 row no-wrap">
+    <div :class="[$style.footerLeft, 'flex flex-center gt-xs q-px-none']">
+      <q-avatar size="32px" class="q-mx-auto">
+        <img :src="env.publicDir + '/statics/Logo/white_logo_yetiforce.png'" alt="yetiforce logo" />
+      </q-avatar>
+      <q-separator dark vertical class="q-my-xs" style="min-height: unset" />
+    </div>
+    <q-toolbar>
+      <div class="flex wrap full-width items-center">
+        <y-breadcrumbs />
+        <y-yeti-info class="q-ml-auto" />
       </div>
     </q-toolbar>
   </q-footer>
@@ -34,22 +35,15 @@ export default {
   },
   computed: {
     ...mapGetters({
-      env: getters.Base.Env.all
+      env: getters.Env.all
     })
   }
 }
 </script>
 
 <style module lang="stylus">
-@import '~quasar-variables';
-
 .footerLeft {
   min-width: 57px;
-}
-
-.footerRight {
-  a {
-    color: $blue-grey-11;
-  }
+  max-width: 57px;
 }
 </style>
