468661e8a8a08726fdd7e1ea5c725619a69c6a7c
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2019-03-14 09:58:17 +0100

    Added minor improvement

--- src/index.template.php
+++ src/index.template.php
@@ -7,20 +7,21 @@
  * @author    Rafal Pospiech <r.pospiech@yetiforce.com>
  * @author    Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
  * @author    Tomasz Poradzewski <t.poradzewski@yetiforce.com>
+ * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
 $lang = \App\Language::getLanguage();
-$env = [
-    'Env' => [
-        'baseURL' => \App\Config::main('site_URL'),
-        'publicDir' => '/dist',
-        'routerMode' => 'hash',
-    ],
-    'Users' => ['isLoggedIn' => true],
-    'Language' => [
-        'lang' => $lang,
-        'translations' => \App\Language::getLanguageData($lang),
-    ],
-];
+$env = App\Json::encode([
+	'Env' => [
+		'baseURL' => \App\Config::main('site_URL'),
+		'publicDir' => '/dist',
+		'routerMode' => 'hash',
+	],
+	'Users' => ['isLoggedIn' => true],
+	'Language' => [
+		'lang' => $lang,
+		'translations' => \App\Language::getLanguageData($lang),
+	],
+]);
 
 ?>
 <!DOCTYPE html>
@@ -30,16 +31,14 @@ $env = [
   <title><%= htmlWebpackPlugin.options.productName %></title>
 
   <meta charset="utf-8">
-  <meta name="description" content="<%= htmlWebpackPlugin.options.productDescription %>">
   <meta name="format-detection" content="telephone=no">
   <meta name="msapplication-tap-highlight" content="no">
   <meta name="viewport"
     content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width<% if (htmlWebpackPlugin.options.ctx.mode.cordova) { %>, viewport-fit=cover<% } %>">
 
-  <link rel="icon" href="/dist/statics/quasar-logo.png" type="image/x-icon">
   <link rel="icon" type="image/png" sizes="32x32" href="/dist/statics/icons/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/dist/statics/icons/favicon-16x16.png">
-  <script>window.env = <?php echo json_encode($env); ?>;</script>
+  <script>window.env = <?php echo $env; ?>;</script>
   <script src="<%= htmlWebpackPlugin.options.modulesFile %>"></script>
 </head>
 
