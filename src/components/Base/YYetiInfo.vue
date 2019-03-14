ba9a50e9f86dfbe10168bb3421f41ef256c7e201
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-03-14 15:04:56 +0100

    Improved footer RWD, cleaned code
    # Conflicts:
    #	src/components/Base/YFooter.vue

--- src/components/Base/YYetiInfo.vue
+++ src/components/Base/YYetiInfo.vue
@@ -4,28 +4,32 @@
     <div class="text-right">&copy; YetiForce.com All rights reserved.</div>
     <q-separator dark />
     <div class="flex flex no-wrap q-py-xs">
-      <a class="q-link" href="https://www.linkedin.com/groups/8177576" rel="noreferrer noopener">
+      <a class="q-link text-inherit" href="https://www.linkedin.com/groups/8177576" rel="noreferrer noopener">
         <q-icon size="16px" name="mdi-linkedin" title="Linkedin"></q-icon>
       </a>
       <q-separator class="q-mx-md" dark vertical />
-      <a class="q-link" href="https://twitter.com/YetiForceEN" rel="noreferrer noopener">
+      <a class="q-link text-inherit" href="https://twitter.com/YetiForceEN" rel="noreferrer noopener">
         <q-icon size="16px" name="mdi-twitter" title="Twitter"></q-icon>
       </a>
       <q-separator class="q-mx-md" dark vertical />
-      <a class="q-link" href="https://www.facebook.com/YetiForce-CRM-158646854306054/" rel="noreferrer noopener">
+      <a
+        class="q-link text-inherit"
+        href="https://www.facebook.com/YetiForce-CRM-158646854306054/"
+        rel="noreferrer noopener"
+      >
         <q-icon size="16px" name="mdi-facebook" title="Facebook"></q-icon>
       </a>
       <q-separator class="q-mx-md" dark vertical />
-      <a class="q-link" href="https://github.com/YetiForceCompany/YetiForceCRM" rel="noreferrer noopener">
+      <a class="q-link text-inherit" href="https://github.com/YetiForceCompany/YetiForceCRM" rel="noreferrer noopener">
         <q-icon size="16px" name="mdi-github-circle" title="Github"></q-icon>
       </a>
       <q-separator class="q-mx-md" dark vertical />
 
-      <a class="q-link" href="https://yetiforce.shop" rel="noreferrer noopener">
+      <a class="q-link text-inherit" href="https://yetiforce.shop" rel="noreferrer noopener">
         <q-icon size="18px" name="mdi-cart-outline" title="yetiforce.shop"></q-icon>
       </a>
       <q-separator class="q-mx-md" dark vertical />
-      <a class="q-link" href="#" role="button">
+      <a class="q-link text-inherit" href="#" role="button">
         <q-icon size="18px" name="mdi-information-outline" title="YetiForceCRM"></q-icon>
       </a>
     </div>
@@ -38,4 +42,4 @@ export default {
 }
 </script>
 
-<style module></style>
+<style module lang="stylus"></style>
