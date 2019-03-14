6abad743ac8425bf3187861b20464a364e74d32d
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-03-14 10:11:30 +0100

    Splited footer to breadcrambs and info components

---
+++ src/components/Base/YBreadcrumbs.vue
@@ -0,0 +1,19 @@
+<!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
+<template>
+  <q-toolbar>
+    <q-breadcrumbs active-color="info">
+      <q-breadcrumbs-el icon="home" to="/" />
+      <q-breadcrumbs-el label="Docs" icon="widgets" to="/docs" />
+      <q-breadcrumbs-el label="Breadcrumbs" icon="navigation" to="/vue-components/breadcrumbs" />
+      <q-breadcrumbs-el label="Build" icon="build" />
+    </q-breadcrumbs>
+  </q-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'YBreadcrumbs'
+}
+</script>
+
+<style module></style>
---
+++ src/components/Base/YYetiInfo.vue
@@ -0,0 +1,41 @@
+<!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
+<template functional>
+  <div>
+    <div class="text-right">&copy; YetiForce.com All rights reserved.</div>
+    <q-separator dark />
+    <div class="flex flex no-wrap q-py-xs">
+      <a class="q-link" href="https://www.linkedin.com/groups/8177576" rel="noreferrer noopener">
+        <q-icon size="16px" name="mdi-linkedin" title="Linkedin"></q-icon>
+      </a>
+      <q-separator class="q-mx-md" dark vertical />
+      <a class="q-link" href="https://twitter.com/YetiForceEN" rel="noreferrer noopener">
+        <q-icon size="16px" name="mdi-twitter" title="Twitter"></q-icon>
+      </a>
+      <q-separator class="q-mx-md" dark vertical />
+      <a class="q-link" href="https://www.facebook.com/YetiForce-CRM-158646854306054/" rel="noreferrer noopener">
+        <q-icon size="16px" name="mdi-facebook" title="Facebook"></q-icon>
+      </a>
+      <q-separator class="q-mx-md" dark vertical />
+      <a class="q-link" href="https://github.com/YetiForceCompany/YetiForceCRM" rel="noreferrer noopener">
+        <q-icon size="16px" name="mdi-github-circle" title="Github"></q-icon>
+      </a>
+      <q-separator class="q-mx-md" dark vertical />
+
+      <a class="q-link" href="https://yetiforce.shop" rel="noreferrer noopener">
+        <q-icon size="18px" name="mdi-cart-outline" title="yetiforce.shop"></q-icon>
+      </a>
+      <q-separator class="q-mx-md" dark vertical />
+      <a class="q-link" href="#" role="button">
+        <q-icon size="18px" name="mdi-information-outline" title="YetiForceCRM"></q-icon>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'YYetiInfo'
+}
+</script>
+
+<style module></style>
