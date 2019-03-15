7247cdf893cfec34fc9497a1468b17867c70fe0e
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-03-15 11:56:59 +0100

    Splited YHeader.vue into YGlobalActions.vue and YGlobalSearch.vue

--- src/components/Base/YHeader.vue
+++ src/components/Base/YHeader.vue
@@ -2,47 +2,23 @@
 <template>
   <q-header elevated class="bg-white text-muted">
     <q-toolbar>
-      <q-btn class="lt-md" round :size="iconSize" flat icon="search" dense />
-      <div class="flex gt-sm">
-        <div :class="['q-pl-md', $style.headerField]">
-          <q-select v-model="searchModule" :options="searchModules" placeholder="Placeholder" dense />
-        </div>
-        <q-input :class="['q-pl-lg', $style.headerField]" v-model="searchText" placeholder="Placeholder" dense>
-          <template v-slot:after>
-            <q-btn round :size="iconSize" flat icon="mdi-format-text" dense />
-            <q-btn round :size="iconSize" flat icon="mdi-feature-search-outline" dense />
-          </template>
-        </q-input>
-      </div>
-      <div class="q-ml-auto">
-        <q-btn round :size="iconSize" flat icon="mdi-plus" />
-        <q-btn round :size="iconSize" flat icon="mdi-email-outline" />
-        <q-btn round :size="iconSize" flat icon="mdi-bell-ring-outline" />
-        <q-btn round :size="iconSize" flat icon="mdi-calendar-multiselect" />
-        <q-btn round :size="iconSize" flat icon="mdi-history" />
-        <q-btn round :size="iconSize" flat icon="mdi-settings-outline" />
-        <q-btn round :size="iconSize" flat icon="mdi-power-standby" />
-      </div>
+      <y-global-search />
+      <y-global-actions class="q-ml-auto" />
     </q-toolbar>
   </q-header>
 </template>
-
 <script>
+import YGlobalSearch from 'components/Base/YGlobalSearch.vue'
+import YGlobalActions from 'components/Base/YGlobalActions.vue'
 export default {
   name: 'YHeader',
+  components: { YGlobalSearch, YGlobalActions },
   data() {
     return {
-      searchModule: 'All records',
-      searchModules: ['All records', 'acc', 'con'],
-      searchText: '',
       iconSize: '.75rem'
     }
   }
 }
 </script>
 
-<style module lang="stylus">
-.headerField {
-  min-width: 200px;
-}
-</style>
+<style module lang="stylus"></style>
