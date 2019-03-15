7b3a58355f22d048a266617efd268197f2fc3a02
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2019-03-15 08:18:56 +0100

    Update lib_roundcube version

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'appVersion' => '5.1.5',
-	'patchVersion' => '2019.03.14',
-	'lib_roundcube' => '0.0.68'
+	'appVersion' => '5.1.6',
+	'patchVersion' => '2019.03.15',
+	'lib_roundcube' => '0.0.69'
 ];
