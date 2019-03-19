38b6548a2418b624b57a9a0e7027df40c06c7419
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2019-03-19 08:44:12 +0100

    Changed license

--- modules/Users/actions/Login.php
+++ modules/Users/actions/Login.php
@@ -1,14 +1,17 @@
 <?php
-/* +**********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.1
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
- * Contributor(s): YetiForce.com
- * ********************************************************************************** */
+/**
+ * Basic login action.
+ *
+ * @package   Action
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
 
+/**
+ * Login action class.
+ */
 class Users_Login_Action extends \App\Controller\Action
 {
 	/**
