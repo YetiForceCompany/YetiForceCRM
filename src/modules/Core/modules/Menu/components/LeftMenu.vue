68c0bba69e8437df5d717a4bf842e3cba24b93a8
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-03-20 18:53:27 +0100

    Added menu and footer improvements

--- src/modules/Core/modules/Menu/components/LeftMenu.vue
+++ src/modules/Core/modules/Menu/components/LeftMenu.vue
@@ -1,14 +1,6 @@
 <!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
 <template>
   <q-list>
-    <q-item header class="bg-black text-white q-toolbar q-px-md">
-      <q-item-section avatar>
-        <q-icon name="mdi-account" />
-      </q-item-section>
-      <q-item-section>
-        <q-item-label>{{ userName }}</q-item-label>
-      </q-item-section>
-    </q-item>
     <menu-item v-for="item in items" :item="item" :key="item.id" />
   </q-list>
 </template>
