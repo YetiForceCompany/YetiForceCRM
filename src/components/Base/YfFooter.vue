886c716f1f17677d2585182e2223e02bd1888069
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-03-21 08:35:51 +0100

    Added hook wrappers to edited components

--- src/components/Base/YfFooter.vue
+++ src/components/Base/YfFooter.vue
@@ -1,13 +1,15 @@
 <!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
 <template>
-  <q-footer elevated class="bg-blue-grey-10 text-blue-grey-11 row no-wrap">
-    <q-toolbar>
-      <div class="flex wrap full-width items-center justify-between">
-        <yf-breadcrumbs />
-        <yf-copyright />
-      </div>
-    </q-toolbar>
-  </q-footer>
+  <hook-wrapper>
+    <q-footer elevated class="bg-blue-grey-10 text-blue-grey-11 row no-wrap">
+      <q-toolbar>
+        <div class="flex wrap full-width items-center justify-between">
+          <yf-breadcrumbs />
+          <yf-copyright />
+        </div>
+      </q-toolbar>
+    </q-footer>
+  </hook-wrapper>
 </template>
 
 <script>
--- src/components/Base/YfGlobalSearch.vue
+++ src/components/Base/YfGlobalSearch.vue
@@ -1,20 +1,22 @@
 <!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
 <template>
-  <div>
-    <q-btn class="lt-md" round :size="iconSize" flat icon="mdi-magnify" dense />
-    <div class="flex gt-sm">
-      <div :class="['q-pl-sm', $style.headerField]">
-        <q-select v-model="searchModule" :options="searchModules" placeholder="Placeholder" dense />
+  <hook-wrapper>
+    <div>
+      <q-btn class="lt-md" round :size="iconSize" flat icon="mdi-magnify" dense />
+      <div class="flex gt-sm">
+        <div :class="['q-pl-sm', $style.headerField]">
+          <q-select v-model="searchModule" :options="searchModules" placeholder="Placeholder" dense />
+        </div>
+        <q-input :class="['q-pl-lg', $style.headerField]" v-model="searchText" placeholder="Placeholder" dense>
+          <template v-slot:after>
+            <q-btn round :size="iconSize" flat icon="mdi-magnify" dense />
+            <q-btn round :size="iconSize" flat icon="mdi-format-text" dense />
+            <q-btn round :size="iconSize" flat icon="mdi-feature-search-outline" dense />
+          </template>
+        </q-input>
       </div>
-      <q-input :class="['q-pl-lg', $style.headerField]" v-model="searchText" placeholder="Placeholder" dense>
-        <template v-slot:after>
-          <q-btn round :size="iconSize" flat icon="mdi-magnify" dense />
-          <q-btn round :size="iconSize" flat icon="mdi-format-text" dense />
-          <q-btn round :size="iconSize" flat icon="mdi-feature-search-outline" dense />
-        </template>
-      </q-input>
     </div>
-  </div>
+  </hook-wrapper>
 </template>
 <script>
 export default {
