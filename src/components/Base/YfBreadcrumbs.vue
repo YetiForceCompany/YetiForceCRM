751120c20923d047a81174d91a596bf8802ef4ba
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-03-21 08:35:51 +0100

    Added hook wrappers to edited components

--- src/components/Base/YfBreadcrumbs.vue
+++ src/components/Base/YfBreadcrumbs.vue
@@ -1,15 +1,17 @@
 <!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
 <template>
-  <q-breadcrumbs active-color="info" :class="[$style.breadcrumbs, '']">
-    <q-breadcrumbs-el icon="mdi-home-outline" :to="'/'" />
-    <q-breadcrumbs-el
-      v-for="route in $route.matched"
-      :key="route.name"
-      v-if="route.path && route.path.split('/').pop()"
-      :label="route.path.split('/').pop()"
-      :to="route.path"
-    />
-  </q-breadcrumbs>
+  <hook-wrapper>
+    <q-breadcrumbs active-color="info" :class="[$style.breadcrumbs, '']">
+      <q-breadcrumbs-el icon="mdi-home-outline" :to="'/'" />
+      <q-breadcrumbs-el
+        v-for="route in $route.matched"
+        :key="route.name"
+        v-if="route.path && route.path.split('/').pop()"
+        :label="route.path.split('/').pop()"
+        :to="route.path"
+      />
+    </q-breadcrumbs>
+  </hook-wrapper>
 </template>
 
 <script>
--- src/components/Base/YfHeader.vue
+++ src/components/Base/YfHeader.vue
@@ -1,16 +1,18 @@
 <!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
 <template>
-  <q-header elevated class="bg-white text-muted">
-    <q-toolbar>
-      <q-toolbar-title class="col-auto">
-        <q-avatar>
-          <img src="https://cdn.quasar-framework.org/logo/svg/quasar-logo.svg" />
-        </q-avatar>
-      </q-toolbar-title>
-      <yf-global-search />
-      <yf-global-actions class="q-ml-auto" />
-    </q-toolbar>
-  </q-header>
+  <hook-wrapper>
+    <q-header elevated class="bg-white text-muted">
+      <q-toolbar>
+        <q-toolbar-title class="col-auto">
+          <q-avatar>
+            <img src="https://cdn.quasar-framework.org/logo/svg/quasar-logo.svg" />
+          </q-avatar>
+        </q-toolbar-title>
+        <yf-global-search />
+        <yf-global-actions class="q-ml-auto" />
+      </q-toolbar>
+    </q-header>
+  </hook-wrapper>
 </template>
 <script>
 import YfGlobalSearch from 'components/Base/YfGlobalSearch.vue'
