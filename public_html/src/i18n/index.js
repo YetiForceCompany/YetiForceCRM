5a9469af314f4d0e16e62032ebef10972c39d8c3
Rafał Pośpiech <r.pospiech@yetiforce.com>
2019-03-28 10:59:11 +0100

    restored i18n

--- public_html/src/i18n/index.js
+++ public_html/src/i18n/index.js
@@ -1,7 +1,7 @@
 /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
-let translations = {}
+let messages = {}
 if (typeof window !== 'undefined' && window.env !== 'undefined') {
-  translations = window.env.Language.translations
+  messages = window.env.Language.translations
 }
 
 let i18n = null
@@ -12,7 +12,7 @@ function createI18n({ app }) {
       locale: '_Base',
       fallbackLocale: '_Base',
       silentTranslationWarn: typeof window.env.Env.dev === 'undefined',
-      translations
+      messages
     })
     i18n = app.i18n
   }
