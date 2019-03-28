aa512b44907a54007cddb28516829060f5ab13c9
Rafał Pośpiech <r.pospiech@yetiforce.com>
2019-03-28 11:23:28 +0100

    prepend license after babel modifications

--- public_html/gulpfile.js
+++ public_html/gulpfile.js
@@ -18,12 +18,6 @@ gulp.task('vue', function() {
     .src('src/**/*.vue')
     .pipe(vueEsCompiler())
     .pipe(
-      gap.prependText(
-        '/* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */',
-        '\n'
-      )
-    )
-    .pipe(
       babel({
         sourceMap: 'both',
         presets: [['@babel/preset-env', { modules: false }]],
@@ -65,6 +59,12 @@ gulp.task('vue', function() {
           ]
         ]
       })
+    )
+    .pipe(
+      gap.prependText(
+        '/* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */',
+        '\n'
+      )
     )
     .pipe(gulp.dest('./src/'))
 })
