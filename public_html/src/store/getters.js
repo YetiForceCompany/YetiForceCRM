5a1422bdf5521e56b92f4c145a5ade5fc4a42b4b
Rafał Pośpiech <r.pospiech@yetiforce.com>
2019-04-02 15:33:17 +0200

    remove ignored files

--- public_html/src/store/getters.js
+++ public_html/src/store/getters.js
@@ -29,6 +29,7 @@ export default {
     }
   },
   "Settings": {
+    "Menu": {},
     "ModuleExample": {}
   }
 }
--- public_html/src/store/mutations.js
+++ public_html/src/store/mutations.js
@@ -32,6 +32,7 @@ export default {
     }
   },
   "Settings": {
+    "Menu": {},
     "ModuleExample": {}
   }
 }
