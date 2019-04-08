811d597b3b954396622f70ce0dd1ba1f280bc7c7
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-04-08 11:52:02 +0200

    Updated generated files

--- public_html/src/store/actions.js
+++ public_html/src/store/actions.js
@@ -1,9 +1,15 @@
 /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
 export default {
   "Base": {
+    "Chat": {
+      "getData": "Base/Chat/getData"
+    },
     "Home": {},
     "ModuleExample": {
       "getData": "Base/ModuleExample/getData"
+    },
+    "ModuleExample2": {
+      "getData": "Base/ModuleExample2/getData"
     }
   },
   "Core": {
--- public_html/src/store/getters.js
+++ public_html/src/store/getters.js
@@ -1,9 +1,15 @@
 /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
 export default {
   "Base": {
+    "Chat": {
+      "dialog": "Base/Chat/dialog"
+    },
     "Home": {},
     "ModuleExample": {
       "testVariable": "Base/ModuleExample/testVariable"
+    },
+    "ModuleExample2": {
+      "testVariable": "Base/ModuleExample2/testVariable"
     }
   },
   "Core": {
