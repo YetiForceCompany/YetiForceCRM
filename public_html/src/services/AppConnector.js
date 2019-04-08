261186c75ef82ade3ea1e10f146a80fcec796523
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-04-08 09:35:52 +0200

    Added websocket promise

--- public_html/src/services/AppConnector.js
+++ public_html/src/services/AppConnector.js
@@ -1,53 +1,42 @@
 /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
 import ApiService from './Api.js'
 import WebSocket from './WebSocket.js'
-export default class AppConnector {
-  constructor(type = 'request', params = { module: '', action: '', title: '', body: '' }) {
-    this.type = type
-    this.params = params
-    this.module = params.module
-    this.action = params.action
-    this.title = params.title
-    this.body = params.body
-    if (type === 'request') {
-      this._request(params)
-    } else if (type === 'webSocket') {
-      this._webSocket(params)
-    }
-  }
-  _request() {
-    return ApiService(this.params)
-  }
-  _webSocket() {
-    let messageID = 1
-    WebSocket.then(function(connection) {
-      console.log(connection)
-      connection.send('asdfasdf')
-
-      connection.onerror = function(error) {
-        console.log('error???')
-
-        // an error occurred when sending/receiving data
-      }
-      connection.onclose = function(error) {
-        console.log('close???')
-
-        // an error occurred when sending/receiving data
-      }
-
-      connection.onmessage = function(message) {
-        // try to decode json (I assume that each message
-        // from server is json)
-        try {
-          const json = JSON.parse(message.data)
-          console.log('Valid???')
-          console.log(message)
-        } catch (e) {
-          console.log("This doesn't look like a valid JSON: ", message.data)
-          return
+const AppConnector = {
+  request(params) {
+    return ApiService(params)
+  },
+  webSocket(params) {
+    WebSocket.then(connection => {
+      connection.send(JSON.stringify(params))
+      return connection
+    })
+  },
+  webSocketPromise(params) {
+    let messageID = Math.random() * 1
+    return new Promise(function(resolve, reject) {
+      WebSocket.then(function(connection) {
+        console.log('send')
+        connection.send(JSON.stringify({ id: messageID, text: 'asdfasdf0' + messageID }))
+        connection.onmessage = function(message) {
+          try {
+            const json = JSON.parse(message.data)
+            console.log(json)
+            console.log(messageID)
+            if (json.id === messageID) {
+              console.log('success')
+              resolve('success')
+              //TODO return promise with resolve here
+            } else {
+              reject('wrong id')
+            }
+          } catch (e) {
+            console.log("This doesn't look like a valid JSON: ", message.data)
+            reject(e)
+            return
+          }
         }
-        // handle incoming message
-      }
+      })
     })
   }
 }
+export default AppConnector
--- public_html/src/services/WebSocket.js
+++ public_html/src/services/WebSocket.js
@@ -3,22 +3,17 @@
 let connection = null
 function connect() {
   if (connection === null) {
-    // connection = new WebSocket('ws://127.0.0.1:1337')
-    // connection.onopen = function(data) {
-    //   console.log('opened')
-    //   console.log(data)
-    //   return connection
-
-    // connection is opened and ready to use
     return new Promise(function(resolve, reject) {
       connection = new WebSocket('ws://127.0.0.1:1337')
       connection.onopen = function() {
         console.log('conn')
-        console.log(connection)
         resolve(connection)
       }
       connection.onerror = function(err) {
         reject(err)
+      }
+      connection.onclose = function(err) {
+        console.log(err)
       }
     })
   } else {
