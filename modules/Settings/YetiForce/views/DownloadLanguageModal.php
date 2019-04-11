e91b156b6b2fbaba22d8ec004f97dc5f359a4fd6
Arkadiusz Adach <32322851+arek1-618@users.noreply.github.com>
2019-04-11 10:16:25 +0200

    [IT548] - Fixed modal window appearance (#9975)
    * [IT548] - Fixed modal window appearance
    
    * Improving the appearance.

--- modules/Settings/YetiForce/views/DownloadLanguageModal.php
+++ modules/Settings/YetiForce/views/DownloadLanguageModal.php
@@ -15,6 +15,7 @@ class Settings_Yetiforce_DownloadLanguageModal_View extends \App\Controller\Moda
 	 * {@inheritdoc}
 	 */
 	public $successBtn = '';
+
 	/**
 	 * Qualified module name.
 	 *
@@ -25,7 +26,7 @@ class Settings_Yetiforce_DownloadLanguageModal_View extends \App\Controller\Moda
 	/**
 	 * {@inheritdoc}
 	 */
-	public function preProcessAjax(\App\Request $request)
+	public function preProcessAjax(App\Request $request)
 	{
 		$this->pageTitle = '<span class="fas fas fa-download mr-2"></span>' . \App\Language::translate('LBL_DOWNLOAD_LANG', $this->qualifiedModuleName);
 		parent::preProcessAjax($request);
@@ -36,7 +37,7 @@ class Settings_Yetiforce_DownloadLanguageModal_View extends \App\Controller\Moda
 	 *
 	 * @param \App\Request $request
 	 */
-	public function process(\App\Request $request)
+	public function process(App\Request $request)
 	{
 		$viewer = $this->getViewer($request);
 		$viewer->assign('QUALIFIED_MODULE', $this->qualifiedModuleName);
