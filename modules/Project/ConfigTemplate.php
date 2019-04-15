0c06ae91af4f22279c2ff1468bb4f98ab5deeaa0
Arkadiusz Adach <a.adach@yetiforce.com>
2019-04-15 09:07:29 +0200

    Removal of unnecessary code.

--- modules/Project/ConfigTemplate.php
+++ modules/Project/ConfigTemplate.php
@@ -6,10 +6,6 @@
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  */
 return [
-	'STATUS_DEFAULT_VALUE' => [
-		'default' => 'PLL_PLANNED',
-		'description' => 'The default value is returned when no rule applies.',
-	],
 	'COLUMNS_IN_HIERARCHY' => [
 		'default' => [],
 		'description' => 'Columns visible in Project hierarchy [$label => $columnName]'
@@ -27,49 +23,6 @@ return [
 		'description' => 'Count Projects in hierarchy',
 		'validation' => '\App\Validator::bool',
 		'sanitization' => '\App\Purifier::bool'
-	],
-	'STATUS_RULES' => [
-		'default' => [
-			'PLL_PLANNED' => [
-				'condition' => 'or',
-				'rules' => [
-					['operator' => 'all_equal', 'value' => 'PLL_PLANNED'],
-					['operator' => 'empty'],
-				],
-			],
-			'PLL_IN_PROGRESSING' => [
-				'condition' => 'and',
-				'rules' => [
-					['operator' => 'is_exist', 'value' => 'PLL_IN_PROGRESSING'],
-				],
-			],
-			'PLL_ON_HOLD' => [
-				'condition' => 'and',
-				'rules' => [
-					['operator' => 'all_equal', 'value' => 'PLL_ON_HOLD', 'automation' => 1],
-				],
-			],
-			'PLL_COMPLETED' => [
-				'condition' => 'or',
-				'rules' => [
-					['operator' => 'all_equal', 'value' => 'PLL_COMPLETED'],
-					['operator' => 'only_contains', 'value' => ['PLL_COMPLETED', 'PLL_CANCELLED']],
-				],
-			],
-			'PLL_IN_APPROVAL' => [
-				'condition' => 'and',
-				'rules' => [
-					['operator' => 'all_equal', 'value' => 'PLL_IN_APPROVAL', 'automation' => 1],
-				],
-			],
-			'PLL_CANCELLED' => [
-				'condition' => 'and',
-				'rules' => [
-					['operator' => 'all_equal', 'value' => 'PLL_CANCELLED'],
-				],
-			],
-		],
-		'description' => 'Definition of rules for automatic status change',
 	],
 	'defaultGanttColors' => [
 		'default' => [
