1ac0f35bab67fbdc9d345c602692b8b7f35b7792
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-04-18 09:29:20 +0200

    Fixed namespace, removed log

--- public_html/src/i18n/index.js
+++ public_html/src/i18n/index.js
@@ -1,7 +1,6 @@
 /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
 let messages = {}
 if (typeof window !== 'undefined' && window.env.Core !== undefined) {
-  console.log(window.env.Core)
   messages = window.env.Core.Language.translations
 }
 
