6adb5f72ed9871aec716fbdf9a2dde285d275e04
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2019-04-26 13:45:10 +0200

    Improved error handling

--- app/Condition.php
+++ app/Condition.php
@@ -28,8 +28,9 @@ class Condition
 	 */
 	public static function validSearchParams(string $moduleName, array $searchParams): array
 	{
-		if (count($searchParams) > 2) {
-			throw new Exceptions\IllegalValue('ERR_NOT_ALLOWED_VALUE');
+		$searchParamsCount = count($searchParams);
+		if ($searchParamsCount > 2) {
+			throw new Exceptions\IllegalValue("ERR_NUMBER_OF_ARGUMENTS_NOT_ALLOWED||{$searchParamsCount}|| > 2||" . Utils::varExport($searchParams, true), 406);
 		}
 		$fields = \Vtiger_Module_Model::getInstance($moduleName)->getFields();
 		$result = [];
@@ -39,18 +40,18 @@ class Condition
 				if (empty($param)) {
 					continue;
 				}
-				$countvariables = count($param);
-				if ($countvariables !== 3 && $countvariables !== 4) {
-					throw new Exceptions\IllegalValue('ERR_NOT_ALLOWED_VALUE');
+				$count = count($param);
+				if (3 !== $count && 4 !== $count) {
+					throw new Exceptions\IllegalValue("ERR_NUMBER_OF_ARGUMENTS_NOT_ALLOWED||{$count}|| <> 3 or 4||" . Utils::varExport($param, true), 406);
 				}
 				if (!isset($fields[$param[0]])) {
-					throw new Exceptions\IllegalValue('ERR_NOT_ALLOWED_VALUE');
+					throw new Exceptions\IllegalValue("ERR_FIELD_NOT_FOUND||{$param[0]}||" . Utils::varExport($param, true), 406);
 				}
 				$fieldModel = $fields[$param[0]];
 				$fieldModel->getUITypeModel()->getDbConditionBuilderValue($param[2], $param[1]);
-				$tempParam[]= $param;
+				$tempParam[] = $param;
 			}
-			$result[]= $tempParam;
+			$result[] = $tempParam;
 		}
 		return $result;
 	}
@@ -67,7 +68,7 @@ class Condition
 	 */
 	public static function validSearchValue(string $value, string $moduleName, string $fieldName, string $operator): string
 	{
-		if ($value !== '') {
+		if ('' !== $value) {
 			\Vtiger_Module_Model::getInstance($moduleName)->getField($fieldName)->getUITypeModel()->getDbConditionBuilderValue($value, $operator);
 		}
 		return $value;
