6d02a3940489e46bc1c8dcebf87be6d230f2b0fe
Tomasz Kur <t.kur@yetiforce.com>
2019-04-29 09:15:59 +0200

    Fixed validation search_params (#10273)

--- app/Condition.php
+++ app/Condition.php
@@ -44,10 +44,19 @@ class Condition
 				if (3 !== $count && 4 !== $count) {
 					throw new Exceptions\IllegalValue("ERR_NUMBER_OF_ARGUMENTS_NOT_ALLOWED||{$count}|| <> 3 or 4||" . Utils::varExport($param, true), 406);
 				}
-				if (!isset($fields[$param[0]])) {
-					throw new Exceptions\IllegalValue("ERR_FIELD_NOT_FOUND||{$param[0]}||" . Utils::varExport($param, true), 406);
+				[$relatedFieldName, $relatedModule, $referenceField] = explode(':', $param[0]);
+				if ($relatedModule) {
+					$relatedFields = \Vtiger_Module_Model::getInstance($relatedModule)->getFields();
+					if (!isset($fields[$referenceField], $relatedFields[$relatedFieldName])) {
+						throw new Exceptions\IllegalValue("ERR_FIELD_NOT_FOUND||{$param[0]}||" . Utils::varExport($param, true), 406);
+					}
+					$fieldModel = $relatedFields[$relatedFieldName];
+				} else {
+					if (!isset($fields[$param[0]])) {
+						throw new Exceptions\IllegalValue("ERR_FIELD_NOT_FOUND||{$param[0]}||" . Utils::varExport($param, true), 406);
+					}
+					$fieldModel = $fields[$param[0]];
 				}
-				$fieldModel = $fields[$param[0]];
 				$fieldModel->getUITypeModel()->getDbConditionBuilderValue($param[2], $param[1]);
 				$tempParam[] = $param;
 			}
