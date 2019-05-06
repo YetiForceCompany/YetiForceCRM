dce59745c53ed3fac5971efd5fbfded0210b994f
TomaszQr <t.kur@yetiforce.com>
2019-05-06 15:27:44 +0200

    Fixed field "country" in portal

--- api/webservice/Portal/BaseModule/Fields.php
+++ api/webservice/Portal/BaseModule/Fields.php
@@ -50,6 +50,14 @@ class Fields extends \Api\Core\BaseAction
 			if ($field->isTreeField()) {
 				$fieldInfo['treeValues'] = \App\Fields\Tree::getTreeValues((int) $field->getFieldParams(), $moduleName);
 			}
+			if ($field->getFieldDataType() === 'country') {
+				$countries = $field->getPicklistValues();
+				array_walk($countries, function (&$item, $key) {
+					$item = \App\Language::translateSingleMod($key, 'Other.Country');
+				});
+				$fieldInfo['picklistvalues'] = $countries;
+				$fieldInfo['isEmptyPicklistOptionAllowed'] = $field->isEmptyPicklistOptionAllowed();
+			}
 			$fields[$field->getId()] = $fieldInfo;
 		}
 		$inventoryFields = [];
