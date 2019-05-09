e70bc2595bb70c03ed1f840c351682743d4d5334
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-05-09 10:00:00 +0200

    Added missing files

---
+++ public_html/src/modules/Base/modules/Basic/layouts/Basic.vue
@@ -0,0 +1,11 @@
+<!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
+<template>
+  <router-view></router-view>
+</template>
+<script>
+const moduleName = 'Base.Basic.Layouts.Basic'
+
+export default {
+  name: moduleName
+}
+</script>
---
+++ public_html/src/modules/Base/modules/Basic/views/List/List.vue
@@ -0,0 +1,13 @@
+<!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
+<template>
+  <list></list>
+</template>
+
+<script>
+import List from './components/List.vue.js'
+const moduleName = 'Base.Basic.Pages.Basic'
+export default {
+  name: moduleName,
+  components: { List }
+}
+</script>
