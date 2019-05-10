b1c088b3e50298bccc63bbef04f1c8a034e7699f
Arkadiusz Adach <a.adach@yetiforce.com>
2019-05-10 13:27:22 +0200

    Improved trimming of zeros

--- public_html/layouts/resources/Fields.js
+++ public_html/layouts/resources/Fields.js
@@ -1481,16 +1481,29 @@ window.App.Fields = {
 			if (!value) {
 				value = 0;
 			}
+			let strDecimal = value.toString().split(".")[1];
+			let numberOfZerosAtTheEnd = 0;
+			if (typeof strDecimal !== "undefined") {
+				for (let i = strDecimal.length - 1; i > 0; --i) {
+					if (strDecimal[i] == "0") {
+						numberOfZerosAtTheEnd++;
+					} else {
+						break;
+					}
+				}
+			}
 			value = parseFloat(value);
 			if (fixed) {
 				value = value.toFixed(numberOfDecimal);
 			}
-			let splittedFloat = value.toString().split('.');
+			let splittedFloat = value.toString().split(".");
 			let integer = App.Fields.Integer.formatToDisplay(splittedFloat[0]);
 			let decimal = splittedFloat[1];
 			if (numberOfDecimal) {
-				if (CONFIG.truncateTrailingZeros && 0===parseInt(decimal)) {
-					return integer;
+				if (!CONFIG.truncateTrailingZeros && decimal) {
+					for (let i = 0; i < numberOfZerosAtTheEnd && decimal.length < numberOfDecimal; ++i) {
+						decimal += "0";
+					}
 				}
 				if (decimal) {
 					return integer + CONFIG.currencyDecimalSeparator + decimal;
