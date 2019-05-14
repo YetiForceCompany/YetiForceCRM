443a54603812b63b9624d59aa3bdc5ed357adf0c
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-05-14 10:43:28 +0200

    Updated license

--- modules/KnowledgeBase/views/Tree.php
+++ modules/KnowledgeBase/views/Tree.php
@@ -1,12 +1,10 @@
 <?php
-/* +***********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
- * *********************************************************************************** */
+/**
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Tomasz Kur <t.kur@yetiforce.com>
+ * @author    Tomasz Poradzewski <t.poradzewski@yetiforce.com>
+ */
 
 class KnowledgeBase_Tree_View extends Vtiger_Index_View
 {
