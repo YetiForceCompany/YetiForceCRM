0b6d789426296cda1a4523de91952ebcc1aace6d
Arkadiusz Adach <32322851+arek1-618@users.noreply.github.com>
2019-05-16 14:06:55 +0200

    Portal 2 - Improvement of the inventory save method (#10414)
    * Portal 2 - Improvement of the inventory save method
    
    * Filling in the address fields
    
    * change getRelatedFieldForModule -> getReferenceFieldsForModule
    
    * Refactoring the code

--- api/webservice/Portal/BaseModule/SaveInventory.php
+++ api/webservice/Portal/BaseModule/SaveInventory.php
@@ -32,13 +32,55 @@ class SaveInventory extends \Api\Core\BaseAction
 		$inventory = $this->controller->request->getArray('inventory');
 		$recordModel = \Vtiger_Record_Model::getCleanInstance($moduleName);
 		$recordModel->set('subject', $moduleName . '/' . date('Y-m-d'));
+		$moduleModel = $recordModel->getModule();
+		$fields = $moduleModel->getReferenceFieldsForModule('Accounts');
+		if ($fieldModel = current($fields)) {
+			$recordModel->set($fieldModel->getFieldName(), $this->getParentCrmId());
+		}
+		foreach ($moduleModel->getFieldsByType('serverAccess') as $fieldModel) {
+			if ((int) $this->controller->app['id'] === (int) $fieldModel->getFieldParams()) {
+				$recordModel->setDataForSave([$fieldModel->getTableName() => [$fieldModel->getColumnName() => 1]]);
+			}
+		}
 		$recordModel->initInventoryData(
 			(new \Api\Portal\BaseModel\SaveInventory($moduleName, $inventory))->getInventoryData(),
 			false
 		);
+		$this->setAddressForRecordModel($recordModel);
 		$recordModel->save();
 		return [
 			'id' => $recordModel->getId(),
+			'moduleName' => $moduleName
 		];
+	}
+
+	/**
+	 * Set address for record model.
+	 *
+	 * @param \Vtiger_Record_Model $recordModel
+	 *
+	 * @return void
+	 */
+	private function setAddressForRecordModel(\Vtiger_Record_Model $recordModel)
+	{
+		$address = $this->controller->request->getArray('address');
+		$addressFields = [
+			'addresslevel1',
+			'addresslevel2',
+			'addresslevel3',
+			'addresslevel4',
+			'addresslevel5',
+			'addresslevel6',
+			'addresslevel7',
+			'addresslevel8',
+			'localnumber',
+			'buildingnumber',
+			'pobox'
+		];
+		foreach ($addressFields as $fieldName) {
+			if (isset($address[$fieldName])) {
+				$recordModel->set("{$fieldName}a", $address[$fieldName]);
+			}
+		}
 	}
 }
