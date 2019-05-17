8f0de6a160ed1e85fa2a44dd842aacb3842de238
TomaszQr <t.kur@yetiforce.com>
2019-05-17 15:58:43 +0200

    Improved description

--- api/webservice/Portal/BaseModule/SaveInventory.php
+++ api/webservice/Portal/BaseModule/SaveInventory.php
@@ -2,6 +2,7 @@
 /**
  * The file contains a the SaveInventory class.
  *
+ * @package Api
  *
  * @copyright YetiForce Sp. z o.o
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
