07cdd8ec41fe9625ef8210ccb0ca964736cc9c09
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-05-22 12:02:58 +0200

    Added @package to comments

--- modules/KnowledgeBase/views/TreeModal.php
+++ modules/KnowledgeBase/views/TreeModal.php
@@ -3,6 +3,8 @@
 /**
  * Knowledge Base modal class.
  *
+ * @package   View
+ *
  * @copyright YetiForce Sp. z o.o
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Tomasz Poradzewski <t.poradzewski@yetiforce.com>
