f73b49d0403b55e09029e656f770298f021829a6
Arkadiusz Adach <a.adach@yetiforce.com>
2019-05-27 12:44:16 +0200

    phpdoc improvement

--- api/webservice/Portal/Record.php
+++ api/webservice/Portal/Record.php
@@ -2,6 +2,8 @@
 /**
  * The file contains: Record class.
  *
+ * @package Api
+ *
  * @copyright YetiForce Sp. z o.o.
  * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author Arkadiusz Adach <a.adach@yetiforce.com>
