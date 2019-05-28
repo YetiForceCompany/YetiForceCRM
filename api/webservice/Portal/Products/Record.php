813275b717882120662afc2a0ac8ac88e3fdce96
Arkadiusz Adach <32322851+arek1-618@users.noreply.github.com>
2019-05-28 10:13:01 +0200

    Update api/webservice/Portal/Products/Record.php
    Co-Authored-By: Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>

--- api/webservice/Portal/Products/Record.php
+++ api/webservice/Portal/Products/Record.php
@@ -24,7 +24,7 @@ class Record extends \Api\Portal\BaseModule\Record
 		$response = parent::get();
 		$recordId = $this->controller->request->getInteger('record');
 		$tax = (float) current($response['rawData']['taxes_info'])['value'] ?? 0.0;
-		$unitPrice = \Api\Portal\Record::getPriceFromPricebook($this->getParentCrmId(), $recordId);
+		$unitPrice = \Api\Portal\Record::getPriceFromPricebook($this->getParentCrmId(), $this->controller->request->getInteger('record'));
 		if (\Api\Portal\Privilege::USER_PERMISSIONS !== $this->getPermissionType()) {
 			$response['ext']['unit_price'] = $unitPrice;
 			$response['ext']['unit_gross'] = $unitPrice + ($unitPrice * $tax / 100.00);
