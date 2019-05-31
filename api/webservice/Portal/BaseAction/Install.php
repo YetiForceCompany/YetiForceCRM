7ac1b9d58c179286bcb0ebad286a8c60f13d0d4c
TomaszQr <t.kur@yetiforce.com>
2019-05-31 11:13:15 +0200

    Added action to check connection

---
+++ api/webservice/Portal/BaseAction/Install.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Action to check configuration.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+
+namespace Api\Portal\BaseAction;
+
+/**
+ * Install class.
+ */
+class Install extends \Api\Core\BaseAction
+{
+	/** @var string[] Allowed request methods */
+	public $allowedMethod = ['POST'];
+
+	/**
+	 * Get modules list.
+	 *
+	 * @return string[]
+	 */
+	public function post()
+	{
+		return $this->controller->request->getRaw('data');
+	}
+}
