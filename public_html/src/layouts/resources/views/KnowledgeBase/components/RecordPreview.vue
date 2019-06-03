8861bcac320590de4d143bc85912d5c096710a1f
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-06-03 10:48:41 +0200

    Added modal fixed header

--- public_html/src/layouts/resources/views/KnowledgeBase/components/RecordPreview.vue
+++ public_html/src/layouts/resources/views/KnowledgeBase/components/RecordPreview.vue
@@ -57,85 +57,93 @@
           </q-btn>
         </slot>
       </q-bar>
-      <q-card-section v-show="record.introduction">
-        <div class="text-subtitle2 text-bold">{{ record.introduction }}</div>
-      </q-card-section>
-      <q-card-section v-show="record.content">
-        <carousel v-if="record.view === 'PLL_PRESENTATION'" :record="record" />
-        <div v-else>
+      <q-card-section :class="['scroll', maximized ? 'modal-full-height' : 'modal-mini']">
+        <div v-show="record.introduction">
+          <div class="text-subtitle2 text-bold">{{ record.introduction }}</div>
+        </div>
+        <div v-show="record.content">
+          <carousel v-if="record.view === 'PLL_PRESENTATION'" :record="record" />
+          <div v-else>
+            <q-separator />
+            <div v-html="record.content"></div>
+          </div>
+        </div>
+        <div v-if="hasRelatedArticles">
           <q-separator />
-          <div v-html="record.content"></div>
+          <records-list
+            v-if="record.related"
+            :data="record.related.Articles"
+            :title="translate('JS_RELATED_ARTICLES')"
+          />
         </div>
-      </q-card-section>
-      <q-card-section v-if="hasRelatedArticles">
-        <q-separator />
-        <records-list v-if="record.related" :data="record.related.Articles" :title="translate('JS_RELATED_ARTICLES')" />
-      </q-card-section>
-      <q-card-section v-if="hasRelatedRecords">
-        <q-separator />
-        <div class="q-pa-md q-table__title">{{ translate('JS_RELATED_RECORDS') }}</div>
-        <div class="q-pa-sm featured-container items-start q-gutter-md">
-          <template v-for="(moduleRecords, parentModule) in record.related">
-            <q-list
-              bordered
-              padding
-              dense
-              :key="parentModule"
-              v-if="parentModule !== 'Articles' && parentModule !== 'ModComments' && moduleRecords.length === undefined"
-            >
-              <q-item header clickable class="text-black flex">
-                <icon :icon="'userIcon-' + parentModule" :size="iconSize" class="mr-2"></icon>
-                {{ parentModule }}
-              </q-item>
-              <q-item
-                clickable
-                v-for="(relatedRecord, relatedRecordId) in moduleRecords"
-                :key="relatedRecordId"
-                class="text-subtitle2"
-                v-ripple
+        <div v-if="hasRelatedRecords">
+          <q-separator />
+          <div class="q-pa-md q-table__title">{{ translate('JS_RELATED_RECORDS') }}</div>
+          <div class="q-pa-sm featured-container items-start q-gutter-md">
+            <template v-for="(moduleRecords, parentModule) in record.related">
+              <q-list
+                bordered
+                padding
+                dense
+                :key="parentModule"
+                v-if="
+                  parentModule !== 'Articles' && parentModule !== 'ModComments' && moduleRecords.length === undefined
+                "
               >
-                <q-item-section class="align-items-center flex-row no-wrap justify-content-start">
-                  <a
-                    class="js-popover-tooltip--record ellipsis"
-                    :href="`index.php?module=${parentModule}&view=Detail&record=${relatedRecordId}`"
-                  >
-                    {{ relatedRecord }}
-                  </a>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </template>
+                <q-item header clickable class="text-black flex">
+                  <icon :icon="'userIcon-' + parentModule" :size="iconSize" class="mr-2"></icon>
+                  {{ parentModule }}
+                </q-item>
+                <q-item
+                  clickable
+                  v-for="(relatedRecord, relatedRecordId) in moduleRecords"
+                  :key="relatedRecordId"
+                  class="text-subtitle2"
+                  v-ripple
+                >
+                  <q-item-section class="align-items-center flex-row no-wrap justify-content-start">
+                    <a
+                      class="js-popover-tooltip--record ellipsis"
+                      :href="`index.php?module=${parentModule}&view=Detail&record=${relatedRecordId}`"
+                    >
+                      {{ relatedRecord }}
+                    </a>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </template>
+          </div>
         </div>
-      </q-card-section>
-      <q-card-section v-if="hasRelatedComments">
-        <q-separator />
-        <div class="q-pa-md q-table__title">{{ translate('JS_COMMENTS') }}</div>
-        <q-list padding>
-          <q-item v-for="(relatedRecord, relatedRecordId) in record.related.ModComments" :key="relatedRecordId">
-            <q-item-section avatar top>
-              <q-avatar size="iconSize">
-                <img v-if="relatedRecord.avatar.url !== undefined" :src="relatedRecord.avatar.url" />
-                <q-icon v-else name="mdi-account" />
-              </q-avatar>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>
-                <a
-                  class="js-popover-tooltip--record"
-                  :href="`index.php?module=Users&view=Detail&record=${relatedRecord.userid}`"
-                  >{{ relatedRecord.userName }}
-                </a>
-              </q-item-label>
-              <q-item-label><div v-html="relatedRecord.comment"></div></q-item-label>
-            </q-item-section>
-            <q-item-section side top>
-              <q-item-label caption>{{ relatedRecord.modifiedShort }}</q-item-label>
-              <q-tooltip anchor="top middle" self="center middle">
-                {{ translate('JS_MODIFIED') + ': ' + relatedRecord.modifiedFull }}
-              </q-tooltip>
-            </q-item-section>
-          </q-item>
-        </q-list>
+        <div v-if="hasRelatedComments">
+          <q-separator />
+          <div class="q-pa-md q-table__title">{{ translate('JS_COMMENTS') }}</div>
+          <q-list padding>
+            <q-item v-for="(relatedRecord, relatedRecordId) in record.related.ModComments" :key="relatedRecordId">
+              <q-item-section avatar top>
+                <q-avatar size="iconSize">
+                  <img v-if="relatedRecord.avatar.url !== undefined" :src="relatedRecord.avatar.url" />
+                  <q-icon v-else name="mdi-account" />
+                </q-avatar>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>
+                  <a
+                    class="js-popover-tooltip--record"
+                    :href="`index.php?module=Users&view=Detail&record=${relatedRecord.userid}`"
+                    >{{ relatedRecord.userName }}
+                  </a>
+                </q-item-label>
+                <q-item-label><div v-html="relatedRecord.comment"></div></q-item-label>
+              </q-item-section>
+              <q-item-section side top>
+                <q-item-label caption>{{ relatedRecord.modifiedShort }}</q-item-label>
+                <q-tooltip anchor="top middle" self="center middle">
+                  {{ translate('JS_MODIFIED') + ': ' + relatedRecord.modifiedFull }}
+                </q-tooltip>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
       </q-card-section>
     </q-card>
   </q-dialog>
@@ -192,5 +200,11 @@ export default {
   padding-top: 3px;
   padding-bottom: 3px;
   height: unset !important;
+}
+.modal-full-height {
+  max-height: calc(100vh - 31.14px);
+}
+.modal-mini {
+  max-height: 75vh;
 }
 </style>
