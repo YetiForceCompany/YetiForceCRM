3d9debc505bba25556bf40a37e03f55cd21faab7
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-06-04 14:04:07 +0200

    Added css improvements in knowledge base view

--- public_html/src/layouts/resources/views/KnowledgeBase/KnowledgeBase.vue
+++ public_html/src/layouts/resources/views/KnowledgeBase/KnowledgeBase.vue
@@ -280,7 +280,7 @@ export default {
 }
 .featured-container {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(33.3%, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   grid-auto-flow: dense;
 }
 </style>
