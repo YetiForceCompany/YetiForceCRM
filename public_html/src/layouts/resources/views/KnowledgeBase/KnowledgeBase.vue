adc5e245410109ca242999be2a2ed3f933c032b3
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2019-06-05 13:01:24 +0200

    Improved a translation

--- public_html/src/layouts/resources/views/KnowledgeBase/KnowledgeBase.vue
+++ public_html/src/layouts/resources/views/KnowledgeBase/KnowledgeBase.vue
@@ -78,7 +78,7 @@
             </q-input>
             <div class="flex items-center q-ml-xs">
               <q-icon name="mdi-information-outline" size="iconSize" />
-              <q-tooltip> {{ translate('JS_SEARCH_INFO') }} </q-tooltip>
+              <q-tooltip> {{ translate('JS_FULL_TEXT_SEARCH_INFO') }} </q-tooltip>
             </div>
             <div>
               <q-toggle v-model="categorySearch" icon="mdi-file-tree" />
