6a1c2066d084537a88d818acfaff23b451ce5d5e
Arkadiusz Adach <a.adach@yetiforce.com>
2019-06-06 10:09:49 +0200

    Fixed receiving information about payments

--- api/webservice/Portal/BaseAction/ReceiveFromPaymentsSystem.php
+++ api/webservice/Portal/BaseAction/ReceiveFromPaymentsSystem.php
@@ -37,29 +37,30 @@ class ReceiveFromPaymentsSystem extends \Api\Core\BaseAction
 	public function put()
 	{
 		$request = $this->controller->request;
-		$paymentSystem = $request->getByType('payment_system', 'Alnum');
+		$paymentSystem = 'PLL_' . \strtoupper($request->getByType('payment_system', 'Alnum'));
 		$transactionId = $request->getByType('transaction_id', 'Alnum');
-		$paymentsInId = (new \App\Db\Query())
-			->select(['paymentsinid'])
-			->from('vtiger_paymentsin')
-			->where(['transaction_id' => $transactionId, 'payment_system' => $paymentSystem])
-			->scalar();
+		$orderId = $request->getInteger('ssingleordersid');
+		$recordModelOrder = \Vtiger_Record_Model::getInstanceById($orderId, 'SSingleOrders');
+		$userId = (int) $recordModelOrder->get('assigned_user_id');
+		$queryGenerator = new \App\QueryGenerator('PaymentsIn', $userId);
+		$queryGenerator->setFields(['paymentsinid']);
+		$queryGenerator->addCondition('transaction_id', $transactionId, 'e');
+		$queryGenerator->addCondition('payment_system', $paymentSystem, 'e');
+		$paymentsInId = $queryGenerator->createQuery()->limit(1)->scalar();
 		if ($paymentsInId) {
 			$recordModel = \Vtiger_Record_Model::getInstanceById($paymentsInId);
 		} else {
 			$recordModel = \Vtiger_Record_Model::getCleanInstance('PaymentsIn');
+			$recordModel->set('assigned_user_id', $userId);
+			$recordModel->set('relatedid', $recordModelOrder->get('accountid'));
+			$recordModel->set('ssingleordersid', $orderId);
+			$recordModel->set('transaction_id', $transactionId);
+			$recordModel->set('paymentsvalue', $request->getByType('paymentsvalue', 'Double'));
+			$recordModel->set('currency_id', \App\Fields\Currency::getCurrencyIdByCode($request->getByType('currency_id')));
+			$recordModel->set('paymentstitle', $request->getByType('paymentstitle', 'Text'));
+			$recordModel->set('payment_system', $paymentSystem);
 		}
-		$orderId = $request->getInteger('ssingleordersid');
-		$recordModelOrder = \Vtiger_Record_Model::getInstanceById($orderId, 'SSingleOrders');
-		$recordModel->set('assigned_user_id', $recordModelOrder->get('assigned_user_id'));
-		$recordModel->set('relatedid', $recordModelOrder->get('accountid'));
-		$recordModel->set('ssingleordersid', $orderId);
 		$recordModel->set('paymentsin_status', $request->getByType('paymentsin_status', 'Alnum'));
-		$recordModel->set('transaction_id', $transactionId);
-		$recordModel->set('paymentsvalue', $request->getByType('paymentsvalue', 'Double'));
-		$recordModel->set('currency_id', \App\Fields\Currency::getCurrencyIdByCode($request->getByType('currency_id')));
-		$recordModel->set('paymentstitle', $request->getByType('paymentstitle', 'Text'));
-		$recordModel->set('payment_system', 'PLL_' . \strtoupper($paymentSystem));
 		$recordModel->save();
 		return ['id' => $recordModel->getId()];
 	}
