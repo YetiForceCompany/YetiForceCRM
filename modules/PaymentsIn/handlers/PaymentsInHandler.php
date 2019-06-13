802aa488395a22dae9e8da769503e8b918c8ad06
Arkadiusz Adach <32322851+arek1-618@users.noreply.github.com>
2019-06-13 11:56:18 +0200

    Event handler payment (#10700)
    * Event handler payment
    
    * Change PaymentsInHandler and database
    
    * Merge branch 'developer' into 2019/06/11/01
    
    # Conflicts:
    #	install/install_schema/data.sql
    #	install/install_schema/scheme.sql
    
    * Moving the code to the model
    
    * Setting the precision to 8 decimal places
    
    * Database
    
    * Refactoring the code
    Adding another condition to check
    
    * Refactoring the code

---
+++ modules/PaymentsIn/handlers/PaymentsInHandler.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The file contains: PaymentsIn handler class.
+ *
+ * @package Handler
+ *
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author Arkadiusz Adach <a.adach@yetiforce.com>
+ */
+class PaymentsIn_PaymentsInHandler_Handler
+{
+	/**
+	 * EntityAfterSave handler function.
+	 *
+	 * @param App\EventHandler $eventHandler
+	 *
+	 * @return void
+	 */
+	public function entityAfterSave(App\EventHandler $eventHandler)
+	{
+		$recordModel = $eventHandler->getRecordModel();
+		if ($this->canUpdatePaymentStatus($recordModel)) {
+			(new \App\BatchMethod(['method' => 'PaymentsIn_Module_Model::updatePaymentStatus', 'params' => [$recordModel->get('ssingleordersid')]]))->save();
+		}
+	}
+
+	/**
+	 * Checking if you can update the payment status.
+	 *
+	 * @param Vtiger_Record_Model $recordModel
+	 *
+	 * @return bool
+	 */
+	private function canUpdatePaymentStatus(Vtiger_Record_Model $recordModel): bool
+	{
+		$fieldModel = $recordModel->getField('ssingleorders_payment_status');
+		$returnValue = null !== $fieldModel && $fieldModel->isActiveField() && !$recordModel->isEmpty('ssingleordersid');
+		if ($returnValue && (int) $recordModel->get('currency_id') !== \App\Record::getCurrencyIdFromInventory($recordModel->get('ssingleordersid'), 'SSingleOrders')) {
+			\App\Log::warning('The payment is in a different currency than the order. SSingleOrdersId: ' . $recordModel->get('ssingleordersid'));
+			$returnValue = false;
+		}
+		return $returnValue && ($recordModel->isNew() || false !== $recordModel->getPreviousValue('paymentsin_status'));
+	}
+}
