ca6293a138ea5561d691a40ad9fbe121d894daba
Arek Solek <a.solek@yetiforce.com>
2019-06-18 11:30:28 +0200

    Added files to RecordFields (#10755)

---
+++ app/Conditions/RecordFields/MultiownerField.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Multi owner condition record field class.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+
+namespace App\Conditions\RecordFields;
+
+/**
+ * Multi owner condition record field class.
+ */
+class MultiownerField extends MultipicklistField
+{
+}
