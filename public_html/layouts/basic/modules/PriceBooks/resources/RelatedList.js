15ab16eb191bda875ba559d98b1395d666439c12
Arkadiusz Adach <a.adach@yetiforce.com>
2019-06-18 09:46:41 +0200

    Refactoring the code, improving the formatting

--- public_html/layouts/basic/modules/PriceBooks/resources/RelatedList.js
+++ public_html/layouts/basic/modules/PriceBooks/resources/RelatedList.js
@@ -1,8 +1,8 @@
 /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
-"use strict";
+'use strict';
 
 Vtiger_RelatedList_Js(
-	"PriceBooks_RelatedList_Js",
+	'PriceBooks_RelatedList_Js',
 	{
 		triggerMassMargin: function() {
 			const self = Vtiger_RelatedList_Js.relatedListInstance;
@@ -10,9 +10,9 @@ Vtiger_RelatedList_Js(
 				let selectedIds = self.readSelectedIds(true);
 				app.showModalWindow({
 					url: 'index.php?module=PriceBooks&view=SpecifyMargin',
-					cb: () => {
-						$('.js-modal__save').on('click', e => {
-							let element = $('.js-margin');
+					cb: modalContainer => {
+						modalContainer.find('.js-modal__save').on('click', e => {
+							let element = modalContainer.find('.js-margin');
 							let resultOfValidation = Vtiger_NumberUserFormat_Validator_Js.invokeValidation(element);
 							if (typeof resultOfValidation !== 'undefined') {
 								element.validationEngine('showPrompt', resultOfValidation, '', 'topLeft', true);
@@ -26,7 +26,7 @@ Vtiger_RelatedList_Js(
 									relatedModule: self.moduleName,
 									selected_ids: selectedIds,
 									record: self.getParentId(),
-									margin: $('.js-margin').val()
+									margin: element.val()
 								}).done(response => {
 									if (response.success) {
 										Vtiger_Detail_Js.reloadRelatedList();
@@ -41,29 +41,29 @@ Vtiger_RelatedList_Js(
 			} else {
 				self.noRecordSelectedAlert();
 			}
-		},
+		}
 	},
 	{
 		registerEditListPrice: function() {
 			let thisInstance = this;
-			let element = this.content.find(".js-edit-listprice");
+			let element = this.content.find('.js-edit-listprice');
 			element.validationEngine(app.validationEngineOptions);
-			element.on("change", function(e) {
+			element.on('change', e => {
 				e.stopPropagation();
 				let element = $(this);
 				element.formatNumber();
-				if (!element.validationEngine("validate")) {
+				if (!element.validationEngine('validate')) {
 					AppConnector.request({
 						module: thisInstance.parentModuleName,
-						record: element.closest(".js-list__row").data("id"),
-						action: "RelationAjax",
-						mode: "addListPrice",
+						record: element.closest('.js-list__row').data('id'),
+						action: 'RelationAjax',
+						mode: 'addListPrice',
 						src_record: thisInstance.parentRecordId,
 						related_module: thisInstance.moduleName,
 						price: element.val()
 					}).done(function(responseData) {
 						if (responseData.result) {
-							Vtiger_Helper_Js.showPnotify({ text: app.vtranslate("JS_SAVE_NOTIFY_OK"), type: "success" });
+							Vtiger_Helper_Js.showPnotify({ text: app.vtranslate('JS_SAVE_NOTIFY_OK'), type: 'success' });
 						}
 					});
 				}
