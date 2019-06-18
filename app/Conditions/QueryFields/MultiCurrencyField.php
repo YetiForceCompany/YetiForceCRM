83754df85d7929b7acb3cb51a9fd593d2139661b
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2019-06-18 10:14:31 +0200

    Added minor improvements

---
+++ app/Conditions/QueryFields/MultiCurrencyField.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Multi currency Query Field Class.
+ *
+ * @package   App
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
+
+namespace App\Conditions\QueryFields;
+
+/**
+ * MultiCurrencyField Class.
+ */
+class MultiCurrencyField extends BaseField
+{
+}
