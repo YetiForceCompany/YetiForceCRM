2fcd90becf6924e95d08ad6618f85776c90d4f2f
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-06-24 10:33:18 +0200

    Updated and cleaned dependencies, cleaned code

--- public_html/src/gulpfile.js
+++ public_html/src/gulpfile.js
@@ -8,7 +8,6 @@
 
 const gulp = require('gulp')
 const stylus = require('gulp-stylus')
-const browserSync = require('browser-sync').create()
 
 const license =
 	'/* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */\n'
@@ -25,11 +24,11 @@ const stylusSrc = 'css/**/*.styl'
 function getCompileCssTask(src = stylusSrc) {
 	return function compileCssTask() {
 		return gulp
-			.src(src, { sourcemaps: true })
+			.src(src, {sourcemaps: true})
 			.pipe(stylus())
 			.pipe(
 				gulp.dest('./css'),
-				{ sourcemaps: true }
+				{sourcemaps: true}
 			)
 	}
 }
@@ -38,17 +37,3 @@ function getCompileCssTask(src = stylusSrc) {
  * Compile css task
  */
 gulp.task('compileCss', getCompileCssTask())
-/**
- * Start dev environment with browser-sync
- */
-gulp.task('dev', function() {
-	ModuleLoader.log = true
-	gulp.watch(stylusSrc).on('all', (eventName, fileName) => {
-		fileName = fileName.replace('\\', '/')
-		console.log(eventName, fileName)
-		gulp.series([getCompileCssTask()])(() => {
-			console.log(eventName, fileName, 'done')
-			browserSync.reload(fileName)
-		})
-	})
-})
