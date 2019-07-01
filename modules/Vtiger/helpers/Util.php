3ea8864b99772b71276e895c4a2f4e746d6b58f5
Tomasz Poradzewski <31520119+poradz@users.noreply.github.com>
2019-07-01 15:13:58 +0200

    Moved new theme styles to new 'gray' theme (#10849)
    * Moved changes by #10714 to different files
    
    * Moved changes by #10730 to new files
    
    * Moved changes by #10741 to new file
    
    * Added gray skin to user config
    
    * Added changes by #10831 to gray theme
    
    * Compiled css files
    
    * Reverted changes by #10830 in global tpl files

--- modules/Vtiger/helpers/Util.php
+++ modules/Vtiger/helpers/Util.php
@@ -1,5 +1,5 @@
 <?php
- /* +***********************************************************************************
+/* +***********************************************************************************
  * The contents of this file are subject to the vtiger CRM Public License Version 1.0
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
@@ -226,7 +226,7 @@ class Vtiger_Util_Helper
 	{
 		return [
 			'twilight' => '#404952',
-			//'modern' => '#0d9605'
+			'gray' => '#d1d1db'
 		];
 	}
 
