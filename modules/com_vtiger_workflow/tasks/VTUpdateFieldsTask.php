2c41b27ecbbcd9c35d5034482415cfcdaafa93dd
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2019-07-01 16:03:14 +0200

    Added minor improved to the fields from workflow tasks

--- modules/com_vtiger_workflow/tasks/VTUpdateFieldsTask.php
+++ modules/com_vtiger_workflow/tasks/VTUpdateFieldsTask.php
@@ -34,7 +34,7 @@ class VTUpdateFieldsTask extends VTTask
 		if (!empty($this->field_value_mapping)) {
 			$fieldValueMapping = \App\Json::decode($this->field_value_mapping);
 		}
-		if (!empty($fieldValueMapping) && count($fieldValueMapping) > 0) {
+		if (!empty($fieldValueMapping) && \count($fieldValueMapping) > 0) {
 			$isNew = $recordModel->isNew();
 			if ($isNew) {
 				$recordModel->isNew = false;
@@ -44,30 +44,22 @@ class VTUpdateFieldsTask extends VTTask
 				$fieldValueType = $fieldInfo['valuetype'];
 				$fieldValue = trim($fieldInfo['value']);
 				$fieldInstance = $moduleFields[$fieldName];
-				if ($fieldValueType === 'expression') {
+				if ('expression' === $fieldValueType) {
 					require_once 'modules/com_vtiger_workflow/expression_engine/include.php';
 					$parser = new VTExpressionParser(new VTExpressionSpaceFilter(new VTExpressionTokenizer($fieldValue)));
 					$expression = $parser->expression();
 					$exprEvaluater = new VTFieldExpressionEvaluater($expression);
 					$fieldValue = $exprEvaluater->evaluate($recordModel);
-					//for Product Unit Price value converted with based product currency
-					if ($fieldInstance && $fieldInstance->getFieldDataType() === 'currency' && $fieldName === 'unit_price') {
-						$fieldValue = $this->calculateProductUnitPrice($fieldValue);
-					}
-				} elseif ($fieldValueType === 'fieldname') {
+				} elseif ('fieldname' === $fieldValueType) {
 					$fieldValue = $recordModel->get($fieldValue);
 				} else {
 					if (preg_match('/([^:]+):boolean$/', $fieldValue, $match)) {
 						$fieldValue = $match[1];
-						if ($fieldValue == 'true') {
+						if ('true' == $fieldValue) {
 							$fieldValue = '1';
 						} else {
 							$fieldValue = '0';
 						}
-					}
-					//for Product Unit Price value converted with based product currency
-					if ($fieldInstance && $fieldInstance->getFieldDataType() === 'currency' && $fieldName === 'unit_price') {
-						$fieldValue = $this->calculateProductUnitPrice($fieldValue);
 					}
 				}
 				$recordModel->set($fieldName, App\Purifier::decodeHtml($fieldValue));
@@ -78,27 +70,5 @@ class VTUpdateFieldsTask extends VTTask
 				$recordModel->isNew = true;
 			}
 		}
-	}
-
-	/**
-	 * Function to calculate Product Unit Price.
-	 * Product Unit Price value converted with based product currency.
-	 *
-	 * @param type $fieldValue
-	 */
-	public function calculateProductUnitPrice($fieldValue)
-	{
-		$currency_details = \App\Fields\Currency::getAll(false);
-		$amountOfElements = count($currency_details);
-		for ($i = 0; $i < $amountOfElements; ++$i) {
-			$curid = $currency_details[$i]['curid'];
-			$cur_checkname = 'cur_' . $curid . '_check';
-			$cur_valuename = 'curname' . $curid;
-			if ($cur_valuename == \App\Request::_get('base_currency') && (\App\Request::_get($cur_checkname) == 'on' || \App\Request::_get($cur_checkname) == 1)) {
-				$fieldValue = $fieldValue * $currency_details[$i]['conversionrate'];
-				\App\Request::_set($cur_valuename, $fieldValue);
-			}
-		}
-		return $fieldValue;
 	}
 }
