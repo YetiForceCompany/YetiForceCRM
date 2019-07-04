6d042142b5c066cec8481152ff5f75d83d1be65d
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2019-07-04 09:25:04 +0200

    Library verification has been added

--- install/Install.php
+++ install/Install.php
@@ -16,8 +16,9 @@ foreach ($requiredVendors as $dir) {
 	}
 }
 // Adjust error_reporting favourable to deployment.
-include_once 'include/RequirementsValidation.php';
+$checkLibrary = true;
 require_once 'include/main/WebUI.php';
+include_once 'include/RequirementsValidation.php';
 require_once 'install/views/Index.php';
 require_once 'install/models/Utils.php';
 require_once 'install/models/InitSchema.php';
