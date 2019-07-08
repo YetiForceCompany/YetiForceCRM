78e6f0b2bd87672688283195b8f041a25db5553b
Adrian Kon <a.kon@yetiforce.com>
2019-07-08 07:42:40 +0200

    Added init variable

--- modules/Vtiger/inventoryfields/Unit.php
+++ modules/Vtiger/inventoryfields/Unit.php
@@ -23,6 +23,7 @@ class Vtiger_Unit_InventoryField extends Vtiger_Basic_InventoryField
 	 */
 	public function getDisplayValue($value, array $rowData = [], bool $rawText = false)
 	{
+		$value = '';
 		if (($rel = $rowData['name'] ?? '') && ($type = \App\Record::getType($rel) && $mapDetail = $this->getMapDetail($type))) {
 			$value = $mapDetail->getDisplayValue($value, false, false, $rawText);
 		}
