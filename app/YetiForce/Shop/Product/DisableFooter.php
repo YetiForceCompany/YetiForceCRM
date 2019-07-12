517e5d29492f57bfd2586483e490c7279b0c585c
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2019-07-12 11:09:52 +0200

    Added minor improvements

---
+++ app/YetiForce/Shop/Product/DisableFooter.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * YetiForce shop DisableFooter file.
+ *
+ * @package   App
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+
+namespace App\YetiForce\Shop\Product;
+
+/**
+ * YetiForce shop DisableFooter class.
+ */
+class DisableFooter extends \App\YetiForce\Shop\AbstractBaseProduct
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public $prices = [
+		's' => 5,
+		'm' => 15,
+		'l' => 30,
+		'xl' => 50,
+	];
+}
