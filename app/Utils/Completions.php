533b2641b1673175099f13dd34cc96734f94ad32
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-07-15 13:03:32 +0200

    Improved displaying completions

--- app/Utils/Completions.php
+++ app/Utils/Completions.php
@@ -69,7 +69,7 @@ class Completions
 		return \preg_replace_callback(
 			static::ROW_REGEX,
 			function (array $matches) use ($format) {
-				return static::decodeRow($matches[0], $matches[1], $matches[2], $format);
+				return static::decodeRow($matches[0], $matches[1], (int) $matches[2], $matches[3], $format);
 			},
 			$textOut
 		);
@@ -191,30 +191,31 @@ class Completions
 	 *
 	 * @param string $baseText
 	 * @param string $type
-	 * @param string $id
+	 * @param int $id
+	 * @param string $label
 	 * @param string $format
 	 *
 	 * @return string
 	 */
-	private static function decodeRow(string $baseText, string $type, string $id, string $format = self::FORMAT_HTML): string
+	private static function decodeRow(string $baseText, string $type, int $id, string $label, string $format = self::FORMAT_HTML): string
 	{
 		$html = '';
 		if ('#' === $type) {
 			switch ($format) {
 				case static::FORMAT_HTML:
-					$html = static::decodeRecord($id);
+					$html = static::decodeRecord($id, $label);
 					break;
 				case static::FORMAT_TEXT:
-					$html = static::decodeRecordText($id);
+					$html = static::decodeRecordText($id, $label);
 					break;
 			}
 		} elseif ('@' === $type) {
 			switch ($format) {
 				case static::FORMAT_HTML:
-					$html = static::decodeOwner($id);
+					$html = static::decodeOwner($id, $label);
 					break;
 				case static::FORMAT_TEXT:
-					$html = static::decodeOwnerText($id);
+					$html = static::decodeOwnerText($id, $label);
 					break;
 			}
 		} else {
@@ -227,15 +228,16 @@ class Completions
 	 * Display record text.
 	 *
 	 * @param int $recordId
+	 * @param string $recordLabel
 	 *
 	 * @return string
 	 */
-	private static function decodeRecordText(int $recordId): string
+	private static function decodeRecordText(int $recordId, string $recordLabel): string
 	{
 		if (\App\Record::isExists($recordId)) {
 			$html = \App\Record::getLabel($recordId);
 		} else {
-			$html = \App\Language::translate('LBL_RECORD_NOT_FOUND');
+			$html = static::deletedRecordTemplate($recordLabel);
 		}
 		return $html;
 	}
@@ -244,18 +246,19 @@ class Completions
 	 * Display record.
 	 *
 	 * @param int $recordId
+	 * @param string $recordLabel
 	 *
 	 * @return string
 	 */
-	private static function decodeRecord(int $recordId): string
+	private static function decodeRecord(int $recordId, string $recordLabel): string
 	{
 		if (!($moduleName = \App\Record::getType($recordId))) {
-			$html = \App\Language::translate('LBL_RECORD_NOT_FOUND');
+			$html = static::deletedRecordTemplate($recordLabel);
 		} elseif (\App\Privilege::isPermitted($moduleName, 'DetailView', $recordId)) {
 			$html = "<a href=\"index.php?module={$moduleName}&view=Detail&record={$recordId}\" class=\"js-popover-tooltip--record\" data-id=\"#{$recordId}\">" .
-				\App\Record::getLabel($recordId) . '</a>&nbsp;';
+				$recordLabel . '</a>&nbsp;';
 		} else {
-			$html = \App\Record::getLabel($recordId);
+			$html = $recordLabel;
 		}
 		return $html;
 	}
@@ -264,13 +267,14 @@ class Completions
 	 * Display owner.
 	 *
 	 * @param int $userId
+	 * @param string $recordLabel
 	 *
 	 * @return string
 	 */
-	private static function decodeOwner(int $userId): string
+	private static function decodeOwner(int $userId, string $recordLabel): string
 	{
 		if (!\App\User::isExists($userId)) {
-			$html = \App\Language::translate('LBL_RECORD_NOT_FOUND');
+			$html = static::deletedRecordTemplate($recordLabel);
 		} else {
 			$isRecordPermitted = \App\Privilege::isPermitted('Users', 'DetailView', $userId);
 			$popoverRecordClass = $isRecordPermitted ? 'js-popover-tooltip--record' : '';
@@ -286,16 +290,29 @@ class Completions
 	 * Display owner text.
 	 *
 	 * @param int $userId
+	 * @param string $recordLabel
 	 *
 	 * @return string
 	 */
-	private static function decodeOwnerText(int $userId): string
+	private static function decodeOwnerText(int $userId, string $recordLabel): string
 	{
 		if (\App\User::isExists($userId)) {
 			$html = \App\User::getUserModel($userId)->getName();
 		} else {
-			$html = \App\Language::translate('LBL_RECORD_NOT_FOUND');
+			$html = static::deletedRecordTemplate($recordLabel);
 		}
 		return $html;
+	}
+
+	/**
+	 * Display deleted record template.
+	 *
+	 * @param string $recordLabel
+	 *
+	 * @return string
+	 */
+	private static function deletedRecordTemplate(string $recordLabel): string
+	{
+		return "<span class=\"text-strike\">{$recordLabel}</span>";
 	}
 }
