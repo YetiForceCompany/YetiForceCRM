5a2413bcb0eafac4ab305762ea3becdc660dc34f
Davide Alghi <ciaky@penguinable.it>
2019-07-15 07:36:29 +0200

    Modified licence text

--- public_html/layouts/basic/modules/Settings/PDF/resources/Import.js
+++ public_html/layouts/basic/modules/Settings/PDF/resources/Import.js
@@ -1,12 +1,4 @@
-/*+***********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
- * Contributor(s): YetiForce.com
- *************************************************************************************/
+/* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
 'use strict';
 
 jQuery.Class('Settings_PDF_Import_Js', {}, {
