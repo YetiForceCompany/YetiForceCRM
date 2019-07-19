5a661c21cc5f318e3f5a8f9a19ca41ab72f556ae
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-07-19 10:30:28 +0200

    Saving shortcuts' sequence in local storage

--- public_html/layouts/basic/modules/Settings/Vtiger/resources/Index.js
+++ public_html/layouts/basic/modules/Settings/Vtiger/resources/Index.js
@@ -96,12 +96,9 @@ $.Class(
 		}
 	},
 	{
-		registerDeleteShortCutEvent: function(shortCutBlock) {
-			var thisInstance = this;
-			if (typeof shortCutBlock === 'undefined') {
-				shortCutBlock = $('div#settingsShortCutsContainer');
-			}
-			shortCutBlock.on('click', '.unpin', function(e) {
+		registerDeleteShortCutEvent: function(shortcutsContainer = $('#settingsShortCutsContainer')) {
+			shortcutsContainer.on('click', '.unpin', e => {
+				e.preventDefault();
 				var actionEle = $(e.currentTarget);
 				var closestBlock = actionEle.closest('.moduleBlock');
 				var fieldId = actionEle.data('id');
@@ -112,13 +109,14 @@ $.Class(
 						enabled: true
 					}
 				});
-				AppConnector.request(actionUrl).done(function(data) {
+				AppConnector.request(actionUrl).done(data => {
 					if (data.result.SUCCESS == 'OK') {
 						closestBlock.remove();
 						var menuItemId = '#' + fieldId + '_menuItem';
 						var shortCutActionEle = $(menuItemId);
 						var imagePath = shortCutActionEle.data('pinimageurl');
 						shortCutActionEle.attr('src', imagePath).data('action', 'pin');
+						this.updateShortcutsStorage(shortcutsContainer);
 						progressIndicatorElement.progressIndicator({
 							mode: 'hide'
 						});
@@ -130,37 +128,35 @@ $.Class(
 						Vtiger_Helper_Js.showPnotify(params);
 					}
 				});
-				e.stopPropagation();
 			});
 		},
 		registerPinShortCutEvent: function(element) {
-			var id = element.data('id');
-			var url =
+			const id = element.data('id');
+			const url =
 				'index.php?module=Vtiger&parent=Settings&action=Basic&mode=updateFieldPinnedStatus&pin=true&fieldid=' + id;
-			var progressIndicatorElement = $.progressIndicator({
+			const progressIndicatorElement = $.progressIndicator({
 				blockInfo: {
 					enabled: true
 				}
 			});
-			AppConnector.request(url).done(function(data) {
+			AppConnector.request(url).done(data => {
 				if (data.result.SUCCESS == 'OK') {
-					var params = {
+					AppConnector.request({
 						fieldid: id,
 						mode: 'getSettingsShortCutBlock',
 						module: 'Vtiger',
 						parent: 'Settings',
 						view: 'IndexAjax'
-					};
-					AppConnector.request(params).done(function(data) {
-						var existingDivBlock = $('#settingsShortCutsContainer');
-						$(data).appendTo(existingDivBlock);
+					}).done(data => {
+						const shortcutsContainer = $('#settingsShortCutsContainer');
+						$(data).appendTo(shortcutsContainer);
+						this.updateShortcutsStorage(shortcutsContainer);
 						progressIndicatorElement.progressIndicator({
 							mode: 'hide'
 						});
-						var params = {
+						Settings_Vtiger_Index_Js.showMessage({
 							text: app.vtranslate('JS_SUCCESSFULLY_PINNED')
-						};
-						Settings_Vtiger_Index_Js.showMessage(params);
+						});
 					});
 				}
 			});
@@ -184,7 +180,7 @@ $.Class(
 		},
 		registerAddShortcutDragDropEvent: function() {
 			var elements = $('.js-menu__item .js-menu__link--draggable');
-			var thisInstance = this;
+			var self = this;
 			var classes = 'ui-draggable-menuShortcut bg-warning';
 			elements.draggable({
 				containment: '#page',
@@ -218,11 +214,26 @@ $.Class(
 						};
 						Vtiger_Helper_Js.showPnotify(params);
 					} else {
-						thisInstance.registerPinShortCutEvent(ui.draggable.parent());
+						self.registerPinShortCutEvent(ui.draggable.parent());
 					}
 				}
 			});
-			shortcutsContainer.sortable();
+			shortcutsContainer.sortable({
+				stop: (event, element) => {
+					self.updateShortcutsStorage(shortcutsContainer);
+				}
+			});
+			if (Quasar.plugins.LocalStorage.has('yf-settings-shortcuts')) {
+				this.alignShortcuts(shortcutsContainer);
+			}
+		},
+		alignShortcuts(container) {
+			for (let item of Quasar.plugins.LocalStorage.getItem('yf-settings-shortcuts')) {
+				container.append(container.find('#' + item));
+			}
+		},
+		updateShortcutsStorage(container) {
+			Quasar.plugins.LocalStorage.set('yf-settings-shortcuts', container.sortable('toArray'));
 		},
 		loadEditorElement: function() {
 			new App.Fields.Text.Editor($('.js-editor'), {});
