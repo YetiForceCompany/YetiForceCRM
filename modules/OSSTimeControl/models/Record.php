5539017095c179870628c0a26f4ce0f50ddaf98d
Arkadiusz Adach <a.adach@yetiforce.com>
2019-07-22 15:45:33 +0200

    Removal of unnecessary code

--- modules/OSSTimeControl/models/Record.php
+++ modules/OSSTimeControl/models/Record.php
@@ -8,23 +8,6 @@
  */
 class OSSTimeControl_Record_Model extends Vtiger_Record_Model
 {
-	const RECALCULATE_STATUS = 'Accepted';
-
-	public static function recalculateTimeControl($id, $name)
-	{
-		$sumTime = (new App\Db\Query())->from('vtiger_osstimecontrol')
-			->innerJoin('vtiger_crmentity', 'vtiger_crmentity.crmid = vtiger_osstimecontrol.osstimecontrolid')
-			->where(['vtiger_crmentity.deleted' => 0, 'osstimecontrol_status' => self::RECALCULATE_STATUS, $name => $id])
-			->sum('sum_time');
-		$metaData = vtlib\Functions::getCRMRecordMetadata($id);
-		$moduleModel = Vtiger_Module_Model::getInstance($metaData['setype']);
-		$focus = $moduleModel->getEntityInstance();
-		if ($moduleModel->getFieldByColumn('sum_time')) {
-			App\Db::getInstance()->createCommand()->update($focus->table_name, ['sum_time' => round($sumTime, 2)], [$focus->table_index => $id])->execute();
-			\HelpDesk_CalculateSumOfExecutionTime_Model::calculate($id);
-		}
-	}
-
 	public function getDuplicateRecordUrl()
 	{
 		$module = $this->getModule();
