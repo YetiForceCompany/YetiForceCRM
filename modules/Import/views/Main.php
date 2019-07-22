19fb31b98f6d4e2f3e20ee123a9ff6192cb0c779
Arek Solek <a.solek@yetiforce.com>
2019-07-22 08:29:14 +0200

    Corrected Notice saving the mapping template during import (#11029)

--- modules/Import/views/Main.php
+++ modules/Import/views/Main.php
@@ -181,7 +181,7 @@ class Import_Main_View extends \App\Controller\View
 			$map['content'] = $saveMapping;
 			$map['module'] = $this->request->get('module');
 			$map['has_header'] = ($hasHeader) ? 1 : 0;
-			$map['assigned_user_id'] = $this->user->id;
+			$map['assigned_user_id'] = $this->user->getCurrentUserId();
 			(new Import_Map_Model($map))->save();
 		}
 	}
