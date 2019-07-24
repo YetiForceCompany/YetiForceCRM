a736ec58d9983760b605abfdbfd427db9bafc362
konadrian <a.kon@yetiforce.com>
2019-07-24 11:21:44 +0200

    Added tickets closing validation (#11049)
    * Added tickets closing validation
    
    * Imporved closed tickets
    
    * Fixed error while quickcreate
    
    * Added comments
    
    * Added improvements

---
+++ modules/HelpDesk/actions/CheckValidateToClose.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Checking Close Validation Action Class.
+ *
+ * @package   Action
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Adrian Kon <a.kon@yetiforce.com>
+ */
+class HelpDesk_CheckValidateToClose_Action extends \App\Controller\Action
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function checkPermission(App\Request $request)
+	{
+		$this->recordModel = Vtiger_Record_Model::getInstanceById($request->getInteger('record'), $request->getModule());
+		if (!$this->recordModel->isEditable()) {
+			throw new \App\Exceptions\NoPermitted('LBL_PERMISSION_DENIED', 406);
+		}
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function process(App\Request $request)
+	{
+		$response = new Vtiger_Response();
+		$response->setResult($this->recordModel->checkValidateToClose($request->getByType('status', 'Text')));
+		$response->emit();
+	}
+}
--- modules/HelpDesk/models/Record.php
+++ modules/HelpDesk/models/Record.php
@@ -74,4 +74,67 @@ class HelpDesk_Record_Model extends Vtiger_Record_Model
 		}
 		return $hierarchy;
 	}
+
+	/**
+	 * Function check if record can be closed.
+	 *
+	 * @param string $status
+	 *
+	 * @return array
+	 */
+	public function checkValidateToClose(string $status): array
+	{
+		if ((\App\Config::module($this->getModuleName(), 'CHECK_IF_RECORDS_HAS_TIME_CONTROL') ||
+		 \App\Config::module($this->getModuleName(), 'CHECK_IF_RELATED_TICKETS_ARE_CLOSED')) &&
+		  \in_array($status, \App\RecordStatus::getStates($this->getModuleName(), \App\RecordStatus::RECORD_STATE_CLOSED))) {
+			return [
+				'hasTimeControl' => [
+					'result' => $this->checkIfHasTimeControl(),
+					'message' => \App\Language::translate('LBL_ADD_TIME_CONTROL', $this->getModuleName())],
+				'relatedTicketsClosed' => [
+					'result' => $this->checkIfRelatedTicketsClosed(),
+					'message' => \App\Language::translate('LBL_CLOSE_RELATED_TICKETS', $this->getModuleName())]
+			];
+		}
+		return ['hasTimeControl' => ['result' => true], 'relatedTicketsClosed' => ['result' => true]];
+	}
+
+	/**
+	 * Function check if records has fill time control.
+	 *
+	 * @return bool
+	 */
+	public function checkIfHasTimeControl(): bool
+	{
+		if (\App\Config::module($this->getModuleName(), 'CHECK_IF_RECORDS_HAS_TIME_CONTROL')) {
+			$queryGenerator = new App\QueryGenerator('OSSTimeControl');
+			$queryGenerator->permissions = false;
+			$queryGenerator->addNativeCondition([\App\ModuleHierarchy::getMappingRelatedField($this->getModuleName()) => $this->getId()]);
+			return $queryGenerator->createQuery()->exists();
+		}
+		return true;
+	}
+
+	/**
+	 *Function check if related records are close.
+	 *
+	 * @return bool
+	 */
+	public function checkIfRelatedTicketsClosed(): bool
+	{
+		if (\App\Config::module($this->getModuleName(), 'CHECK_IF_RELATED_TICKETS_ARE_CLOSED')) {
+			$queryGenerator = new App\QueryGenerator($this->getModuleName());
+			$queryGenerator->permissions = false;
+			$queryGenerator->addNativeCondition(['parentid' => $this->getId()]);
+			$statusFieldName = \App\RecordStatus::getFieldName($this->getModuleName());
+			foreach (\App\RecordStatus::getStates($this->getModuleName(), \App\RecordStatus::RECORD_STATE_OPEN) as $openStatus) {
+				$queryGenerator->addCondition($statusFieldName, $openStatus, 'e', false);
+			}
+			foreach (\App\RecordStatus::getStates($this->getModuleName(), \App\RecordStatus::RECORD_STATE_NO_CONCERN) as $noConcernStatus) {
+				$queryGenerator->addCondition($statusFieldName, $noConcernStatus, 'e', false);
+			}
+			return !$queryGenerator->createQuery()->exists();
+		}
+		return true;
+	}
 }
