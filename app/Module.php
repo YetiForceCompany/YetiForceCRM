5d97b126e207a187642be367a2d96c44dff9ddd7
Arkadiusz Adach <a.adach@yetiforce.com>
2019-07-25 11:04:10 +0200

    test

--- app/Module.php
+++ app/Module.php
@@ -266,6 +266,7 @@ class Module
 					throw new Exceptions\NoPermitted("Cannot open file ($filename)");
 				}
 				$moduleMeta = static::getModuleMeta();
+				\var_dump($moduleMeta);
 				$newbuf = "<?php\n";
 				$newbuf .= '$tab_seq_array=' . Utils::varExport($moduleMeta['tabPresence']) . ";\n";
 				$newbuf .= 'return ' . Utils::varExport($moduleMeta) . ";\n";
