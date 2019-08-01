ba1d43e72de1e5fab06c01b4c5105ec67afc0e86
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-08-01 20:36:49 +0200

    Added mergeDeepReactive function to utils

--- public_html/src/layouts/basic/modules/Chat/utils/utils.js
+++ public_html/src/layouts/basic/modules/Chat/utils/utils.js
@@ -1,4 +1,11 @@
-const getGroupIcon = roomType => {
+/**
+ * Helper methods
+ *
+ * @description Helper methods
+ * @license YetiForce Public License 3.0
+ * @author Tomasz Poradzewski <t.poradzewski@yetiforce.com>
+ */
+export function getGroupIcon(roomType) {
 	switch (roomType) {
 		case 'crm':
 			return 'yfi-favorite-room'
@@ -9,4 +16,33 @@ const getGroupIcon = roomType => {
 	}
 }
 
-export { getGroupIcon }
+/**
+ * Helper function to merge multiple objects with reactivity enabled
+ *
+ * @param   {object}  target target object
+ * @param   {object[]}  source objects
+ *
+ * @return  {object}  merged object
+ */
+export function mergeDeepReactive(target, ...sources) {
+	if (!sources.length) {
+		return Vue.observable(target)
+	}
+	const isObject = item => {
+		return item && typeof item === 'object' && !Array.isArray(item)
+	}
+	const source = sources.shift()
+	if (isObject(target) && isObject(source)) {
+		for (const key in source) {
+			if (isObject(source[key])) {
+				if (typeof target[key] === 'undefined') {
+					Vue.set(target, key, {})
+				}
+				mergeDeepReactive(target[key], source[key])
+			} else {
+				Vue.set(target, key, source[key])
+			}
+		}
+	}
+	return mergeDeepReactive(target, ...sources)
+}
