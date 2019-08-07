7071526a0e0a7138ada0e6eccfaa01e5fbc27f9c
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-08-07 08:33:21 +0200

    Added icons action

---
+++ modules/Settings/Vtiger/actions/Icons.php
@@ -0,0 +1,27 @@
+<?php
+/* +**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.1
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * Contributor(s): YetiForce Sp. z o.o.
+ * ********************************************************************************** */
+
+class Settings_Vtiger_Icons_Action extends Settings_Vtiger_Basic_Action
+{
+	/**
+	 * Process.
+	 *
+	 * @param \App\Request $request
+	 *
+	 * @throws \ReflectionException
+	 */
+	public function process(\App\Request $request)
+	{
+		$response = new Vtiger_Response();
+		$response->setResult(Settings_Vtiger_Icons_Model::getAll());
+		$response->emit();
+	}
+}
