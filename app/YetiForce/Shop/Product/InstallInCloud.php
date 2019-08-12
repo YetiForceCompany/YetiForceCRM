9e93de79c704549ed9ad3b4808cfd64946718f1e
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2019-08-12 15:09:10 +0200

    Added improvements in shop

---
+++ app/YetiForce/Shop/Product/InstallInCloud.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * YetiForce shop InstallInCloud file.
+ *
+ * @package   App
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+
+namespace App\YetiForce\Shop\Product;
+
+/**
+ * YetiForce shop InstallInCloud class.
+ */
+class InstallInCloud extends \App\YetiForce\Shop\AbstractBaseProduct
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public $prices = [
+		'Micro' => 15,
+		'Small' => 25,
+		'Medium' => 50,
+		'Large' => 100,
+	];
+	/**
+	 * {@inheritdoc}
+	 */
+	public $featured = true;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function verify($cache = true): bool
+	{
+		return true;
+	}
+}
