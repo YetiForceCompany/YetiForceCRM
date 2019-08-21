195ece7b31ccfd4e861ec70a6fcc3e56c1d767a3
Arkadiusz Dudek <a.dudek@yetiforce.com>
2019-08-21 09:43:53 +0200

    Fixed #11094 (#11273)

--- install/install_schema/scheme.sql
+++ install/install_schema/scheme.sql
@@ -5562,7 +5562,7 @@ CREATE TABLE `vtiger_field` (
   KEY `field_sequence_idx` (`sequence`),
   KEY `field_uitype_idx` (`uitype`),
   CONSTRAINT `fk_1_vtiger_field` FOREIGN KEY (`tabid`) REFERENCES `vtiger_tab` (`tabid`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2830 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2832 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `vtiger_fieldmodulerel` */
 
@@ -8785,6 +8785,8 @@ CREATE TABLE `vtiger_users` (
   `sync_carddav_default_country` varchar(255) DEFAULT NULL,
   `default_search_module` varchar(25) DEFAULT NULL,
   `default_search_override` tinyint(1) DEFAULT NULL,
+  `primary_phone` varchar(50) DEFAULT NULL,
+  `primary_phone_extra` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email1` (`email1`),
   KEY `user_user_name_idx` (`user_name`),
