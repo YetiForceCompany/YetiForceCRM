db8d706d46dfb8d50fe6893792e9a4be2da6bdce
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-09-06 13:30:37 +0200

    Corrected s_yf_address_finder_config table

--- modules/Settings/ApiAddress/actions/SaveConfig.php
+++ modules/Settings/ApiAddress/actions/SaveConfig.php
@@ -11,8 +11,7 @@ class Settings_ApiAddress_SaveConfig_Action extends Settings_Vtiger_Basic_Action
 	public function process(App\Request $request)
 	{
 		$moduleName = $request->getModule(false);
-		$value = $request->isEmpty('elements') ? $request->getArray() : '';
-		$result = Settings_ApiAddress_Module_Model::getInstance($moduleName)->setConfig($request->getArray('elements', 'Alnum'));
+		$result = Settings_ApiAddress_Module_Model::getInstance($moduleName)->setConfig($request->getArray('elements'));
 
 		if ($result) {
 			$result = ['success' => true, 'message' => \App\Language::translate('LBL_SAVE_NOTIFY_OK', $moduleName)];
