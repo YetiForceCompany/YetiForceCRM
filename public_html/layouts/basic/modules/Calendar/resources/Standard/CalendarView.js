f9467760f9ee42c8c18369c28d412cdc85e5d09c
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-09-10 16:31:33 +0200

    Corrected calendar standard permission

--- public_html/layouts/basic/modules/Calendar/resources/Standard/CalendarView.js
+++ public_html/layouts/basic/modules/Calendar/resources/Standard/CalendarView.js
@@ -22,6 +22,7 @@ window.Calendar_Calendar_Js = class extends Calendar_Js {
 		let self = this,
 			options = {
 				selectable: self.eventCreate,
+				editable: self.eventEdit,
 				select: function(start, end) {
 					self.selectDays(start, end);
 					self.getCalendarView().fullCalendar('unselect');
