a24445700c37905948bc2b4392a8fa97e4ef8422
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-09-11 10:28:25 +0200

    Corrected comments

--- modules/Users/views/Detail.php
+++ modules/Users/views/Detail.php
@@ -1,18 +1,16 @@
 <?php
-/* +***********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
- * Contributor(s): YetiForce.com
- * *********************************************************************************** */
+/**
+ * Users detail view class.
+ *
+ * @package   View
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Tomasz Poradzewski <t.poradzewski@yetiforce.com>
+ */
 
 /**
  * Class Users_Detail_View.
- *
- * @package View
  */
 class Users_Detail_View extends Vtiger_Detail_View
 {
