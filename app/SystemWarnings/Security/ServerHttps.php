c98f76b88f5fb98a6991a42e9813e8dc9e775235
Arek Solek <a.solek@yetiforce.com>
2019-09-20 14:10:58 +0200

    Deleted translation (#11557)

--- app/SystemWarnings/Security/ServerHttps.php
+++ app/SystemWarnings/Security/ServerHttps.php
@@ -33,7 +33,7 @@ class ServerHttps extends \App\SystemWarnings\Template
 		if (0 === $this->status) {
 			$this->link = 'https://yetiforce.com/en/knowledge-base/documentation/implementer-documentation/item/web-server-requirements';
 			$this->linkTitle = \App\Language::translate('BTN_CONFIGURE_HTTPS', 'Settings:SystemWarnings');
-			$this->description = \App\Language::translateArgs('LBL_MISSING_HTTPS', 'Settings:SystemWarnings', '<a target="_blank" rel="noreferrer noopener" href="' . \App\Language::translate('LBL_CONFIG_DOC_URL', 'Settings:SystemWarnings') . '"><u>' . \App\Language::translate('LBL_CONFIG_DOC_URL_LABEL', 'Settings:SystemWarnings') . '</u></a>');
+			$this->description = \App\Language::translateArgs('LBL_MISSING_HTTPS', 'Settings:SystemWarnings', '<a target="_blank" rel="noreferrer noopener" href="https://yetiforce.com/en/knowledge-base/documentation/implementer-documentation/item/web-server-requirement"><u>' . \App\Language::translate('LBL_CONFIG_DOC_URL_LABEL', 'Settings:SystemWarnings') . '</u></a>');
 		}
 	}
 }
