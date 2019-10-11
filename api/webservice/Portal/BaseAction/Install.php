5e8dd469d4f33ca68f2328673e3e2d1d9011dc1a
Arek Solek <a.solek@yetiforce.com>
2019-10-11 12:44:05 +0200

    Development of interactive OpenAPI documentation. (#11717)
    * Development of interactive OpenAPI documentation.
    
    * Corrected commit
    
    * Changed description

--- api/webservice/Portal/BaseAction/Install.php
+++ api/webservice/Portal/BaseAction/Install.php
@@ -26,9 +26,59 @@ class Install extends \Api\Core\BaseAction
 	}
 
 	/**
-	 * Get modules list.
+	 * Put method.
 	 *
-	 * @return string[]
+	 * @return array
+	 *
+	 * @OA\Put(
+	 *		path="/webservice/BaseAction/Install",
+	 *		summary="Install the system",
+	 *		tags={"BaseAction"},
+	 *		security={
+	 *			{"basicAuth" : "", "ApiKeyAuth" : ""}
+	 *    },
+	 *		@OA\RequestBody(
+	 *  			required=false,
+	 *  			description="Base action install request body",
+	 *	  ),
+	 *    @OA\Parameter(
+	 *        name="X-ENCRYPTED",
+	 *        in="header",
+	 *        required=true,
+	 * 				@OA\Schema(ref="#/components/schemas/X-ENCRYPTED")
+	 *    ),
+	 *		@OA\Response(
+	 *				response=200,
+	 *				description="Base action details",
+	 *				@OA\JsonContent(ref="#/components/schemas/BaseActionInstallResponseBody"),
+	 *				@OA\XmlContent(ref="#/components/schemas/BaseActionInstallResponseBody"),
+	 *     		@OA\MediaType(
+	 *         		mediaType="text/html",
+	 *         		@OA\Schema(ref="#/components/schemas/BaseActionInstallResponseBody")
+	 *     		),
+	 *		),
+	 * ),
+	 * @OA\Schema(
+	 * 		schema="BaseActionInstallResponseBody",
+	 * 		title="Base action install",
+	 * 		description="The representation of a base action install",
+	 *		type="object",
+	 *  	@OA\Property(
+	 *       	property="status",
+	 *        description="A numeric value of 0 or 1 that indicates whether the communication is valid. 1 - sukcess , 0 - error",
+	 * 				enum={"0", "1"},
+	 *     	  type="integer",
+	 * 		),
+	 *    @OA\Property(
+	 *     	  property="result",
+	 *     	 	description="Content of responses from a given method",
+	 *    	 	type="array"
+	 *    ),
+	 * ),
+	 * @OA\Tag(
+	 *   name="BaseAction",
+	 *   description="Access to user methods"
+	 * )
 	 */
 	public function put()
 	{
--- api/webservice/Portal/Users/Logout.php
+++ api/webservice/Portal/Users/Logout.php
@@ -39,6 +39,7 @@ class Logout extends \Api\Core\BaseAction
 	 *    },
 	 *		@OA\RequestBody(
 	 *  			required=false,
+	 * 				description="Users logout request body",
 	 *	  ),
 	 *    @OA\Parameter(
 	 *        name="X-ENCRYPTED",
