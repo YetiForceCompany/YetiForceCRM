d326835fbc9853d4545b1a87ea5f39d300ac5df5
Arek Solek <a.solek@yetiforce.com>
2019-10-16 13:50:03 +0200

    Development of interactive OpenAPI documentation. (#11745)
    * Development of interactive OpenAPI documentation.
    
    * Corrected commit
    
    * Added response code and changed description
    
    * Changed descriptions
    
    * Corrected the object define

--- api/webservice/Portal/BaseAction/Modules.php
+++ api/webservice/Portal/BaseAction/Modules.php
@@ -15,9 +15,64 @@ class Modules extends \Api\Core\BaseAction
 	public $allowedMethod = ['GET'];
 
 	/**
-	 * Get modules list.
+	 * Get method.
 	 *
-	 * @return string[]
+	 * @return array
+	 *
+	 * @OA\Get(
+	 *		path="/webservice/BaseAction/Modules",
+	 *		summary="Gets the module list action, along with their translated action",
+	 *		tags={"BaseAction"},
+	 *		security={
+	 *			{"basicAuth" : "", "ApiKeyAuth" : "", "token" : ""}
+	 *		},
+	 *		@OA\RequestBody(
+	 *				required=false,
+	 *				description="The content of the request is empty",
+	 *		),
+	 *		@OA\Parameter(
+	 *			name="X-ENCRYPTED",
+	 *			in="header",
+	 *			required=true,
+	 *				@OA\Schema(ref="#/components/schemas/X-ENCRYPTED")
+	 *		),
+	 *		@OA\Response(
+	 *			response=200,
+	 *			description="List of active modules",
+	 *			@OA\JsonContent(ref="#/components/schemas/BaseActionModulesResponseBody"),
+	 *			@OA\XmlContent(ref="#/components/schemas/BaseActionModulesResponseBody"),
+	 *			@OA\MediaType(
+	 *				mediaType="text/html",
+	 *				@OA\Schema(ref="#/components/schemas/BaseActionModulesResponseBody")
+	 *			),
+	 *		),
+	 *		@OA\Response(
+	 *				response=401,
+	 *				description="No sent token OR Invalid token",
+	 *		),
+	 *		@OA\Response(
+	 *				response=403,
+	 *				description="No permissions for module",
+	 *		),
+	 * ),
+	 * @OA\Schema(
+	 *		schema="BaseActionModulesResponseBody",
+	 *		title="List items",
+	 *		description="List of obtained items",
+	 *		type="object",
+	 *		@OA\Property(
+	 *			property="status",
+	 *			description="A numeric value of 0 or 1 that indicates whether the communication is valid. 1 - sukcess , 0 - error",
+	 *			enum={"0", "1"},
+	 *			type="integer",
+	 *		),
+	 *		@OA\Property(
+	 *			property="result",
+	 *			description="List of modules accessed",
+	 *			type="object",
+	 *			@OA\Property(property="items", description="Module name", type="string", example="Accounts"),
+	 * 		),
+	 *	),
 	 */
 	public function get()
 	{
