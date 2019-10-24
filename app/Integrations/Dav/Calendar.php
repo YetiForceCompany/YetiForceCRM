54bee0123b20806607c047ca50d1000d6491b0aa
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2019-10-24 12:20:16 +0200

    Optimized and rebuilt functions for finding relationships with e-mail messages

--- app/Integrations/Dav/Calendar.php
+++ app/Integrations/Dav/Calendar.php
@@ -730,13 +730,13 @@ class Calendar
 					throw new \Sabre\DAV\Exception\BadRequest('Invalid email');
 				}
 				if ('CHAIR' === $attendee['ROLE']->getValue()) {
-					$users = \App\Fields\Email::findCrmidByEmail($value, ['Users']);
+					$users = $this->findRecordByEmail($value, ['Users']);
 					if (!empty($users)) {
 						continue;
 					}
 				}
 				$crmid = 0;
-				$records = \App\Fields\Email::findCrmidByEmail($value, array_keys(array_merge(\App\ModuleHierarchy::getModulesByLevel(), \App\ModuleHierarchy::getModulesByLevel(4))));
+				$records = $this->findRecordByEmail($value, array_keys(array_merge(\App\ModuleHierarchy::getModulesByLevel(), \App\ModuleHierarchy::getModulesByLevel(4))));
 				if (!empty($records)) {
 					$recordCrm = current($records);
 					$crmid = $recordCrm['crmid'];
@@ -919,5 +919,60 @@ class Calendar
 			'lastOccurence' => $lastOccurence,
 			'uid' => $uid,
 		];
+	}
+
+	/**
+	 * Find crm id by email.
+	 *
+	 * @param int|string $value
+	 * @param array      $allowedModules
+	 * @param array      $skipModules
+	 *
+	 * @return array
+	 */
+	public function findRecordByEmail($value, array $allowedModules = [], array $skipModules = [])
+	{
+		$db = \App\Db::getInstance();
+		$rows = $fields = [];
+		$dataReader = (new \App\Db\Query())->select(['vtiger_field.columnname', 'vtiger_field.tablename', 'vtiger_field.fieldlabel', 'vtiger_field.tabid', 'vtiger_tab.name'])
+			->from('vtiger_field')->innerJoin('vtiger_tab', 'vtiger_field.tabid = vtiger_tab.tabid')
+			->where(['vtiger_tab.presence' => 0])
+			->andWhere(['<>', 'vtiger_field.presence', 1])
+			->andWhere(['or', ['uitype' => 13], ['uitype' => 104]])->createCommand()->query();
+		while ($row = $dataReader->read()) {
+			$fields[$row['name']][$row['tablename']][$row['columnname']] = $row;
+		}
+		$queryUnion = null;
+		foreach ($fields as $moduleName => $moduleFields) {
+			if (($allowedModules && !\in_array($moduleName, $allowedModules)) || \in_array($moduleName, $skipModules)) {
+				continue;
+			}
+			$instance = \CRMEntity::getInstance($moduleName);
+			$isEntityType = isset($instance->tab_name_index['vtiger_crmentity']);
+			foreach ($moduleFields as $tablename => $columns) {
+				$tableIndex = $instance->tab_name_index[$tablename];
+				$query = (new \App\Db\Query())->select(['crmid' => $tableIndex, 'modules' => new \yii\db\Expression($db->quoteValue($moduleName))])
+					->from($tablename);
+				if ($isEntityType) {
+					$query->innerJoin('vtiger_crmentity', "vtiger_crmentity.crmid = {$tablename}.{$tableIndex}")->where(['vtiger_crmentity.deleted' => 0]);
+				}
+				$orWhere = ['or'];
+				foreach ($columns as $row) {
+					$orWhere[] = ["{$row['tablename']}.{$row['columnname']}" => $value];
+				}
+				$query->andWhere($orWhere);
+				if ($queryUnion) {
+					$queryUnion->union($query);
+				} else {
+					$queryUnion = $query;
+				}
+			}
+		}
+		$rows = $queryUnion->all();
+		$labels = \App\Record::getLabel(array_column($rows, 'crmid'));
+		foreach ($rows as &$row) {
+			$row['label'] = $labels[$row['crmid']];
+		}
+		return $rows;
 	}
 }
