885c199b9202dea438803e86aee037256782b111
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2019-11-06 11:29:13 +0100

    Added to search the address book

--- modules/MailIntegration/actions/Mail.php
+++ modules/MailIntegration/actions/Mail.php
@@ -24,6 +24,7 @@ class MailIntegration_Mail_Action extends \App\Controller\Action
 		parent::__construct();
 		$this->exposeMethod('deleteRelation');
 		$this->exposeMethod('addRelation');
+		$this->exposeMethod('findEmail');
 	}
 
 	/**
@@ -34,11 +35,14 @@ class MailIntegration_Mail_Action extends \App\Controller\Action
 		if (!Users_Privileges_Model::getCurrentUserPrivilegesModel()->hasModulePermission($request->getModule())) {
 			throw new \App\Exceptions\NoPermitted('LBL_PERMISSION_DENIED', 406);
 		}
-		if (!\App\Privilege::isPermitted('OSSMailView', 'DetailView', $request->getInteger('mailId'))) {
-			throw new \App\Exceptions\NoPermittedToRecord('ERR_NO_PERMISSIONS_FOR_THE_RECORD', 406);
-		}
-		if (!\App\Privilege::isPermitted($request->getByType('recordModule', 'Alnum'), 'DetailView', $request->getInteger('record'))) {
-			throw new \App\Exceptions\NoPermittedToRecord('ERR_NO_PERMISSIONS_FOR_THE_RECORD', 406);
+		$mode = $request->getMode();
+		if ('deleteRelation' === $mode || 'addRelation' === $mode) {
+			if (!\App\Privilege::isPermitted('OSSMailView', 'DetailView', $request->getInteger('mailId'))) {
+				throw new \App\Exceptions\NoPermittedToRecord('ERR_NO_PERMISSIONS_FOR_THE_RECORD', 406);
+			}
+			if (!\App\Privilege::isPermitted($request->getByType('recordModule', 'Alnum'), 'DetailView', $request->getInteger('record'))) {
+				throw new \App\Exceptions\NoPermittedToRecord('ERR_NO_PERMISSIONS_FOR_THE_RECORD', 406);
+			}
 		}
 	}
 
@@ -80,6 +84,31 @@ class MailIntegration_Mail_Action extends \App\Controller\Action
 		$result = $relationModel->addRelation($request->getInteger('mailId'), $request->getInteger('record'));
 		$response = new Vtiger_Response();
 		$response->setResult($result);
+		$response->emit();
+	}
+
+	/**
+	 * Find the email address in the address book.
+	 *
+	 * @param App\Request $request
+	 *
+	 * @return void
+	 */
+	public function findEmail(App\Request $request)
+	{
+		$search = $request->getByType('search', 'Text');
+		$contacts = [];
+		$addressBookFile = ROOT_DIRECTORY . '/cache/addressBook/mails_' . App\User::getCurrentUserRealId() . '.php';
+		if (is_file($addressBookFile)) {
+			include $addressBookFile;
+			foreach ($bookMails as $row) {
+				if (false !== stripos($row, $search)) {
+					$contacts[] = $row;
+				}
+			}
+		}
+		$response = new Vtiger_Response();
+		$response->setResult($contacts);
 		$response->emit();
 	}
 }
