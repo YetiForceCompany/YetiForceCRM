9c61fa46d98e00571665f0421fc64e4233bacf88
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-11-07 18:49:12 +0100

    Improved copying e-mails to outlook

--- layouts/basic/modules/MailIntegration/MessageCompose.tpl
+++ layouts/basic/modules/MailIntegration/MessageCompose.tpl
@@ -1,6 +1,9 @@
 {*<!-- {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} -->*}
 {strip}
 <!-- tpl-MailIntegration-MessageCompose -->
-	<input type="text" class="form-control " name="fname" style="width: 94%;margin: 10px;">
+<div class="d-flex flex-column p-1">
+	<label for="searchMail">{\App\Language::translate('LBL_SEARCH_AND_COPY_EMAIL', $MODULE_NAME)}</label>
+	<input id="searchMail" type="text" class="form-control js-search-input" name="searchInput" placeholder="{\App\Language::translate('LBL_SEARCH_EMAIL', $MODULE_NAME)}">
+</div>
 <!-- /tpl-MailIntegration-MessageCompose -->
 {/strip}
--- public_html/layouts/basic/modules/MailIntegration/resources/outlookMessageCompose.js
+++ public_html/layouts/basic/modules/MailIntegration/resources/outlookMessageCompose.js
@@ -1,34 +1,34 @@
 Office.onReady(info => {
 	if (info.host === Office.HostType.Outlook) {
 		const listItemTemplate = user => {
-			let itemData = user.split(' <');
-			const userName = itemData[0];
-			const userMail = itemData[1].slice(0, -1);
 			return `
-			<li class="c-search-item">
+			<li class="c-search-item js-search-item">
 				<div class="d-flex flex-nowrap">
 					<div class="d-flex flex-wrap">
 						<div>
-							${userName}
+							${user.name}
 						</div>
 						<div>
-							${userMail}
+							${user.mail}
 						</div>
 					</div>
 					<div class="btn-group flex-nowrap align-items-center">
-						<button class="c-search-item__btn btn btn-xs btn-outline-primary">DW</button>
-						<button class="c-search-item__btn btn btn-xs btn-outline-primary">UDW</button>
+						<button class="c-search-item__btn btn btn-xs btn-outline-primary" data-copy-target="cc">
+							${app.vtranslate('JS_CC')}
+						</button>
+						<button class="c-search-item__btn btn btn-xs btn-outline-primary" data-copy-target="bcc">
+							${app.vtranslate('JS_BCC')}
+						</button>
 					</div>
 				</div>
 			</li>`;
 		};
 		$.widget('ui.autocomplete', $.ui.autocomplete, {
 			_renderItem: function(ul, item) {
-				console.log(item);
-				return $(listItemTemplate(item.label)).appendTo(ul);
+				return $(listItemTemplate(item)).appendTo(ul);
 			}
 		});
-		$('.form-control').autocomplete({
+		$('.js-search-input').autocomplete({
 			delay: '600',
 			minLength: '3',
 			classes: {
@@ -42,18 +42,34 @@ Office.onReady(info => {
 					search: request.term
 				})
 					.done(resp => {
-						response(resp.result);
+						const data = resp.result.map(user => {
+							let userData = user.split(' <');
+							const name = userData[0];
+							const mail = userData[1].slice(0, -1);
+							return { name, mail };
+						});
+						response(data);
 					})
 					.fail(function(error) {
 						console.error(error);
 					});
 			},
-			select: function(event, ui) {
-				var selectedItemData = ui.item;
-				if (typeof selectedItemData.type !== 'undefined' && selectedItemData.type == 'no results') {
-					return false;
-				}
-				selectedItemData.name = selectedItemData.value;
+			select: function({ toElement }, { item }) {
+				const newRecipient = [
+					{
+						displayName: item.name,
+						emailAddress: item.mail
+					}
+				];
+				const recipientsField = toElement.dataset.copyTarget ? toElement.dataset.copyTarget : 'to';
+				Office.context.mailbox.item[recipientsField].addAsync(newRecipient, function(result) {
+					if (result.error) {
+						Office.context.mailbox.item.notificationMessages.replaceAsync('error', {
+							type: 'errorMessage',
+							message: app.vtranslate('JS_ERROR') + ' ' + result.error
+						});
+					}
+				});
 			}
 		});
 	}
