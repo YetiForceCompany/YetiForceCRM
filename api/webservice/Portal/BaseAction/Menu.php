8362ee3d1936ede985e7748d5d64e0cf14e9b690
Arek Solek <a.solek@yetiforce.com>
2019-11-08 07:34:51 +0100

    Development of interactive OpenAPI documentation. (#11743)
    * Development of interactive OpenAPI documentation.
    
    * Changed summary
    
    * corrected commit
    
    * Corrected commit

--- api/webservice/Portal/BaseAction/Menu.php
+++ api/webservice/Portal/BaseAction/Menu.php
@@ -18,9 +18,77 @@ class Menu extends \Api\Core\BaseAction
 	public $allowedMethod = ['GET'];
 
 	/**
-	 * Get modules list.
+	 * Get method.
 	 *
-	 * @return string[]
+	 * @return array
+	 *
+	 * @OA\Get(
+	 *		path="/webservice/BaseAction/Menu",
+	 *		summary="Base action menu into the system",
+	 *		tags={"BaseAction"},
+	 *		security={
+	 *			{"basicAuth" : "", "ApiKeyAuth" : "", "token" : ""}
+	 *    },
+	 *		@OA\RequestBody(
+	 *  			required=false,
+	 *  			description="Request body does not occur",
+	 *	  ),
+	 *    @OA\Parameter(
+	 *        name="X-ENCRYPTED",
+	 *        in="header",
+	 *        required=true,
+	 * 				@OA\Schema(ref="#/components/schemas/X-ENCRYPTED")
+	 *    ),
+	 *		@OA\Response(
+	 *				response=200,
+	 *				description="Menu details",
+	 *				@OA\JsonContent(ref="#/components/schemas/BaseActionMenuResponseBody"),
+	 *				@OA\XmlContent(ref="#/components/schemas/BaseActionMenuResponseBody"),
+	 *     		@OA\MediaType(
+	 *         		mediaType="text/html",
+	 *         		@OA\Schema(ref="#/components/schemas/BaseActionMenuResponseBody")
+	 *     		),
+	 *		),
+	 * ),
+	 * @OA\Schema(
+	 * 		schema="BaseActionMenuResponseBody",
+	 * 		title="Base action menu",
+	 * 		description="Base action menu response body",
+	 *		type="object",
+	 *  	@OA\Property(
+	 *       	property="status",
+	 *        description="A numeric value of 0 or 1 that indicates whether the communication is valid. 1 - sukcess , 0 - error",
+	 * 				enum={"0", "1"},
+	 *     	  type="integer",
+	 * 		),
+	 *    @OA\Property(
+	 *     	  property="result",
+	 *     	 	description="Menu items selected in the system, consists of parents and children",
+	 *    	 	type="object",
+	 * 				@OA\Property(
+	 * 					property="items",
+	 * 					type="object",
+	 * 					title="Parent parameters",
+	 * 					@OA\Property(property="id", type="integer"),
+	 * 					@OA\Property(property="tabid", type="integer"),
+	 * 					@OA\Property(property="mod", type="string"),
+	 * 					@OA\Property(property="name", type="string"),
+	 * 					@OA\Property(property="type", type="string"),
+	 * 					@OA\Property(property="sequence", type="integer"),
+	 * 					@OA\Property(property="newwindow", type="integer"),
+	 * 					@OA\Property(property="dataurl", type="string"),
+	 * 					@OA\Property(property="icon", type="string"),
+	 * 					@OA\Property(property="parent", type="integer"),
+	 * 					@OA\Property(property="hotkey", type="string"),
+	 * 					@OA\Property(property="filters", type="string"),
+	 * 					@OA\Property(
+	 * 						property="childs",
+	 * 						type="object",
+	 * 						title="Children parameters",
+	 * 				),
+	 * 			),
+	 *    ),
+	 * ),
 	 */
 	public function get()
 	{
