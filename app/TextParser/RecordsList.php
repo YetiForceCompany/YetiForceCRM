86809be5510561c1ece33e3fe8fae83fe4514afc
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2019-11-22 04:52:39 +0100

    Added minor improvements

--- app/TextParser/RecordsList.php
+++ app/TextParser/RecordsList.php
@@ -2,6 +2,8 @@
 /**
  * Records list table.
  *
+ * @package App
+ *
  * @copyright YetiForce Sp. z o.o
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
