2e4b30525228ca8c543a223719e647cb4c19c3f8
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-11-27 11:55:55 +0100

    Fixed #12139

---
+++ public_html/src/layouts/basic/modules/Chat/components/Rooms/RoomUser.vue
@@ -0,0 +1,32 @@
+<!-- /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */ -->
+<template>
+  <RoomList
+    :filterRooms="filterRooms"
+    :roomData="roomData"
+    :roomType="roomType"
+  />
+</template>
+<script>
+import RoomList from './RoomList.vue'
+import { createNamespacedHelpers } from 'vuex'
+const { mapGetters, mapMutations, mapActions } = createNamespacedHelpers('Chat')
+export default {
+  name: 'RoomUser',
+  components: { RoomList },
+  props: {
+    roomData: {
+      type: Array,
+      required: true
+    },
+    roomType: {
+      type: String,
+      required: true
+    },
+    filterRooms: {
+      type: String,
+      required: true
+    }
+	}
+}
+</script>
+<style lang="sass" scoped></style>
