da3c716ed7d3be3b7aa292991e4895bf350cdd11
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2019-12-11 16:15:42 +0100

    Hid overlay scrollbar on calendar view

--- public_html/layouts/resources/app.js
+++ public_html/layouts/resources/app.js
@@ -354,6 +354,13 @@ var App = (window.App = {
 			},
 			initPage() {
 				let scrollbarContainer = $('.mainBody');
+				if (
+					scrollbarContainer.closest('[class$=calendarextended-container], [class$=calendar-container]')
+						.length &&
+					$(window).width() > app.breakpoints.lg
+				) {
+					return;
+				}
 				if (!scrollbarContainer.length) {
 					scrollbarContainer = $('body');
 				}
