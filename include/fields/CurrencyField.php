2c45512e0c69b51a9681d494377041c98286602e
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2020-01-03 15:26:59 +0100

    Fixed CurrencyField.php (#12326 , #11662)

--- include/fields/CurrencyField.php
+++ include/fields/CurrencyField.php
@@ -233,7 +233,7 @@ class CurrencyField
 				break;
 			case '$1.0':
 			default:
-				$returnValue = html_entity_decode($currencySymbol, ENT_COMPAT, $defaultCharset) . ' ' . $currencyValue;
+				$returnValue = html_entity_decode($currencySymbol, ENT_COMPAT) . ' ' . $currencyValue;
 		}
 		return $returnValue;
 	}
