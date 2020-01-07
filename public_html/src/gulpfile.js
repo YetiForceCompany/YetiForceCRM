3750467a8c81517b855f0e4fa8345e66a7ae0235
Tomasz Poradzewski <t.poradzewski@yetiforce.com>
2020-01-07 22:04:49 +0100

    Added minify-css task to gulpfile.js

--- public_html/src/gulpfile.js
+++ public_html/src/gulpfile.js
@@ -10,6 +10,8 @@ const gulp = require('gulp')
 const stylus = require('gulp-stylus')
 const cleanCSS = require('gulp-clean-css')
 const autoprefixer = require('gulp-autoprefixer')
+const sourcemaps = require('gulp-sourcemaps')
+const rename = require('gulp-rename')
 
 const license =
 	'/* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */\n'
@@ -48,8 +50,45 @@ function getCompileCssTask(src = stylusSrc) {
 			.pipe(gulp.dest('./css'), { sourcemaps: true })
 	}
 }
-
 /**
- * Compile css task
+ * Compile .css file
+ *
+ * @param {string|array} src
+ *
+ * @returns {function} task
  */
+function getMinifyCssTask() {
+	return function minifyCssTask() {
+		const stylesPath = '../layouts/basic/styles/'
+		return gulp
+			.src(`${stylesPath}Main.css`)
+			.pipe(sourcemaps.init())
+			.pipe(
+				rename({
+					suffix: '.min'
+				})
+			)
+			.pipe(
+				autoprefixer(
+					'safari 6',
+					'ios 7',
+					'ie 11',
+					'last 2 Chrome versions',
+					'last 2 Firefox versions',
+					'Explorer >= 11',
+					'last 1 Edge versions'
+				)
+			)
+			.pipe(
+				cleanCSS({}, details => {
+					console.log(`${details.name}: ${details.stats.originalSize}`)
+					console.log(`${details.name}: ${details.stats.minifiedSize}`)
+				})
+			)
+			.pipe(sourcemaps.write())
+			.pipe(gulp.dest(stylesPath))
+	}
+}
+
 gulp.task('compileCss', getCompileCssTask())
+gulp.task('minify-css', getMinifyCssTask())
