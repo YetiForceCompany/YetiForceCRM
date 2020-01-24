e519c242457eebb30a7478bbd404d6f5860ca23d
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2020-01-24 22:57:58 +0100

    Rebuilding performance tests

--- app/Utils/Benchmarks.php
+++ app/Utils/Benchmarks.php
@@ -82,9 +82,18 @@ class Benchmarks
 		}
 		$stringTime = microtime(true) - $timeStart;
 		return [
-			'math' => (int) ($mathOperations / $mathTime),
-			'hash' => (int) ($hashOperations / $hashTime),
-			'string' => (int) ($stringOperations / $stringTime),
+			'math' => [
+				'operations' => $mathOperations,
+				'time' => (int) ($mathOperations / $mathTime)
+			],
+			'hash' => [
+				'operations' => $hashOperations,
+				'time' => (int) ($hashOperations / $hashTime)
+			],
+			'string' => [
+				'operations' => $stringOperations,
+				'time' => (int) ($stringOperations / $stringTime)
+			],
 		];
 	}
 
@@ -121,8 +130,76 @@ class Benchmarks
 		}
 
 		return [
-			'read' => (int) ($readOperations / $readTime),
-			'write' => (int) ($writeOperations / $writeTime),
+			'read' => [
+				'operations' => $readOperations,
+				'time' => (int) ($readOperations / $readTime)
+			],
+			'write' => [
+				'operations' => $writeOperations,
+				'time' => (int) ($writeOperations / $writeTime)
+			],
+		];
+	}
+
+	/**
+	 * Hard drive benchmark.
+	 *
+	 * @return array
+	 */
+	private static function hardDrive(): array
+	{
+		$countGroup = 50;
+		$maxTime = 0.2;
+		$read = $write = [];
+		$dir = ROOT_DIRECTORY . \DIRECTORY_SEPARATOR . 'cache' . \DIRECTORY_SEPARATOR . 'speed' . \DIRECTORY_SEPARATOR;
+		foreach ([1 => 207, 10 => 2050, 100 => 20500] as $key => $value) {
+			$writeOperations = 0;
+			$fileContent = str_repeat('12345', $value);
+			$timeStart = microtime(true);
+			$t = 0;
+			while ((microtime(true) - $timeStart) < $maxTime) {
+				$dirGroup = $dir . $key . \DIRECTORY_SEPARATOR . $t . \DIRECTORY_SEPARATOR;
+				if (!is_dir($dirGroup)) {
+					mkdir($dirGroup, 0755, true);
+				}
+				for ($i = 0; $i < $countGroup; ++$i) {
+					file_put_contents("{$dirGroup}{$i}.txt", $fileContent);
+					++$writeOperations;
+				}
+				$t += $countGroup;
+			}
+			$write[$key] = [
+				'operations' => $writeOperations,
+				'time' => (int) ($writeOperations / (microtime(true) - $timeStart))
+			];
+		}
+		foreach ([1, 10, 100] as $value) {
+			$readOperations = 0;
+			$timeStart = microtime(true);
+			$dirGroup = $dir . $value . \DIRECTORY_SEPARATOR;
+			for ($i = 0; $i < 4; ++$i) {
+				foreach (new \DirectoryIterator($dirGroup) as $itemGroup) {
+					if (!$itemGroup->isDot() && $itemGroup->isDir()) {
+						foreach (new \DirectoryIterator($itemGroup->getPathname()) as $item) {
+							if ($item->isFile()) {
+								$fileContent = file_get_contents($item->getPathname());
+								++$readOperations;
+							}
+						}
+					}
+				}
+			}
+			$read[$value] = [
+				'operations' => $readOperations,
+				'time' => (int) ($readOperations / (microtime(true) - $timeStart))
+			];
+		}
+		register_shutdown_function(function () {
+			\vtlib\Functions::recurseDelete('cache/speed');
+		});
+		return [
+			'read' => $read,
+			'write' => $write,
 		];
 	}
 
@@ -131,6 +208,7 @@ class Benchmarks
 		return [
 			'cpu' => self::cpu(),
 			'ram' => self::ram(),
+			'hardDrive' => self::hardDrive(),
 		];
 	}
 
@@ -141,32 +219,10 @@ class Benchmarks
 	 */
 	public static function test()
 	{
-		$dir = ROOT_DIRECTORY . \DIRECTORY_SEPARATOR . 'cache' . \DIRECTORY_SEPARATOR . 'speed' . \DIRECTORY_SEPARATOR;
-		if (!is_dir($dir)) {
-			mkdir($dir, 0755);
-		}
-		$testStartTime = microtime(true);
-		$filesWrite = 0;
-		while ((microtime(true) - $testStartTime) < 1) {
-			file_put_contents("{$dir}{$testStartTime}{$filesWrite}.txt", $testStartTime);
-			++$filesWrite;
-		}
-		$iterator = new \DirectoryIterator($dir);
-		$readS = microtime(true);
-		foreach ($iterator as $item) {
-			if ($item->isFile()) {
-				file_get_contents($item->getPathname());
-			}
-		}
-		$filesRead = $filesWrite / (microtime(true) - $readS);
-		$testStartTime = microtime(true);
-		\vtlib\Functions::recurseDelete('cache/speed');
 		$dbs = microtime(true);
 		\App\Db::getInstance()->createCommand('SELECT BENCHMARK(1000000,1+1);')->execute();
 		$dbe = microtime(true);
 		return [
-			'FilesRead' => (int) $filesRead,
-			'FilesWrite' => $filesWrite,
 			'DB' => (int) (1000000 / ($dbe - $dbs))
 		];
 	}
--- layouts/basic/modules/Settings/ConfReport/Speed.tpl
+++ layouts/basic/modules/Settings/ConfReport/Speed.tpl
@@ -1,9 +1,77 @@
 {*<!-- {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} -->*}
 {strip}
+	{assign var=OPS value=\App\Language::translate('LBL_PER_SECOND', $QUALIFIED_MODULE)}
 	<div class="tpl-Settings-ConfReport-Speed modal-body">
-<pre>
-		{print_r($BENCHMARKS)}
-</pre>
+		<div class="card">
+			<h5 class="card-header">{\App\Language::translate('LBL_CPU', $QUALIFIED_MODULE)}</h5>
+			<div class="card-body">
+				{if isset($BENCHMARKS['cpu'])}
+					math: {App\Fields\Integer::formatToDisplay($BENCHMARKS['cpu']['math']['time'])}{$OPS} ({App\Fields\Integer::formatToDisplay($BENCHMARKS['cpu']['math']['operations'])})<br>
+					hash: {App\Fields\Integer::formatToDisplay($BENCHMARKS['cpu']['hash']['time'])}{$OPS} ({App\Fields\Integer::formatToDisplay($BENCHMARKS['cpu']['hash']['operations'])})<br>
+					string: {App\Fields\Integer::formatToDisplay($BENCHMARKS['cpu']['string']['time'])}{$OPS} ({App\Fields\Integer::formatToDisplay($BENCHMARKS['cpu']['string']['operations'])})
+				{/if}
+			</div>
+		</div>
+		<div class="card mt-2">
+			<h5 class="card-header">{\App\Language::translate('LBL_RAM', $QUALIFIED_MODULE)}</h5>
+			<div class="card-body">
+				{if isset($BENCHMARKS['ram'])}
+					read: {App\Fields\Integer::formatToDisplay($BENCHMARKS['ram']['read']['time'])}{$OPS} ({App\Fields\Integer::formatToDisplay($BENCHMARKS['ram']['read']['operations'])})<br>
+					write: {App\Fields\Integer::formatToDisplay($BENCHMARKS['ram']['write']['time'])}{$OPS} ({App\Fields\Integer::formatToDisplay($BENCHMARKS['ram']['write']['operations'])})
+				{/if}
+			</div>
+		</div>
+		<div class="card mt-2">
+			<h5 class="card-header">Hard drive</h5>
+			<div class="card-body">
+				{if isset($BENCHMARKS['hardDrive'])}
+					<table class="table">
+					<thead>
+						<tr>
+							<th scope="col">File size</th>
+							<th scope="col">{\App\Language::translate('LBL_READ_TEST', $QUALIFIED_MODULE)}</th>
+							<th scope="col">{\App\Language::translate('LBL_WRITE_TEST', $QUALIFIED_MODULE)}</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td scope="row">1 KB</td>
+							<td>
+								{App\Fields\Integer::formatToDisplay($BENCHMARKS['hardDrive']['read'][1]['time'])}{$OPS}
+								({App\Fields\Integer::formatToDisplay($BENCHMARKS['hardDrive']['read'][1]['operations'])})
+							</td>
+							<td>
+								{App\Fields\Integer::formatToDisplay($BENCHMARKS['hardDrive']['write'][1]['time'])}{$OPS}
+								({App\Fields\Integer::formatToDisplay($BENCHMARKS['hardDrive']['write'][1]['operations'])})
+							</td>
+						</tr>
+						<tr>
+							<td scope="row">10 KB</td>
+							<td>
+								{App\Fields\Integer::formatToDisplay($BENCHMARKS['hardDrive']['read'][10]['time'])}{$OPS}
+								({App\Fields\Integer::formatToDisplay($BENCHMARKS['hardDrive']['read'][10]['operations'])})
+							</td>
+							<td>
+								{App\Fields\Integer::formatToDisplay($BENCHMARKS['hardDrive']['write'][10]['time'])}{$OPS}
+								({App\Fields\Integer::formatToDisplay($BENCHMARKS['hardDrive']['write'][10]['operations'])})
+							</td>
+						</tr>
+						<tr>
+							<td scope="row">100 KB</td>
+							<td>
+								{App\Fields\Integer::formatToDisplay($BENCHMARKS['hardDrive']['read'][100]['time'])}{$OPS}
+								({App\Fields\Integer::formatToDisplay($BENCHMARKS['hardDrive']['read'][100]['operations'])})
+							</td>
+							<td>
+								{App\Fields\Integer::formatToDisplay($BENCHMARKS['hardDrive']['write'][100]['time'])}{$OPS}
+								({App\Fields\Integer::formatToDisplay($BENCHMARKS['hardDrive']['write'][100]['operations'])})
+							</td>
+						</tr>
+					</tbody>
+					</table>
+				{/if}
+			</div>
+		</div>
 		{* <hr>
 		<h5>{\App\Language::translate('LBL_READ_TEST', $QUALIFIED_MODULE)}
 			: {$TESTS['FilesRead']}{\App\Language::translate('LBL_PER_SECOND', $QUALIFIED_MODULE)}</h5>
