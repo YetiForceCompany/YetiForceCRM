627e3d1cef22f2d670dcea0a9a4c596c45d4141e
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2020-02-25 16:00:39 +0100

    Upgrade yii2 to 2.0.32

--- app/Db/Drivers/Pgsql/ColumnSchemaBuilder.php
+++ app/Db/Drivers/Pgsql/ColumnSchemaBuilder.php
@@ -1,13 +1,17 @@
 <?php
+/**
+ * Column schema builder file is the schema builder for PgSQL databases.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ * @author    Tomasz Kur <t.kur@yetiforce.com>
+ */
 
 namespace App\Db\Drivers\Pgsql;
 
 /**
- * ColumnSchemaBuilder is the schema builder for PgSQL databases.
- *
- * @copyright YetiForce Sp. z o.o
- * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
- * @author    Tomasz Kur <t.kur@yetiforce.com>
+ * Column schema builder class is the schema builder for PgSQL databases.
  */
 class ColumnSchemaBuilder extends \yii\db\ColumnSchemaBuilder
 {
--- app/Db/Importers/Base.php
+++ app/Db/Importers/Base.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Base file for database import.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
 
 namespace App\Db\Importers;
 
@@ -6,10 +13,6 @@ use yii\db\Schema;
 
 /**
  * Base class for database import.
- *
- * @copyright YetiForce Sp. z o.o
- * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
- * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
 class Base
 {
