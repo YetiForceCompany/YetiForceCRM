54e7e6b4d7cbd14bf399bce5f3d2e001427dc402
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2020-03-12 06:37:55 +0100

    Improved calendar widgets data

--- modules/Vtiger/dashboards/CreatedNotMineActivities.php
+++ modules/Vtiger/dashboards/CreatedNotMineActivities.php
@@ -12,7 +12,7 @@ class Vtiger_CreatedNotMineActivities_Dashboard extends Vtiger_IndexAjax_View
 	{
 		$currentUser = Users_Record_Model::getCurrentUserModel();
 
-		$moduleName = $request->getModule();
+		$moduleName = 'Home';
 		$page = $request->getInteger('page');
 		$linkId = $request->getInteger('linkid');
 		$sortOrder = $request->getForSql('sortorder');
