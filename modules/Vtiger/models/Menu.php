50806c076c4a18f81adff43c78577819498ac419
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2020-03-13 15:01:05 +0100

    Icons update

--- modules/Vtiger/models/Menu.php
+++ modules/Vtiger/models/Menu.php
@@ -203,7 +203,7 @@ class Vtiger_Menu_Model
 		}
 		if (!empty($menu['icon'])) {
 			if (false !== strpos($menu['icon'], 'fa-')) {
-				return '<span class="fa-lg fa-fw ' . $menu['icon'] . ' c-menu__item__icon"></span>';
+				return '<span class="' . $menu['icon'] . ' c-menu__item__icon"></span>';
 			}
 			if (false !== strpos($menu['icon'], 'adminIcon-') || false !== strpos($menu['icon'], 'AdditionalIcon-') || false !== strpos($menu['icon'], 'yfi-') || false !== strpos($menu['icon'], 'yfm-') || false !== strpos($menu['icon'], 'mdi-')) {
 				return '<span class="c-menu__item__icon ' . $menu['icon'] . '" aria-hidden="true"></span>';
