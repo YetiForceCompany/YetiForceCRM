6e620aabcfc0283692de5e801c6371367490357d
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2020-03-25 13:17:09 +0100

    Rebuilding integration with Magento

--- app/Integrations/Magento/Synchronizator/Invoice.php
+++ app/Integrations/Magento/Synchronizator/Invoice.php
@@ -16,7 +16,7 @@ namespace App\Integrations\Magento\Synchronizator;
 /**
  * Category class.
  */
-class Invoice extends Integrators\Invoice
+class Invoice extends Record
 {
 	/**
 	 * {@inheritdoc}
@@ -140,5 +140,27 @@ class Invoice extends Integrators\Invoice
 		$searchCriteria[] = 'searchCriteria[filter_groups][3][filters][0][field]=store_id';
 		$searchCriteria[] = 'searchCriteria[filter_groups][3][filters][0][conditionType]=in';
 		return implode('&', $searchCriteria);
+	}
+
+	/**
+	 * Method to get invoices form Magento.
+	 *
+	 * @param array $ids
+	 *
+	 * @throws \App\Exceptions\AppException
+	 * @throws \ReflectionException
+	 *
+	 * @return array
+	 */
+	public function getInvoices(array $ids = []): array
+	{
+		$items = [];
+		$data = \App\Json::decode($this->connector->request('GET', $this->config->get('storeCode') . '/V1/invoices?' . $this->getSearchCriteria($ids, $this->config->get('invoiceLimit'))));
+		if (!empty($data['items'])) {
+			foreach ($data['items'] as $item) {
+				$items[$item['entity_id']] = $item;
+			}
+		}
+		return $items;
 	}
 }
