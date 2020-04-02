f110e9c31b5e10c57b1465df782f90e9a3e2ac5e
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2020-04-02 13:50:03 +0200

    Rebuilding integration with Magento

--- app/Integrations/Magento/Synchronizator/Maps/Base.php
+++ app/Integrations/Magento/Synchronizator/Maps/Base.php
@@ -16,6 +16,12 @@ namespace App\Integrations\Magento\Synchronizator\Maps;
 abstract class Base
 {
 	/**
+	 * Synchronizer.
+	 *
+	 * @var \App\Integrations\Magento\Synchronizator\Base
+	 */
+	protected $synchronizer;
+	/**
 	 * Fields which are not exist in Magento but needed in YetiForce.
 	 *
 	 * @var string[]
@@ -32,7 +38,11 @@ abstract class Base
 	 *
 	 * @var string[]
 	 */
-	public static $fieldsType = [];
+	public static $fieldsType = [
+		'salutationtype' => 'map',
+		'addresslevel1a' => 'country',
+		'addresslevel1b' => 'country',
+	];
 	/**
 	 * Fields default value.
 	 *
@@ -59,11 +69,57 @@ abstract class Base
 	 */
 	public $dataCrm = [];
 	/**
-	 * Synchronizer.
+	 * Mapped billing fields.
 	 *
-	 * @var \App\Integrations\Magento\Synchronizator\Base
+	 * @var string[]
 	 */
-	protected $synchronizer;
+	public static $billingFields = [
+		'addresslevel1a' => 'country_id',
+		'addresslevel2a' => 'region|region',
+		'addresslevel5a' => 'city',
+		'addresslevel7a' => 'postcode',
+		'addresslevel8a' => 'street|0',
+		'buildingnumbera' => 'street|1',
+		'first_name_a' => 'firstname',
+		'last_name_a' => 'lastname',
+		'phone_a' => 'telephone',
+		'email_a' => 'email',
+		'vat_id_a' => 'vat_id',
+		'company_name_a' => 'company',
+		'phone' => 'telephone',
+		'mobile' => 'fax',
+	];
+	/**
+	 * Mapped shipping fields.
+	 *
+	 * @var string[]
+	 */
+	public static $shippingFields = [
+		'addresslevel1b' => 'country_id',
+		'addresslevel2b' => 'region|region',
+		'addresslevel5b' => 'city',
+		'addresslevel7b' => 'postcode',
+		'addresslevel8b' => 'street|0',
+		'buildingnumberb' => 'street|1',
+		'first_name_b' => 'firstname',
+		'last_name_b' => 'lastname',
+		'phone_b' => 'telephone',
+		'email_b' => 'email',
+		'vat_id_b' => 'vat_id',
+		'company_name_b' => 'company',
+		'phone' => 'telephone',
+		'mobile' => 'fax',
+	];
+
+	/**
+	 * Contacts_gender map.
+	 *
+	 * @var string[]
+	 */
+	public static $salutationtype = [
+		'1' => 'Mr.',
+		'2' => 'Mrs.',
+	];
 
 	/**
 	 * Constructor.
@@ -271,5 +327,120 @@ abstract class Base
 			}
 		}
 		return $parsedData;
+	}
+
+	/**
+	 * Return address data.
+	 *
+	 * @param string $type
+	 *
+	 * @return array
+	 */
+	public function getAddressDataCrm(string $type): array
+	{
+		$addressData = [];
+		foreach ($this->getAddressFieldsCrm($type) as $fieldNameCrm => $fieldName) {
+			$addressData[$fieldNameCrm] = $this->getAddressFieldValue($type, $fieldNameCrm, $fieldName);
+		}
+		return $addressData;
+	}
+
+	/**
+	 * Return address fields crm.
+	 *
+	 * @param string $type
+	 *
+	 * @return array
+	 */
+	public function getAddressFieldsCrm(string $type): array
+	{
+		$fieldsType = $type . 'Fields';
+		if (!empty(self::${$fieldsType})) {
+			$fields = self::${$fieldsType};
+		}
+		return $fields ?? [];
+	}
+
+	/**
+	 * Get address field value from Magento.
+	 *
+	 * @param string $type
+	 * @param string $fieldNameCrm
+	 * @param string $fieldName
+	 *
+	 * @return array|mixed
+	 */
+	public function getAddressFieldValue(string $type, string $fieldNameCrm, string $fieldName)
+	{
+		$methodName = 'getCrm' . \ucfirst($fieldNameCrm);
+		if (!empty($fieldParsed = $this->getAddressDataByType($type))) {
+			if (\method_exists($this, $methodName)) {
+				$fieldParsed = $this->{$methodName}();
+			} else {
+				$fieldLevels = explode('|', $fieldName);
+				if (($elements = \count($fieldLevels)) > 1) {
+					foreach ($fieldLevels as $level => $fieldLevel) {
+						if (isset($fieldParsed[$fieldLevel])) {
+							$fieldParsed = $fieldParsed[$fieldLevel];
+						} else {
+							if ($elements !== $level + 1) {
+								$fieldParsed = '';
+							}
+							break;
+						}
+					}
+				} else {
+					$fieldParsed = $fieldParsed[$fieldName] ?? '';
+				}
+				if (null !== $fieldParsed && isset(static::$fieldsType[$fieldNameCrm])) {
+					switch (static::$fieldsType[$fieldNameCrm]) {
+						case 'implode':
+							$fieldParsed = implode(', ', $fieldParsed);
+							break;
+						case 'country':
+							$fieldParsed = \App\Fields\Country::getCountryName($fieldParsed);
+							break;
+					}
+				} else {
+					$fieldParsed = \is_array($fieldParsed) ? null : $fieldParsed;
+				}
+			}
+		} else {
+			$fieldParsed = '';
+		}
+		return $fieldParsed;
+	}
+
+	/**
+	 * Get given type address data.
+	 *
+	 * @param string $addressType
+	 *
+	 * @return array
+	 */
+	public function getAddressDataByType(string $addressType)
+	{
+		$data = [];
+		$addressName = 'default_' . $addressType;
+		foreach ($this->data['addresses'] as $address) {
+			if (isset($this->data[$addressName]) && $address['id'] == $this->data[$addressName]) {
+				$data = $address;
+			}
+		}
+		return $data ?? current($this->data['addresses']) ?? [];
+	}
+
+	/**
+	 * Get birthday.
+	 *
+	 * @return string|null
+	 */
+	public function getCrmBirthday()
+	{
+		$dob = $this->data['dob'] ?? $this->data['customer_dob'] ?? false;
+		if (!$dob || '0000-00-00' === $dob) {
+			return null;
+		}
+		return $dob;
 	}
 }
