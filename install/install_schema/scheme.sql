18d349b66c86536e13971ad95b4c696595dde1da
Arkadiusz Dudek <a.dudek@yetiforce.com>
2020-04-20 19:56:33 +0200

    Update Currency and Country list

--- install/install_schema/scheme.sql
+++ install/install_schema/scheme.sql
@@ -1660,7 +1660,7 @@ CREATE TABLE `u_yf_countries` (
   KEY `code` (`code`),
   KEY `phone` (`status`,`phone`),
   KEY `uitype` (`status`,`uitype`)
-) ENGINE=InnoDB AUTO_INCREMENT=243 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=252 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `u_yf_crmentity_label` */
 
@@ -5223,7 +5223,7 @@ CREATE TABLE `vtiger_currencies` (
   `currency_code` varchar(50) DEFAULT NULL,
   `currency_symbol` varchar(11) DEFAULT NULL,
   PRIMARY KEY (`currencyid`)
-) ENGINE=InnoDB AUTO_INCREMENT=140 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=181 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `vtiger_currency` */
 
