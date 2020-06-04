1517f86c2a01bb1cd2bafcf9784d61042205c317
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2020-06-04 08:15:59 +0200

    Fixed #13081 [bug] Send email task WF, missing message sender (from).

--- app/Mailer.php
+++ app/Mailer.php
@@ -17,7 +17,7 @@ class Mailer
 		1 => 'LBL_WAITING_TO_BE_SENT',
 		2 => 'LBL_ERROR_DURING_SENDING',
 	];
-	public static $quoteJsonColumn = ['to', 'cc', 'bcc', 'attachments', 'params'];
+	public static $quoteJsonColumn = ['from', 'to', 'cc', 'bcc', 'attachments', 'params'];
 	public static $quoteColumn = ['smtp_id', 'date', 'owner', 'status', 'from', 'subject', 'content', 'to', 'cc', 'bcc', 'attachments', 'priority', 'params'];
 
 	/** @var \PHPMailer\PHPMailer\PHPMailer PHPMailer instance */
