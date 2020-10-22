edf954cb4aea7ea5c08c9dbf00f06814cd8bafca
Arek Solek <arkadiusz_s9887@wp.pl>
2020-10-22 09:51:37 +0200

    Deleted getListViewEntries function

--- modules/Settings/FieldsDependency/models/ListView.php
+++ modules/Settings/FieldsDependency/models/ListView.php
@@ -18,52 +18,9 @@ class Settings_FieldsDependency_ListView_Model extends Settings_Vtiger_ListView_
 	public function getBasicListQuery()
 	{
 		$query = parent::getBasicListQuery();
-		if (!empty($sourceModule = $this->get('sourceModule'))) {
+		if ($sourceModule = $this->get('sourceModule')) {
 			$query->where(['tabid' => $sourceModule]);
 		}
 		return $query;
-	}
-
-	/**
-	 * Function to get the list view entries.
-	 *
-	 * @param Vtiger_Paging_Model $pagingModel
-	 *
-	 * @return array
-	 */
-	public function getListViewEntries($pagingModel)
-	{
-		$module = $this->getModule();
-		$parentModuleName = $module->getParentName();
-		if (!empty($parentModuleName)) {
-			$qualifiedModuleName = $parentModuleName . ':' . $module->getName();
-		}
-		$recordModelClass = Vtiger_Loader::getComponentClassName('Model', 'Record', $qualifiedModuleName);
-		$query = $this->getBasicListQuery();
-		$listFields = array_keys($module->listFields);
-		$listFields[] = $module->baseIndex;
-		$startIndex = $pagingModel->getStartIndex();
-		$pageLimit = $pagingModel->getPageLimit();
-		$query->limit($pageLimit + 1)->offset($startIndex);
-		$orderBy = $this->getForSql('orderby');
-		if (!empty($orderBy)) {
-			$query->orderBy($orderBy . ' ' . $this->getForSql('sortorder'));
-		}
-		$dataReader = $query->createCommand()->query();
-		$listViewRecordModels = [];
-		while ($row = $dataReader->read()) {
-			$record = new $recordModelClass();
-			$record->setData($row);
-			$listViewRecordModels[$record->getId()] = $record;
-		}
-		$pagingModel->calculatePageRange($dataReader->count());
-		if ($dataReader->count() > $pageLimit) {
-			array_pop($listViewRecordModels);
-			$pagingModel->set('nextPageExists', true);
-		} else {
-			$pagingModel->set('nextPageExists', false);
-		}
-		$dataReader->close();
-		return $listViewRecordModels;
 	}
 }
