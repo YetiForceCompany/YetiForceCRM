0bfc10cbaae9391fa9aa6ec01e1c061b6b652a99
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2020-11-21 23:19:12 +0100

    Added minor improvements

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'appVersion' => '5.3.370',
-	'patchVersion' => '2020.11.20',
-	'lib_roundcube' => '0.0.104'
+	'appVersion' => '5.3.371',
+	'patchVersion' => '2020.11.21',
+	'lib_roundcube' => '0.0.105'
 ];
