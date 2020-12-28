b4ecf39333f0b21324bcff6024ee886c0e01c492
Arek Solek <arkadiusz_s9887@wp.pl>
2020-12-28 13:55:08 +0100

    Added url

--- modules/Project/summary_blocks/Milestones.php
+++ modules/Project/summary_blocks/Milestones.php
@@ -13,40 +13,23 @@
  */
 class Milestones
 {
-	/**
-	 * Name.
-	 *
-	 * @var string
-	 */
+	/** @var string Block name */
 	public $name = 'LBL_MILESTONES';
 
-	/**
-	 * Sequence.
-	 *
-	 * @var int
-	 */
+	/** @var int Block sequence */
 	public $sequence = 1;
 
-	/**
-	 * Reference.
-	 *
-	 * @var string
-	 */
+	/** @var string Block reference */
 	public $reference = 'ProjectMilestone';
 
-	/**
-	 * Name icon.
-	 *
-	 * @var string
-	 */
+	/** @var string Block icon */
 	public $icon = 'yfm-ProjectMilestone';
 
-	/**
-	 * Type.
-	 *
-	 * @var string
-	 */
+	/** @var string Block type */
 	public $type = 'badge';
+
+	/** @var array Block status */
+	public $status = ['PLL_IN_PROGRESSING', 'PLL_IN_APPROVAL'];
 
 	/**
 	 * Process.
@@ -57,20 +40,23 @@ class Milestones
 	 */
 	public function process(Vtiger_Record_Model $recordModel)
 	{
+		$badgeLink = \Vtiger_Relation_Model::getInstance(\Vtiger_Module_Model::getInstance($recordModel->getModuleName()), \Vtiger_Module_Model::getInstance($this->reference))->getListUrl($recordModel);
 		$query = (new App\Db\Query())->from('vtiger_projectmilestone')
 			->innerJoin('vtiger_crmentity', 'vtiger_projectmilestone.projectmilestoneid = vtiger_crmentity.crmid')
 			->where(['vtiger_projectmilestone.projectid' => $recordModel->getId(), 'vtiger_crmentity.deleted' => 0]);
 		$total = $query->count();
-		$open = $query->andWhere(['vtiger_projectmilestone.projectmilestone_status' => ['PLL_IN_PROGRESSING', 'PLL_IN_APPROVAL']])->count();
+		$open = $query->andWhere(['vtiger_projectmilestone.projectmilestone_status' => $this->status])->count();
 		return [
 			[
 				'label' => \App\Language::translate('LBL_OPEN'),
 				'value' => $open,
-				'class' => 'badge-success'
+				'class' => 'badge-success',
+				'badgeLink' => $badgeLink . '&search_params=' . urlencode(\App\Json::encode([[['projectmilestone_status', 'e', $this->status]]]))
 			], [
 				'label' => \App\Language::translate('LBL_ALL'),
 				'value' => $total,
-				'class' => 'badge-secondary'
+				'class' => 'badge-secondary',
+				'badgeLink' => $badgeLink
 			]
 		];
 	}
--- modules/Project/summary_blocks/Tasks.php
+++ modules/Project/summary_blocks/Tasks.php
@@ -13,40 +13,23 @@
  */
 class Tasks
 {
-	/**
-	 * Name.
-	 *
-	 * @var string
-	 */
+	/** @var string Block name */
 	public $name = 'LBL_TASKS_LIST';
 
-	/**
-	 * Sequence.
-	 *
-	 * @var int
-	 */
+	/** @var int Block sequence */
 	public $sequence = 2;
 
-	/**
-	 * Reference.
-	 *
-	 * @var string
-	 */
+	/** @var string Block reference */
 	public $reference = 'ProjectTask';
 
-	/**
-	 * Name icon.
-	 *
-	 * @var string
-	 */
+	/** @var string Block icon */
 	public $icon = 'yfm-ProjectTask';
 
-	/**
-	 * Type.
-	 *
-	 * @var string
-	 */
+	/** @var string Block type */
 	public $type = 'badge';
+
+	/** @var array Block type */
+	public $status = ['PLL_IN_PROGRESSING', 'PLL_IN_APPROVAL', 'PLL_SUBMITTED_COMMENTS'];
 
 	/**
 	 * Process.
@@ -57,20 +40,23 @@ class Tasks
 	 */
 	public function process(Vtiger_Record_Model $recordModel)
 	{
+		$badgeLink = \Vtiger_Relation_Model::getInstance(\Vtiger_Module_Model::getInstance($recordModel->getModuleName()), \Vtiger_Module_Model::getInstance($this->reference))->getListUrl($recordModel);
 		$query = (new App\Db\Query())->from('vtiger_projecttask')
 			->innerJoin('vtiger_crmentity', 'vtiger_projecttask.projecttaskid = vtiger_crmentity.crmid')
 			->where(['vtiger_projecttask.projectid' => $recordModel->getId(), 'vtiger_crmentity.deleted' => 0]);
 		$total = $query->count();
-		$open = $query->andWhere(['vtiger_projecttask.projecttaskstatus' => ['PLL_IN_PROGRESSING', 'PLL_IN_APPROVAL', 'PLL_SUBMITTED_COMMENTS']])->count();
+		$open = $query->andWhere(['vtiger_projecttask.projecttaskstatus' => $this->status])->count();
 		return [
 			[
 				'label' => \App\Language::translate('LBL_OPEN'),
 				'value' => $open,
-				'class' => 'badge-success'
+				'class' => 'badge-success',
+				'badgeLink' => $badgeLink . '&search_params=' . urlencode(\App\Json::encode([[['projecttaskstatus', 'e', $this->status]]]))
 			], [
 				'label' => \App\Language::translate('LBL_ALL'),
 				'value' => $total,
-				'class' => 'badge-secondary'
+				'class' => 'badge-secondary',
+				'badgeLink' => $badgeLink
 			]
 		];
 	}
