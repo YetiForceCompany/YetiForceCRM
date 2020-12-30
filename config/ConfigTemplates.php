b0ab547e7a0afb5787672d6ca744fd2e4c930786
Adrian Koń <konadrian@wp.pl>
2020-12-30 08:59:01 +0100

    Added posibility of hidding buttons in related records

--- config/ConfigTemplates.php
+++ config/ConfigTemplates.php
@@ -782,6 +782,10 @@ return [
 			'default' => true,
 			'description' => 'Fill in the record creation form with the data used in filtering (search_params)'
 		],
+		'hideButtonsLeftSide' => [
+			'default' => false,
+			'description' => 'Hide buttons visible in the record in related module '
+		],
 	],
 	'search' => [
 		'GLOBAL_SEARCH_SELECT_MODULE' => [
