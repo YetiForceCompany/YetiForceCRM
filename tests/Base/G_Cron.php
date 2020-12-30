fba8248134e1257356bcb61a73e8050383d3d30b
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2020-12-30 11:35:05 +0100

    tests

--- tests/Base/G_Cron.php
+++ tests/Base/G_Cron.php
@@ -30,7 +30,7 @@ class G_Cron extends \Tests\Base
 					'username' => 'yetiforcegitdevelopery@gmail.com',
 					'mail_host' => 'imap.gmail.com',
 					'language' => 'en_US',
-					'preferences' => 'a:3:{s:9:"junk_mbox";s:12:"[Gmail]/Spam";s:10:"trash_mbox";s:12:"[Gmail]/Kosz";s:11:"client_hash";s:32:"0e1f51526f56ef769dbd1f58a674f106";}',
+					'preferences' => 'a:1:{s:11:"client_hash";s:16:"UmfW5Tgq7vMU35P0";}',
 					'password' => $_SERVER['YETI_MAIL_PASS'],
 					'crm_user_id' => '1',
 					'actions' => 'CreatedEmail,CreatedHelpDesk,BindAccounts,BindContacts,BindLeads,BindHelpDesk,BindSSalesProcesses,BindCampaigns,BindCompetition,BindOSSEmployees,BindPartners,BindProject,BindServiceContracts,BindVendors',
