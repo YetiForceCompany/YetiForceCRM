bfd2cba9584d831340fb713e5173a2dcc332d404
Arek Solek <arkadiusz_s9887@wp.pl>
2021-01-26 14:29:59 +0100

    Corrected list load view in SLAPolicy

--- modules/Settings/SlaPolicy/actions/DeleteAjax.php
+++ modules/Settings/SlaPolicy/actions/DeleteAjax.php
@@ -15,12 +15,12 @@ class Settings_SlaPolicy_DeleteAjax_Action extends Settings_Vtiger_Delete_Action
 	 */
 	public function process(App\Request $request)
 	{
-		$recordModel = Settings_SlaPolicy_Record_Model::getInstanceById($request->getInteger('record'));
-		if ($recordModel) {
-			$result = $recordModel->delete();
+		$recordId = $request->getInteger('record');
+		$recordModel = Settings_SlaPolicy_Record_Model::getInstanceById($recordId);
+		if ($recordModel && !empty($recordModel->delete())) {
+			$result = $result = ['success' => true];
 		} else {
-			$result = ['success' => false];
-			\App\Log::error('Not found SLA Policy record with this id :' . $request->getInteger('record'));
+			$result = ['success' => false, 'message' => "Not found SLA Policy record with this id: $recordId"];
 		}
 		$response = new Vtiger_Response();
 		$response->setResult($result);
