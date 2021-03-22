901101492a6fc92852e5feb34938ee9eb69af384
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-03-22 13:36:34 +0100

    A new product has been added to the store - YetiForceDav

---
+++ app/YetiForce/Shop/Product/YetiForceDav.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * YetiForce shop YetiForceDav file.
+ *
+ * @package   App
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+
+namespace App\YetiForce\Shop\Product;
+
+/**
+ * YetiForce shop YetiForceDav class.
+ */
+class YetiForceDav extends \App\YetiForce\Shop\AbstractBaseProduct
+{
+	/** {@inheritdoc} */
+	public $label = 'YetiForce DAV Integration';
+
+	/** {@inheritdoc} */
+	public $category = 'Integrations';
+
+	/** {@inheritdoc} */
+	public $website = 'https://yetiforce.com/pl/yetiforce-dav-integration';
+
+	/** {@inheritdoc} */
+	public $prices = [
+		'Micro' => 5,
+		'Small' => 12,
+		'Medium' => 25,
+		'Large' => 50,
+		'Corporation' => 100,
+	];
+
+	/** {@inheritdoc} */
+	public $featured = true;
+
+	/** {@inheritdoc} */
+	public function verify(): array
+	{
+		$message = $status = true;
+		if (\App\YetiForce\Register::getProducts('YetiForceDav')) {
+			[$status, $message] = \App\YetiForce\Shop::checkWithMessage('YetiForceDav');
+		} else {
+			$message = 'LBL_PAID_FUNCTIONALITY_ACTIVATED';
+			$status = !(new \App\Db\Query())->from('dav_users')->exists();
+		}
+		return ['status' => $status, 'message' => $message];
+	}
+
+	/** {@inheritdoc} */
+	public function getAdditionalButtons(): array
+	{
+		$links = [
+			\Vtiger_Link_Model::getInstanceFromValues([
+				'linklabel' => 'Website',
+				'relatedModuleName' => '_Base',
+				'linkicon' => 'fas fa-globe',
+				'linkhref' => true,
+				'linkExternal' => true,
+				'linktarget' => '_blank',
+				'linkurl' => $this->website,
+				'linkclass' => 'btn-info',
+				'showLabel' => 1,
+			]),
+		];
+		if (\App\Security\AdminAccess::isPermitted('Dav')) {
+			$links[] = \Vtiger_Link_Model::getInstanceFromValues([
+				'linklabel' => 'LBL_DAV_KEYS',
+				'relatedModuleName' => 'Settings:Dav',
+				'linkicon' => 'yfi yfi-dav',
+				'linkhref' => true,
+				'linkurl' => 'index.php?parent=Settings&module=Dav&view=Keys',
+				'linkclass' => 'btn-primary',
+				'showLabel' => 1,
+			]);
+		}
+		return $links;
+	}
+}
