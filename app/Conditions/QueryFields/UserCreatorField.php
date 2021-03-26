d1fb3792816e13e16d2edd3102a6820533814fa0
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2021-03-26 11:00:43 +0100

    Added new filter option for 'Created by' field - Not created by owner

--- app/Conditions/QueryFields/UserCreatorField.php
+++ app/Conditions/QueryFields/UserCreatorField.php
@@ -8,7 +8,17 @@ namespace App\Conditions\QueryFields;
  * @copyright YetiForce Sp. z o.o
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Tomasz Kur <t.kur@yetiforce.com>
+ * @author    Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
  */
 class UserCreatorField extends OwnerField
 {
+	/**
+	 * Not created by owner.
+	 *
+	 * @return array
+	 */
+	public function operatorNco()
+	{
+		return ['<>', $this->getColumnName(), new \yii\db\Expression($this->getTableName() . '.smownerid')];
+	}
 }
--- app/Conditions/RecordFields/UserCreatorField.php
+++ app/Conditions/RecordFields/UserCreatorField.php
@@ -8,7 +8,17 @@ namespace App\Conditions\RecordFields;
  * @copyright YetiForce Sp. z o.o
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ * @author    Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
  */
 class UserCreatorField extends OwnerField
 {
+	/**
+	 * Not created by owner.
+	 *
+	 * @return array
+	 */
+	public function operatorNco()
+	{
+		return $this->getValue() != $this->recordModel->get('assigned_user_id');
+	}
 }
