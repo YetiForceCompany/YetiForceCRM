f5a4b8fa816123fd63e7a1872e7a377b898d1847
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-03-26 22:36:27 +0100

    Update tests

--- tests/setup/dependency.sh
+++ tests/setup/dependency.sh
@@ -2,26 +2,29 @@
 #########################################
 # Installation dependency
 #########################################
-cd "$(dirname "$0")/../../"
-echo " -----  Install yarn for public_html directory (mode $INSTALL_MODE) -----"
 
-if [ "$INSTALL_MODE" != "PROD" ]; then
-    yarn install --force --modules-folder "./public_html/libraries"
-	yarn list
-else
-    yarn install --force --modules-folder "./public_html/libraries" --production=true --ignore-optional
+if [ "$GUI_MODE" == "true" ]; then
+	cd "$(dirname "$0")/../../"
+	echo " -----  Install yarn for public_html directory (mode $INSTALL_MODE) -----"
+
+	if [ "$INSTALL_MODE" != "PROD" ]; then
+		yarn install --force --modules-folder "./public_html/libraries"
+		yarn list
+	else
+		yarn install --force --modules-folder "./public_html/libraries" --production=true --ignore-optional
+	fi
+
+
+	echo " -----  Install yarn for public_html directory (mode $INSTALL_MODE) -----"
+	cd public_html/src
+	if [ "$INSTALL_MODE" != "PROD" ]; then
+		yarn install --force
+		yarn list
+	else
+	yarn install --force --production=true --ignore-optional
+	fi
+	cd ../../
 fi
-
-
-echo " -----  Install yarn for public_html directory (mode $INSTALL_MODE) -----"
-cd public_html/src
-if [ "$INSTALL_MODE" != "PROD" ]; then
-    yarn install --force
-	yarn list
-else
-   yarn install --force --production=true --ignore-optional
-fi
-cd ../../
 
 echo " -----  Install composer -----"
 composer -V
@@ -33,9 +36,4 @@ if [ "$INSTALL_MODE" != "PROD" ]; then
 	composer install --no-interaction --no-interaction
 else
 	composer install --no-interaction --no-dev --no-interaction
-fi
-
-if [ "$INSTALL_MODE" == "TEST" ]; then
-	mysql -u root yetiforce < install/install_schema/scheme.sql
-	mysql -u root yetiforce < install/install_schema/data.sql
 fi
