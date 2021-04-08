c8d37c4f6878e878789bcb770fa9025c3df3e7be
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-04-08 12:01:06 +0200

    Added minor improvements

---
+++ public_html/layouts/basic/modules/Users/resources/YetiForce.js
@@ -0,0 +1,18 @@
+/* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
+'use strict';
+
+$.Class(
+	'Users_YetiForce_JS',
+	{},
+	{
+		/**
+		 * Register events
+		 * @param {jQuery} modal Modal container
+		 */
+		registerEvents: function (modal) {
+			setTimeout(function () {
+				modal.modal('hide');
+			}, 7000);
+		}
+	}
+);
