701460f064352ae186f86136cfa456b9e2d2b414
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-04-14 21:38:21 +0200

    Tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,7 +18,6 @@ if (!class_exists('Vtiger_WebUI')) {
 }
 
 $installDatabase = true;
-$debug = true;
 \App\Process::$requestMode = 'TEST';
 
 //fix phpunit console for windows
@@ -58,14 +57,4 @@ if ($installDatabase) {
 	}
 } else {
 	echo 'Skipped test database install ...' . PHP_EOL;
-}
-if ($debug) {
-	echo 'error_reporting: ' . error_reporting() . ' | ' . \App\Utils::varExport(\App\ErrorHandler::error2string(error_reporting()), true) . PHP_EOL;
-	echo 'error_log: ' . ini_get('error_log') . PHP_EOL;
-	echo 'log_errors: ' . ini_get('log_errors') . PHP_EOL;
-	echo 'max_execution_time: ' . ini_get('max_execution_time') . PHP_EOL;
-	echo 'display_errors: ' . ini_get('display_errors') . PHP_EOL;
-	echo 'disable_functions: ' . ini_get('disable_functions') . PHP_EOL;
-	echo 'xdebug.enable: ' . ini_get('xdebug.enable') . PHP_EOL;
-	echo 'cgi.fix_pathinfo: ' . ini_get('cgi.fix_pathinfo') . PHP_EOL;
 }
