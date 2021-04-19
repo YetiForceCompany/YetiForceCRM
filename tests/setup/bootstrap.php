f4d98591d41d1d20865ff7b537252a7ded90fdfa
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-04-19 10:34:40 +0200

    tests/setup/bootstrap.php

--- tests/setup/bootstrap.php
+++ tests/setup/bootstrap.php
@@ -8,7 +8,9 @@
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
+$path = getcwd();
 chdir(__DIR__ . '/../../');
+
 set_include_path(getcwd());
 if (!\defined('ROOT_DIRECTORY')) {
 	\define('ROOT_DIRECTORY', getcwd());
@@ -58,3 +60,4 @@ if ($installDatabase) {
 } else {
 	echo 'Skipped test database install ...' . PHP_EOL;
 }
+chdir($path);
