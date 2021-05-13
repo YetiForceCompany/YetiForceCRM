ea64443a0aff6402e93e221ba9ac0644b4bc3abd
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-05-14 00:07:08 +0200

    tests/setup/dependency.sh

--- tests/setup/dependency.sh
+++ tests/setup/dependency.sh
@@ -4,9 +4,11 @@
 #########################################
 if [ "$GUI_MODE" == "true" ]; then
 	cd "$(dirname "$0")/../../"
-
 	echo " -----  Install yarn for public_html directory (mode $INSTALL_MODE) -----"
-
+	pwd
+	echo " -----  yarn -v -----"
+	yarn -v
+	echo " -----  yarn install -----"
 	if [ "$INSTALL_MODE" != "PROD" ]; then
 		yarn install --modules-folder "./public_html/libraries"
 		yarn list
@@ -15,18 +17,20 @@ if [ "$GUI_MODE" == "true" ]; then
 	fi
 
 	echo " -----  Install yarn for public_html directory (mode $INSTALL_MODE) -----"
-
 	cd public_html/src
+	pwd
+	echo " -----  yarn install -----"
 	if [ "$INSTALL_MODE" != "PROD" ]; then
 		yarn install
 		yarn list
 	else
-	yarn install --production=true --ignore-optional
+		yarn install --production=true --ignore-optional
 	fi
 	cd ../../
 fi
 
 echo " -----  Install composer (mode $INSTALL_MODE) -----"
+pwd
 composer -V
 if [ "$INSTALL_MODE" != "PROD" ]; then
 	rm -rf composer.json
@@ -34,8 +38,8 @@ if [ "$INSTALL_MODE" != "PROD" ]; then
 	mv composer_dev.json composer.json
 	mv composer_dev.lock composer.lock
 	echo " -----  composer install --no-interaction --no-interaction --quiet -----"
-	composer install --no-interaction --no-interaction --quiet
+	composer install --no-interaction
 else
-	echo " -----  composer install --no-interaction --no-dev --no-interaction -----"
-	composer install --no-interaction --no-dev --no-interaction
+	echo " -----  composer install --no-interaction --no-dev -----"
+	composer install --no-interaction --no-dev
 fi
