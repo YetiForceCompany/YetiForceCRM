441c6edaa2afb79c88121c2150be76c4d21a4cea
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-05-18 00:17:26 +0200

    modules/Vtiger/inventoryfields/Currency.php

--- modules/Vtiger/inventoryfields/Currency.php
+++ modules/Vtiger/inventoryfields/Currency.php
@@ -84,13 +84,8 @@ class Vtiger_Currency_InventoryField extends Vtiger_Basic_InventoryField
 			if (!is_numeric($value) || !isset(\App\Fields\Currency::getAll()[$value])) {
 				throw new \App\Exceptions\Security("ERR_ILLEGAL_FIELD_VALUE||$columnName||" . print_r($value, true), 406);
 			}
-		} else {
-			if (!is_numeric($value)) {
-				throw new \App\Exceptions\Security("ERR_ILLEGAL_FIELD_VALUE||$columnName||" . print_r($value, true), 406);
-			}
-			if (\App\TextParser::getTextLength($value) > $this->customMaximumLength[$columnName]) {
-				throw new \App\Exceptions\Security("ERR_VALUE_IS_TOO_LONG||$columnName||" . print_r($value, true), 406);
-			}
+		} elseif (!\is_array($value) && \App\TextParser::getTextLength($value) > $this->customMaximumLength[$columnName]) {
+			throw new \App\Exceptions\Security("ERR_VALUE_IS_TOO_LONG||$columnName||" . print_r($value, true), 406);
 		}
 	}
 }
