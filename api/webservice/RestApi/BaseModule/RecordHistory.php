b9340adceb2eebf45b75296f507b0f1a44c9de26
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-06-21 12:53:37 +0200

    Reconstruction of the APIRest/Portal

--- api/webservice/RestApi/BaseModule/RecordHistory.php
+++ api/webservice/RestApi/BaseModule/RecordHistory.php
@@ -56,16 +56,9 @@ class RecordHistory extends \Api\Core\BaseAction
 	 *		security={{"basicAuth" : {}, "ApiKeyAuth" : {}, "token" : {}}},
 	 *		@OA\Parameter(name="moduleName", in="path", @OA\Schema(type="string"), description="Module name", required=true, example="Contacts"),
 	 *		@OA\Parameter(name="recordId", in="path", @OA\Schema(type="integer"), description="Record id", required=true, example=116),
-	 *		@OA\Parameter(name="x-row-limit", in="header", @OA\Schema(type="integer"), description="Get rows limit, default: 1000", required=false, example=1000),
+	 *		@OA\Parameter(name="x-row-limit", in="header", @OA\Schema(type="integer"), description="Get rows limit, default: 100", required=false, example=50),
 	 *		@OA\Parameter(name="x-row-offset", in="header", @OA\Schema(type="integer"), description="Offset, default: 0", required=false, example=0),
-	 *		@OA\Parameter(
-	 *			name="x-start-with",
-	 *			description="Show history from given ID",
-	 *			@OA\Schema(type="integer"),
-	 *			in="header",
-	 *			example=5972,
-	 *			required=false
-	 *		),
+	 *		@OA\Parameter(name="x-start-with", in="header", @OA\Schema(type="integer"), description="Show history from given ID", required=false, example=5972),
 	 *		@OA\Parameter(name="x-raw-data", in="header", @OA\Schema(type="integer", enum={0, 1}), description="Gets raw data", required=false, example=1),
 	 *		@OA\Parameter(name="X-ENCRYPTED", in="header", @OA\Schema(ref="#/components/schemas/Header-Encrypted"), required=true),
 	 *		@OA\Response(
@@ -93,37 +86,39 @@ class RecordHistory extends \Api\Core\BaseAction
 	 *		description="Action module for recent activities in CRM",
 	 *		type="object",
 	 *		@OA\Property(property="status", type="integer", enum={0, 1}, title="A numeric value of 0 or 1 that indicates whether the communication is valid. 1 - success , 0 - error"),
-	 *		@OA\Property(
-	 *			property="result",
-	 *			title="Returns recent activities that took place in CRM",
-	 *			type="object",
-	 *			@OA\Property(
-	 *				property="response",
-	 *				title="Contains what actions have been performed and returns the data that has changed",
-	 *				type="object",
-	 *				@OA\AdditionalProperties(
-	 *					type="object",
-	 *					title="Key indicating the number of changes made to a given record",
-	 * 					@OA\Property(property="time", type="string", title="Showing the exact date on which the change took place",  example="2019-10-07 08:32:38"),
-	 *					@OA\Property(property="owner", type="string", title="Username of the user who made the change", example="System Admin"),
-	 *					@OA\Property(property="status", type="string", title="Name of the action that was carried out", example="changed"),
-	 * 					@OA\Property(property="rawTime", type="string", title="Showing the exact date on which the change took place",  example="2019-10-07 08:32:38"),
-	 *					@OA\Property(property="rawOwner", type="integer", title="User ID of the user who made the change", example=1),
-	 *					@OA\Property(property="rawStatus", type="string", title="The name of the untranslated label", example="LBL_UPDATED"),
-	 *					@OA\Property(
-	 *						property="data",
-	 *						type="object",
-	 *						title="Field system name",
-	 *						@OA\AdditionalProperties(
-	 *							@OA\Property(property="from", type="string", title="Value before change, dynamically collected value - the data type depends on the field type", example="Jan Kowalski"),
-	 *							@OA\Property(property="to", type="string", title="Value after change, dynamically collected value - the data type depends on the field type", example="Jan Nowak"),
-	 *							@OA\Property(property="rawFrom", type="string", title="Value before change", example="Jan Kowalski"),
-	 *							@OA\Property(property="rawTo", type="string", title="Value after change", example="Jan Nowak"),
+	 *		@OA\Property(property="result", type="object", title="Returns recent activities that took place in CRM",
+	 *			@OA\AdditionalProperties(type="object", title="Key indicating the number of changes made to a given record",
+	 * 				@OA\Property(property="time", type="string", title="Showing the exact date on which the change took place", example="2019-10-07 08:32:38"),
+	 *				@OA\Property(property="owner", type="string", title="Username of the user who made the change", example="System Admin"),
+	 *				@OA\Property(property="status", type="string", title="Name of the action that was carried out", example="changed"),
+	 * 				@OA\Property(property="rawTime", type="string", title="Showing the exact date on which the change took place",  example="2019-10-07 08:32:38"),
+	 *				@OA\Property(property="rawOwner", type="integer", title="User ID of the user who made the change", example=1),
+	 *				@OA\Property(property="rawStatus", type="string", title="The name of the untranslated label", example="LBL_UPDATED"),
+	 *				@OA\Property(property="data", title="Additional information",
+	 *					oneOf={
+	 *						@OA\Property(type="object", title="Record data create",
+	 *							@OA\AdditionalProperties(
+	 *								@OA\Property(property="label", type="string", title="Translated field label", example="Name"),
+	 *								@OA\Property(property="value", type="string", title="Value, the data type depends on the field type", example="Jan Kowalski"),
+	 *								@OA\Property(property="raw", type="string", title="Value in database format, only available in `x-raw-data`", example="Jan Kowalski"),
+	 *							),
+	 *						),
+	 *						@OA\Property(type="object", title="Record data change", description="Edit, conversation",
+	 *							@OA\AdditionalProperties(
+	 *								@OA\Property(property="label", type="string", title="Translated field label", example="Name"),
+	 *								@OA\Property(property="from", type="string", title="Value before change, the data type depends on the field type", example="Jan Kowalski"),
+	 *								@OA\Property(property="to", type="string", title="Value after change, the data type depends on the field type", example="Jan Nowak"),
+	 *								@OA\Property(property="rawFrom", type="string", title="Value before change, value in database format, only available in `x-raw-data`", example="Jan Kowalski"),
+	 *								@OA\Property(property="rawTo", type="string", title="Value after change, value in database format, only available in `x-raw-data`", example="Jan Nowak"),
+	 *							),
+	 *						),
+	 *						@OA\Property(type="object", title="Operations on related records", description="Adding relations, removing relations, transferring records",
 	 *							@OA\Property(property="targetModule", type="string", title="The name of the target related module", example="Contacts"),
+	 *							@OA\Property(property="targetModuleLabel", type="string", title="Translated module name", example="Kontakt"),
 	 *							@OA\Property(property="targetLabel", type="string", title="The label name of the target related module", example="Jan Kowalski"),
 	 *							@OA\Property(property="targetId", type="integer", title="Id of the target related module", example=394),
 	 *						),
-	 *					),
+	 *					},
 	 *				),
 	 *			),
 	 *		),
@@ -154,24 +149,34 @@ class RecordHistory extends \Api\Core\BaseAction
 				$row['rawOwner'] = $recordModel->get('whodid') ?: 0;
 				$row['rawStatus'] = $recordModel->getStatusLabel();
 			}
-			if ($recordModel->isCreate() || $recordModel->isUpdate() || $recordModel->isTransferEdit()) {
+			if (($isCreate = $recordModel->isCreate()) || $recordModel->isUpdate() || $recordModel->isTransferEdit()) {
 				$data = [];
 				foreach ($recordModel->getFieldInstances() as $fieldModel) {
 					if ($fieldModel && ($fieldInstance = $fieldModel->getFieldInstance()) && $fieldInstance->isViewable() && 5 !== $fieldModel->getFieldInstance()->getDisplayType()) {
 						$fieldName = $fieldInstance->getName();
-						$data[$fieldName]['from'] = $fieldInstance->getUITypeModel()->getHistoryDisplayValue($fieldModel->get('prevalue'), $recordModel, true);
-						$data[$fieldName]['to'] = $fieldInstance->getUITypeModel()->getHistoryDisplayValue($fieldModel->get('postvalue'), $recordModel, true);
+						$data[$fieldName]['label'] = $fieldInstance->getFullLabelTranslation();
+						if ($isCreate) {
+							$data[$fieldName]['value'] = $fieldInstance->getUITypeModel()->getHistoryDisplayValue($fieldModel->get('postvalue'), $recordModel, true);
+						} else {
+							$data[$fieldName]['from'] = $fieldInstance->getUITypeModel()->getHistoryDisplayValue($fieldModel->get('prevalue'), $recordModel, true);
+							$data[$fieldName]['to'] = $fieldInstance->getUITypeModel()->getHistoryDisplayValue($fieldModel->get('postvalue'), $recordModel, true);
+						}
 						if ($isRawData) {
-							$data[$fieldName]['rawFrom'] = $fieldModel->get('prevalue');
-							$data[$fieldName]['rawTo'] = $fieldModel->get('postvalue');
+							if ($isCreate) {
+								$data[$fieldName]['raw'] = $fieldModel->get('postvalue');
+							} else {
+								$data[$fieldName]['rawFrom'] = $fieldModel->get('prevalue');
+								$data[$fieldName]['rawTo'] = $fieldModel->get('postvalue');
+							}
 						}
 					}
 				}
 				$row['data'] = $data;
-			} elseif ($recordModel->isRelationLink() || $recordModel->isRelationUnLink()) {
+			} elseif ($recordModel->isRelationLink() || $recordModel->isRelationUnLink() || $recordModel->isTransferLink() || $recordModel->isTransferUnLink()) {
 				$relationInstance = $recordModel->getRelationInstance();
 				$row['data'] = [
 					'targetModule' => $relationInstance->get('targetmodule'),
+					'targetModuleLabel' => \App\Language::translateSingularModuleName($relationInstance->get('targetmodule')),
 					'targetLabel' => $relationInstance->getValue(),
 				];
 				if ($isRawData) {
