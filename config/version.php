2c5c2c0716c2a4f5a7bd26708ea5f52329885166
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2021-07-07 15:18:15 +0200

    Improved conditions for quick create view

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'appVersion' => '6.2.1',
+	'appVersion' => '6.2.2',
 	'patchVersion' => '2021.07.07',
 	'lib_roundcube' => '0.1.29',
 ];
--- modules/Vtiger/views/QuickCreateAjax.php
+++ modules/Vtiger/views/QuickCreateAjax.php
@@ -134,7 +134,7 @@ class Vtiger_QuickCreateAjax_View extends Vtiger_IndexAjax_View
 				continue;
 			}
 			if (isset($this->recordStructure[$fieldName])) {
-				if ($this->fields[$fieldName]->isEditable() && '' === $this->recordStructure[$fieldName]->get('fieldvalue') || null === $this->recordStructure[$fieldName]->get('fieldvalue')) {
+				if ($this->fields[$fieldName]->isEditable() && ('' === $this->recordStructure[$fieldName]->get('fieldvalue') || null === $this->recordStructure[$fieldName]->get('fieldvalue'))) {
 					$this->recordStructure[$fieldName]->set('fieldvalue', $fieldValue);
 				}
 			} else {
