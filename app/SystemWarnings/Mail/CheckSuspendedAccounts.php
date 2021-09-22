1ad4fa6b8568dcb123557131dc9bafbeec540943
Arek Solek <arkadiusz_s9887@wp.pl>
2021-09-22 15:03:50 +0200

    Added checking of suspended email accounts in the warnings system (#15211)
    * Added checking of suspended email accounts in the warnings system
    
    * Added element li

---
+++ app/SystemWarnings/Mail/CheckSuspendedAccounts.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Checking suspended accounts file.
+ *
+ * @package App
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 4.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Arkadiusz Sołek <a.solek@yetiforce.com>
+ */
+
+namespace App\SystemWarnings\Mail;
+
+/**
+ * Checking suspended accounts class.
+ */
+class CheckSuspendedAccounts extends \App\SystemWarnings\Template
+{
+	/** @var string Modal header title */
+	protected $title = 'LBL_CHECK_SUSPENDED_ACCOUNTS';
+
+	/**
+	 * Checks for suspended email accounts.
+	 *
+	 * @return void
+	 */
+	public function process(): void
+	{
+		$data = (new \App\Db\Query())->select(['username'])->from('roundcube_users')->where(['crm_status' => 2])->column();
+		if (!$data) {
+			$this->status = 1;
+		} else {
+			$this->status = 0;
+			$userSuspendedList = '<ul>';
+			foreach ($data as $value) {
+				$userSuspendedList .= "<li> $value </li>";
+			}
+			$userSuspendedList .= '<ul>';
+			$this->description = \App\Language::translateArgs('LBL_CHECK_SUSPENDED_ACCOUNTS_DESC', 'Settings:SystemWarnings', $userSuspendedList);
+		}
+	}
+}
