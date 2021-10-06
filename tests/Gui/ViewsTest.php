3f488e991501ba63ed0fce2db9401d9125f94541
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2021-10-06 15:47:54 +0200

    Improved test

--- tests/Gui/ViewsTest.php
+++ tests/Gui/ViewsTest.php
@@ -52,7 +52,8 @@ final class Gui_ViewsTest extends \Tests\GuiBase
 			'url' => $this->driver->getCurrentURL(),
 			'search_params' => \vtlib\Functions::getQueryParams($this->driver->getCurrentURL())['search_params'],
 		];
-		static::assertSame("[[['accountname', 'a', 'YetiForce']]]", \vtlib\Functions::getQueryParams($this->driver->getCurrentURL())['search_params'] ?? []);
+		$searchParams = \vtlib\Functions::getQueryParams($this->driver->getCurrentURL())['search_params'] ?? '[]';
+		static::assertSame([[['accountname', 'a', 'YetiForce']]], \App\Json::decode($searchParams));
 		$this->findError();
 
 		$this->driver->executeScript("$('.js-change-order[data-columnname=\"accountname\"]').click()");
