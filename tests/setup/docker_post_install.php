72d54da96f7322ff93ae87ad301683cd8620a332
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-10-11 20:48:38 +0200

    tests/setup/docker_post_install.php

--- tests/setup/docker_post_install.php
+++ tests/setup/docker_post_install.php
@@ -16,19 +16,6 @@ if (file_exists($path)) {
 	file_put_contents($path, $content);
 }
 
-$path = '/etc/php/' . getenv('PHP_VER') . '/fpm/pool.d/www.conf';
-if (file_exists($path)) {
-	$conf = PHP_EOL . ';####' . PHP_EOL . ';# Best FastCGI Process Manager configuration for YetiForceCRM' . PHP_EOL . ';####' . PHP_EOL . PHP_EOL .
-	'env[PROVIDER] = docker' . PHP_EOL .
-	'php_admin_value[open_basedir] = /var/www/html/:/tmp/:/var/tmp/:/etc/nginx/ssl/:/etc/ssl/' . PHP_EOL .
-	'clear_env = no' . PHP_EOL .
-	'request_terminate_timeout = 600' . PHP_EOL .
-	'pm.max_requests = 5000' . PHP_EOL .
-	'pm.max_children = 6' . PHP_EOL .
-	'pm.process_idle_timeout = 600s;';
-	file_put_contents($path, $conf, FILE_APPEND);
-}
-
 if ('TEST' === getenv('INSTALL_MODE')) {
 	chdir(__DIR__ . '/../../');
 	require_once 'include/main/WebUI.php';
