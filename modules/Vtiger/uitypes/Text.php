2d04d5703b858ad58f1c4389d1100603e77c09a7
Adrian Koń <konadrian@wp.pl>
2021-11-12 10:25:46 +0100

    Improved getting text value for api

--- modules/Vtiger/uitypes/Text.php
+++ modules/Vtiger/uitypes/Text.php
@@ -101,8 +101,7 @@ class Vtiger_Text_UIType extends Vtiger_Base_UIType
 	/** {@inheritdoc} */
 	public function getApiDisplayValue($value, Vtiger_Record_Model $recordModel)
 	{
-		$value = \App\Utils\Completions::decode($value, \App\Utils\Completions::FORMAT_TEXT);
-		return $this->getDisplayValue($value, $recordModel->getId(), $recordModel, true, false);
+		return \App\Utils\Completions::decode($value, \App\Utils\Completions::FORMAT_TEXT);
 	}
 
 	/** {@inheritdoc} */
