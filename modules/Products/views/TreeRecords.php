9025d21f50601686c593116cd4fabe854f1a253c
Adrian Koń <konadrian@wp.pl>
2021-11-14 07:17:49 +0100

    Improved tree view in products

--- modules/Products/views/TreeRecords.php
+++ modules/Products/views/TreeRecords.php
@@ -19,36 +19,38 @@ class Products_TreeRecords_View extends Vtiger_TreeRecords_View
 		$viewer = $this->getViewer($request);
 		$filter = $request->has('filter') ? $request->getByType('filter', 'Alnum') : \App\CustomView::getInstance($baseModuleName)->getViewId();
 		$viewer->assign('VIEWID', $filter);
-		if ($request->isEmpty('branches', true) && $request->isEmpty('category', true)) {
-			return;
-		}
-		$branches = $request->getArray('branches', 'Text');
-		$category = $request->getArray('category', 'Alnum');
-
 		$moduleName = $request->getModule();
-		$viewer = $this->getViewer($request);
-		$multiReferenceFields = \Vtiger_MultiReferenceValue_UIType::getFieldsByModules($baseModuleName, $moduleName);
-		$multiReferenceFieldId = reset($multiReferenceFields);
-		if (!$multiReferenceFieldId || !($fieldInfo = \App\Field::getFieldInfo($multiReferenceFieldId))) {
-			return;
+		if ($request->isEmpty('branches', true) && $request->isEmpty('category', true)) {
+			$listEntries = $listHeaders = [];
+		} else {
+			$branches = $request->getArray('branches', 'Text');
+			$category = $request->getArray('category', 'Alnum');
+
+			$moduleName = $request->getModule();
+			$viewer = $this->getViewer($request);
+			$multiReferenceFields = \Vtiger_MultiReferenceValue_UIType::getFieldsByModules($baseModuleName, $moduleName);
+			$multiReferenceFieldId = reset($multiReferenceFields);
+			if (!$multiReferenceFieldId || !($fieldInfo = \App\Field::getFieldInfo($multiReferenceFieldId))) {
+				return;
+			}
+			$listViewModel = Vtiger_ListView_Model::getInstance($baseModuleName, $filter);
+			$queryGenerator = $listViewModel->getQueryGenerator();
+			if (!empty($branches)) {
+				$queryGenerator->addCondition($fieldInfo['fieldname'], implode('##', $branches), 'e');
+			}
+			if (!empty($category)) {
+				$query = (new \App\Db\Query())
+					->select(['crmid'])
+					->from('u_#__crmentity_rel_tree')
+					->where(['module' => App\Module::getModuleId($baseModuleName), 'relmodule' => App\Module::getModuleId($moduleName), 'tree' => $category]);
+				$queryGenerator->addNativeCondition(['in', 'vtiger_crmentity.crmid', $query], false);
+			}
+			$listEntries = $listViewModel->getAllEntries();
+			if (0 === \count($listEntries)) {
+				return;
+			}
+			$listHeaders = $listViewModel->getListViewHeaders();
 		}
-		$listViewModel = Vtiger_ListView_Model::getInstance($baseModuleName, $filter);
-		$queryGenerator = $listViewModel->getQueryGenerator();
-		if (!empty($branches)) {
-			$queryGenerator->addCondition($fieldInfo['fieldname'], implode('##', $branches), 'e');
-		}
-		if (!empty($category)) {
-			$query = (new \App\Db\Query())
-				->select(['crmid'])
-				->from('u_#__crmentity_rel_tree')
-				->where(['module' => App\Module::getModuleId($baseModuleName), 'relmodule' => App\Module::getModuleId($moduleName), 'tree' => $category]);
-			$queryGenerator->addNativeCondition(['in', 'vtiger_crmentity.crmid', $query], false);
-		}
-		$listEntries = $listViewModel->getAllEntries();
-		if (0 === \count($listEntries)) {
-			return;
-		}
-		$listHeaders = $listViewModel->getListViewHeaders();
 
 		$viewer->assign('ENTRIES', $listEntries);
 		$viewer->assign('HEADERS', $listHeaders);
