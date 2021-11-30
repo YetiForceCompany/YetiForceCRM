3e91eac7dcaab2877c2d88f6754724b0b1fec365
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-11-30 10:42:43 +0100

    lib_roundcube 0.2.2 upgrade Roundcube Webmail 1.5.1

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'appVersion' => '6.2.237',
-	'patchVersion' => '2021.11.29',
-	'lib_roundcube' => '0.2.1',
+	'appVersion' => '6.2.238',
+	'patchVersion' => '2021.11.30',
+	'lib_roundcube' => '0.2.2',
 ];
