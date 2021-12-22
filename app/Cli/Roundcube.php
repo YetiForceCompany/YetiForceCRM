696d410a3cf8af28f928060b8157a777b8e1fc5d
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-12-22 12:31:20 +0100

    Improved YetiForce CLI (Roundcube)

---
+++ app/Cli/Roundcube.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Roundcube cli file.
+ *
+ * @package Cli
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 4.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+
+namespace App\Cli;
+
+/**
+ * Roundcube cli class.
+ */
+class Roundcube extends Base
+{
+	/** {@inheritdoc} */
+	public $moduleName = 'Roundcube';
+
+	/** @var string[] Methods list */
+	public $methods = [
+		'clearUsersPreferences' => 'Clear users preferences',
+		'clearUsersCache' => 'Clear users cache',
+		'clearUsersSession' => 'Clear users session',
+	];
+
+	/**
+	 * Clear users preferences.
+	 *
+	 * @return void
+	 */
+	public function clearUsersPreferences(): void
+	{
+		\App\Db::getInstance()->createCommand()->update('roundcube_users', ['preferences' => ''])->execute();
+		if (!$this->climate->arguments->defined('action')) {
+			$this->cli->actionsList('Roundcube');
+		}
+	}
+
+	/**
+	 * Clear users cache.
+	 *
+	 * @return void
+	 */
+	public function clearUsersCache(): void
+	{
+		$createCommand = \App\Db::getInstance()->createCommand();
+		foreach (['roundcube_cache', 'roundcube_cache_index', 'roundcube_cache_messages', 'roundcube_cache_shared', 'roundcube_cache_thread'] as $table) {
+			$createCommand->truncateTable($table)->execute();
+		}
+		if (!$this->climate->arguments->defined('action')) {
+			$this->cli->actionsList('Roundcube');
+		}
+	}
+
+	/**
+	 * Clear users session.
+	 *
+	 * @return void
+	 */
+	public function clearUsersSession(): void
+	{
+		\App\Db::getInstance()->createCommand()->truncateTable('roundcube_session')->execute();
+		if (!$this->climate->arguments->defined('action')) {
+			$this->cli->actionsList('Roundcube');
+		}
+	}
+}
