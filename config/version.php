0caf62361a7771048e716d97d2409c56d4b9d994
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-12-23 10:38:01 +0100

    lib_roundcube 0.2.4 Support for skins has been added

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'appVersion' => '6.3.17',
-	'patchVersion' => '2021.12.22',
-	'lib_roundcube' => '0.2.3',
+	'appVersion' => '6.3.18',
+	'patchVersion' => '2021.12.23',
+	'lib_roundcube' => '0.2.4',
 ];
