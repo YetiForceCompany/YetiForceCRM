7e054c2158192d793f7724da1afb1a7f473d53f2
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2022-01-03 23:16:19 +0100

    roundcube 0.2.7  Added font icons

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'appVersion' => '6.3.23',
-	'patchVersion' => '2021.12.31',
-	'lib_roundcube' => '0.2.6',
+	'appVersion' => '6.3.24',
+	'patchVersion' => '2022.01.03',
+	'lib_roundcube' => '0.2.7',
 ];
