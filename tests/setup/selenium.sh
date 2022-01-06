6762d50427e44db9fad361ea636a4350197473c1
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2022-01-06 23:12:58 +0100

    Update selenium.sh

--- tests/setup/selenium.sh
+++ tests/setup/selenium.sh
@@ -10,8 +10,8 @@
 serverUrl='http://127.0.0.1:4444'
 
 # Versions
-CHROME_DRIVER_VERSION=`curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE_95`
-#CHROME_DRIVER_VERSION="95.0.4638.17"
+CHROME_DRIVER_VERSION=`curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE_97`
+#CHROME_DRIVER_VERSION="97.0.4692.71"
 SELENIUM_STANDALONE_VERSION=3.9.1
 SELENIUM_SUBDIR=$(echo "$SELENIUM_STANDALONE_VERSION" | cut -d"." -f-2)
 
