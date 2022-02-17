13a068e97298858883d4aaee46d864e549021453
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2022-02-17 09:44:51 +0100

    Improved error handling of the integration bar with the mail client

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'appVersion' => '6.3.77',
+	'appVersion' => '6.3.78',
 	'patchVersion' => '2022.02.17',
 	'lib_roundcube' => '0.2.11',
 ];
--- modules/OSSMail/views/MailActionBar.php
+++ modules/OSSMail/views/MailActionBar.php
@@ -31,41 +31,44 @@ class OSSMail_MailActionBar_View extends Vtiger_Index_View
 			OSSMail_Record_Model::setAccountUserData($rcId, ['crm_status' => OSSMail_Record_Model::MAIL_BOX_STATUS_ACTIVE]);
 			$account['crm_status'] = OSSMail_Record_Model::MAIL_BOX_STATUS_ACTIVE;
 		}
-
-		$mailViewModel = OSSMailView_Record_Model::getCleanInstance('OSSMailView');
-		$folderDecode = \App\Utils::convertCharacterEncoding($request->getRaw('folder'), 'UTF7-IMAP', 'UTF-8');
-		$folderDecode = \App\Purifier::purifyByType($folderDecode, 'Text');
-		$folderDecode = \App\Purifier::decodeHtml($folderDecode);
-		$modelMailScanner = Vtiger_Record_Model::getCleanInstance('OSSMailScanner');
-		$folder = \App\Utils::convertCharacterEncoding($folderDecode, 'UTF-8', 'UTF7-IMAP');
-		$mbox = \OSSMail_Record_Model::imapConnect($account['username'], \App\Encryption::getInstance()->decrypt($account['password']), $account['mail_host'], $folder, true, [], $account);
-		$record = $mailViewModel->checkMailExist($uid, $folderDecode, $rcId, $mbox);
-		if (!($record) && !empty($account['actions']) && false !== strpos($account['actions'], 'CreatedEmail')
-			&& isset(array_column($modelMailScanner->getFolders($rcId), 'folder', 'folder')[$folderDecode])
-		) {
-			if ($mail = OSSMail_Record_Model::getMail($mbox, $uid)) {
-				$return = OSSMailScanner_Record_Model::executeActions($account, $mail, $folderDecode, $params);
-				if (!empty($return['CreatedEmail'])) {
-					$record = $return['CreatedEmail']['mailViewId'];
+		try {
+			$mailViewModel = OSSMailView_Record_Model::getCleanInstance('OSSMailView');
+			$folderDecode = \App\Utils::convertCharacterEncoding($request->getRaw('folder'), 'UTF7-IMAP', 'UTF-8');
+			$folderDecode = \App\Purifier::purifyByType($folderDecode, 'Text');
+			$folderDecode = \App\Purifier::decodeHtml($folderDecode);
+			$modelMailScanner = Vtiger_Record_Model::getCleanInstance('OSSMailScanner');
+			$folder = \App\Utils::convertCharacterEncoding($folderDecode, 'UTF-8', 'UTF7-IMAP');
+			$mbox = \OSSMail_Record_Model::imapConnect($account['username'], \App\Encryption::getInstance()->decrypt($account['password']), $account['mail_host'], $folder, true, [], $account);
+			$record = $mailViewModel->checkMailExist($uid, $folderDecode, $rcId, $mbox);
+			if (!($record) && !empty($account['actions']) && false !== strpos($account['actions'], 'CreatedEmail')
+		&& isset(array_column($modelMailScanner->getFolders($rcId), 'folder', 'folder')[$folderDecode])
+	) {
+				if ($mail = OSSMail_Record_Model::getMail($mbox, $uid)) {
+					$return = OSSMailScanner_Record_Model::executeActions($account, $mail, $folderDecode, $params);
+					if (!empty($return['CreatedEmail'])) {
+						$record = $return['CreatedEmail']['mailViewId'];
+					}
+				} else {
+					App\Log::warning("Email not found. username: {$account['username']}, folder: $folder, uid: $uid ", __METHOD__);
 				}
-			} else {
-				App\Log::warning("Email not found. username: {$account['username']}, folder: $folder, uid: $uid ", __METHOD__);
+			} elseif ($record && !\App\Privilege::isPermitted('OSSMailView', 'DetailView', $record)) {
+				$recordModel = Vtiger_Record_Model::getInstanceById($record, $mailViewModel->getModule());
+				$sharedOwner = $recordModel->isEmpty('shownerid') ? [] : explode(',', $recordModel->get('shownerid'));
+				$sharedOwner[] = \App\User::getCurrentUserId();
+				$recordModel->set('shownerid', implode(',', $sharedOwner))->save();
 			}
-		} elseif ($record && !\App\Privilege::isPermitted('OSSMailView', 'DetailView', $record)) {
-			$recordModel = Vtiger_Record_Model::getInstanceById($record, $mailViewModel->getModule());
-			$sharedOwner = $recordModel->isEmpty('shownerid') ? [] : explode(',', $recordModel->get('shownerid'));
-			$sharedOwner[] = \App\User::getCurrentUserId();
-			$recordModel->set('shownerid', implode(',', $sharedOwner))->save();
+			$viewer = $this->getViewer($request);
+			$viewer->assign('RECORD', $record);
+			if ($record) {
+				$relatedRecords = $mailViewModel->getRelatedRecords($record);
+				$viewer->assign('RELATED_RECORDS', $relatedRecords);
+			}
+			\App\ModuleHierarchy::getModulesByLevel(0);
+			$viewer->assign('MODULE_NAME', $moduleName);
+			$viewer->assign('URL', App\Config::main('site_URL'));
+			$viewer->view('MailActionBar.tpl', $moduleName);
+		} catch (\Throwable $e) {
+			\App\Log::error($e->getMessage() . PHP_EOL . $e->__toString(), 'OSSMail');
 		}
-		$viewer = $this->getViewer($request);
-		$viewer->assign('RECORD', $record);
-		if ($record) {
-			$relatedRecords = $mailViewModel->getRelatedRecords($record);
-			$viewer->assign('RELATED_RECORDS', $relatedRecords);
-		}
-		\App\ModuleHierarchy::getModulesByLevel(0);
-		$viewer->assign('MODULE_NAME', $moduleName);
-		$viewer->assign('URL', App\Config::main('site_URL'));
-		$viewer->view('MailActionBar.tpl', $moduleName);
 	}
 }
