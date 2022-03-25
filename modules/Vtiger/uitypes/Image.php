acfec9cc6346ec4fdea25c51bffbe14a27f1ff56
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2022-03-25 13:13:22 +0100

    Added image uitype file

---
+++ modules/Vtiger/uitypes/Image.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * UIType Image.
+ *
+ * @package UIType
+ *
+ * @copyright YetiForce S.A.
+ * @license   YetiForce Public License 5.0 (licenses/LicenseEN.txt or yetiforce.com)
+ */
+/**
+ * UIType Image Field Class.
+ */
+class Vtiger_Image_UIType extends Vtiger_MultiImage_UIType
+{
+	/** {@inheritdoc} */
+	public const LIMIT = 1;
+}
