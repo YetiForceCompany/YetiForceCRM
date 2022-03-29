dec6ca04acfe94f4ec98cd3ca8e68eb89d175c09
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2022-03-29 13:02:28 +0200

    Improved SLA policy permissions

--- modules/ServiceContracts/actions/PolicyDeleteAjax.php
+++ modules/ServiceContracts/actions/PolicyDeleteAjax.php
@@ -8,14 +8,19 @@
  * @license   YetiForce Public License 5.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Rafal Pospiech <r.pospiech@yetiforce.com>
  * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ * @author    Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
  */
 class ServiceContracts_PolicyDeleteAjax_Action extends \App\Controller\Action
 {
 	/** {@inheritdoc} */
 	public function checkPermission(App\Request $request)
 	{
-		$record = Vtiger_DetailView_Model::getInstance($request->getModule(), $request->getInteger('record'));
-		if (!$record->getRecord()->isViewable() || $request->isEmpty('rowId', true)) {
+		if ($request->isEmpty('record') || $request->isEmpty('rowId', true)) {
+			throw new \App\Exceptions\NoPermittedToRecord('ERR_NO_PERMISSIONS_FOR_THE_RECORD', 406);
+		}
+		$record = Vtiger_Record_Model::getInstanceById($request->getInteger('record'), $request->getModule());
+		$userPrivilegesModel = Users_Privileges_Model::getCurrentUserPrivilegesModel();
+		if (!$record->isViewable() || !$userPrivilegesModel->hasModuleActionPermission($record->getModuleName(), 'ServiceContractsSla') || !$userPrivilegesModel->hasModulePermission($request->getByType('targetModule', \App\Purifier::ALNUM))) {
 			throw new \App\Exceptions\NoPermittedToRecord('ERR_NO_PERMISSIONS_FOR_THE_RECORD', 406);
 		}
 	}
--- modules/ServiceContracts/actions/PolicySaveAjax.php
+++ modules/ServiceContracts/actions/PolicySaveAjax.php
@@ -8,14 +8,20 @@
  * @license   YetiForce Public License 5.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Rafal Pospiech <r.pospiech@yetiforce.com>
  * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ * @author    Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
  */
 class ServiceContracts_PolicySaveAjax_Action extends \App\Controller\Action
 {
 	/** {@inheritdoc} */
 	public function checkPermission(App\Request $request)
 	{
-		$record = Vtiger_DetailView_Model::getInstance($request->getModule(), $request->getInteger('record'));
-		if (!$record->getRecord()->isViewable()) {
+		if ($request->isEmpty('record')) {
+			throw new \App\Exceptions\NoPermittedToRecord('ERR_NO_PERMISSIONS_FOR_THE_RECORD', 406);
+		}
+
+		$record = Vtiger_Record_Model::getInstanceById($request->getInteger('record'), $request->getModule());
+		$userPrivilegesModel = Users_Privileges_Model::getCurrentUserPrivilegesModel();
+		if (!$record->isViewable() || !$userPrivilegesModel->hasModuleActionPermission($record->getModuleName(), 'ServiceContractsSla') || !$userPrivilegesModel->hasModulePermission($request->getByType('target', \App\Purifier::ALNUM))) {
 			throw new \App\Exceptions\NoPermittedToRecord('ERR_NO_PERMISSIONS_FOR_THE_RECORD', 406);
 		}
 	}
