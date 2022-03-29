ce68356a31ab57ce890442130471131c59446f33
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2022-03-29 13:55:39 +0200

    Added minor improvements

---
+++ app/Conditions/RecordFields/BarcodeField.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Barcode query condition record field file.
+ *
+ * @package UIType
+ *
+ * @copyright YetiForce S.A.
+ * @license   YetiForce Public License 5.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+
+namespace App\Conditions\RecordFields;
+
+/**
+ * Barcode query condition record field class.
+ */
+class BarcodeField extends BaseField
+{
+}
