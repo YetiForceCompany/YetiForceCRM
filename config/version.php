57d763557105a161678b9b8aa8d3e95824982ff5
Sławomir Rembiesa <s.rembiesa@yetiforce.com>
2022-04-05 08:13:09 +0200

    local changes

--- config/version.php
+++ config/version.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-<<<<<<< HEAD
-	'appVersion' => '6.3.170',
-=======
 	'appVersion' => '6.3.172',
->>>>>>> d933310e71305301676343277eeff1c47c19b25c
 	'patchVersion' => '2022.04.04',
 	'lib_roundcube' => '0.2.12',
 ];
