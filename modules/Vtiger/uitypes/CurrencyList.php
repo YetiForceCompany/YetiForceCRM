772ad22214cc8c43d47f90d63858209ec27e2667
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2022-05-19 11:48:19 +0200

    Update CurrencyList.php

--- modules/Vtiger/uitypes/CurrencyList.php
+++ modules/Vtiger/uitypes/CurrencyList.php
@@ -34,9 +34,7 @@ class Vtiger_CurrencyList_UIType extends Vtiger_Picklist_UIType
 	/** {@inheritdoc} */
 	public function getDisplayValue($value, $record = false, $recordModel = false, $rawText = false, $length = false)
 	{
-		$currencyList = $this->getPicklistValues();
-
-		return !$value ? $value : \App\Purifier::encodeHtml($currencyList[$value] ?? $value);
+		return $value ? \App\Purifier::encodeHtml($this->getPicklistValues()[$value] ?? $value) :  '';
 	}
 
 	/** {@inheritdoc} */
