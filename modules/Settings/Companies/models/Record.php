4824bf5b402869199692f3df4b6b11e76e8a39d3
rembiesa <103192653+rembiesa@users.noreply.github.com>
2022-05-20 10:47:19 +0200

    PHP 8.1 & fixing display if no logo (nullable value secure) (#16069)
    * PHP 8.1 & fixing display if no logo (nullable value secure)
    
    * Update Record.php
    
    Co-authored-by: Radosław Skrzypczak <r.skrzypczak@yetiforce.com>

--- modules/Settings/Companies/models/Record.php
+++ modules/Settings/Companies/models/Record.php
@@ -143,7 +143,7 @@ class Settings_Companies_Record_Model extends Settings_Vtiger_Record_Model
 	 */
 	public function getDisplayValue(string $key)
 	{
-		$value = $this->get($key);
+		$value = $this->get($key) ?? '';
 		switch ($key) {
 			case 'type':
 				$value = \App\Language::translate(self::TYPES[$value], 'Settings::Companies');
@@ -152,7 +152,7 @@ class Settings_Companies_Record_Model extends Settings_Vtiger_Record_Model
 				$value = \App\Language::translate(\App\YetiForce\Register::STATUS_MESSAGES[(int) $value], 'Settings::Companies');
 				break;
 			case 'tabid':
-				$value = \App\Module::getModuleName($value);
+				$value = \App\Module::getModuleName((int) $value);
 				break;
 			case 'industry':
 				$value = App\Language::translate($value);
