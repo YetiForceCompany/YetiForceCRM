57f6382da513305583076d39c0e65d0888dd55c5
Sławomir Rembiesa <s.rembiesa@yetiforce.com>
2022-05-24 13:04:33 +0200

    Fixed 500 error in SaveAjax.php for RecordAllocation

--- modules/Settings/RecordAllocation/actions/SaveAjax.php
+++ modules/Settings/RecordAllocation/actions/SaveAjax.php
@@ -30,14 +30,14 @@ class Settings_RecordAllocation_SaveAjax_Action extends Settings_Vtiger_Save_Act
 			]
 		]);
 		$oldValues = Settings_RecordAllocation_Module_Model::getRecordAllocationByModule($data['type'], $data['module']);
-		$oldValues = array_merge((array) ($oldValues[$data['userid'][0]]['users'] ?? []), (array) ($oldValues[$data['userid'][0]]['groups'] ?? []));
+		$oldValues = array_merge((array) ($oldValues[$data['userid']]['users'] ?? []), (array) ($oldValues[$data['userid']]['groups'] ?? []));
 
 		$moduleInstance = Settings_Vtiger_Module_Model::getInstance($qualifiedModuleName);
 		$moduleInstance->set('type', $data['type']);
 		$moduleInstance->save(array_filter($data));
 		Settings_RecordAllocation_Module_Model::resetDataVariable();
 		$newValues = Settings_RecordAllocation_Module_Model::getRecordAllocationByModule($data['type'], $data['module']);
-		$newValues = array_merge((array) ($newValues[$data['userid'][0]]['users'] ?? []), (array) ($newValues[$data['userid'][0]]['groups'] ?? []));
+		$newValues = array_merge((array) ($newValues[$data['userid']]['users'] ?? []), (array) ($newValues[$data['userid']]['groups'] ?? []));
 		$prevDetail['userId'] = implode(',', $oldValues);
 		$newDetail['userId'] = implode(',', $newValues);
 
