77902759b0ad936758c0456f436b1c101dad7ea3
konadrian <konadrian@wp.pl>
2022-05-25 15:11:52 +0200

    Revert changes (#16100)

--- public_html/layouts/basic/modules/Settings/LayoutEditor/resources/LayoutEditor.js
+++ public_html/layouts/basic/modules/Settings/LayoutEditor/resources/LayoutEditor.js
@@ -402,7 +402,7 @@ $.Class(
 				params['inventory'] = true;
 				selectedFields = $(target).val();
 			} else {
-				selectedFields = array_keys(thisInstance.getSelectedFields($(target)));
+				selectedFields = thisInstance.getSelectedFields($(target));
 			}
 			params['module'] = app.getModuleName();
 			params['parent'] = app.getParentModuleName();
