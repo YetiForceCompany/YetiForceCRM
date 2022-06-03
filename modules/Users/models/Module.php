48919090d67cbe8b1fe5fb230d3aa713554568e8
Sławomir Rembiesa <s.rembiesa@yetiforce.com>
2022-06-03 13:57:59 +0200

    Change verifing function to isExists.

--- modules/Users/models/Module.php
+++ modules/Users/models/Module.php
@@ -223,7 +223,7 @@ class Users_Module_Model extends Vtiger_Module_Model
 		$users = $userIds = [];
 		if (isset($switchUsers[$baseUserId])) {
 			foreach ($switchUsers[$baseUserId] as $userId => &$userName) {
-				if (!self::checkUserIsActive($userId)) {
+				if (!\App\User::isExists($userId)) {
 					continue;
 				}
 				$users[$userId] = ['userName' => $userName];
@@ -264,14 +264,5 @@ class Users_Module_Model extends Vtiger_Module_Model
 			$editFields[] = $field['fieldname'];
 		}
 		return $editFields;
-	}
-
-	private static function checkUserIsActive(int $userId)
-	{
-		$userStatus = (new \App\Db\Query())->select(['vtiger_users.status'])->from('vtiger_users')->where(['id' => $userId])->createCommand()->query();
-		if ('Active' === $userStatus->readAll()[0]['status']) {
-			return true;
-		}
-		return false;
 	}
 }
