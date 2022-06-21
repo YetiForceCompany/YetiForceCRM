be319245b532034eef46e41776aec5079248a4f6
Sławomir Rembiesa <s.rembiesa@yetiforce.com>
2022-06-21 11:35:35 +0200

    Add integration with MF in CheckBankAccount.php

---
+++ app/RecordCollectors/CheckBankAccount.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Check Bank Accounts Numbers record collector file.
+ *
+ * @package App
+ *
+ * @see https://wl-test.mf.gov.pl/
+ *
+ * @copyright YetiForce S.A.
+ * @license   YetiForce Public License 5.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Sławomir Rembiesa <s.rembiesa@yetiforce.com>
+ */
+
+namespace App\RecordCollectors;
+
+/**
+ * Check Bank Accounts Numbers record collector class.
+ */
+class CheckBankAccount extends Base
+{
+	/** {@inheritdoc} */
+	protected static $allowedModules = ['Accounts'];
+
+	/** {@inheritdoc} */
+	public $icon = 'fa-solid fa-building-columns';
+
+	/** {@inheritdoc} */
+	public $label = 'LBL_POLAND_CHECK_BANK_ACCOUNT';
+
+	/** {@inheritdoc} */
+	public $displayType = 'Summary';
+
+	/** {@inheritdoc} */
+	public $description = 'LBL_POLAND_CHECK_BANK_ACCOUNT';
+
+	/** {@inheritdoc} */
+	protected $fields = [
+		'vatNumber' => [
+			'labelModule' => '_Base',
+			'label' => 'Vat ID',
+			'typeofdata' => 'V~M',
+		],
+	];
+
+	/** {@inheritdoc} */
+	protected $modulesFieldsMap = [
+		'Accounts' => [
+			'vatNumber' => 'vat_id',
+		]
+	];
+
+	/** @var string MF sever address */
+	protected $url = 'https://wl-test.mf.gov.pl/';
+
+	/** {@inheritdoc} */
+	public function search(): array
+	{
+		$response = [];
+		$vatNumber = str_replace([' ', ',', '.', '-'], '', $this->request->getByType('vatNumber', 'Text'));
+		try {
+			$response = (new \GuzzleHttp\Client(\App\RequestHttp::getOptions()))->request('GET', $this->url . 'api/search/nip/' . $vatNumber, [
+				'verify' => false,
+				'query' => [
+					'date' => date('Y-m-d'),
+				]
+			]);
+		} catch (\GuzzleHttp\Exception\ClientException $e) {
+			\App\Log::warning($e->getMessage(), 'RecordCollectors');
+			$response['error'] = $e->getMessage();
+		}
+		//todo
+		return [];
+	}
+}
