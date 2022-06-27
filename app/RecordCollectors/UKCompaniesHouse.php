61db7addfe8f8c4b43f3f869fe3f35cc225d814f
Sławomir Rembiesa <s.rembiesa@yetiforce.com>
2022-06-27 13:08:51 +0200

    Api improve

--- app/RecordCollectors/UKCompaniesHouse.php
+++ app/RecordCollectors/UKCompaniesHouse.php
@@ -150,7 +150,7 @@ class UKCompaniesHouse extends Base
 	{
 		try {
 			$response = (new \GuzzleHttp\Client(\App\RequestHttp::getOptions()))->request('GET', $this->url . 'company/' . $ncr, [
-				'auth' => ['8f27bb27-4af7-44c7-ad71-ed59d5c15ab2', ''],
+				'auth' => ['API_KEY_HERE', ''],
 			]);
 		} catch (\GuzzleHttp\Exception\ClientException $e) {
 			\App\Log::warning($e->getMessage(), 'RecordCollectors');
