9aa78371b4926d2e9ac4a8920d02702ad43115fd
Sławomir Rembiesa <s.rembiesa@yetiforce.com>
2022-06-30 08:14:16 +0200

    Add messages to List.js

--- public_html/layouts/basic/modules/Settings/RecordCollector/resources/List.js
+++ public_html/layouts/basic/modules/Settings/RecordCollector/resources/List.js
@@ -24,12 +24,33 @@ $.Class(
 				});
 			});
 		},
+		/**
+		 * register function for showing config modal.
+		 */
 		registerConfigModal: function () {
 			$('.js-show-config-modal').on('click', function () {
 				const recordCollectorName = this.dataset.name;
 				app.showModalWindow(null, 'index.php?module=RecordCollector&parent=Settings&view=ConfigModal&recordCollectorName=' + recordCollectorName,  function (modal) {
 					modal.on('click', ".js-modal__save", function() {
-						//todo
+						let form = modal.find('form');
+						AppConnector.request({
+							module: 'RecordCollector',
+							parent: 'Settings',
+							action: 'SaveConfig',
+							collector: recordCollectorName,
+							config: form.serializeFormData(),
+						}).done((data) => {
+							app.showNotify({
+								type: 'success',
+								text: data.result.message
+							});
+							app.hideModalWindow();
+						}).fail((data) => {
+							app.showNotify({
+								type: 'error',
+								text: data
+							});
+						});
 					})
 				});
 			});
