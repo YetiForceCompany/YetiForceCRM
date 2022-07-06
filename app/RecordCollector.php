1e57b28c76278d5fd218f0cfbda3797c68d29afc
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2022-07-06 16:55:15 +0200

    Update app/RecordCollector.php

--- app/RecordCollector.php
+++ app/RecordCollector.php
@@ -40,7 +40,7 @@ class RecordCollector
 	 * @param string $displayType
 	 * @param string $moduleName
 	 *
-	 * @return array
+	 * @return RecordCollectors\Base
 	 */
 	public static function getAllByType(string $displayType, string $moduleName): array
 	{
