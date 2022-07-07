e26cee05693a218e1823024f5f16a81593d1f159
Adrian Koń <a.kon@yetiforce.com>
2022-07-07 09:22:11 +0200

    Added possibility to narrow results in list view (#16242)
    * Added possibility to narrow results in list view
    
    * Added Improvements
    
    * Added improvements
    
    * Improved method description

--- app/Conditions/QueryFields/DoubleField.php
+++ app/Conditions/QueryFields/DoubleField.php
@@ -10,7 +10,35 @@ namespace App\Conditions\QueryFields;
  * @copyright YetiForce S.A.
  * @license   YetiForce Public License 5.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Tomasz Kur <t.kur@yetiforce.com>
+ * @author    Adrian Kon <a.kon@yetiforce.com>
  */
 class DoubleField extends IntegerField
 {
+	/**
+	 * Auto operator, it allows you to use formulas: >10 <40, >1, <7.
+	 *
+	 * @return array
+	 */
+	public function operatorA(): array
+	{
+		$value = \App\Purifier::decodeHtml($this->value);
+		$condition = ['and'];
+		$conditionFound = false;
+		$explodeBySpace = explode(' ', $value);
+		foreach ($explodeBySpace as $valueToCondition) {
+			$operatorWasFound = false;
+			foreach (static::$extendedOperators as $exo) {
+				if (false !== strpos($valueToCondition, $exo) && false === $operatorWasFound) {
+					$ev = explode($exo, $valueToCondition);
+					$condition[] = [$exo, $this->getColumnName(),  $ev[1]];
+					$conditionFound = true;
+					$operatorWasFound = true;
+				}
+			}
+		}
+		if (!$conditionFound) {
+			return parent::operatorE();
+		}
+		return $condition;
+	}
 }
