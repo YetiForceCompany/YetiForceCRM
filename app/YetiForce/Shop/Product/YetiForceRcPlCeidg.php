502329e47b648b2a1732172fd01f230cca9273e3
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2022-07-22 13:40:48 +0200

    Improvements in the store

--- app/YetiForce/Shop/Product/YetiForceRcPlCeidg.php
+++ app/YetiForce/Shop/Product/YetiForceRcPlCeidg.php
@@ -4,6 +4,8 @@
  *
  * @package App
  *
+ * @see App\RecordCollectors\PlCeidg
+ *
  * @copyright YetiForce S.A.
  * @license   YetiForce Public License 5.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
--- app/YetiForce/Shop/Product/YetiForceRcPlVatPayerStatus.php
+++ app/YetiForce/Shop/Product/YetiForceRcPlVatPayerStatus.php
@@ -4,6 +4,8 @@
  *
  * @package App
  *
+ * @see App\RecordCollectors\PlVatPayerStatusVerification
+ *
  * @copyright YetiForce S.A.
  * @license   YetiForce Public License 5.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
