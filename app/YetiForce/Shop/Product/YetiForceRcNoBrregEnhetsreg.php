c216ad8a84a9c24d877c23a75a25a94fda87d4bd
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2022-08-01 14:20:40 +0200

    Added improvements in record collector

--- app/YetiForce/Shop/Product/YetiForceRcNoBrregEnhetsreg.php
+++ app/YetiForce/Shop/Product/YetiForceRcNoBrregEnhetsreg.php
@@ -2,9 +2,9 @@
 /**
  * YetiForce shop YetiForceRcNoBrregEnhetsreg file.
  *
- * @package App
- *
  * @see App\RecordCollectors\NoBrregEnhetsregisteret
+ *
+ * @package App
  *
  * @copyright YetiForce S.A.
  * @license   YetiForce Public License 5.0 (licenses/LicenseEN.txt or yetiforce.com)
--- app/YetiForce/Shop/Product/YetiForceRcUsaEdgar.php
+++ app/YetiForce/Shop/Product/YetiForceRcUsaEdgar.php
@@ -2,9 +2,9 @@
 /**
  * YetiForce shop YetiForceRcUsaEdgar file.
  *
- * @package App
- *
  * @see App\RecordCollectors\UsaEdgarRegistryFromSec
+ *
+ * @package App
  *
  * @copyright YetiForce S.A.
  * @license   YetiForce Public License 5.0 (licenses/LicenseEN.txt or yetiforce.com)
