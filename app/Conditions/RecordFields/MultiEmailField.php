2566868dd8a5230c9e2fa03b0c8c0eef4916cf36
Arek Solek <arkadiusz_s9887@wp.pl>
2023-03-13 15:42:21 +0100

    Added condition to multi email field (#16921)
    * Added condition to multi email field

--- app/Conditions/RecordFields/MultiEmailField.php
+++ app/Conditions/RecordFields/MultiEmailField.php
@@ -16,4 +16,40 @@ namespace App\Conditions\RecordFields;
  */
 class MultiEmailField extends BaseField
 {
+	/** @var string Separator. */
+	protected $separator = ',';
+
+	/** {@inheritdoc} */
+	public function getValue(): array
+	{
+		$value = [];
+		if (!empty(parent::getValue()) && \in_array($this->operator, ['e', 'n'])) {
+			$value = array_map(fn ($email) => $email['e'], \App\Json::decode(parent::getValue()));
+		}
+		return $value;
+	}
+
+	/** {@inheritdoc} */
+	public function operatorE(): bool
+	{
+		return (bool) array_intersect($this->getValue(), explode($this->separator, $this->value));
+	}
+
+	/** {@inheritdoc} */
+	public function operatorN(): bool
+	{
+		return (bool) !array_intersect($this->getValue(), explode($this->separator, $this->value));
+	}
+
+	/** {@inheritdoc} */
+	public function operatorC(): bool
+	{
+		return $this->operatorE();
+	}
+
+	/** {@inheritdoc} */
+	public function operatorK(): bool
+	{
+		return $this->operatorN();
+	}
 }
