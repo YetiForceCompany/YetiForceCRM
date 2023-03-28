f7b3c37a3adb2c6b29278789810899b88c35b15a
Arek Solek <arkadiusz_s9887@wp.pl>
2023-03-28 07:23:16 +0200

    Corrected condition for multi domain field (#16985)

--- app/Conditions/RecordFields/MultiDomainField.php
+++ app/Conditions/RecordFields/MultiDomainField.php
@@ -16,9 +16,34 @@ namespace App\Conditions\RecordFields;
  */
 class MultiDomainField extends BaseField
 {
+	/** @var string Separator. */
+	protected $separator = ',';
+
 	/** {@inheritdoc} */
-	public function getValue()
+	public function getValue(): array
 	{
-		return trim(parent::getValue(), ',');
+		$value = [];
+		if (!empty(parent::getValue()) && \in_array($this->operator, ['e', 'n'])) {
+			$value = explode($this->separator, trim(parent::getValue(), ','));
+		}
+		return $value;
+	}
+
+	/** {@inheritdoc} */
+	public function operatorE(): bool
+	{
+		return (bool) array_intersect($this->getValue(), explode($this->separator, $this->value));
+	}
+
+	/** {@inheritdoc} */
+	public function operatorN(): bool
+	{
+		return (bool) !array_intersect($this->getValue(), explode($this->separator, $this->value));
+	}
+
+	/** {@inheritdoc} */
+	public function operatorC(): bool
+	{
+		return $this->operatorE();
 	}
 }
