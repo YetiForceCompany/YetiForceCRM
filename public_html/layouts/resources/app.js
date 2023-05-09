1e885519a3157c495c015efed0c03aab6e36d57f
Arek Solek <arkadiusz_s9887@wp.pl>
2023-05-09 14:37:19 +0200

    Centered modal window. (#17062)
    * Centered modal window.
    
    * Coccrected commit

--- public_html/layouts/resources/app.js
+++ public_html/layouts/resources/app.js
@@ -1674,6 +1674,7 @@ const app = (window.app = {
 					}, this)
 				);
 		};
+
 		const modalContainer = container.find('.modal:first');
 		modalContainer.one('shown.bs.modal', function () {
 			thisInstance.registerDataTables(modalContainer.find('.js-modal-data-table'));
@@ -1699,6 +1700,7 @@ const app = (window.app = {
 		modalContainer.modal(params);
 		app.registerFormsEvents(modalContainer);
 		thisInstance.registerModalEvents(modalContainer, sendByAjaxCb);
+		app.registerModalPosition(modalContainer);
 	},
 	showModalWindow: function (data, url, cb, paramsObject = {}) {
 		if (!app.isCurrentWindowTarget('app.showModalWindow', arguments)) {
@@ -1762,6 +1764,7 @@ const app = (window.app = {
 				$('body').addClass('modal-open');
 			}
 		});
+
 		Window.lastModalId = modalId;
 		if (data) {
 			thisInstance.showModalData(data, container, paramsObject, cb, url, sendByAjaxCb);
@@ -3802,8 +3805,25 @@ const app = (window.app = {
 		container.on('click', '.js-print-container', function (_) {
 			app.printModal($($(this).data('container')).children());
 		});
+	},
+
+	/**
+	 * Sets the position of the modal window.
+	 * @param {jQuery} container
+	 */
+	registerModalPosition: function (container) {
+		if (CONFIG.centerModalWindow) {
+			let modalContent = container.find('.modal-dialog');
+			modalContent.css({ 'margin-top': '0px', 'margin-bottom': '0px' });
+			let modalHeight = modalContent.height();
+			if (app.getScreenHeight() > modalHeight) {
+				let difference = app.getScreenHeight() - modalHeight;
+				modalContent.css('top', difference / 2);
+			}
+		}
 	}
 });
+
 $(function () {
 	Quasar.iconSet.set(Quasar.iconSet.mdiV3);
 	let document = $(this);
