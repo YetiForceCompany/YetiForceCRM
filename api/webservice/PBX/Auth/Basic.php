14459c7b04b61934cf0e3f8fa0064b0f562d35a9
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2023-05-17 17:44:28 +0200

    Added minor improvements in PBX

--- api/webservice/PBX/Auth/Basic.php
+++ api/webservice/PBX/Auth/Basic.php
@@ -32,9 +32,19 @@ class Basic extends \Api\Core\Auth\Basic
 	/** {@inheritdoc}  */
 	public function authenticate(string $realm): bool
 	{
-		$apiKey = $this->api->request->getHeaders()['x-api-key'] ?? null;
-		if (!$apiKey || $apiKey !== \App\Encryption::getInstance()->decrypt($this->api->app['api_key'])) {
-			throw new \Api\Core\Exception('Invalid api key', 401);
+		$apiKey = \App\Encryption::getInstance()->decrypt($this->api->app['api_key']);
+		$headers = $this->api->request->getHeaders();
+		if (!empty($headers['x-api-key'])) {
+			if ($headers['x-api-key'] !== $apiKey) {
+				throw new \Api\Core\Exception('Invalid api key', 401);
+			}
+		} elseif (!empty($headers['x-api-signature'])) {
+			$sig = base64_encode(hash_hmac('sha256', file_get_contents('php://input'), $apiKey, true));
+			if ($sig !== $headers['x-api-signature']) {
+				throw new \Api\Core\Exception('Invalid signature', 401);
+			}
+		} else {
+			throw new \Api\Core\Exception('No API key or signature', 401);
 		}
 		return true;
 	}
--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'appVersion' => '6.4.223',
+	'appVersion' => '6.4.224',
 	'patchVersion' => '2023.05.17',
 	'lib_roundcube' => '0.3.4',
 ];
