eadcdcc9b23c17f01145be85ee0d798e433be2f0
Fabian Ferkaluk <f.ferkaluk@yetiforce.com>
2023-07-06 15:18:09 +0200

    Changes in unit tests - MapCordinates

--- tests/App/Fields/MapCoordinates.php
+++ tests/App/Fields/MapCoordinates.php
@@ -23,14 +23,14 @@ class MapCoordinates extends \Tests\Base
 	{
 		return [
 			['decimal', 'decimal', ['lat' => 52.23155431436567, 'lon' => 21.00499528120955], ['lat' => 52.23155431436567, 'lon' => 21.00499528120955]],
-			['decimal', 'degrees', ['lat' => 52.23155431436567, 'lon' => 21.00499528120955], ['lat' => '52°13\'53.5955"N', 'lon' => '21°0\'17.983"E']],
-			['decimal', 'degrees', ['lat' => 30, 'lon' => 33.3], ['lat' => '33°N', 'lon' => '33°18\'E']],
+			['decimal', 'degrees', ['lat' => 52.23155431436567, 'lon' => 21.00499528120955], ['lat' => '52°13\'53.5955"N', 'lon' => '21°00\'17.983"E']],
+			['decimal', 'degrees', ['lat' => 30, 'lon' => 33.3], ['lat' => '30°N', 'lon' => '33°18\'E']],
 			['degrees', 'decimal', ['lat' => 'N 50°12\'13.1188"', 'lon' => 'E 21°0\'17.983"'], ['lat' => 50.203644111111, 'lon' => 21.004995277778]],
 			['degrees', 'decimal', ['lat' => 'N50°12\'13.1188"', 'lon' => 'E21°0\'17.983"'], ['lat' => 50.203644111111, 'lon' => 21.004995277778]],
 			['degrees', 'decimal', ['lat' => '50°12\'13.1188" N', 'lon' => '21°0\'17.983" E'], ['lat' => 50.203644111111, 'lon' => 21.004995277778]],
 			['degrees', 'decimal', ['lat' => '50°12\'13.1188"N', 'lon' => '21°0\'17.983"E'], ['lat' => 50.203644111111, 'lon' => 21.004995277778]],
 			['decimal', 'codeplus', ['lat' => 52.23155431436567, 'lon' => 21.00499528120955], '9G4362J3+JXH5'],
-			['codeplus', 'decimal', '9G4362J3+GR', ['lat' => 52.2313125, 'lon' => 21.0045625]],
+			['codeplus', 'decimal', '9G4362J3+GR', ['lat' => 52.23131249999999, 'lon' => 21.004562500000006]],
 		];
 	}
 
@@ -48,7 +48,7 @@ class MapCoordinates extends \Tests\Base
 	 */
 	public function testConvert(string $from, string $to, $value, $result): void
 	{
-		$this->assertSame($result, \App\Fields\MapCoordinates::convert($from, $to, $value));
+		$this->assertEquals($result, \App\Fields\MapCoordinates::convert($from, $to, $value));
 	}
 
 	/**
